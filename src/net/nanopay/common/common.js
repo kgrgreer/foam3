1ebe248f7eb506db0539aa6c9de598024e0bc5cc
Kirk Eaton <kirk@nanopay.net>
2017-08-22 12:30:41 -0400

    Moved common back to net/nanopay/common

---
+++ src/net/nanopay/common/common.js
@@ -0,0 +1,13 @@
+FOAM_FILES([
+  { name: 'model/Account' },
+  { name: 'model/AccountInfo' },
+  { name: 'model/AccountLimit' },
+  { name: 'model/Address' },
+  { name: 'model/BankAccount' },
+  { name: 'model/BankAccountInfo' },
+  { name: 'model/PadAccount' },
+  { name: 'model/Phone' },
+  { name: 'model/User' },
+  { name: 'model/UserAccountInfo' },
+  { name: 'ui/SignIn' }
+]);
