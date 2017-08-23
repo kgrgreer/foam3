9c565e21e01b87158f02b5547ec370b1fd50e50d
Kirk Eaton <kirk@nanopay.net>
2017-08-23 08:45:51 -0400

    Added top level gen.sh script that runs all the other ones

---
+++ gen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd admin-portal/src && ./gen.sh && cd ../..
+cd b2b/src && ./gen.sh && cd ../..
+cd common/src && ./gen.sh && cd ../..
+cd ingenico/src && ./gen.sh && cd ../..
+cd retail/src && ./gen.sh && cd ../..
