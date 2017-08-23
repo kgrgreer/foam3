cf4638ee0b8b6b8b152de1f3d911740c7d22889f
Kirk Eaton <kirk@nanopay.net>
2017-08-23 08:32:54 -0400

    Added empty classes.js files and gen.sh files to ingenico and admin-portal projects

---
+++ admin-portal/classes.js
@@ -0,0 +1,22 @@
+var classes = [
+
+];
+
+var abstractClasses = [
+
+];
+
+var skeletons = [
+
+];
+
+var proxies = [
+
+];
+
+module.exports = {
+    classes: classes,
+    abstractClasses: abstractClasses,
+    skeletons: skeletons,
+    proxies: proxies
+}
