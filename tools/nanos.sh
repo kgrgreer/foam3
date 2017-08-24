729c0678abb9948b3bb2e795dd6a8a5f5f60c4f2
Patrick <zahnno@gmail.com>
2017-08-24 14:57:32 -0400

    client dao on invoiceresolutions

--- tools/nanos.sh
+++ tools/nanos.sh
@@ -3,4 +3,4 @@
 cd NANOPAY/
 mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
 cd ../
-java -cp `cat foam2/build/cp.txt`:`cat NANOPAY/**/cp.txt` foam.nanos.boot.Boot
+java -cp `cat foam2/build/cp.txt`:`cat NANOPAY/**/cp.txt`:NANOPAY/b2b/target/b2b-0.0.1.jar foam.nanos.boot.Boot
