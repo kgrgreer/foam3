ab28a5764c4f1675c835c66f99304cb5b1497649
Tanay Parikh <me@tanayparikh.com>
2017-08-25 15:38:35 -0400

    Refactored ChipView to FOAM

--- admin-portal/src/net/nanopay/admin-portal/ui/user/SendMoneyView.js
+++ admin-portal/src/net/nanopay/admin-portal/ui/user/SendMoneyView.js
@@ -102,7 +102,7 @@ foam.CLASS({
                 .addClass('tag-container')
                 .forEach(labels, function(label) {
                     this.tag({
-                      class: 'net.nanopay.admin.ui.shared.ChipView',
+                      class: 'foam.u2.view.ChipView',
                       data: label
                     })
                 });
