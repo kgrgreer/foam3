d62607c14246c643a5fe9a475a9b338efd07538c
marcogbarcellos <marcogbarcellos@gmail.com>
2017-09-07 12:36:46 -0400

    adding PMs to interac models and exporting models on classes.js

--- interac/classes.js
+++ interac/classes.js
@@ -1,18 +1,24 @@
 var classes = [
+  'net.nanopay.interac.model.Payee',
+  'net.nanopay.interac.model.CountryAgent',
+  'net.nanopay.interac.model.Identification'
 ];
 
 var abstractClasses = [
+
 ];
 
 var skeletons = [
+
 ];
 
 var proxies = [
+
 ];
 
 module.exports = {
-    classes: classes,
-    abstractClasses: abstractClasses,
-    skeletons: skeletons,
-    proxies: proxies
+  classes: classes,
+  abstractClasses: abstractClasses,
+  skeletons: skeletons,
+  proxies: proxies
 }
