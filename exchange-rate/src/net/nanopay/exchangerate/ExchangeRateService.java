c5bafb42c8e0c0d84e39d9f629972e94b2518df0
William Dias <wrddias@gmail.com>
2017-09-07 00:01:45 -0400

    Fix compilation errors for exchange service

--- exchange-rate/src/net/nanopay/exchangerate/ExchangeRateService.java
+++ exchange-rate/src/net/nanopay/exchangerate/ExchangeRateService.java
@@ -1,46 +1,54 @@
 package net.nanopay.exchangerate;
 
 import foam.core.ContextAwareSupport;
+import foam.core.Detachable;
 import foam.core.FObject;
 import foam.dao.*;
-import foam.mlang.MLang.*;
+import foam.mlang.MLang;
 import net.nanopay.exchangerate.model.ExchangeRate;
 import net.nanopay.exchangerate.model.ExchangeRateQuote;
 
+import java.net.*;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.json.simple.JSONParser;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class ExchangeRateService
   extends ContextAwareSupport
-  implements ExchangeRate
+  implements ExchangeRateInterface
 {
   protected DAO exchangeRateDAO_;
+  protected Integer feeAmount = 150;
 
   @Override
   public ExchangeRateQuote getRate(String from, String to, Long amount)
     throws RuntimeException
   {
-    ArraySink a = (ArraySink) exchangeRateDAO_.where(
-      AND(
-        EQ(ExchangeRate.FROM, from),
-        EQ(ExchangeRate.TO, to)
-      )
-    ).limit(1).select();
-
-
     ExchangeRateQuote quote = new ExchangeRateQuote();
 
-    quote.setExchangeRateId(a.id);
     quote.setFromCurrency(from);
     quote.setToCurrency(to);
     quote.setFromAmount(amount);
-    quote.setToAmount(amount * a.rate);
-    quote.setRate(a.rate);
-    quote.setFeesAmount(1);
-    quote.setFeesPercentage(1);
-    quote.setExpirationDate(a.expirationDate);
+
+    exchangeRateDAO_.where(
+      MLang.AND(
+        MLang.EQ(ExchangeRate.FROM_CURRENCY, from),
+        MLang.EQ(ExchangeRate.TO_CURRENCY, to)
+      )
+    ).select(new AbstractSink() {
+      @Override
+      public void put(FObject obj, Detachable sub) {
+        quote.setExchangeRateId(((ExchangeRate) obj).getId());
+        quote.setToAmount(amount * ((ExchangeRate) obj).getRate());
+        quote.setRate(((ExchangeRate) obj).getRate());
+        quote.setExpirationDate(((ExchangeRate) obj).getExpirationDate());
+        quote.setFeesAmount(feeAmount);
+        quote.setFeesPercentage(feeAmount / amount);
+      }
+    });
 
     return quote;
   }
@@ -48,17 +56,17 @@ public class ExchangeRateService
   public void fetchRates()
       throws RuntimeException
   {
-    URLConnection connection = new URL("http://api.fixer.io/latest?base=CAD").openStream();
-    connection.setRequestProperty("Accept-Charset", "UTF-8");
-    InputStream response = connection.getInputStream();
+    // URLConnection connection = new URL("http://api.fixer.io/latest?base=CAD").openStream();
+    // connection.setRequestProperty("Accept-Charset", "UTF-8");
+    // InputStream response = connection.getInputStream();
 
-    JSONParser jsonParser = new JSONParser();
+    // JSONParser jsonParser = new JSONParser();
 
-    JSONObject parsedResponse = (JSONObject)jsonParser.parse(
-      new InputStreamReader(response, "UTF-8")
-    );
+    // JSONObject parsedResponse = (JSONObject)jsonParser.parse(
+    //   new InputStreamReader(response, "UTF-8")
+    // );
 
-    System.out.println(parsedResponse.toString());
+    // System.out.println(parsedResponse.toString());
   }
 
   @Override
