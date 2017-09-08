7517165c4fb4caad142f8734547984c5565c357d
Kirk Eaton <kirk@nanopay.net>
2017-09-08 13:02:59 -0400

    Removed test select statement from accountDAO

--- interac/src/net/nanopay/interac/ui/Controller.js
+++ interac/src/net/nanopay/interac/ui/Controller.js
@@ -83,10 +83,6 @@ foam.CLASS({
 
       net.nanopay.interac.Data.create(undefined, this);
 
-      this.accountDAO.select().then(function (a) {
-        console.log('array =', a.array);
-      })
-
       // var message = this.iso20022.GENERATE_PACS008_MESSAGE(1).then(function (message) {
       //   if ( ! message ) return;
       //   console.log(message);
