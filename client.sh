cdfd3e0a7982d5cad591fd5a47d58bf04b12e5ca
Waseem <waseem@nanopay.net>
2017-09-08 13:31:56 -0400

    created test files

--- client.sh
+++ client.sh
@@ -5,34 +5,22 @@ set -e
 
 cd ..
 find foam2/src NANOPAY/**/src -type f -name countries -exec cat {} \; > countries
+find foam2/src NANOPAY/**/src -type f -name countryAgents -exec cat {} \; > countryAgents
 find foam2/src NANOPAY/**/src -type f -name crons -exec cat {} \; > crons
 find foam2/src NANOPAY/**/src -type f -name exchangerate -exec cat {} \; > exchangerate
 find foam2/src NANOPAY/**/src -type f -name exportDriverRegistrys -exec cat {} \; > exportDriverRegistrys
 find foam2/src NANOPAY/**/src -type f -name groups -exec cat {} \; > groups
+find foam2/src NANOPAY/**/src -type f -name invoiceResolutions -exec cat {} \; > invoiceResolutions
 find foam2/src NANOPAY/**/src -type f -name languages -exec cat {} \; > languages
 find foam2/src NANOPAY/**/src -type f -name menus -exec cat {} \; > menus
+find foam2/src NANOPAY/**/src -type f -name payee -exec cat {} \; > payee
 find foam2/src NANOPAY/**/src -type f -name permissions -exec cat {} \; > permissions
 find foam2/src NANOPAY/**/src -type f -name regions -exec cat {} \; > regions
 find foam2/src NANOPAY/**/src -type f -name script -exec cat {} \; > script
-find foam2/src NANOPAY/**/src -type f -name tests -exec cat {} \; > tests
 find foam2/src NANOPAY/**/src -type f -name services -exec cat {} \; > services
+find foam2/src NANOPAY/**/src -type f -name transactions -exec cat {} \; > transactions
+find foam2/src NANOPAY/**/src -type f -name tests -exec cat {} \; > tests
 find foam2/src NANOPAY/**/src -type f -name users -exec cat {} \; > users
 
-cd foam2/src
-./gen.sh
 
-cd ../build
-mvn compile package
-mvn install:install-file -Dfile="target/foam-1.0-SNAPSHOT.jar" -DgroupId=com.google -DartifactId=foam -Dversion=1.0 -Dname=foam -Dpackaging=jar
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
-
-cd ../../NANOPAY
-./gen.sh
-
-# NANOPAY doesn't all build successfully at the moment
-set +e
-mvn clean install
-set -e
-
-cd ..
 ./NANOPAY/tools/nanos.sh
