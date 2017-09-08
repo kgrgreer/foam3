fb6eae9d5e4bde46e144be66b769876aa45f658e
Patrick <zahnno@gmail.com>
2017-09-08 14:34:20 -0400

    styling change

--- b2b/src/net/nanopay/b2b/model/Invoice.js
+++ b2b/src/net/nanopay/b2b/model/Invoice.js
@@ -105,7 +105,7 @@ foam.CLASS({
       aliases: [ 'a' ],
       required: true,
       tableCellFormatter: function(a) {
-        this.start().style({'text-align': 'right', 'padding-right': '20px'}).add('$' + a.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')).end();
+        this.start().style({'padding-right': '20px'}).add('$' + a.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')).end();
       }
     },
     {
