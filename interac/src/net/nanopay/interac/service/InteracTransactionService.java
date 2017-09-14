2b00a4a5e62da2976296296a74a19baac56f8993
MarcRoopchand <marc.roopchand@gmail.com>
2017-09-14 15:38:22 -0400

    Added basic validation for transaction object

--- interac/src/net/nanopay/interac/service/InteracTransactionService.java
+++ interac/src/net/nanopay/interac/service/InteracTransactionService.java
@@ -1,15 +1,21 @@
 package net.nanopay.interac;
 
+import foam.dao.DAO;
 import net.nanopay.interac.service.InteracService;
 import net.nanopay.tx.model.Transaction;
 import net.nanopay.tx.TransactionService;
 import foam.core.ContextAwareSupport;
+
+import java.util.Random;
+import java.util.UUID;
 
 public class InteracTransactionService
   extends ContextAwareSupport
   implements InteracService
 {
   protected TransactionService service;
+  protected DAO canadianTransactionDAO;
+  protected DAO indiaTransactionDAO;
 
   @Override
   public void start() {
@@ -18,6 +24,44 @@ public class InteracTransactionService
 
   @Override
   public Transaction transferValue(Transaction transaction) throws java.lang.RuntimeException {
-    return new Transaction();
+    if ( transaction instanceof Transaction ) {
+      if ( transaction.getPayerId() <= 0 ) {
+        throw new RuntimeException("Invalid Payer id");
+      }
+
+      if ( transaction.getPayeeId() <= 0 ) {
+        throw new RuntimeException("Invalid Payee id");
+      }
+
+      if ( transaction.getAmount() < 0 ) {
+        throw new RuntimeException("Invalid amount");
+      }
+
+      if ( transaction.getRate() <= 0 ) {
+        throw new RuntimeException("Invalid rate");
+      }
+
+      if ( transaction.getPurpose() == null ) {
+        throw new RuntimeException("Invalid purpose");
+      }
+
+      if ( transaction.getFees() < 0 ) {
+        throw new RuntimeException("Invalid fees");
+      }
+
+      String referenceNumber = "CAxxx" + UUID.randomUUID().toString().substring(0, 3).toUpperCase();
+
+      Random random = new Random();
+      char[] digits = new char[13];
+      digits[0] = (char) (random.nextInt(9) + '1');
+      for ( int i = 1; i < 13; i++ ) {
+        digits[i] = (char) (random.nextInt(10) + '0');
+      }
+
+      transaction.setReferenceNumber(referenceNumber);
+      transaction.setImpsReferenceNumber(Long.parseLong(new String(digits)));
+    }
+
+    return transaction;
   }
 }
