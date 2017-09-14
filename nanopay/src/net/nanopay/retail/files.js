eaacaaee43283ee9315b1e21500586c1012b470a
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2017-09-14 14:42:43 -0400

    Added device files to files.js

--- nanopay/src/net/nanopay/retail/files.js
+++ nanopay/src/net/nanopay/retail/files.js
@@ -1,4 +1,9 @@
 FOAM_FILES([
   { name: 'net/nanopay/retail/client/Client' },
-  { name: 'net/nanopay/retail/model/Device' }
+  { name: 'net/nanopay/retail/model/Device' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicesForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicesNameForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicesTypeForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicesSerialForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicesPasswordForm' }
 ]);
