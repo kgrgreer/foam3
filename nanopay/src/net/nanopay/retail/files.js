bdd5d134b86145f648befc2e0c6a3b708e243d3c
Kirk Eaton <kirk@nanopay.net>
2017-09-14 16:20:52 -0400

    Fixed typos in files.js

--- nanopay/src/net/nanopay/retail/files.js
+++ nanopay/src/net/nanopay/retail/files.js
@@ -2,10 +2,10 @@ FOAM_FILES([
   { name: 'net/nanopay/retail/client/Client' },
   { name: 'net/nanopay/retail/model/Device' },
   { name: 'net/nanopay/retail/model/DeviceStatus' },
-  { name: 'net/nanopay/retail/ui/devices/form/DevicesForm' },
-  { name: 'net/nanopay/retail/ui/devices/form/DevicesNameForm' },
-  { name: 'net/nanopay/retail/ui/devices/form/DevicesTypeForm' },
-  { name: 'net/nanopay/retail/ui/devices/form/DevicesSerialForm' },
-  { name: 'net/nanopay/retail/ui/devices/form/DevicesPasswordForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DeviceForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DeviceNameForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DeviceTypeForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DeviceSerialForm' },
+  { name: 'net/nanopay/retail/ui/devices/form/DevicePasswordForm' },
   { name: 'net/nanopay/retail/ui/devices/DeviceCTACard' }
 ]);
