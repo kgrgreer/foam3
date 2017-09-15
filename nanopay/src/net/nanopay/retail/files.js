7b96cb09e2cc423fc9e087a39c729c5b4a3e3c06
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2017-09-15 11:18:16 -0400

    Ported BankCTACard and moved DeviceCTACard

--- nanopay/src/net/nanopay/retail/files.js
+++ nanopay/src/net/nanopay/retail/files.js
@@ -2,11 +2,12 @@ FOAM_FILES([
   { name: 'net/nanopay/retail/client/Client' },
   { name: 'net/nanopay/retail/model/Device' },
   { name: 'net/nanopay/retail/model/DeviceStatus' },
+  { name: 'net/nanopay/retail/ui/DeviceCTACard' },
+  { name: 'net/nanopay/retail/ui/BankCTACard' },
   { name: 'net/nanopay/retail/ui/devices/form/DeviceForm' },
   { name: 'net/nanopay/retail/ui/devices/form/DeviceNameForm' },
   { name: 'net/nanopay/retail/ui/devices/form/DeviceTypeForm' },
   { name: 'net/nanopay/retail/ui/devices/form/DeviceSerialForm' },
   { name: 'net/nanopay/retail/ui/devices/form/DevicePasswordForm' },
-  { name: 'net/nanopay/retail/ui/devices/DeviceCTACard' },
   { name: 'net/nanopay/retail/ui/devices/DevicesView' }
 ]);
