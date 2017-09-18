a5ed74009cac865498644c4e6edc522cf70e5cc8
Patrick <zahnno@gmail.com>
2017-09-18 15:53:40 -0400

    Transfer view created

--- nanopay/src/net/nanopay/TempMenu.js
+++ nanopay/src/net/nanopay/TempMenu.js
@@ -20,6 +20,7 @@ foam.CLASS({
         {                         id: 'expenses',         label: 'Payable',                     handler: { class: 'foam.nanos.menu.ViewMenu', view: { class: 'net.nanopay.invoice.ui.ExpensesView'} } },
         { /*parent: 'support',*/  id: 'aaainvoices',      label: 'Invoices',                       handler: { class: 'foam.nanos.menu.DAOMenu',  daoKey: 'invoiceDAO' } },
         {                         id: 'devices',          label: 'Devices',                        handler: { class: 'foam.nanos.menu.ViewMenu', view: { class: 'net.nanopay.retail.ui.devices.DevicesView' } } },
+        {                         id: 'transfer',          label: 'Transfer',                        handler: { class: 'foam.nanos.menu.ViewMenu', view: { class: 'net.nanopay.ui.TransferView' } } },        
  //       { parent: 'support',  id: 'data',         label: 'View Data',            handler: { class: 'foam.nanos.menu.ViewMenu', view: { class: 'net.nanopay.b2b.DebugView' } } }
       ], foam.nanos.menu.Menu, this.__context__).forEach(this.menuDAO.put.bind(this.menuDAO));
     }
---
+++ nanopay/src/net/nanopay/ui/TransferView.js
@@ -0,0 +1,26 @@
+foam.CLASS({
+  package: 'net.nanopay.ui',
+  name: 'TransferView',
+  extends: 'foam.u2.View',
+
+  documentation: "View to Transfer Amounts ",
+
+  axioms: [
+    foam.u2.CSS.create({
+      code: function CSS() {/*
+      */}
+    })
+  ],
+
+  properties: [
+  ],
+
+  methods: [
+    function initE(){
+      this.SUPER();
+    }
+  ],
+
+  listeners:[
+  ],
+});
