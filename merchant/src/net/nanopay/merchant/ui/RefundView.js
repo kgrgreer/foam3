1a9d78fccaa88f7b04b505e324f8d9579c0e56bb
Kirk Eaton <kirk@nanopay.net>
2017-09-19 10:59:15 -0400

    Added date to transaction object

--- merchant/src/net/nanopay/merchant/ui/RefundView.js
+++ merchant/src/net/nanopay/merchant/ui/RefundView.js
@@ -141,7 +141,8 @@ foam.CLASS({
       this.transactionDAO.put(this.Transaction.create({
         payerId: this.user.id,
         payeeId: this.data.user.id,
-        amount: this.data.amount
+        amount: this.data.amount,
+        date: new Date()
       }))
       .then(function () {
         self.stack.push(self.SuccessView.create({ refund: true, data: self.data }));
