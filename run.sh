34e2ba1a98f5aea9158e1f450a4760738e6e81b8
Kirk Eaton <kirk@nanopay.net>
2017-09-20 10:03:12 -0400

    moved concatentation of services to nanos.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,7 @@
 # Exit on first failure
 set -e
 
-cd ..
-find foam2/src NANOPAY/**/src -type f -name accounts -exec cat {} \; -exec echo \; > accounts
-find foam2/src NANOPAY/**/src -type f -name banks -exec cat {} \; -exec echo \; > banks
-find foam2/src NANOPAY/**/src -type f -name bankAccounts -exec cat {} \; -exec echo \; > bankAccounts
-find foam2/src NANOPAY/**/src -type f -name services -exec cat {} \; -exec echo \; > services
-find foam2/src NANOPAY/**/src -type f -name transactions -exec cat {} \; -exec echo \; > transactions
-find foam2/src NANOPAY/**/src -type f -name users -exec cat {} \; -exec echo \; > users
-find foam2/src NANOPAY/**/src -type f -name payees -exec cat {} \; -exec echo \; > payees
-
-cd foam2/src
+cd ../foam2/src
 ./gen.sh
 
 cd ../build
