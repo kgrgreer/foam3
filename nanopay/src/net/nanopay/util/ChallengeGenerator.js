5863c360af3f3c6891ff6f88345961fdfecbddba
Kirk Eaton <kirk@nanopay.net>
2017-09-20 14:44:22 -0400

    Added more descriptive comment

--- nanopay/src/net/nanopay/util/ChallengeGenerator.js
+++ nanopay/src/net/nanopay/util/ChallengeGenerator.js
@@ -6,7 +6,7 @@ foam.CLASS({
 
   methods: [
     /**
-     * Generates a random 8 character challenge
+     * Generates a random 8 character hex string to be used as a transaction challenge
      **/
     function generateChallenge() {
       return ('00000000' + Math.floor(Math.random() * (4294967295 - 1) + 1).toString(16).toUpperCase()).slice(-8);
