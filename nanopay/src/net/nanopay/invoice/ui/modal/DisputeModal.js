72e35f3cd5c97293e5dcfe990fe616688b73b39e
Patrick <zahnno@gmail.com>
2017-09-26 11:31:42 -0400

    dispute modal

--- nanopay/src/net/nanopay/invoice/ui/modal/DisputeModal.js
+++ nanopay/src/net/nanopay/invoice/ui/modal/DisputeModal.js
@@ -13,9 +13,19 @@ foam.CLASS({
   implements: [
     'net.nanopay.ui.modal.ModalStyling'
   ],
-  
-  properties:[
-    'invoice'
+
+  imports: [
+    'user'
+  ],
+
+  properties: [
+    'invoice',
+    {
+      name: 'type',
+      expression: function(invoice, user){
+        return user.id ? invoice.payeeId : invoice.payerId
+      }
+    }
   ],
 
   axioms: [
@@ -34,7 +44,7 @@ foam.CLASS({
     function initE(){
       this.SUPER();
       var self = this;
-
+      debugger;
       this
       .tag(this.ModalHeader.create({
         title: 'Dispute'
@@ -44,11 +54,11 @@ foam.CLASS({
           .start().addClass('key-value-container')
             .start()
               .start().addClass('key').add("Company").end()
-              .start().addClass('value').add(this.invoice.payeeName).end()
+              .start().addClass('value').add(this.type ? this.invoice.payeeName : this.invoice.payerName).end()
             .end()
             .start()
               .start().addClass('key').add("Amount").end()
-              .start().addClass('value').add("CAD $1234.56").end()
+              .start().addClass('value').add('$', this.invoice.amount.toFixed(2)).end()
             .end()
           .end()
           .start().addClass('label').add("Note").end()
