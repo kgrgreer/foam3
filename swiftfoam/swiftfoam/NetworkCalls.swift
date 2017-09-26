9cce57f658d9694232d661b5ba72d64dff59b981
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2017-09-26 16:04:17 -0400

    Added async calls to add transaction

--- swiftfoam/swiftfoam/NetworkCalls.swift
+++ swiftfoam/swiftfoam/NetworkCalls.swift
@@ -8,12 +8,52 @@
 
 import Foundation
 
-func transferValueByEmail(payer: String, payee: String, amount: Int, callback: @escaping (Any?) -> Void) throws {
+func transferValueBy(transaction: Transaction, callback: @escaping (Any?) -> Void) {
+  let boxContext = BoxContext()
+  let X = boxContext.__subContext__
+
+  let httpBox = X.create(HTTPBox.self)!
+  httpBox.url = "http://localhost:8080/transactionDAO"
+
+  let dao = X.create(ClientDAO.self)!
+  dao.delegate = httpBox
+
+  let placedTransaction = (try? dao.put(transaction)) as? Transaction
+  DispatchQueue.main.async {
+    callback(placedTransaction)
+  }
+}
+
+func transferValueBy(payer payerId: Int,
+                     payee payeeId: Int,
+                     amount: Int,
+                     rate: Float? = 1,
+                     purpose: TransactionPurpose?,
+                     fees: Int? = 0,
+                     notes: String? = "",
+                     callback: @escaping (Any?) -> Void)
+{
   DispatchQueue.global(qos: .userInitiated).async {
-    // TODO: API call.
+    let boxContext = BoxContext()
+    let X = boxContext.__subContext__
+
+    let httpBox = X.create(HTTPBox.self)!
+    httpBox.url = "http://localhost:8080/transactionDAO"
+
+    let dao = X.create(ClientDAO.self)!
+    dao.delegate = httpBox
+
+    let transaction = X.create(Transaction.self)!
+    transaction.payerId = payerId
+    transaction.payeeId = payeeId
+    transaction.amount = amount
+    transaction.rate = rate!
+    transaction.fees = fees!
+    transaction.notes = notes!
+
+    let placedTransaction = (try? dao.put(transaction)) as? Transaction
     DispatchQueue.main.async {
-      // TODO: Send back data from API call.
-      callback("something")
+      callback(placedTransaction)
     }
   }
 }
--- swiftfoam/swiftfoamTests/swiftfoamTests.swift
+++ swiftfoam/swiftfoamTests/swiftfoamTests.swift
@@ -30,12 +30,6 @@ class swiftfoamTests: XCTestCase {
     let boxContext = BoxContext()
     let X = boxContext.__subContext__
 
-    let httpBox = X.create(HTTPBox.self)!
-    httpBox.url = "http://localhost:8080/transactionDAO"
-
-    let dao = X.create(ClientDAO.self)!
-    dao.delegate = httpBox
-
     let t = X.create(Transaction.self)!
     t.payerId = 1
     t.payeeId = 2
@@ -44,18 +38,45 @@ class swiftfoamTests: XCTestCase {
     t.fees = 20
     t.notes = "Mike's test!"
 
-    let t2 = (try? dao.put(t)) as? Transaction
-    XCTAssertNotNil(t2)
-    XCTAssertEqual(t2?.payerId, 1)
-    XCTAssertEqual(t2?.payeeId, 2)
-    XCTAssertEqual(t2?.amount, 5000)
-    XCTAssertEqual(t2?.rate, 15)
-    XCTAssertEqual(t2?.fees, 20)
-    XCTAssertEqual(t2?.notes, "Mike's test!")
+    transferValueBy(payer: t.payerId,
+                    payee: t.payeeId,
+                    amount: t.amount,
+                    rate: t.rate,
+                    purpose: nil,
+                    fees: t.fees,
+                    notes: t.notes)
+    {
+      response in
+      let t2 = response as? Transaction
+      XCTAssertNotNil(t2)
+      XCTAssertEqual(t2?.payerId, 1)
+      XCTAssertEqual(t2?.payeeId, 2)
+      XCTAssertEqual(t2?.amount, 5000)
+      XCTAssertEqual(t2?.rate, 15)
+      XCTAssertEqual(t2?.fees, 20)
+      XCTAssertEqual(t2?.notes, "Mike's test!")
 
-    XCTAssertNotEqual(t.compareTo(t2), 0)
-    t.id = t2!.id
-    t.date = t2!.date
-    XCTAssertEqual(t.compareTo(t2), 0)
+      XCTAssertNotEqual(t.compareTo(t2), 0)
+      t.id = t2!.id
+      t.date = t2!.date
+      XCTAssertEqual(t.compareTo(t2), 0)
+    }
+
+    transferValueBy(transaction: t) {
+      response in
+      let t2 = response as? Transaction
+      XCTAssertNotNil(t2)
+      XCTAssertEqual(t2?.payerId, 1)
+      XCTAssertEqual(t2?.payeeId, 2)
+      XCTAssertEqual(t2?.amount, 5000)
+      XCTAssertEqual(t2?.rate, 15)
+      XCTAssertEqual(t2?.fees, 20)
+      XCTAssertEqual(t2?.notes, "Mike's test!")
+
+      XCTAssertNotEqual(t.compareTo(t2), 0)
+      t.id = t2!.id
+      t.date = t2!.date
+      XCTAssertEqual(t.compareTo(t2), 0)
+    }
   }
 }
