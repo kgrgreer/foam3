1ff80ef3dacd1239cc32833b9a7d09455d79cfd1
Mike Carcasole <mikecarcasole@gmail.com>
2017-09-26 10:56:25 -0400

    Add a test to put into the transaction dao and update calls to dao to try because the methods now throw.

--- swiftfoam/swiftfoamTests/swiftfoamTests.swift
+++ swiftfoam/swiftfoamTests/swiftfoamTests.swift
@@ -11,7 +11,7 @@ class swiftfoamTests: XCTestCase {
     super.tearDown()
   }
 
-  func testTransactionDAO() {
+  func testSelectTransactionDAO() {
     let boxContext = BoxContext()
     let X = boxContext.__subContext__
 
@@ -21,8 +21,32 @@ class swiftfoamTests: XCTestCase {
     let dao = X.create(ClientDAO.self)!
     dao.delegate = httpBox
 
-    let sink = dao.skip(0).limit(1).select(ArraySink()) as? ArraySink
+    let sink = (try? dao.skip(0).limit(1).select(ArraySink())) as? ArraySink
     let transaction = sink?.array[0] as? Transaction
     XCTAssertNotNil(transaction)
   }
+
+  func testPutTransactionDAO() {
+    let boxContext = BoxContext()
+    let X = boxContext.__subContext__
+
+    let httpBox = X.create(HTTPBox.self)!
+    httpBox.url = "http://localhost:8080/transactionDAO"
+
+    let dao = X.create(ClientDAO.self)!
+    dao.delegate = httpBox
+
+    let t = X.create(Transaction.self)!
+    t.payerId = 1
+    t.payeeId = 2
+    t.amount = 5000
+    t.date = "2017-09-26T14:49:20.548Z"
+    t.rate = 15
+    t.fees = 20
+    t.notes = "Mike's test!"
+
+    let t2 = (try? dao.put(t)) as? Transaction
+    XCTAssertNotNil(t2)
+  }
+
 }
