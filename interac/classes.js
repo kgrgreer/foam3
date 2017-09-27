d43ef73109fba5a7d960c389150c210a34317d2a
Waseem <waseem@nanopay.net>
2017-09-27 11:01:27 -0400

    moved to server

--- interac/classes.js
+++ interac/classes.js
@@ -3,7 +3,8 @@ var classes = [
   'net.nanopay.interac.model.CountryAgent',
   'net.nanopay.interac.model.Identification',
   'net.nanopay.interac.model.Pacs008IndiaPurpose',
-  'net.nanopay.interac.model.Pacs008ISOPurpose'
+  'net.nanopay.interac.model.Pacs008ISOPurpose',
+  'net.nanopay.interac.model.DateAndPlaceOfBirth'
 
 ];
 
