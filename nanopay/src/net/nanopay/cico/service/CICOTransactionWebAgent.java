6307d929fa7bdf6009383d172876927ec98a0949
Kirk Eaton <kirk@nanopay.net>
2017-10-02 11:38:55 -0400

    Changed Outputter to be the CSV outputter

--- nanopay/src/net/nanopay/cico/service/CICOTransactionWebAgent.java
+++ nanopay/src/net/nanopay/cico/service/CICOTransactionWebAgent.java
@@ -3,7 +3,7 @@ package net.nanopay.cico.service;
 import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.Sink;
-import foam.lib.json.Outputter;
+import foam.lib.csv.Outputter;
 import foam.lib.json.OutputterMode;
 import foam.nanos.http.WebAgent;
 
@@ -17,7 +17,7 @@ public class CICOTransactionWebAgent
   public void execute(X x) {
     PrintWriter out = (PrintWriter) x.get(PrintWriter.class);
     DAO transactionDAO = (DAO) x.get("transactionDAO");
-    Sink outputter = new Outputter(out, OutputterMode.STORAGE);
+    Sink outputter = new Outputter(out, OutputterMode.STORAGE, false);
     // TODO: figure out where clause
     outputter = transactionDAO.select(outputter);
   }
