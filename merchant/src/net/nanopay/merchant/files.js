a8d4e9d397b8da9512989d2a27c224fe4de3f62a
Kirk Eaton <kirk@nanopay.net>
2017-10-04 09:20:42 -0400

    Added files.js. Removed transaction client

---
+++ merchant/src/net/nanopay/merchant/files.js
@@ -0,0 +1,19 @@
+FOAM_FILES([
+  { name: 'net/nanopay/merchant/ui/setup/SetupErrorView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/setup/SetupSuccessView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/setup/SetupView', flags: [ 'web' ] },
+
+  { name: 'net/nanopay/merchant/ui/transaction/EmptyTransactionListView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/transaction/TransactionDetailView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/transaction/TransactionListView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/transaction/TransactionRowView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/transaction/TransactionToolbar', flags: [ 'web' ] },
+
+  { name: 'net/nanopay/merchant/ui/Controller', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/ErrorView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/HomeView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/QRCodeView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/RefundView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/SuccessView', flags: [ 'web' ] },
+  { name: 'net/nanopay/merchant/ui/ToolbarView', flags: [ 'web' ] }
+]);
--- merchant/src/net/nanopay/merchant/ui/Controller.js
+++ merchant/src/net/nanopay/merchant/ui/Controller.js
@@ -8,7 +8,6 @@ foam.CLASS({
   arequire: function() { return foam.nanos.client.ClientBuilder.create(); }, 
   implements: [
     'foam.nanos.client.Client',
-    'net.nanopay.tx.client.Client',
     'net.nanopay.retail.client.Client'
   ],
 
