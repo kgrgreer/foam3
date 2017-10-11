22899ffd8e1f8fb51b977fbbeadf7bb89bccfccc
Kirk Eaton <kirk@nanopay.net>
2017-10-11 15:13:45 -0400

    Added functionality to put a device into the deviceDAO

---
+++ nanopay/src/net/nanopay/retail/model/DeviceType.js
@@ -0,0 +1,21 @@
+foam.ENUM({
+  package: 'net.nanopay.retail.model',
+  name: 'DeviceType',
+
+  documentation: 'Enum for device types',
+
+  values: [
+    {
+      name: 'APPLE',
+      label: 'Apple'
+    },
+    {
+      name: 'ANDROID',
+      label: 'Android'
+    },
+    {
+      name: 'INGENICO',
+      label: 'Ingenico'
+    }
+  ]
+});
