dc9ecdc6b8dcd17cfd4fd29e5cb9b54fd5e5fb4a
MarcRoopchand <marc.roopchand@gmail.com>
2017-10-12 13:15:43 -0400

    Updated script to have right permission

--- root/tomcatInstall.sh
+++ root/tomcatInstall.sh
@@ -1,9 +1,8 @@
 sudo mkdir -p /usr/local
 cd /usr/local
-curl -o apache-tomcat-8.5.23.zip http://mirror.csclub.uwaterloo.ca/apache/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.zip
-unzip apache-tomcat-8.5.23.zip
-rm apache-tomcat-8.5.23.zip
-
+sudo curl -o apache-tomcat-8.5.23.zip http://mirror.csclub.uwaterloo.ca/apache/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.zip
+sudo unzip apache-tomcat-8.5.23.zip
+sudo rm apache-tomcat-8.5.23.zip
 sudo ln -s /usr/local/apache-tomcat-8.5.23 /Library/Tomcat
 sudo chmod +x /Library/Tomcat/bin/*.sh
 
