6b7e277b09247d8a3dd8662a74a69fc0153e9547
MarcRoopchand <marc.roopchand@gmail.com>
2017-10-12 13:30:29 -0400

    Removed ln folder before linking

--- root/tomcatInstall.sh
+++ root/tomcatInstall.sh
@@ -3,6 +3,6 @@ cd /usr/local
 sudo curl -o apache-tomcat-8.5.23.zip http://mirror.csclub.uwaterloo.ca/apache/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.zip
 sudo unzip apache-tomcat-8.5.23.zip
 sudo rm apache-tomcat-8.5.23.zip
+sudo rm -rf /Library/Tomcat
 sudo ln -s /usr/local/apache-tomcat-8.5.23 /Library/Tomcat
 sudo chmod +x /Library/Tomcat/bin/*.sh
-
