7488cc15dc4d1f36f1b7c7230a8a1aa829c18866
Waseem <waseem@nanopay.net>
2017-10-19 15:42:35 -0400

    set changes back to master

--- root/build.sh
+++ root/build.sh
@@ -4,17 +4,29 @@ set -e
 #Shutdown tomcat if already running
 /$CATALINA_HOME/bin/shutdown.sh
 
-#Remove build folder and rebuild nanopay
-cd ../
-rm -rf build
+#Remove build folder and rebuild foam
+rm -rf ../../foam2/build/
+cd ../../foam2/src/
 ./gen.sh
-
-cd build
-mvn clean compile package 
-mvn install:install-file -Dfile="target/nanofoam-0.0.1.jar" -DgroupId=net.nanopay -DartifactId=nanofoam -Dversion=1.0 -Dname=nanoPay -Dpackaging=jar
+cd ../build/
+mvn compile package
+mvn install:install-file -Dfile="target/foam-1.0-SNAPSHOT.jar" -DgroupId=com.google -DartifactId=foam -Dversion=1.0 -Dname=foam -Dpackaging=jar
 mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
+
+#Remove build folder and rebuild nanopay
+cd ../../NANOPAY/
+./gen.sh
+mvn clean install
+
+cd nanopay/
+rm -rf build/
+cd src/
+./gen.sh
+cd ..
+mvn clean install
+
 #Build root which is 1 war file of all projects
-cd ../root
+cd ../root/
 mvn clean install
 
 #Copy over files to tomcat location
