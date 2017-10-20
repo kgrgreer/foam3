0c25a9ddea4f42500e0a44e6a61912bbe976764e
marcogbarcellos <marcogbarcellos@gmail.com>
2017-10-20 14:58:36 -0400

    adding first version of serviceProviderTransactionDAO

---
+++ nanopay/src/net/nanopay/cico/service/ServiceProviderFeeTransactionDAO.java
@@ -0,0 +1,48 @@
+package net.nanopay.cico.service;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import net.nanopay.tx.model.Transaction;
+import net.nanopay.cico.model.TransactionType;
+import net.nanopay.cico.model.TransactionStatus;
+
+public class ServiceProviderFeeTransactionDAO
+  extends ProxyDAO
+{
+  public ServiceProviderFeeTransactionDAO(DAO delegate) {
+    setDelegate(delegate);
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) throws RuntimeException {
+    Transaction transaction = (Transaction) obj;
+
+    if ( transaction.getBankAccountId() == null ) {
+      throw new RuntimeException("Invalid bank account");
+    }
+
+    Long firstLock  = transaction.getPayerId() < transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
+    Long secondLock = transaction.getPayerId() > transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
+
+    synchronized ( firstLock ) {
+      synchronized ( secondLock ) {
+        try {
+          if ( transaction.getCicoStatus() == null ) {
+            transaction.setCicoStatus(TransactionStatus.NEW);
+          }
+          // Change later to check whether payeeId or payerId are ACTIVE brokers to set CASHIN OR CASHOUT...
+          if ( transaction.getType() == null ) {
+            transaction.setType(TransactionType.CASHOUT);
+          }
+
+          return getDelegate().put(transaction);
+
+        } catch (RuntimeException e) {
+          throw e;
+        }
+      }
+    }
+  }
+}
