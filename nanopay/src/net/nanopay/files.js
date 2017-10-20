dc85ac9c316bd3f14bab9d7145d4a0635dbd826c
marcogbarcellos <marcogbarcellos@gmail.com>
2017-10-20 16:21:31 -0400

    adding VariableFee to files.js

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -54,6 +54,7 @@ FOAM_FILES([
   { name: 'net/nanopay/tx/model/Fee' },
   { name: 'net/nanopay/tx/model/FixedFee' },
   { name: 'net/nanopay/tx/model/PercentageFee' },
+  { name: 'net/nanopay/tx/model/VariableFee' },
   { name: 'net/nanopay/model/Broker' },
 
   { name: 'net/nanopay/tx/ui/TransactionsView', flags: ['web'] },
