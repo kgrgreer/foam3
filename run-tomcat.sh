acad691973bb5a6774e6e8f353fd33ed9cd6f7bf
MarcRoopchand <marc.roopchand@gmail.com>
2017-10-24 15:49:00 -0400

    Updated build script for submodule

--- run-tomcat.sh
+++ run-tomcat.sh
@@ -12,44 +12,44 @@ mvn clean install
 
 #Copy over files to tomcat location
 cp target/ROOT.war $CATALINA_HOME/webapps
-cp server.xml $CATALINA_HOME/conf
+#cp server.xml $CATALINA_HOME/conf
 
 #Concatenate files into one services file
-cd ../
-find foam2/src NANOPAY/**/src -type f -name accounts -exec cat {} \; > accounts
-find foam2/src NANOPAY/**/src -type f -name branches -exec cat {} \; > branches
-find foam2/src NANOPAY/**/src -type f -name bankAccounts -exec cat {} \; > bankAccounts
-find foam2/src NANOPAY/**/src -type f -name branches -exec cat {} \; > branches
-find foam2/src NANOPAY/**/src -type f -name brokers -exec cat {} \; > brokers
-find foam2/src NANOPAY/**/src -type f -name businesses -exec cat {} \; > businesses
-find foam2/src NANOPAY/**/src -type f -name canadaTransactions -exec cat {} \; > canadaTransactions
-find foam2/src NANOPAY/**/src -type f -name cicoServiceProviders -exec cat {} \; > cicoServiceProviders
-find foam2/src NANOPAY/**/src -type f -name countries -exec cat {} \; > countries
-find foam2/src NANOPAY/**/src -type f -name countryAgents -exec cat {} \; > countryAgents
-find foam2/src NANOPAY/**/src -type f -name cronjobs -exec cat {} \; > cronjobs
-find foam2/src NANOPAY/**/src -type f -name currency -exec cat {} \; > currency
-find foam2/src NANOPAY/**/src -type f -name devices -exec cat {} \; > devices
-find foam2/src NANOPAY/**/src -type f -name dateofbirth -exec cat {} \; > dateofbirth
-find foam2/src NANOPAY/**/src -type f -name exchangeRates -exec cat {} \; > exchangeRates
-find foam2/src NANOPAY/**/src -type f -name exportDriverRegistrys -exec cat {} \; > exportDriverRegistrys
-find foam2/src NANOPAY/**/src -type f -name groups -exec cat {} \; > groups
-find foam2/src NANOPAY/**/src -type f -name historyRecords -exec cat {} \; > historyRecords
-find foam2/src NANOPAY/**/src -type f -name indiaTransactions -exec cat {} \; > indiaTransactions
-find foam2/src NANOPAY/**/src -type f -name identification -exec cat {} \; > identification
-find foam2/src NANOPAY/**/src -type f -name invoices -exec cat {} \; > invoices
-find foam2/src NANOPAY/**/src -type f -name invoiceResolutions -exec cat {} \; > invoiceResolutions
-find foam2/src NANOPAY/**/src -type f -name languages -exec cat {} \; > languages
-find foam2/src NANOPAY/**/src -type f -name menus -exec cat {} \; > menus
-find foam2/src NANOPAY/**/src -type f -name pacs8india -exec cat {} \; > pacs8india
-find foam2/src NANOPAY/**/src -type f -name pacs8iso -exec cat {} \; > pacs8iso
-find foam2/src NANOPAY/**/src -type f -name payees -exec cat {} \; > payees
-find foam2/src NANOPAY/**/src -type f -name permissions -exec cat {} \; > permissions
-find foam2/src NANOPAY/**/src -type f -name regions -exec cat {} \; > regions
-find foam2/src NANOPAY/**/src -type f -name scripts -exec cat {} \; > scripts
-find foam2/src NANOPAY/**/src -type f -name services -exec cat {} \; > services
-find foam2/src NANOPAY/**/src -type f -name tests -exec cat {} \; > tests
-find foam2/src NANOPAY/**/src -type f -name transactions -exec cat {} \; > transactions
-find foam2/src NANOPAY/**/src -type f -name users -exec cat {} \; > users
+
+find **/src -type f -name accounts -exec cat {} \; > accounts
+find **/src -type f -name branches -exec cat {} \; > branches
+find **/src -type f -name bankAccounts -exec cat {} \; > bankAccounts
+find **/src -type f -name branches -exec cat {} \; > branches
+find **/src -type f -name brokers -exec cat {} \; > brokers
+find **/src -type f -name businesses -exec cat {} \; > businesses
+find **/src -type f -name canadaTransactions -exec cat {} \; > canadaTransactions
+find **/src -type f -name cicoServiceProviders -exec cat {} \; > cicoServiceProviders
+find **/src -type f -name countries -exec cat {} \; > countries
+find **/src -type f -name countryAgents -exec cat {} \; > countryAgents
+find **/src -type f -name cronjobs -exec cat {} \; > cronjobs
+find **/src -type f -name currency -exec cat {} \; > currency
+find **/src -type f -name devices -exec cat {} \; > devices
+find **/src -type f -name dateofbirth -exec cat {} \; > dateofbirth
+find **/src -type f -name exchangeRates -exec cat {} \; > exchangeRates
+find **/src -type f -name exportDriverRegistrys -exec cat {} \; > exportDriverRegistrys
+find **/src -type f -name groups -exec cat {} \; > groups
+find **/src -type f -name historyRecords -exec cat {} \; > historyRecords
+find **/src -type f -name indiaTransactions -exec cat {} \; > indiaTransactions
+find **/src -type f -name identification -exec cat {} \; > identification
+find **/src -type f -name invoices -exec cat {} \; > invoices
+find **/src -type f -name invoiceResolutions -exec cat {} \; > invoiceResolutions
+find **/src -type f -name languages -exec cat {} \; > languages
+find **/src -type f -name menus -exec cat {} \; > menus
+find **/src -type f -name pacs8india -exec cat {} \; > pacs8india
+find **/src -type f -name pacs8iso -exec cat {} \; > pacs8iso
+find **/src -type f -name payees -exec cat {} \; > payees
+find **/src -type f -name permissions -exec cat {} \; > permissions
+find **/src -type f -name regions -exec cat {} \; > regions
+find **/src -type f -name scripts -exec cat {} \; > scripts
+find **/src -type f -name services -exec cat {} \; > services
+find **/src -type f -name tests -exec cat {} \; > tests
+find **/src -type f -name transactions -exec cat {} \; > transactions
+find **/src -type f -name users -exec cat {} \; > users
 
 #Copy over all JDAO files to /bin
 cp accounts $CATALINA_HOME/bin/
@@ -90,6 +90,7 @@ cp users $CATALINA_HOME/bin/
 rm -rf $CATALINA_HOME/bin/foam2/
 cp -r foam2/ $CATALINA_HOME/bin/foam2
 
+cd ../
 rm -rf $CATALINA_HOME/bin/NANOPAY/
 cp -r NANOPAY/ $CATALINA_HOME/bin/NANOPAY
 
