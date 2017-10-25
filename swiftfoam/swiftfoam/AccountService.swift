5803287a0d56ca1612dd620ddd5894c4a3947d0f
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2017-10-25 11:38:58 -0400

    Grab account using owner

--- swiftfoam/swiftfoam/AccountService.swift
+++ swiftfoam/swiftfoam/AccountService.swift
@@ -13,24 +13,62 @@ public class AccountService: Service {
     super.init(withURL: ServiceURL.Account)
   }
 
-  public func verify(account: Account,
-                     withPassword pass: String,
-                     callback: @escaping (Any?) -> Void)
-  {
-    
+  // Helps determine type of error to handle on frontend
+  public enum AccountError: ServiceErrorProtocol {
+    case AccountNotFound
+    public func description() -> String {
+      switch(self) {
+      case .AccountNotFound:
+        return "Account cannot be found for that id."
+      }
+    }
   }
 
-  public func update(account: Account,
-                     forUserId id: Int,
-                     callback: @escaping (Any?) -> Void)
-  {
-    // TODO: Add User Authenticity
-  }
+  public func getAccountFor(userId id: Int, callback: @escaping (Any?) -> Void) {
+    DispatchQueue.global(qos: .userInitiated).async {
+      guard UserService.instance.isUserLoggedIn() else {
+        DispatchQueue.main.async {
+          callback(UserService.UserError.UserNotLoggedIn)
+        }
+        return
+      }
+      do {
+        let pred = self.X.create(Eq.self, args: [
+          "arg1": Account.classInfo().axiom(byName: "owner"),
+          "arg2": id,
+          ])
 
-  public func getAccounts(startingAt skip:  Int? = 0,
-                          withLimit  limit: Int? = 100,
-                          callback:  @escaping (Any?) -> Void)
-  {
-    // TODO: Add User Authenticity
+        guard let accountSink = (try self.dao.`where`(pred).skip(0).limit(1).select(ArraySink())) as? ArraySink else {
+          DispatchQueue.main.async {
+            callback(ServiceError.ConversionFailed)
+          }
+          return
+        }
+
+        guard accountSink.array.count > 0 else {
+          // User Not Found.
+          DispatchQueue.main.async {
+            callback(AccountError.AccountNotFound)
+          }
+          return
+        }
+
+        guard let account = accountSink.array[0] as? Account else {
+          DispatchQueue.main.async {
+            callback(ServiceError.ConversionFailed)
+          }
+          return
+        }
+
+        DispatchQueue.main.async {
+          callback(account)
+        }
+      } catch let e {
+        NSLog(((e as? FoamError)?.toString()) ?? "Error!")
+        DispatchQueue.main.async {
+          callback(ServiceError.Failed)
+        }
+      }
+    }
   }
 }
