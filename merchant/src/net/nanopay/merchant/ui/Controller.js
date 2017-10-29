6c30f39a98f35c839d772ab6ec5139f4b67f9347
Kirk Eaton <kirk@nanopay.net>
2017-10-29 19:19:41 -0400

    Modified ic-arrow-left to make it perfect square. Modified onMenuItemClicked to handle case where image is clicked

--- merchant/src/net/nanopay/merchant/ui/Controller.js
+++ merchant/src/net/nanopay/merchant/ui/Controller.js
@@ -33,14 +33,83 @@ foam.CLASS({
       code: function CSS() {/*
         ^ {
           width: 320px;
-          background-color: #2c4389;
+          background-color: #2C4389;
         }
-        ^ .mdc-toolbar--fixed {
+        ^ .sidenav {
+          display: none;
+          height: 100%;
+          width: 250px;
+          position: fixed;
+          z-index: 1;
+          top: 0;
+          left: 0;
+          background-color: #FFFFFF;
+          overflow-x: hidden;
+          box-shadow: 10px 8px 10px -5px rgba(0, 0, 0, 0.2),
+            0px 16px 24px 2px rgba(0, 0, 0, 0.14),
+            0px 6px 30px 5px rgba(0, 0, 0, 0.12);
+        }
+        ^ .sidenav.open {
+          display: block;
+        }
+        ^ .toolbar {
+          height: 56px;
+          background-color: #4054B5;
           -webkit-box-shadow: none;
           box-shadow: none;
           position: relative;
-          height: 56px;
         }
+        ^ .toolbar-icon {
+          height: 100%;
+          padding-left: 20px;
+          padding-right: 20px;
+          float: left;
+        }
+        ^ .toolbar-title {
+          font-size: 16px;
+          line-height: 56px;
+        }
+        ^ .sidenav-list-item {
+          height: 90px;
+        }
+        ^ .sidenav-list-item.selected {
+          background-color: #f1f1f1;
+        }
+        ^ .sidenav-list-item a {
+          font-family: Roboto;
+          font-size: 16px;
+          font-weight: 500;
+          color: #595959;
+          line-height: 90px;
+          text-decoration: none;
+        }
+        ^ .sidenav-list-icon i {
+          display: inline-block;
+          height: 100%;
+          vertical-align: middle;
+        }
+        ^ .sidenav-list-icon img {
+          width: 40px;
+          height: 40px;
+          object-fit: contain;
+          vertical-align: middle;
+        }
+        ^ .sidenav-list-item.back {
+          height: 56px;
+          background-color: #26A96C;
+        }
+        ^ .sidenav-list-item.back a {
+          color: #FFFFFF;
+          line-height: 56px;
+          text-decoration: none;
+        }
+        ^ .sidenav-list-icon.back img {
+          width: 20px;
+          height: 20px;
+        }
+
+
+
         ^ .mdc-list-item {
           min-height: 90px;
           background-color: #ffffff;
@@ -63,7 +132,6 @@ foam.CLASS({
           color: #ffffff;
         }
         ^ .mdc-list-item.selected {
-          background-color: #f1f1f1;
         }
         ^ .mdc-list-item__start-detail {
           height: 90px;
@@ -71,8 +139,7 @@ foam.CLASS({
           margin-top: 50px;
         }
         ^ .mdc-list-item__start-detail img {
-          width: 40px;
-          height: 40px;
+
           object-fit: contain;
         }
         ^ .mdc-list-item__start-detail.back {
@@ -94,7 +161,6 @@ foam.CLASS({
   ],
 
   properties: [
-    'drawer',
     {
       class: 'Boolean',
       name: 'showHeader',
@@ -179,77 +245,117 @@ foam.CLASS({
 
       this
         .addClass(this.myClass())
-        .start('div').addClass('mdc-toolbar mdc-toolbar--fixed').show(this.showHeader$)
-          .start('div').addClass('mdc-toolbar__row')
-            .start('section').addClass('mdc-toolbar__section mdc-toolbar__section--align-start')
-              .start('button').addClass('merchant-menu material-icons mdc-toolbar__icon--menu')
-                .add(this.toolbarIcon$)
-                .on('click', this.onMenuClicked)
+        // sidebar
+        .start('div').addClass('sidenav')
+          .start('div').addClass('sidenav-list-item back')
+            .start('a').attrs({ href: '#' })
+              .start('i').addClass('sidenav-list-icon back')
+                .attrs({ 'aria-hidden': true })
+                .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-arrow-left.png' })
               .end()
-              .start('span').addClass('mdc-toolbar__title catalog-title').add(this.toolbarTitle$).end()
+              .add('Back')
             .end()
+            .on('click', this.onMenuItemClicked)
+          .end()
+          .start('div').addClass('sidenav-list-item selected')
+            .start('a').attrs({ href: '#' })
+              .start('i').addClass('sidenav-list-icon')
+                .attrs({ 'aria-hidden': true })
+                .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-home.png' })
+              .end()
+              .add('Home')
+            .end()
+            .on('click', this.onMenuItemClicked)
+          .end()
+          .start('div').addClass('sidenav-list-item')
+            .start('a').attrs({ href: '#' })
+              .start('i').addClass('sidenav-list-icon')
+                .attrs({ 'aria-hidden': true })
+                .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-transactions.png' })
+              .end()
+              .add('Transactions')
+            .end()
+            .on('click', this.onMenuItemClicked)
+          .end()
+          .start('div').addClass('sidenav-list-item')
+            .start('a').attrs({ href: '#' })
+              .start('i').addClass('sidenav-list-icon')
+                .attrs({ 'aria-hidden': true })
+                .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-tip.png' })
+              .end()
+              .add('Tip')
+            .end()
+            .on('click', this.onMenuItemClicked)
           .end()
         .end()
 
-        .start('aside').addClass('mdc-temporary-drawer')
-          .start('nav').addClass('mdc-temporary-drawer__drawer')
-            .start('nav').addClass('mdc-temporary-drawer__content mdc-list-group')
-              .start('div').addClass('mdc-list')
-                .start('a').addClass('mdc-list-item back')
-                  .attrs({ href: '#' })
-                  .start('i').addClass('mdc-list-item__start-detail back')
-                    .attrs({ 'aria-hidden': true })
-                    .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-arrow-left.png' })
-                  .end()
-                  .add('Back')
-                  .on('click', this.onMenuItemClicked)
-                .end()
-
-                .start('a').addClass('mdc-list-item selected')
-                  .attrs({ href: '#' })
-                  .start('i').addClass('mdc-list-item__start-detail')
-                    .attrs({ 'aria-hidden': true })
-                    .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-home.png' })
-                  .end()
-                  .add('Home')
-                  .on('click', this.onMenuItemClicked)
-                .end()
-                .start('a').addClass('mdc-list-item')
-                  .attrs({ href: '#' })
-                  .start('i').addClass('mdc-list-item__start-detail')
-                    .attrs({ 'aria-hidden': true })
-                    .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-transactions.png' })
-                  .end()
-                  .add('Transactions')
-                  .on('click', this.onMenuItemClicked)
-                .end()
-
-                .start('a').addClass('mdc-list-item')
-                  .start('i').addClass('mdc-list-item__start-detail')
-                    .attrs({ 'aria-hidden': true })
-                    .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-tip.png' })
-                  .end()
-                  .add('Tip')
-                  .on('click', this.onMenuItemClicked)
-                  .tag({ class: 'net.nanopay.ui.ToggleSwitch', data$: this.tipEnabled$ })
-                .end()
-
-              .end()
-            .end()
+        // toolbar
+        .start('div').addClass('toolbar')
+          .start('button').addClass('toolbar-icon material-icons')
+            .add(this.toolbarIcon$)
+            .on('click', this.onMenuClicked)
           .end()
+          .start('div').addClass('toolbar-title').add(this.toolbarTitle$).end()
         .end()
+
+//        .start('div').addClass('mdc-toolbar mdc-toolbar--fixed').show(this.showHeader$)
+//          .start('div').addClass('mdc-toolbar__row')
+//            .start('section').addClass('mdc-toolbar__section mdc-toolbar__section--align-start')
+
+//              .start('span').addClass('mdc-toolbar__title catalog-title').add(this.toolbarTitle$).end()
+//            .end()
+//          .end()
+//        .end()
+//
+//        .start('aside').addClass('mdc-temporary-drawer')
+//          .start('nav').addClass('mdc-temporary-drawer__drawer')
+//            .start('nav').addClass('mdc-temporary-drawer__content mdc-list-group')
+//              .start('div').addClass('mdc-list')
+//                .start('a').addClass('mdc-list-item back')
+//                  .attrs({ href: '#' })
+//                  .start('i').addClass('mdc-list-item__start-detail back')
+//                    .attrs({ 'aria-hidden': true })
+//                    .tag({ class: 'foam.u2.tag.Image', data: 'images/ic-arrow-left.png' })
+//                  .end()
+//                  .add('Back')
+//                  .on('click', this.onMenuItemClicked)
+//                .end()
+//
+//                .start('a').addClass('mdc-list-item selected')
+//                  .attrs({ href: '#' })
+//                  .start('i').addClass('mdc-list-item__start-detail')
+//                    .attrs({ 'aria-hidden': true })
+//                  .end()
+//                  .add('Home')
+//                  .on('click', this.onMenuItemClicked)
+//                .end()
+//                .start('a').addClass('mdc-list-item')
+//                  .attrs({ href: '#' })
+//                  .start('i').addClass('mdc-list-item__start-detail')
+//                    .attrs({ 'aria-hidden': true })
+//
+//                  .end()
+//                  .add('Transactions')
+//                  .on('click', this.onMenuItemClicked)
+//                .end()
+//
+//                .start('a').addClass('mdc-list-item')
+
+//                  .add('Tip')
+//                  .on('click', this.onMenuItemClicked)
+//                  .tag({ class: 'net.nanopay.ui.ToggleSwitch', data$: this.tipEnabled$ })
+//                .end()
+//
+//              .end()
+//            .end()
+//          .end()
+//        .end()
+
       this
         .start('div')
           .addClass('stack-wrapper')
           .tag({ class: 'foam.u2.stack.StackView', data: this.stack, showActions: false })
         .end()
-
-
-      this.onload.sub(function () {
-        var drawerEl = document.querySelector('.mdc-temporary-drawer');
-        var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
-        this.drawer = new MDCTemporaryDrawer(drawerEl);
-      });
     }
   ],
 
@@ -260,20 +366,23 @@ foam.CLASS({
         this.toolbarIcon = 'menu';
         this.stack.back();
       } else {
-        drawer.open = true;
+        var sidenav = document.querySelector('.sidenav');
+        sidenav.classList.add('open');
       }
     },
 
     function onMenuItemClicked (e) {
-      var clicked = e.target.text.trim();
+      e = ( ! e.target.textContent ) ? e.path[3] : e.target;
+      var clicked = e.textContent.trim();
       // if clicked is null, don't do anything
       if ( ! clicked ) {
         return;
       }
 
+      var sidenav = document.querySelector('.sidenav');
       // if we are on the same screen or have clicked the back button, close drawer
       if ( this.toolbarTitle === clicked || clicked === 'Back' ) {
-        drawer.open = false;
+        sidenav.classList.remove('open');
         return;
       }
 
@@ -284,10 +393,10 @@ foam.CLASS({
       }
 
       // remove selected class from current and add to new
-      var selected = document.getElementsByClassName('mdc-list-item selected')[0];
+      var selected = document.getElementsByClassName('sidenav-list-item selected')[0];
       if ( selected ) {
         selected.classList.remove('selected');
-        e.target.classList.add('selected')
+        e.classList.add('selected')
       }
 
       this.toolbarTitle = clicked;
@@ -300,7 +409,7 @@ foam.CLASS({
           this.stack.push({ class: 'net.nanopay.merchant.ui.transaction.TransactionListView' });
           break;
       }
-      drawer.open = false;
+      sidenav.classList.remove('open');
     }
   ]
 });
