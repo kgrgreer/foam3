c8045aba992ca6960b6e8ae1a57e31ef9c3d4d98
marcogbarcellos <marcogbarcellos@gmail.com>
2017-10-31 15:59:31 -0400

    adding service provider transaction dao

--- nanopay/src/net/nanopay/cico/service/ServiceProviderFeeTransactionDAO.java
+++ nanopay/src/net/nanopay/cico/service/ServiceProviderFeeTransactionDAO.java
@@ -15,43 +15,36 @@ import net.nanopay.tx.model.Transaction;
 public class ServiceProviderFeeTransactionDAO
   extends ProxyDAO
 {
-  protected DAO userDAO_;
-  protected DAO serviceProviderDAO_;
-
-  public ServiceProviderFeeTransactionDAO(DAO delegate) {
-    setDelegate(delegate);
-  }
 
   public ServiceProviderFeeTransactionDAO(X x, DAO delegate) {
     setX(x);
     setDelegate(delegate);
   }
 
-  protected DAO getUserDAO() {
-    if ( userDAO_ == null ) {
-      userDAO_ = (DAO) getX().get("localUserDAO");
-    }
-    return userDAO_;
-  }
-
-  protected DAO getServiceProviderDAO() {
-    if ( serviceProviderDAO_ == null ) {
-      serviceProviderDAO_ = (DAO) getX().get("cicoServiceProviderDAO");
-    }
-    return serviceProviderDAO_;
+  public ServiceProviderFeeTransactionDAO(DAO delegate) {
+    setDelegate(delegate);
   }
 
   @Override
   public FObject put_(X x, FObject obj) throws RuntimeException {
     Transaction transaction = (Transaction) obj;
-    User payee = (User) getUserDAO().find(transaction.getPayeeId());
-    User payer = (User) getUserDAO().find(transaction.getPayerId());
+
+    X context = x != null ? x : getX();
+
+    System.out.println("getX() :"+getX());
+    System.out.println("x from PUT:"+x);
+
+    DAO userDAO = (DAO) context.get("localUserDAO");
+    DAO serviceProviderDAO = (DAO) context.get("cicoServiceProviderDAO");
+
+    User payee = (User) userDAO.find(transaction.getPayeeId());
+    User payer = (User) userDAO.find(transaction.getPayerId());
 
     if ( transaction.getProviderId() == null ) {
-      throw new RuntimeException("Service Provider is not defined.");
+      return obj;
     }
 
-    ServiceProvider serviceProvider = (ServiceProvider) getServiceProviderDAO().find(transaction.getProviderId());
+    ServiceProvider serviceProvider = (ServiceProvider) serviceProviderDAO.find(transaction.getProviderId());
 
     if ( serviceProvider.getUserId() == null ) {
       throw new RuntimeException("Service Provider has no user defined.");
@@ -64,7 +57,8 @@ public class ServiceProviderFeeTransactionDAO
     //Creating Another transaction for the broker fees
     Transaction serviceProviderTransaction = new Transaction();
 
-    serviceProviderTransaction.setPayeeId((long) serviceProvider.getUserId());
+    System.out.println("serviceProvider USER :"+serviceProvider.getUserId());
+    serviceProviderTransaction.setPayeeId((Long) serviceProvider.getUserId());
 
     if ( transaction.getType() == TransactionType.CASHOUT ) {
       serviceProviderTransaction.setPayerId(transaction.getPayerId());
@@ -73,19 +67,24 @@ public class ServiceProviderFeeTransactionDAO
     } else {
       throw new RuntimeException("Transaction Type is not defined.");
     }
-
-
-    serviceProviderTransaction.setAmount( ((Fee) serviceProvider.getFee()).getFee(transaction.getAmount()));
-
+    System.out.println("Transaction PAYER ID :"+transaction.getPayerId());
+    System.out.println("Transaction PAYEE ID :"+transaction.getPayeeId());
+    System.out.println("serviceProviderTransaction PAYER ID :"+serviceProviderTransaction.getPayerId());
+    System.out.println("serviceProviderTransaction PAYEE ID :"+serviceProviderTransaction.getPayeeId());
+    Long serviceProviderTransactionFee = ((Fee) serviceProvider.getFee()).getFee(transaction.getAmount());
+    System.out.println("SERVICE PROVIDER :"+serviceProvider);
+    System.out.println("SERVICE PROVIDER FEE:"+serviceProvider.getFee());
+    System.out.println("SERVICE PROVIDER TRANSACTION FEE:"+serviceProviderTransactionFee);
+    serviceProviderTransaction.setAmount( serviceProviderTransactionFee > 0 ? serviceProviderTransactionFee : 100);
+    System.out.println("serviceProviderTransaction AMOUNT:"+serviceProviderTransaction.getAmount());
     Long firstLock  = transaction.getPayerId() < transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
     Long secondLock = transaction.getPayerId() > transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
 
     synchronized ( firstLock ) {
       synchronized ( secondLock ) {
         try {
-          DAO transactionDAO = (TransactionDAO) x.get("transactionDAO");
-
-          serviceProviderTransaction = (Transaction) transactionDAO.put(serviceProviderTransaction);
+          DAO tDAO = (DAO) context.get("transactionDAO");
+          serviceProviderTransaction = (Transaction) tDAO.put(serviceProviderTransaction);
 
           //Adding the created transaction to the "main" transaction
           transaction.setFeeTransactions(Arrays.append(transaction.getFeeTransactions(), serviceProviderTransaction));
