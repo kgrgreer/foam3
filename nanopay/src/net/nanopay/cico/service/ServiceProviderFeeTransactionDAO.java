5e41c316932cad78045825160d500758af32d134
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-01 15:51:36 -0400

    fixing ServicerProviderFeeTransactionDAO logic

--- nanopay/src/net/nanopay/cico/service/ServiceProviderFeeTransactionDAO.java
+++ nanopay/src/net/nanopay/cico/service/ServiceProviderFeeTransactionDAO.java
@@ -9,7 +9,7 @@ import foam.util.Arrays;
 import net.nanopay.cico.model.ServiceProvider;
 import net.nanopay.cico.model.TransactionType;
 import net.nanopay.tx.TransactionDAO;
-import net.nanopay.tx.model.Fee;
+import net.nanopay.tx.model.FeeInterface;
 import net.nanopay.tx.model.Transaction;
 
 public class ServiceProviderFeeTransactionDAO
@@ -29,26 +29,20 @@ public class ServiceProviderFeeTransactionDAO
   public FObject put_(X x, FObject obj) throws RuntimeException {
     Transaction transaction = (Transaction) obj;
 
-    X context = x != null ? x : getX();
+    if ( transaction.getProviderId() == null ) {
+      return getDelegate().put_(x, transaction);
+    }
 
-    DAO userDAO = (DAO) context.get("localUserDAO");
-    DAO serviceProviderDAO = (DAO) context.get("cicoServiceProviderDAO");
+    DAO userDAO = (DAO) x.get("localUserDAO");
+    DAO serviceProviderDAO = (DAO) x.get("cicoServiceProviderDAO");
 
     User payee = (User) userDAO.find(transaction.getPayeeId());
     User payer = (User) userDAO.find(transaction.getPayerId());
 
-    if ( transaction.getProviderId() == null ) {
-      return obj;
-    }
-
     ServiceProvider serviceProvider = (ServiceProvider) serviceProviderDAO.find(transaction.getProviderId());
 
-    if ( serviceProvider.getUserId() == null ) {
-      throw new RuntimeException("Service Provider has no user defined.");
-    }
-
-    if ( serviceProvider.getFee() == null ) {
-      throw new RuntimeException("Service Provider has no fee defined.");
+    if ( serviceProvider.getUserId() == null || serviceProvider.getFee() == null ) {
+      return getDelegate().put_(x, transaction);
     }
 
     //Creating Another transaction for the broker fees
@@ -64,10 +58,7 @@ public class ServiceProviderFeeTransactionDAO
       throw new RuntimeException("Transaction Type is not defined.");
     }
 
-    //temporary test
-    Long serviceProviderTransactionFee = ((Fee) serviceProvider.getFee()).getFee(transaction.getAmount());
-    //temporary test
-    serviceProviderTransaction.setAmount( serviceProviderTransactionFee > 0 ? serviceProviderTransactionFee : 100);
+    serviceProviderTransaction.setAmount( ((FeeInterface) serviceProvider.getFee()).getFee(transaction.getAmount()) );
 
     Long firstLock  = transaction.getPayerId() < transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
     Long secondLock = transaction.getPayerId() > transaction.getPayeeId() ? transaction.getPayerId() : transaction.getPayeeId();
@@ -75,13 +66,13 @@ public class ServiceProviderFeeTransactionDAO
     synchronized ( firstLock ) {
       synchronized ( secondLock ) {
         try {
-          DAO tDAO = (DAO) context.get("transactionDAO");
+          DAO tDAO = (DAO) x.get("transactionDAO");
           serviceProviderTransaction = (Transaction) tDAO.put(serviceProviderTransaction);
 
           //Adding the created transaction to the "main" transaction
           transaction.setFeeTransactions(Arrays.append(transaction.getFeeTransactions(), serviceProviderTransaction));
 
-          return getDelegate().put(transaction);
+          return getDelegate().put_(x, transaction);
 
         } catch (RuntimeException e) {
           throw e;
