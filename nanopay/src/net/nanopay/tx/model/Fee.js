03f8d89a2d583de9e4592962c0abf94415ffee79
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-01 15:48:39 -0400

    Fee to implement FeeInterface and removing old methods

--- nanopay/src/net/nanopay/tx/model/Fee.js
+++ nanopay/src/net/nanopay/tx/model/Fee.js
@@ -1,43 +1,13 @@
 foam.CLASS({
   package: 'net.nanopay.tx.model',
   name: 'Fee',
-
+  abstract: true,
+  implements: ['net.nanopay.tx.model.FeeInterface'],
   properties: [
     {
       class: 'foam.core.Enum',
       of: 'net.nanopay.tx.model.FeeType',
       name: 'type'
-    }
-  ],
-
-  methods: [
-    {
-      name: 'getFee',
-      args: [
-        {
-          name: 'transactionAmount',
-          javaType: 'Long'
-        }
-      ],
-      javaReturns: 'Long',
-      javaCode: ' return 0L; ',
-      code: function() {
-        return 0;
-      }
-    },
-    {
-      name: 'getTotalAmount',
-      args: [
-        {
-          name: 'transactionAmount',
-          javaType: 'Long'
-        }
-      ],
-      javaReturns: 'Long',
-      javaCode: ' return transactionAmount; ',
-      code: function() {
-        return transactionAmount;
-      }
     }
   ]
  });
