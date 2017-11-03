b419a1cb52614eb03a953dc18bc9d69376e32377
Mike Carcasole <mikecarcasole@gmail.com>
2017-11-03 17:04:03 -0400

    Add a TransactionRowIBView to swiftfoam.

---
+++ swiftfoam/src/TransactionRowIBView.js
@@ -0,0 +1,9 @@
+foam.CLASS({
+  class: 'foam.swift.ui.GenIBOutletDetailViewModel',
+  name: 'TransactionRowIBView',
+  classes: [
+    'foam.nanos.auth.User',
+    'net.nanopay.tx.model.Transaction',
+  ],
+  viewType: 'UITableViewCell',
+});
