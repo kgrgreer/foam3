e5f835f4e33cc0033ed13dbbc57aa61c913397ef
Kirk Eaton <kirk@nanopay.net>
2017-11-07 20:12:05 -0500

    Added ResetPasswordWebAgent

---
+++ nanopay/src/net/nanopay/auth/password/ResetPasswordWebAgent.java
@@ -0,0 +1,13 @@
+package net.nanopay.auth.password;
+
+import foam.core.X;
+import foam.nanos.http.WebAgent;
+
+public class ResetPasswordWebAgent
+    implements WebAgent
+{
+  @Override
+  public void execute(X x) {
+    // TODO: redirect to reset password screen
+  }
+}
