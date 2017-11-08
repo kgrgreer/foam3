11570458b740651d0ad9429a3879418be4260e60
blakegreen <blake@nanopay.net>
2017-11-08 16:57:40 -0500

    Added placeholders for tables with empty data, also added error handling if the user tries cashing in or out without a bank account

--- nanopay/src/net/nanopay/cico/ui/bankAccount/BankAccountsView.js
+++ nanopay/src/net/nanopay/cico/ui/bankAccount/BankAccountsView.js
@@ -109,7 +109,8 @@ foam.CLASS({
     { name: 'TitleVerified',    message: 'Verified Account(s)' },
     { name: 'TitleUnverified',  message: 'Unverified Account(s)' },
     { name: 'ActionAdd',        message: 'Add a new bank account' },
-    { name: 'MyBankAccounts',   message: 'My Bank Accounts' }
+    { name: 'MyBankAccounts',   message: 'My Bank Accounts' },
+    { name: 'placeholderText',  message: 'You don\'t have any bank accounts right now. Click the Add a bank account button to add a new bank account.' }
   ],
 
   methods: [
@@ -152,6 +153,7 @@ foam.CLASS({
               summaryView: this.BankAccountTableView.create()
             })
           .end()
+          .tag({ class: 'net.nanopay.ui.Placeholder', dao: this.bankAccountDAO, message: this.placeholderText, image: 'images/icon_bank_account_black.png' })
     }
   ],
 
