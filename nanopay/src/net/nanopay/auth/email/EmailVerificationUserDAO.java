46e8090396c3a85d45826920e389c63125a1f1cf
Kirk Eaton <kirk@nanopay.net>
2017-11-08 19:17:06 -0800

    Added EmailVerificationUserDAO decorator

---
+++ nanopay/src/net/nanopay/auth/email/EmailVerificationUserDAO.java
@@ -0,0 +1,28 @@
+package net.nanopay.auth.email;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.mlang.sink.Count;
+import foam.nanos.auth.User;
+
+public class EmailVerificationUserDAO
+    extends ProxyDAO
+{
+  public EmailVerificationUserDAO(X x, DAO delegate) {
+    setX(x);
+    setDelegate(delegate);
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    EmailTokenService emailToken = (EmailTokenService) getX().get("emailToken");
+    User user = (User) obj;
+    // assume new user if id is 0 or cannot find user in DAO
+    if ( user.getId() == 0 || getDelegate().find(user.getId()) == null ) {
+      emailToken.generateToken(user);
+    }
+    return super.put_(x, obj);
+  }
+}
