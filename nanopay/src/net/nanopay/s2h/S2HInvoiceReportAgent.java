d663c3a7cb511b1f2c00bf03770830b6524e6471
Waseem <waseem@nanopay.net>
2017-11-10 16:43:00 -0500

    set up reporting by time periods

--- nanopay/src/net/nanopay/s2h/S2HInvoiceReportAgent.java
+++ nanopay/src/net/nanopay/s2h/S2HInvoiceReportAgent.java
@@ -29,8 +29,9 @@ public class S2HInvoiceReportAgent
     int year = calendar.get(Calendar.YEAR);
     int month = calendar.get(Calendar.MONTH);
     int day = calendar.get(Calendar.DAY_OF_MONTH);
-    dayStart.set(year, month, day-14, 0, 0, 0);
     dayEnd.set(year, month, day, 0, 0, 0);
+    dayStart.setTimeInMillis(dayEnd.getTimeInMillis()- 1213200000);
+
     dayEnd.setTimeInMillis(dayEnd.getTimeInMillis()- 1000);
   }
   public S2HInvoiceReportAgent(int Startyear, int Startmonth, int Startday){
@@ -78,7 +79,7 @@ public class S2HInvoiceReportAgent
         list += "<tr><td align=\"left\">"+ invoice.getInvoiceNumber()+"</td><td style=\"text-align: left\">" + invoice.getIssueDate() + "</td><td style=\"text-align: left\">"+userDAO.find(invoice.getPayerId()).getProperty(User.ORGANIZATION.getName())+ "</td><td style=\"text-align: left\">" + invoice.getStatus() + "</td><td style=\"text-align: right\">"+  formatter.format(invoice.getAmount()) +"</td></tr>";
         sum += invoice.getAmount();
       }
-      list +="<tr><td align=\"left\"></td><td style=\"text-align: left\"><b>TOTAL</b></td><td style=\"text-align: right\"><b>"+  formatter.format(sum) +"</b></td></tr>";
+      list +="<tr><td align=\"left\"></td><td align=\"left\"></td><td style=\"text-align: left\"><b>TOTAL</b></td><td align=\"left\"></td><td style=\"text-align: right\"><b>"+  formatter.format(sum) +"</b></td></tr>";
       args.put("title1", "Invoices between: ");
       args.put("list1", list);
       args.put("date1", dayStart.getTime());
