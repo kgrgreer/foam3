98692a83a9a3226e28abab358113129313af35cf
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-14 15:12:38 -0500

    adding user transaction limit services and interfaces to files.js

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -49,6 +49,8 @@ FOAM_FILES([
   { name: 'net/nanopay/retail/ui/devices/form/DevicePasswordForm', flags: ['web'] },
 
   // tx
+  { name: 'net/nanopay/tx/UserTransactionLimitInterface' },
+  { name: 'net/nanopay/tx/client/ClientUserTransactionLimitService' },
   { name: 'net/nanopay/tx/model/TransactionPurpose' },
   { name: 'net/nanopay/tx/model/TransactionLimitTimeFrame' },
   { name: 'net/nanopay/tx/model/TransactionLimitType' },
@@ -60,6 +62,7 @@ FOAM_FILES([
   { name: 'net/nanopay/tx/model/InformationalFee' },
   { name: 'net/nanopay/tx/model/FixedFee' },
   { name: 'net/nanopay/tx/model/PercentageFee' },
+
   { name: 'net/nanopay/model/Broker' },
 
   { name: 'net/nanopay/tx/ui/TransactionsView', flags: ['web'] },
