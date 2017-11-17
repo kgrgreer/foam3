9cac07c8f2d6516333e680c8a20064e43c8a99e2
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-17 12:05:32 -0500

    implementing interface to return correct types

--- nanopay/src/net/nanopay/tx/client/ClientUserTransactionLimitService.js
+++ nanopay/src/net/nanopay/tx/client/ClientUserTransactionLimitService.js
@@ -2,6 +2,10 @@ foam.CLASS({
   package: 'net.nanopay.tx.client',
   name: 'ClientUserTransactionLimitService',
 
+  implements: [
+    'net.nanopay.tx.UserTransactionLimit'
+  ],
+
   properties: [
     {
       class: 'Stub',
