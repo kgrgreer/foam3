e8771f068e641756902a7b8ac027c7f26fa76406
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-17 11:28:54 -0500

    fixing last commit problem

--- nanopay/src/net/nanopay/tx/client/ClientUserTransactionLimitService.js
+++ nanopay/src/net/nanopay/tx/client/ClientUserTransactionLimitService.js
@@ -2,10 +2,6 @@ foam.CLASS({
   package: 'net.nanopay.tx.client',
   name: 'ClientUserTransactionLimitService',
 
-  implements: [
-    'net.nanopay.tx.UserTransactionLimit'
-  ],
-
   properties: [
     {
       class: 'Stub',
