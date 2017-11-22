612915b6c8aa45b8d2607d48d1b8b6c5df3dac66
blakegreen <blake@nanopay.net>
2017-11-22 16:37:14 -0500

    Setup all shopper information properties for inputting data on the shopper info wizard view page

--- nanopay/src/net/nanopay/admin/ui/form/AddShopperInfoForm.js
+++ nanopay/src/net/nanopay/admin/ui/form/AddShopperInfoForm.js
@@ -8,7 +8,8 @@ foam.CLASS({
   imports: [
     'viewData',
     'goBack',
-    'goNext'
+    'goNext',
+    'regionDAO'
   ],
 
   axioms: [
@@ -21,9 +22,122 @@ foam.CLASS({
 
   messages: [
     { name: 'Step', message: 'Step 1: Fill in shopper\'s info and create a password' },
-    { name: 'Title', message: 'Personal Information' },
+    { name: 'PersonalInformation', message: 'Personal Information' },
     { name: 'UploadImage', message: 'Upload Image' },
     { name: 'FirstName', message: 'First Name' },
     { name: 'LastName', message: 'Last Name' },
+    { name: 'EmailAddress', message: 'Email Address' },
+    { name: 'PhoneNumber', message: 'Phone Number' },
+    { name: 'Birthday', message: 'Birthday' },
+    { name: 'HomeAddress', message: 'Home Address' },
+    { name: 'StNo', message: 'St No.' },
+    { name: 'StName', message: 'St Name' },
+    { name: 'AddressLine', message: 'Address line' },
+    { name: 'City', message: 'City' },
+    { name: 'Province', message: 'Province' },
+    { name: 'PostalCode', message: 'Postal Code' },
+    { name: 'Password', message: 'Password' },
+    { name: 'CreateAPassword', message: 'Create a Password' }
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'firstName',
+      postSet: function(oldValue, newValue) {
+        this.viewData.firstName = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'lastName',
+      postSet: function(oldValue, newValue) {
+        this.viewData.lastName = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'emailAddress',
+      postSet: function(oldValue, newValue) {
+        this.viewData.emailAddress = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'phoneNumber',
+      postSet: function(oldValue, newValue) {
+        this.viewData.phoneNumber = newValue;
+      }
+    },
+    {
+      class: 'Date',
+      name: 'birthday',
+      tableCellFormatter: function(date) {
+        this.add(date ? date.toISOString().substring(0,10) : '');
+      }
+    },
+    {
+      class: 'String',
+      name: 'streetNumber',
+      postSet: function(oldValue, newValue) {
+        this.viewData.streetNumber = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'streetName',
+      postSet: function(oldValue, newValue) {
+        this.viewData.streetName = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'addressLine',
+      postSet: function(oldValue, newValue) {
+        this.viewData.addressLine = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'city',
+      postSet: function(oldValue, newValue) {
+        this.viewData.city = newValue;
+      }
+    },
+    {
+      name: 'province',
+      view: function(_, X) {
+        return foam.u2.view.ChoiceView.create({
+          dao: X.regionDAO,
+          objToChoice: function(a){
+            return [a.id, a.name];
+          }
+        })
+      }
+    },
+    {
+      class: 'String',
+      name: 'postalCode',
+      postSet: function(oldValue, newValue) {
+        this.viewData.postalCode = newValue;
+      }
+    },
+    {
+      class: 'String',
+      name: 'password',
+      postSet: function(oldValue, newValue) {
+        this.viewData.password = newValue;
+      }
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+      this
+        .addClass(this.myClass())
+        .start()
+        .end()
+    }
   ]
-})
+});
