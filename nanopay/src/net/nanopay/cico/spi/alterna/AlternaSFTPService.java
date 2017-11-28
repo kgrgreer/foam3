15aee77cf395e4d643e1e47e6ddbf069bb330033
marcogbarcellos <marcogbarcellos@gmail.com>
2017-11-28 17:03:15 -0500

    add alterna sftp version draw

--- nanopay/src/net/nanopay/cico/spi/alterna/AlternaSFTPService.java
+++ nanopay/src/net/nanopay/cico/spi/alterna/AlternaSFTPService.java
@@ -1,8 +1,17 @@
+package net.nanopay.cico.spi.alterna;
+
+import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
 
 import foam.core.ContextAwareSupport;
 import foam.core.Detachable;
@@ -11,66 +20,72 @@ import foam.dao.AbstractSink;
 import foam.dao.DAO;
 import foam.mlang.MLang;
 import foam.nanos.pm.PM;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Date;
-import net.nanopay.fx.model.ExchangeRate;
-import net.nanopay.fx.model.ExchangeRateQuote;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import foam.nanos.http.WebAgent;
+import net.nanopay.cico.spi.alterna.SFTPService;
+import net.nanopay.cico.spi.alterna.AlternaWebAgent;
 
-import org.apache.commons.net.PrintCommandListener;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPSClient;
-import org.apache.commons.net.ftp.FTPReply;
+public class AlternaSFTPService extends ContextAwareSupport implements SFTPService {
 
-public class AlternaSFTPService extends ContextAwareSupport {
 
-  FTPSClient ftp = null;
+  protected WebAgent agent_;
 
-  public static void main(String[] args) throws Exception {
+  protected final String HOST = "ftp.eftcanada.com";
+  protected final int PORT = 22;
+  protected final String USER = "eftcadtest1";
+  protected final String PASSWORD = "nLGlp8Du";
+  protected final String WORKING_DIR = "/";
+
+  @Override
+  public void sendCICOFile() {
+    agent_.execute(getX());
+
     String fileName = "test.txt";
-    String SFTPHOST = "ftp.eftcanada.com";
-    int SFTPPORT = 22;
-    String SFTPUSER = "eftcadtest1";
-    String SFTPPASS = "nLGlp8Du";
-    String SFTPWORKINGDIR = "/";
 
     Session session = null;
     Channel channel = null;
     ChannelSftp channelSftp = null;
     System.out.println("preparing the host information for sftp.");
+    try {
 
-    JSch jsch = new JSch();
-    session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
-    session.setPassword(SFTPPASS);
-    java.util.Properties config = new java.util.Properties();
-    config.put("StrictHostKeyChecking", "no");
-    session.setConfig(config);
-    System.out.println("Session:"+session);
-    session.connect();
-    System.out.println("Host connected.");
-    channel = session.openChannel("sftp");
-    channel.connect();
-    System.out.println("sftp channel opened and connected.");
-    channelSftp = (ChannelSftp) channel;
-    channelSftp.cd(SFTPWORKINGDIR);
-    File f = new File(fileName);
-    f.createNewFile();
-    channelSftp.put(new FileInputStream(f), f.getName());
-    System.out.println("File transfered successfully to host.");
+      JSch jsch = new JSch();
+      session = jsch.getSession(USER, HOST, PORT);
+      session.setPassword(PASSWORD);
+      java.util.Properties config = new java.util.Properties();
+      config.put("StrictHostKeyChecking", "no");
+      session.setConfig(config);
+      System.out.println("Session:"+session);
+      session.connect();
+      System.out.println("Host connected.");
+      channel = session.openChannel("sftp");
+      channel.connect();
+      System.out.println("sftp channel opened and connected.");
+      channelSftp = (ChannelSftp) channel;
+      channelSftp.cd(WORKING_DIR);
+      File f = new File(fileName);
+      f.createNewFile();
+      channelSftp.put(new FileInputStream(f), f.getName());
+      System.out.println("File transfered successfully to host.");
+      channelSftp.exit();
+      System.out.println("sftp Channel exited.");
+      channel.disconnect();
+      System.out.println("Channel disconnected.");
+      session.disconnect();
 
+    } catch( JSchException | IOException | SftpException e ) {
+       System.out.println("Exception: " + e);
+    } /*catch( IOException e ) {
+      System.out.println("Exception: " + e);
+    } catch( SftpException e ) {
+      System.out.println("Exception: " + e);
+    } */finally {
+      System.out.println("Host Session disconnected.");
+    }
+  }
 
-    channelSftp.exit();
-    System.out.println("sftp Channel exited.");
-    channel.disconnect();
-    System.out.println("Channel disconnected.");
-    session.disconnect();
-    System.out.println("Host Session disconnected.");
+  @Override
+  public void start() {
+    agent_ = (WebAgent) getX().get("alterna");
+
   }
 
 }
