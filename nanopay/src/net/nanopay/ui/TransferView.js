bdca86bb4bec4eb59835a8c076af536f7325a906
Kevin Greer <kgrgreer@gmail.com>
2017-12-17 12:20:11 -0500

    Switch to new css: style.

--- nanopay/src/net/nanopay/ui/TransferView.js
+++ nanopay/src/net/nanopay/ui/TransferView.js
@@ -17,75 +17,71 @@ foam.CLASS({
     'foam.u2.dialog.NotificationMessage'
   ],
 
-  axioms: [
-    foam.u2.CSS.create({
-      code: function CSS() {/*
-        ^{
-          width: 992px;
-          margin: auto;
-        }
-        ^ .label{
-          margin-left: 0;
-          margin-top: 15px;
-        }
-        ^ .foam-u2-tag-Select{
-          width: 100%;
-          height: 40px;
-          border-radius: 0;
-          -webkit-appearance: none;
-          -moz-appearance: none;
-          appearance: none;
-          padding: 0 15px;
-          border: solid 1px rgba(164, 179, 184, 0.5);
-          background-color: white;
-          outline: none;
-        }
-        ^ .caret {
-          position: relative;
-        }
-        ^ .caret:before {
-          content: '';
-          position: absolute;
-          top: -22px;
-          left: 420px;
-          border-top: 7px solid #a4b3b8;
-          border-left: 7px solid transparent;
-          border-right: 7px solid transparent;
-        }
-        ^ .caret:after {
-          content: '';
-          position: absolute;
-          left: 12px;
-          top: 0;
-          border-top: 0px solid #ffffff;
-          border-left: 0px solid transparent;
-          border-right: 0px solid transparent;
-        }
-        ^ .white-container{
-          width: 450px;
-          height: 380px;
-          margin: auto;
-          margin-top: 50px;
-        }
-        ^ .half-small-input-box{
-          width: 100%;
-        }
-        ^ .light-roboto-h2{
-          margin-left: 150px;
-          margin-bottom: 15px;
-        }
-        ^ .btn{
-          margin-top: 25px;
-        }
-        ^ .property-note {
-          height: auto;
-        }
-        ^ .net-nanopay-ui-ActionView-transferValue {
-          margin-right: 0;
-        }
-      */}
-    })
-  ],
+  css: `
+    ^{
+      width: 992px;
+      margin: auto;
+    }
+    ^ .label{
+      margin-left: 0;
+      margin-top: 15px;
+    }
+    ^ .foam-u2-tag-Select{
+      width: 100%;
+      height: 40px;
+      border-radius: 0;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      padding: 0 15px;
+      border: solid 1px rgba(164, 179, 184, 0.5);
+      background-color: white;
+      outline: none;
+    }
+    ^ .caret {
+      position: relative;
+    }
+    ^ .caret:before {
+      content: '';
+      position: absolute;
+      top: -22px;
+      left: 420px;
+      border-top: 7px solid #a4b3b8;
+      border-left: 7px solid transparent;
+      border-right: 7px solid transparent;
+    }
+    ^ .caret:after {
+      content: '';
+      position: absolute;
+      left: 12px;
+      top: 0;
+      border-top: 0px solid #ffffff;
+      border-left: 0px solid transparent;
+      border-right: 0px solid transparent;
+    }
+    ^ .white-container{
+      width: 450px;
+      height: 380px;
+      margin: auto;
+      margin-top: 50px;
+    }
+    ^ .half-small-input-box{
+      width: 100%;
+    }
+    ^ .light-roboto-h2{
+      margin-left: 150px;
+      margin-bottom: 15px;
+    }
+    ^ .btn{
+      margin-top: 25px;
+    }
+    ^ .property-note {
+      height: auto;
+    }
+    ^ .net-nanopay-ui-ActionView-transferValue {
+      margin-right: 0;
+    }
+  `,
 
   properties: [
     'payee',
@@ -156,8 +152,8 @@ foam.CLASS({
         var transaction = self.Transaction.create({
           payeeId: self.payees,
           payerId: self.user.id,
-          amount: self.transferAmount,
-          notes: self.note
+          amount:  self.transferAmount,
+          notes:   self.note
         });
 
         self.transactionDAO.put(transaction).then(function(response) {
@@ -166,7 +162,6 @@ foam.CLASS({
         }).catch(function(error) {
           self.add(self.NotificationMessage.create({ message: error.message, type: 'error' }));
         });
-      
       }
     }
   ]
