596677b7fb806a71f518f4d2a71d6dfa846c7ead
blakegreen <blake@nanopay.net>
2018-01-02 16:58:27 -0500

    Fixed Record Payment button and void drop down in sales detail view, fixed margins and table width to match rest of page

--- nanopay/src/net/nanopay/invoice/ui/SalesDetailView.js
+++ nanopay/src/net/nanopay/invoice/ui/SalesDetailView.js
@@ -5,7 +5,9 @@ foam.CLASS({
 
   requires: [ 
     'net.nanopay.invoice.model.Invoice', 
-    'foam.u2.dialog.Popup' 
+    'foam.u2.PopupView',
+    'foam.u2.dialog.Popup',
+    'foam.u2.dialog.NotificationMessage'
   ],
 
   imports: [ 
@@ -15,7 +17,10 @@ foam.CLASS({
     'ctrl'
   ],
 
-  exports: [ 'hideReceivableSummary' ],
+  exports: [
+    'as data',
+    'hideReceivableSummary' 
+  ],
   
   implements: [
     'foam.mlang.Expressions', 
@@ -24,19 +29,75 @@ foam.CLASS({
   axioms: [
     foam.u2.CSS.create({
       code: function CSS() {/*
+        ^ {
+          width: 962px;
+        }
         ^ h5{
           opacity: 0.6;
           font-size: 20px;
           font-weight: 300;
           line-height: 1;
           color: #093649;
-          padding-top: 70px;
+        }
+        ^ .net-nanopay-ui-ActionView-backAction {
+          border: 1px solid lightgrey;
+          background-color: rgba(164, 179, 184, 0.1);
+        }
+        ^ .net-nanopay-ui-ActionView-recordPayment {
+          background-color: #59A5D5;
+          border: solid 1px #59A5D5;
+          color: white;
+          float: right;
+          margin-right: 1px;
+        }
+        ^ .net-nanopay-ui-ActionView-voidDropDown {
+          width: 30px;
+          height: 40px;
+          background-color: #59A5D5;
+          border: solid 1px #59A5D5;
+          float: right;
+        }
+        ^ .net-nanopay-ui-ActionView-voidDropDown::after {
+          content: ' ';
+          position: absolute;
+          height: 0;
+          width: 0;
+          border: 6px solid transparent;
+          border-top-color: white;
+          transform: translate(-6.5px, -1px);
+        }
+        ^ .popUpDropDown {
+          padding: 0;
+          z-index: 10000;
+          width: 165px;
+          background: white;
+          opacity: 1;
+          box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.19);
+          position: absolute;
+        }
+        ^ .popUpDropDown > div {
+          width: 165px;
+          height: 30px;
+          font-size: 14px;
+          font-weight: 300;
+          letter-spacing: 0.2px;
+          color: #093649;
+          line-height: 30px;
+        }
+        ^ .popUpDropDown > div:hover {
+          background-color: #59a5d5;
+          color: white;
+          cursor: pointer;
         }
         */
       }
     })
   ],
 
+  properties: [
+    'voidMenuBtn_',
+    'voidPopUp_'
+  ],
 
   methods: [
     function initE() {
@@ -47,17 +108,8 @@ foam.CLASS({
       this
         .addClass(this.myClass())
         .start(this.BACK_ACTION).end()
-        .tag({ 
-          class: 'net.nanopay.invoice.ui.shared.ActionInterfaceButton', 
-          data: this.data,
-          detailActions: { 
-            invoice: this.data,
-            buttonLabel: 'Record Payment', 
-            buttonAction: this.recordPaymentModal,
-            subMenu1: 'Void',
-            subMenuAction1: this.voidPopUp 
-          }
-        })
+        .start(this.VOID_DROP_DOWN, null, this.voidMenuBtn_$).end()
+        .start(this.RECORD_PAYMENT).end()
         .start('h5').add('Bill to ', this.data.payerName).end()
         .tag({ class: 'net.nanopay.invoice.ui.shared.SingleItemView', data: this.data })
         .start('h2').addClass('light-roboto-h2').style({ "margin-bottom": "0px"})
@@ -70,23 +122,6 @@ foam.CLASS({
     }
   ],
 
-  listeners: [
-    function recordPaymentModal(){
-      if(this.data.paymentMethod.name != "NONE"){
-        this.add(foam.u2.dialog.NotificationMessage.create({ message: 'Invoice has been ' + this.data.paymentMethod.label + '.', type: 'error' }));
-        return;
-      }
-      this.ctrl.add(this.Popup.create().tag({class: 'net.nanopay.invoice.ui.modal.RecordPaymentModal', invoice: this.data }));
-    },
-    function voidPopUp(){
-      if(this.data.paymentMethod.name != "NONE"){
-        this.add(foam.u2.dialog.NotificationMessage.create({ message: 'Invoice has been ' + this.data.paymentMethod.label + '.', type: 'error' }));
-        return;
-      }
-      this.ctrl.add(this.Popup.create().tag({class: 'net.nanopay.invoice.ui.modal.DisputeModal', invoice: this.data }));
-    }
-  ],
-
   actions: [
     {
       name: 'backAction',
@@ -94,6 +129,48 @@ foam.CLASS({
       code: function(X){
         X.stack.push({ class: 'net.nanopay.invoice.ui.SalesView'});
       }
+    },
+    {
+      name: 'recordPayment',
+      label: 'Record Payment',
+      code: function(X) {
+        var self = this;
+        if(this.data.paymentMethod.name != "NONE"){
+          self.add(self.NotificationMessage.create({ message: 'Invoice has been ' + this.data.paymentMethod.label + '.', type: 'error' }));
+          return;
+        }
+        X.ctrl.add(foam.u2.dialog.Popup.create(undefined, X).tag({class: 'net.nanopay.invoice.ui.modal.RecordPaymentModal', invoice: this.data }));
+      }
+    },
+    {
+      name: 'voidDropDown',
+      label: '',
+      code: function(X) {
+         var self = this;
+
+         self.voidPopUp_ = foam.u2.PopupView.create({
+           width: 165,
+           x: -137,
+           y: 40
+         })
+         self.voidPopUp_.addClass('popUpDropDown')
+          .start('div').add('Void')
+            .on('click', this.voidPopUp)
+          .end()
+        self.voidMenuBtn_.add(self.voidPopUp_)
+      }
+    }
+  ],
+
+  listeners: [
+    function voidPopUp() {
+      var self = this;
+      self.voidPopUp_.remove();
+      if(this.data.paymentMethod.name != "NONE"){
+        self.add(self.NotificationMessage.create({ message: 'Invoice has been ' + this.data.paymentMethod.label + '.', type: 'error' }));
+        return;
+      }
+      this.ctrl.add(this.Popup.create().tag({class: 'net.nanopay.invoice.ui.modal.DisputeModal', invoice: this.data }));
     }
   ]
 });
