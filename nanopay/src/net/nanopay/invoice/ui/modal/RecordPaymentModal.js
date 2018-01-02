19ca3d34c56d1513ff8a441848dee982599a438a
blakegreen <blake@nanopay.net>
2018-01-02 12:23:37 -0500

    Fixed record payment modal

--- nanopay/src/net/nanopay/invoice/ui/modal/RecordPaymentModal.js
+++ nanopay/src/net/nanopay/invoice/ui/modal/RecordPaymentModal.js
@@ -7,7 +7,6 @@ foam.CLASS({
   documentation: 'Record Payment Modal',
 
   requires: [
-    'net.nanopay.ui.modal.ModalHeader',
     'foam.u2.dialog.NotificationMessage'
   ],
 
@@ -32,6 +31,10 @@ foam.CLASS({
     'invoice'
   ],
 
+  messages: [
+    { name: 'Title', message: 'Record Payment' }
+  ],
+
   axioms: [
     foam.u2.CSS.create({
       code: function CSS() {/*
@@ -43,6 +46,40 @@ foam.CLASS({
         ^ .net-nanopay-ui-ActionView-close{
           right: -280px !important;
         }
+        ^ .popUpHeader {
+          width: 448px;
+          height: 40px;
+          background-color: #093649;
+        }
+        ^ .popUpTitle {
+          width: 198px;
+          height: 40px;
+          font-family: Roboto;
+          font-size: 14px;
+          line-height: 40.5px;
+          letter-spacing: 0.2px;
+          text-align: left;
+          color: #ffffff;
+          margin-left: 20px;
+          display: inline-block;
+        }
+        ^ .net-nanopay-ui-ActionView-closeButton {
+          width: 24px;
+          height: 24px;
+          margin: 0;
+          margin-top: 7px;
+          margin-right: 20px;
+          display: inline-block;
+          float: right;
+          outline: 0;
+          border: none;
+          background: transparent;
+          box-shadow: none;
+        }
+        ^ .net-nanopay-ui-ActionView-closeButton:hover {
+          background: transparent;
+          background-color: transparent;
+        }
     */}
     })
   ],
@@ -53,11 +90,12 @@ foam.CLASS({
       var self = this;
 
       this
-      .tag(this.ModalHeader.create({
-        title: 'Record Payment'
-      }))
       .addClass(this.myClass())
       .start()
+        .start().addClass('popUpHeader')
+          .start().add(this.Title).addClass('popUpTitle').end()
+          .add(this.CLOSE_BUTTON)
+        .end()
         .start().addClass('key-value-container')
           .start()
             .start().addClass('key').add('Company').end()
@@ -78,6 +116,13 @@ foam.CLASS({
   ],
 
   actions: [
+    {
+      name: 'closeButton',
+      icon: 'images/ic-cancelwhite.svg',
+      code: function(X) {
+        X.closeDialog();
+      }
+    },
     {
       name: 'record',
       label: 'Record Payment',
