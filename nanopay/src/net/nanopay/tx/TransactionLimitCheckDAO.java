2d16dcde8ceae4c7efa01d6fd925942a298acf55
Kevin Greer <kgrgreer@gmail.com>
2018-01-13 17:23:11 -0500

    Formatting.

--- nanopay/src/net/nanopay/tx/TransactionLimitCheckDAO.java
+++ nanopay/src/net/nanopay/tx/TransactionLimitCheckDAO.java
@@ -19,7 +19,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class TransactionLimitCheckDAO
-    extends ProxyDAO {
+  extends ProxyDAO
+{
+
+  // Constants used to get the predefined limit values from the Transaction limits(through the property name)
+  protected static final String DEFAULT_USER_TRANSACTION_LIMIT   = "default_user";
+  protected static final String DEFAULT_BROKER_TRANSACTION_LIMIT = "default_broker";
 
   public TransactionLimitCheckDAO(DAO delegate) {
     setDelegate(delegate);
@@ -29,17 +34,14 @@ public class TransactionLimitCheckDAO
     setX(x);
     setDelegate(delegate);
   }
-  // Constants used to get the predefined limit values from the Transaction limits(through the property name)
-  private static final String DEFAULT_USER_TRANSACTION_LIMIT = "default_user";
-  private static final String DEFAULT_BROKER_TRANSACTION_LIMIT = "default_broker";
 
-  @Override
+  // @Override
   public FObject put_(X x, FObject fObject) throws RuntimeException {
     Transaction transaction = (Transaction) fObject;
 
-    DAO userDAO = (DAO) getX().get("localUserDAO");
-    User payee = (User) userDAO.find(transaction.getPayeeId());
-    User payer = (User) userDAO.find(transaction.getPayerId());
+    DAO userDAO = (DAO)  getX().get("localUserDAO");
+    User payee  = (User) userDAO.find(transaction.getPayeeId());
+    User payer  = (User) userDAO.find(transaction.getPayerId());
 
     if ( payee == null || payer == null ) {
       throw new RuntimeException("No Payer or Payee.");
@@ -51,8 +53,8 @@ public class TransactionLimitCheckDAO
     synchronized ( firstLock ) {
       synchronized ( secondLock ) {
 
-        if ( ! limitsNotAbove(transaction, payer, isBroker(payer), TransactionLimitType.SEND, true) ||
-            ! limitsNotAbove(transaction, payee, isBroker(payee), TransactionLimitType.RECEIVE, false) ) {
+        if ( ! limitsNotAbove(transaction, payer, isBroker(payer), TransactionLimitType.SEND,    true) ||
+             ! limitsNotAbove(transaction, payee, isBroker(payee), TransactionLimitType.RECEIVE, false) ) {
           throw new RuntimeException("Transaction Limits overstepped.");
         }
 
@@ -62,21 +64,23 @@ public class TransactionLimitCheckDAO
   }
 
   // Checking whether user is a Broker
-  private boolean isBroker(User user) {
-    Sink count = new Count();
+  protected boolean isBroker(User user) {
+    Sink count    = new Count();
     DAO brokerDAO = (DAO) getX().get("brokerDAO");
+
     count = brokerDAO.where(EQ(user.getId(), Broker.USER_ID)).limit(1).select(count);
+
     return ( (Count) count).getValue() > 0;
   }
 
   // Checking if user overstepped its limits
-  private boolean limitsNotAbove(Transaction transaction, User user, boolean isBroker, TransactionLimitType type, boolean isPayer) {
+  protected boolean limitsNotAbove(Transaction transaction, User user, boolean isBroker, TransactionLimitType type, boolean isPayer) {
     DAO transactionLimitDAO = (DAO) getX().get("transactionLimitDAO");
     TransactionLimit[] userLimits = (TransactionLimit[]) user.getTransactionLimits();
 
     for ( TransactionLimitTimeFrame timeFrame : TransactionLimitTimeFrame.values() ) {
-      boolean isDefault = true;
-      long userLimitValue = 0;
+      boolean isDefault      = true;
+      long    userLimitValue = 0;
 
       for ( TransactionLimit userLimit : userLimits ) {
         if ( userLimit.getTimeFrame() == timeFrame && userLimit.getType() == type ) {
@@ -92,7 +96,7 @@ public class TransactionLimitCheckDAO
         }
         DAO sumLimitDAO = transactionLimitDAO.where(AND(
             EQ(limitName, TransactionLimit.NAME),
-            EQ(type, TransactionLimit.TYPE),
+            EQ(type,      TransactionLimit.TYPE),
             EQ(timeFrame, TransactionLimit.TIME_FRAME)));
 
         userLimitValue = ((Double)(((Sum) sumLimitDAO.select(SUM(TransactionLimit.AMOUNT))).getValue())).longValue();
@@ -105,7 +109,7 @@ public class TransactionLimitCheckDAO
   }
 
   // Check if user reached period for a given timeframe and limit
-  private boolean isOverTimeFrameLimit(Transaction transaction, User user, TransactionLimitTimeFrame timeFrame, long limit, boolean isPayer) {
+  protected boolean isOverTimeFrameLimit(Transaction transaction, User user, TransactionLimitTimeFrame timeFrame, long limit, boolean isPayer) {
 
     long userTransactionAmount = 0;
     switch( (TransactionLimitTimeFrame) timeFrame ) {
@@ -127,7 +131,7 @@ public class TransactionLimitCheckDAO
   }
 
   // Getting user amount spent given a time period
-  private long getTransactionAmounts(User user, boolean isPayer, int calendarType) {
+  protected long getTransactionAmounts(User user, boolean isPayer, int calendarType) {
 
     Date firstDate = getDayOfCurrentPeriod(calendarType, MaxOrMin.MIN);
     Date lastDate = getDayOfCurrentPeriod(calendarType, MaxOrMin.MAX);
@@ -141,12 +145,12 @@ public class TransactionLimitCheckDAO
   }
 
   // Enum to facilitate getting Max or Min hour of each date
-  private enum MaxOrMin {
+  protected enum MaxOrMin {
     MAX, MIN;
   }
 
   // return min or max date:hour for a specific period according to parameters
-  private Date getDayOfCurrentPeriod(int period, MaxOrMin maxOrMin) {
+  protected Date getDayOfCurrentPeriod(int period, MaxOrMin maxOrMin) {
     // get start of this week in milliseconds
     Calendar cal = Calendar.getInstance();
 
@@ -154,12 +158,13 @@ public class TransactionLimitCheckDAO
       cal.set(period, cal.getActualMaximum(period));
       return getEndOfDay(cal);
     }
+
     cal.set(period, cal.getActualMinimum(period));
     return getStartOfDay(cal);
   }
 
   // Setting hours, minutes, seconds and milliseconds to maximum
-  private Date getEndOfDay(Calendar calendar) {
+  protected Date getEndOfDay(Calendar calendar) {
     calendar.set(Calendar.HOUR_OF_DAY, 23);
     calendar.set(Calendar.MINUTE, 59);
     calendar.set(Calendar.SECOND, 59);
@@ -168,7 +173,7 @@ public class TransactionLimitCheckDAO
   }
 
   // Setting hours, minutes, seconds and milliseconds to minimum
-  private Date getStartOfDay(Calendar calendar) {
+  protected Date getStartOfDay(Calendar calendar) {
     calendar.set(Calendar.HOUR_OF_DAY, 0);
     calendar.set(Calendar.MINUTE, 0);
     calendar.set(Calendar.SECOND, 0);
