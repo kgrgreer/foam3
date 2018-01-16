4e466b4da209bcc4661967a1fa34da583407d437
Kirk Eaton <kirk@nanopay.net>
2018-01-15 19:33:15 -0500

    Modified TransactionTableView to show the total value instead of just the amount. Modified Transaction model and added a tableCellFormatter to total, and made the label for total say Amount

--- nanopay/src/net/nanopay/tx/model/Transaction.js
+++ nanopay/src/net/nanopay/tx/model/Transaction.js
@@ -154,11 +154,19 @@ foam.CLASS({
     {
       class: 'Currency',
       name: 'total',
+      label: 'Amount',
       transient: true,
       expression: function (amount, tip) {
         return amount + tip;
       },
-      javaGetter: `return getAmount() + getTip();`
+      javaGetter: `return getAmount() + getTip();`,
+      tableCellFormatter: function(amount) {
+        var formattedAmount = total / 100;
+        this
+          .start()
+            .add(' CAD $', formattedAmount.toFixed(2))
+          .end();
+      }
     },
     {
       class: 'FObjectProperty',
