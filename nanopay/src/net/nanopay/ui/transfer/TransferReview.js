3ab8929fbd9d13d6b7ebb792b44b95a05f2a462b
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2018-01-18 16:24:19 -0500

    Added another reference to the used account

--- nanopay/src/net/nanopay/ui/transfer/TransferReview.js
+++ nanopay/src/net/nanopay/ui/transfer/TransferReview.js
@@ -165,6 +165,7 @@ foam.CLASS({
           .start().addClass('transferRateContainer')
             .callIf(this.type == 'regular', function() {
               this.start('p').addClass('currencyAmount-L').add('$ ', parseFloat(self.viewData.fromAmount).toFixed(2)).end()
+              this.start('p').add('Paid from Account Number: ' + '***' + self.viewData.account.accountNumber.substring(self.viewData.account.accountNumber.length - 4, self.viewData.account.accountNumber.length)).end()
             })
           .end()
           // .start('div').addClass('pricingCol')
