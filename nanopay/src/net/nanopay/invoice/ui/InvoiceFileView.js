8d75b15309cd102f79c2b425bf8a0a5b1957ca44
Kirk Eaton <kirk@nanopay.net>
2018-01-26 14:37:52 -0500

    Added link to file after upload

--- nanopay/src/net/nanopay/invoice/ui/InvoiceFileView.js
+++ nanopay/src/net/nanopay/invoice/ui/InvoiceFileView.js
@@ -12,6 +12,10 @@ foam.CLASS({
     'blobService'
   ],
 
+  exports: [
+    'as data'
+  ],
+
   properties: [
     'data'
   ],
@@ -28,6 +32,35 @@ foam.CLASS({
     ^ .attachment-btn {
       margin: 10px 0;
     }
+    ^ .attachment-item {
+      width: 175px;
+      height: 40px;
+      background-color: #ffffff;
+      padding: 10px;
+    }
+    ^ .attachment-filename {
+      height: 16px;
+      font-size: 12px;
+      line-height: 1.33;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #59a5d5;
+    }
+    ^ .attachment-item-size {
+      width: 16.7px;
+      height: 8px;
+      font-size: 6px;
+      line-height: 1.33;
+      letter-spacing: 0.1px;
+      text-align: left;
+      color: #a4b3b8;
+    }
+    ^ .net-nanopay-ui-ActionView-remove {
+      width: 12px;
+      height: 12px;
+      object-fit: contain;
+      float: right;
+    }
   `,
 
   methods: [
@@ -43,17 +76,43 @@ foam.CLASS({
             .attrs({ type: 'file', accept: 'application/pdf' })
             .on('change', this.onChange)
           .end()
-          .start().addClass('attachment-btn white-blue-button btn')
-            .add('Add Attachment')
-            .on('click', this.onAddAttachmentClicked)
-          .end()
           .add(this.slot(function(data) {
             var file = data && data.data;
-            var url = file && self.blobService.urlFor(file);
-            return ! url ? this.E('span') : this.E('a').attrs({ href: url }).add('Download')
+            if ( ! file ) {
+              return this.E().start()
+                .addClass('attachment-btn white-blue-button btn')
+                .add('Add Attachment')
+                .on('click', self.onAddAttachmentClicked)
+                .end()
+            } else {
+              return this.E()
+                .addClass('attachment-item')
+                .start('a').addClass('attachment-filename')
+                  .attrs({
+                    href: self.data.data$.map(function (data) {
+                      return self.BlobBlob.isInstance(data) ?
+                        URL.createObjectURL(data.blob) :
+                        ( self.blobService.urlFor(data) || '');
+                    }),
+                    target: '_blank'
+                  })
+                  .add(data.filename)
+                .end()
+                .add(this.REMOVE);
+            }
           }, this.data$))
           .add('Maximum size 10MB')
         .end()
+    }
+  ],
+
+  actions: [
+    {
+      name: 'remove',
+      icon: 'images/ic-delete.svg',
+      code: function (X) {
+        this.data = null;
+      }
     }
   ],
 
