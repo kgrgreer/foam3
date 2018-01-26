e8477aa73ce4ef0aac4f721e237bb1fd6f8d4e96
Kirk Eaton <kirk@nanopay.net>
2018-01-26 14:50:16 -0500

    Added filesize to InvoiceFileView

--- nanopay/src/net/nanopay/invoice/ui/InvoiceFileView.js
+++ nanopay/src/net/nanopay/invoice/ui/InvoiceFileView.js
@@ -126,6 +126,7 @@ foam.CLASS({
 
       this.data = this.File.create({
         filename: file.name,
+        filesize: file.size,
         mimeType: file.type,
         data: this.BlobBlob.create({
           blob: file
