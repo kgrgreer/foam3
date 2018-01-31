f4d5000bdcb91bde570026c5cad79fddbf41cc15
blakegreen <blake@nanopay.net>
2018-01-31 12:04:09 -0500

    Setting new user and pushing array hours after data is set, added closed checkbox which hides and shows hours accordingly

--- nanopay/src/net/nanopay/settings/business/BusinessHoursView.js
+++ nanopay/src/net/nanopay/settings/business/BusinessHoursView.js
@@ -1,52 +1,66 @@
 foam.CLASS({
   package: 'net.nanopay.settings.business',
   name: 'BusinessHoursView',
-  extends: 'foam.u2.View',
+  extends: 'foam.u2.Controller',
 
   documentation: 'View displaying business hours',
 
+  imports: [ 
+    'user',
+    'userDAO'
+  ],
+
+  requires: [
+    'foam.nanos.auth.Address',
+    'foam.nanos.auth.DayOfWeek',
+    'foam.nanos.auth.Hours',
+    'foam.u2.dialog.NotificationMessage'
+  ],
+
   css: `
-  ^ .Container {
-    width: 992px;
-    min-height: 80px;
-    margin-top: 30px;
-    margin-bottom: 20px;
-    padding: 20px;
-    border-radius: 2px;
-    background-color: white;
-    box-sizing: border-box;
-  }
-  ^ .boxTitle {
-    opacity: 0.6;
-    font-family: 'Roboto';
-    font-size: 20px;
-    font-weight: 300;
-    line-height: 20px;
-    letter-spacing: 0.3px;
-    text-align: left;
-    color: #093649;
-    display: inline-block;
-  }
-  ^ .labelTitle {
-    font-family: Roboto;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 0.2px;
-    text-align: left;
-    color: #093649;
-    margin-bottom: 15px;
-  }
-  ^ .businessHourLabels {
-    width: 30px;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 0.2px;
-    text-align: left;
-    color: #093649;
-    margin-bottom: 15px;
-    display: inline-block;
-  }
+    ^ .Container {
+      width: 992px;
+      min-height: 80px;
+      margin-top: 30px;
+      margin-bottom: 20px;
+      padding: 20px;
+      border-radius: 2px;
+      background-color: white;
+      box-sizing: border-box;
+    }
+    ^ .boxTitle {
+      opacity: 0.6;
+      font-family: 'Roboto';
+      font-size: 20px;
+      font-weight: 300;
+      line-height: 20px;
+      letter-spacing: 0.3px;
+      text-align: left;
+      color: #093649;
+      display: inline-block;
+    }
+    ^ .labelTitle {
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: bold;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #093649;
+      margin-bottom: 15px;
+    }
+    ^ .businessHourLabels {
+      width: 30px;
+      font-size: 14px;
+      font-weight: bold;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #093649;
+      margin-bottom: 15px;
+      display: inline-block;
+      visibility: visible !important;
+    }
     ^ .dayOfWeekDiv {
+      display: inline-block;
       margin-top: 20px;
     }
     ^ .net-nanopay-ui-ActionView-saveButton {
@@ -92,36 +106,21 @@ foam.CLASS({
       background-color: white;
       outline: none;
     }
-    ^ .property-hours {
+    ^ .foam-u2-TimeView {
+      height: 30px;
+      outline: none;
       margin-left: 20px;
-      margin-right: 5px;
-      display: inline-block;
-    }
-    ^ .property-periods {
       margin-right: 20px;
-      display: inline-block;
     }
-    ^ .caret {
-      pointer-events: none;
-      position: relative;
+    ^ .closed-text {
+      font-size: 12px;
+      font-weight: normal;
+      font-style: normal;
+      font-stretch: normal;
+      color: #093649;
     }
-    .caret:before {
-      content: '';
-      position: absolute;
-      top: 12px;
-      left: 49px;
-      border-top: 7px solid #a4b3b8;
-      border-left: 7px solid transparent;
-      border-right: 7px solid transparent;
-    }
-    .caret:after {
-      content: '';
-      position: absolute;
-      left: 12px;
-      top: 0;
-      border-top: 0px solid #ffffff;
-      border-left: 0px solid transparent;
-      border-right: 0px solid transparent;
+    ^ .box-width {
+      width: 600px;
     }
   `,
 
@@ -132,33 +131,136 @@ foam.CLASS({
       value: false
     },
     {
-      name: 'hours',
-      view: {
-        class: 'foam.u2.view.ChoiceView',
-        choices: [
-          '1:00','1:30',
-          '2:00','2:30',
-          '3:00','3:30',
-          '4:00','4:30',
-          '5:00','5:30',
-          '6:00','6:30',
-          '7:00','7:30',
-          '8:00','8:30',
-          '9:00','9:30',
-          '10:00','10:30',
-          '11:00','11:30',
-          '12:00','12:30'
-        ]
+      class: 'Boolean',
+      name: 'checkBoxClosedMonday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedTuesday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedWednesday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedThursday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedFriday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedSaturday',
+      value: false
+    },
+    {
+      class: 'Boolean',
+      name: 'checkBoxClosedSunday',
+      value: false
+    },
+    {
+      class: 'Time',
+      name: 'mondayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
       }
     },
     {
-      name: 'periods',
-      view: {
-        class: 'foam.u2.view.ChoiceView',
-        choices: [
-          'AM',
-          'PM'
-        ]
+      class: 'Time',
+      name: 'mondayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'tuesdayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'tuesdayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'wednesdayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'wednesdayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'thursdayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'thursdayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'fridayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'fridayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'saturdayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'saturdayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'sundayStartTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
+      }
+    },
+    {
+      class: 'Time',
+      name: 'sundayEndTime',
+      postSet: function(oldValue, newValue) {
+        return newValue;
       }
     }
   ],
@@ -192,6 +294,7 @@ foam.CLASS({
 
   methods: [
     function initE() {
+      this.SUPER();
 
       this
       .addClass(this.myClass())
@@ -200,118 +303,76 @@ foam.CLASS({
         .start('div').addClass('toggleDiv')
           .tag({ class: 'net.nanopay.ui.ToggleSwitch', data$: this.businessHoursEnabled$ })
         .end()
-        .start().addClass(this.businessHoursEnabled$.map(function(e) { return e ? 'show' : 'hide' }))
-          .start().addClass('dayOfWeekDiv')
+        .start().addClass(this.businessHoursEnabled$.map(function(e) { return e ? 'show' : 'hide' })).addClass('box-width')
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedMonday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.MondayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.MONDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.MONDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedMonday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedTuesday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.TuesdayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.TUESDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.TUESDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedTuesday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedWednesday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.WednesdayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.WEDNESDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.WEDNESDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedWednesday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedThursday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.ThursdayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.THURSDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.THURSDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedThursday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedFriday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.FridayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.FRIDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.FRIDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedFriday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedSaturday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.SaturdayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.SATURDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.SATURDAY_END_TIME).end()
           .end()
-          .start().addClass('dayOfWeekDiv')
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedSaturday$ })
+            .add('Closed').addClass('closed-text')
+          .end()
+          .start().addClass('dayOfWeekDiv').addClass(this.checkBoxClosedSunday$.map(function(e) { return e ? 'hide' : 'show' }))
             .start().add(this.SundayLabel).addClass('businessHourLabels').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.SUNDAY_START_TIME).end()
             .start().add(this.ToLabel).addClass('labelTitle inline').end()
-            .start(this.HOURS)
-              .start('div').addClass('caret').end()
-            .end()
-            .start(this.PERIODS)
-              .start('div').addClass('caret').end()
-            .end()
+            .start(this.SUNDAY_END_TIME).end()
+          .end()
+          .start().addClass('inline')
+            .tag({class: 'foam.u2.CheckBox', data$: this.checkBoxClosedSunday$ })
+            .add('Closed').addClass('closed-text')
           .end()
           .start().add(this.SAVE_BUTTON).end()
         .end()
@@ -325,7 +386,68 @@ foam.CLASS({
       label: 'Save',
       code: function (X) {
         // Save business hours
+
+        var self = this;
+
+        var businessHoursArray = [];
+
+        var mondayHours = this.Hours.create({
+          day : this.DayOfWeek.MONDAY,
+          startTime: this.mondayStartTime,
+          endTime: this.mondayEndTime
+        });
+        var tuesdayHours = this.Hours.create({
+          day : this.DayOfWeek.TUESDAY,
+          startTime: this.tuesdayStartTime,
+          endTime: this.tuesdayEndTime
+        });
+        var wednesdayHours = this.Hours.create({
+          day : this.DayOfWeek.WEDNESDAY,
+          startTime: this.wednesdayStartTime,
+          endTime: this.wednesdayEndTime
+        });
+        var thursdayHours = this.Hours.create({
+          day : this.DayOfWeek.THURSDAY,
+          startTime: this.thursdayStartTime,
+          endTime: this.thursdayEndTime
+        });
+        var fridayHours = this.Hours.create({
+          day : this.DayOfWeek.FRIDAY,
+          startTime: this.fridayStartTime,
+          endTime: this.fridayEndTime
+        });
+        var saturdayHours = this.Hours.create({
+          day : this.DayOfWeek.SATURDAY,
+          startTime: this.saturdayStartTime,
+          endTime: this.saturdayEndTime
+        });
+        var sundayHours = this.Hours.create({
+          day : this.DayOfWeek.SUNDAY,
+          startTime: this.sundayStartTime,
+          endTime: this.sundayEndTime
+        });
+
+        businessHoursArray.push(
+          mondayHours, 
+          tuesdayHours, 
+          wednesdayHours,
+          thursdayHours,
+          fridayHours,
+          saturdayHours,
+          sundayHours
+        );
+        
+        this.user.address.hours = businessHoursArray;
+ 
+        this.userDAO.put(this.user).then(function (response) {
+          self.user.copyFrom(response);
+          self.add(self.NotificationMessage.create({ message: 'Business hours sucessfully saved.', type: '' }));
+        }).catch(function (error) {
+          self.add(self.NotificationMessage.create({ message: error.message, type: 'error' }));
+        })
       }
+
     }
   ]
+
 }); 
