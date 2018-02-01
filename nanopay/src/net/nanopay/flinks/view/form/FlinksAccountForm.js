935fb9d7ac17bfcbc5aabf3959c08836299fa3fc
xuerongCode <xuerong.work@gmail.com>
2018-02-01 11:02:56 -0500

    fix element color

--- nanopay/src/net/nanopay/flinks/view/form/FlinksAccountForm.js
+++ nanopay/src/net/nanopay/flinks/view/form/FlinksAccountForm.js
@@ -75,7 +75,7 @@ foam.CLASS({
         }
 
         ^ .net-nanopay-ui-ActionView-closeButton {
-          float: right;
+          float: left;
           margin: 0;
           outline: none;
           min-width: 136px;
@@ -87,6 +87,7 @@ foam.CLASS({
           font-weight: lighter;
           letter-spacing: 0.2px;
           margin-right: 40px;
+          margin-left: 1px;
         }
 
         ^ .net-nanopay-ui-ActionView-nextButton:disabled {
