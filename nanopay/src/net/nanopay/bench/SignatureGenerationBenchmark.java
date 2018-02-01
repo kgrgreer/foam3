c3e1f36d755ffb7454282d93816e751aa7e208c6
Kirk Eaton <kirk@nanopay.net>
2018-02-01 09:30:09 -0500

    Added SignatureGeneration and SignatureVerification benchmarks

---
+++ nanopay/src/net/nanopay/bench/SignatureGenerationBenchmark.java
@@ -0,0 +1,40 @@
+package net.nanopay.bench;
+
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ListSink;
+import foam.dao.Sink;
+import net.nanopay.tx.model.Transaction;
+
+import java.util.List;
+
+public class SignatureGenerationBenchmark
+    extends SignatureBenchmark
+{
+  protected List transactions_;
+  protected DAO transactionDAO_;
+
+  public SignatureGenerationBenchmark(String keypairAlgorithm, String hashingAlgorithm) {
+    super(keypairAlgorithm, hashingAlgorithm);
+  }
+
+  @Override
+  public void setup(X x) {
+    transactionDAO_ = (DAO) x.get("localTransactionDAO");
+
+    Sink sink = new ListSink();
+    sink = transactionDAO_.select(sink);
+    transactions_ = ((ListSink) sink).getData();
+  }
+
+  @Override
+  public void execute(X x) {
+    try {
+      // sign using SHA256 with RSA
+      int n = (int) (Math.random() * transactions_.size());
+      ((Transaction) transactions_.get(n)).sign("SHA256withRSA", keypair_.getPrivate());
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
+}
