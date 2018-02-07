4c0a547e746f4d104ab95d451d85f4b81332d6fc
Kirk Eaton <kirk@nanopay.net>
2018-02-07 12:31:10 -0500

    Revert "pulled user out of Transaction to do verify email check"
    This reverts commit bf22ac2fb165f5f3ae50520a5f1f6649a922a040.

--- nanopay/src/net/nanopay/tx/EmailVerifiedTransactionDAO.java
+++ nanopay/src/net/nanopay/tx/EmailVerifiedTransactionDAO.java
@@ -5,10 +5,9 @@ import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.auth.User;
-import net.nanopay.tx.model.Transaction;
 
 public class EmailVerifiedTransactionDAO
-  extends ProxyDAO
+    extends ProxyDAO
 {
   public EmailVerifiedTransactionDAO(X x, DAO delegate) {
     super(x, delegate);
@@ -16,13 +15,11 @@ public class EmailVerifiedTransactionDAO
 
   @Override
   public FObject put_(X x, FObject obj) {
-    DAO userDAO = (DAO) x.get("userDAO");
-    Transaction transaction = (Transaction) obj;
-    User user = (User) userDAO.find(transaction.getPayerId());
+    User user = (User) x.get("user");
     if ( user == null ) {
       throw new RuntimeException("User is not logged in");
     }
-   
+
     if ( ! user.getEmailVerified() ) {
       throw new RuntimeException("You must verify your email to send money");
     }
