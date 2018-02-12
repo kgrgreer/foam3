f84c0fd45868066c7100b9cb2bdb928fbd690039
Waseem <waseem@nanopay.net>
2018-02-12 11:56:06 -0500

    added new email

---
+++ nanopay/src/net/nanopay/invoice/InvoiceMakeEmailDAO.java
@@ -0,0 +1,54 @@
+package net.nanopay.invoice;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.nanos.app.AppConfig;
+import foam.nanos.auth.User;
+import foam.nanos.notification.email.EmailMessage;
+import foam.nanos.notification.email.EmailService;
+import net.nanopay.invoice.model.Invoice;
+import net.nanopay.tx.model.Transaction;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
+public class InvoiceMakeEmailDAO
+  extends ProxyDAO
+{
+  protected DAO userDAO_;
+
+  public InvoiceMakeEmailDAO(X x, DAO delegate) {
+    super(x, delegate);
+    userDAO_ = (DAO) x.get("localUserDAO");
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    try {
+      Invoice invoice = (Invoice) obj;
+      AppConfig config = (AppConfig) x.get("appConfig");
+      NumberFormat formatter = NumberFormat.getCurrencyInstance();
+      User payee = (User) userDAO_.find_(x, invoice.getPayeeId());
+      User payer = (User) userDAO_.find_(x, invoice.getPayerId());
+      if ( payee.getId() == invoice.getCreatedBy() )
+      {
+        return getDelegate().put_(x, obj);
+      }
+      EmailService email = (EmailService) x.get("email");
+      EmailMessage message = new EmailMessage();
+      message.setTo(new String[]{payer.getEmail()});
+      HashMap<String, Object> args = new HashMap<>();
+      args.put("amount", formatter.format(invoice.getAmount()/100));
+      args.put("fromEmail", payee.getEmail());
+      args.put("fromName", payee.getEmail());
+      args.put("date", invoice.getDueDate());
+      args.put("link", config.getUrl());
+      email.sendEmailFromTemplate(payer, message, "newInvoice", args);
+
+    } catch(Throwable t) {
+      t.printStackTrace();
+    }
+    return getDelegate().put_(x, obj);
+  }
+}
