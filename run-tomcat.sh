860c9b312ed3b17354dfa95e27da24bab14a9d9d
Amy Meng <amymeng5277@gmail.com>
2018-02-13 13:37:19 -0500

    1.Add wrap to against the space.

--- run-tomcat.sh
+++ run-tomcat.sh
@@ -22,80 +22,80 @@ function usage {
 }
 
 function rmdir {
-    if test -d $1 ; then
-        rm -rf $1
+    if test -d "$1" ; then
+        rm -rf "$1"
     fi
 }
 
 function rmfile {
-    if test -f $1 ; then
-        rm -f $1
+    if test -f "$1" ; then
+        rm -f "$1"
     fi
 }
 
 function shutdown_tomcat {
-    $CATALINA_HOME/bin/shutdown.sh > /dev/null 2>&1
+    "$CATALINA_HOME/bin/shutdown.sh" > /dev/null 2>&1
 }
 
 function build_NANOPAY_into_tomcat {
-    WEBAPPS=$CATALINA_HOME/webapps
+    WEBAPPS="$CATALINA_HOME/webapps"
 
-    cd $NANOPAY_HOME
+    cd "$NANOPAY_HOME"
 
     ./gen.sh
     mvn clean install
 
     # Copy over war and server config files.
     # These are needed before server startup
-    rmfile $WEBAPPS/ROOT.war
-    rmdir $WEBAPPS/ROOT
+    rmfile "$WEBAPPS/ROOT.war"
+    rmdir "$WEBAPPS/ROOT"
 
-    unzip $NANOPAY_HOME/target/ROOT.war -d $WEBAPPS/ROOT
+    unzip "$NANOPAY_HOME/target/ROOT.war" -d "$WEBAPPS/ROOT"
 
-    cd $NANOPAY_HOME
+    cd "$NANOPAY_HOME"
     ./find.sh
 
     #Copy over all JDAO files to /bin
-    cp brokers $CATALINA_HOME/bin/
-    cp businessSectors $CATALINA_HOME/bin/
-    cp businessTypes $CATALINA_HOME/bin/
-    cp cicoServiceProviders $CATALINA_HOME/bin/
-    cp countries $CATALINA_HOME/bin/
-    cp cronjobs $CATALINA_HOME/bin/
-    cp currency $CATALINA_HOME/bin/
-    cp dugs $CATALINA_HOME/bin/
-    cp emailTemplates $CATALINA_HOME/bin/
-    cp exportDriverRegistrys $CATALINA_HOME/bin/
-    cp groups $CATALINA_HOME/bin/
-    cp languages $CATALINA_HOME/bin/
-    cp menus $CATALINA_HOME/bin/
-    cp -n permissions $CATALINA_HOME/bin/ &
-    cp regions $CATALINA_HOME/bin/
-    cp scripts $CATALINA_HOME/bin/
-    cp services $CATALINA_HOME/bin/
-    cp tests $CATALINA_HOME/bin/
-    cp transactionLimits $CATALINA_HOME/bin/
-    cp -n users $CATALINA_HOME/bin/ &
-    cp institutions $CATALINA_HOME/bin/
+    cp brokers "$CATALINA_HOME/bin/"
+    cp businessSectors "$CATALINA_HOME/bin/"
+    cp businessTypes "$CATALINA_HOME/bin/"
+    cp cicoServiceProviders "$CATALINA_HOME/bin/"
+    cp countries "$CATALINA_HOME/bin/"
+    cp cronjobs "$CATALINA_HOME/bin/"
+    cp currency "$CATALINA_HOME/bin/"
+    cp dugs "$CATALINA_HOME/bin/"
+    cp emailTemplates "$CATALINA_HOME/bin/"
+    cp exportDriverRegistrys "$CATALINA_HOME/bin/"
+    cp groups "$CATALINA_HOME/bin/"
+    cp languages "$CATALINA_HOME/bin/"
+    cp menus "$CATALINA_HOME/bin/"
+    cp -n permissions "$CATALINA_HOME/bin/" &
+    cp regions "$CATALINA_HOME/bin/"
+    cp scripts "$CATALINA_HOME/bin/"
+    cp services "$CATALINA_HOME/bin/"
+    cp tests "$CATALINA_HOME/bin/"
+    cp transactionLimits "$CATALINA_HOME/bin/"
+    cp -n users "$CATALINA_HOME/bin/" &
+    cp institutions "$CATALINA_HOME/bin/"
 
     # Some older scripts may have copied foam2/nanopay/merchant as their own webapps.
-    rmdir $WEBAPPS/foam2
-    rmdir $WEBAPPS/nanopay
-    rmdir $WEBAPPS/merchant
+    rmdir "$WEBAPPS/foam2"
+    rmdir "$WEBAPPS/nanopay"
+    rmdir "$WEBAPPS/merchant"
 
     # Copy over static web files to ROOT
-    cp -r foam2 $WEBAPPS/ROOT/foam2
-    cp -r nanopay $WEBAPPS/ROOT/nanopay
-    cp -r merchant $WEBAPPS/ROOT/merchant
-    cp -r favicon $WEBAPPS/ROOT/favicon
+    cp -r foam2 "$WEBAPPS/ROOT/foam2"
+    cp -r nanopay "$WEBAPPS/ROOT/nanopay"
+    cp -r merchant "$WEBAPPS/ROOT/merchant"
+    cp -r favicon "$WEBAPPS/ROOT/favicon"
 
     # Move images to ROOT/images
-    rmdir $WEBAPPS/ROOT/images
-    mkdir $WEBAPPS/ROOT/images
-    cp -r $WEBAPPS/ROOT/nanopay/src/net/nanopay/images $WEBAPPS/ROOT
-    cp -r $WEBAPPS/ROOT/merchant/src/net/nanopay/merchant/images $WEBAPPS/ROOT
+    rmdir "$WEBAPPS/ROOT/images"
+    mkdir "$WEBAPPS/ROOT/images"
+    cp -r "$WEBAPPS/ROOT/nanopay/src/net/nanopay/images" "$WEBAPPS/ROOT"
+    cp -r "$WEBAPPS/ROOT/merchant/src/net/nanopay/merchant/images" "$WEBAPPS/ROOT"
 
-    rmdir $WEBAPPS/ROOT/foam2/src/com
+    rmdir "$WEBAPPS/ROOT/foam2/src/com"
 }
 
 function start_tomcat {
@@ -110,7 +110,7 @@ function start_tomcat {
         ARGS="$ARGS start"
     fi
 
-    $CATALINA_HOME/bin/catalina.sh $ARGS
+    "$CATALINA_HOME/bin/catalina.sh" $ARGS
 }
 
 
@@ -121,7 +121,7 @@ while [ -z "$CATALINA_HOME" ]; do
     if [ ! -z "$CATALINA_HOME" ]; then
         break
     fi
-    testcatalina $HOME/tools/tomcat
+    testcatalina "$HOME/tools/tomcat"
     if [ ! -z "$CATALINA_HOME" ]; then
         break
     fi
