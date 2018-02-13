4b542385dbab7ec66f3fd542263716cac802cfa4
Waseem <waseem@nanopay.net>
2018-02-13 13:37:05 -0500

    added email for void invoices and verified emails

---
+++ nanopay/src/net/nanopay/bank/AccountVerifiedEmailDAO.java
@@ -0,0 +1,53 @@
+package net.nanopay.bank;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.nanos.app.AppConfig;
+import foam.nanos.auth.User;
+import foam.nanos.notification.email.EmailMessage;
+import foam.nanos.notification.email.EmailService;
+import net.nanopay.invoice.model.Invoice;
+import net.nanopay.model.Account;
+import net.nanopay.model.BankAccount;
+import net.nanopay.tx.model.Transaction;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
+public class AccountVerifiedEmailDAO
+  extends ProxyDAO
+{
+  protected DAO userDAO_;
+
+  public AccountVerifiedEmailDAO(X x, DAO delegate) {
+    super(x, delegate);
+    userDAO_ = (DAO) x.get("localUserDAO");
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    try{
+      BankAccount account = (BankAccount) obj;
+      AppConfig config = (AppConfig) x.get("appConfig");
+      NumberFormat formatter = NumberFormat.getCurrencyInstance();
+      User owner = (User) account.getOwner();
+      if (! account.getStatus().equals("Verified")){
+        System.out.println("----------MVP-------");
+        return getDelegate().put_(x, obj);
+      }
+      System.out.println("----------BOMBER-------");
+      EmailService email = (EmailService) x.get("email");
+      EmailMessage message = new EmailMessage();
+      message.setTo(new String[]{owner.getEmail()});
+      HashMap<String, Object> args = new HashMap<>();
+      args.put("account", account.getAccountNumber().substring(account.getAccountNumber().length() - 4));
+      email.sendEmailFromTemplate(owner, message, "verifiedBank", args);
+
+    } catch(Throwable t) {
+      System.out.println("----------SCARLETTE-------");
+      t.printStackTrace();
+    }
+    return getDelegate().put_(x, obj);
+  }
+}
---
+++ nanopay/src/net/nanopay/invoice/InvoiceVoidEmailDAO.java
@@ -0,0 +1,57 @@
+package net.nanopay.invoice;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.nanos.app.AppConfig;
+import foam.nanos.auth.User;
+import foam.nanos.notification.email.EmailMessage;
+import foam.nanos.notification.email.EmailService;
+import net.nanopay.invoice.model.Invoice;
+import net.nanopay.tx.model.Transaction;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
+public class InvoiceVoidEmailDAO
+  extends ProxyDAO
+{
+  protected DAO userDAO_;
+
+  public InvoiceVoidEmailDAO(X x, DAO delegate) {
+    super(x, delegate);
+    userDAO_ = (DAO) x.get("localUserDAO");
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    try{
+      Invoice invoice = (Invoice) obj;
+      AppConfig config = (AppConfig) x.get("appConfig");
+      NumberFormat formatter = NumberFormat.getCurrencyInstance();
+      User payer = (User) userDAO_.find_(x, invoice.getPayerId());
+      if (! invoice.getPaymentMethod().name().equals("VOID")){
+        System.out.println("----------MVP-------");
+        System.out.println(invoice.getPaymentMethod().name());
+        System.out.println(invoice.getPaymentMethod().ordinal());
+        System.out.println(invoice.getPaymentMethod().toString());
+
+        return getDelegate().put_(x, obj);
+      }
+      System.out.println("----------BOMBER-------");
+      EmailService email = (EmailService) x.get("email");
+      EmailMessage message = new EmailMessage();
+      message.setTo(new String[]{payer.getEmail()});
+      HashMap<String, Object> args = new HashMap<>();
+      args.put("account", invoice.getId());
+      args.put("amount", formatter.format(invoice.getAmount()));
+      args.put("link", config.getUrl());
+      email.sendEmailFromTemplate(payer, message, "voidInvoice", args);
+
+    } catch(Throwable t) {
+      System.out.println("----------SCARLETTE-------");
+      t.printStackTrace();
+    }
+    return getDelegate().put_(x, obj);
+  }
+}
