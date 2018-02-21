17cd52e46db16555c1856f8aef447e3e9fd68ba7
Joel Hughes <jhughes@jhughes.local>
2018-02-21 08:01:10 -0500

    Add subdomain support for staging, demo, integration, production

--- appconfig.sh
+++ appconfig.sh
@@ -5,19 +5,29 @@ sed -i -e "s/name: 'version'/name: 'version', value: '${VERSION}'/g" foam2/src/f
 
 # Set AppConfig.url
 HTTP="https"
-URL=`echo ${VERSION} | sed 's/.*\///' | sed 's/-v.*//' | sed 's/-/./g'`
-URL="${URL}.nanopay.net"
-if [[ $URL = *"b2b.prod"* ]]; then
-        url = "portal.nanopay.net"
-elif [[ $URL = *"interac-staging"* ]]; then
-    url = "interac-staging.nanopay.net"
-    HTTP = "https"
-elif [[ $URL = *"staging"* ]]; then
-        url = "localhost:8080"
-    HTTP = "http"
-fi
+DOMAIN=".nanopay.net"
+SUB=`echo ${VERSION} | sed 's/.*\///' | sed 's/-v.*//'`
 
-URL=${HTTP}://${URL}
+if [[ $SUB = *"b2b.prod"* ]]; then
+    SUB = "portal"
+elif [[ $SUB = *"cc.demo"* ]]; then
+    SUB = "sandbox"
+elif [[ $SUB = *"integration"* ]]; then
+    SUB=`echo ${SUB} | sed 's/./-/g'`
+    # replace integration with staging?
+    #SUB=`echo ${SUB} | sed 's/staging/integration/g'`
+elif [[ $SUB = *"staging"* ]]; then
+    HTTP="http"
+    SUB=`echo ${SUB} | sed 's/-/./g'`
+elif [[ $SUB = *"demo"* ]]; then
+    SUB=`echo ${SUB} | sed 's/./-/g'`
+else
+    HTTP="http"
+    SUB="localhost:8080"
+    DOMAIN=""
+fi
+SUB="${SUB}${DOMAIN}"
+URL=${HTTP}://${SUB}
 echo URL=${URL}
 sed -i -e "s,http://localhost:8080,${URL}," foam2/src/foam/nanos/app/AppConfig.js
 
