471d1d06ecc6b45a78277cc6dd0725e453261a6b
Kirk Eaton <kirk@nanopay.net>
2018-02-22 13:31:08 -0500

    Removed Payment.js

--- b2b/src/net/nanopay/b2b/files.js
+++ b2b/src/net/nanopay/b2b/files.js
@@ -3,7 +3,6 @@ FOAM_FILES([
   { name: 'net/nanopay/b2b/model/Document' },
   { name: 'net/nanopay/b2b/model/Invoice' },
   { name: 'net/nanopay/b2b/model/InvoiceResolution' },
-  { name: 'net/nanopay/b2b/model/Payment' },
   { name: 'net/nanopay/b2b/model/PendingPayment' },
   { name: 'net/nanopay/b2b/model/relationships' },
 ])
