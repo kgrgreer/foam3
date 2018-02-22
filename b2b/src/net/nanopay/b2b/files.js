3bb6893912a9881d458dca4d7dea3d368f042a0e
Kirk Eaton <kirk@nanopay.net>
2018-02-22 14:47:43 -0500

    Deleted relationships.js

--- b2b/src/net/nanopay/b2b/files.js
+++ b2b/src/net/nanopay/b2b/files.js
@@ -2,5 +2,4 @@ FOAM_FILES([
   { name: 'net/nanopay/b2b/model/Business' },
   { name: 'net/nanopay/b2b/model/Document' },
   { name: 'net/nanopay/b2b/model/Invoice' },
-  { name: 'net/nanopay/b2b/model/relationships' },
 ])
