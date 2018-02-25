6d640bee8cf1093531c31c9d879c438b2243ceb0
Kevin Greer <kgrgreer@gmail.com>
2018-02-25 12:29:32 -0500

    Cleanup and improved logging.

--- nanopay/src/net/nanopay/bank/AccountDeletedEmailDAO.java
+++ nanopay/src/net/nanopay/bank/AccountDeletedEmailDAO.java
@@ -6,12 +6,13 @@ import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.app.AppConfig;
 import foam.nanos.auth.User;
+import foam.nanos.logger.Logger;
 import foam.nanos.notification.email.EmailMessage;
 import foam.nanos.notification.email.EmailService;
 import java.util.HashMap;
 import net.nanopay.model.BankAccount;
 
-//Send an email when a bank account is deleted
+// Send an email when a bank account is deleted
 public class AccountDeletedEmailDAO
   extends ProxyDAO
 {
@@ -24,20 +25,23 @@ public class AccountDeletedEmailDAO
 
   @Override
   public FObject remove_(X x, FObject obj) {
-    BankAccount account = (BankAccount) super.remove_(x, obj);
-    AppConfig config = (AppConfig) x.get("appConfig");
-    User owner = (User) userDAO_.find_(x, account.getOwner());
-    EmailService email = (EmailService) x.get("email");
+    BankAccount account  = (BankAccount) super.remove_(x, obj);
+    AppConfig   config   = (AppConfig) x.get("appConfig");
+    User         owner   = (User) userDAO_.find_(x, account.getOwner());
+    EmailService email   = (EmailService) x.get("email");
     EmailMessage message = new EmailMessage();
+
     message.setTo(new String[]{owner.getEmail()});
     HashMap<String, Object> args = new HashMap<>();
     args.put("link", config.getUrl());
     args.put("account", account.getAccountNumber().substring(account.getAccountNumber().length() - 4));
+
     try{
       email.sendEmailFromTemplate(owner, message, "deletedBank", args);
     } catch(Throwable t) {
-      t.printStackTrace();
+      ((Logger) x.get(Logger.class)).error("Error sending account verified email.", t);
     }
+
     return account;
   }
 }
--- nanopay/src/net/nanopay/bank/BankEmailDAO.java
+++ nanopay/src/net/nanopay/bank/BankEmailDAO.java
@@ -5,13 +5,13 @@ import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.auth.User;
+import foam.nanos.logger.Logger;
 import foam.nanos.notification.email.EmailMessage;
 import foam.nanos.notification.email.EmailService;
 import java.util.HashMap;
 import net.nanopay.model.BankAccount;
 
-
-//Sends an email when a Bank account is created
+// Sends an email when a Bank account is created
 public class BankEmailDAO
   extends ProxyDAO
 {
@@ -25,26 +25,28 @@ public class BankEmailDAO
   @Override
   public FObject put_(X x, FObject obj) {
     BankAccount account = (BankAccount) obj;
-    User user = (User) userDAO_.find_(x, account.getOwner());
+    User        user    = (User) userDAO_.find_(x, account.getOwner());
 
     //Don't send an email if the account already exists
     if ( find(account.getId()) != null )
-    {
       return getDelegate().put_(x, obj);
-    }
+
     account = (BankAccount) super.put_(x, obj);
-    EmailService email = (EmailService) x.get("email");
+
+    EmailService email   = (EmailService) x.get("email");
     EmailMessage message = new EmailMessage();
+
     message.setTo(new String[]{user.getEmail()});
     HashMap<String, Object> args = new HashMap<>();
     args.put("name", user.getFirstName());
     args.put("account", account.getAccountNumber().substring(account.getAccountNumber().length() - 4));
+
     try {
       email.sendEmailFromTemplate(user, message, "addBank", args);
     } catch(Throwable t) {
-      t.printStackTrace();
+      ((Logger) x.get(Logger.class)).error("Error sending bank account created email.", t);
     }
-    return account;
 
+    return account;
   }
 }
