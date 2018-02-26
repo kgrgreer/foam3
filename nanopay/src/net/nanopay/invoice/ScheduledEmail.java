4b380371fd6867fd3b7f5a6ec84ab35b70dd12f0
Waseem <waseem@nanopay.net>
2018-02-26 12:45:38 -0500

    created cron for scheduled invoice notification

--- nanopay/src/net/nanopay/invoice/ScheduledEmail.java
+++ nanopay/src/net/nanopay/invoice/ScheduledEmail.java
@@ -7,6 +7,9 @@ import foam.nanos.app.AppConfig;
 import foam.nanos.auth.User;
 import foam.nanos.notification.email.EmailMessage;
 import foam.nanos.notification.email.EmailService;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import net.nanopay.invoice.model.Invoice;
 import static foam.mlang.MLang.*;
@@ -18,6 +21,7 @@ public class ScheduledEmail
   Calendar today = Calendar.getInstance();
   Calendar startTime = Calendar.getInstance();
   Calendar endTime = Calendar.getInstance();
+  Calendar dueDate = Calendar.getInstance();
 
   public void execute(X x) {
 
@@ -26,10 +30,6 @@ public class ScheduledEmail
     endTime.setTimeInMillis(startTime.getTimeInMillis() + ((1000*60*60*24)-1) );
     DAO   invoiceDAO = (DAO) x.get("invoiceDAO");
     DAO      userDAO = (DAO) x.get("userDAO");
-    System.out.println("HIT 1");
-    System.out.println(today.getTime());
-    System.out.println(startTime.getTime());
-    System.out.println(endTime.getTime());
 
     invoiceDAO = invoiceDAO.where(
       AND(
@@ -40,22 +40,25 @@ public class ScheduledEmail
     List<Invoice> invoicesList = (List)((ListSink)invoiceDAO.select(new ListSink())).getData();
     EmailService         email = (EmailService) x.get("email");
     AppConfig           config = (AppConfig) x.get("appConfig");
-    EmailMessage       message = new EmailMessage();
+    NumberFormat     formatter = NumberFormat.getCurrencyInstance();
+    EmailMessage       message;
     HashMap<String, Object> args;
     User user;
     User payee;
-    System.out.println(invoicesList.size());
     for (Invoice invoice: invoicesList){
-      args = new HashMap<>();
-      user = (User) userDAO.find(invoice.getPayerId());
-      payee = (User) userDAO.find(invoice.getPayeeId());
+      args    = new HashMap<>();
+      message = new EmailMessage();
+      user    = (User) userDAO.find(invoice.getPayerId());
+      payee   = (User) userDAO.find(invoice.getPayeeId());
+      message.setTo(new String[]{user.getEmail()});
+      dueDate.setTime(invoice.getPaymentDate());
+      SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-YYYY");
       args.put("account", invoice.getId());
-      args.put("amount", invoice.getAmount());
-      args.put("date", invoice.getPaymentDate());
+      args.put("amount", formatter.format(invoice.getAmount()/100.00));
+      args.put("date", dateFormat.format(invoice.getPaymentDate()));
       args.put("link", config.getUrl());
       args.put("name", user.getFirstName());
       args.put("toEmail", payee.getEmail());
-      System.out.println("Sent");
       email.sendEmailFromTemplate(user, message, "schedule-paid", args);
     }
   }
