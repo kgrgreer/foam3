25d6781ddadb9606328a62e546fdf7e007ff7ed5
Joel Hughes <joel@nanopay.net>
2018-02-26 09:15:08 -0500

    Remove combined jenkins script jenkins supports multiple build scripts, but each must be configured indpendently.

--- tools/jenkins/cc.sh
+++ tools/jenkins/cc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Build specifics for Connected-City
 
 sed -i -e "s,<welcome-file>/nanopay/src/net/nanopay/index.html</welcome-file>,<welcome-file>/nanopay/src/net/nanopay/connected-city.html</welcome-file>,g" WEB-INF/web.xml
@@ -7,3 +6,5 @@ sed -i -e "s,<welcome-file>/nanopay/src/net/nanopay/index.html</welcome-file>,<w
 sed -i -e "s,<param-value>/merchant/src/net/nanopay/merchant/index.html</param-value>,<param-value>/merchant/src/net/nanopay/merchant/connected-city.html</param-value>,g" WEB-INF/web.xml
 
 sed -i -e "s/\"from\":\"info@nanopay.net\",\"replyTo\":\"noreply@nanopay.net\",\"displayName\":\"nanopay Corporation\"/\"from\":\"info@connectedcity.net\",\"replyTo\":\"noreply@connectedcity.net\",\"displayName\":\"ConnectedCity\"/g" nanopay/src/services
+
+exit 0
