12158b6e0eba0a1fa12cf99b2b7398e9479ba48e
Kirk Eaton <kirk@nanopay.net>
2018-02-26 10:37:11 -0500

    Added generation of pain and tsin message

--- gen.sh
+++ gen.sh
@@ -16,7 +16,13 @@ clean() {
 # Clean top level build folder
 clean
 
-node tools/xsd/index.js net.nanopay.iso20022 pacs.002.001.09.xsd pacs.008.001.06.xsd pacs.028.001.01.xsd
+node tools/xsd/index.js net.nanopay.iso20022 \
+  pacs.002.001.09.xsd \
+  pacs.008.001.06.xsd \
+  pacs.028.001.01.xsd \
+  pain.007.001.07.xsd \
+  tsin.004.001.01.xsd
+
 node tools/xsd/index.js net.nanopay.fx.ascendantfx.model AFXLinkCustom.svc.xsd
 
 # For each project, grabs java code
