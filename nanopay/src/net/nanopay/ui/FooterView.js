e95643c634f59f77b4d6c7547cf8a0113579228a
pakkiraju <prad.akkiraju@gmail.com>
2018-03-01 10:58:15 -0500

    Added terms and conditions to footerView

--- nanopay/src/net/nanopay/ui/FooterView.js
+++ nanopay/src/net/nanopay/ui/FooterView.js
@@ -32,8 +32,63 @@ foam.CLASS({
         ^ .copyright-label {
           margin-right: 50px;
           float: right;
-          opacity: 0.3;
+          opacity: 0.6;
+          font-family: Roboto;
+          font-size: 14px;
+          font-weight: 300;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: normal;
+          letter-spacing: 0.2px;
+          text-align: left;
+          color: #272727;
+          width: auto !Important;
+          padding: 0 10px !Important;
         }
+        .net-nanopay-ui-ActionView-goToTerm {
+          background: transparent;
+          opacity: 0.6;
+          font-family: Roboto;
+          font-size: 14px;
+          font-weight: 300;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: normal;
+          letter-spacing: 0.2px;
+          text-align: left;
+          color: #272727;
+          width: auto !Important;
+          padding: 0 10px !Important;
+        }
+        .net-nanopay-ui-ActionView-goToPrivacy {
+          background: transparent;
+          opacity: 0.6;
+          font-family: Roboto;
+          font-size: 14px;
+          font-weight: 300;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: normal;
+          letter-spacing: 0.2px;
+          text-align: left;
+          color: #272727;
+          width: auto !Important;
+          padding: 0 10px !Important;
+        }
+        .net-nanopay-ui-ActionView-goToNanopay {
+          background: transparent;
+          opacity: 0.6;
+          font-family: Roboto;
+          font-size: 14px;
+          font-weight: 300;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: normal;
+          letter-spacing: 0.2px;
+          text-align: left;
+          color: #272727;
+          width: auto !Important;
+          padding: 0 10px !Important;
       */}
     })
   ],
@@ -44,9 +99,48 @@ foam.CLASS({
       this
         .addClass(this.myClass())
         .start()
-          .start('h3').add('Powered by nanopay').end()
+          .start('h3')
+            .start(this.GO_TO_NANOPAY).end()
+            .add('|')
+            .start(this.GO_TO_TERM).end()
+            .add('|')
+            .start(this.GO_TO_PRIVACY).end()
+          .end()
           .start('h3').addClass('copyright-label').add('Copyright © 2018 ' + this.webApp + '. All right reserved.').end()
         .end();
     }
+  ],
+
+  actions: [
+     {
+      name: 'goToNanopay',
+      label: 'nanopay.net',
+      code: function(X) {
+        var self = this;
+        var nanopayURL = 'https://nanopay.net';
+        self.window.location.assign(nanopayURL);
+      }
+    },
+    {
+      name: 'goToTerm',
+      label: 'Terms and Conditions',
+      code: function(X) {
+        var self = this;
+        //var alternaUrl = self.window.location.orgin + "/termsandconditions/"
+        var termsURL = 'https://nanopay.net/termsandconditions/';
+        self.window.location.assign(termsURL);
+      }
+    },
+    {
+      name: 'goToPrivacy',
+      label: 'Privacy Policy',
+      code: function(X) {
+        var self = this;
+        //var alternaUrl = self.window.location.orgin + "/privacy-policy/"
+        var priacyURL = 'https://nanopay.net/privacy-policy/';
+        self.window.location.assign(privacyURL);
+      }
+    }
   ]
+
 });
