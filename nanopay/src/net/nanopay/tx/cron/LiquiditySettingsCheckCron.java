1d827f832e2225c2678040b7d2a55a1e228cd808
penzital <penzital888@gmail.com>
2018-03-02 15:26:52 -0500

    style, indent, comments corrected

--- nanopay/src/net/nanopay/tx/cron/LiquiditySettingsCheckCron.java
+++ nanopay/src/net/nanopay/tx/cron/LiquiditySettingsCheckCron.java
@@ -15,89 +15,84 @@ import net.nanopay.tx.model.Transaction;
 import static foam.mlang.MLang.*;
 
 import java.util.List;
-
+/*
+    Cronjob checks Liquidity Settings.
+    Does cash in/out depending on user/group Liquidity Settings setup
+ */
 public class LiquiditySettingsCheckCron implements ContextAgent {
+  protected long amount_;
+  protected TransactionType type_;
+  protected CashOutFrequency frequency_;
 
-    protected long amount;
-    protected TransactionType type;
-    protected CashOutFrequency frequency;
+  public LiquiditySettingsCheckCron(CashOutFrequency frequency){
+    this.frequency_ = frequency;
+  }
+
+  @Override
+  public void execute(X x) {
+    long bankId;
+    DAO userDAO_ = (DAO) x.get("userDAO");
+    DAO accountDAO_ = (DAO) x.get("localAccountDAO");
+    DAO bankAccountDAO_ = (DAO) x.get("localBankAccountDAO");
+    DAO liquiditySettingsDAO = (DAO) x.get("liquiditySettingsDAO");
+    DAO groupDAO = (DAO) x.get("groupDAO");
+    List users = ((ListSink)userDAO_.select(new ListSink())).getData();
     long balance;
+    for( int i=0; i<users.size(); i++ ){
+      User user = (User) users.get(i);
+      Account acc = (Account) accountDAO_.find(((User) users.get(i)).getId());
+      //DAO banks = user.getBankAccounts();
+      BankAccount bank = (BankAccount) bankAccountDAO_.find(AND(
+              EQ(BankAccount.OWNER, user.getId()),
+              EQ(BankAccount.STATUS, "Verified")
+              )
+      );
+      if ( bank != null && acc != null ){
+        balance = acc.getBalance();
+        bankId = bank.getId();
+      } else continue;
 
-    public LiquiditySettingsCheckCron(CashOutFrequency frequency){
-        this.frequency = frequency;
-    }
-
-    @Override
-    public void execute(X x) {
-        long bankId;
-        DAO userDAO_ = (DAO) x.get("userDAO");
-        DAO accountDAO_ = (DAO) x.get("localAccountDAO");
-        DAO bankAccountDAO_ = (DAO) x.get("localBankAccountDAO");
-        DAO liquiditySettingsDAO = (DAO) x.get("liquiditySettingsDAO");
-        DAO groupDAO = (DAO) x.get("groupDAO");
-        List users = ((ListSink)userDAO_.select(new ListSink())).getData();
-        long balance;
-        for(int i=0;i<users.size();i++){
-            User user = (User) users.get(i);
-
-            Account acc = (Account) accountDAO_.find(((User) users.get(i)).getId());
-            //DAO banks = user.getBankAccounts();
-            BankAccount bank = (BankAccount) bankAccountDAO_.find(AND(
-                    EQ(BankAccount.OWNER, user.getId()),
-                    EQ(BankAccount.STATUS, "Verified")
-                    )
-            );
-            if (bank!=null && acc!=null){
-                balance = acc.getBalance();
-                bankId = bank.getId();
-            } else continue;
-
-
-            LiquiditySettings ls = (LiquiditySettings) liquiditySettingsDAO.find(user.getId());
-            if( ls != null ){
-                if(ls.getBankAccountId()>0){
-                    bankId = ls.getBankAccountId();
-                }
-                if(checkBalance(ls, balance) && ls.getCashOutFrequency()==frequency){
-                    addTransaction(x, user.getId(), bankId);
-                }
-            } else{
-                Group group = (Group) groupDAO.find(user.getGroup());
-                ls = group.getLiquiditySettings();
-                if(checkBalance(ls, balance) && ls.getCashOutFrequency()==frequency){
-                    addTransaction(x, user.getId(), bankId);
-                }
-            }
+      LiquiditySettings ls = (LiquiditySettings) liquiditySettingsDAO.find(user.getId());
+      if( ls != null ){
+        if( ls.getBankAccountId() > 0 ){
+          bankId = ls.getBankAccountId();
         }
-    }
-
-
-    public boolean checkBalance(LiquiditySettings ls, long balance){
-        if(balance>ls.getMaximumBalance()){
-            amount = balance - ls.getMaximumBalance();
-            type = TransactionType.CASHOUT;
-            return true;
+        if( checkBalance(ls, balance) && ls.getCashOutFrequency() == frequency_ ){
+          addTransaction(x, user.getId(), bankId);
         }
-        else if(balance < ls.getMinimumBalance()){
-            amount = ls.getMinimumBalance() - balance;
-            type = TransactionType.CASHIN;
-            return true;
+      } else{
+        Group group = (Group) groupDAO.find(user.getGroup());
+        ls = group.getLiquiditySettings();
+        if( checkBalance(ls, balance) && ls.getCashOutFrequency() == frequency_ ){
+          addTransaction(x, user.getId(), bankId);
         }
-        else return false;
+      }
     }
-
-    public void addTransaction(X x, long userId, long bankId){
-        Transaction transaction = new Transaction.Builder(x)
-                .setPayeeId(userId)
-                .setPayerId(userId)
-                .setAmount(amount)
-                .setType(type)
-                .setBankAccountId(bankId)
-                .build();
-        DAO txnDAO = (DAO) x.get("transactionDAO");
-        txnDAO.put_(x, transaction);
+  }
+  public boolean checkBalance(LiquiditySettings ls, long balance){
+    if( balance > ls.getMaximumBalance() ){
+      amount_ = balance - ls.getMaximumBalance();
+      type_ = TransactionType.CASHOUT;
+      return true;
     }
-
+    if( balance < ls.getMinimumBalance() ){
+      amount_ = ls.getMinimumBalance() - balance;
+      type_ = TransactionType.CASHIN;
+      return true;
+    }
+    return false;
+  }
+  public void addTransaction(X x, long userId, long bankId){
+    Transaction transaction = new Transaction.Builder(x)
+            .setPayeeId(userId)
+            .setPayerId(userId)
+            .setAmount(amount_)
+            .setType(type_)
+            .setBankAccountId(bankId)
+            .build();
+    DAO txnDAO = (DAO) x.get("transactionDAO");
+    txnDAO.put_(x, transaction);
+  }
 }
 
 
