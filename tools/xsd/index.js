63babdfa04a02502570e5c2156db63309fc5f2aa
Kirk Eaton <kirk@nanopay.net>
2018-03-05 12:51:11 -0500

    Removed filter of simpleTypes

--- tools/xsd/index.js
+++ tools/xsd/index.js
@@ -252,12 +252,8 @@ module.exports = {
   blacklist: blacklist
 };`
 
-simpleTypes = simpleTypes.filter(function (element, index) {
-  return index == simpleTypes.indexOf(element);
-});
-
-classes = classes.filter(function (element, index) {
-  return index == classes.indexOf(element);
+classes = classes.filter(function (element, index, self) {
+  return index == self.indexOf(element);
 });
 
 var files = Object.keys(simpleTypes).concat(classes).sort().map(function (file) {
