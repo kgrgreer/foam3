8c686d94261943e5a27e9eae0e85acebaca2b9fa
Kirk Eaton <kirk@nanopay.net>
2018-03-05 11:35:20 -0500

    Added toJSON methods for ISO date formats

--- nanopay/src/net/nanopay/iso20022/refinements.js
+++ nanopay/src/net/nanopay/iso20022/refinements.js
@@ -2,51 +2,53 @@ foam.CLASS({
   refines: 'net.nanopay.iso20022.ISODate',
 
   properties: [
-    {
-      name: 'toXML',
-      value: function toXML(value, Outputter) {
-        // returns date in the following format: YYYY-MM-DD
-        // pads month and date with leading zeros
-        var year = value.getUTCFullYear();
-        var month = value.getUTCMonth() + 1;
-        month = ('00' + month).slice(-2);
+    [ 'toJSON', this.formatDate ],
+    [ 'toXML',  this.formatDate ]
+  ],
 
-        var date = value.getUTCDate();
-        date = ('00' + date).slice(-2);
+  function formatDate (value, Outputter) {
+    // returns date in the following format: YYYY-MM-DD
+    // pads month and date with leading zeros
+    var year = value.getUTCFullYear();
+    var month = value.getUTCMonth() + 1;
+    month = ('00' + month).slice(-2);
 
-        return year + '-' + month + '-' + date;
-      }
-    }
-  ]
+    var date = value.getUTCDate();
+    date = ('00' + date).slice(-2);
+
+    return year + '-' + month + '-' + date;
+  }
 });
 
 foam.CLASS({
   refines: 'net.nanopay.iso20022.ISODateTime',
 
   properties: [
-    {
-      name: 'toXML',
-      value: function toXML(value, Outputter) {
-        // returns date in the following format: YYYY-MM-DDThh:mm:ss.sss+/-hh:mm
-        // pads hour and minute in offset with leading zeros
-        var isoString = value.toISOString();
-        isoString = isoString.substring(0, isoString.length - 1);
+    [ 'toJSON', this.formatDate ],
+    [ 'toXML',  this.formatDate ]
+  ],
 
-        var timezoneOffset = value.getTimezoneOffset();
-        if ( timezoneOffset < 0 ) {
-          timezoneOffset *= -1
-          isoString += '+';
-        } else {
-          isoString += '-';
-        }
+  methods: [
+    function formatDate (value, Outputter) {
+      // returns date in the following format: YYYY-MM-DDThh:mm:ss.sss+/-hh:mm
+      // pads hour and minute in offset with leading zeros
+      var isoString = value.toISOString();
+      isoString = isoString.substring(0, isoString.length - 1);
 
-        // calculate hour and minute offset
-        var hourOffset = ('00' + (Math.trunc(timezoneOffset / 60))).slice(-2);
-        timezoneOffset = ('00' + (timezoneOffset - ( hourOffset * 60 ))).slice(-2);
-
-        isoString += hourOffset + ':' + timezoneOffset;
-        return isoString
+      var timezoneOffset = value.getTimezoneOffset();
+      if ( timezoneOffset < 0 ) {
+        timezoneOffset *= -1
+        isoString += '+';
+      } else {
+        isoString += '-';
       }
+
+      // calculate hour and minute offset
+      var hourOffset = ('00' + (Math.trunc(timezoneOffset / 60))).slice(-2);
+      timezoneOffset = ('00' + (timezoneOffset - ( hourOffset * 60 ))).slice(-2);
+
+      isoString += hourOffset + ':' + timezoneOffset;
+      return isoString;
     }
   ]
 });
@@ -55,25 +57,27 @@ foam.CLASS({
   refines: 'net.nanopay.iso20022.ISOTime',
 
   properties: [
-    {
-      name: 'toXML',
-      value: function toXML(value, Outputter) {
-        // returns date in the following format: HH:mm:ss.sssZ
-        // pads all values with leading zeros
-        var hours = value.getUTCHours();
-        hours = ('00' + hours).slice(-2);
+    [ 'toJSON', this.formatDate ],
+    [ 'toXML',  this.formatDate ]
+  ],
 
-        var minutes = value.getUTCMinutes();
-        minutes = ('00' + minutes).slice(-2);
+  methods: [
+    function formatDate (value, Outputter) {
+      // returns date in the following format: HH:mm:ss.sssZ
+      // pads all values with leading zeros
+      var hours = value.getUTCHours();
+      hours = ('00' + hours).slice(-2);
 
-        var seconds = value.getUTCSeconds();
-        seconds = ('00' + seconds).slice(-2);
+      var minutes = value.getUTCMinutes();
+      minutes = ('00' + minutes).slice(-2);
 
-        var milliseconds = value.getUTCMilliseconds();
-        milliseconds = ('000' + milliseconds).slice(-3);
+      var seconds = value.getUTCSeconds();
+      seconds = ('00' + seconds).slice(-2);
 
-        return hours + ':' + minutes + ':' + seconds + '.' + milliseconds + 'Z';
-      }
+      var milliseconds = value.getUTCMilliseconds();
+      milliseconds = ('000' + milliseconds).slice(-3);
+
+      return hours + ':' + minutes + ':' + seconds + '.' + milliseconds + 'Z';
     }
   ]
 });
