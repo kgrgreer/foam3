e9987ab56309efa7e6dc13b976ba520efc2704a1
Kirk Eaton <kirk@nanopay.net>
2018-03-08 15:24:03 -0500

    Added skeleton of InvitationHistoryItemView

---
+++ nanopay/src/net/nanopay/invite/ui/InvitationHistoryItemView.js
@@ -0,0 +1,17 @@
+foam.CLASS({
+  package: 'net.nanopay.invoice.ui',
+  name: 'InvitationHistoryItemView',
+  extends: 'foam.u2.View',
+
+  implements: [
+    'foam.u2.history.HistoryItemView'
+  ],
+
+  documentation: 'View displaying history for invitation item',
+
+  methods: [
+    function outputRecord(parentView, record) {
+
+    }
+  ]
+});
