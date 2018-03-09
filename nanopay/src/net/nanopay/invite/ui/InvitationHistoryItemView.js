42f39241ead4e4838695a478729b9e16f09fb1c1
Kirk Eaton <kirk@nanopay.net>
2018-03-09 08:30:42 -0500

    Fixed sequence number issue with invitationHistoryt

--- nanopay/src/net/nanopay/invite/ui/InvitationHistoryItemView.js
+++ nanopay/src/net/nanopay/invite/ui/InvitationHistoryItemView.js
@@ -1,5 +1,5 @@
 foam.CLASS({
-  package: 'net.nanopay.invoice.ui',
+  package: 'net.nanopay.invite.ui',
   name: 'InvitationHistoryItemView',
   extends: 'foam.u2.View',
 
@@ -10,8 +10,23 @@ foam.CLASS({
   documentation: 'View displaying history for invitation item',
 
   methods: [
-    function outputRecord(parentView, record) {
 
+    function formateDate(timestamp) {
+
+    },
+
+
+    function outputRecord(parentView, record) {
+      var self = this;
+
+
+
+      return parentView
+        .style({' padding-left': '20px' })
+        .start('')
+
+      console.log('parentView', parentView);
+      console.log('record = ', record);
     }
   ]
 });
