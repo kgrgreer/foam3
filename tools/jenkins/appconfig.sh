d1cbef7698b361792d4ebb0d9b2ff198dd7d6324
waseemchamaaa <waseemchamaa6@gmail.com>
2018-03-12 11:34:49 -0400

    placed quotes in the appopriate place

--- tools/jenkins/appconfig.sh
+++ tools/jenkins/appconfig.sh
@@ -4,21 +4,21 @@
 RELEASE=`echo ${GIT_BRANCH} | sed 's/.*\///'`
 NAME=`echo ${RELEASE} | sed 's/-v.*//'`
 VERSION=`echo ${RELEASE} | sed 's/.*-v//'`
-MODE="'DEVELOPMENT'"
+MODE="DEVELOPMENT"
 YEAR=`date +%Y`
 COPYRIGHT="(c) Copyright ${YEAR} nanopay Corporation. All Rights Reserved"
 
 echo
 if [[ "${NAME}" = *"test"* ]]; then
-    MODE="'TEST'" #"TEST"
+    MODE="TEST" #"TEST"
 elif [[ "${NAME}" = *"prod"* ]]; then
-    MODE="'PRODUCTION'" #"PRODUCTION"
+    MODE="PRODUCTION" #"PRODUCTION"
 elif [[ "${NAME}" = *"integration"* ]]; then
-    MODE="'DEMO'" #"DEMO"
+    MODE="DEMO" #"DEMO"
 elif [[ "${NAME}" = *"demo"* ]]; then
-    MODE="'DEMO'" #"DEMO"
+    MODE="DEMO" #"DEMO"
 elif [[ "${NAME}" = *"staging"* ]]; then
-    MODE="'STAGING'" #"STAGING"
+    MODE="STAGING" #"STAGING"
 fi
 
 echo RELEASE=${RELEASE}
@@ -33,9 +33,9 @@ sed -i -e "s/name: 'version'/name: 'version', value: '${VERSION}'/g" foam2/src/f
 
 sed -i -e "s/name: 'copyright'/name: 'copyright', value: '${COPYRIGHT}'/g" foam2/src/foam/nanos/app/AppConfig.js
 
-sed -i -e "s/name: 'mode'/name: 'mode', value: ${MODE}/g" foam2/src/foam/nanos/app/AppConfig.js
+sed -i -e "s/name: 'mode'/name: 'mode', value: '${MODE}'/g" foam2/src/foam/nanos/app/AppConfig.js
 
-sed -i -e "s/MODE=1/MODE=${MODE}/g" find.sh
+sed -i -e "s/MODE=1/MODE='${MODE}'/g" find.sh
 
-sed -i -e "s/VERSION=1/VERSION=${VERSION}/g" find.sh
+sed -i -e "s/VERSION=1/VERSION='${VERSION}'/g" find.sh
 exit 0
