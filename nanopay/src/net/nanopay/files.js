e686df1a6b146f297a4378772f981ba4ab5de6ac
Kirk Eaton <kirk@nanopay.net>
2018-03-13 16:36:58 -0400

    Added CreateUserOnInvitationCreateDAO

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -161,6 +161,7 @@ FOAM_FILES([
   { name: 'net/nanopay/invite/ui/QuestionView', flags: ['web'] },
   { name: 'net/nanopay/invite/ui/QuestionnaireView', flags: ['web'] },
   { name: 'net/nanopay/invite/InvitationTokenService' },
+  { name: 'net/nanopay/invite/CreateUserOnInvitationCreateDAO', flags: ['java'] },
 
   // invoice
   { name: 'net/nanopay/invoice/model/Invoice'},
