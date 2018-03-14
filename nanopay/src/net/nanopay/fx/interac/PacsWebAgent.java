54519936d7209c35823ea0237c27b0d840925372
Minsun <minsun@nanopay.net>
2018-03-14 16:57:29 -0400

    pacs msg refinements for Interac

--- nanopay/src/net/nanopay/fx/interac/PacsWebAgent.java
+++ nanopay/src/net/nanopay/fx/interac/PacsWebAgent.java
@@ -27,9 +27,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
-import net.nanopay.fx.interac.model.PacsModel008;
-import net.nanopay.fx.interac.model.PacsModel002;
-import net.nanopay.fx.interac.model.PacsModel028;
+import net.nanopay.iso20022.Pacs00800106;
+import net.nanopay.iso20022.Pacs00200109;
+import net.nanopay.iso20022.Pacs02800101;
 
 public class PacsWebAgent
   implements WebAgent
@@ -81,9 +81,10 @@ public class PacsWebAgent
           outputterJson.setOutputClassNames(false);
 
           if ( "008".equals(msg) ) {
-            PacsModel008 pacsModel008 = (PacsModel008) jsonParser.parseString(data, PacsModel008.class);
+            //PacsModel008 pacsModel008 = (PacsModel008) jsonParser.parseString(data, PacsModel008.class);
+            Pacs00800106 pacs00800106 = (Pacs00800106) jsonParser.parseString(data, Pacs00800106.class);
 
-            if ( pacsModel008 == null || "".equals(pacsModel008) ) {
+            if ( pacs00800106 == null || "".equals(pacs00800106) ) {
               out.println("Parse Error");
 
               String message = getParsingError(x, buffer_.toString());
@@ -93,13 +94,13 @@ public class PacsWebAgent
               return;
             }
 
-            PacsModel002 pacsModel002 = pacsModel008.generatePacs002Msgby008Msg();
+            Pacs00200109 pacs00200109 = pacs00800106.generatePacs002Msgby008Msg();
 
-            outputterJson.output(pacsModel002);
+            outputterJson.output(pacs00200109);
           } else {
-            PacsModel028 pacsModel028 = (PacsModel028) jsonParser.parseString(data, PacsModel028.class);
+            Pacs02800101 pacs02800101 = (Pacs02800101) jsonParser.parseString(data, Pacs02800101.class);
 
-            if ( pacsModel028 == null || "".equals(pacsModel028) ) {
+            if ( pacs02800101 == null || "".equals(pacs02800101) ) {
               out.println("Parse Error");
 
               String message = getParsingError(x, buffer_.toString());
@@ -109,45 +110,45 @@ public class PacsWebAgent
               return;
             }
 
-            PacsModel002 pacsModel002 = pacsModel028.generatePacs002Msgby028Msg();
+            Pacs00200109 pacs00200109 = pacs02800101.generatePacs002Msgby028Msg();
 
-            outputterJson.output(pacsModel002);
+            outputterJson.output(pacs00200109);
           }
 
           response.setContentType("application/json");
           out.println(outputterJson.toString());
-       } else if ( "xml".equals(format) ) {
-         XMLSupport      xmlSupport = new XMLSupport();
-         XMLInputFactory factory    = XMLInputFactory.newInstance();
-         StringReader    reader     = new StringReader(data);
-         XMLStreamReader xmlReader  = factory.createXMLStreamReader(reader);
-         List<FObject>   objList    = xmlSupport.fromXML(x, xmlReader, PacsModel008.class);
-
-         if ( objList.size() == 0 ) {
-           out.println("Parse Error : ");
-
-           String message = getParsingError(x, buffer_.toString());
-           logger.error(message + ", input: " + buffer_.toString());
-           out.println(message);
-           out.flush();
-           return;
-         }
-         if ( objList.size() == 0 ) System.out.println("eeee");
-
-
-         PacsModel008 pacsModel008 = null;
-         Iterator i = objList.iterator();
-         while ( i.hasNext() ) {
-           pacsModel008 = (PacsModel008)i.next();
-           PacsModel002 pacsModel002 = pacsModel008.generatePacs002Msgby008Msg();
-           response.setContentType("application/xml");
-           out.println(xmlSupport.toXMLString(pacsModel002));
-         }
-
-         //PacsModel002  pacsModel002 = pacsModel008.generatePacs002Msg();
-         //response.setContentType("application/xml");
-         //out.println(xmlSupport.toXMLString(pacsModel002));
-    }
+        } //else if ( "xml".equals(format) ) {
+    //      XMLSupport      xmlSupport = new XMLSupport();
+    //      XMLInputFactory factory    = XMLInputFactory.newInstance();
+    //      StringReader    reader     = new StringReader(data);
+    //      XMLStreamReader xmlReader  = factory.createXMLStreamReader(reader);
+    //      List<FObject>   objList    = xmlSupport.fromXML(x, xmlReader, PacsModel008.class);
+    //
+    //      if ( objList.size() == 0 ) {
+    //        out.println("Parse Error : ");
+    //
+    //        String message = getParsingError(x, buffer_.toString());
+    //        logger.error(message + ", input: " + buffer_.toString());
+    //        out.println(message);
+    //        out.flush();
+    //        return;
+    //      }
+    //      if ( objList.size() == 0 ) System.out.println("eeee");
+    //
+    //
+    //      PacsModel008 pacsModel008 = null;
+    //      Iterator i = objList.iterator();
+    //      while ( i.hasNext() ) {
+    //        pacsModel008 = (PacsModel008)i.next();
+    //        PacsModel002 pacsModel002 = pacsModel008.generatePacs002Msgby008Msg();
+    //        response.setContentType("application/xml");
+    //        out.println(xmlSupport.toXMLString(pacsModel002));
+    //      }
+    //
+    //      PacsModel002  pacsModel002 = pacsModel008.generatePacs002Msg();
+    //      response.setContentType("application/xml");
+    //      out.println(xmlSupport.toXMLString(pacsModel002));
+    // }
       }
     } catch (Throwable t) {
       out.println("Error " + t);
