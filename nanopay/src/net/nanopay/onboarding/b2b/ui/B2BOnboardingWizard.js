22151d9ea26f2b6d64a8b0d61dbf135914c41d57
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2018-03-16 14:05:04 -0400

    Prelim B2BOnboardingWizard

---
+++ nanopay/src/net/nanopay/onboarding/b2b/ui/B2BOnboardingWizard.js
@@ -0,0 +1,45 @@
+foam.CLASS({
+  package: 'net.nanopay.onboarding.b2b.ui',
+  name: 'B2BOnboardingWizard',
+  extends: 'net.nanopay.ui.wizard.WizardView',
+
+  documentation: 'Onboarding Wizard for new B2B users.',
+
+  requires: [
+    'foam.u2.dialog.NotificationMessage'
+  ],
+
+  imports: [
+    'stack'
+  ],
+
+  axioms: [
+    foam.u2.CSS.create({code: net.nanopay.ui.wizard.WizardView.getAxiomsByClass(foam.u2.CSS)[0].code})
+  ],
+
+  methods: [
+    function init() {
+      this.views = [
+        { parent: 'addB2BUser', id: 'form-addB2BUser-principleOwner', label: 'Principle Owner(s) Profile', view: { class: 'net.nanopay.onboarding.b2b.ui.AddPrincipleOwnersForm' } }
+      ];
+      this.SUPER();
+    }
+  ],
+
+  actions: [
+    {
+      name: 'goBack',
+      label: 'Back',
+      code: function(X) {
+        this.stack.pop();
+      }
+    },
+    {
+      name: 'goNext',
+      label: 'Next',
+      code: function() {
+        this.stack.pop();
+      }
+    }
+  ]
+});
