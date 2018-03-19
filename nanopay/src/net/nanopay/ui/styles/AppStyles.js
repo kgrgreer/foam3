d90e0c113cfff03001220b518083ad19ec359c5c
Kirk Eaton <kirk@nanopay.net>
2018-03-19 09:58:56 -0400

    Fixed status bubble styling

--- nanopay/src/net/nanopay/ui/styles/AppStyles.js
+++ nanopay/src/net/nanopay/ui/styles/AppStyles.js
@@ -498,6 +498,7 @@ foam.CLASS({
           margin-left: 5px;
           width: 77px;
           height: 20px;
+          display: inline-block;
         }
         .Compliance-Status-Requested span {
           width: 59px;
@@ -509,12 +510,12 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #59a5d5;
-          display: inline-block;
         }
         .Compliance-Status-Passed {
           margin-left: 5px;
           width: 62px;
           height: 20px;
+          display: inline-block;
         }
         .Compliance-Status-Passed span {
           width: 42px;
@@ -526,12 +527,12 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #1cc2b7;
-          display: inline-block;
         }
         .Compliance-Status-Failed {
           margin-left: 5px;
           width: 53px;
           height: 20px;
+          display: inline-block;
         }
         .Compliance-Status-Failed span {
           width: 34px;
@@ -543,13 +544,13 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #d81e05;
-          display: inline-block;
         }
 
         .Invite-Status-Pending {
           margin-left: 5px;
           width: 65px;
           height: 20px;
+          display: inline-block;
         }
         .Invite-Status-Pending span {
           width: 45px;
@@ -561,12 +562,12 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #a4b3b8;
-          display: inline-block;
         }
         .Invite-Status-Submitted {
           margin-left: 5px;
           width: 77px;
           height: 20px;
+          display: inline-block;
         }
         .Invite-Status-Submitted span {
           width: 57px;
@@ -578,12 +579,12 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           border: solid 1px #2cab70;
-          display: inline-block;
         }
         .Invite-Status-Active {
           margin-left: 5px;
           width: 55px;
           height: 20px;
+          display: inline-block;
         }
         .Invite-Status-Active span {
           width: 35px;
@@ -595,12 +596,12 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #2cab70;
-          display: inline-block;
         }
         .Invite-Status-Disabled {
           margin-left: 5px;
           width: 68px;
           height: 20px;
+          display: inline-block;
         }
         .Invite-Status-Disabled span {
           width: 48px;
@@ -612,7 +613,6 @@ foam.CLASS({
           padding: 0 10px 0 10px;
           border-radius: 100px;
           background-color: #093649;
-          display: inline-block;
         }
       */}
     })
