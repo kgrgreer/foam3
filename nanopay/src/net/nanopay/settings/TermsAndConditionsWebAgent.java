8e044eb7f9609ba3b05352eee7fe00feb1c22b8a
waseemchamaaa <waseemchamaa6@gmail.com>
2018-04-02 13:01:38 -0400

    changed means of grabbing last element in DAO

--- nanopay/src/net/nanopay/settings/TermsAndConditionsWebAgent.java
+++ nanopay/src/net/nanopay/settings/TermsAndConditionsWebAgent.java
@@ -13,28 +13,39 @@ import foam.nanos.http.WebAgent;
 import net.nanopay.model.TermsAndConditions;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import static foam.mlang.MLang.*;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 
 public class TermsAndConditionsWebAgent
         implements WebAgent {
 
-    @Override
-    public void execute(X x) {
-      PrintWriter        out      = x.get(PrintWriter.class);
-      HttpServletRequest request  = x.get(HttpServletRequest.class);
-      DAO                tcDAO    = (DAO) x.get("termsAndConditionsDAO");
-      String             version  = request.getParameter("version");
-      TermsAndConditions terms;
-      if ( version.equals("") ) { 
-        ListSink listSink = (ListSink) tcDAO.select(new ListSink());
-        List     termsList = listSink.getData();
-        terms = (TermsAndConditions) termsList.get(termsList.size()-1);
-      } else {
-        terms = (TermsAndConditions) tcDAO.find(EQ(TermsAndConditions.ID,Long.valueOf(version)));
-      }
-      out.println(terms.getBody());
+  @Override
+  public void execute(X x) {
+    HttpServletRequest  request  = x.get(HttpServletRequest.class);
+    HttpServletResponse response = x.get(HttpServletResponse.class);
+    DAO                 tcDAO    = (DAO) x.get("termsAndConditionsDAO");
+    String              version  = request.getParameter("version");
+    TermsAndConditions  terms;
+    PrintWriter         out      = null;
+    try {
+      out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), StandardCharsets.ISO_8859_1), true);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    if ( version.equals("") ) {
+      ListSink listSink = (ListSink) tcDAO.orderBy(new foam.mlang.order.Desc(TermsAndConditions.ID)).limit(1).select(new ListSink());
+
+      terms = (TermsAndConditions) listSink.getData().get(0);
+    } else {
+      terms = (TermsAndConditions) tcDAO.find(EQ(TermsAndConditions.ID,Long.valueOf(version)));
+    }
+    out.println(terms.getBody());
+  }
 }
 
