2c5c3b7fb45ca2cb6efb2af8b99feab35af81119
Kevin Greer <kgrgreer@gmail.com>
2018-04-06 14:50:36 -0400

    Add web flag to EFTReturnFileCredentials since it needs to be configured in the GUI.

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -164,7 +164,7 @@ FOAM_FILES([
   { name: 'net/nanopay/cico/model/ServiceProvider' },
   { name: 'net/nanopay/cico/model/TransactionType' },
   { name: 'net/nanopay/cico/model/Transaction' },
-  { name: 'net/nanopay/cico/model/EFTReturnFileCredentials' },
+  { name: 'net/nanopay/cico/model/EFTReturnFileCredentials', flags: ['web'] },
   { name: 'net/nanopay/cico/service/BankAccountVerifier' },
   { name: 'net/nanopay/cico/service/ClientBankAccountVerifierService' },
   { name: 'net/nanopay/cico/ui/bankAccount/AddBankView', flags: ['web'] },
