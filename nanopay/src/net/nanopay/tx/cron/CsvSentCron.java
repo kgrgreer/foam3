287c9ad9fac3b3e6bab81947ad07a8521a32c666
Kristina Smirnova <penzital888@gmail.com>
2018-04-07 10:17:58 -0400

    Update CsvSentCron.java

--- nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
+++ nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
@@ -3,6 +3,7 @@ package net.nanopay.tx.cron;
 import foam.core.ContextAgent;
 import foam.core.X;
 import net.nanopay.cico.spi.alterna.AlternaSFTPService;
+import net.nanopay.cico.spi.alterna.CsvUtil;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -13,7 +14,7 @@ public class CsvSentCron implements ContextAgent {
   @Override
   public void execute(X x){
     AlternaSFTPService sftp = (AlternaSFTPService) x.get("alternaSftp");
-    List<Integer> cadHolidays = Arrays.asList(1, 50, 89, 141, 183, 218, 246, 281, 316, 359, 360);
+    List<Integer> cadHolidays = CsvUtil.cadHolidays;
     Calendar today = Calendar.getInstance();
     if ( today.get(Calendar.DAY_OF_WEEK) != 7 && today.get(Calendar.DAY_OF_WEEK) != 1 && !cadHolidays.contains(today.get(Calendar.DAY_OF_YEAR)) ) {
       sftp.sendCICOFile();
