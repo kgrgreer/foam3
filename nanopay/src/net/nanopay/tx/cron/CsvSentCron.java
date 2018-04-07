6784f3c4599ad30b398cab6dbb9b3ab9ee317e1e
Kevin Greer <kgrgreer@gmail.com>
2018-04-07 11:57:51 -0400

    Formatting.

--- nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
+++ nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
@@ -9,16 +9,27 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
-//Every business day the cronjob generates csv file with CICO transactions and sends it to EftCanada
-public class CsvSentCron implements ContextAgent {
-
+/**
+ * Every business day this cronjob generates csv file with
+ * CICO transactions and sends it to EftCanada.
+ **/
+public class CsvSentCron
+  implements ContextAgent
+{
   @Override
   public void execute(X x){
-    AlternaSFTPService sftp = (AlternaSFTPService) x.get("alternaSftp");
-    List<Integer> cadHolidays = CsvUtil.cadHolidays;
-    Calendar today = Calendar.getInstance();
-    if ( today.get(Calendar.DAY_OF_WEEK) != 7 && today.get(Calendar.DAY_OF_WEEK) != 1 && !cadHolidays.contains(today.get(Calendar.DAY_OF_YEAR)) ) {
-      sftp.sendCICOFile();
-    }
+    AlternaSFTPService sftp  = (AlternaSFTPService) x.get("alternaSftp");
+    Calendar           today = Calendar.getInstance();
+
+    // Don't run on Saturday
+    if ( today.get(Calendar.DAY_OF_WEEK) == 7 ) return;
+
+    // Don't run on Sunday
+    if ( today.get(Calendar.DAY_OF_WEEK) == 1 ) return;
+
+    // Don't run on holidays
+    if ( CsvUtil.cadHolidays.contains(today.get(Calendar.DAY_OF_YEAR)) ) return;
+
+    sftp.sendCICOFile();
   }
 }
