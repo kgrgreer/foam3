e1401d3235415a5ea3a3e5edd0759d445899d6e4
waseemchamaaa <waseemchamaa6@gmail.com>
2018-04-09 11:12:02 -0400

    fixed bug in which payerId was Integer

--- nanopay/src/net/nanopay/fresh/FreshBook.java
+++ nanopay/src/net/nanopay/fresh/FreshBook.java
@@ -5,6 +5,7 @@ import foam.dao.DAO;
 import foam.lib.json.Outputter;
 import foam.nanos.auth.User;
 import foam.nanos.http.WebAgent;
+import foam.nanos.logger.Logger;
 import net.nanopay.fresh.model.*;
 import net.nanopay.invoice.model.Invoice;
 import org.apache.http.HttpResponse;
@@ -30,7 +31,7 @@ import java.util.Date;
 
 
 public class FreshBook
-  implements WebAgent
+        implements WebAgent
 {
   public void execute(X x)
   {
@@ -41,15 +42,15 @@ public class FreshBook
     FreshConfig config = new FreshConfig();
     config.setCode(req.getParameter("code"));
     out.println(
-      "<HTML>" +
-        "<BODY>" +
-        "<div  style=\" position: absolute; display: block; left: 40%; top: 40%;\">\n" +
-        "<p>\n" +
-        "FreshBooks Syncing in Progress Please wait......\n" +
-        "</p>\n" +
-        "</div> " +
-        "</BODY>"+
-        "</HTML>");
+            "<HTML>" +
+                    "<BODY>" +
+                    "<div  style=\" position: absolute; display: block; left: 40%; top: 40%;\">\n" +
+                    "<p>\n" +
+                    "FreshBooks Syncing in Progress Please wait......\n" +
+                    "</p>\n" +
+                    "</div> " +
+                    "</BODY>"+
+                    "</HTML>");
     try {
       //Sends the client info to FreshBooks to retrieve the access Token
       HttpClient httpclient = HttpClients.createDefault();
@@ -57,13 +58,13 @@ public class FreshBook
       httppost.setHeader("Content-Type","application/json");
       httppost.setHeader("Api-Version","alpha");
       String body = "{ \n" +
-        "\"grant_type\": \""+config.getGrant_type()+"\",\n" +
-        "\"client_secret\": \""+config.getClient_secret()+"\",\n" +
-        "\"code\": \""+config.getCode()+"\",\n" +
-        "\"client_id\": \""+config.getClient_id()+"\",\n" +
-        "\"redirect_uri\": \""+config.getRedirect_uri()+"\"\n" +
-        "}";
-      System.out.println(body);
+              "\"grant_type\": \""+config.getGrant_type()+"\",\n" +
+              "\"client_secret\": \""+config.getClient_secret()+"\",\n" +
+              "\"code\": \""+config.getCode()+"\",\n" +
+              "\"client_id\": \""+config.getClient_id()+"\",\n" +
+              "\"redirect_uri\": \""+config.getRedirect_uri()+"\"\n" +
+              "}";
+      //System.out.println(body);
       httppost.setEntity(new StringEntity(body, ContentType.APPLICATION_FORM_URLENCODED));
       HttpResponse response = httpclient.execute(httppost);
 
@@ -76,8 +77,8 @@ public class FreshBook
 
       //Parses the response and sets to a model that matches
       token = (FreshToken) parser.parseString(line,token.getClassInfo().getObjClass());
-      System.out.println(line);
-      System.out.println(token.getAccess_token());
+      //System.out.println(line);
+      //System.out.println(token.getAccess_token());
 
       //Second call to api to retrieve the user using the accessToken
       HttpGet httpget =  new HttpGet("https://api.freshbooks.com/auth/api/v1/users/me");
@@ -87,37 +88,37 @@ public class FreshBook
       response = httpclient.execute(httpget);
       rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
       line = rd.readLine();
-      System.out.println(line);
+      //System.out.println(line);
       FreshResponse fResponse = new FreshResponse();
 
       //Parses the response and sets to a model that matches
       Outputter jout = new Outputter();
       fResponse = (FreshResponse) parser.parseString(line,fResponse.getClassInfo().getObjClass());
-      System.out.println(jout.stringify(fResponse));
-      System.out.println((FreshCurrent) fResponse.getResponse());
+      //System.out.println(jout.stringify(fResponse));
+      //System.out.println((FreshCurrent) fResponse.getResponse());
       FreshCurrent current = (FreshCurrent) fResponse.getResponse();
       FreshBusiness business = (FreshBusiness) current.getBusiness_memberships()[0].getBusiness();
 
       //Get the account id to start grabbing invoices
       String accountId = business.getAccount_id();
-      System.out.println(accountId);
+      //System.out.println(accountId);
 
 
       httpclient = HttpClients.createDefault();
       httpget =  new HttpGet("https://api.freshbooks.com/accounting/account/"+accountId+"/invoices/invoices");
-      System.out.println(httpget.getURI());
+      //System.out.println(httpget.getURI());
       httpget.setHeader("Authorization", "Bearer "+token.getAccess_token());
       httpget.setHeader("Content-Type","application/json");
       httpget.setHeader("Api-Version","alpha");
       response = httpclient.execute(httpget);
       rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
       line = rd.readLine();
-      System.out.println(line);
+      //System.out.println(line);
 
       FreshInvoiceResponse fLResponse = new FreshInvoiceResponse();
       fLResponse = (FreshInvoiceResponse) parser.parseString(line,fLResponse.getClassInfo().getObjClass());
       FreshInvoice[] invoices = (FreshInvoice[]) fLResponse.getResponse().getResult().getInvoices();
-      System.out.println(jout.stringify(invoices[0]));
+      //System.out.println(jout.stringify(invoices[0]));
       Calendar cal = Calendar.getInstance();
       for (int i = 0; i<invoices.length; i++)
       {
@@ -125,7 +126,8 @@ public class FreshBook
         User user = (User) x.get("user");
         DAO invoiceDAO = (DAO) x.get("invoiceDAO");
         Invoice account = new Invoice();
-        account.setAmount(Double.parseDouble(invoice.getAmount().getAmount()));
+        String newStr = invoice.getAmount().getAmount().replace(".", "");;
+        account.setAmount(Long.parseLong(newStr));
         Date date;
         if(!invoice.getCreate_date().equals("")) {
           date = new SimpleDateFormat("yyyy-MM-dd").parse(invoice.getCreate_date());
@@ -142,16 +144,20 @@ public class FreshBook
         account.setFreshbooksInvoiceNumber(invoice.getInvoice_number());
         account.setCurrencyCode(invoice.getAmount().getCode());
         account.setPayeeId(user.getId());
-        account.setPayerId(1350);
+        account.setCreatedBy(user.getId());
+        account.setPayerId(Long.parseLong("1350"));
         account.setFreshbooksInvoiceId(invoice.getInvoiceid());
+        account.setStatus(account.getStatus());
         if(invoice.getStatus() == 1) { account.setDraft(true);}
+        //System.out.println(jout.stringify(account));
         invoiceDAO.put(account);
       }
       resp.sendRedirect("/");
 
-    }catch (Throwable e){
-      System.out.println("*******************************BAD NEWS");
-      e.printStackTrace();
+    }catch (Throwable t){
+      //System.out.println("*******************************BAD NEWS");
+      ((Logger) x.get(Logger.class)).error("Error retrieving FreshBooks info.", t);
+      //e.printStackTrace();
       return;
     }
 
