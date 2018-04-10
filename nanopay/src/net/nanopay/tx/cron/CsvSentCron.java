ec64764f4e26ae12ee431fda988cc70456992879
zaczhang <zaczhang19@gmail.com>
2018-04-10 12:16:03 -0400

    make sure CsvSentCron can only be called in Production environment

--- nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
+++ nanopay/src/net/nanopay/tx/cron/CsvSentCron.java
@@ -2,12 +2,14 @@ package net.nanopay.tx.cron;
 
 import foam.core.ContextAgent;
 import foam.core.X;
+import foam.dao.DAO;
+import foam.nanos.app.AppConfig;
+import foam.nanos.app.Mode;
+import foam.nanos.boot.NSpec;
 import net.nanopay.cico.spi.alterna.AlternaSFTPService;
 import net.nanopay.cico.spi.alterna.CsvUtil;
 
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.List;
 
 /**
  * Every business day this cronjob generates csv file with
@@ -21,11 +23,15 @@ public class CsvSentCron
     AlternaSFTPService sftp  = (AlternaSFTPService) x.get("alternaSftp");
     Calendar           today = Calendar.getInstance();
 
+    DAO nSpecDao = (DAO)x.get("nSpecDAO");
+    String currentMode = ((Mode) ((AppConfig) ((NSpec) nSpecDao.find("appConfig")).getService()).getMode()).getLabel();
+    if ( ! currentMode.equals("Production") ) return;
+
     // Don't run on Saturday
-    if ( today.get(Calendar.DAY_OF_WEEK) == 7 ) return;
+    if ( today.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) return;
 
     // Don't run on Sunday
-    if ( today.get(Calendar.DAY_OF_WEEK) == 1 ) return;
+    if ( today.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) return;
 
     // Don't run on holidays
     if ( CsvUtil.cadHolidays.contains(today.get(Calendar.DAY_OF_YEAR)) ) return;
