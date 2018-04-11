77b9f5946a24db5a37445f72a1280874e3fbda68
Minsun <minsun@nanopay.net>
2018-04-11 11:12:53 -0400

    added messageId

--- nanopay/src/net/nanopay/tx/model/Transaction.js
+++ nanopay/src/net/nanopay/tx/model/Transaction.js
@@ -226,6 +226,10 @@ foam.CLASS({
       name: 'notes',
       visibility: foam.u2.Visibility.RO,
       documentation: 'Transaction notes'
+    },
+    {
+      class: 'String',
+      name: 'messageId'
     }
   ],
 
