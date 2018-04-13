14bbe541c8b2e18556455a8e4458385e03fd01d5
Minsun <minsun@nanopay.net>
2018-04-13 14:33:17 -0400

    changed the flow to create user and transaction based on PacsMsg

--- nanopay/src/net/nanopay/iso20022/refinements.js
+++ nanopay/src/net/nanopay/iso20022/refinements.js
@@ -121,8 +121,16 @@ foam.CLASS({
     'net.nanopay.tx.TransactionDAO',
     'net.nanopay.tx.model.TransactionStatus',
     'net.nanopay.tx.model.Transaction',
+    'net.nanopay.cico.model.TransactionType',
     'java.util.Date',
-    'foam.dao.DAO'
+    'foam.dao.DAO',
+    'foam.nanos.auth.User',
+    'net.nanopay.model.Account',
+    'net.nanopay.model.BankAccount',
+    'foam.nanos.auth.Address',
+    'foam.nanos.auth.Phone',
+    'java.util.Random',
+    'static foam.mlang.MLang.EQ'
   ],
 
   methods: [
@@ -146,15 +154,176 @@ foam.CLASS({
           pacs00200109.getFIToFIPmtStsRpt().setOrgnlGrpInfAndSts(new OriginalGroupHeader13[length_]);
           pacs00200109.getFIToFIPmtStsRpt().setGrpHdr(grpHdr53);
 
-          DAO txnDAO = (DAO) getX().get("transactionDAO");
+          DAO userDAO = (DAO) getX().get("userDAO");
+          DAO bankAccountDAO = (DAO) getX().get("bankAccountDAO");
+          String addrLine = "";
+          long senderId =  0 ;
+          long receiverId = 0;
 
-          for ( int i = 0 ; i < length_ ; i++ ) {
-            Transaction txn = (Transaction) txnDAO.find(Long.parseLong((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getPmtId().getTxId()));
-            String strStatus = "";
+           Random rand = new Random();
+           for ( int i = 0 ; i < length_ ; i++ ) {
 
-            if ( txn != null ) {
-              strStatus = ( (TransactionStatus) txn.getStatus() ).getLabel();
+             try {
+               //DAO senderDAO = (DAO) userDAO.find(EQ(User.EMAIL, (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getCtctDtls().getEmailAdr()));
+
+               // Create a Sender
+               User sender = new User();
+
+               //if ( senderDAO == null ) {
+                 senderId = rand.nextInt(1000) + 10000;
+                 sender.setId(senderId);
+                 sender.setEmail((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getCtctDtls().getEmailAdr());
+
+                 Phone senderPhone = new Phone();
+                 senderPhone.setVerified(true);
+                 senderPhone.setNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getCtctDtls().getPhneNb());
+
+                 sender.setPhone(senderPhone);
+
+                 sender.setEmailVerified(true);
+                 sender.setFirstName((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getNm());
+                 sender.setBirthday((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getId().getPrvtId().getDtAndPlcOfBirth().getBirthDt());
+                 sender.setGroup("system");
+                 sender.setSpid("iterac");
+                 sender.setBusinessTypeId(0);
+                 sender.setBusinessSectorId(1);
+                 //sender.setStatus("Active");
+                 sender.setOnboarded(true);
+
+                 Address senderAddress = new Address();
+                 addrLine = "";
+
+                 if ( (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getStrtNm() != null |
+                        ((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getStrtNm()).equals("") ) {  //structured
+                          senderAddress.setStructured(true);
+                          senderAddress.setStreetName((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getStrtNm());
+                          senderAddress.setSuite((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getBldgNb());
+                 } else {
+                   for ( int j = 0; j < (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getAdrLine().length; j++ ) {
+                     addrLine += (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getAdrLine()[j] + " ";
+                   }
+
+                   senderAddress.setAddress1(addrLine);
+                 }
+                 senderAddress.setCity((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getTwnNm());
+                 senderAddress.setCountryId((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getCtry());
+                 senderAddress.setRegionId((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getCtrySubDvsn());
+                 senderAddress.setPostalCode((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getPstlAdr().getPstCd());
+
+                 sender.setAddress(senderAddress);
+
+                 userDAO.put(sender);
+
+                // Create a Sender's BankAccount
+                BankAccount senderBankAcct = new BankAccount();
+                senderBankAcct.setId(senderId);
+                senderBankAcct.setX(getX());
+                senderBankAcct.setAccountNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtrAcct().getId().getOthr().getId());
+                senderBankAcct.setCurrencyCode((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getInstdAmt().getCcy());
+                senderBankAcct.setAccountName("Default");
+                // senderBankAcct.setInstitutionNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtrAgt().);
+                // senderBankAcct.setTransitNumber();
+                senderBankAcct.setStatus("Verified");
+                senderBankAcct.setVerificationAttempts(1);
+                senderBankAcct.setSetAsDefault(true);
+                senderBankAcct.setOwner(senderId);
+
+                bankAccountDAO.put(senderBankAcct);
+              //} else {
+              //   sender = (User) senderDAO.find(EQ(User.EMAIL, (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtr().getCtctDtls().getEmailAdr()));
+              //   senderId = sender.getId();
+              // }
+
+              //DAO receiverDAO = (DAO) userDAO.where(EQ(User.EMAIL, (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getCtctDtls().getEmailAdr()));
+
+              // Create a Receiver
+              User receiver = new User();
+
+              //if ( receiver == null) {
+                receiver = new User();
+                receiverId = rand.nextInt(1000) + 10000;
+                receiver.setId(receiverId);
+                receiver.setEmail((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getCtctDtls().getEmailAdr());
+
+                Phone receiverPhone = new Phone();
+                receiverPhone.setVerified(true);
+                receiverPhone.setNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getCtctDtls().getPhneNb());
+
+                receiver.setPhone(receiverPhone);
+
+                receiver.setEmailVerified(true);
+                receiver.setFirstName((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getNm());
+                receiver.setBirthday((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getId().getPrvtId().getDtAndPlcOfBirth().getBirthDt());
+                receiver.setGroup("system");
+                receiver.setSpid("iterac");
+                receiver.setBusinessTypeId(0);
+                receiver.setBusinessSectorId(1);
+                //receiver.setStatus("Active");
+                receiver.setOnboarded(true);
+
+                Address receiverAddress = new Address();
+                addrLine = "";
+
+                if ( (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getStrtNm() != null |
+                       ((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getStrtNm()).equals("") ) {  //structured
+                         receiverAddress.setStructured(true);
+                         receiverAddress.setStreetName((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getStrtNm());
+                         receiverAddress.setSuite((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getBldgNb());
+                } else {
+                  for ( int j = 0; j < (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getAdrLine().length; j++ ) {
+                    addrLine += (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getAdrLine()[j] + " ";
+                  }
+
+                  receiverAddress.setAddress1(addrLine);
+                }
+                receiverAddress.setCity((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getTwnNm());
+                receiverAddress.setCountryId((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getCtry());
+                receiverAddress.setRegionId((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getCtrySubDvsn());
+                receiverAddress.setPostalCode((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getPstlAdr().getPstCd());
+
+                receiver.setAddress(receiverAddress);
+
+                userDAO.put(receiver);
+
+                // Create a Receiver's BankAccount
+                BankAccount receiverBankAcct = new BankAccount();
+                receiverBankAcct.setId(receiverId);
+                receiverBankAcct.setX(getX());
+                receiverBankAcct.setAccountNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtrAcct().getId().getOthr().getId());
+                receiverBankAcct.setCurrencyCode((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getIntrBkSttlmAmt().getCcy());
+                receiverBankAcct.setAccountName("Default");
+                // receiverBankAcct.setInstitutionNumber((this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getDbtrAgt().);
+                // receiverBankAcct.setTransitNumber();
+                receiverBankAcct.setStatus("Verified");
+                receiverBankAcct.setVerificationAttempts(1);
+                receiverBankAcct.setSetAsDefault(true);
+                receiverBankAcct.setOwner(receiverId);
+
+                bankAccountDAO.put(receiverBankAcct);
+            //} else {
+            //   receiver = (User) receiverDAO.find(EQ(User.EMAIL, (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getCdtr().getCtctDtls().getEmailAdr()));
+            //   receiverId = receiver.getId();
+            // }
+
+              //Create a Transaction
+              double txAmt = (this.getFIToFICstmrCdtTrf().getCdtTrfTxInf())[i].getInstdAmt().getXmlValue();  //Long.parseLong(Double.toString(txAmt))
+
+              Transaction transaction = new Transaction.Builder(getX())
+                .setStatus(TransactionStatus.ACSP)
+                .setPayerId(senderId)
+                .setPayeeId(receiverId)
+                .setAmount(1100)
+                .setType(TransactionType.NONE)
+                .setBankAccountId(receiverId)
+                .setMessageId(this.getFIToFICstmrCdtTrf().getGrpHdr().getMsgId())
+                .build();
+                DAO txnDAO = (DAO) getX().get("transactionDAO");
+                txnDAO.put(transaction);
+            } catch (Throwable t) {
+              t.printStackTrace();
             }
+
+            String strStatus = "ACSP";
 
             PaymentTransaction91 paymentTransaction91 = new PaymentTransaction91();
 
@@ -176,7 +345,7 @@ foam.CLASS({
             orgnlGrpInfAndSts.setGrpSts(strStatus);
 
             pacs00200109.getFIToFIPmtStsRpt().getOrgnlGrpInfAndSts()[i] = orgnlGrpInfAndSts;
-          }
+           }
 
           return pacs00200109;
           `
@@ -191,7 +360,9 @@ foam.CLASS({
 
   javaImports: [
     'net.nanopay.tx.TransactionDAO',
+    'net.nanopay.tx.model.TransactionStatus',
     'net.nanopay.tx.model.Transaction',
+    'net.nanopay.cico.model.TransactionType',
     'java.util.Date',
     'foam.dao.DAO'
   ],
@@ -225,7 +396,8 @@ foam.CLASS({
               String strStatus = "";
 
               if ( txn != null ) {
-                strStatus = txn.getStatus();
+                //strStatus = txn.getStatus();
+                strStatus = ( (TransactionStatus) txn.getStatus() ).getLabel();
               }
 
               orgnlGrpInfAndSts.setOrgnlMsgId((this.getFIToFIPmtStsReq().getOrgnlGrpInf())[i].getOrgnlMsgId());
