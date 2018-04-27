e933c25f9cdc9650fab570623170b2db96b2c037
Kevin Greer <kgrgreer@gmail.com>
2018-04-27 17:05:18 -0400

    Formatting.

--- nanopay/src/net/nanopay/flinks/view/form/FlinksBankPadAuthorization.js
+++ nanopay/src/net/nanopay/flinks/view/form/FlinksBankPadAuthorization.js
@@ -2,41 +2,43 @@ foam.CLASS({
   package: 'net.nanopay.flinks.view.form',
   name: 'FlinksBankPadAuthorization',
   extends: 'net.nanopay.cico.ui.bankAccount.form.BankPadAuthorization',
-  css: 
-  `
-  ^ .net-nanopay-ui-ActionView-nextButton {
-    margin-left: 264px;
-    box-sizing: border-box;
-    background-color: #59a5d5;
-    outline: none;
-    border:none;
-    width: 136px;
-    height: 40px;
-    border-radius: 2px;
-    font-size: 12px;
-    font-weight: lighter;
-    letter-spacing: 0.2px;
-    color: #FFFFFF;
-  }
 
-  ^ .net-nanopay-ui-ActionView-closeButton:hover:enabled {
-    cursor: pointer;
-  }
+  css: `
+    ^ .net-nanopay-ui-ActionView-nextButton {
+      margin-left: 264px;
+      box-sizing: border-box;
+      background-color: #59a5d5;
+      outline: none;
+      border:none;
+      width: 136px;
+      height: 40px;
+      border-radius: 2px;
+      font-size: 12px;
+      font-weight: lighter;
+      letter-spacing: 0.2px;
+      color: #FFFFFF;
+    }
 
-  ^ .net-nanopay-ui-ActionView-closeButton {
-    float: left;
-    margin: 0;
-    outline: none;
-    min-width: 136px;
-    height: 40px;
-    border-radius: 2px;
-    background-color: rgba(164, 179, 184, 0.1);
-    box-shadow: 0 0 1px 0 rgba(9, 54, 73, 0.8);
-    font-size: 12px;
-    font-weight: lighter;
-    letter-spacing: 0.2px;
-    margin-left: 1px;
-  }`,
+    ^ .net-nanopay-ui-ActionView-closeButton:hover:enabled {
+      cursor: pointer;
+    }
+
+    ^ .net-nanopay-ui-ActionView-closeButton {
+      float: left;
+      margin: 0;
+      outline: none;
+      min-width: 136px;
+      height: 40px;
+      border-radius: 2px;
+      background-color: rgba(164, 179, 184, 0.1);
+      box-shadow: 0 0 1px 0 rgba(9, 54, 73, 0.8);
+      font-size: 12px;
+      font-weight: lighter;
+      letter-spacing: 0.2px;
+      margin-left: 1px;
+    }
+  `,
+
   methods: [
     function initE() {
       this.SUPER();
@@ -49,6 +51,7 @@ foam.CLASS({
       .end()
     }
   ],
+
   actions: [
     {
       name: 'nextButton',
@@ -64,8 +67,6 @@ foam.CLASS({
         //console.log('close the form');
         X.form.goBack();
       }
-    },
+    }
   ]
-
-
 });
