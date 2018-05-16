a63933b2e766b4447e2506e5bc94be0fd644c06f
xuerongCode <xuerong.work@gmail.com>
2018-05-16 11:26:02 -0400

    add documentation

--- nanopay/src/net/nanopay/cico/model/PaymentPlatformUserReference.js
+++ nanopay/src/net/nanopay/cico/model/PaymentPlatformUserReference.js
@@ -1,6 +1,10 @@
 foam.CLASS({
   package: 'net.nanopay.cico.model',
   name: 'PaymentPlatformUserReference',
+  documentation: `The model use to store user reference that record in a specified payment platform.
+                  We do not store payment card info in our system, so we need to store payment card info in the payment platform that user will use.
+                  Then, we need to let payment platform to create a user and store the cards.
+                  The user reference use to specify user in the payment platform`,
 
   properties: [
     {
