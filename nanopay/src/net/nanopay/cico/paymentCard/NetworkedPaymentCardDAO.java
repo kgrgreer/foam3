94f8c50fb9e16c10b01791d47b6c4eb14ed1d6e0
Kenny Qi Yen Kan <kanqiyen@gmail.com>
2018-05-16 16:37:27 -0400

    Moved Regex creation to class constant. Performance

--- nanopay/src/net/nanopay/cico/paymentCard/NetworkedPaymentCardDAO.java
+++ nanopay/src/net/nanopay/cico/paymentCard/NetworkedPaymentCardDAO.java
@@ -21,6 +21,14 @@ import static foam.mlang.MLang.EQ;
 public class NetworkedPaymentCardDAO
     extends ProxyDAO
 {
+
+  final Pattern REGEX_VISA       = Pattern.compile("^4[0-9]{12}(?:[0-9]{3})?$");
+  final Pattern REGEX_MASTERCARD = Pattern.compile("^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$");
+  final Pattern REGEX_DISCOVER   = Pattern.compile("^6(?:011|5[0-9]{2})[0-9]{12}$");
+  final Pattern REGEX_AMEX       = Pattern.compile("^3[47][0-9]{13}$");
+  final Pattern REGEX_DINERSCLUB = Pattern.compile("^3(?:0[0-5]|[68][0-9])[0-9]{11}");
+  final Pattern REGEX_JCB        = Pattern.compile("^(?:2131|1800|35\\d{3})\\d{11}$");
+
   public NetworkedPaymentCardDAO(X x, DAO delegate) {
     setX(x);
     setDelegate(delegate);
@@ -50,32 +58,26 @@ public class NetworkedPaymentCardDAO
   }
 
   private boolean isVisa(String number) {
-    Pattern regex = Pattern.compile("^4[0-9]{12}(?:[0-9]{3})?$");
-    return regex.matcher(number).matches();
+    return REGEX_VISA.matcher(number).matches();
   }
 
   private boolean isMasterCard(String number) {
-    Pattern regex = Pattern.compile("^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$");
-    return regex.matcher(number).matches();
+    return REGEX_MASTERCARD.matcher(number).matches();
   }
 
   private boolean isDiscover(String number) {
-    Pattern regex = Pattern.compile("^6(?:011|5[0-9]{2})[0-9]{12}$");
-    return regex.matcher(number).matches();
+    return REGEX_DISCOVER.matcher(number).matches();
   }
 
   private boolean isAmex(String number) {
-    Pattern regex = Pattern.compile("^3[47][0-9]{13}$");
-    return regex.matcher(number).matches();
+    return REGEX_AMEX.matcher(number).matches();
   }
 
   private boolean isDinersClub(String number) {
-    Pattern regex = Pattern.compile("^3(?:0[0-5]|[68][0-9])[0-9]{11}");
-    return regex.matcher(number).matches();
+    return REGEX_DINERSCLUB.matcher(number).matches();
   }
 
   private boolean isJCB(String number) {
-    Pattern regex = Pattern.compile("^(?:2131|1800|35\\d{3})\\d{11}$");
-    return regex.matcher(number).matches();
+    return REGEX_JCB.matcher(number).matches();
   }
 }
