84e9b7df4e734378200558cd25f954518cb45acf
Adam Van Ymeren <adam@vany.ca>
2018-05-18 10:25:59 -0400

    Build java code in place, exclude unnecessary foam2 classes (Jetty deps for example)
    gen.sh updated to not copy over all source classes, only generate code.
    
    pom.xml updated to include all java source files from where they reside normally, jetty dependencies removed and associated source files are exlcuded.
    
    This should improve compilation error messages as they will point to the actual source file rather than a copy of it when its not a generated file.

--- gen.sh
+++ gen.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # This file
-# - cleans build folder for all projects under NANOPAY
+# - cleans build
 # - generates the java code for all models
-# - move all java code to the root directory build folder
 
 # Remove the build folder if it exist and create a new one
 clean() {
-  if [ -d "$build/" ]; then
+  if [ -d "build/" ]; then
     rm -rf build
     mkdir build
   fi
@@ -25,19 +24,4 @@ node tools/xsd/index.js net.nanopay.iso20022 \
 
 node tools/xsd/index.js net.nanopay.fx.ascendantfx.model AFXLinkCustom.svc.xsd
 
-# For each project, grabs java code
-for d in *; do
-  if [ "$d" = 'foam2' ] || [ "$d" = 'interac' ] || [ "$d" = 'merchant' ] || [ "$d" = 'nanopay' ]; then
-    cd $d
-    clean
-
-    cd src/
-    find . -name '*.java' | cpio -pdm ../../build/
-
-    cd ../../
-  fi
-done
-
-#Generate java files from models and move to build folderses 
-cwd=$(pwd)
-node foam2/tools/genjava.js $cwd/tools/classes.js $cwd/build $cwd
+node foam2/tools/genjava.js tools/classes.js build $PWD
