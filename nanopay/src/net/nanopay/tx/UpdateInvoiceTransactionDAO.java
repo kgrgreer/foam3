62ff8688b0c249a2673ed9946ab4bc3e960ee378
Troy Wolters <troywolters@MacBook-Pro-7.local>
2018-05-22 12:41:20 -0400

    Prevent storage of transactions for already paid invoices

--- nanopay/src/net/nanopay/tx/UpdateInvoiceTransactionDAO.java
+++ nanopay/src/net/nanopay/tx/UpdateInvoiceTransactionDAO.java
@@ -31,7 +31,12 @@ public class UpdateInvoiceTransactionDAO
         if ( invoice == null ) {
           throw new RuntimeException("Invoice not found");
         }
-
+        if (
+          invoice.paymentStatus == PaymentStatus.PAID ||
+          invoice.paymentStatus == PaymentStatus.NANOPAY
+        ) {
+          throw new RuntimeException("Invoice already paid");
+        }
         invoice.setPaymentId(transaction.getId());
         invoice.setPaymentDate(transaction.getDate());
         invoice.setPaymentMethod(PaymentStatus.CHEQUE);
