a03fbacf797dfa4008eb75d8c83b67a4fcc929ce
Troy Wolters <troywolters@MacBook-Pro-7.local>
2018-05-22 13:05:01 -0400

    Add check for NANOPAY payment status

--- nanopay/src/net/nanopay/invoice/model/Invoice.js
+++ nanopay/src/net/nanopay/invoice/model/Invoice.js
@@ -193,7 +193,8 @@ foam.CLASS({
       expression: function(draft, paymentId, dueDate, paymentDate, paymentMethod) {
         if ( draft ) return 'Draft';
         if ( paymentMethod.name == 'VOID' ) return 'Void';
-        if ( paymentMethod.name == "CHEQUE" ) return "Paid";
+        if ( paymentMethod.name == 'CHEQUE' ) return 'Paid';
+        if ( paymentMethod.name == 'NANOPAY' ) return 'Paid';
         if ( paymentId === this.DISPUTED_INVOICE ) return 'Disputed';
         if ( paymentId > 0 || paymentDate < Date.now() && paymentId == this.RECORDED_PAYMENT) return 'Paid';
         if ( paymentDate > Date.now() && paymentId == 0 || paymentDate > Date.now() && paymentId == this.RECORDED_PAYMENT) return ('Scheduled');
@@ -201,13 +202,13 @@ foam.CLASS({
           if ( dueDate.getTime() < Date.now() ) return 'Overdue';
           if ( dueDate.getTime() < Date.now() + 24*3600*7*1000 ) return 'Due';
         }
-        if ( paymentMethod.name == 'CHEQUE') return "Paid";
         return 'Due';
       },
       javaGetter: `
         if ( getDraft() ) return "Draft";
         if ( getPaymentMethod().toString() == "VOID" ) return "Void";
         if ( getPaymentMethod().toString() == "CHEQUE" ) return "Paid";
+        if ( getPaymentMethod().toString() == "NANOPAY" ) return "Paid";
         if ( getPaymentId() == -1 ) return "Disputed";
         if ( getPaymentId() > 0 ) return "Paid";
         if ( getPaymentDate() != null ){
