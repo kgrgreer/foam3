1b86b6cbc6b2b45789634325a65a87ec5c920747
Joel Hughes <joel@nanopay.net>
2018-05-25 11:48:18 -0400

    Remove accidental commit with .bck

--- tools/journal_migration/BankAccountStatus.sh
+++ tools/journal_migration/BankAccountStatus.sh
@@ -3,5 +3,5 @@
 # Migrate BankAccount.status Strings to BankAccountStatus enums
 
 if [ -f "$JOURNAL_HOME/bankAccounts" ]; then
-    perl -p -i'.bck' -e 's/status\":\"Unverified\"/status\":0/; s/status\":\"Verified\"/status\":1/; s/status:\":\"Disabled\"/status\":2/g;' "$JOURNAL_HOME"/bankAccounts
+    perl -p -i -e 's/status\":\"Unverified\"/status\":0/; s/status\":\"Verified\"/status\":1/; s/status:\":\"Disabled\"/status\":2/g;' "$JOURNAL_HOME"/bankAccounts
 fi
