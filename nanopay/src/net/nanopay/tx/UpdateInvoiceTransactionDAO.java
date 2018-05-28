e93542aeecac8a6afd8f22e9829bd8c0631735c2
Amy Meng <amymeng5277@gmail.com>
2018-05-28 14:24:14 -0400

    fixed bugs for invoice not related to transaction

--- nanopay/src/net/nanopay/tx/UpdateInvoiceTransactionDAO.java
+++ nanopay/src/net/nanopay/tx/UpdateInvoiceTransactionDAO.java
@@ -7,10 +7,10 @@ import foam.dao.ProxyDAO;
 import net.nanopay.invoice.model.Invoice;
 import net.nanopay.invoice.model.PaymentStatus;
 import net.nanopay.tx.model.Transaction;
+import net.nanopay.tx.model.TransactionStatus;
 
 public class UpdateInvoiceTransactionDAO
-  extends ProxyDAO
-{
+    extends ProxyDAO {
   protected DAO invoiceDAO_;
 
   public UpdateInvoiceTransactionDAO(X x, DAO delegate) {
@@ -21,24 +21,42 @@ public class UpdateInvoiceTransactionDAO
 
   @Override
   public FObject put_(X x, FObject obj) {
-    FObject     ret         = super.put_(x, obj);
     Transaction transaction = (Transaction) obj;
+    Invoice invoice = (Invoice) invoiceDAO_.find(transaction.getInvoiceId());
+
+    if ( transaction.getInvoiceId() != 0 ) {
+      if ( invoice == null ) {
+        throw new RuntimeException("Invoice not found");
+      }
+      if ( "Paid".equals(invoice.getStatus()) && transaction.getStatus() != TransactionStatus.DECLINED ) {
+        throw new RuntimeException("Invoice already paid");
+      }
+    }
+    FObject ret = super.put_(x, obj);
+
 
     // find invoice
     if ( transaction.getPayerId() != transaction.getPayeeId() ) {
-      if ( transaction.getInvoiceId() != 0 ) {
-        Invoice invoice = (Invoice) invoiceDAO_.find(transaction.getInvoiceId());
-        if ( invoice == null ) {
-          throw new RuntimeException("Invoice not found");
-        }
-        if ( "Paid".equals(invoice.getStatus()) ) {
-          throw new RuntimeException("Invoice already paid");
-        }
+
+      if ( transaction.getStatus() == TransactionStatus.COMPLETED ) {
         invoice.setPaymentId(transaction.getId());
         invoice.setPaymentDate(transaction.getDate());
         invoice.setPaymentMethod(PaymentStatus.CHEQUE);
         invoiceDAO_.put(invoice);
       }
+      if ( transaction.getStatus() == TransactionStatus.PENDING ) {
+        invoice.setPaymentId(transaction.getId());
+        invoice.setPaymentDate(transaction.getDate());
+        invoice.setPaymentMethod(PaymentStatus.PENDING);
+        invoiceDAO_.put(invoice);
+      }
+//      if(transaction.getStatus() == TransactionStatus.DECLINED){
+//        invoice.setPaymentId(transaction.getId());
+//        invoice.setPaymentDate(transaction.getDate());
+//        invoice.setPaymentMethod(PaymentStatus.NONE);
+//        invoice.setStatus("Unpaid");
+//        invoiceDAO_.put(invoice);
+//      }
     }
 
     return ret;
