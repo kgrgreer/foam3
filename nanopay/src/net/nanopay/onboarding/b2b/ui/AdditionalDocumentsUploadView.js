9a9a9bcd3369b83b941e052a7b4c1c90b9806189
waseemchamaaa <waseemchamaa6@gmail.com>
2018-05-28 11:44:10 -0400

    removed ability to delete already uploaded documents

--- nanopay/src/net/nanopay/onboarding/b2b/ui/AdditionalDocumentsUploadView.js
+++ nanopay/src/net/nanopay/onboarding/b2b/ui/AdditionalDocumentsUploadView.js
@@ -116,6 +116,7 @@ foam.CLASS({
               class: 'net.nanopay.invoice.ui.InvoiceFileView',
               data: documents[i],
               fileNumber: i + 1,
+              removeHidden:true
             });
           }
           return e;
