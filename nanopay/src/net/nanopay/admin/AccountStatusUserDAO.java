4f17bf91f3c41c71ff69761209c8615da900fbdf
Kirk Eaton <kirk@nanopay.net>
2018-05-28 09:07:58 -0400

    Replaced preSet function with DAO decorator

---
+++ nanopay/src/net/nanopay/admin/AccountStatusUserDAO.java
@@ -0,0 +1,43 @@
+package net.nanopay.admin;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.util.SafetyUtil;
+import foam.core.PropertyInfo;
+import foam.nanos.auth.User;
+import foam.nanos.logger.Logger;
+
+/**
+ * This DAO decorator updates the previousStatus property on the user model
+ * to be equal to the user's old status whenever the status property is updated
+ */
+public class AccountStatusUserDAO
+  extends ProxyDAO
+{
+  protected Logger logger_;
+
+  public AccountStatusUserDAO(X x, DAO delegate) {
+    super(x, delegate);
+    logger_ = (Logger) x.get("logger");
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    try {
+      User newUser = (User) obj;
+      User oldUser = (User) getDelegate().find(newUser.getId());
+      PropertyInfo prop = (PropertyInfo) User.getOwnClassInfo().getAxiomByName("status");
+
+      // if new status and old status are different then set previous status
+      if (SafetyUtil.compare(prop.get(newUser), prop.get(oldUser)) != 0) {
+        newUser.setPreviousStatus(oldUser.getStatus());
+      }
+    } catch (Throwable t) {
+      logger_.error("Error updated previous status", t);
+    }
+
+    return super.put_(x, obj);
+  }
+}
