8c0e5e8b0021ed9e4f385a950eb822cfdbe19014
Adam Van Ymeren <adam@vany.ca>
2018-05-29 12:55:21 -0400

    Make standalone nanos run with embedded jetty.
    Few changes in this commit:
    
    1) Take the removal of the http service out of nanopay/src/services, instead we inject it manually into services when build.sh is run in tomcat mode.
    
    2) Update pom-standalone.xml to copy all dependency jars to lib/ directory and set the classpath and main class on the resulting jar.
    
    3) Update pom-standalone.xml to not use maven-assembly plugin as it is unbearably slow.  Copying dependencies and using classpath is much faster and simpler in some regards.
    
    4) Update build.sh script to match the new setup of the standalone nanos jar.

--- build.sh
+++ build.sh
@@ -152,6 +152,11 @@ function deploy_journals {
         fi
     done < $JOURNALS
 
+    # If not running nanos standalone, disable built in http server.
+    if [ "$RUN_NANOS" -eq 0 ]; then
+        echo 'r({"class":"foam.nanos.boot.NSpec", "name":"http"})' >> "$JOURNAL_HOME/services.0"
+    fi
+
     # one-time copy of runtime journals from /opt/tomcat/bin to /mnt/journals
     if [ "$CATALINA_HOME" == "/opt/tomcat" ]; then
         COUNT="$(ls -l $JOURNAL_HOME | grep -v '.0' | wc -l | sed 's/ //g')"
@@ -257,10 +262,11 @@ function start_nanos {
     mvn clean
     ./find.sh "$NANOPAY_HOME" "$JOURNAL_OUT"
     ./gen.sh
-    mvn install
-    mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
+
+    mvn -f pom-standalone.xml install
     deploy_journals
-    exec java $JAVA_OPTS -cp `cat cp.txt`:`realpath target/*.jar | paste -sd ":" -` foam.nanos.boot.Boot
+
+    exec java $JAVA_OPTS -jar target/root-0.0.1.jar
 }
 
 function testcatalina {
