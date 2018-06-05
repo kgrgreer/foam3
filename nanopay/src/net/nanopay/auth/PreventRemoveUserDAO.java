d2597a82e1f07625d63dda7f0fee36d48ed0c06b
penzital <penzital888@gmail.com>
2018-06-05 11:56:45 -0400

    prevent user deletion if one has invoices or txn

---
+++ nanopay/src/net/nanopay/auth/PreventRemoveUserDAO.java
@@ -0,0 +1,54 @@
+package net.nanopay.auth;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.mlang.sink.Count;
+import foam.nanos.auth.User;
+import net.nanopay.invoice.model.Invoice;
+import net.nanopay.tx.model.Transaction;
+
+import static foam.mlang.MLang.EQ;
+import static foam.mlang.MLang.OR;
+
+public class PreventRemoveUserDAO
+  extends ProxyDAO {
+
+  public PreventRemoveUserDAO(X x, DAO delegate) {
+    setX(x);
+    setDelegate(delegate);
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    User result    = (User) super.put_(x, obj);
+    return result;
+  }
+
+  @Override
+  public FObject remove_(X x, FObject obj) {
+    User user = (User) obj;
+
+    Count count = new Count();
+    long total;
+    DAO transactionDAO = (DAO) x.get("transactionDAO");
+    DAO invoiceDAO = (DAO) x.get("invoiceDAO");
+
+    total = ((Count) transactionDAO.where(OR(
+        EQ(Transaction.PAYER_ID, user.getId()),
+        EQ(Transaction.PAYEE_ID, user.getId())
+    )).limit(1).select(count)).getValue();
+
+    total += ((Count) invoiceDAO.where(OR(
+      EQ(Invoice.PAYEE_ID, user.getId()),
+      EQ(Invoice.PAYER_ID, user.getId())
+    )).limit(1).select(count)).getValue();
+
+    if ( total > 0 ) {
+      user.setEnabled(false);
+      return super.put_(x, obj);
+    }
+    return super.remove_(x, obj);
+  }
+}
