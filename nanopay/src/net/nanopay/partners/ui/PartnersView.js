ecb97265f7b6d41536612784fd4287fc434eaba0
Henry Han <henry.han@usask.ca>
2018-06-12 12:34:18 -0400

    Refactoring parters selection query

--- nanopay/src/net/nanopay/partners/ui/PartnersView.js
+++ nanopay/src/net/nanopay/partners/ui/PartnersView.js
@@ -260,18 +260,15 @@ foam.CLASS({
       isFramed: true,
       code: function() {
         var self = this;
-        this.userDAO.where(
-          this.AND(this.NEQ(this.User.ID, this.user.id),
-            this.EQ(this.User.GROUP, 'business'))
-        ).select(this.COUNT()).then(function(p) {
-          self.partnerCount = 'Showing ' + p.value.toString() + ' Businesses';
-        });
-
         var partnersData = this.userDAO.where(
           this.AND(this.NEQ(this.User.ID, this.user.id),
             this.EQ(this.User.GROUP, 'business'))
         );
         this.createContactCardView(partnersData);
+
+        partnersData.select(this.COUNT()).then(function(p) {
+          self.partnerCount = 'Showing ' + p.value.toString() + ' Businesses';
+        });
       }
     },
     {
@@ -295,18 +292,15 @@ foam.CLASS({
       isFramed: true,
       code: function() {
         var self = this;
-        this.userDAO.where(
-          this.AND(this.NEQ(this.User.ID, this.user.id),
-            this.EQ(this.User.GROUP, 'business'))
-        ).select(this.COUNT()).then(function(p) {
-          self.partnerCount = 'Showing ' + p.value.toString() + ' Businesses';
-        });
-
         var partnersData = this.userDAO.where(
           this.AND(this.NEQ(this.User.ID, this.user.id),
             this.EQ(this.User.GROUP, 'business'))
         );
         this.createContactCardView(partnersData);
+
+        partnersData.select(this.COUNT()).then(function(p) {
+          self.partnerCount = 'Showing ' + p.value.toString() + ' Businesses';
+        });
         this.connected_button_select = false;
         this.contacts_button_select = true;
       }
