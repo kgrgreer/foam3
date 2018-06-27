728971f35b9afbcb6bd3144277522e9aa6f01f42
deejes <devansh@nanopay.net>
2018-06-27 14:53:22 -0400

    add pseudocode for replay function

--- nanopay/src/net/nanopay/security/SigningJournal.js
+++ nanopay/src/net/nanopay/security/SigningJournal.js
@@ -9,6 +9,41 @@ foam.CLASS({
       name: 'algorithm',
       documentation: 'Signing algorithm',
       value: 'SHA256withRSA'
-    }
+    },
+    {
+      name: 'replay',
+      javaCode: `
+        /**
+         * objectId
+         * User
+         * algorithm
+         * signature
+         * value of the object
+         * 
+         * 
+         * Goal => We get an object, and we want to make sure its signature is what we would expect
+         * 
+         * 
+         * Psuedocode =>
+         * 
+         * check that all the values exist and that the user is valid and has a key
+         * then
+         * try {
+         * if ( algorithm.sign(value, user.privateKey) != signature) {
+         *  throw exception
+         * }
+         * }
+         * catch {
+         *  logger.log(errorMessage)
+         * }
+         * finally{
+         *  successfully read the entry....
+         * }
+         * 
+         * 
+        */
+       
+      `
+    },
   ]
 });
