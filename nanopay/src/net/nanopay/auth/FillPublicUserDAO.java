1f401ebc86edb57cde6880bca1e969b7984794f2
Patrick <zahnno@gmail.com>
2018-06-28 20:58:07 -0400

    public user DAO

---
+++ nanopay/src/net/nanopay/auth/FillPublicUserDAO.java
@@ -0,0 +1,35 @@
+package net.nanopay.auth;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.dao.Sink;
+import foam.mlang.order.Comparator;
+import foam.mlang.predicate.Predicate;
+import foam.nanos.auth.User;
+import net.nanopay.auth.PublicUserInfo;
+
+public class FillPublicUserDAO
+  extends ProxyDAO
+{
+  protected DAO userDAO_;
+  protected DAO publicUserDAO_;
+
+  public FillPublicUserDAO(
+      X x,
+      DAO delegate
+  ) {
+    super(x, delegate);
+    userDAO_ = (DAO) x.get("localUserDAO");
+    publicUserDAO_ = (DAO) x.get("publicUserDAO");
+  }
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    User result    = (User) super.put_(x, obj);
+    PublicUserInfo entity = new PublicUserInfo(result);
+    publicUserDAO_.put(entity);
+    return super.put_(x, obj);
+  }
+}
