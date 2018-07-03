a99f65a28c1bb9abbed4c68478ce7efb2f7028fb
Troy Wolters <troy@nanopay.net>
2018-07-03 16:20:27 -0400

    Refactor DisputeModal

--- nanopay/src/net/nanopay/invoice/ui/modal/DisputeModal.js
+++ nanopay/src/net/nanopay/invoice/ui/modal/DisputeModal.js
@@ -33,6 +33,12 @@ foam.CLASS({
       name: 'note',
       view: 'foam.u2.tag.TextArea',
       value: ''
+    },
+    {
+      name: 'companyName',
+      expression: function(type) {
+        return type ? this.invoice.payeeName : this.invoice.payerName;
+      }
     }
   ],
 
@@ -44,41 +50,42 @@ foam.CLASS({
     }
   `,
 
+  messages: [
+    {
+      name: 'VoidSuccess',
+      message: 'Invoice voided.'
+    }
+  ],
+
   methods: [
     function initE() {
       this.SUPER();
 
       this
-      .tag(this.ModalHeader.create({
-        title: 'Void'
-      }))
-      .addClass(this.myClass())
-        .start()
-          .start().addClass('key-value-container')
-            .start()
-              .start().addClass('key').add('Company').end()
-              .start().addClass('value')
-                .add(
-                  this.type ? this.invoice.payeeName :
-                      this.invoice.payerName
-                )
+        .tag(this.ModalHeader.create({ title: 'Void' }))
+        .addClass(this.myClass())
+          .start()
+            .start().addClass('key-value-container')
+              .start()
+                .start().addClass('key').add('Company').end()
+                .start().addClass('value')
+                  .add(this.companyName)
+                .end()
+              .end()
+              .start()
+                .start().addClass('key').add('Amount').end()
+                .start().addClass('value')
+                  .add(this.invoice.targetCurrency)
+                  .add(' ')
+                  .add((this.invoice.amount/100).toFixed(2))
+                  .end()
               .end()
             .end()
-            .start()
-              .start().addClass('key').add('Amount').end()
-              .start().addClass('value')
-                .add(
-                  this.invoice.targetCurrency, ' ',
-                      (this.invoice.amount/100).toFixed(2)
-                )
-                .end()
-            .end()
+            .start().addClass('label').add('Note').end()
+            .start(this.NOTE).addClass('input-box').end()
+            .start(this.VOIDED).addClass('blue-button').addClass('btn').end()
           .end()
-          .start().addClass('label').add('Note').end()
-          .start(this.NOTE).addClass('input-box').end()
-          .start(this.VOIDED).addClass('blue-button').addClass('btn').end()
-        .end()
-      .end();
+        .end();
     }
   ],
 
@@ -90,9 +97,12 @@ foam.CLASS({
         this.invoice.paymentMethod = this.PaymentStatus.VOID;
         this.invoice.note = X.data.note;
         this.invoiceDAO.put(this.invoice);
-        ctrl.add(this.NotificationMessage.create({ message: 'Invoice voided.', type: '' }));
+        ctrl.add(this.NotificationMessage.create({
+          message: this.VoidSuccess,
+          type: ''
+        }));
         X.closeDialog();
       }
     }
   ]
-})
+});
