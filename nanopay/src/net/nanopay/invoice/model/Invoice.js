b9c797ddf7cfe3fef783705c626bcdb44f9926ca
Troy Wolters <troy@nanopay.net>
2018-07-04 09:43:10 -0400

    Add missing periods to documentation

--- nanopay/src/net/nanopay/invoice/model/Invoice.js
+++ nanopay/src/net/nanopay/invoice/model/Invoice.js
@@ -107,14 +107,14 @@ foam.CLASS({
       class: 'FObjectProperty',
       of: 'net.nanopay.auth.PublicUserInfo',
       name: 'payee',
-      documentation: `The party receiving the payment`,
+      documentation: `The party receiving the payment.`,
       storageTransient: true
     },
     {
       class: 'FObjectProperty',
       of: 'net.nanopay.auth.PublicUserInfo',
       name: 'payer',
-      documentation: `The party making the payment`,
+      documentation: `The party making the payment.`,
       storageTransient: true
     },
     {
