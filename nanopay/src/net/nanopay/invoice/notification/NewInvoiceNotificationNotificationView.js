84adc37841d78f08938bcc5c67eee53096849e44
Henry Han <henry.han@usask.ca>
2018-07-04 12:16:15 -0400

    Fix invoice notification grammar issue

--- nanopay/src/net/nanopay/invoice/notification/NewInvoiceNotificationNotificationView.js
+++ nanopay/src/net/nanopay/invoice/notification/NewInvoiceNotificationNotificationView.js
@@ -39,7 +39,7 @@ foam.CLASS({
       this.addClass(this.myClass())
       .start().addClass('msg')
         .add(this.name$)
-        .add(` just send you a ${i.invoiceType.toLowerCase()} invoice of $${this.addCommas((i.amount/100).toFixed(2))}.`)
+        .add(` just sent you a ${i.invoiceType.toLowerCase()} invoice of $${this.addCommas((i.amount/100).toFixed(2))}.`)
       .end()
       .start(this.LINK).end();
     }
