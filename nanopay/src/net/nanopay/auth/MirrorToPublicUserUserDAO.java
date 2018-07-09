f4295be7bb64862bd0e01f36dfe28d743e8a5760
Patrick <zahnno@gmail.com>
2018-07-09 16:12:59 -0400

    renamed fillpublic decorator/added remove override to userDAO to remove publicusers

--- nanopay/src/net/nanopay/auth/MirrorToPublicUserUserDAO.java
+++ nanopay/src/net/nanopay/auth/MirrorToPublicUserUserDAO.java
@@ -2,30 +2,37 @@ package net.nanopay.auth;
 
 import foam.core.FObject;
 import foam.core.X;
-import foam.dao.DAO;
-import foam.dao.ProxyDAO;
-import foam.dao.Sink;
+import foam.dao.*;
 import foam.mlang.order.Comparator;
 import foam.mlang.predicate.Predicate;
 import foam.nanos.auth.User;
 import net.nanopay.auth.PublicUserInfo;
+import java.util.List;
 
 /*
   Decorator on localUserDAO that updates or creates new public users & places them
   within the publicUserDAO. Public users information reflect user info.
 */
 
-public class FillPublicUserDAO
+public class MirrorToPublicUserUserDAO
   extends ProxyDAO
 {
   protected DAO publicUserDAO_;
 
-  public FillPublicUserDAO(
+  public MirrorToPublicUserUserDAO(
       X x,
       DAO delegate
   ) {
     super(x, delegate);
-    publicUserDAO_ = (DAO) x.get("localPublicUserDAO");
+    publicUserDAO_ = (DAO) x.get("localPublicUserDAO"); 
+    // ArraySink sink = (ArraySink) super.select(new ArraySink());
+    // List users = sink.getArray();
+
+    // for ( int i = 0 ; i < users.size() ; i++ ) {
+    //   User user = (User) users.get(i);
+    //   PublicUserInfo publicUser = new PublicUserInfo(user);
+    //   publicUserDAO_.put(publicUser);
+    // }
   }
 
   @Override
@@ -36,5 +43,13 @@ public class FillPublicUserDAO
     PublicUserInfo entity = new PublicUserInfo(result);
     publicUserDAO_.put(entity);
     return result;
+  }
+
+  @Override
+  public FObject remove_(X x, FObject obj) {
+    PublicUserInfo publicUser = (PublicUserInfo) publicUserDAO_.find(obj);
+
+    publicUserDAO_.remove(publicUser);
+    return super.remove_(x, obj);
   }
 }
