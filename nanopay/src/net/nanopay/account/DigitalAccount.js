99e0eb1ba34c26e0f6d8cf4946c3492359d08733
Joel Hughes <joel@nanopay.net>
2018-07-09 15:55:31 -0400

    Add DigitalAccount

---
+++ nanopay/src/net/nanopay/account/DigitalAccount.js
@@ -0,0 +1,7 @@
+foam.CLASS({
+  package: 'net.nanopay.account',
+  name: 'DigitalAccount',
+  extends: 'net.nanopay.account.Account',
+
+  documentation: 'Digital Account. NOTE: name open to change.'
+});
