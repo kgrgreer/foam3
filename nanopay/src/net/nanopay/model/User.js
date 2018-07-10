0bfa4c6a2983ba53855b872a4ce6bf1757294423
Patrick <zahnno@gmail.com>
2018-07-10 16:52:11 -0400

    documentation for user model

--- nanopay/src/net/nanopay/model/User.js
+++ nanopay/src/net/nanopay/model/User.js
@@ -21,22 +21,20 @@ foam.CLASS({
       targetDAOKey: 'businessSectorDAO',
       name: 'businessSectorId',
       of: 'net.nanopay.model.BusinessSector',
-      documentation: 'General economic grouping for business'
-    },
-    {
-      class: 'String',
-      name: 'clearingId',
-      label: 'Clearing ID'
+      documentation: 'General economic grouping for business.'
     },
     {
       class: 'Boolean',
       name: 'invited',
-      value: false
+      value: false,
+      documentation: 'Determines if user was invited to the' +
+          ' platform, dictates invitation email.',
     },
     {
       class: 'Reference',
       of: 'foam.nanos.auth.User',
-      name: 'invitedBy'
+      name: 'invitedBy',
+      documentation: 'Reference to inviter.'
     },
     {
       class: 'foam.core.Enum',
@@ -48,7 +46,9 @@ foam.CLASS({
       class: 'foam.core.Enum',
       of: 'net.nanopay.admin.model.AccountStatus',
       name: 'status',
-      tableCellFormatter: function (status) {
+      documentation: 'User status (pending, active, disabled).' +
+          ' Dictates portal accessibility.',
+      tableCellFormatter: function(status) {
         var bgColour = '#a4b3b8';
         var borderColour = '#a4b3b8';
         var textColour = '#ffffff';
@@ -72,7 +72,7 @@ foam.CLASS({
               'padding': '3px 10px 3px 10px',
               'display': 'inline-block'
             })
-          .end()
+          .end();
         }
       }
     },
@@ -80,7 +80,8 @@ foam.CLASS({
       class: 'foam.core.Enum',
       of: 'net.nanopay.admin.model.ComplianceStatus',
       name: 'compliance',
-      tableCellFormatter: function (status) {
+      documentation: 'Admin user account approval status',
+      tableCellFormatter: function(status) {
         return status.label;
       }
     },
@@ -94,17 +95,21 @@ foam.CLASS({
       class: 'foam.nanos.fs.FileArray',
       name: 'additionalDocuments',
       documentation: 'Additional documents for compliance verification',
-      view: { class: 'net.nanopay.onboarding.b2b.ui.AdditionalDocumentsUploadView' }
+      view: {
+        class: 'net.nanopay.onboarding.b2b.ui.AdditionalDocumentsUploadView'
+      }
     },
     {
       class: 'FObjectArray',
       of: 'foam.nanos.auth.User',
-      name: 'principalOwners'
+      name: 'principalOwners',
+      documentation: 'Owners of business.'
     },
     {
       class: 'String',
       name: 'jobTitle',
       label: 'Job Title',
+      documentation: 'Job title of user.',
       validateObj: function(jobTitle) {
         var re = /^[a-zA-Z0-9 ]{1,35}$/;
         if ( jobTitle.length > 0 && ! re.test(jobTitle) ) {
@@ -115,33 +120,37 @@ foam.CLASS({
     {
       class: 'String',
       name: 'principleType',
-      label: 'Principal Type'
+      label: 'Principal Type',
+      documentation: 'Type of principle owner (shareholder, owner etc...)'
     },
     {
       class: 'Boolean',
       name: 'portalAdminCreated',
+      documentation: 'Determines whether user was created by an admin user.',
       value: false,
     },
-    {
-      class: 'Boolean',
-      name: 'welcomeEmailSent',
-      value: false,
-    },
-
     // NOTE: The following is subject to change and is not finalized.
     {
       class: 'FObjectProperty',
       of: 'foam.nanos.auth.Phone',
       name: 'businessPhone',
-      factory: function () { return this.Phone.create(); },
+      factory: function() {
+        return this.Phone.create();
+      },
       view: { class: 'foam.nanos.auth.PhoneDetailView' }
     },
     {
       class: 'String',
       name: 'businessIdentificationNumber',
       transient: true,
-      getter: function() { return this.businessRegistrationNumber; },
-      setter: function(x) { this.businessRegistrationNumber = x; },
+      documentation: 'A number issued by an Issuing Authority such' +
+          ' a the CRA. Used as an identified as well as for tax purposes.',
+      getter: function() {
+        return this.businessRegistrationNumber;
+      },
+      setter: function(x) {
+        this.businessRegistrationNumber = x;
+      },
       javaGetter: `return getBusinessRegistrationNumber();`,
       javaSetter: `setBusinessRegistrationNumber(val);`
     },
@@ -149,11 +158,14 @@ foam.CLASS({
       class: 'String',
       name: 'businessRegistrationNumber',
       width: 35,
-      documentation: 'Business Identification Number (BIN)',
-      validateObj: function (businessRegistrationNumber) {
+      documentation: 'Business Identification Number (BIN) ' +
+          'that identifies your business to federal, ' +
+          'provincial, and municipal governments.',
+      validateObj: function(businessRegistrationNumber) {
         var re = /^[a-zA-Z0-9 ]{1,35}$/;
-        if (  businessRegistrationNumber.length > 0 && ! re.test(businessRegistrationNumber) ) {
-          return 'Invalid registration number.'
+        if ( businessRegistrationNumber.length > 0 &&
+              ! re.test(businessRegistrationNumber) ) {
+          return 'Invalid registration number.';
         }
       }
     },
@@ -161,31 +173,44 @@ foam.CLASS({
       class: 'String',
       name: 'issuingAuthority',
       transient: true,
-      getter: function() { return this.businessRegistrationAuthority; },
-      setter: function(x) { this.businessRegistrationAuthority = x; },
+      documentation: 'An organization that has the' +
+          ' power to issue an official document.',
+      getter: function() {
+        return this.businessRegistrationAuthority;
+      },
+      setter: function(x) {
+        this.businessRegistrationAuthority = x;
+      },
       javaGetter: `return getBusinessRegistrationAuthority();`,
       javaSetter: `setBusinessRegistrationAuthority(val);`
     },
     {
       class: 'String',
       name: 'businessRegistrationAuthority',
+      documentation: 'An organization that has the' +
+          ' power to issue & process a business registration.',
       width: 35,
-      validateObj: function (businessRegistrationAuthority) {
+      validateObj: function(businessRegistrationAuthority) {
         var re = /^[a-zA-Z0-9 ]{1,35}$/;
-        if ( businessRegistrationAuthority.length > 0 && ! re.test(businessRegistrationAuthority) ) {
+        if ( businessRegistrationAuthority.length > 0 &&
+            ! re.test(businessRegistrationAuthority) ) {
           return 'Invalid issuing authority.';
         }
       }
     },
     {
       class: 'Date',
-      name: 'businessRegistrationDate'
+      name: 'businessRegistrationDate',
+      documentation: 'Date the business was registered ' +
+          'by their issuing authority.'
     },
     {
       class: 'FObjectProperty',
       of: 'foam.nanos.auth.Address',
       name: 'businessAddress',
-      factory: function () { return this.Address.create(); },
+      factory: function() {
+        return this.Address.create();
+      },
       view: { class: 'foam.nanos.auth.AddressDetailView' }
     },
     {
@@ -196,13 +221,17 @@ foam.CLASS({
     {
       class: 'Boolean',
       name: 'onboarded',
+      documentation: 'Signifies completion of business ' +
+          'registration. Dictates portal views after' +
+          'compliance & account approval.',
       value: false
     },
     {
       class: 'Boolean',
       name: 'createdPwd',
       value: false,
-      documentation: 'determines whether user is using his own unique password or one that was system generated.'
+      documentation: 'determines whether user is using' +
+          ' his own unique password or one that was system generated.'
     },
     {
       class: 'Int',
