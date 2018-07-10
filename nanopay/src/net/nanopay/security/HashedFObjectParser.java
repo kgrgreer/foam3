e02da05b54ebc9c385413db8eaf01d7efdc62f0b
Kirk Eaton <kirk@nanopay.net>
2018-07-10 14:51:11 -0400

    Added HashedFObjectParser and HashedJSONParser and MessageDigest class

---
+++ nanopay/src/net/nanopay/security/HashedFObjectParser.java
@@ -0,0 +1,64 @@
+package net.nanopay.security;
+
+import foam.lib.json.FObjectParser;
+import foam.lib.json.ObjectNullParser;
+import foam.lib.parse.*;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+
+public class HashedFObjectParser
+  extends ObjectNullParser
+{
+  public HashedFObjectParser() {
+    this(null);
+  }
+
+  public HashedFObjectParser(final Class defaultClass) {
+    super(
+      new Parser() {
+        @Override
+        public PStream parse(PStream ps, ParserContext x) {
+          // parse FObject
+          PStream ps1 = ps.apply(new FObjectParser(defaultClass), x);
+          if ( ps1 == null || ps1.value() == null ) {
+            throw new RuntimeException("Digest verification failed");
+          }
+
+          // get journal entry as a string
+          String message = ps.substring(ps1);
+
+          // parse message digest
+          PStream ps2 = ps1.apply(new Seq1(1,
+            new Optional(new Literal(",")),
+            new FObjectParser(net.nanopay.security.MessageDigest.class)
+          ), x);
+          if ( ps2 == null ) {
+            return ps.setValue(ps1.value());
+          }
+
+          // get message digest value
+          net.nanopay.security.MessageDigest messageDigest =
+            (net.nanopay.security.MessageDigest) ps2.value();
+
+          // calculate digest based on JSON message
+          java.security.MessageDigest md = null;
+          try {
+            md = java.security.MessageDigest.getInstance(messageDigest.getAlgorithm());
+            md.update(message.getBytes(StandardCharsets.UTF_8));
+          } catch ( Throwable t ) {
+            throw new RuntimeException("Digest verification failed");
+          }
+
+          // check if calculated digest matches stored digest
+          String digest = Hex.toHexString(md.digest());
+          if ( ! digest.equals(messageDigest.getDigest()) ) {
+            throw new RuntimeException("Digest verification failed");
+          }
+
+          return ps.setValue(ps1.value());
+        }
+      }
+    );
+  }
+}
---
+++ nanopay/src/net/nanopay/security/HashedJSONParser.java
@@ -0,0 +1,27 @@
+package net.nanopay.security;
+
+import foam.core.FObject;
+import foam.lib.json.ExprParser;
+import foam.lib.json.JSONParser;
+import foam.lib.parse.Parser;
+import foam.lib.parse.ParserContext;
+import foam.lib.parse.ParserContextImpl;
+import foam.lib.parse.StringPStream;
+
+public class HashedJSONParser
+  extends JSONParser
+{
+  protected Parser parser = new HashedFObjectParser();
+
+  @Override
+  public FObject parseString(String data, Class defaultClass) {
+    StringPStream ps = stringps;
+
+    ps.setString(data);
+    ParserContext x = new ParserContextImpl();
+    x.set("X", getX());
+    ps = (StringPStream) ps.apply(defaultClass == null ? parser : new HashedFObjectParser(defaultClass), x);
+
+    return ps == null ? null : (FObject) ps.value();
+  }
+}
