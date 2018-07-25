bfbc6d9d51b063f679da1344ea78097884ecd974
penzital <penzital888@gmail.com>
2018-07-25 11:19:01 -0400

    fix getOwner()

--- nanopay/src/net/nanopay/tx/TransactionEntitiesDAO.java
+++ nanopay/src/net/nanopay/tx/TransactionEntitiesDAO.java
@@ -65,8 +65,8 @@ public class TransactionEntitiesDAO extends ProxyDAO
   {
     FObject clone = obj.fclone();
     Transaction tx = (Transaction) clone;
-    User payer = (User) tx.findSourceAccount(x_).findOwner(getX());
-    User payee = (User) tx.findDestinationAccount(x_).findOwner(getX());
+    User payer = (User) userDAO_.find(tx.findSourceAccount(x_).getOwner());
+    User payee = (User) userDAO_.find( tx.findDestinationAccount(x_).getOwner());
 
     if (payer == null) {
       logger_.error(String.format("Transaction: %d Payer with Id: %d not found", tx.getId(), payer.getId()));
