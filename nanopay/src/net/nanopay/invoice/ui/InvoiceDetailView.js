d86f8a59adf58278b52bc661e3e14154ed850707
Troy Wolters <troy@nanopay.net>
2018-08-02 11:32:39 -0400

    Fix date validation

--- nanopay/src/net/nanopay/invoice/ui/InvoiceDetailView.js
+++ nanopay/src/net/nanopay/invoice/ui/InvoiceDetailView.js
@@ -302,7 +302,7 @@ foam.CLASS({
           return;
         }
 
-        if ( ! ( dueDate instanceof Date ) ) {
+        if ( ! (dueDate instanceof Date && ! isNaN(dueDate.getTime())) ) {
           this.add(foam.u2.dialog.NotificationMessage.create({
             message: 'Please Enter Valid Due Date yyyy-mm-dd.',
             type: 'error'
