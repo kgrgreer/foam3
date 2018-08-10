567acec8d293aad90a41efab3c37be371c881ef0
Devansh Jain <devansh@nanopay.net>
2018-08-10 10:14:41 -0400

    algorithm used test for UserKeyPairGenerationDAO

--- nanopay/src/net/nanopay/security/UserKeyPairGenerationDAOTest.js
+++ nanopay/src/net/nanopay/security/UserKeyPairGenerationDAOTest.js
@@ -2,20 +2,36 @@ foam.CLASS({
   package: 'net.nanopay.security',
   name: 'UserKeyPairGenerationDAOTest',
   extends: 'foam.nanos.test.Test',
+  
+  imports: [
+    'keyStoreManager',
+    'keyPairDAO',
+    'privateKeyDAO',
+    'publicKeyDAO'
+  ],
 
   javaImports: [
     // 'nanopay.security.UserKeyPairGenerationDAO',
     'foam.core.EmptyX',
+    'net.nanopay.security.KeyPairEntry',
+    'net.nanopay.security.PrivateKeyEntry',
+    'net.nanopay.security.PublicKeyEntry',
+    'foam.core.X',
     'foam.dao.DAO',
+    'foam.core.FObject',
     'foam.dao.MDAO',
+    'foam.dao.Sink', 
     'foam.mlang.sink.Count',
     'foam.nanos.auth.User',
     'foam.util.SafetyUtil',
-
+    'static foam.mlang.MLang.EQ',
     'java.io.BufferedReader',
     'java.io.BufferedWriter',
     'java.io.File',
-    'java.util.UUID'
+    'java.util.UUID',
+    'java.security.KeyPair',
+    'java.security.KeyPairGenerator',
+    
   ],
 
   constants: [
@@ -31,13 +47,7 @@ foam.CLASS({
           .setEmail("kirk@nanopay.net")
           .build()
       `
-    },
-    // {
-    //   type: 'String',
-    //   name: 'EXPECTED',
-    //   documentation: 'Expected journal output',
-    //   value: 'p({"class":"foam.nanos.auth.User","id":1000,"firstName":"Kirk","lastName":"Eaton","email":"kirk@nanopay.net"},{"algorithm":"SHA-256","digest":"1f62e5366081be2b9ac3ff75bacec01bad128e64ab758438361b5e11ba90f5d5"})'
-    // }
+    }
   ],
 
   methods: [
@@ -45,7 +55,7 @@ foam.CLASS({
       name: 'runTest',
       javaCode: `
         // construct tests
-        UserKeyPairGenerationDAO_KeysUseProvidedAlgorithm();
+        UserKeyPairGenerationDAO_KeysUseProvidedAlgorithm(x);
         // UserKeyPairGenerationDAO_KeysUseProvidedKeySize();
         // UserKeyPairGenerationDAO_PrivateKeyEncrypted();
         // UserKeyPairGenerationDAO_PublicKeyBase64Encrypted();
@@ -56,39 +66,46 @@ foam.CLASS({
     },
     {
       name: 'UserKeyPairGenerationDAO_KeysUseProvidedAlgorithm',
+      args: [
+        {
+          name: 'x', javaType: 'foam.core.X'
+        }
+      ],
       javaCode: `
-      DAO UserKeyPairDAO = new net.nanopay.security.UserKeyPairGenerationDAO.Builder(getX()).build();
-      System.err.print(UserKeyPairDAO);
-      UserKeyPairDAO.put_(getX(), INPUT);
-      // foam.core.FObject resp = UserKeyPairDAO.put_(getX(), INPUT);
-      test( false, "DJs First test Message!" );
+  // Create the DAOs
+  DAO keyPairDAO = (DAO) new net.nanopay.security.KeyPairDAO.Builder(x).setDelegate(new foam.dao.EasyDAO.Builder(x).setAuthenticate(false).setOf(net.nanopay.security.KeyPairEntry.getOwnClassInfo()).setSeqNo(true).setJournaled(true).setJournalName(\"keyPairs\").build()).build();;
+  DAO UserKeyPairGenerationDAO = (DAO) new net.nanopay.security.UserKeyPairGenerationDAO.Builder(x).setDelegate(new foam.dao.MDAO(foam.nanos.auth.User.getOwnClassInfo())).build();
+  DAO privateKeyDAO = (DAO) new net.nanopay.security.PrivateKeyDAO.Builder(x).setAlias(\"net.nanopay.security.PrivateKeyDAO\").setDelegate(new foam.dao.EasyDAO.Builder(x).setAuthenticate(false).setOf(net.nanopay.security.PrivateKeyEntry.getOwnClassInfo()).setSeqNo(true).setJournaled(true).setJournalName(\"privateKeys\").build()).build();
+  DAO publicKeyDAO = (DAO)  new net.nanopay.security.PublicKeyDAO.Builder(x).setDelegate(new foam.dao.EasyDAO.Builder(x).setAuthenticate(false).setOf(net.nanopay.security.PublicKeyEntry.getOwnClassInfo()).setSeqNo(true).setJournaled(true).setJournalName(\"publicKeys\").build()).build();
 
+  // Put to DAO and find keys generated
+  UserKeyPairGenerationDAO.put_(x, INPUT);
+  KeyPairEntry keypairDaoFind = (KeyPairEntry) keyPairDAO.inX(x).find( EQ(KeyPairEntry.OWNER, INPUT.getId()) );
+  PrivateKeyEntry kprivate = (PrivateKeyEntry) privateKeyDAO.find_(x, keypairDaoFind.privateKeyId_);
+  PublicKeyEntry kpublic = (PublicKeyEntry) publicKeyDAO.find_(x, keypairDaoFind.publicKeyId_);
 
-      /*
-      So we want to test that the key uses the algorithm we provided.
+  // Cast DAO being tested to FObject to access properties
+  FObject i = (FObject) UserKeyPairGenerationDAO;
 
-      So we will first have to construct the DAO,
-          How do we construct the DAO? It doesn't exist in the context, because
-          it is not being made. So maybe we gotta construct a whole instance in here.
-          Okay, lets do that..
-      then put to it,
-      then get the key generated, 
-      then examine the algorithm used to generate that key.
+  // Run tests
+  test( (i.getProperty("algorithm")).equals(kprivate.algorithm_) , "Private key uses algorithm set in UKPGDao" );
+  test( (i.getProperty("algorithm")).equals(kpublic.algorithm_) , "Public key uses algorithm set in UKPGDao" );
 
-      */
+  // Will changing the properties work? Test it...
 
+  /*
+  So we want to test that the key uses the algorithm we provided.
 
+  So we will first have to construct the DAO,
+      How do we construct the DAO? It doesn't exist in the context, because
+      it is not being made. So maybe we gotta construct a whole instance in here.
+      Okay, lets do that..
+  then put to it,
+  then get the key generated, 
+  then examine the algorithm used to generate that key.
 
-      // DAO UserKeyPairDAO = new net.nanopay.security.UserKeyPairGenerationDAO.Builder(getX()).build();
-      // test( UserK)
-        // HashingJournal journal = new HashingJournal.Builder(getX()).build();
-        // test("SHA-256".equals(journal.getAlgorithm()), "Algorithm is set to SHA-256 by default");
-        // test(! journal.getDigestRequired(), "Digest required is set to false by default");
-        // test(! journal.getRollDigests(), "Roll digests is set to false by default");
-        // test(journal.getPreviousDigest() == null, "Previous digest is empty by default");
-        // test(journal.getOutputter() instanceof HashingOutputter, "Outputter is an instance of HashingOutputter");
-        // test(journal.getParser() instanceof HashedJSONParser, "Parser is an instance of HashedJSONParser");
-      `
+  */
+`
     },
 
   ]
