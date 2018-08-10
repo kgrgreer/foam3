7a8ba62c83b43c486d3faeb3ec7bf35bd8b3b380
Kirk Eaton <kirk@nanopay.net>
2018-08-10 14:51:58 -0400

    Finished phone verification token service

--- nanopay/src/net/nanopay/auth/sms/PhoneVerificationTokenService.js
+++ nanopay/src/net/nanopay/auth/sms/PhoneVerificationTokenService.js
@@ -10,26 +10,33 @@ foam.CLASS({
   ],
 
   javaImports: [
-    'com.authy.AuthyApiClient',
-    'com.authy.api.Params',
-    'com.authy.api.PhoneVerification',
-    'com.authy.api.Verification',
+    'com.twilio.Twilio',
+    'com.twilio.rest.api.v2010.account.Message',
+    'com.twilio.type.PhoneNumber',
     'foam.dao.DAO',
-    'foam.nanos.auth.Phone'
+    'foam.mlang.MLang',
+    'foam.nanos.auth.Phone',
+    'foam.nanos.auth.User',
+    'foam.nanos.auth.token.Token',
+    'foam.util.SecurityUtil',
+    'java.util.Calendar'
   ],
 
   properties: [
     {
       class: 'String',
       name: 'accountSid',
+      documentation: 'Twilio account id'
     },
     {
       class: 'String',
-      name: 'authToken'
+      name: 'authToken',
+      documentation: 'Twilio auth token'
     },
     {
       class: 'String',
-      name: 'phoneNumber'
+      name: 'phoneNumber',
+      documentation: 'Twilio phone number'
     }
   ],
 
@@ -43,43 +50,64 @@ foam.CLASS({
     {
       name: 'generateTokenWithParameters',
       javaCode: `
-        AuthyApiClient client = getClient();
+        DAO tokenDAO = (DAO) x.get("tokenDAO");
+
         // don't send token if already verified
         Phone phone = user.getPhone();
-        if ( phone.getVerified() ) {
+        if (phone.getVerified()) {
           throw new RuntimeException("Phone already verified");
         }
 
-        // TODO: Remove hardcoded country code of 1
-        PhoneVerification phoneVerification = client.getPhoneVerification();
-        Verification verification = phoneVerification.start(phone.getNumber(), "1", "sms", new Params());
-        if ( ! Boolean.parseBoolean(verification.getSuccess()) ) {
-          throw new RuntimeException(verification.getMessage());
-        }
-        return true;
+        String data = String.format("%04d",
+          SecurityUtil.GetSecureRandom().nextInt(10000));
+        tokenDAO.put(new Token.Builder(x)
+          .setUserId(user.getId())
+          .setExpiry(generateExpiryDate())
+          .setData(data)
+          .build());
+
+        return Message.creator(new PhoneNumber(phone.getNumber()), new PhoneNumber(getPhoneNumber()),
+          "Your MintChip phone verification pin is: " + data).create() != null;
       `
     },
     {
       name: 'processToken',
       javaCode: `
-        DAO userDAO = (DAO) getX().get("localUserDAO");
-        AuthyApiClient client = getClient();
-        // if already verified, return true
+        DAO userDAO = (DAO) x.get("localUserDAO");
+        DAO tokenDAO = (DAO) x.get("tokenDAO");
+        Calendar calendar = Calendar.getInstance();
+
+        // find token
+        Token result = (Token) tokenDAO.inX(x).find(MLang.AND(
+          MLang.EQ(Token.PROCESSED, false),
+          MLang.GT(Token.EXPIRY, calendar.getTime()),
+          MLang.EQ(Token.DATA, token)));
+
+        if ( result == null ) {
+          throw new RuntimeException("Token not found");
+        }
+
+        // find user from token
+        user = (User) userDAO.find_(x, result.getUserId());
+        if ( user == null ) {
+          throw new RuntimeException("User not found");
+        }
+
         Phone phone = user.getPhone();
         if ( phone.getVerified() ) {
           throw new RuntimeException("Phone already verified");
         }
 
-        // TODO: Remove hardcoded country code of 1
-        PhoneVerification phoneVerification = client.getPhoneVerification();
-        Verification verification = phoneVerification.check(phone.getNumber(), "1", token);
-        if ( ! verification.isOk() ) {
-          throw new RuntimeException("Error validating code");
-        }
-
+        // update phone to verified
+        user = (User) user.fclone();
         phone.setVerified(true);
         user.setPhone(phone);
-        userDAO.put(user);
+        userDAO.put_(x, user);
+
+        // update token
+        result = (Token) result.fclone();
+        result.setProcessed(true);
+        tokenDAO.put_(x, result);
         return true;
       `
     }
