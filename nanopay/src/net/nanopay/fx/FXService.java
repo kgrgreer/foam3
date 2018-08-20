eabe27783f41459066c9a88c359aa2f0603238c7
Greenspot <mayowa.olurin@gmail.com>
2018-08-20 11:31:09 +0100

    Created INBankAccount

--- nanopay/src/net/nanopay/fx/FXService.java
+++ nanopay/src/net/nanopay/fx/FXService.java
@@ -1,10 +1,7 @@
 package net.nanopay.account;
 
 import foam.core.ContextAwareSupport;
-import foam.core.Detachable;
-import foam.dao.AbstractSink;
 import foam.dao.DAO;
-import foam.mlang.MLang;
 import foam.nanos.NanoService;
 import java.util.Date;
 import net.nanopay.fx.AcceptFXRate;
@@ -55,19 +52,10 @@ public class FXService
     public FXAccepted acceptFXRate(AcceptFXRate request) throws RuntimeException {
         FXAccepted fxAccepted = this.fxAdapter.acceptFXRate(request);
         if ( null != fxAccepted ) {
-            final FXQuote quote = new FXQuote();
-            fxQuoteDAO_.where(
-                    MLang.EQ(FXQuote.ID, fxAccepted.getId())
-            ).select(new AbstractSink() {
-                @Override
-                public void put(Object obj, Detachable sub) {
-                    quote.setExpiryTime(((FXQuote) obj).getExpiryTime());
-                    quote.setQuoteDateTime(((FXQuote) obj).getQuoteDateTime());
-                    quote.setId(((FXQuote) obj).getId());
-                    quote.setStatus(ExchangeRateStatus.ACCEPTED.getName());
+            // Get FXQuote by ID and update status
+            FXQuote quote = (FXQuote) fxQuoteDAO_.find(request.getId());
+            quote.setStatus(ExchangeRateStatus.ACCEPTED.getName());
 
-                }
-            });
             fxQuoteDAO_.put_(x_, quote);
 
         }
