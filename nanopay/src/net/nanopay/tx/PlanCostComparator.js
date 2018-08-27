2245b23ad3085310c4fffdcdc2cb841ecf126ec3
Greenspot <mayowa.olurin@gmail.com>
2018-08-27 11:48:30 +0100

    Created PlanComparator to select best transaction Plan

---
+++ nanopay/src/net/nanopay/tx/PlanCostComparator.js
@@ -0,0 +1,34 @@
+foam.CLASS({
+  package: 'net.nanopay.tx',
+  name: 'PlanCostComparator',
+
+  documentation: ``,
+
+  implements: ['net.nanopay.tx.PlanComparator'],
+
+  methods: [
+    {
+      name: 'compare',
+      javaReturns: 'int',
+      args: [
+        {
+          name: 'o1',
+          javaType: 'Object'
+        },
+        {
+          name: 'o2',
+          javaType: 'Object'
+        }
+      ],
+      javaCode: `
+        if ( o1 instanceof PlanTransaction &&  o2 instanceof PlanTransaction ) {
+          PlanTransaction plan1 = (PlanTransaction) o1;
+          PlanTransaction plan2 = (PlanTransaction) o2;
+          return ((Long)plan1.getCost()).compareTo((Long)plan2.getCost());
+        }
+        return 0;
+`
+    },
+  ]
+
+});
