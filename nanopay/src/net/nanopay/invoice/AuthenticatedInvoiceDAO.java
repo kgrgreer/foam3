6d8478c2443600d74eec871591cde4bb12a7affd
Nauna <anna.doulatshahi@gmail.com>
2018-08-28 17:56:54 -0400

    merge update

--- nanopay/src/net/nanopay/invoice/AuthenticatedInvoiceDAO.java
+++ nanopay/src/net/nanopay/invoice/AuthenticatedInvoiceDAO.java
@@ -38,18 +38,8 @@ public class AuthenticatedInvoiceDAO extends ProxyDAO {
     if ( invoice == null ) {
       throw new IllegalArgumentException("Cannot put null");
     }
-
-    /* NOTE:  https://github.com/nanoPayinc/NANOPAY/issues/3349 
-     * SUGGESTS: "prevent users who haven't created the initial invoice from making changes" 
-     * THEREFORE: Below is commented out and replaced with following check (lines: 48-51) */
-
-    // Check if the user is related to the invoice or if the user has global access permission.
-    // if ( ! this.isRelated(user, invoice) && ! auth.check(x, GLOBAL_INVOICE_READ) ) {
-    //   throw new AuthorizationException();
-    // }
-
-    // Check if the user is the creator of the invoice.
-    if ( ! this.isCreatedByCurrentUser(user, invoice)) {
+    // Check if the user is the creator of the invoice or if the user has global access permission.
+    if ( ! this.isRelated(user, invoice) && ! auth.check(x, GLOBAL_INVOICE_READ) ) {
       throw new AuthorizationException();
     }
     // Whether the invoice exist or not, utilize put method and dao will handle it.
@@ -61,13 +51,9 @@ public class AuthenticatedInvoiceDAO extends ProxyDAO {
     User user = this.getUser(x);
     Invoice invoice = (Invoice) super.find_(x, id);
 
-    if ( invoice.getDraft() && ( invoice.getCreatedBy() != user.getId() ) ) {
-      throw new AuthorizationException();
-    }
-    if ( invoice != null ) {
-      // Check if user is related to the invoice, or user is admin,
-      // or user has the authentication.
-      if ( ! this.isRelated(user, invoice) && ! auth.check(x, GLOBAL_INVOICE_READ) ) {
+    if ( invoice != null && ! auth.check(x, GLOBAL_INVOICE_READ)) {
+      // Check if user is related to the invoice
+      if ( ! this.isRelated(user, invoice) ) {
         throw new AuthorizationException();
       }
       // limiting draft invoice to those who created the invoice.
@@ -80,26 +66,39 @@ public class AuthenticatedInvoiceDAO extends ProxyDAO {
 
   @Override
   public Sink select_(X x, Sink sink, long skip, long limit, Comparator order, Predicate predicate) {
-    DAO dao = this.getFilteredDAO(x, GLOBAL_INVOICE_READ);
+    User user = this.getUser(x);
+    long id = user.getId();
+    boolean global = auth.check(x, GLOBAL_INVOICE_READ);
+
+    // If user has the global access permission, get all the invoices; otherwise,
+    // only return related invoices and drafts that user created
+    DAO dao = global ? getDelegate() : getDelegate().where(AND(OR(EQ(Invoice.PAYEE_ID, id), EQ(Invoice.PAYER_ID, id))), NOT(AND(EQ(Invoice.DRAFT, true), NEQ(Invoice.CREATED_BY, id))));
     return dao.select_(x, sink, skip, limit, order, predicate);
   }
 
   @Override
   public FObject remove_(X x, FObject obj) {
-    this.getUser(x);
-    if ( ! auth.check(x, GLOBAL_INVOICE_DELETE) ) {
+    User user = this.getUser(x);
+    Invoice invoice = (Invoice) obj;
+
+    if ( ! (auth.check(x, GLOBAL_INVOICE_DELETE) || user.getId() == invoice.getCreatedBy()) ) {
       throw new AuthorizationException();
+    }
+
+    if ( ! invoice.getDraft() ) {
+      throw new AuthorizationException("Only invoice drafts can be deleted.");
     }
     return getDelegate().remove_(x, obj);
   }
 
   @Override
   public void removeAll_(X x, long skip, long limit, Comparator order, Predicate predicate) {
-    this.getUser(x);
-    if ( ! auth.check(x, GLOBAL_INVOICE_DELETE) ) {
-      throw new AuthorizationException();
-    }
-    getDelegate().removeAll_(x, skip, limit, order, predicate);
+    User user = this.getUser(x);
+    boolean global = auth.check(x, GLOBAL_INVOICE_DELETE);
+
+    DAO dao = global ? getDelegate().where(EQ(Invoice.DRAFT, true))
+        : getDelegate().where(AND(EQ(Invoice.CREATED_BY, user.getId()), EQ(Invoice.DRAFT, true)));
+    dao.removeAll_(x, skip, limit, order, predicate);
   }
 
   protected User getUser(X x) {
@@ -110,34 +109,11 @@ public class AuthenticatedInvoiceDAO extends ProxyDAO {
     return user;
   }
 
-  protected DAO getFilteredDAO(X x, String permission) {
-    User user = this.getUser(x);
-    long id = user.getId();
-    boolean global = auth.check(x, permission);
-
-    // First filtering DAO to exclude any invoice drafts that were not created by current user
-    DAO tempDAO = getDelegate().where(NOT(AND(EQ(Invoice.DRAFT, true), NEQ(Invoice.CREATED_BY, id))));
-
-    // Next: If user has the global access permission, get all the invoices; otherwise,
-    // only return related invoices.
-    DAO dao = global ? tempDAO : tempDAO.
-        where(AND(OR(EQ(Invoice.PAYEE_ID, id), EQ(Invoice.PAYER_ID, id))));
-    return dao;
-  }
-
   // If the user is payee or payer of the invoice.
   protected boolean isRelated(User user, Invoice invoice) {
     long id = user.getId();
     boolean isPayee = (long) invoice.getPayeeId() == id;
     boolean isPayer = (long) invoice.getPayerId() == id;
     return  isPayee || isPayer;
-  }
-
-  // If the user is creator of the invoice.
-  protected boolean isCreatedByCurrentUser(User user, Invoice invoice) {
-    long id = user.getId();
-    long creator = invoice.getCreatedBy();
-    System.out.println("Creator = " + creator + "current user = " + id);
-    return  id == creator;
   }
 }
