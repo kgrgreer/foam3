eaf660a8fb76c8a56f8110ffbee2b88a19d66272
penzital <penzital888@gmail.com>
2018-08-28 14:45:35 -0400

    added DigitalTransaction class

--- nanopay/tools/classes.js
+++ nanopay/tools/classes.js
@@ -125,6 +125,7 @@ var classes = [
   'net.nanopay.tx.model.TransactionStatus',
   'net.nanopay.tx.model.TransactionEntity',
   'net.nanopay.tx.model.Transaction',
+  'net.nanopay.tx.model.DigitalTransaction',
   'net.nanopay.tx.CompositeTransaction',
   'net.nanopay.tx.CompositeTransactionDAO',
   'net.nanopay.tx.PlanTransaction',
