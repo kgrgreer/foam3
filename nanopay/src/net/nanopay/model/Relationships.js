e25a801997bb370b770f028be9a3d7dc77f27668
Troy Wolters <troy@nanopay.net>
2018-08-29 12:04:25 -0400

    Simplify partner access

--- nanopay/src/net/nanopay/model/Relationships.js
+++ nanopay/src/net/nanopay/model/Relationships.js
@@ -144,16 +144,15 @@ foam.CLASS({
   refines: 'foam.nanos.auth.UserUserJunction',
   properties: [
     {
-      class: 'FObjectProperty',
-      of: 'net.nanopay.auth.PublicUserInfo',
-      name: 'partnerOneInfo',
-      documentation: 'User associated to partner relationship.',
+      class: 'Long',
+      name: 'partnerId',
+      documentation: 'The id of the partnered user.',
       storageTransient: true
     },
     {
       class: 'FObjectProperty',
       of: 'net.nanopay.auth.PublicUserInfo',
-      name: 'partnerTwoInfo',
+      name: 'partnerInfo',
       documentation: 'User associated to partner relationship.',
       storageTransient: true
     }
