e56a963987724a7ca36bc4f882d70ed259270479
Troy Wolters <troy@nanopay.net>
2018-09-07 08:23:26 -0400

    Use short form method definition in BalanceView

--- nanopay/src/net/nanopay/sme/ui/BalanceView.js
+++ nanopay/src/net/nanopay/sme/ui/BalanceView.js
@@ -32,32 +32,28 @@ foam.CLASS({
         .end();
       this.generateBalanceCards();
     },
-    {
-      name: 'generateBalanceCards',
+    function generateBalanceCards() {
       /**
        * Add balance cards to the view for each currency denomination that the
        * user has accounts in.
-       * @return {void}
        */
-      code: function generateBalanceCards() {
-        return this.user.accounts.select(this.GroupBy.create({
-          arg1: this.Account.DENOMINATION,
-          arg2: this.ArraySink.create()
-        })).then((result) => {
-          result.groupKeys.forEach((denomination) => {
-            var group = result.groups[denomination];
-            Promise
-              .all(group.array.map((account) => account.findBalance(this)))
-              .then((values) => values.reduce((acc, c) => acc + c))
-              .then((balance) => {
-                this.add(this.BalanceCard.create({
-                  balance: balance,
-                  denomination: denomination
-                }));
-              });
-          });
+      return this.user.accounts.select(this.GroupBy.create({
+        arg1: this.Account.DENOMINATION,
+        arg2: this.ArraySink.create()
+      })).then((result) => {
+        result.groupKeys.forEach((denomination) => {
+          var group = result.groups[denomination];
+          Promise
+            .all(group.array.map((account) => account.findBalance(this)))
+            .then((values) => values.reduce((acc, c) => acc + c))
+            .then((balance) => {
+              this.add(this.BalanceCard.create({
+                balance: balance,
+                denomination: denomination
+              }));
+            });
         });
-      }
+      });
     }
   ]
 });
