4a140d0ee309ebb5475a02df7e11034e4c12747e
Devansh Jain <devansh@nanopay.net>
2018-09-07 16:54:52 -0400

    Setup listener and serve page based on request validity

--- nanopay/src/net/nanopay/security/PII/PIIReportDownload.js
+++ nanopay/src/net/nanopay/security/PII/PIIReportDownload.js
@@ -5,9 +5,22 @@ foam.CLASS({
 
   documentation: 'View for downloading Personal Information Report',
 
-  imports: [
-    'window'
+  implements: [
+    'foam.mlang.Expressions'
   ],
+
+  imports: [
+    'window',
+    'viewPIIRequestsDAO',
+    'ctrl',
+    'notificationDAO'
+  ],
+
+
+  requires: [ 
+    'foam.dao.ArraySink',
+    'net.nanopay.security.PII.ViewPIIRequests',
+],
 
   exports: [],
 
@@ -37,32 +50,73 @@ foam.CLASS({
   }
   `,
 
-  properties: [],
+  properties: [
+    {
+      class: 'Boolean',
+      name: 'valid',
+      value: 'false'
+    }
+  ],
 
   methods: [
+
+    function checkPermissionStatus(instance, userID) {
+      vprDAO = this.viewPIIRequestsDAO;
+      // set valid to true if there exists a valid request for the user
+      this.viewPIIRequestsDAO.where(
+        this.AND(
+          this.LT(new Date(), this.ViewPIIRequests.REQUEST_EXPIRES_AT),
+          this.EQ(this.ViewPIIRequests.CREATED_BY, userID))
+        ).select(
+          this.COUNT()).then(
+            function(count) {
+              console.log(count.value)
+              instance.valid = count.value > 0 ? true : false;
+            });
+    },
     function initE() {
       this.SUPER();
+      var self = this;
+      currentUserID = 1348;
+      // set up listener on valid and display a request button or option to download a report
+      this.valid$.sub(function() {
+        if ( self.valid ) {
+          console.log('eah')
+          self.addClass(self.myClass())
+              .start()
+              .start().addClass('light-roboto-h2').add('PII Report Download').end()
+                .start().add(self.DOWNLOAD_JSON).end()
+              .end();
+        } 
+        else {
+          self.addClass(self.myClass())
+            .start()
 
-      valid = true;
-      valid = false;
+          .start('div')
+          .start(self.VIEW_REQUEST).addClass('update-BTN').end()
+        .end()
+        .end();
+        }
+      });
+      this.checkPermissionStatus(self, currentUserID);
 
-      if ( valid ) {
-        this.addClass(this.myClass())
-          .start()
-          .start().addClass('light-roboto-h2').add('PII Report Download').end()
-            .start().add(this.DOWNLOAD_JSON).end()
-          .end();
-      } else {
-        this.addClass(this.myClass())
-          .start()
-          // .start().addClass('light-roboto-h2').add('PII Report Download').end()
-            // .start().add(this.DOWNLOAD_JSON).end()
-          .end();
-      }
+      // TODO replace with current user ID
     }
   ],
 
   actions: [
+    {
+      name: 'viewRequest',
+      label: 'View My PII',
+      code: function(X) {
+        console.log(X);
+        console.log(this.net.nanopay.security.PII.ViewPIIRequests);
+        console.log(self.net.nanopay.security.PII.ViewPIIRequests);
+        vpr = this.net.nanopay.security.PII.ViewPIIRequests.create();
+        X.viewPIIRequestsDAO.put(vpr);
+      }
+    },
+    
     {
       name: 'downloadJSON',
       label: 'Download PII',
@@ -72,6 +126,8 @@ foam.CLASS({
 
         var PIIUrl = self.window.location.origin + "/service/PIIWebAgent";
         self.window.location.assign(PIIUrl);
+        
+
       }
     }
   ]
