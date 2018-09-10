8ef69363b32d84e5278809855462cd32c99ebc67
Kirk Eaton <kirk@nanopay.net>
2018-09-10 11:30:36 -0400

    Added mappings for businessProcessCatalogue

--- nanopay/src/net/nanopay/iso20022/ISO20022MappingGenerator.java
+++ nanopay/src/net/nanopay/iso20022/ISO20022MappingGenerator.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import foam.util.SafetyUtil;
 import org.apache.commons.text.StringEscapeUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -22,29 +23,96 @@ import java.util.stream.IntStream;
 
 public class ISO20022MappingGenerator {
 
-  public static void main(String[] args) throws Exception {
-    File file = new File("tools/xsd/iso20022/20180314_ISO20022_2013_eRepository.iso20022");
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder builder = factory.newDocumentBuilder();
-    Document doc = builder.parse(file);
+  protected static List<Element> getChildElements(NodeList children) {
+    return getChildElements(children, null);
+  }
 
-    // normalize document
-    doc.getDocumentElement().normalize();
+  protected static List<Element> getChildElements(NodeList children, String name) {
+    return IntStream.range(0, children.getLength())
+      .mapToObj(children::item)
+      .filter(node -> node.getNodeType() == Node.ELEMENT_NODE)
+      .filter(node -> SafetyUtil.isEmpty(name) || name.equals(node.getNodeName()))
+      .map(node -> (Element) node)
+      .collect(Collectors.toList());
+  }
 
+  protected static void processBusinessProcessCatalogue(Document doc, StringBuilder sb) {
+    // parse businessProcessCatalogue
+    Node catalogue = doc.getElementsByTagName("businessProcessCatalogue").item(0);
+    NodeList catalogueChildNodes = catalogue.getChildNodes();
+
+    List<Element> entries = getChildElements(catalogueChildNodes);
+    Iterator entriesI = entries.iterator();
+
+    while ( entriesI.hasNext() ) {
+      Element entry = (Element) entriesI.next();
+
+      NodeList entryChildNodes = entry.getChildNodes();
+      List<Element> elements = getChildElements(entryChildNodes, "messageDefinition");
+      if ( elements.size() == 0 ) {
+        continue;
+      }
+
+      Iterator elementsI = elements.iterator();
+      while ( elementsI.hasNext() ) {
+        Element element = (Element) elementsI.next();
+        String elementName = element.getAttribute("name");
+        String elementDefinition = element.getAttribute("definition");
+        String elementXmlTag = element.getAttribute("xmlTag");
+
+        sb.append("\"")
+          .append(StringEscapeUtils.escapeJson(elementName))
+          .append("\":{\"documentation\":\"")
+          .append(StringEscapeUtils.escapeJson(elementDefinition))
+          .append("\",\"properties\":{");
+
+        // parse message building blocks
+        NodeList elementChildNodes = element.getChildNodes();
+        List<Element> messageElements = getChildElements(elementChildNodes, "messageBuildingBlock");
+
+        Iterator messageElementsI = messageElements.iterator();
+        while ( messageElementsI.hasNext() ) {
+          Element messageElement = (Element) messageElementsI.next();
+          String messageElementName = messageElement.getAttribute("name");
+          String messageElementDefinition = messageElement.getAttribute("definition");
+          String messageElementXmlTag = messageElement.getAttribute("xmlTag");
+
+          sb.append("\"")
+            .append(StringEscapeUtils.escapeJson(messageElementXmlTag))
+            .append("\":{")
+            .append("\"name\":\"")
+            .append(StringEscapeUtils.escapeJson(messageElementName))
+            .append("\",")
+            .append("\"documentation\":\"")
+            .append(StringEscapeUtils.escapeJson(messageElementDefinition))
+            .append("\"}");
+
+          if ( messageElementsI.hasNext() ) {
+            sb.append(",");
+          }
+        }
+
+        sb.append("}}");
+        if ( elementsI.hasNext() ) {
+          sb.append(",");
+        }
+      }
+
+      if ( entriesI.hasNext() ) {
+        sb.append(",");
+      }
+    }
+  }
+
+  protected static void processDataDictionary(Document doc, StringBuilder sb) {
+    // parse dataDictionary
     Node dictionary = doc.getElementsByTagName("dataDictionary").item(0);
     NodeList dictionaryChildNodes = dictionary.getChildNodes();
 
-    // get child elements
-    List<Element> entries = IntStream.range(0, dictionaryChildNodes.getLength())
-      .mapToObj(dictionaryChildNodes::item)
-      .filter(node -> node.getNodeType() == Node.ELEMENT_NODE)
-      .map(node -> (Element) node)
-      .collect(Collectors.toList());
-
     // iterate through top level dictionary
-    StringBuilder sb = new StringBuilder().append("{");
-
+    List<Element> entries = getChildElements(dictionaryChildNodes);
     Iterator entriesI = entries.iterator();
+
     while ( entriesI.hasNext() ) {
       Element entry = (Element) entriesI.next();
       String entryName = entry.getAttribute("name");
@@ -57,13 +125,7 @@ public class ISO20022MappingGenerator {
         .append("\",\"properties\":{");
 
       NodeList elementChildNodes = entry.getChildNodes();
-      List<Element> elements = IntStream.range(0, elementChildNodes.getLength())
-        .mapToObj(elementChildNodes::item)
-        .filter(node -> node.getNodeType() == Node.ELEMENT_NODE)
-        .filter(node -> "messageElement".equals(node.getNodeName()))
-        .map(node -> (Element) node)
-        .collect(Collectors.toList());
-
+      List<Element> elements = getChildElements(elementChildNodes, "messageElement");
 
       Iterator elementsI = elements.iterator();
       while ( elementsI.hasNext() ) {
@@ -93,6 +155,20 @@ public class ISO20022MappingGenerator {
         sb.append(",");
       }
     }
+  }
+
+  public static void main(String[] args) throws Exception {
+    File file = new File("tools/xsd/iso20022/20180314_ISO20022_2013_eRepository.iso20022");
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    Document doc = builder.parse(file);
+
+    // normalize document
+    doc.getDocumentElement().normalize();
+
+    StringBuilder sb = new StringBuilder().append("{");
+    processBusinessProcessCatalogue(doc, sb);
+    processDataDictionary(doc, sb);
     sb.append("}");
 
 
