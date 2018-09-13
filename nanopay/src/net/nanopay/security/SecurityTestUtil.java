96c3ddfa80a84e8c503efd833f1ca27488af9fc1
Kirk Eaton <kirk@nanopay.net>
2018-09-13 14:57:48 -0400

    Added helper method to create a test context for security related tests

--- nanopay/src/net/nanopay/security/SecurityTestUtil.java
+++ nanopay/src/net/nanopay/security/SecurityTestUtil.java
@@ -1,7 +1,15 @@
 package net.nanopay.security;
 
+import foam.core.X;
+import foam.core.XFactory;
+import foam.dao.DAO;
+
 class SecurityTestUtil {
 
+  /**
+   * Check to see if SoftHSM is installed on the system
+   * @return true if SoftHSM is installed, false otherwise
+   */
   static boolean IsSoftHSMInstalled() {
     try {
       return new ProcessBuilder("softhsm2-util", "--help").start().waitFor() == 0;
@@ -10,6 +18,10 @@ class SecurityTestUtil {
     }
   }
 
+  /**
+   * Resets the SoftHSM token for future usage
+   * @return true if successfully reset, false otherwise
+   */
   static boolean ResetSoftHSM() {
     try {
       // delete existing test token, ignoring errors
@@ -40,5 +52,50 @@ class SecurityTestUtil {
       t.printStackTrace();
       return false;
     }
+  }
+
+  /**
+   * Creates a test context with test key store manager
+   * @param x context to modify
+   * @return updated context
+   */
+  static X CreateSecurityTestContext(X x) {
+    return CreateSecurityTestContext(x, new PKCS12KeyStoreManager.Builder(x)
+      .setKeyStorePath("/tmp/nanopay/keys/keystore.p12")
+      .setPassphrasePath("/tmp/nanopay/keys/passphrase")
+      .build());
+  }
+
+
+  static X CreateSecurityTestContext(X x, KeyStoreManager manager) {
+    x = x.putFactory("keyStoreManager", new XFactory() {
+      @Override
+      public Object create(X x) {
+        try {
+          manager.unlock();
+          return manager;
+        } catch ( Throwable t ) {
+          throw new RuntimeException(t);
+        }
+      }
+    });
+
+    // replace private key dao context with new one
+    DAO privateKeyDAO = (DAO) x.get("privateKeyDAO");
+    ((foam.core.ContextAware) privateKeyDAO).setX(x);
+    x = x.put("privateKeyDAO", privateKeyDAO);
+
+    // replace public key dao context with new one
+    DAO publicKeyDAO = (DAO) x.get("publicKeyDAO");
+    ((foam.core.ContextAware) publicKeyDAO).setX(x);
+    x = x.put("publicKeyDAO", publicKeyDAO);
+
+    // replace key pair dao context with new one
+    DAO keyPairDAO = (DAO) x.get("keyPairDAO");
+    ((foam.core.ContextAware) keyPairDAO).setX(x);
+    x = x.put("keyPairDAO", keyPairDAO);
+
+    // return updated context
+    return x;
   }
 }
