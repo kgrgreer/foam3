73eeb087fb5746bdc4708a53c79d4e5d2dca1eaf
Devansh Jain <devansh@nanopay.net>
2018-09-17 13:56:48 -0400

    Get userID from context, call addTimeToRequest for auditing

--- nanopay/src/net/nanopay/security/PII/PIIWebAgent.java
+++ nanopay/src/net/nanopay/security/PII/PIIWebAgent.java
@@ -7,7 +7,8 @@ import jdk.nashorn.api.scripting.JSObject;
 import net.nanopay.security.PII.PIIReportGenerator;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletOutputStream;
-
+import net.nanopay.security.PII.PIIReportGenerator;
+import foam.nanos.auth.User;
 
 
 public class PIIWebAgent
@@ -18,21 +19,28 @@ public class PIIWebAgent
   public synchronized void execute(X x) {
     try {
       HttpServletResponse response = x.get(HttpServletResponse.class);
-      PIIReportGenerator pi = new PIIReportGenerator();
-      // TODO pass user id as argument
-      String json = (pi.getPIIData(x, 1l)).toString();
+      
+      User user = (User) x.get("user");
+      Long userId = user.getId();
 
+      // Generate PII report
+      PIIReportGenerator reportGenerator = new PIIReportGenerator();
+      String json = (reportGenerator.getPIIData(x, userId)).toString();
+
+      // Trigger download of generated PII report
       response.setContentType("application/json");
       response.setCharacterEncoding("UTF-8");
-      // TODO: improve filename
-      response.setHeader("Content-disposition", "attachment; filename=\"pii\"");
+      response.setHeader("Content-disposition", "attachment; filename=\"PIIData\"");
       ServletOutputStream out = response.getOutputStream();
-      
-     out.write(json.getBytes());
-     out.flush();
+      out.write(json.getBytes());
+      out.flush();
+
+      // Add download time to request
+      reportGenerator.addTimeToPIIRequest(x);
 
     } catch (Throwable t) {
       throw new RuntimeException(t);
     }
+
   }
 }
