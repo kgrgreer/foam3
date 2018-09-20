9f5d85607ec19c74b1e545b4fb7c6994e1691e15
Nauna <anna.doulatshahi@gmail.com>
2018-09-20 14:11:43 -0400

    cronjob cancel on holding account after 30day expire

--- nanopay/src/net/nanopay/tx/cron/ExpirationContactHoldAccCron.java
+++ nanopay/src/net/nanopay/tx/cron/ExpirationContactHoldAccCron.java
@@ -2,16 +2,22 @@ package net.nanopay.tx.cron;
 
 import foam.core.ContextAgent;
 import foam.core.X;
+import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.nanos.app.AppConfig;
 import foam.nanos.app.Mode;
+import foam.core.FObject;
 import net.nanopay.invoice.model.Invoice;
 import net.nanopay.invoice.model.InvoiceStatus;
 import net.nanopay.tx.alterna.AlternaSFTPService;
 import net.nanopay.tx.alterna.CsvUtil;
-import static foam.mlang.MLang.EQ;
-
+import static foam.mlang.MLang.*;
+import java.util.Date;
+import foam.core.*;
+import foam.dao.*;
+import java.util.*;
 import java.util.Calendar;
+import foam.dao.Sink;
 
 /**
  * Every day this cronjob checks if invoice has passed the 30 day
@@ -22,11 +28,22 @@ public class ExpirationContactHoldAccCron
   @Override
   public void execute(X x) {
     DAO invoiceDAO_  = (DAO) x.get("invoiceDAO");
-    Calendar   today = Calendar.getInstance();
+    Calendar endTime = Calendar.getInstance();
+    Calendar today   = Calendar.getInstance();
+    ArraySink filteredInvoice = new ArraySink();
 
-    //ArraySink filteredInvoice = invoiceDAO_.select().where(AND(EQ(Invoice.STATUS, InvoiceStatus.PENDING_ACCEPTANCE), LT(Invoice.PAYMENTDATE + 30days, Date.now()) );
+    today.setTime(new Date());
 
-    //filteredInvoice.
+    invoiceDAO_.where(
+        AND(
+          EQ( Invoice.STATUS, InvoiceStatus.PENDING_ACCEPTANCE), 
+          GTE( Invoice.PAYMENT_DATE, today.getTimeInMillis() - (1000*60*60*24*30))
+            //((endTime.setTime(Invoice.PAYMENT_DATE)).getTimeInMillis() + (1000*60*60*24*30)), today.getTimeInMillis())
+          )).select(filteredInvoice);
 
+    
+    for(Object pendingInvoice : filteredInvoice.getArray()){
+      invoiceDAO_.put((Invoice)pendingInvoice);
+    }
   }
 }
