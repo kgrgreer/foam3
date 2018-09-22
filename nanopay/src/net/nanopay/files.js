26f5de47afddfce0384266df4eb9a47833ca9155
Patrick Zanowski <zahnno@gmail.com>
2018-09-22 17:37:44 -0400

    Added new prevent duplicate email dao, nanopay specific

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -35,6 +35,7 @@ FOAM_FILES([
   { name: 'net/nanopay/ui/wizard/WizardView', flags: ['web'] },
   { name: 'net/nanopay/auth/email/EmailTokenService' },
   { name: 'net/nanopay/auth/ExternalInvoiceTokenService' },
+  { name: 'net/nanopay/auth/PreventDuplicateEmailDAO' },
   { name: 'net/nanopay/auth/sms/PhoneVerificationTokenService', flags: ['web'] },
   { name: 'net/nanopay/auth/ui/SignUpView', flags: ['web'] },
   { name: 'net/nanopay/auth/ui/SignInView', flags: ['web'] },
