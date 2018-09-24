93f217e4f8450df2278bc139cb7eeedae1c54b39
Devansh Jain <devansh@nanopay.net>
2018-09-24 13:23:33 -0400

    Add test for logging of downloadedAt time of PIIReport

--- nanopay/src/net/nanopay/security/PII/ViewPIIRequestsDAOTest.js
+++ nanopay/src/net/nanopay/security/PII/ViewPIIRequestsDAOTest.js
@@ -1,5 +1,5 @@
 foam.CLASS({
-  package: 'net.nanopay.security.PII',
+  package: 'net.nanopay.security.pii',
   name: 'ViewPIIRequestsDAOTest',
   extends: 'foam.nanos.test.Test',
 
@@ -9,20 +9,40 @@ foam.CLASS({
     'foam.dao.DAO',
     'foam.nanos.auth.User',
     'static foam.mlang.MLang.EQ',
+    'java.util.Date',
+    'java.util.Calendar',
+    'java.util.ArrayList',
+    'net.nanopay.security.pii.PIIRequestStatus',
+    'net.nanopay.security.pii.PIIReportGenerator'
+
+  ],
+
+  constants: [
+    {
+      type: 'User',
+      name: 'INPUT',
+      documentation: 'Original input',
+      value: `
+        new User.Builder(EmptyX.instance())
+          .setId(1100)
+          .setFirstName("Rumple")
+          .setLastName("Stiltskin")
+          .setEmail("rumple@stiltskin.au")
+          .build()
+      `
+    }
   ],
 
   methods: [
     {
       name: 'runTest',
       javaCode: `
-        DAO vprDAO = (DAO) x.get("ViewPIIRequestsDAO");
-        x = x.put("viewPIIRequestDAO", vprDAO);
-
-        // run tests
-        ViewPIIRequestsDAO_DAOIsAuthenticated(x);
-        ViewPIIRequestsDAO_ApprovedValidRequestIsFrozenExpectDownloadedAt(x);
-        ViewPIIRequestsDAO_DownloadTimesAreLogged(x);
-        ViewPIIRequestsDAO_EnforcesOnlyValidOneRequestPerUser(x);
+  DAO vprDAO = (DAO) x.get("viewPIIRequestsDAO");
+  // run tests
+  // ViewPIIRequestsDAO_DAOIsAuthenticated(x);
+  ViewPIIRequestsDAO_ApprovedValidRequestIsFrozen(x, vprDAO);
+  ViewPIIRequestsDAO_DownloadTimesAreLogged(x, vprDAO);
+  // ViewPIIRequestsDAO_EnforcesOnlyValidOneRequestPerUser(x);
       `
     },
     {
@@ -37,29 +57,32 @@ foam.CLASS({
   `
     },
     {
-      name: 'ViewPIIRequestsDAO_ApprovedValidRequestIsFrozenExpectDownloadedAt',
+      name: 'ViewPIIRequestsDAO_ApprovedValidRequestIsFrozen',
       args: [
         {
           name: 'x', javaType: 'foam.core.X'
+        },
+        {
+          name: 'vprDAO', javaType: 'foam.dao.DAO'
         }
       ],
       javaCode: `
-      // create a new request and put to dao
-
-      // approve that request
-
-      // try to change the user and approved by and save, assert that they are unchanged
-      
-      // try to change the times and save, assert that they haven't changed
-      
-      
-      // try to change the status and test that it hasn't changed
-
-
-
-
-
-  test( true , "Hello from the viewPIIReqeustsDAO tests!!" );
+  // Create new request, set it to approved and put to DAO
+  ViewPIIRequests piiRequest = new ViewPIIRequests();
+  piiRequest.setId(100);
+  piiRequest.setViewRequestStatus(PIIRequestStatus.APPROVED);
+  vprDAO.put(piiRequest);
+  // Find object from DAO
+  FObject daoRequestObject = vprDAO.find(100);
+  // Modify the object and put it to DAO again
+  piiRequest.setViewRequestStatus(PIIRequestStatus.PENDING);
+  piiRequest.setCreated(new Date());
+  piiRequest.setRequestExpiresAt(new Date());
+  vprDAO.put(piiRequest);
+  // Find the object again
+  FObject modifiedDaoRequestObject = vprDAO.find(100);
+  // Confirm that the dao Object was not modified    
+  test( daoRequestObject.equals(modifiedDaoRequestObject) , "updating an approved request doesnt work" );
   `
     },
     {
@@ -67,24 +90,28 @@ foam.CLASS({
       args: [
         {
           name: 'x', javaType: 'foam.core.X'
-        }
-      ],
-      javaCode: `
-  // run a download and see if the time is logged in the DownloadedAt prop
-
-`
-    },
-    {
-      name: 'ViewPIIRequestsDAO_DownloadTimesAreLogged',
-      args: [
+        },
         {
-          name: 'x', javaType: 'foam.core.X'
+          name: 'vprDAO', javaType: 'foam.dao.DAO'
         }
       ],
       javaCode: `
-  // run a download and see if the time is logged in the DownloadedAt prop
-
-`
+  // Create new request, set it to approved and put to DAO
+  ViewPIIRequests piiRequest = new ViewPIIRequests();
+  piiRequest.setId(100);
+  piiRequest.setViewRequestStatus(PIIRequestStatus.APPROVED);
+  vprDAO.put(piiRequest);
+  // Find object from DAO
+  FObject daoRequestObject = vprDAO.find(100);
+  ArrayList downloadedAt = (ArrayList) daoRequestObject.getProperty("downloadedAt");
+  // Simulate a download and get the object again
+  PIIReportGenerator prg = new PIIReportGenerator();
+  prg.addTimeToPIIRequest(x);
+  FObject modifiedDaoRequestObject = vprDAO.find(100);
+  ArrayList modifiedDownloadedAt = (ArrayList) modifiedDaoRequestObject.getProperty("downloadedAt");
+  // Test that the downloadedAt array is larger than before
+  test( downloadedAt.size() < modifiedDownloadedAt.size() , "downloadedAt is modified when a download is triggered" );
+      `
     },
     {
       name: 'ViewPIIRequestsDAO_EnforcesOnlyValidOneRequestPerUser',
