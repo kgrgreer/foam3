7a3e6554a9830eef2a298680815c910b36c548d1
Kirk Eaton <kirk@nanopay.net>
2018-09-28 13:56:14 -0400

    Modified PIIOutputter to not output class names

--- nanopay/src/net/nanopay/security/pii/PIIOutputter.java
+++ nanopay/src/net/nanopay/security/pii/PIIOutputter.java
@@ -1,8 +1,17 @@
 package net.nanopay.security.pii;
 
+import foam.lib.json.OutputterMode;
+
+import java.io.PrintWriter;
+
 public class PIIOutputter
   extends foam.lib.json.Outputter
 {
+  public PIIOutputter(PrintWriter writer, OutputterMode mode) {
+    super(writer, mode);
+    outputClassNames_ = false;
+  }
+
   @Override 
   protected Boolean maybeOutputProperty(foam.core.FObject fo, foam.core.PropertyInfo prop, boolean includeComma) {
     return prop.containsPII() && super.maybeOutputProperty(fo, prop, includeComma);
