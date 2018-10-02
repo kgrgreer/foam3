62330a148de9138875bf24a8127285a5f28b669c
zaczhang <zaczhang19@gmail.com>
2018-10-02 14:11:26 -0400

    add models for PaymentRequest, PaymentResponse, ReversalRequest and ReversalResponse

---
+++ nanopay/src/net/nanopay/kotak/ReversalRequest.js
@@ -0,0 +1,27 @@
+foam.CLASS({
+  package: 'net.nanopay.kotak',
+  name: 'ReversalRequest',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'reqId',
+    },
+    {
+      class: 'String',
+      name: 'msgSrc'
+    },
+    {
+      class: 'String',
+      name: 'clientCode'
+    },
+    {
+      class: 'String',
+      name: 'datePost'
+    },
+    {
+      class: 'String',
+      name: 'msgId'
+    }
+  ]
+});
