9f78b9f6230143c059f46318f1594bb2dec1e28b
Kirk Eaton <kirk@nanopay.net>
2018-10-04 10:03:24 -0400

    Modelled MerkleTreeReceiptGenerator

--- nanopay/src/net/nanopay/security/receipt/ReceiptGenerator.js
+++ nanopay/src/net/nanopay/security/receipt/ReceiptGenerator.js
@@ -2,6 +2,10 @@ foam.INTERFACE({
   package: 'net.nanopay.security.receipt',
   name: 'ReceiptGenerator',
 
+  implements: [
+    'foam.nanos.NanoService'
+  ],
+
   properties: [
     {
       class: 'FObjectProperty',
@@ -13,13 +17,37 @@ foam.INTERFACE({
   methods: [
     {
       name: 'add',
+      documentation: `
+        Adds an FObject to the underlying receipt generating model.
+        An FObject added here will be able to have a receipt generated
+        for it later.
+      `,
       args: [
-        { class: 'FObjectProperty', name: 'obj' }
+        {
+          class: 'FObjectProperty',
+          name: 'obj'
+        }
       ]
     },
     {
       name: 'build',
-      javaReturns: 'void'
+      javaReturns: 'void',
+      documentation: `
+        Optional intermediate step that builds necessary models
+        (i.e. a Merkle Tree) from which to generate receipts.`
+    },
+    {
+      name: 'generate',
+      javaReturns: 'net.nanopay.security.receipt.Receipt',
+      documentation: `
+        Generates a receipt given an FObject.
+      `,
+      args: [
+        {
+          class: 'FObjectProperty',
+          name: 'obj'
+        }
+      ]
     }
   ]
 });
