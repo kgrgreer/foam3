9e91986da59df6eb2d441805674ae6786c868aa6
penzital <penzital888@gmail.com>
2018-10-11 13:37:44 -0400

    added BalanceHistory model

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -253,6 +253,7 @@ FOAM_FILES([
   { name: 'net/nanopay/cico/ui/AlternaEFTDownload', flags: ['web'] },
   { name: 'net/nanopay/tx/cico/CITransaction' },
   { name: 'net/nanopay/tx/FeeAccount' },
+  { name: 'net/nanopay/tx/BalanceHistory' },
   { name: 'net/nanopay/tx/cico/VerificationTransaction' },
   { name: 'net/nanopay/tx/cico/COTransaction' },
   { name: 'net/nanopay/tx/alterna/AlternaFormat' },
