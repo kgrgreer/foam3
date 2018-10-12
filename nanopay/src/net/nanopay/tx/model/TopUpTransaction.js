903c6cf9d31c6d38f13ca15ac17d35a3acb8b55f
penzital <penzital888@gmail.com>
2018-10-12 13:18:18 -0400

    update TopupTxn createTransfers

--- nanopay/src/net/nanopay/tx/model/TopUpTransaction.js
+++ nanopay/src/net/nanopay/tx/model/TopUpTransaction.js
@@ -13,15 +13,51 @@ foam.CLASS({
       buildJavaClass: function(cls) {
         cls.extras.push(foam.java.Code.create({
           data: `
-            public HashMap<String, Transfer[]> mapTransfers() {
+            public Transfer[] createTransfers(foam.core.X x, Transaction oldTxn) {
 
-              HashMap<String, Transfer[]> hm = new HashMap<String, Transfer[]>();
-              if ( ! isActive() ) return hm;
-              hm.put(getSourceCurrency(), new Transfer[]{
-                new Transfer.Builder(getX()).setAccount(getDestinationAccount()).setAmount(getTotal()).build()
-              });
-              return hm;
-            }
+                      Transfer [] tr = new Transfer[] {};
+                      Account account = findSourceAccount(x);
+                      TrustAccount trustAccount = TrustAccount.find(x, account);
+
+                      if ( getStatus() == TransactionStatus.COMPLETED ) {
+
+                        Transfer transfer = new Transfer.Builder(getX())
+                                              .setDescription(trustAccount.getName()+" Cash-In")
+                                              .setAccount(trustAccount.getId())
+                                              .setAmount(-getTotal())
+                                              .build();
+                        tr = new Transfer[] {
+                          transfer,
+                          new Transfer.Builder(getX())
+                            .setDescription("Cash-In")
+                            .setAccount(getDestinationAccount())
+                            .setAmount(getTotal())
+                            .build()
+                        };
+                      } else if ( getStatus() == TransactionStatus.DECLINED &&
+                                  oldTxn != null &&
+                                  oldTxn.getStatus() == TransactionStatus.COMPLETED ) {
+
+                        Transfer transfer = new Transfer.Builder(x)
+                                              .setDescription(trustAccount.getName()+" Cash-In DECLINED")
+                                              .setAccount(trustAccount.getId())
+                                              .setAmount(getTotal())
+                                              .build();
+                        tr = new Transfer[] {
+                          transfer,
+                          new Transfer.Builder(x)
+                            .setDescription("Cash-In DECLINED")
+                            .setAccount(getDestinationAccount())
+                            .setAmount(-getTotal())
+                            .build()
+                        };
+                        setStatus(TransactionStatus.REVERSE);
+                      } else return new Transfer[0];
+                      Transfer[] replacement = Arrays.copyOf(getTransfers(), getTransfers().length + tr.length);
+                      System.arraycopy(tr, 0, replacement, getTransfers().length, tr.length);
+                      return replacement;
+
+              }
         `}));
       }
     }
