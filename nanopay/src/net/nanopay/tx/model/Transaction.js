4f813c25e688a4d3ded7d35f7a1c3b84f772e82a
Greenspot <mayowa.olurin@gmail.com>
2018-10-22 01:14:32 +0100

    Removed pacs008EndToEndId from Transaction

--- nanopay/src/net/nanopay/tx/model/Transaction.js
+++ nanopay/src/net/nanopay/tx/model/Transaction.js
@@ -224,11 +224,6 @@ foam.CLASS({
       name: 'messageId'
     },
     {
-      documentation: `Defined by ISO 20220 (Pacs008)`,
-      class: 'String',
-      name: 'pacs008EndToEndId'
-    },
-    {
       class: 'String',
       name: 'sourceCurrency',
       value: 'CAD'
