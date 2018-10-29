73f903905efc4200401e5f020dcc10cd3e26d8bd
Kirk Eaton <kirk@nanopay.net>
2018-10-29 13:29:35 -0400

    Uncommented toJSON code in AbstractBitMapPropertyInfo

--- nanopay/src/net/nanopay/iso8583/AbstractBitMapPropertyInfo.java
+++ nanopay/src/net/nanopay/iso8583/AbstractBitMapPropertyInfo.java
@@ -35,38 +35,39 @@ public abstract class AbstractBitMapPropertyInfo
 
     StringBuilder sb = sb_.get();
     java.util.BitSet set = (java.util.BitSet) value;
+    int bits = set.size();
 
-//    // output the bitset as a hex string
-//    for ( int i = 0 ; i < bits ; i++ ) {
-//      int bit = set.get(i) ? 1 : 0;
-//      if ( digit == 1 ) {
-//        sum += bit * 8;
-//      } else if ( digit == 2 ) {
-//        sum += bit * 4;
-//      } else if ( digit == 3 ) {
-//        sum += bit * 2;
-//      } else if ( digit == 4 || i < bits + 1 ) {
-//        sum += bit;
-//        digit = 0;
-//        if ( sum < 10 ) {
-//          sb.append(sum);
-//        } else if ( sum == 10 ) {
-//          sb.append('a');
-//        } else if ( sum == 11 ) {
-//          sb.append('b');
-//        } else if ( sum == 12 ) {
-//          sb.append('c');
-//        } else if ( sum == 13 ) {
-//          sb.append('d');
-//        } else if ( sum == 14 ) {
-//          sb.append('e');
-//        } else if ( sum == 15 ) {
-//          sb.append('f');
-//        }
-//        sum = 0;
-//      }
-//      digit += 1;
-//    }
+    // output the bitset as a hex string
+    for ( int i = 0 ; i < bits ; i++ ) {
+      int bit = set.get(i) ? 1 : 0;
+      if ( digit == 1 ) {
+        sum += bit * 8;
+      } else if ( digit == 2 ) {
+        sum += bit * 4;
+      } else if ( digit == 3 ) {
+        sum += bit * 2;
+      } else if ( digit == 4 || i < bits + 1 ) {
+        sum += bit;
+        digit = 0;
+        if ( sum < 10 ) {
+          sb.append(sum);
+        } else if ( sum == 10 ) {
+          sb.append('a');
+        } else if ( sum == 11 ) {
+          sb.append('b');
+        } else if ( sum == 12 ) {
+          sb.append('c');
+        } else if ( sum == 13 ) {
+          sb.append('d');
+        } else if ( sum == 14 ) {
+          sb.append('e');
+        } else if ( sum == 15 ) {
+          sb.append('f');
+        }
+        sum = 0;
+      }
+      digit += 1;
+    }
 
     outputter.output(sb.toString());
   }
