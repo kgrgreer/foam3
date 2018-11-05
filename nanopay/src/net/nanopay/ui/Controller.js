6b1a79cffc0bdd40e1ce212489ec10b1408cfc32
Mike Carcasole <mikecarcasole@gmail.com>
2018-11-05 08:38:12 -0800

    Add flow document styling to nanopay's controller.

--- nanopay/src/net/nanopay/ui/Controller.js
+++ nanopay/src/net/nanopay/ui/Controller.js
@@ -85,6 +85,57 @@ foam.CLASS({
     .net-nanopay-ui-ActionView-exportButton img {
       margin-right: 5px;
     }
+
+    .foam-flow-Document {
+      background-color: #ffffff;
+      color: #4c555a;
+      max-width: 1000px;
+      margin: auto;
+      padding: 20px;
+      line-height: 26px;
+      font-size: 14px;
+      font-weight: 500;
+      -webkit-font-smoothing: antialiased;
+    }
+    .foam-flow-Document h1 {
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 32px;
+    }
+    .foam-flow-Document h2 {
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 26px;
+    }
+    .foam-flow-Document h3 {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 22px;
+    }
+    .foam-flow-Document h1,
+    .foam-flow-Document h2,
+    .foam-flow-Document h3,
+    .foam-flow-Document h4,
+    .foam-flow-Document h5 {
+      margin: 12px 0 0 0;
+      color: #292e31;
+    }
+    .foam-flow-Document p {
+      margin-bottom: 0;
+      margin-top: 20px;
+    }
+    .foam-flow-Document code {
+      background-color: black;
+      color: white;
+      display: block;
+      font-family: monospace;
+      padding: 20px;
+      white-space: pre;
+    }
+    .foam-flow-Document a {
+      color: rgb(0, 153, 229);
+      text-decoration-line: none;
+    }
   `,
 
   constants: [
