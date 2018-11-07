d5788a542261743f17d433846aed5cfbe449740d
Devansh Jain <devansh@nanopay.net>
2018-11-07 12:05:11 -0500

    Use threadlocal instance of zxcvbn

--- nanopay/src/net/nanopay/sme/passwordutil/PasswordStrengthCalculator.js
+++ nanopay/src/net/nanopay/sme/passwordutil/PasswordStrengthCalculator.js
@@ -10,12 +10,28 @@ foam.CLASS({
     'net.nanopay.sme.passwordutil.PasswordEntropy'
   ],
 
+  axioms: [
+    {
+      name: 'javaExtras',
+      buildJavaClass: function(cls) {
+        cls.extras.push(foam.java.Code.create({
+          data:
+          `protected ThreadLocal<Zxcvbn> zxcvbn = new ThreadLocal<Zxcvbn>() {
+            @Override
+            protected Zxcvbn initialValue() {
+              return new Zxcvbn();
+            }
+          };`
+        }));
+      }
+    }
+  ],
+
   methods: [
     {
       name: 'getPasswordStrength',
       javaCode: `
-        Zxcvbn zxcvbn = new Zxcvbn();
-        return zxcvbn.measure(password).getScore();
+        return zxcvbn.get().measure(password).getScore();
       `
     }
   ]
