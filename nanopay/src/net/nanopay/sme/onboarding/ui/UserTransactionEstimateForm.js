f2796013679c0b300091fbfda658274043e3d336
Patrick Zanowski <zahnno@gmail.com>
2018-11-07 20:11:36 -0500

    Added fields and input to user transaction estimate form

--- nanopay/src/net/nanopay/sme/onboarding/ui/UserTransactionEstimateForm.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/UserTransactionEstimateForm.js
@@ -1,5 +1,145 @@
 foam.CLASS({
   package: 'net.nanopay.sme.onboarding.ui',
   name: 'UserTransactionEstimateForm',
-  extends: 'net.nanopay.ui.wizard.WizardSubView'
+  extends: 'net.nanopay.ui.wizard.WizardSubView',
+
+  documentation: `Third step in the business registration wizard.
+      Responsible for capturing transaction business information.`,
+
+    css: `
+    ^ {
+      width: 488px;
+    }
+    ^ .foam-u2-tag-Select {
+      width: 100%;
+      height: 35px;
+      margin-bottom: 10px;
+    }
+    ^ .label {
+      margin-left: 0px;
+    }
+    ^ .foam-u2-TextField {
+      width: 100%;
+      height: 35px;
+      margin-bottom: 10px;
+    }
+    ^ .foam-u2-view-RadioView {
+      display: inline-block;
+      margin-right: 5px;
+      float: right;
+    }
+    ^ .foam.u2.CheckBox {
+      display: inline-block;
+    }
+    ^ .inline {
+      margin: 15px;
+    }
+  `,
+
+  properties: [
+    {
+      name: 'baseCurrency',
+      view: function(_, X) {
+        return foam.u2.view.ChoiceView.create({
+          dao: X.currencyDAO,
+          objToChoice: function(a) {
+            return [a.id, a.name];
+          }
+        });
+      }
+    },
+    {
+      class: 'Boolean',
+      name: 'defaultCurrency'
+    },
+    {
+      class: 'String',
+      name: 'revenueEstimate'
+    },
+    {
+      name: 'internationalPayments',
+      documentation: 'Radio button determining business partaking in international payments.',
+      view: {
+        class: 'foam.u2.view.RadioView',
+        choices: [
+          'Yes',
+          'No'
+        ],
+        value: 'No'
+      }
+    },
+    {
+      class: 'String',
+      name: 'purposeField'
+    },
+    {
+      class: 'String',
+      name: 'annualField'
+    },
+    {
+      class: 'String',
+      name: 'estimatedField',
+      view: {
+        class: 'foam.u2.view.ChoiceView',
+        choices: [
+          'Less than $100,000',
+          'More than $100,000'
+        ]
+      }
+    }
+
+  ],
+
+  messages: [
+    { name: 'TITLE', message: 'Details about your transaction' },
+    { name: 'BASE_CURRENCY_LABEL', message: 'Base Currency' },
+    { name: 'DEFAULT_MESSAGE', message: 'This will be the default currency for sending and receiving payments.' },
+    { name: 'REVENUE_ESTIMATE_LABEL', message: 'Annual revenue estimate in your base currency' },
+    { name: 'INTERNATIONAL_PAYMENTS_LABEL', message: 'Are you sending or receiving international payments' },
+    { name: 'SECOND_TITLE', message: 'International transfers' },
+    { name: 'CURRENCY_TYPE', message: 'U.S. Dollars' },
+    { name: 'PURPOSE_LABEL', message: 'Purpose of Transactions' },
+    { name: 'ANNUAL_LABEL', message: 'Annual Number of Transactions' },
+    { name: 'ESTIMATED_LABEL', message: 'Estimated Annual Volume in USD' }
+  ],
+
+  methods: [
+    function initE() {
+      this.addClass(this.myClass())
+      .start()
+        .start().addClass('subTitle').add(this.TITLE).end()
+        .start().addClass('label-input')
+          .start().addClass('label').add(this.BASE_CURRENCY_LABEL).end()
+          .start(this.BASE_CURRENCY).end()
+        .end()
+        .tag({ class: 'foam.u2.CheckBox', data$: this.defaultCurrency$ }).addClass('inline')
+        .start().addClass('inline').add(this.DEFAULT_MESSAGE).end()
+        .start().addClass('label-input')
+          .start().addClass('inline').add(this.REVENUE_ESTIMATE_LABEL).end()
+          .start().add(this.REVENUE_ESTIMATE).end()
+        .end()
+        .start().addClass('label-input')
+          .start().addClass('inline').add(this.INTERNATIONAL_PAYMENTS_LABEL).end()
+          .start(this.INTERNATIONAL_PAYMENTS).addClass('inline').end()
+        .end()
+        .start().addClass('subTitle').add(this.SECOND_TITLE).end()
+        .start().addClass('label-input')
+          .start({ class: 'foam.u2.tag.Image', data: 'images/flags/us.png' })
+          .start().addClass('inline').add(this.CURRENCY_TYPE).end()
+        .end()
+        .start().addClass('label-input')
+          .start().addClass('label').add(this.PURPOSE_LABEL).end()
+          .start(this.PURPOSE_FIELD).end()
+        .end()
+        .start().addClass('label-input')
+          .start().addClass('label').add(this.ANNUAL_LABEL).end()
+          .start(this.ANNUAL_FIELD).end()
+        .end()
+        .start().addClass('label-input')
+          .start().addClass('label').add(this.ESTIMATED_LABEL).end()
+          .start(this.ESTIMATED_FIELD).end()
+        .end()
+      .end();
+    }
+  ]
 });
