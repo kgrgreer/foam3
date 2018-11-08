a72295ffd05dad351ee42701f64a551fdb00780f
Patrick Zanowski <zahnno@gmail.com>
2018-11-07 19:40:53 -0500

    Added mutiple fields to businessform

--- nanopay/src/net/nanopay/sme/onboarding/ui/BusinessForm.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/BusinessForm.js
@@ -2,9 +2,15 @@ foam.CLASS({
   package: 'net.nanopay.sme.onboarding.ui',
   name: 'BusinessForm',
   extends: 'net.nanopay.ui.wizard.WizardSubView',
+  documentation: 'Second step in the business registration wizard. Responsible for capturing business information.',
+
+  implements: [
+    'foam.mlang.Expressions'
+  ],
 
   requires: [
     'foam.nanos.auth.Address',
+    'foam.nanos.auth.Country',
     'foam.nanos.auth.Phone',
     'foam.nanos.auth.Region',
     'foam.nanos.auth.User',
@@ -18,9 +24,26 @@ foam.CLASS({
     ^ .foam-u2-tag-Select {
       width: 100%;
       height: 35px;
+      margin-bottom: 10px;
     }
     ^ .label {
       margin-left: 0px;
+    }
+    ^ .foam-u2-TextField {
+      width: 100%;
+      height: 35px;
+      margin-bottom: 10px;
+    }
+    ^ .foam-u2-view-RadioView {
+      display: inline-block;
+      margin-right: 5px;
+      float: right;
+    }
+    ^ .foam.u2.CheckBox {
+      display: inline-block;
+    }
+    ^ .inline {
+      margin: 15px;
     }
   `,
 
@@ -31,9 +54,16 @@ foam.CLASS({
       documentation: 'Toggles additional input for operating business name.'
     },
     {
-      class: 'Boolean',
       name: 'holdingCompany',
-      documentation: 'Radio button determining business is a holding company.'
+      documentation: 'Radio button determining business is a holding company.',
+      view: {
+        class: 'foam.u2.view.RadioView',
+        choices: [
+          'Yes',
+          'No'
+        ],
+        value: 'No'
+      }
     },
     {
       class: 'Boolean',
@@ -68,12 +98,73 @@ foam.CLASS({
     },
     {
       class: 'String',
+      name: 'registeredBusinessNameField',
+      documentation: 'Registered business name field.'
+    },
+    {
+      class: 'String',
       name: 'operatingBusinessNameField',
       documentation: 'Operating business name field.'
     },
-    // {
-    //   class: ''
-    // },
+    {
+      class: 'String',
+      name: 'taxNumberField',
+      documentation: 'Tax identification number field.'
+    },
+    {
+      name: 'countryField',
+      view: function(_, X) {
+        return foam.u2.view.ChoiceView.create({
+          dao: X.countryDAO.where(X.data.OR(
+            X.data.EQ(X.data.Country.NAME, 'Canada'),
+            X.data.EQ(X.data.Country.NAME, 'USA')
+          )),
+          objToChoice: function(a) {
+            return [a.id, a.name];
+          }
+        });
+      }
+    },
+    {
+      class: 'String',
+      name: 'streetNumberField',
+      documentation: 'Street number field.'
+    },
+    {
+      class: 'String',
+      name: 'streetNameField',
+      documentation: 'Street name field.'
+    },
+    {
+      class: 'String',
+      name: 'suiteField',
+      documentation: 'Suite field.'
+    },
+    {
+      name: 'provinceField',
+      documentation: 'Dropdown detailing and providing choice selection of provinces dictated by country chosen.',
+      view: function(_, X) {
+        var choices = X.data.slot(function(countryField) {
+          return X.regionDAO.where(this.EQ(this.Region.COUNTRY_ID, countryField || ''));
+        });
+        return foam.u2.view.ChoiceView.create({
+          objToChoice: function(region) {
+            return [region.id, region.name];
+          },
+          dao$: choices
+        });
+      }
+    },
+    {
+      class: 'String',
+      name: 'cityField',
+      documentation: 'City field.'
+    },
+    {
+      class: 'String',
+      name: 'postalCodeField',
+      documentation: 'Postal code field.'
+    },
     {
       class: 'String',
       name: 'phoneNumberField',
@@ -85,8 +176,12 @@ foam.CLASS({
       documentation: 'Business website field.'
     },
     {
-      name: 'supportFiles',
-      documentation: 'Contains any supporting files associated to verifying the business.'
+      class: 'foam.nanos.fs.FileArray',
+      name: 'additionalDocuments',
+      documentation: 'Additional documents for compliance verification.',
+      view: {
+        class: 'net.nanopay.onboarding.b2b.ui.AdditionalDocumentsUploadView'
+      }
     }
   ],
 
@@ -100,7 +195,14 @@ foam.CLASS({
     { name: 'TAX_ID_LABEL', message: 'Tax Identification Number(US Only)' },
     { name: 'HOLDING_QUESTION', message: 'Is this a holding company?' },
     { name: 'SECOND_TITLE', message: 'Business contact information' },
-    { name: 'BUSINESS_ADDRESS_LABEL', message: 'Business Address' },
+    { name: 'COUNTRY_LABEL', message: 'Country' },
+    { name: 'STREET_NUMBER_LABEL', message: 'Street Number' },
+    { name: 'STREET_NAME_LABEL', message: 'Street Name' },
+    { name: 'ADDRESS_LABEL', message: 'Address 2 (optional)' },
+    { name: 'ADDRESS_HINT', message: 'Apartment, suite, unit, building, floor, etc.' },
+    { name: 'PROVINCE_LABEL', message: 'Province' },
+    { name: 'CITY_LABEL', message: 'City' },
+    { name: 'POSTAL_CODE_LABEL', message: 'Postal Code' },
     { name: 'PRIMARY_RESIDENCE_LABEL', message: 'My business address is also my primary residence' },
     { name: 'PHONE_NUMBER_LABEL', message: 'Phone Number' },
     { name: 'WEBSITE_LABEL', message: 'Website (Optional)' },
@@ -121,7 +223,66 @@ foam.CLASS({
             .start().addClass('label').add(this.INDUSTRY_LABEL).end()
             .start(this.INDUSTRY_FIELD).end()
           .end()
-        .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.BUSINESS_NAME_LABEL).end()
+            .start(this.REGISTERED_BUSINESS_NAME_FIELD).end()
+          .end()
+          .tag({ class: 'foam.u2.CheckBox', data$: this.operating$ })
+          .start().addClass('inline').add(this.OPERATING_QUESTION).end()
+          .start().show(this.operating$)
+            .start().addClass('label-input')
+              .start().addClass('label').add(this.OPERATING_BUSINESS_NAME_LABEL).end()
+              .start(this.OPERATING_BUSINESS_NAME_FIELD).end()
+            .end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.TAX_ID_LABEL).end()
+            .start(this.TAX_NUMBER_FIELD).end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('inline').add(this.HOLDING_QUESTION).end()
+            .start(this.HOLDING_COMPANY).end()
+          .start()
+          .start().addClass('subTitle').add(this.SECOND_TITLE).end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.COUNTRY_LABEL).end()
+            .startContext({ data: this })
+              .start(this.COUNTRY_FIELD).end()
+            .endContext()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.STREET_NUMBER_LABEL).end()
+            .start(this.STREET_NUMBER_FIELD).end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.STREET_NAME_LABEL).end()
+            .start(this.STREET_NAME_FIELD).end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.ADDRESS_LABEL).end()
+            .start(this.SUITE_FIELD).end()
+            .start().addClass('label').add(this.ADDRESS_HINT).end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.PROVINCE_LABEL).end()
+            .startContext({ data: this })
+              .start(this.PROVINCE_FIELD).end()
+            .endContext()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.CITY_LABEL).end()
+            .start(this.CITY_FIELD).end()
+          .end()
+          .start().addClass('label-input')
+            .start().addClass('label').add(this.POSTAL_CODE_LABEL).end()
+            .start(this.POSTAL_CODE_FIELD).end()
+          .end()
+          .tag({ class: 'foam.u2.CheckBox', data$: this.primaryResidence$ })
+          .start().addClass('inline').add(this.PRIMARY_RESIDENCE_LABEL).end()
+          .start().addClass('subTitle').add(this.THIRD_TITLE).end()
+          .start().addClass('title').add(this.UPLOAD_DESCRIPTION).end()
+          .start(this.ADDITIONAL_DOCUMENTS).end()
+        .end();
     }
   ]
 });
