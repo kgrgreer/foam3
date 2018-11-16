4578d5e67df929edc214cc57993f73f8608770e0
Joel Hughes <joel@nanopay.net>
2018-11-16 08:55:16 -0500

    Tested

--- tools/upgrade/AllNSpec_01.sh
+++ tools/upgrade/AllNSpec_01.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 JOURNAL_HOME="/opt/nanopay/journals"
+#JOURNAL_HOME="/Users/jhughes/workspace/repos/NANOPAY/tmp/journals"
 
 # Prepend the nspec name to all journal entries in preparation for merging into single journal or image file.
 # Creates an array of the file names
@@ -50,11 +51,13 @@ do
   name=${arr[0]}
   dao=${arr[1]}
 
+  echo "name:${name} dao:${dao}"
+
   if [ -f "$JOURNAL_HOME/$name" ]; then
-    perl -p -i -e 's/^p(.*?)/$dao.p\1/g;' "$JOURNAL_HOME/$name"
+    perl -p -i -e "s/^p(.*?)/${dao}.p\1/g;" "$JOURNAL_HOME/$name"
   fi
   if [ -f "$JOURNAL_HOME/$name.0" ]; then
-    perl -p -i -e 's/^p(.*?)/$dao.p\1/g;' "$JOURNAL_HOME/$name.0"
+    perl -p -i -e "s/^p(.*?)/${dao}.p\1/g;" "$JOURNAL_HOME/$name.0"
   fi
 done
 exit 0
