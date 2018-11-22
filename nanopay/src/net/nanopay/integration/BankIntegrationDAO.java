bc9071097af4d677087ce42bffc5da8195b16c33
waseemchamaaa <waseemchamaa6@gmail.com>
2018-11-22 13:38:56 -0500

    added select for bank integration

---
+++ nanopay/src/net/nanopay/integration/BankIntegrationDAO.java
@@ -0,0 +1,43 @@
+package net.nanopay.integration;
+
+
+import foam.core.X;
+import foam.dao.ArraySink;
+import foam.dao.DAO;
+import foam.dao.ProxyDAO;
+import foam.nanos.auth.User;
+import net.nanopay.integration.quick.QuickIntegrationService;
+import net.nanopay.integration.xero.XeroIntegrationService;
+import java.util.List;
+
+
+public class BankIntegrationDAO
+  extends ProxyDAO {
+
+  public BankIntegrationDAO(X x, DAO delegate) {
+    setX(x);
+    setDelegate(delegate);
+  }
+
+  // Collects data from the account, transaction, user and balance DAO and formats and sets the data for the DigitalAccountInfo Model
+  public foam.dao.Sink select_(foam.core.X x, foam.dao.Sink sink, long skip, long limit, foam.mlang.order.Comparator order, foam.mlang.predicate.Predicate predicate) {
+
+    User                    user       = (User) x.get("user");
+    XeroIntegrationService  xero       = (XeroIntegrationService) x.get("xeroSignIn");
+    QuickIntegrationService quick      = (QuickIntegrationService) x.get("quickSignIn");
+    List<AccountingBankAccount> bankList;
+    // Grabs all Digital Accounts
+    switch ( user.getIntegrationCode() ) {
+      case 1: { bankList = xero.pullBanks(x, user); break;}
+      case 2: { bankList = quick.pullBanks(x, user); break;}
+      default:{ bankList = null; break; }
+    }
+    if ( sink == null ){
+      sink = new ArraySink();
+    }
+    for (AccountingBankAccount bank: bankList ) {
+      sink.put(bank, null);
+    }
+    return sink;
+  }
+}
