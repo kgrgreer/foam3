6047cd11d69f0dc7d7bc9325d24b697f3d6aa749
Troy Wolters <troy@nanopay.net>
2018-11-23 13:03:23 -0500

    Fix payer name not getting set in email

--- nanopay/src/net/nanopay/auth/PublicUserInfo.js
+++ nanopay/src/net/nanopay/auth/PublicUserInfo.js
@@ -30,6 +30,11 @@ foam.CLASS({
       visibility: foam.u2.Visibility.RO
     },
     {
+      class: 'String',
+      name: 'organization',
+      visibility: foam.u2.Visibility.RO
+    },
+    {
       class: 'EMail',
       name: 'email'
     },
@@ -69,18 +74,22 @@ foam.CLASS({
     {
       name: 'label',
       code: function() {
-        return typeof this.firstName === 'string' && this.firstName.length > 0
-            ? typeof this.lastName === 'string' && this.lastName.length > 0
-                ? `${this.firstName} ${this.lastName}`
-                : this.firstName
+        return this.organization
+          ? this.organization
+          : this.firstName
+            ? this.lastName
+              ? `${this.firstName} ${this.lastName}`
+              : this.firstName
             : 'Unknown';
       },
       javaReturns: 'String',
       javaCode: `
-        return ! SafetyUtil.isEmpty(this.getFirstName())
+        return ! SafetyUtil.isEmpty(this.getOrganization())
+          ? this.getOrganization()
+          : ! SafetyUtil.isEmpty(this.getFirstName())
             ? ! SafetyUtil.isEmpty(this.getLastName())
-                ? this.getFirstName() + " " + this.getLastName()
-                : this.getFirstName()
+              ? this.getFirstName() + " " + this.getLastName()
+              : this.getFirstName()
             : "Unknown";
       `
     }
@@ -95,6 +104,7 @@ foam.CLASS({
             setId(user.getId());
             setFirstName(user.getFirstName());
             setLastName(user.getLastName());
+            setOrganization(user.getOrganization());
             setBusinessName(user.getBusinessName());
             setEmail(user.getEmail());
             setProfilePicture(user.getProfilePicture());
