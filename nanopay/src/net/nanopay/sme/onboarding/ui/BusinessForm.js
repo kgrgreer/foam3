2cef6f405ac541c93951e7ff5961778fdd183f64
shreymahendru <shreymahendru@gmail.com>
2018-11-23 12:42:32 -0500

    bussinessType factory

--- nanopay/src/net/nanopay/sme/onboarding/ui/BusinessForm.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/BusinessForm.js
@@ -127,7 +127,7 @@ foam.CLASS({
         });
       },
       factory: function() {
-        if ( this.viewData.user.businessTypeId || this.viewData.user.businessTypeId == 0 ) return this.viewData.user.businessTypeId;
+        if ( this.viewData.user.businessTypeId ) return this.viewData.user.businessTypeId;
       },
       postSet: function(o, n) {
         this.viewData.user.businessTypeId = n;
