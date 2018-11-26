8995cfd5297615aade41c048d8cebf3dcc921215
Patrick Zanowski <zahnno@gmail.com>
2018-11-26 12:10:41 -0500

    Added nanopay specific preventDuplicateEmailDAO

---
+++ nanopay/src/net/nanopay/auth/email/PreventDuplicateEmailDAO.js
@@ -0,0 +1,77 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'net.nanopay.auth.email',
+  name: 'PreventDuplicateEmailDAO',
+
+  documentation: 'DAO decorator that prevents putting a user with the same email',
+
+  javaImports: [
+    'net.nanopay.model.Business',
+    'net.nanopay.contacts.Contact',
+    'foam.core.FObject',
+    'foam.core.X',
+    'foam.dao.DAO',
+    'foam.dao.ProxyDAO',
+    'foam.nanos.auth.User',
+    'foam.mlang.MLang',
+    'foam.mlang.sink.Count',
+    'foam.util.Email',
+    'foam.util.SafetyUtil',
+  ],
+
+  extends: 'foam.dao.ProxyDAO',
+
+  methods: [
+    {
+      name: 'put_',
+      javaCode: `
+  if ( obj instanceof Business || obj instanceof Contact ) {
+    return super.put_(x, obj);
+  }
+
+  User user = (User) obj;
+  boolean newUser = ( getDelegate().find(user.getId()) == null );
+
+  if ( newUser ) {
+    if ( SafetyUtil.isEmpty(user.getEmail()) ) {
+      throw new RuntimeException("Email required");
+    }
+
+    if ( ! Email.isValid(user.getEmail()) ) {
+      throw new RuntimeException("Invalid Email");
+    }
+  }
+
+  Count count = new Count();
+  count = (Count) ((DAO) getX().get("localUserDAO"))
+      .where(MLang.AND(
+        MLang.EQ(User.EMAIL, user.getEmail()),
+        MLang.NEQ(User.ID,  user.getId())
+      )).limit(1).select(count);
+
+  if ( count.getValue() == 1 ) {
+    throw new RuntimeException("User with same email address already exists: " + user.getEmail());
+  }
+
+  return super.put_(x, obj);
+      `,
+   }
+  ],
+
+  axioms: [
+    {
+      buildJavaClass: function(cls) {
+        cls.extras.push(`
+public PreventDuplicateEmailDAO(foam.core.X x, foam.dao.DAO delegate) {
+  super(x, delegate);
+}
+        `);
+      },
+    },
+  ],
+});
