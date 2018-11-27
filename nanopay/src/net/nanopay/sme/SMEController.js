05ddf928a706d38bd3ad2c9874a8aea07bf91a57
Henry Han <henry.han@usask.ca>
2018-11-27 17:19:00 -0500

    Removed the duplicate onUserUpdate function call in SMEController

--- nanopay/src/net/nanopay/sme/SMEController.js
+++ nanopay/src/net/nanopay/sme/SMEController.js
@@ -162,8 +162,6 @@ foam.CLASS({
       this.client.agentAuth.getCurrentAgent(this).then(function(result) {
         if ( result ) {
           self.agent = result;
-
-          self.onUserUpdate();
         }
       }).catch(function(err) {
         self.requestLogin().then(function() {
