6405484faea22c2785d4716a777fcafbba806901
Zac Zhang <zaczhang19@gmail.com>
2018-11-29 17:57:32 -0500

    fix CircleCi failure

--- nanopay/src/net/nanopay/tx/KotakCOTransaction.js
+++ nanopay/src/net/nanopay/tx/KotakCOTransaction.js
@@ -96,56 +96,5 @@ foam.CLASS({
       class: 'String',
       name: 'UTRNumber'
     }
-  ],
-
-  methods: [
-    {
-      name: 'createTransfers',
-      args: [
-        {
-          name: 'x',
-          javaType: 'foam.core.X'
-        }
-      ],
-      javaReturns: 'Transfer[]',
-      javaCode: `
-      Transfer [] tr = new Transfer[] {};
-      Account account = findSourceAccount(x);
-      TrustAccount trustAccount = TrustAccount.find(x, account.findOwner(x), "INR");
-
-      if ( getStatus() == TransactionStatus.PENDING ) {
-        Transfer transfer = new Transfer.Builder(x)
-                              .setDescription(trustAccount.getName()+" Cash-Out to INR Trust Account")
-                              .setAccount(trustAccount.getId())
-                              .setAmount(getSettlementAmount())
-                              .build();
-        tr = new Transfer[] {
-          transfer,
-          new Transfer.Builder(x)
-            .setDescription("Cash-Out from CAD Digital Account")
-            .setAccount(getSourceAccount())
-            .setAmount(-getTotal())
-            .build()
-        };
-      } else if ( getStatus() == TransactionStatus.DECLINED ) {
-        Transfer transfer = new Transfer.Builder(x)
-                              .setDescription(trustAccount.getName()+" Cash-Out DECLINED")
-                              .setAccount(trustAccount.getId())
-                              .setAmount(-getSettlementAmount())
-                              .build();
-        tr = new Transfer[] {
-          transfer,
-          new Transfer.Builder(x)
-            .setDescription("Cash-Out DECLINED")
-            .setAccount(getSourceAccount())
-            .setAmount(getTotal())
-            .build()
-        };
-      }
-
-      add(tr);
-      return getTransfers();
-      `
-    }
   ]
 });
