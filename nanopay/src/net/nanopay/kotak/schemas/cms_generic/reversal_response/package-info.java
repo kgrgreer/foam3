5e5382c47969852da4e0026171404488a6c08058
Zac Zhang <zaczhang19@gmail.com>
2018-11-29 16:20:56 -0500

    remove kotak wsimport implementation code

--- nanopay/src/net/nanopay/kotak/schemas/cms_generic/reversal_response/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.kotak.com/schemas/CMS_Generic/Reversal_Response.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.nanopay.kotak.schemas.cms_generic.reversal_response;
