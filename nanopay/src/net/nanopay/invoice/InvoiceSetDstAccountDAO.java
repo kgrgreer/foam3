fc996471259d6535e82df4b77e507022814229e3
Troy Wolters <troy@nanopay.net>
2018-12-07 14:45:34 -0500

    Fix backend logic for invoicing contacts and businesses

--- nanopay/src/net/nanopay/invoice/InvoiceSetDstAccountDAO.java
+++ nanopay/src/net/nanopay/invoice/InvoiceSetDstAccountDAO.java
@@ -1,111 +1,49 @@
 package net.nanopay.invoice;
 
-import static foam.mlang.MLang.EQ;
-import static foam.mlang.MLang.AND;
-import static foam.mlang.MLang.NOT;
-import static foam.mlang.MLang.INSTANCE_OF;
-
-import java.util.List;
-
 import foam.core.FObject;
 import foam.core.X;
-import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
-import foam.nanos.auth.AuthService;
 import foam.nanos.auth.User;
-import foam.util.SafetyUtil;
-import net.nanopay.account.DigitalAccount;
 import net.nanopay.bank.BankAccount;
 import net.nanopay.contacts.Contact;
 import net.nanopay.invoice.model.Invoice;
-import net.nanopay.invoice.model.InvoiceStatus;
+import net.nanopay.model.Business;
 
-// check if Payee is a User or Contact
-// if user check if User has a bank account
-//    if yes destination account is set to User account
-//    else destination account set to Payer owned Holding Account (Digital Account)
-// else /* User is a Contact */
-//    if Contact has a bank Account destination account set to Contact bank account //TODO: this flow is for future implementation
-//    else destination account set to Payer owned Holding Account (Digital Account)
-
+/**
+ * This DAO decorator sets the destination account on invoices.
+ */
 public class InvoiceSetDstAccountDAO extends ProxyDAO {
-
-  public final static String INVOICE_HOLDING_ACCOUNT = "invoice.holdingAccount";
+  public DAO localUserDAO_;
 
   public InvoiceSetDstAccountDAO(X x, DAO delegate) {
     super(x, delegate);
+    localUserDAO_ = (DAO) x.get("localUserDAO");
   }
 
   @Override
   public FObject put_(X x, FObject obj) {
-    if ( obj == null ) {
-      throw new RuntimeException("Cannot put null");
-    }
-    AuthService auth = (AuthService) x.get("auth");
-    User currentUser = (User) x.get("user");
+    if ( obj == null ) throw new RuntimeException("Cannot put null");
+
     Invoice invoice = (Invoice) obj;
 
-    // We only care about invoices whose status is pendingAcceptance, inTransit, depositingMoney
-    if ( invoice.getStatus() == InvoiceStatus.PENDING_ACCEPTANCE  ||
-      invoice.getStatus() == InvoiceStatus.IN_TRANSIT ||
-      invoice.getStatus() == InvoiceStatus.DEPOSITING_MONEY ||
-      invoice.getPayerId() != currentUser.getId()) {
+    // Ignore updates. We only care about new invoices in this decorator.
+    if ( getDelegate().inX(x).find(invoice.getId()) != null ) {
       return super.put_(x, obj);
-  }
-
-    DAO bareUserDAO = ((DAO) x.get("bareUserDAO")).inX(x);
-    DAO contactDAO = ((DAO) x.get("contactDAO")).inX(x);
-    User payer = (User) bareUserDAO.find(invoice.getPayerId());
-    User payee = (User) bareUserDAO.find(invoice.getPayeeId());
-
-
-    if ( payer == null ) {
-      throw new RuntimeException("Payer of invoiceID" + invoice.getId() + " is not set." );
     }
-    // If payee is a contact:
-    // If payee is signed up as an agent, set payeeId to be their business
-    // If not, set the external flag to true
-    Contact potentialContact = (Contact) contactDAO.find(payee.getId());
-    if ( potentialContact != null ) { // Payee is a contact
-      User signedUpPayee = getUserByEmail(bareUserDAO, potentialContact.getEmail());
-      if ( signedUpPayee != null ) {  // Contact has signed up, set payee id to be the assosciated business
-        invoice.setPayeeId(potentialContact.getBusinessId());
-      } else { // Contact has not signed up
-        invoice.setExternal(true);
+
+    User payee = (User) localUserDAO_.inX(x).find(invoice.getPayeeId());
+
+    if ( payee instanceof Business || payee instanceof Contact )  {
+      BankAccount payeeBankAccount = BankAccount.findDefault(x, payee, invoice.getDestinationCurrency());
+
+      if ( payeeBankAccount == null ) {
+        throw new RuntimeException("Cannot send money to " + payee.label() + " because they do not have a " + invoice.getDestinationCurrency() + " bank account.");
       }
-    }
 
-    if ( auth.check(x, INVOICE_HOLDING_ACCOUNT) )  { // Confirms that payer is an ablii user
-      setDestinationAccount(x, payee, payer, invoice);
+      invoice.setDestinationAccount(payeeBankAccount.getId());
     }
 
     return super.put_(x, invoice);
-  }
-
-  public User getUserByEmail(DAO bareUserDAO, String emailAddress) {
-    User user = (User) bareUserDAO.find(AND(
-      EQ(User.EMAIL, emailAddress),
-      NOT(INSTANCE_OF(Contact.class))));
-    if ( user != null ) return user;
-    return null;
-  }
-
-  public void setDestinationAccount(X x, User payee, User payer, Invoice invoice){
-    // if payee has default account in destinationCurrency, set it as destination account
-    BankAccount payeeBankAccount = BankAccount.findDefault(x, payee, invoice.getDestinationCurrency());
-    if ( payeeBankAccount != null ) { 
-      invoice.setDestinationAccount(payeeBankAccount.getId());
-      return;
-    }
-    // if payee has no bank account, set the destination account to the payers digitalAccount in the destinationCurrency
-    // NOTE: the following code can be enabelled for holding account capability
-    DigitalAccount payerDigitalAccount = DigitalAccount.findDefault(x, payer, invoice.getDestinationCurrency());
-    if ( payerDigitalAccount != null ) {
-      invoice.setDestinationAccount(payerDigitalAccount.getId());
-      return;
-    }   
-    throw new RuntimeException("UserID " + payee.getId() + " does not have a default account in" + invoice.getDestinationCurrency() );
-
   }
 }
