1d79767b94fd8a41a61eb1b0d394d130711c95df
Chanmann Lim <chanmannlim@gmail.com>
2018-12-07 10:22:30 -0500

    Fix permission check to use local context

--- nanopay/src/net/nanopay/flinks/RestrictedReadFlinksAccountDAO.java
+++ nanopay/src/net/nanopay/flinks/RestrictedReadFlinksAccountDAO.java
@@ -11,14 +11,12 @@ import net.nanopay.flinks.model.FlinksAccountsDetailResponse;
 import java.util.Iterator;
 
 public class RestrictedReadFlinksAccountDAO extends foam.dao.ProxyDAO {
-  protected AuthService auth;
   protected static final String[] RESTRICTED_FIELDS = {
     "AccountNumber", "Balance", "Holder", "Title", "Transactions"
   };
 
   public RestrictedReadFlinksAccountDAO(foam.core.X x, foam.dao.DAO delegate) {
     super(x, delegate);
-    auth = (AuthService) x.get("auth");
   }
 
   public foam.dao.Sink select_(foam.core.X x, foam.dao.Sink sink, long skip, long limit, foam.mlang.order.Comparator order, foam.mlang.predicate.Predicate predicate) {
@@ -29,7 +27,7 @@ public class RestrictedReadFlinksAccountDAO extends foam.dao.ProxyDAO {
 
       Iterator i = ((ArraySink) result).getArray().iterator();
       while (i.hasNext()) {
-        FlinksAccountsDetailResponse obj = restrict((FObject) i.next());
+        FlinksAccountsDetailResponse obj = restrict(x, (FObject) i.next());
         arraySink.put(obj, null);
       }
       return arraySink;
@@ -41,18 +39,19 @@ public class RestrictedReadFlinksAccountDAO extends foam.dao.ProxyDAO {
     FObject result = super.find_(x, id);
 
     if (result != null) {
-      restrict(result);
+      return restrict(x, result);
     }
     return result;
   }
 
-  public FlinksAccountsDetailResponse restrict(FObject obj) {
+  public FlinksAccountsDetailResponse restrict(foam.core.X x, FObject obj) {
     FlinksAccountsDetailResponse result = (FlinksAccountsDetailResponse) obj.fclone();
     AccountWithDetailModel[] accounts = result.getAccounts();
+    AuthService auth = (AuthService) x.get("auth");
 
     for (AccountWithDetailModel account : accounts) {
       for (String field : RESTRICTED_FIELDS) {
-        if (!auth.check(getX(), "AccountWithDetailModel.read." + field)) {
+        if (!auth.check(x, "AccountWithDetailModel.read." + field)) {
           reset(account, field);
         }
       }
