23b52e4ed30ec6a0c43d011f9fa0f2e209bf9647
Henry Han <henry.han@usask.ca>
2018-12-14 12:11:34 -0500

    Do not show the date in the invoice history if the newValue of the paymentDate is not undefined

--- nanopay/src/net/nanopay/invoice/ui/history/InvoiceStatusHistoryItemView.js
+++ nanopay/src/net/nanopay/invoice/ui/history/InvoiceStatusHistoryItemView.js
@@ -133,10 +133,9 @@ foam.CLASS({
     function outputRecord(parentView, record) {
       var self = this;
       var attributes = this.getAttributes(record);
-      var hasDisplayDate = record.updates.some(u => u.name === 'paymentDate');
-      var displayDate = hasDisplayDate
-        ? new Date(record.updates.find(u => u.name === 'paymentDate').newValue)
-        : null;
+      var update = record.updates.find((u) => u.name === 'paymentDate');
+      var hasDisplayDate = update && update.newValue != null;
+      var displayDate = hasDisplayDate ? new Date(update.newValue) : null;
 
       return parentView
         .addClass(this.myClass())
@@ -148,7 +147,7 @@ foam.CLASS({
           .start('div')
             .style({ 'padding-left': '30px' })
             .start('span').addClass('statusTitle')
-              .add("Invoice has been marked as ", )
+              .add('invoice has been marked as ', )
             .end()
             .start('div').addClass(attributes.labelDecoration)
               .start('span').add(attributes.labelText)
