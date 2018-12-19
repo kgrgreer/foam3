0b13dae8e002d771e3479f597c7e581fbe771666
Troy Wolters <troy@nanopay.net>
2018-12-19 16:16:30 -0500

    Fix bug where contact dropdown wouldn't be populated

--- nanopay/src/net/nanopay/contacts/ContactController.js
+++ nanopay/src/net/nanopay/contacts/ContactController.js
@@ -68,7 +68,7 @@ foam.CLASS({
                 if ( self.hasPassedCompliance() ) {
                   X.menuDAO.find('sme.quickAction.request').then((menu) => {
                     menu.handler.view = Object.assign(menu.handler.view, {
-                      invoice: self.Invoice.create({ payerId: this.id }),
+                      invoice: self.Invoice.create({ contactId: this.id }),
                       isPayable: false
                     });
                     menu.launch(X, X.controllerView);
@@ -82,7 +82,7 @@ foam.CLASS({
                 if ( self.hasPassedCompliance() ) {
                   X.menuDAO.find('sme.quickAction.send').then((menu) => {
                     menu.handler.view = Object.assign(menu.handler.view, {
-                      invoice: self.Invoice.create({ payeeId: this.id }),
+                      invoice: self.Invoice.create({ contactId: this.id }),
                       isPayable: true
                     });
                     menu.launch(X, X.controllerView);
