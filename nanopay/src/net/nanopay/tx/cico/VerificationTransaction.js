82846a314e199df7438fd7ff8760e33b485189b3
penzital <penzital888@gmail.com>
2018-12-27 22:46:33 -0500

    declined verification txn notification

--- nanopay/src/net/nanopay/tx/cico/VerificationTransaction.js
+++ nanopay/src/net/nanopay/tx/cico/VerificationTransaction.js
@@ -3,4 +3,31 @@ foam.CLASS({
   name: 'VerificationTransaction',
   extends: 'net.nanopay.tx.model.Transaction',
 
+  javaImports: [
+    'foam.dao.DAO',
+    'foam.nanos.notification.Notification',
+    'net.nanopay.tx.model.Transaction',
+    'net.nanopay.tx.model.TransactionStatus'
+  ],
+
+  methods: [
+    {
+      name: 'sendReverseNotification',
+      args: [
+        { name: 'x', javaType: 'foam.core.X' },
+        { name: 'oldTxn', javaType: 'net.nanopay.tx.model.Transaction' }
+      ],
+      javaCode: `
+      if ( getStatus() != TransactionStatus.DECLINED ) return;
+      DAO notificationDAO = ((DAO) x.get("notificationDAO"));
+      Notification notification = new Notification();
+      notification.setEmailIsEnabled(true);
+      notification.setBody("Verification transaction id: " + getId() + " was declined.");
+      notification.setNotificationType("Verification transaction declined");
+      notification.setGroupId("support");
+      notificationDAO.put(notification);
+
+      `
+    }
+  ]
 });
