9065051e1f062a6e6570087edc5e127ebeb49354
waseemchamaaa <waseemchamaa6@gmail.com>
2019-01-14 16:11:14 -0500

    got rid of https

--- nanopay/src/net/nanopay/contacts/PreventDuplicateContactEmailDAO.java
+++ nanopay/src/net/nanopay/contacts/PreventDuplicateContactEmailDAO.java
@@ -10,6 +10,7 @@ import foam.nanos.auth.AuthenticationException;
 import foam.nanos.auth.User;
 import foam.util.SafetyUtil;
 import net.nanopay.integration.quick.model.QuickContact;
+import net.nanopay.integration.xero.model.XeroContact;
 
 import java.util.List;
 
@@ -56,7 +57,7 @@ public class PreventDuplicateContactEmailDAO extends ProxyDAO {
 
     if ( data.size() == 1 ) {
       Contact existingContact = (Contact) data.get(0);
-      if ( existingContact.getId() != toPut.getId() && ! ( existingContact instanceof QuickContact ) ) {
+      if ( existingContact.getId() != toPut.getId() && ! ( existingContact instanceof QuickContact ) && ! ( existingContact instanceof XeroContact ) ) {
         throw new RuntimeException("You already have a contact with that email address.");
       }
     }
