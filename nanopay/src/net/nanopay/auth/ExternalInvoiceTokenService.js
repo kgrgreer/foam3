0748fb6c9ab69bd38ddb36d51076a293ef56ebdf
Troy Wolters <troy@nanopay.net>
2019-01-14 16:09:24 -0500

    Refactoring

--- nanopay/src/net/nanopay/auth/ExternalInvoiceTokenService.js
+++ nanopay/src/net/nanopay/auth/ExternalInvoiceTokenService.js
@@ -85,7 +85,10 @@ foam.CLASS({
         token = (Token) tokenDAO.put(token);
 
         // Determines email template to be sent based on status.
-        if ( invoice.getStatus().equals(InvoiceStatus.PAID) || invoice.getStatus().equals(InvoiceStatus.PENDING_ACCEPTANCE) ) {
+        if (
+          invoice.getStatus() == InvoiceStatus.PAID ||
+          invoice.getStatus() == InvoiceStatus.PENDING_ACCEPTANCE
+        ) {
           emailTemplate = "external-invoice-payment";
         } else {
           // If the external userId is equal to payeeId, then it is a payable
