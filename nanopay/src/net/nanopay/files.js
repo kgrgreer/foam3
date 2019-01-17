74e6dc41bb7feeb6a5c1ab800308f370104c53f1
Troy Wolters <troy@nanopay.net>
2019-01-17 08:44:39 -0500

    Delete ReadDisabledUserDAO
    It was created specifically to not show deleted or frozen (disabled)
    businesses in the select business view in Ablii, but requirements
    changed after this was implemented. The new requirements state that
    frozen businesses should show up in the list but in a disabled state
    instead of not showing up in the list at all. Since that can be
    accomplished using an mlang, this decorator can be removed from the
    chain of decorators on localUserDAO and since it's not reusable, it can
    be deleted.

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -735,7 +735,6 @@ FOAM_FILES([
   { name: 'net/nanopay/meter/AdditionalDocumentsUpdatedIpHistoryDAO' },
   { name: 'net/nanopay/meter/SigningOfficerAssignedIpHistoryDAO' },
   { name: 'net/nanopay/meter/Report' },
-  { name: 'net/nanopay/meter/ReadDisabledUserDAO' },
   { name: 'net/nanopay/meter/LogoutDisabledBusinessAgentsDAO' },
   { name: 'net/nanopay/meter/SkipNullReferencedPropertyDAO' },
   { name: 'net/nanopay/meter/BusinessStatusContactDAO' },
