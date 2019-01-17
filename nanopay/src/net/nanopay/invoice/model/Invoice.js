407d5e2efca48a672ef7d625f220d555b26b5a23
waseemchamaaa <waseemchamaa6@gmail.com>
2019-01-17 11:16:01 -0500

    added check if buisnessid is 0

--- nanopay/src/net/nanopay/invoice/model/Invoice.js
+++ nanopay/src/net/nanopay/invoice/model/Invoice.js
@@ -483,23 +483,27 @@ foam.CLASS({
         if ( ! isPayeeIdGiven && ! isInvoiceToContact ) {
           throw new IllegalStateException("Payee id must be an integer greater than zero.");
         } else {
-            if ( isPayeeIdGiven ) {
-              User payee = (User) bareUserDAO.find(this.getPayeeId());
-              if ( payee == null ) {
-                throw new IllegalStateException("No user, contact, or business with the provided payeeId exists.");
-              }
-            }
+          User payee = (User) bareUserDAO.find(
+            isPayeeIdGiven ? this.getPayeeId() : contact.getBusinessId());
+          if ( payee == null && contact.getBusinessId() != 0 ) {
+            throw new IllegalStateException("No user, contact, or business with the provided payeeId exists.");
+          }
+          if ( payee != null && SafetyUtil.equals(payee.getStatus(), AccountStatus.DISABLED) ) {
+            throw new IllegalStateException("Payee user is disabled.");
+          }
         }
 
         if ( ! isPayerIdGiven && ! isInvoiceToContact  ) {
           throw new IllegalStateException("Payer id must be an integer greater than zero.");
         } else {
-            if ( isPayerIdGiven ) {
-              User payer = (User) bareUserDAO.find(this.getPayerId());
-              if ( payer == null ) {
-                throw new IllegalStateException("No user, contact, or business with the provided payerId exists.");
-              }
-            }
+          User payer = (User) bareUserDAO.find(
+            isPayerIdGiven ? this.getPayerId() : contact.getBusinessId());
+          if ( payer == null && contact.getBusinessId() != 0) {
+            throw new IllegalStateException("No user, contact, or business with the provided payerId exists.");
+          }
+          if ( payer != null && SafetyUtil.equals(payer.getStatus(), AccountStatus.DISABLED) ) {
+            throw new IllegalStateException("Payer user is disabled.");
+          }
         }
       `
     }
