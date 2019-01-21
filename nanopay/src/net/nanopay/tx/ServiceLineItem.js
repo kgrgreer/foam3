8ab37ddbeb0f39f5055e931eaee1e6d5f16d5b55
penzital <penzital888@gmail.com>
2019-01-21 17:23:24 -0500

    cleanup

--- nanopay/src/net/nanopay/tx/ServiceLineItem.js
+++ nanopay/src/net/nanopay/tx/ServiceLineItem.js
@@ -1,8 +1,3 @@
-/**
- * @license
- * Copyright 2018 The FOAM Authors. All Rights Reserved.
- * http://www.apache.org/licenses/LICENSE-2.0
- */
 foam.CLASS({
   package: 'net.nanopay.tx',
   name: 'ServiceLineItem',
