c14677615b742349aaa3e96424af8424a13c4e0b
Patrick Zanowski <zahnno@gmail.com>
2019-01-23 15:59:23 -0500

    Added company name to PAD capture and relevant form

--- nanopay/src/net/nanopay/cico/ui/bankAccount/modalForm/CABankPADForm.js
+++ nanopay/src/net/nanopay/cico/ui/bankAccount/modalForm/CABankPADForm.js
@@ -203,7 +203,8 @@ foam.CLASS({
           agree3: this.viewData.agree3,
           institutionNumber: this.bank.institutionNumber,
           branchId: this.bank.branchId, // branchId = transit number
-          accountNumber: this.bank.accountNumber
+          accountNumber: this.bank.accountNumber,
+          companyName: this.viewData.padCompanyName
         }));
         this.bank.address = user.address;
         this.bank = await this.bankAccountDAO.put(this.bank);
