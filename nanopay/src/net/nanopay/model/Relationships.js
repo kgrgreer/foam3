148b8a54096038026b123f8b680465c7386a69fd
Kenny Kan <kanqiyen@gmail.com>
2019-01-23 11:29:08 -0500

    Added table cell formatter to institution relationship to display the correct institution number rather than our system id

--- nanopay/src/net/nanopay/model/Relationships.js
+++ nanopay/src/net/nanopay/model/Relationships.js
@@ -23,7 +23,18 @@ foam.RELATIONSHIP({
   targetModel: 'net.nanopay.bank.BankAccount',
   forwardName: 'bankAccounts',
   inverseName: 'institution',
-  cardinality: '1:*'
+  cardinality: '1:*',
+  targetProperty: {
+    tableCellFormatter: function(value, obj, axiom) {
+      var self = this;
+      this.__subSubContext__.institutionDAO.find(value)
+      .then( function( institution ) {
+        self.add(institution.institutionNumber);
+      }).catch( function( error ) {
+        self.add(error);
+      });
+    }
+  }
 });
 
 foam.RELATIONSHIP({
@@ -382,48 +393,4 @@ foam.RELATIONSHIP({
   inverseName: 'parent',
   sourceProperty: { view: { class: 'foam.u2.view.ReferenceView', placeholder: '--' } },
   targetProperty: { view: { class: 'foam.u2.view.ReferenceView', placeholder: '--' } }
-});
-
-foam.RELATIONSHIP({
-  sourceModel: 'foam.nanos.auth.ServiceProvider',
-  targetModel: 'net.nanopay.tx.model.TransactionFee',
-  forwardName: 'transactionFees',
-  inverseName: 'spid',
-  cardinality: '1:*',
-  targetProperty: {
-    hidden: true
-  }
-});
-
-foam.RELATIONSHIP({
-  sourceModel: 'foam.nanos.auth.ServiceProvider',
-  targetModel: 'net.nanopay.tx.LineItemType',
-  forwardName: 'lineItemTypes',
-  inverseName: 'spid',
-  cardinality: '1:*',
-  targetProperty: {
-    hidden: true
-  }
-});
-
-foam.RELATIONSHIP({
-  sourceModel: 'foam.nanos.auth.ServiceProvider',
-  targetModel: 'net.nanopay.tx.LineItemFee',
-  forwardName: 'lineItemFees',
-  inverseName: 'spid',
-  cardinality: '1:*',
-  targetProperty: {
-    hidden: true
-  }
-});
-
-foam.RELATIONSHIP({
-  sourceModel: 'foam.nanos.auth.ServiceProvider',
-  targetModel: 'net.nanopay.tax.LineItemTax',
-  forwardName: 'lineItemTax',
-  inverseName: 'spid',
-  cardinality: '1:*',
-  targetProperty: {
-    hidden: true
-  }
 });
