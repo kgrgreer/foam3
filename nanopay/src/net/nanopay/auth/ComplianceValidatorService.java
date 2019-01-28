3ed2a9954e97dcb27b7f80d86d1ffbd17dd2338b
blakegreen <blake@nanopay.net>
2019-01-28 12:10:13 -0500

    Added ComplianceValidatorSercice

---
+++ nanopay/src/net/nanopay/auth/ComplianceValidatorService.java
@@ -0,0 +1,13 @@
+package net.nanopay.auth;
+
+import foam.core.FObject;
+import foam.nanos.auth.UserAndGroupAuthService;
+import foam.core.X;
+
+public class ComplianceValidatorService extends UserAndGroupAuthService {
+
+  public ComplianceValidatorService(X x) {
+    super(x);
+  }
+
+}
