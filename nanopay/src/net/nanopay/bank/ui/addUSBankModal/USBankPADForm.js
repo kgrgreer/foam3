f2f79e2f6de62bdbfc85c9bb1bb640391b22023a
Troy Wolters <troy@nanopay.net>
2019-01-28 08:49:03 -0500

    Fix bug where company name wasn't being saved for US bank accounts

--- nanopay/src/net/nanopay/bank/ui/addUSBankModal/USBankPADForm.js
+++ nanopay/src/net/nanopay/bank/ui/addUSBankModal/USBankPADForm.js
@@ -199,7 +199,8 @@ foam.CLASS({
           agree1: this.viewData.agree1,
           agree2: this.viewData.agree2,
           branchId: this.bank.branchId,
-          accountNumber: this.bank.accountNumber
+          accountNumber: this.bank.accountNumber,
+          companyName: this.viewData.companyName
         }));
         this.bank.address = user.address;
         this.bank = await this.bankAccountDAO.put(this.bank);
