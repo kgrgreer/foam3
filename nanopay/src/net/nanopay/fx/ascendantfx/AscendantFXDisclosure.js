1aba0b3e9732893f413d16e56f5de33a7700a8a7
Greenspot <mayowa.olurin@gmail.com>
2019-01-29 12:25:01 +0100

    Modify Disclosures. added TransactionType

--- nanopay/src/net/nanopay/fx/ascendantfx/AscendantFXDisclosure.js
+++ nanopay/src/net/nanopay/fx/ascendantfx/AscendantFXDisclosure.js
@@ -6,20 +6,4 @@ foam.CLASS({
 
   documentation: `Hold Ascendant FX specific disclosures`,
 
-  properties: [
-    {
-      class: 'Reference',
-      of: 'foam.nanos.auth.Country',
-      name: 'country',
-      documentation: 'For Country specific disclosures,'
-    },
-    {
-      class: 'Reference',
-      of: 'foam.nanos.auth.Region',
-      name: 'state',
-      documentation: 'For State/Province/Region specific disclosures'
-    },
-
-  ],
-
 });
