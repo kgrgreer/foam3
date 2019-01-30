d7b77b70f58a472ce2a085cda33ef7e42f413943
blakegreen <blake@nanopay.net>
2019-01-30 12:28:23 -0500

    Moved blacklistDAO and BlacklistEntityType to nanopay repo

---
+++ nanopay/src/net/nanopay/auth/BlacklistEntityType.js
@@ -0,0 +1,14 @@
+foam.ENUM({
+  package: 'net.nanopay.auth',
+  name: 'BlacklistEntityType',
+  values: [
+    {
+      name: 'USER',
+      label: 'User'
+    },
+    {
+      name: 'BUSINESS',
+      label: 'Business'
+    }
+  ]
+});
