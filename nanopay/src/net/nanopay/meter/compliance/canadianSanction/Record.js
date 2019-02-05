b41e981745300052eb16bc1d99dc5fe632963c1d
Chanmann Lim <chanmannlim@gmail.com>
2019-02-05 16:44:43 -0500

    Format code

--- nanopay/src/net/nanopay/meter/compliance/canadianSanction/Record.js
+++ nanopay/src/net/nanopay/meter/compliance/canadianSanction/Record.js
@@ -1,57 +1,57 @@
 foam.CLASS({
-	package: 'net.nanopay.meter.compliance.canadianSanction',
-	name: 'Record',
+  package: 'net.nanopay.meter.compliance.canadianSanction',
+  name: 'Record',
 
-	properties: [
-		{
-			class: 'Long',
-			name: 'id'
-		},
-		{
-			class: 'String',
-			name: 'Country'
-		},
-		{
-			class: 'String',
-			name: 'Entity'
-		},
-		{
-			class: 'String',
-			name: 'Title'
-		},
-		{
-			class: 'String',
-			name: 'LastName'
-		},
-		{
-			class: 'String',
-			name: 'GivenName'
-		},
-		{
-			class: 'String',
-			name: 'Aliases'
-		},
-		{
-			class: 'String',
-			name: 'DateOfBirth'
-		},
-		{
-			class: 'String',
-			name: 'Schedule'
-		},
-		{
-			class: 'Short',
-			name: 'Item'
-		}
-	],
+  properties: [
+    {
+      class: 'Long',
+      name: 'id'
+    },
+    {
+      class: 'String',
+      name: 'Country'
+    },
+    {
+      class: 'String',
+      name: 'Entity'
+    },
+    {
+      class: 'String',
+      name: 'Title'
+    },
+    {
+      class: 'String',
+      name: 'LastName'
+    },
+    {
+      class: 'String',
+      name: 'GivenName'
+    },
+    {
+      class: 'String',
+      name: 'Aliases'
+    },
+    {
+      class: 'String',
+      name: 'DateOfBirth'
+    },
+    {
+      class: 'String',
+      name: 'Schedule'
+    },
+    {
+      class: 'Short',
+      name: 'Item'
+    }
+  ],
 
-	axioms: [
-		{
-			buildJavaClass: function (cls) {
-				cls.extras.push(`
-					public static String datasetChecksum = "";
+  axioms: [
+    {
+      buildJavaClass: function (cls) {
+        cls.extras.push(`
+          public static String datasetChecksum = "";
         `);
-			},
-		},
-	]
+      },
+    },
+  ]
 });
