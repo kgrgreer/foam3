37550d027d0bf2c8800e4a7b795cec55c6739aae
Joel Hughes <joel@nanopay.net>
2019-02-06 08:53:15 -0500

    Update Accounts migration script to handle unhashed CABankAccounts

--- tools/journal_migration/Accounts_01.sh
+++ tools/journal_migration/Accounts_01.sh
@@ -4,5 +4,12 @@
 
 export JOURNAL_HOME=/opt/nanopay/journals
 if [ -f "$JOURNAL_HOME/accounts" ]; then
-    perl -p -i -e 's/enabled\":false/deleted\":true/g;' "$JOURNAL_HOME"/accounts
+    perl -p -i -e 's/(.*?)net.nanopay.account.DigitalAccount(.*?)enabled\":false(.*?)\{\"algorithm.*\}/\1net.nanopay.account.DigitalAccount\2deleted\":true\3/g;' "$JOURNAL_HOME"/accounts
+    
 fi
+if [ -f "$JOURNAL_HOME/accounts" ]; then
+    perl -p -i -e 's/(.*?)net.nanopay.bank.CABankAccount(.*?)enabled\":false(.*?)/\1net.nanopay.bank.CABankAccount\2deleted\":true\3/g;' "$JOURNAL_HOME"/accounts
+    
+fi
+
+# /,{"algorithm":.*}//
