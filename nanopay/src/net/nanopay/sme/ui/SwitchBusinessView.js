82d52682b69161d653f0232768e048a38bd8fb60
Henry Han <henry.han@usask.ca>
2019-02-11 09:29:13 -0500

    agent.email needs to be a slot

--- nanopay/src/net/nanopay/sme/ui/SwitchBusinessView.js
+++ nanopay/src/net/nanopay/sme/ui/SwitchBusinessView.js
@@ -284,7 +284,7 @@ foam.CLASS({
           .start('div').addClass(this.myClass('current-signin'))
             .add(this.CURRENTLY_SIGNED_IN, ' ', this.user.businessName)
             .start('div').addClass(this.myClass('current-signin-email'))
-              .add(this.agent.email)
+              .add(this.agent$.email$)
             .end()
           .end()
           .start()
