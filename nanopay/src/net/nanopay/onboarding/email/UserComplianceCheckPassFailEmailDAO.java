df8b5c183d4afbc336204ef27a292f48679a322a
Nauna <anna.doulatshahi@gmail.com>
2019-02-19 07:51:41 -0500

    additive checks on user prior sending email of compliance change

--- nanopay/src/net/nanopay/onboarding/email/UserComplianceCheckPassFailEmailDAO.java
+++ nanopay/src/net/nanopay/onboarding/email/UserComplianceCheckPassFailEmailDAO.java
@@ -21,34 +21,39 @@ public class UserComplianceCheckPassFailEmailDAO
 
     @Override
     public FObject put_(X x, FObject obj) {
+      Logger logger = (Logger) getX().get("logger");
+
       // Checks if User exists.
       User user = (User) obj;
-      if ( user == null || user.getId() ==0 )
+      if ( user == null || user.getId() == 0 )
         return getDelegate().put_(x, obj);
 
+      // Checks if User has existed which is really just a duplicate check of user.getId() == 0, or at least it should.
       User oldUser = (User) getDelegate().find(user.getId());
+      if ( oldUser == null || oldUser.getId() == 0 )
+        return getDelegate().put_(x, obj);
 
       // Make sure to only send on complaince status change from PASSED or FAILED
-      if ( ! ComplianceStatus.REQUESTED.equals(oldUser.getCompliance()) &&
+      if ( ! ComplianceStatus.REQUESTED.equals(oldUser.getCompliance()) ||
        ! ( ComplianceStatus.PASSED.equals(user.getCompliance()) || ComplianceStatus.FAILED.equals(user.getCompliance()) ) )
         return getDelegate().put_(x, obj);
 
-      user = (User) super.put_(x , obj);
-      // user sending the email
-      // User                    admin        = (User) getDelegate().find(user.getInvitedBy());
-      
+      if ( ! user.getEmailVerified() )
+        logger.error("user(id=" + user.getId() + ") has had Compliance status changed to " + user.getCompliance() + " but is unable to be notified due to user's email is not yet verified.");
+
+      user = (User) getDelegate().put_(x , obj);
       EmailService            email        = (EmailService) x.get("email");
       EmailMessage            message      = new EmailMessage();
       HashMap<String, Object> args         = new HashMap<>();
 
       message.setTo(new String[]{user.getEmail()});
       args.put("business",        user.label());
-      args.put("statusUpdate",    (ComplianceStatus.PASSED.equals(user.getCompliance()) ? "PASSED all checks!" : "unfortately not passed all checks"));
+      args.put("statusUpdate",    (ComplianceStatus.PASSED.equals(user.getCompliance()) ? "PASSED all checks!" : "unfortunately not passed all checks"));
 
       try {
         email.sendEmailFromTemplate(x, user, message, "compliance-Notification-to-user", args);
-      } catch (Throwable t) {
-        (x.get(Logger.class)).error("Error sending compliance-Notification-to-user email.", t);
+      } catch (Exception e) {
+        logger.error("Error sending compliance-Notification-to-user email.", e);
       }
 
       return user;
