cf54f8974a4601307603bf603c41e51dbf92dd8c
Kevin Greer <kgrgreer@gmail.com>
2019-02-26 13:25:10 -0500

    improve account summary view

--- nanopay/src/net/nanopay/account/ui/AccountSummaryView.js
+++ nanopay/src/net/nanopay/account/ui/AccountSummaryView.js
@@ -31,38 +31,63 @@ foam.CLASS({
           data: this.accountDAO
         }, 'Table'],
         [
+          /*
+            1. Aggregate and Shadow accounts have a clear background.
+            2. Aggregate and zero balance accounts have a gray circle, otherwise green
+            3. currency alphabetic code is displayed after account type
+            4. aggregate account balances are shown in gray non-bold, otherwise black & bold
+            5. open-closed indicator shown for accounts with children
+          */
           function() { return {
             class: 'net.nanopay.account.ui.AccountTreeGraph',
             formatNode: function() {
               var isShadow = this.data.name.indexOf('Shadow') != -1;
-              if ( isShadow ) { this.color = '#ddd'; }
-              // Account Name
-              this.add(this.Label.create({color: 'black', x: -this.width/2+14, y: 7, text: this.data.name, font: 'bold 12px sans-serif'}));
+              var leftPos  = -this.width/2+8;
+              var type     = this.data.type.replace('Account', '');
 
-              // Account Type
-              var type = this.data.type.replace('Account', '');
-              if ( type == 'Digital' ) type = 'Virtual';
-              this.add(this.Label.create({color: 'gray',  x: -this.width/2+14, y: this.height-22, text: type}));
+              if ( isShadow || type == 'Aggregate' ) { this.color = '#edf0f5'; }
+              // Account Name
+              this.add(this.Label.create({color: 'black', x: leftPos, y: 7, text: this.data.name, font: 'bold 12px sans-serif'}));
 
               // Balance and Denomination Indicator
               this.data.findBalance(this.__subContext__).then(function(balance) {
                 this.__subContext__.currencyDAO.find(this.data.denomination).then(function(denom) {
                   var c = denom.color;
                   this.add(this.Line.create({
-                    startX: -this.width/2+7,
-                    startY: 5,
-                    endX: -this.width/2+7,
-                    endY: this.height-5,
+                    startX: -this.width/2+1,
+                    startY: 0,
+                    endX: -this.width/2+1,
+                    endY: this.height,
                     color: c,
                     lineWidth: 4
                   }));
+                  /*
+                  this.add(this.Line.create({
+                    startX: -this.width/2+1,
+                    startY: 0,
+                    endX: -this.width/2+1,
+                    endY: this.height*.6,
+                    color: 'rgba(255,255,255,0.5)',
+                    lineWidth: 4
+                  }));
+                  */
 
-                  this.add(this.Label.create({color: 'gray',  x: this.width/2-4,  y: this.height-22, align: 'end', text: denom.format(balance)}));
+                  var circleColour = balance && ! type == 'Aggregate' ? '#080' : '#ddd';
+                  this.add(foam.graphics.Circle.create({color: circleColour, x: this.width/2-14, y: this.height-14, radius: 5, border: null}));
+
+                  // Account Type
+                  if ( type == 'Digital' ) type = 'Virtual';
+                  this.add(this.Label.create({color: 'gray',  x: leftPos, y: 22, text: type + ' (' + denom.alphabeticCode + ')'}));
+
+                  var balanceColour = type == 'Aggregate' ? 'gray' : 'black';
+                  var balanceFont   = type == 'Aggregate' ? '12px sans-serif' : 'bold 12px sans-serif';
+                  this.add(this.Label.create({color: balanceColour, font: balanceFont, x: leftPos,  y: this.height-21, text: denom.format(balance)}));
                 }.bind(this));
               }.bind(this));
 
             },
-            nodeWidth: 165,
+            nodeWidth: 185,
+            nodeHeight: 80,
             padding: 10,
             width: 1350,
             height: 1000,
