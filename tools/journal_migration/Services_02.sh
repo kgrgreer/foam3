4dd358d1dca2993c53c3522130ee4087b8f23442
Joel Hughes <joel@nanopay.net>
2019-03-11 16:58:33 -0400

    Update paths to images, error files, and index.html
    Strip image paths, now all relative to static root "/images"
    Strip error file paths, and change to use errorPage.html
    Strip paths to index.html files. Also ablii index.html is now ablii.html and similarly merchant index.html is not merchant.html
    
    CPF-560 #time 2h

---
+++ tools/journal_migration/Services_02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# correct paths in services for jar file deployment
+
+export JOURNAL_HOME=/opt/nanopay/journals
+if [ -f "$JOURNAL_HOME/services" ]; then
+    perl -p -i -e 's/pathSpec\":\"\/images\/\*\",initParameters:\{\"paths\":\".*\"\}\},/pathSpec\":\"\/images\/\*\",initParameters:\{\"paths\":\"images\"\}\},/g;' "$JOURNAL_HOME"/services
+fi
+
+if [ -f "$JOURNAL_HOME/services" ]; then
+    perl -p -i -e 's/\/nanopay\/src\/net\/nanopay\/error.html/\/errorPage.html/g;' "$JOURNAL_HOME"/services
+fi
+
+if [ -f "$JOURNAL_HOME/services" ]; then
+    perl -p -i -e 's/\/nanopay\/src\/net\/nanopay\/index.html/\/index.html/g; s/\/nanopay\/src\/net\/nanopay\/sme\/index.html/\/ablii.html/g; s/\/nanopay\/src\/net\/nanopay\/merchant\/index.html/\/merchant.html/g;' "$JOURNAL_HOME"/services
+fi
