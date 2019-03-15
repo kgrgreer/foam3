d30256122feefb6c6fad375393bb85f960350bc7
Michal Pasternak <michal.pasternak@nanopay.net>
2019-03-15 13:35:00 -0400

    make proposed changes

--- nanopay/src/net/nanopay/tx/cron/CompoundInterest.java
+++ nanopay/src/net/nanopay/tx/cron/CompoundInterest.java
@@ -1,11 +1,15 @@
 package net.nanopay.tx.cron;
 
 import foam.core.ContextAgent;
+import foam.core.Detachable;
 import foam.core.X;
 import foam.dao.ArraySink;
+import foam.dao.AbstractSink;
 import foam.dao.DAO;
 import net.nanopay.account.LoanAccount;
 import java.util.List;
+
+import static foam.mlang.MLang.INSTANCE_OF;
 
 /**
  Cronjob applies the amount of interest that each account owes
@@ -16,16 +20,23 @@ public class CompoundInterest implements ContextAgent {
 
   @Override
   public void execute(X x) {
-    List Accounts = ((ArraySink) ((DAO) x.get("accountDAO")).select(new ArraySink())).getArray();
-    for ( Object obj : Accounts ) {
-      if ( obj instanceof LoanAccount ){
-        LoanAccount la = (LoanAccount) ((LoanAccount) obj).fclone();
-        if (la.getAccruedInterest()>0){
-          la.compound(x);
-          ((DAO) x.get("accountDAO")).put_(x,la);
+    DAO accountDAO = (DAO) x.get("accountDAO");
+
+    accountDAO
+      .where(
+        INSTANCE_OF(LoanAccount.class)
+      )
+      .select(new AbstractSink(){
+        @Override
+        public void put(Object o, Detachable d ) {
+          LoanAccount la = (LoanAccount) ((LoanAccount) o).deepClone();
+          if ( la.getAccruedInterest() > 0 ){
+            la.compound(x);
+            accountDAO.put_(x,la);
+          }
         }
       }
-    }
+      );
   }
 }
 
