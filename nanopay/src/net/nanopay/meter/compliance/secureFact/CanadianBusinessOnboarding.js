2f1501b794ad397aa3140a4116661675dd3d653c
Chanmann Lim <chanmann@nanopay.net>
2019-03-20 15:57:24 -0400

    Update Securefact rules for onboarding flow
    CPF-68 #time 30m

--- nanopay/src/net/nanopay/meter/compliance/secureFact/CanadianBusinessOnboarding.js
+++ nanopay/src/net/nanopay/meter/compliance/secureFact/CanadianBusinessOnboarding.js
@@ -1,12 +1,13 @@
 foam.CLASS({
   package: 'net.nanopay.meter.compliance.secureFact',
-  name: 'CanadianUserPredicate',
+  name: 'CanadianBusinessOnboarding',
   extends: 'foam.mlang.predicate.AbstractPredicate',
   implements: ['foam.core.Serializable'],
 
   javaImports: [
     'foam.nanos.auth.Address',
     'foam.nanos.auth.User',
+    'net.nanopay.admin.model.ComplianceStatus',
     'net.nanopay.model.Business',
     'static foam.mlang.MLang.*',
   ],
@@ -16,9 +17,10 @@ foam.CLASS({
       name: 'f',
       javaCode: `
         return AND(
-          EQ(DOT(NEW_OBJ, INSTANCE_OF(User.getOwnClassInfo()))    , true),
-          EQ(DOT(NEW_OBJ, INSTANCE_OF(Business.getOwnClassInfo())), false),
-          EQ(DOT(DOT(NEW_OBJ, User.ADDRESS), Address.COUNTRY_ID)  , "CA")
+          EQ(DOT(NEW_OBJ, INSTANCE_OF(Business.getOwnClassInfo())), true),
+          EQ(DOT(DOT(NEW_OBJ, User.ADDRESS), Address.COUNTRY_ID)  , "CA"),
+          EQ(DOT(NEW_OBJ, User.COMPLIANCE), ComplianceStatus.REQUESTED),
+          NEQ(DOT(OLD_OBJ, User.COMPLIANCE), ComplianceStatus.REQUESTED)
         ).f(obj);
       `
     }
