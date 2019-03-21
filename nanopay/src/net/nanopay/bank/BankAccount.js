39e97ce8a358244282c711dbb39d76414d10e6dd
Henry Han <henry.han@usask.ca>
2019-03-21 17:31:14 -0400

    Show proper valid msg when account number is empty

--- nanopay/src/net/nanopay/bank/BankAccount.js
+++ nanopay/src/net/nanopay/bank/BankAccount.js
@@ -67,7 +67,9 @@ foam.CLASS({
       validateObj: function(accountNumber) {
         var accNumberRegex = /^[0-9]{1,30}$/;
 
-        if ( ! accNumberRegex.test(accountNumber) ) {
+        if ( accountNumber === '' ) {
+          return 'Routing number required.';
+        } else if ( ! accNumberRegex.test(accountNumber) ) {
           return 'Invalid account number.';
         }
       }
