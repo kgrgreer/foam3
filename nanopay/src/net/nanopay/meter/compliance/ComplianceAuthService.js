28fdd4edbe484c978b637091095033ec4f7250b7
blakegreen <blake@nanopay.net>
2019-03-21 13:33:18 -0400

    Using find with predicate to fetch blacklists instead of where with predicate

--- nanopay/src/net/nanopay/meter/compliance/ComplianceAuthService.js
+++ nanopay/src/net/nanopay/meter/compliance/ComplianceAuthService.js
@@ -45,31 +45,28 @@ foam.CLASS({
         Permission permission_ = new AuthPermission(permission);
     
         if ( ! complianceService.checkUserCompliance(x) ) {
-          ArraySink sink   = (ArraySink) ((DAO) getBlacklistDAO()).where(
+          Group userBlacklist   = (Group) ((DAO) getBlacklistDAO()).find(
             MLang.EQ(Blacklist.ENTITY_TYPE, BlacklistEntityType.USER)
-          ).select(new ArraySink());
-          assert sink.getArray().size() >= 1 : "User compliance blacklist does not exist";
-          Group userBlacklist = (Group) sink.getArray().get(0);
+          );
+          assert userBlacklist != null : "User compliance blacklist does not exist";
           if ( userBlacklist.implies(x, permission_) ) {
             throw new AuthorizationException();
           }
         }
         if ( ! complianceService.checkBusinessCompliance(x) ) {
-          ArraySink sink = (ArraySink) ((DAO) getBlacklistDAO()).where(
+          Group businessBlacklist = (Group) ((DAO) getBlacklistDAO()).find(
             MLang.EQ(Blacklist.ENTITY_TYPE, BlacklistEntityType.BUSINESS)
-          ).select(new ArraySink());
-          assert sink.getArray().size() >= 1 : "Business compliance blacklist does not exist";
-          Group businessBlacklist = (Group) sink.getArray().get(0);
+          );
+          assert businessBlacklist != null : "Business compliance blacklist does not exist";
           if ( businessBlacklist.implies(x, permission_) ) {
             throw new AuthorizationException();
           }
         }
         if ( ! complianceService.checkAccountCompliance(x) ) {
-          ArraySink sink = (ArraySink) ((DAO) getBlacklistDAO()).where(
+          Group accountBlacklist = (Group) ((DAO) getBlacklistDAO()).find(
             MLang.EQ(Blacklist.ENTITY_TYPE, BlacklistEntityType.ACCOUNT)
-          ).select(new ArraySink());
-          assert sink.getArray().size() >= 1 : "Account compliance blacklist does not exist";
-          Group accountBlacklist = (Group) sink.getArray().get(0);
+          );
+          assert accountBlacklist != null : "Account compliance blacklist does not exist";
           if ( accountBlacklist.implies(x, permission_) ) {
             throw new AuthorizationException();
           }
