fd2291a0b27a036f91d001a1dc4ca40f1646d0bd
Danny Tharma <daniel@nanopay.net>
2019-03-27 14:45:15 -0400

    CPF-736 #comment name refactored the AbliiContactBankAccountAutoverifyDAO to AbliiBankAccountAutoverifyDAO to account for both autoverifying Contact Bank Accounts and US Bank accounts #time 10m

--- nanopay/src/net/nanopay/account/AbliiBankAccountAutoverifyDAO.java
+++ nanopay/src/net/nanopay/account/AbliiBankAccountAutoverifyDAO.java
@@ -10,14 +10,14 @@ import net.nanopay.contacts.Contact;
 import net.nanopay.bank.BankAccountStatus;
 
 // we only need a put_ override in this decorator, since it only deals with the CREATION of contact bank accounts
-public class AbliiContactBankAccountAutoverifyDAO
+public class AbliiBankAccountAutoverifyDAO
    extends ProxyDAO
 {
- public AbliiContactBankAccountAutoverifyDAO(DAO delegate) {
+ public AbliiBankAccountAutoverifyDAO(DAO delegate) {
    setDelegate(delegate);
  }
 
- public AbliiContactBankAccountAutoverifyDAO(X x, DAO delegate) {
+ public AbliiBankAccountAutoverifyDAO(X x, DAO delegate) {
    setX(x);
    setDelegate(delegate);
  }
@@ -25,6 +25,7 @@ public class AbliiContactBankAccountAutoverifyDAO
  @Override
  public FObject put_(X x, FObject obj) {
     /**
+     * 1. CONTACT BANK ACCOUNTS
      * In order to check if obj entails a Contact's bank account being added
      * We will first check if the userId is different from the bankAccountOwnerId
      * If it is then we can proceed to check if the bankAccountOwner is of type Contact
@@ -35,6 +36,9 @@ public class AbliiContactBankAccountAutoverifyDAO
      * this autoverified bank account WILL NOT CARRY OVER to their ablii account, as they will have to still set up an
      * account and verify it using the micro-deposit even if they are using the same bank information
      * Contact Bank Accounts are exlusively meant to just RECEIVE money from ablii users
+     * 
+     * FOR US BANK ACCOUNTS
+     * 
      */
 
    User user = (User) x.get("user");
@@ -45,6 +49,7 @@ public class AbliiContactBankAccountAutoverifyDAO
    Account bankAccountObj = (Account) obj; 
    long bankAccountOwnerId = bankAccountObj.getOwner();
 
+   // 1. CONTACT BANK ACCOUNTS
    if ( userId != bankAccountOwnerId ) {
 
     // grabbing the bankAccountOwner object directly from the userDAO by looking up the bankAccountOwnerId
