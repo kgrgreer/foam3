6020bae20cff6072d64c355ab463bb5be2390e57
Joel Hughes <joel@nanopay.net>
2019-03-31 13:34:35 -0400

    Skip agent and compliance test when user is system user (id == 1)
    CPF-805 #time 2h

--- nanopay/src/net/nanopay/meter/compliance/NanopayComplianceService.js
+++ nanopay/src/net/nanopay/meter/compliance/NanopayComplianceService.js
@@ -10,6 +10,7 @@ foam.CLASS({
 
   javaImports: [
     'foam.nanos.auth.User',
+    'foam.nanos.session.Session',
     'net.nanopay.admin.model.ComplianceStatus',
     'net.nanopay.model.Business'
   ],
@@ -34,14 +35,19 @@ foam.CLASS({
       type: 'Boolean',
       javaCode: `
         User user = (User) x.get("user");
-        if ( user != null ) {
+        if ( user != null && user.getId() != 1 ) {
           User agent = (User) x.get("agent");
           foam.nanos.logger.Logger logger = (foam.nanos.logger.Logger) x.get("logger");
           if ( agent == null ) {
-//            foam.nanos.logger.Logger logger = (foam.nanos.logger.Logger) x.get("logger");
-            logger.warning(this.getClass().getSimpleName(), "'agent' not found in context, falling back to compliance check on 'user' rather than 'agent'. ");
+            Session session = x.get(Session.class);
+            agent = (User) session.getContext().get("agent");
+            if ( agent != null ) {
+              logger.debug(this.getClass().getSimpleName(), "'agent'", agent.getId(), "found in SESSION context for user", user.getId());
+            } else {
+              logger.warning(this.getClass().getSimpleName(), "'agent' NOT found in context, falling back to compliance check on 'user'", user.getId(), "rather than 'agent'. ");
+            }
           } else {
-            logger.info(this.getClass().getSimpleName(), "'agent' found in context");
+            logger.debug(this.getClass().getSimpleName(), "'agent'", agent.getId(), "found in context for user", user.getId());
           }
           if ( agent != null &&
                user instanceof Business ) {
