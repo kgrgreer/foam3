0e3872fba4ff2452700cadad63eb1ee46d56ffbf
blakegreen <blake@nanopay.net>
2019-04-03 15:53:58 -0400

    Finished setting up response models for a Dow Jones Name Search

---
+++ nanopay/src/net/nanopay/meter/compliance/dowJones/model/MatchPayload.js
@@ -0,0 +1,62 @@
+foam.CLASS({
+  package: 'net.nanopay.meter.compliance.dowJones.model',
+  name: 'MatchPayload',
+
+  documentation: 'Payload response data for a Match in the Dow Jones Risk Database.',
+
+  properties: [
+    {
+      class: 'Array',
+      of: 'String',
+      name: 'riskIcons',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'primaryName',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'countryCode',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'title',
+      visibility: 'RO'
+    },
+    {
+      class: 'Boolean',
+      name: 'subsidiary',
+      visibility: 'RO'
+    },
+    {
+      class: 'net.nanopay.meter.compliance.dowJones.model.MatchedName',
+      name: 'matchedName',
+      visibility: 'RO'
+    },
+    {
+      class: 'Date',
+      name: 'matchedDateOfBirth',
+      visibility: 'RO'
+    },
+    {
+      class: 'FObjectArray',
+      of: 'Date',
+      name: 'datesOfBirth',
+      visibility: 'RO'
+    },
+    {
+      class: 'FObjectArray',
+      of: 'foam.nanos.auth.Country',
+      name: 'countries',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'gender',
+      visibility: 'RO'
+    }
+  ]
+});
---
+++ nanopay/src/net/nanopay/meter/compliance/dowJones/model/MatchedName.js
@@ -0,0 +1,15 @@
+foam.CLASS({
+  package: 'net.nanopay.meter.compliance.dowJones.model',
+  name: 'MatchedName',
+  extends: 'String',
+
+  documentation: 'Matched name string',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'nameType',
+      visibility: 'RO'
+    }
+  ]
+});
