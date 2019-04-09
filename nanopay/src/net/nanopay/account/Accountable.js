184db01c6d696f08316bafde7042511f5d9a9df2
Michal Pasternak <michal.pasternak@nanopay.net>
2019-04-09 09:21:36 -0400

    add some comments

--- nanopay/src/net/nanopay/account/Accountable.js
+++ nanopay/src/net/nanopay/account/Accountable.js
@@ -1,17 +1,23 @@
 foam.INTERFACE({
   package: 'net.nanopay.account',
   name: 'Accountable',
-  documentation: '
-  The Accountable interface allows an object to have a balance, as well as forwards, and source funds
+  documentation: ' The Accountable interface allows an object to have a balance, as well as forwards, and source funds
   from specified accounts. Accountable objects can be thought of as decorators for other accounts.
 
   ',
   methods: [
     {
       name: 'handle',
-      documentation: 'Properly handle a transaction that either originates from, or its destination ',
-      type: 'Any',
+      documentation: 'Properly handle a transaction that either originates from, or its destination.
+       Case 1: forwarding txns: return the same txn with child txn(s)
+       Case 2: funding txns: return the same txn with parent txn(s?) set for this txn.
+       Case 3: nothing happens: return the same txn',
+      type: 'net.nanopay.tx.model.Transaction',
       args: [
+        {
+          name: 'x',
+          type: 'Context'
+        },
         {
           name: 'transaction',
           type: 'net.nanopay.tx.model.Transaction',
@@ -30,6 +36,8 @@ foam.INTERFACE({
       ]
     },
     {
+    // TODO: decide if findTrust Account makes sense in Interface. What is the trust account of User/Invoice?
+    // the trust account of final ending account of accountification chain?
       name: 'findTrust',
       type: 'net.nanopay.account.TrustAccount',
       args: [
