614dce7c911f4f52afe57af6de8807548a3876d1
Michal Pasternak <michal.pasternak@nanopay.net>
2019-04-09 09:36:21 -0400

    fix quotes

--- nanopay/src/net/nanopay/account/Accountable.js
+++ nanopay/src/net/nanopay/account/Accountable.js
@@ -1,17 +1,15 @@
 foam.INTERFACE({
   package: 'net.nanopay.account',
   name: 'Accountable',
-  documentation: ' The Accountable interface allows an object to have a balance, as well as forwards, and source funds
-  from specified accounts. Accountable objects can be thought of as decorators for other accounts.
-
-  ',
+  documentation: ` The Accountable interface allows an object to have a balance, as well as forwards, and source funds
+  from specified accounts. Accountable objects can be thought of as decorators for other accounts.`,
   methods: [
     {
       name: 'handle',
-      documentation: 'Properly handle a transaction that either originates from, or its destination.
+      documentation: `Properly handle a transaction that either originates from, or its destination.
        Case 1: forwarding txns: return the same txn with child txn(s)
        Case 2: funding txns: return the same txn with parent txn(s?) set for this txn.
-       Case 3: nothing happens: return the same txn',
+       Case 3: nothing happens: return the same txn`,
       type: 'net.nanopay.tx.model.Transaction',
       args: [
         {
@@ -46,10 +44,8 @@ foam.INTERFACE({
           type: 'Context'
         }
       ],
-      documentation: 'There are three ways to find an accounts trustAccount. 1. Explicit reference,
-       2. Walking the tree of accounts upwards to the top parent, 3. get the default based on currency.'
+      documentation: `There are three ways to find an accounts trustAccount. 1. Explicit reference,
+       2. Walking the tree of accounts upwards to the top parent, 3. get the default based on currency.`
     }
   ]
-
-
 })
