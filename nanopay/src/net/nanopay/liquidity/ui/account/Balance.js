02b70aa1b99d5aad1909056a49d0557d9d809e25
Mike Carcasole <mikecarcasole@gmail.com>
2019-04-09 13:53:13 -0400

    Add details to properties in Balance and Overview models

--- nanopay/src/net/nanopay/liquidity/ui/account/Balance.js
+++ nanopay/src/net/nanopay/liquidity/ui/account/Balance.js
@@ -1,9 +1,26 @@
 foam.CLASS({
   package: 'net.nanopay.liquidity.ui.account',
   name: 'Balance',
+  imports: [
+    'data'
+  ],
   properties: [
-    'currency',
-    'balance'
+    {
+      class: 'String',
+      name: 'currency',
+      visibility: 'RO',
+      expression: function(data$denomination) {
+        return data$denomination;
+      }
+    },
+    {
+      class: 'Long',
+      name: 'balance',
+      visibility: 'RO',
+      expression: function(data$balance) {
+        return data$balance;
+      }
+    }
   ],
   actions: [
     {
--- nanopay/src/net/nanopay/liquidity/ui/account/Overview.js
+++ nanopay/src/net/nanopay/liquidity/ui/account/Overview.js
@@ -1,12 +1,51 @@
 foam.CLASS({
   package: 'net.nanopay.liquidity.ui.account',
   name: 'Overview',
-  properties: [
-    'createdOn',
-    'createdBy',
-    'accountType',
-    'lastTransaction',
-    'averageTransactionSize',
-    'fundedBy'
+  imports: [
+    'data'
   ],
+  properties: [
+    {
+      class: 'DateTime',
+      name: 'createdOn',
+      visibility: 'RO',
+      expression: function(data$created) {
+        return data$created;
+      }
+    },
+    {
+      class: 'Reference',
+      of: 'foam.nanos.auth.User',
+      name: 'createdBy',
+      visibility: 'RO',
+      expression: function(data$createdBy) {
+        return data$createdBy;
+      }
+    },
+    {
+      class: 'String',
+      name: 'accountType',
+      visibility: 'RO',
+      expression: function(data$type) {
+        return data$type;
+      }
+    },
+    {
+      class: 'String',
+      name: 'lastTransaction',
+      visibility: 'RO',
+      value: 'TODO'
+    },
+    {
+      class: 'Float',
+      name: 'averageTransactionSize',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'fundedBy',
+      visibility: 'RO',
+      value: 'TODO'
+    }
+  ]
 });
