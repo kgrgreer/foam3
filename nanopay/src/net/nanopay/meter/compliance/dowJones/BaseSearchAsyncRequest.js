1ee89f6b24b2f34e53c99a0ba125f471119b056e
blakegreen <blake@nanopay.net>
2019-04-10 15:19:11 -0400

    Took out BaseSearchAsyncRequest

--- nanopay/src/net/nanopay/meter/compliance/dowJones/BaseSearchAsyncRequest.js
+++
@@ -1,7 +0,0 @@
-foam.CLASS({
-  package: 'net.nanopay.meter.compliance.dowJones',
-  name: 'BaseSearchAsyncRequest',
-  extends: 'net.nanopay.meter.compliance.dowJones.BaseSearchRequest',
-
-  documentation: 'model for Dow Jones Search Async Request'
-});
