761a02de365710d9d74e9bd7fbdc5e85b01bfc57
penzital <penzital888@gmail.com>
2019-04-16 11:22:56 -0400

    remove corrupted user record

--- tools/journal_migration/Users_03.sh
+++ tools/journal_migration/Users_03.sh
@@ -3,6 +3,10 @@
 # Fix missing "})p({"
 
 if [ -f "$JOURNAL_HOME/users" ]; then
-    perl -p -i -e 's/}{/})\np({/' "$JOURNAL_HOME"/users
+    echo JOURNAL_HOME = $JOURNAL_HOME
+    perl -p -i -e 's/}{/})\np({/' "$JOURNAL_HOME/users"
 
+# remove corrupted file
+    sed -i.bak '/id\":id\"/d' "$JOURNAL_HOME/users"
+    rm "$JOURNAL_HOME/users.bak"
 fi
