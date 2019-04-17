56f43bf2c22fa392fecd4db3f313bad0fb1193ae
Henry Han <henry.han@usask.ca>
2019-04-17 13:37:35 -0400

    Update code style

--- nanopay/src/net/nanopay/cico/ui/bankAccount/modalForm/CABankPADForm.js
+++ nanopay/src/net/nanopay/cico/ui/bankAccount/modalForm/CABankPADForm.js
@@ -86,8 +86,8 @@ foam.CLASS({
   messages: [
     { name: 'TITLE', message: 'Connect using a void check' },
     { name: 'INSTRUCTIONS', message: 'Connect to your account without signing in to online banking.\nPlease ensure your details are entered properly.' },
-    { name: 'CONNECTING', message: 'Connecting... This may take a few minutes.'},
-    { name: 'INVALID_FORM', message: 'Please complete the form before proceeding.'},
+    { name: 'CONNECTING', message: 'Connecting... This may take a few minutes.' },
+    { name: 'INVALID_FORM', message: 'Please complete the form before proceeding.' },
     { name: 'ERROR_FIRST', message: 'First name cannot be empty.' },
     { name: 'ERROR_LAST', message: 'Last name cannot be empty.' },
     { name: 'ERROR_FLENGTH', message: 'First name cannot exceed 70 characters.' },
@@ -117,9 +117,15 @@ foam.CLASS({
             .end()
           .end()
           .start('p').addClass(this.myClass('instructions')).add(this.INSTRUCTIONS).end()
-          .start({ class: 'net.nanopay.bank.ui.BankPADForm' , viewData$: this.viewData$ }).enableClass(this.myClass('shrink'), this.isConnecting$).end()
+          .start({ class: 'net.nanopay.bank.ui.BankPADForm', viewData$: this.viewData$ })
+            .enableClass(this.myClass('shrink'), this.isConnecting$)
+          .end()
         .end()
-        .start({class: 'net.nanopay.sme.ui.wizardModal.WizardModalNavigationBar', back: this.BACK, next: this.NEXT}).end();
+        .start({
+          class: 'net.nanopay.sme.ui.wizardModal.WizardModalNavigationBar',
+          back: this.BACK,
+          next: this.NEXT
+        }).end();
     },
 
     function validateForm() {
