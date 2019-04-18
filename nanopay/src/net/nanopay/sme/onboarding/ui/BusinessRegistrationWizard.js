ce6dad0fb6f0ae2dee8759ac711dcf1f982540cb
Henry Han <henry.han@usask.ca>
2019-04-18 14:08:45 -0400

    Fix the validation check of the benificial owner

--- nanopay/src/net/nanopay/sme/onboarding/ui/BusinessRegistrationWizard.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/BusinessRegistrationWizard.js
@@ -508,10 +508,12 @@ foam.CLASS({
       return true;
     },
 
-    function validateBeneficialOwners() {
-      var beneficialOwnersCount = this.viewData.user.beneficialOwners.length;
-      if ( ! this.viewData.noBeneficialOwners && ! this.viewData.publiclyTradedEntity ) {
-        if ( beneficialOwnersCount <= 0 ) {
+    async function validateBeneficialOwners() {
+      var beneficialOwnersCount = await this.viewData.user.beneficialOwners
+        .select(this.COUNT());
+      if ( ! this.viewData.noBeneficialOwners
+        && ! this.viewData.publiclyTradedEntity ) {
+        if ( beneficialOwnersCount.value <= 0 ) {
           return false;
         }
       }
@@ -643,7 +645,7 @@ foam.CLASS({
               }
             }
 
-            if ( ! this.validateBeneficialOwners() ) {
+            if ( ! await this.validateBeneficialOwners() ) {
               this.notify(this.ERROR_NO_BENEFICIAL_OWNERS, 'error');
               return;
             }
