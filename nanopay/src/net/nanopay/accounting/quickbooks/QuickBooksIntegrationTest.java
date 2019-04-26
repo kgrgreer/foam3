9d3e69a23f8d8f0048434e553e52ca688fe16602
Neelkanth Patel <neel@nanopay.net>
2019-04-26 10:59:17 -0400

    add tests for xero and quickbooks

---
+++ nanopay/src/net/nanopay/accounting/quickbooks/QuickBooksIntegrationTest.java
@@ -0,0 +1,281 @@
+package net.nanopay.accounting.quickbooks;
+
+import com.intuit.ipp.data.*;
+import com.xero.model.Contact;
+import com.xero.model.CurrencyCode;
+import com.xero.model.InvoiceStatus;
+import foam.core.X;
+import foam.dao.ArraySink;
+import foam.dao.DAO;
+import foam.mlang.MLang;
+import foam.nanos.auth.User;
+import foam.test.TestUtils;
+import foam.util.Auth;
+import foam.util.SafetyUtil;
+import net.nanopay.accounting.ContactMismatchPair;
+import net.nanopay.accounting.ResultResponse;
+import net.nanopay.accounting.quickbooks.QuickbooksIntegrationService;
+import net.nanopay.accounting.resultresponse.ContactResponseItem;
+import net.nanopay.accounting.resultresponse.InvoiceResponseItem;
+import net.nanopay.accounting.xero.XeroIntegrationService;
+import net.nanopay.accounting.xero.XeroToken;
+import net.nanopay.iso20022.CurrencyReference2;
+import net.nanopay.model.Currency;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class QuickBooksIntegrationTest extends foam.nanos.test.Test {
+
+  List<NameBase> contact = new ArrayList<>();
+  List<Transaction> invoice = new ArrayList<>();
+
+  @Override
+  public void runTest(X x) {
+
+    QuickbooksIntegrationService quickbooksService = new QuickbooksIntegrationService();
+    quickbooksService.setX(x);
+    try {
+      quickbooksService.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return;
+    }
+    createToken(x);
+    testContactSync(x, quickbooksService);
+    testInvoiceSync(x, quickbooksService);
+
+  }
+
+  public void testContactSync(X x, QuickbooksIntegrationService quickbooksService) {
+    NameBase quickBooksContact = new Customer();
+    ContactMismatchPair result;
+    HashMap<String, List<ContactResponseItem>> contactErrors = quickbooksService.initContactErrors();
+    Date date = new Date();
+    ModificationMetaData metaData = new ModificationMetaData();
+    metaData.setLastUpdatedTime(date);
+    EmailAddress email = new EmailAddress();
+    email.setAddress("");
+
+
+    // empty everything, should fail
+    quickBooksContact.setCompanyName("");
+    quickBooksContact.setGivenName("");
+    quickBooksContact.setFamilyName("");
+    quickBooksContact.setPrimaryEmailAddr(null);
+    quickBooksContact.setId("1");
+    quickBooksContact.setMetaData(metaData);
+    test(! quickbooksService.isValidContact(quickBooksContact, contactErrors), "Empty Contact" );
+
+    // empty email, should fail
+    quickBooksContact = new Customer();
+    quickBooksContact.setCompanyName("empty email Business name");
+    quickBooksContact.setGivenName("");
+    quickBooksContact.setFamilyName("");
+    quickBooksContact.setPrimaryEmailAddr(null);
+    quickBooksContact.setId("2");
+    quickBooksContact.setMetaData(metaData);
+    test(! quickbooksService.isValidContact(quickBooksContact, contactErrors), "Empty Email & business name" );
+
+    try {
+      email = new EmailAddress();
+      email.setAddress("aqbo.net");
+      // invalid email, should fail
+      quickBooksContact = new Customer();
+      quickBooksContact.setCompanyName("invalid email business name");
+      quickBooksContact.setGivenName("");
+      quickBooksContact.setFamilyName("");
+      quickBooksContact.setPrimaryEmailAddr(email);
+      quickBooksContact.setId("3");
+      quickBooksContact.setMetaData(metaData);
+      quickbooksService.importContact(x,quickBooksContact);
+      test(false, "Invalid Email");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Invalid Email");
+    }
+
+    email = new EmailAddress();
+    email.setAddress("aqbo@nanopay.net");
+    // empty business name, should fail
+    quickBooksContact = new Customer();
+    quickBooksContact.setCompanyName("");
+    quickBooksContact.setGivenName("");
+    quickBooksContact.setFamilyName("");
+    quickBooksContact.setPrimaryEmailAddr(email);
+    quickBooksContact.setId("4");
+    quickBooksContact.setMetaData(metaData);
+    test(! quickbooksService.isValidContact(quickBooksContact, contactErrors), "Empty Business name" );
+
+    try {
+      email = new EmailAddress();
+      email.setAddress("optionalnameqbo@nanopay.net");
+      // optional first and last name, should pass
+      quickBooksContact = new Customer();
+      quickBooksContact.setCompanyName("Optional business name");
+      quickBooksContact.setGivenName("");
+      quickBooksContact.setFamilyName("");
+      quickBooksContact.setPrimaryEmailAddr(email);
+      quickBooksContact.setId("5");
+      quickBooksContact.setMetaData(metaData);
+      result = quickbooksService.importContact(x, quickBooksContact);
+      if ( result == null ) {
+        test(true, "Optional First & last Names");
+      } else {
+        test(false, "Optional First & last Names");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Optional First & last Names");
+    }
+
+    try {
+      email = new EmailAddress();
+      email.setAddress("bqbo@nanopay.net");
+      // number in first name, should fail
+      quickBooksContact = new Customer();
+      quickBooksContact.setCompanyName("company name");
+      quickBooksContact.setGivenName("n4me");
+      quickBooksContact.setFamilyName("last");
+      quickBooksContact.setPrimaryEmailAddr(email);
+      quickBooksContact.setId("6");
+      quickBooksContact.setMetaData(metaData);
+      quickbooksService.importContact(x,quickBooksContact);
+      test(false, "Number in First Name");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Number in First Name");
+    }
+
+    try {
+      // number in last name, should fail
+      quickBooksContact = new Customer();
+      quickBooksContact.setCompanyName("company name");
+      quickBooksContact.setGivenName("name");
+      quickBooksContact.setFamilyName("l4st");
+      quickBooksContact.setPrimaryEmailAddr(email);
+      quickBooksContact.setId("7");
+      quickBooksContact.setMetaData(metaData);
+      quickbooksService.importContact(x,quickBooksContact);
+      test(false, "Number in Last Name");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Number in Last Name");
+    }
+
+    try {
+      email = new EmailAddress();
+      email.setAddress("validfieldsqbo@nanopay.net");
+      // valid everything, should pass
+      quickBooksContact = new Customer();
+      quickBooksContact.setCompanyName("valid fields business name");
+      quickBooksContact.setGivenName("first");
+      quickBooksContact.setFamilyName("last");
+      quickBooksContact.setPrimaryEmailAddr(email);
+      quickBooksContact.setId("8");
+      quickBooksContact.setMetaData(metaData);
+      result = quickbooksService.importContact(x, quickBooksContact);
+      if ( result == null ) {
+        test(true, "All Valid Fields Contact");
+      } else {
+        test(false, "All Valid Fields Contact");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "All Valid Fields Contact");
+    }
+  }
+
+  public void testInvoiceSync(X x, QuickbooksIntegrationService quickbookService) {
+
+    Transaction quickbooksInvoice = new com.intuit.ipp.data.Invoice();
+    HashMap<String, List<InvoiceResponseItem>> invoiceErrors = quickbookService.initInvoiceErrors();
+    Date date = new Date();
+    ModificationMetaData metaData = new ModificationMetaData();
+    metaData.setLastUpdatedTime(date);
+    ReferenceType currencyRef = new ReferenceType();
+    currencyRef.setValue("INR");
+    currencyRef.setName("currencyCode");
+
+    ReferenceType customerRef = new ReferenceType();
+    customerRef.setValue("");
+    customerRef.setName("customerId");
+
+    try {
+      // invalid currency
+      quickbooksInvoice.setCurrencyRef(currencyRef);
+      quickbooksInvoice.setId("1");
+      quickbooksInvoice.setMetaData(metaData);
+      if ( quickbookService.importInvoice(x, quickbooksInvoice, invoiceErrors) == null ) {
+        test(false, "Invalid Currency");
+      } else {
+        test(true, "Invalid Currency");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Invalid Currency");
+    }
+
+    currencyRef = new ReferenceType();
+    currencyRef.setValue("CAD");
+    currencyRef.setName("currencyCode");
+
+    try {
+      // missing contact, should fail
+      quickbooksInvoice = new com.intuit.ipp.data.Invoice();
+      quickbooksInvoice.setCurrencyRef(currencyRef);
+      quickbooksInvoice.setDocNumber("32893");
+      ((Invoice) quickbooksInvoice).setDueDate(date);
+      ((Invoice) quickbooksInvoice).setBalance(new BigDecimal(5));
+      ((Invoice) quickbooksInvoice).setTotalAmt(new BigDecimal(5));
+      ((Invoice) quickbooksInvoice).setCustomerRef(customerRef);
+      quickbooksInvoice.setId("2");
+      quickbooksInvoice.setMetaData(metaData);
+      if ( quickbookService.importInvoice(x, quickbooksInvoice, invoiceErrors) == null ) {
+        test(false, "Missing Contact");
+      } else {
+        test(true, "Missing Contact");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Missing Contact");
+    }
+
+    customerRef = new ReferenceType();
+    customerRef.setValue("8");
+    customerRef.setName("customerId");
+
+    try {
+      // all valid fields should pass
+      quickbooksInvoice = new com.intuit.ipp.data.Invoice();
+      quickbooksInvoice.setCurrencyRef(currencyRef);
+      quickbooksInvoice.setDocNumber("4908348934");
+      ((Invoice) quickbooksInvoice).setDueDate(date);
+      ((Invoice) quickbooksInvoice).setBalance(new BigDecimal(9));
+      ((Invoice) quickbooksInvoice).setTotalAmt(new BigDecimal(9));
+      ((Invoice) quickbooksInvoice).setCustomerRef(customerRef);
+      quickbooksInvoice.setId("3");
+      quickbooksInvoice.setMetaData(metaData);
+      if ( quickbookService.importInvoice(x, quickbooksInvoice, invoiceErrors) == null ) {
+        test(true, "Valid Fields Invoice");
+      } else {
+        test(false, "Valid Fields Invoice");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Valid Fields Invoice");
+    }
+  }
+
+  public void createToken(X x) {
+    DAO tokenDAO =  (DAO) x.get("quickbooksTokenDAO");
+    User user = (User) x.get("user");
+    QuickbooksToken token = new QuickbooksToken();
+    token.setId(user.getId());
+    token.setBusinessName("test company");
+    token.setRealmId("1234567890");
+    tokenDAO.put(token);
+  }
+
+}
---
+++ nanopay/src/net/nanopay/accounting/xero/XeroIntegrationTest.java
@@ -0,0 +1,221 @@
+package net.nanopay.accounting.xero;
+
+import com.xero.model.Contact;
+import com.xero.model.CurrencyCode;
+import com.xero.model.Invoice;
+import com.xero.model.InvoiceStatus;
+import foam.core.X;
+import foam.dao.DAO;
+import foam.nanos.auth.User;
+import net.nanopay.accounting.ContactMismatchPair;
+import net.nanopay.accounting.resultresponse.ContactResponseItem;
+import net.nanopay.accounting.resultresponse.InvoiceResponseItem;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class XeroIntegrationTest extends foam.nanos.test.Test {
+
+  Contact contact = new Contact();
+  List<Invoice> invoice = new ArrayList<>();
+
+  @Override
+  public void runTest(X x) {
+    XeroIntegrationService xeroService = new XeroIntegrationService();
+    xeroService.setX(x);
+    createToken(x);
+    testContactSync(x, xeroService);
+    testInvoiceSync(x, xeroService);
+  }
+
+  public void createToken(X x) {
+    DAO tokenDAO =  (DAO) x.get("xeroTokenDAO");
+    User user = (User) x.get("user");
+    XeroToken token = new XeroToken();
+    token.setId(user.getId());
+    token.setBusinessName("test company");
+    token.setOrganizationId("1234567890");
+    tokenDAO.put(token);
+  }
+
+  public void testContactSync(X x, XeroIntegrationService xeroService ) {
+    HashMap<String, List<ContactResponseItem>> contactErrors = xeroService.initContactErrors();
+    Contact xeroContact = new Contact();
+    Calendar date = new GregorianCalendar();
+    ContactMismatchPair result;
+
+    // empty everything, should fail
+    xeroContact.setName("");
+    xeroContact.setFirstName("");
+    xeroContact.setLastName("");
+    xeroContact.setEmailAddress("");
+    xeroContact.setContactID("1");
+    xeroContact.setUpdatedDateUTC(date);
+    test( ! xeroService.isValidContact(xeroContact,contactErrors), "Empty Contanct" );
+
+    // empty email, should fail
+    xeroContact = new Contact();
+    xeroContact.setName("Business name");
+    xeroContact.setFirstName("");
+    xeroContact.setLastName("");
+    xeroContact.setEmailAddress("");
+    xeroContact.setContactID("2");
+    xeroContact.setUpdatedDateUTC(date);
+    test( ! xeroService.isValidContact(xeroContact, contactErrors), "Empty Email" );
+
+    // empty business name, should fail
+    xeroContact = new Contact();
+    xeroContact.setName("");
+    xeroContact.setFirstName("");
+    xeroContact.setLastName("");
+    xeroContact.setEmailAddress("a@nanopay.net");
+    xeroContact.setContactID("3");
+    xeroContact.setUpdatedDateUTC(date);
+    test( ! xeroService.isValidContact(xeroContact, contactErrors), "Empty Business name" );
+
+    try {
+      // invalid email, should fail
+      xeroContact = new Contact();
+      xeroContact.setName("Business name");
+      xeroContact.setFirstName("");
+      xeroContact.setLastName("");
+      xeroContact.setEmailAddress("a.net");
+      xeroContact.setContactID("4");
+      xeroContact.setUpdatedDateUTC(date);
+      result = xeroService.importContact(x, xeroContact);
+      test(false, "Invalid Email");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Invalid Email");
+    }
+
+    try {
+      // optional first and last name, should pass
+      xeroContact = new Contact();
+      xeroContact.setName("Optional business name");
+      xeroContact.setFirstName("");
+      xeroContact.setLastName("");
+      xeroContact.setEmailAddress("optionalname@nanopay.net");
+      xeroContact.setContactID("5");
+      xeroContact.setUpdatedDateUTC(date);
+      result = xeroService.importContact(x, xeroContact);
+      test(true, "Optional name");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Optional name");
+    }
+
+    try {
+      // number in first name, should fail
+      xeroContact = new Contact();
+      xeroContact.setName("Business name");
+      xeroContact.setFirstName("f1rst");
+      xeroContact.setLastName("last");
+      xeroContact.setEmailAddress("b@nanopay.net");
+      xeroContact.setContactID("6");
+      xeroContact.setUpdatedDateUTC(date);
+      result = xeroService.importContact(x, xeroContact);
+      test(false, "Number first name");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Number in first name");
+    }
+
+    try {
+      // number in last name, should fail
+      xeroContact = new Contact();
+      xeroContact.setName("Business name");
+      xeroContact.setFirstName("first");
+      xeroContact.setLastName("1ast");
+      xeroContact.setEmailAddress("b@nanopay.net");
+      xeroContact.setContactID("7");
+      xeroContact.setUpdatedDateUTC(date);
+      result = xeroService.importContact(x, xeroContact);
+      test(false, "Number Last Name");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(true, "Number in Last Name");
+    }
+
+    try {
+      // valid everything, should pass
+      xeroContact = new Contact();
+      xeroContact.setName("valid fields business name");
+      xeroContact.setFirstName("first");
+      xeroContact.setLastName("last");
+      xeroContact.setEmailAddress("validfields@nanopay.net");
+      xeroContact.setContactID("8");
+      xeroContact.setUpdatedDateUTC(date);
+      result = xeroService.importContact(x, xeroContact);
+      test(true, "All Valid Contacts");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "All Valid Contacts");
+    }
+
+  }
+
+  public void testInvoiceSync(X x, XeroIntegrationService xeroService) {
+    Invoice xeroInvoice = new Invoice();
+    HashMap<String, List<InvoiceResponseItem>> invoiceErrors = xeroService.initInvoiceErrors();
+    Calendar date = new GregorianCalendar();
+
+    try {
+      // invalid currency
+      xeroInvoice.setCurrencyCode(CurrencyCode.INR);
+      xeroInvoice.setInvoiceID("1");
+      xeroInvoice.setUpdatedDateUTC(date);
+      test( ! xeroService.importInvoice(x, xeroInvoice, invoiceErrors), "Invalid Currency");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Invalid Currency");
+    }
+
+    try {
+      // not authorized, should fail
+      xeroInvoice = new Invoice();
+      xeroInvoice.setCurrencyCode(CurrencyCode.CAD);
+      xeroInvoice.setStatus(InvoiceStatus.DRAFT);
+      xeroInvoice.setInvoiceID("2");
+      xeroInvoice.setUpdatedDateUTC(date);
+      test( ! xeroService.importInvoice(x, xeroInvoice, invoiceErrors), "Not Authorized");
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Not Authorized");
+    }
+
+    try {
+      // missing contact, should fail
+      xeroInvoice = new Invoice();
+      xeroInvoice.setCurrencyCode(CurrencyCode.CAD);
+      xeroInvoice.setStatus(InvoiceStatus.AUTHORISED);
+      xeroInvoice.setDueDate(date);
+      xeroInvoice.setDate(date);
+      xeroInvoice.setAmountDue(new BigDecimal(5));
+      xeroInvoice.setInvoiceID("3");
+      xeroInvoice.setUpdatedDateUTC(date);
+      test( ! xeroService.importInvoice(x, xeroInvoice, invoiceErrors), "Missing Contact" );
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "Missing Contact");
+    }
+
+    try {
+      // all valid fields should pass
+      xeroInvoice = new Invoice();
+      xeroInvoice.setCurrencyCode(CurrencyCode.CAD);
+      xeroInvoice.setStatus(InvoiceStatus.AUTHORISED);
+      xeroInvoice.setDueDate(date);
+      xeroInvoice.setDate(date);
+      xeroInvoice.setContact(contact);
+      xeroInvoice.setAmountDue(new BigDecimal(6));
+      xeroInvoice.setInvoiceID("4");
+      xeroInvoice.setUpdatedDateUTC(date);
+      test( ! xeroService.importInvoice(x, xeroInvoice, invoiceErrors), "All Fields Valid" );
+    } catch (Exception e) {
+      e.printStackTrace();
+      test(false, "All Fields Valid");
+    }
+  }
+
+}
