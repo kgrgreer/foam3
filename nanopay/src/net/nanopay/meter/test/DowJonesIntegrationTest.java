b878a140ac5a4c170b7e54fe8997bd3a167fc7e7
blakegreen <blake@nanopay.net>
2019-04-29 13:51:11 -0400

    Added test for Dow Jones person name search and entity name search

--- nanopay/src/net/nanopay/meter/test/DowJonesIntegrationTest.java
+++ nanopay/src/net/nanopay/meter/test/DowJonesIntegrationTest.java
@@ -1,42 +1,49 @@
 package net.nanopay.meter.test;
 
 import foam.core.X;
+import foam.dao.DAO;
 import foam.nanos.test.Test;
-import net.nanopay.meter.compliance.dowJones.DowJonesCredentials;
-import net.nanopay.meter.compliance.dowJones.DowJonesService;
+import foam.nanos.auth.User;
+import net.nanopay.meter.compliance.dowJones.*;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Base64;
-import java.util.Calendar;
-import java.util.Date;
+import static foam.mlang.MLang.*;
 
-// apache
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.client.utils.HttpClientUtils;
-import org.apache.http.client.config.RequestConfig;
 
 public class DowJonesIntegrationTest extends Test {
 
+  private DowJonesService dowJonesService_;
+  User testUser_;
+  X x_;
+
   @Override
   public void runTest(X x) {
-    DowJonesService dowJonesService = (DowJonesService) x.get("dowJonesService");
-
-    String firstName = "Blake";
-    String lastName = "Green";
-
-    String entityName = "Apothecary";
-
-    dowJonesService.personNameSearch(x, firstName, lastName, null);
-    //dowJonesService.entityNameSearch(x, entityName, null);
+    dowJonesService_ = (DowJonesService) x.get("dowJonesService");
+    x_ = x;
+    testUser_ = addUser("blakeDowJonesTest@dowjonestest.com");
+    setUpTest();
   }
-  
+
+  private void setUpTest() {
+    BaseSearchResponse personNameObj;
+    BaseSearchResponse entityNameObj;
+    personNameObj = dowJonesService_.personNameSearch(x_, testUser_.getFirstName(), testUser_.getLastName(), null);
+    entityNameObj = dowJonesService_.entityNameSearch(x_, testUser_.getBusinessName(), null);
+    test(personNameObj != null, "Dow Jones Person Response Object Created.");
+    test(entityNameObj != null, "Dow Jones Entity Response Object Created.");
+  }
+
+  public User addUser(String email) {
+    User user = (User) ((DAO) x_.get("localUserDAO")).find(EQ(User.EMAIL, email));
+    if ( user == null ) {
+      user = new User();
+      user.setEmail(email);
+      user.setFirstName("Blake");
+      user.setLastName("Green");
+      user.setEmailVerified(true);
+      user.setBusinessName("nanopay");
+      user = (User) (((DAO) x_.get("localUserDAO")).put_(x_, user)).fclone();
+    } 
+    return user;
+  }
+
 }
