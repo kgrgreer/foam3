93047d764ca434e85a787a6cefe899a77c155919
Troy Wolters <troy@nanopay.net>
2019-05-01 14:11:56 -0400

    Add custom Contact DetailView for BackOffice

---
+++ nanopay/src/net/nanopay/meter/ContactDetailView.js
@@ -0,0 +1,61 @@
+foam.CLASS({
+  package: 'net.nanopay.meter',
+  name: 'ContactDetailView',
+  extends: 'foam.u2.DetailView',
+
+  requires: [
+    'foam.core.Property',
+    'net.nanopay.contacts.Contact',
+    'net.nanopay.accounting.xero.model.XeroContact',
+    'net.nanopay.accounting.quickbooks.model.QuickbooksContact',
+  ],
+
+  css: `
+    ^ {
+      background-color: #fafafa;
+      border: 1px solid #e2e2e3;
+      border-radius: 4px;
+      margin-top: 8px;
+    }
+
+    ^ td {
+      padding: 8px 16px;
+    }
+
+    ^ .foam-u2-PropertyView-label {
+      font-weight: bold;
+    }
+  `,
+
+  properties: [
+    {
+      name: 'properties',
+      factory: function() {
+        var self = this;
+        var model = this.Contact.clone();
+        var syncedAxiom = this.Property.create({
+          class: 'Boolean',
+          name: 'syncedFromAccounting',
+          label: 'Synced from accounting?',
+          view: 'foam.u2.CheckBox',
+          factory: function() {
+            return self.QuickbooksContact.isInstance(this) ||
+                   self.XeroContact.isInstance(this);
+          }
+        });
+        model.installAxiom(syncedAxiom);
+        return [
+          this.Contact.ID,
+          this.Contact.BUSINESS_NAME,
+          this.Contact.EMAIL,
+          this.Contact.CREATED.clone().copyFrom({ label: 'Date created' }),
+          this.Contact.OWNER.clone().copyFrom({ label: 'Created By' }),
+          this.Contact.SIGN_UP_STATUS,
+          this.Contact.DELETED,
+          this.Contact.SYNCED_FROM_ACCOUNTING,
+          this.Contact.ACCOUNTS,
+        ];
+      }
+    }
+  ]
+});
