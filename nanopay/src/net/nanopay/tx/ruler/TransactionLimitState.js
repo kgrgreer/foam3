494a0cc17f23bad06930be819f9783d5c17bce61
penzital <penzital888@gmail.com>
2019-05-02 14:40:57 -0400

    added doc for TransactionLimitState

--- nanopay/src/net/nanopay/tx/ruler/TransactionLimitState.js
+++ nanopay/src/net/nanopay/tx/ruler/TransactionLimitState.js
@@ -1,6 +1,11 @@
 foam.CLASS({
   package: 'net.nanopay.tx.ruler',
   name: 'TransactionLimitState',
+  documentation: 'The class stores the limit itslef and date when that limit was set.' +
+  'Responsible for updating the limit as well.' +
+  'The object is stored in a hash map on the AbstractTransactionLimitRule' +
+  'along with the object that it stores limit for(account, business, user..)',
+
 
   properties: [
     {
