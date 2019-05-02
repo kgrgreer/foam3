ea8be8aadf3604a71b9ae14e224b8b0809d6d3a5
Chanmann Lim <chanmann@nanopay.net>
2019-05-02 13:53:18 -0400

    Support generic bank to bank transaction instead of just Ablii transaction
    by checking if both the source and destination accounts are bank accounts.

--- nanopay/src/net/nanopay/meter/compliance/identityMind/predicate/B2BTransaction.js
+++ nanopay/src/net/nanopay/meter/compliance/identityMind/predicate/B2BTransaction.js
@@ -1,11 +1,17 @@
 foam.CLASS({
   package: 'net.nanopay.meter.compliance.identityMind.predicate',
   name: 'B2BTransaction',
+
+  documentation: 'Predicate for bank to bank transaction.',
+
   extends: 'foam.mlang.predicate.AbstractPredicate',
   implements: ['foam.core.Serializable'],
 
   javaImports: [
-    'net.nanopay.tx.AbliiTransaction',
+    'foam.core.X',
+    'net.nanopay.account.Account',
+    'net.nanopay.bank.BankAccount',
+    'net.nanopay.tx.model.Transaction',
     'static foam.mlang.MLang.*',
   ],
 
@@ -13,9 +19,12 @@ foam.CLASS({
     {
       name: 'f',
       javaCode: `
-        return EQ(
-          DOT(NEW_OBJ, INSTANCE_OF(AbliiTransaction.class)), true
-        ).f(obj);
+        Transaction transaction = (Transaction) NEW_OBJ.f(obj);
+        Account sourceAccount = transaction.findSourceAccount((X) obj);
+        Account destinationAccount = transaction.findDestinationAccount((X) obj);
+
+        return sourceAccount instanceof BankAccount
+          && destinationAccount instanceof BankAccount;
       `
     }
   ]
