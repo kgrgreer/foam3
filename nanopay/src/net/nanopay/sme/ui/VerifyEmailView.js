be746a6b5a40954c9a4b63eb402a5cf18848b589
Patrick Zanowski <patrick@nanopay.net>
2019-05-06 15:01:36 -0400

    Created new verify email view references to existing changed

--- nanopay/src/net/nanopay/sme/ui/VerifyEmailView.js
+++ nanopay/src/net/nanopay/sme/ui/VerifyEmailView.js
@@ -1,14 +1,9 @@
-/**
- * @license
- * Copyright 2018 The FOAM Authors. All Rights Reserved.
- * http://www.apache.org/licenses/LICENSE-2.0
- */
 foam.CLASS({
   package: 'net.nanopay.sme.ui',
-  name: 'VerifyEmail',
+  name: 'VerifyEmailView',
   extends: 'foam.u2.Controller',
 
-  documentation: 'Resend verification email view (Deprecated since release 3.0)',
+  documentation: 'Resend verification email view',
 
   requires: [
     'foam.u2.dialog.NotificationMessage'
