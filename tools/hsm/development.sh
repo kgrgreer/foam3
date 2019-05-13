315e702b50eaa090cadffd81d616c021e72dfd76
Dhiren Audich <dhiren@nanopay.net>
2019-05-13 14:25:10 -0400

    added tests to check if tokens already exist before deleting; added softhsm setup to the build script

--- tools/hsm/development.sh
+++ tools/hsm/development.sh
@@ -2,63 +2,72 @@
 # Setting up SoftHSM for local development
 ################################################################################
 
-# Exit on first failure
-set -e
-
-function quit {
-  # Unset error on exit
-  set +e
-  exit $1
-}
-
 function install_softhsm {
   # Check if softhsm2 is already installed
   if ! type softhsm2-util > /dev/null ; then
-    printf "WARNING :: softhsm2 not installed!\n\n"
+    printf "WARNING :: softhsm2 not installed!\n"
     brew install softhsm
-    printf "INFO :: softhsm2 installed.\n\n"
+    printf "INFO :: softhsm2 installed.\n"
   else
-    printf "INFO :: softhsm2 already installed!\n\n"
+    printf "INFO :: softhsm2 already installed!\n"
   fi
 }
 
 function create_slots {
   # Create fresh slots
-  softhsm2-util --init-token --label "development" --so-pin $PIN --pin $PIN --free
-  softhsm2-util --init-token --label "testing" --so-pin $PIN --pin $PIN --free
-  printf "INFO :: New slots for development and testing created...\n\n"
+  if [[ $DEVELOPMENT_SLOT -eq 0 ]]; then
+    softhsm2-util --init-token --label "development" --so-pin $PIN --pin $PIN --free
+  fi
+
+  if [[ $TESTING_SLOT -eq 0 ]]; then
+    softhsm2-util --init-token --label "testing" --so-pin $PIN --pin $PIN --free
+  fi
+
+  printf "INFO :: New slots for development and testing created...\n"
 }
 
 function migrate_slots {
   # Check if the slots already exist and add them if they don't
   for d in ./hsm/dev/*; do
     if [[ ! -d "/usr/local/var/lib/softhsm/tokens/{$d}" ]]; then
-      cp -r dev/0c67db3b-39d0-6d8a-f146-a683a1f0988b /usr/local/var/lib/softhsm/tokens
+      cp -r $ROOT/dev/0c67db3b-39d0-6d8a-f146-a683a1f0988b /usr/local/var/lib/softhsm/tokens
     fi
   done
   for d in ./hsm/test/*; do
     if [[ ! -d "/usr/local/var/lib/softhsm/tokens/{$d}" ]]; then
-      cp -r test/2c994c25-5565-55f2-0f4f-9458ac0f1537 /usr/local/var/lib/softhsm/tokens
+      cp -r $ROOT/test/2c994c25-5565-55f2-0f4f-9458ac0f1537 /usr/local/var/lib/softhsm/tokens
     fi
   done
 
-  printf "INFO :: Tokens: development and testing, have been updated locally.\n\n"
+  printf "INFO :: Tokens: development and testing, have been updated locally.\n"
 }
 
 function clean {
-  softhsm2-util --delete-token --token "development"
-  softhsm2-util --delete-token --token "testing"
-  printf "INFO :: Tokens: development and testing, have been deleted.\n\n"
+  softhsm2-util --show-slots | grep -q 'development'
+  DEVELOPMENT_SLOT=$?
+  if [[ $DEVELOPMENT_SLOT -eq 0 ]]; then
+    softhsm2-util --delete-token --token "development"
+  fi
+
+  softhsm2-util --show-slots | grep -q "testing"
+  TESTING_SLOT=$?
+  if [[ $TESTING_SLOT -eq 0 ]]; then
+    softhsm2-util --delete-token --token "testing"
+  fi
+
+  printf "INFO :: Tokens: development and testing, have been deleted.\n"
 }
 
 CREATE=0
 PIN="Secret.123"
+ROOT="."
 
-while getopts "cp:" opt ; do
+while getopts "cp:r:" opt ; do
     case $opt in
         c) CREATE=1 ;;
         p) PIN=$OPTARG ;;
-        ?) usage ; quit 1 ;;
+        r) ROOT=$OPTARG ;;
+        ?) usage ; exit 1 ;;
     esac
 done
 
@@ -71,6 +80,6 @@ else
   migrate_slots
 fi
 
-printf "INFO :: SoftHSM successfully setup.\n\n"
+printf "INFO :: SoftHSM successfully setup.\n"
 softhsm2-util --show-slots
-quit 0
+exit 0
