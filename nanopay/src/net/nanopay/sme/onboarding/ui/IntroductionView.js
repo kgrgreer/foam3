6786099f08146cd3bc804d94dd05bbda4d946c47
Patrick Zanowski <patrick@nanopay.net>
2019-05-15 13:34:31 -0400

    Minor styling fix on currency drop down and onbooarding rewording

--- nanopay/src/net/nanopay/sme/onboarding/ui/IntroductionView.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/IntroductionView.js
@@ -53,7 +53,7 @@ foam.CLASS({
     { name: 'GUIDE_MESSAGE_REQUIREMENTS', message: 'Here are some things you need to get this done:' },
     { name: 'BUSINESS_ADDRESS', message: 'Business Address' },
     { name: 'BUSINESS_REGISTRATION_INFO', message: 'Business Registration Information' },
-    { name: 'PROOF_OF_REGISTRATION', message: 'Proof of Business Registration' },
+    { name: 'PROOF_OF_REGISTRATION', message: 'Business Information' },
     { name: 'SIGNING_OFFICER', message: 'Signing Officer Information & Identification' },
     { name: 'COMPANY_OWNERSHIP', message: 'Company Ownership Information' },
     // Second Section of messages
