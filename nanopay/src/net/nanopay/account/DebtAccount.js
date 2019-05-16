7eab741d721c16a7b3bbdec4c6547b81c0a421d6
Joel Hughes <joel@nanopay.net>
2019-05-16 11:38:56 -0400

    Add debtorAccount and creditorAccount

--- nanopay/src/net/nanopay/account/DebtAccount.js
+++ nanopay/src/net/nanopay/account/DebtAccount.js
@@ -7,10 +7,32 @@ foam.CLASS({
   properties: [
     // name: 'terms' - future - capture the repayment, interest, ...
     {
-      name: 'creditorAccount',
+      name: 'debtorAccount',
+      label: 'Debtor',
       class: 'Reference',
       of: 'net.nanopay.account.Account',
-      documentation: 'The account backing the debit amount.',
+      documentation: 'The account which owes this debt.',
+      visibility: 'RO',
+      view: function(_, X) {
+        return foam.u2.view.ChoiceView.create({
+          dao: X.accountDAO.where(
+            X.data.AND(
+              X.data.NOT(X.data.INSTANCE_OF(X.data.ZeroAccount))
+            )
+          ),
+          placeholder: '--',
+          objToChoice: function(lenderAccount) {
+            return [debtorAccount.id, debtorAccount.name];
+          }
+        });
+      }
+    },
+    {
+      name: 'creditorAccount',
+      label: 'Creditor',
+      class: 'Reference',
+      of: 'net.nanopay.account.Account',
+      documentation: 'The account which is owned this debt.',
       view: function(_, X) {
         return foam.u2.view.ChoiceView.create({
           dao: X.accountDAO.where(
@@ -24,75 +46,38 @@ foam.CLASS({
           }
         });
       }
+    },
+    {
+      class: 'Long',
+      name: 'limit',
+      value: 0
     }
   ],
-  // methods: [
-  //   {
-  //     documentation: 'Allow Account specific validation of balance operation. Trust accounts can be negative, for example.',
-  //     name: 'validateAmount',
-  //     args: [
-  //       {
-  //         name: 'x',
-  //         type: 'Context'
-  //       },
-  //       {
-  //         name: 'balance',
-  //         type: 'net.nanopay.account.Balance'
-  //       },
-  //       {
-  //         name: 'amount',
-  //         type: 'Long'
-  //       }
-  //     ],
-  //     javaCode: `
-  //       // // debt balance will be negative
-  //       // // but allow positive until logic for overpaying exists.
 
-  //       // long bal = balance == null ? 0L : balance.getBalance();
-  //       // if ( amount > 0 &&
-  //       //      amount > -balance.getBalance()) {
-  //       //   throw new RuntimeException("Invalid transfer, "+this.getClass().getSimpleName()+" account balance must remain <= 0. " + this.getClass().getSimpleName()+"."+getName());
-  //       // }
-  //     `
-  //   }
-  // ]
-
-  axioms: [
+  methods: [
     {
-      buildJavaClass: function(cls) {
-        cls.extras.push(`
-          static public DebtAccount findDebtAccount(X x, Account owner, Account creditor) {
-            Logger logger = (Logger) x.get("logger");
-            DebtAccount account = null;
-
-            synchronized(String.valueOf(owner.getId()).intern()) {
-              logger.info(DebtAccount.class.getSimpleName(), "findDebtAccount", "owner", owner.getId(), "creditor", creditor);
-              DAO dao = (DAO) x.get("debtAccountDAO");
-              List accounts = ((ArraySink) dao
-                .where(
-                  AND(
-                    EQ(Account.ENABLED, true),
-                    EQ(Account.OWNER, owner.getId()),
-                    EQ(Account.CREDITOR, creditor.getId())
-                  )
-                )
-                .select(new ArraySink())).getArray();
-              if ( accounts.size() == 0 ) {
-                account = new DebtAccount();
-                account.setDenomination(creditor.getDenomination());
-                account.setOwner(owner.getId());
-                account = (DebtAccount) accountDAO.put(account);
-              } else {
-                if ( accounts.size() > 1 ) {
-                  logger.warning(DigitalAccount.class.getClass().getSimpleName(), "multiple accounts found for", "owner", owner.getId(), "creditor", creditor.getId());
-                }
-                account = (DebtAccount) accounts.get(0);
-              }
-              return account;
-            }
-          }`
-        );
-      }
+      documentation: 'Debt account is always negative',
+      name: 'validateAmount',
+      args: [
+        {
+          name: 'x',
+          type: 'Context'
+        },
+        {
+          name: 'balance',
+          type: 'net.nanopay.account.Balance'
+        },
+        {
+          name: 'amount',
+          type: 'Long'
+        }
+      ],
+      javaCode: `
+        if ( amount > 0 &&
+             amount > -balance.getBalance()) {
+          throw new RuntimeException("Invalid transfer, "+this.getClass().getSimpleName()+" account balance must remain <= 0. " + this.getClass().getSimpleName()+"."+getName());
+        }
+      `
     }
   ]
-});
+);
