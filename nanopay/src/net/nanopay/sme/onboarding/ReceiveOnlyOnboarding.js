cef5ac8af3dad16bee7937f0f9848e31f8081dad
Danny Tharma <daniel@nanopay.net>
2019-05-16 10:37:13 -0400

    Fixed stupid stuff from receiveonly

--- nanopay/src/net/nanopay/sme/onboarding/ReceiveOnlyOnboarding.js
+++ nanopay/src/net/nanopay/sme/onboarding/ReceiveOnlyOnboarding.js
@@ -2,4 +2,11 @@ foam.CLASS({
   package: 'net.nanopay.sme.onboarding',
   name: 'ReceiveOnlyOnboarding',
   ids: ['name'],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'name',
+    }
+  ]
 });
