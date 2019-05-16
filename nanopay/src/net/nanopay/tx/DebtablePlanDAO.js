644b8722f3056aec36db03ff0153f33dc2b1cc82
Joel Hughes <joel@nanopay.net>
2019-05-16 10:32:47 -0400

    Add DetablePlanDAO

---
+++ nanopay/src/net/nanopay/tx/DebtablePlanDAO.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2018 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'net.nanopay.tx',
+  name: 'DebtablePlanDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  documentation: ``,
+
+  methods: [
+    {
+      name: 'put_',
+      javaCode: `
+      Logger logger = (Logger) x.get("logger");
+      TransactionQuote quote = (TransactionQuote) obj;
+      Transaction request = quote.getRequestTransaction();
+
+      logger.debug(this.getClass().getSimpleName(), "put", quote);
+
+      Account sourceAccount = request.findSourceAccount(x);
+      Account destinationAccount = request.findDestinationAccount(x);
+
+      if ( sourceAccount instanceof Debtable ) {
+        Account creditor = ((Detable) sourceAccount).getCreditorAccount();
+        Transaction d = new DebtTransaction.Builder(x)
+          .setSourceAccount(creditorAccount)
+          .setDestinationAccount(sourceAccount)
+          .build();
+        d.setNext(request);
+        quote.setRequestTransaction(d);
+      }
+      `
+    }
+  ]
