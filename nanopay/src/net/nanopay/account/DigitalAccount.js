7a2f2b5a46aa4f205de725bd66ef5256e16d3e60
Joel Hughes <joel@nanopay.net>
2019-05-16 19:03:08 -0400

    Temporary hack to find default Overdraft accounts

--- nanopay/src/net/nanopay/account/DigitalAccount.js
+++ nanopay/src/net/nanopay/account/DigitalAccount.js
@@ -46,6 +46,9 @@ foam.CLASS({
       buildJavaClass: function(cls) {
         cls.extras.push(`
           static public DigitalAccount findDefault(X x, User user, String currency) {
+            return findDefault(x, user, currency, null);
+          }
+          static public DigitalAccount findDefault(X x, User user, String currency, DigitalAccount instance) {
             Logger logger = (Logger) x.get("logger");
             DigitalAccount account = null;
 
@@ -77,13 +80,11 @@ foam.CLASS({
 
               DAO accountDAO  = user.getAccounts(x);
 
-              Class currentClass = new Object() { }.getClass().getEnclosingClass();
-
               List accounts = ((ArraySink) accountDAO
                 .where(
                   AND(
                     EQ(Account.ENABLED, true),
-                    INSTANCE_OF(currentClass),
+                    INSTANCE_OF(instance == null ? DigitalAccount.class : instance.getClass()),
                     EQ(Account.DENOMINATION, denomination),
                     EQ(Account.IS_DEFAULT, true)
                   )
@@ -91,13 +92,7 @@ foam.CLASS({
                 .select(new ArraySink())).getArray();
 
               if ( accounts.size() == 0 ) {
-                try {
-                  java.lang.reflect.Method m = currentClass.getMethod("Builder", foam.core.X.class);
-                  account = (DigitalAccount) m.invoke(null, x);
-                } catch ( Throwable t ) {
-                  logger.error("failed to instantiate "+currentClass.getName(), t);
-                  account = new DigitalAccount();
-                }
+                account = instance == null ? new DigitalAccount() : instance;
                 account.setDenomination(denomination);
                 account.setIsDefault(true);
                 account.setOwner(user.getId()); // required until user.getAccounts()
