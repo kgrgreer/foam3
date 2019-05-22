f3dc5949ee6c871adc31f67f7430779f13de5902
Joel Hughes <joel@nanopay.net>
2019-05-21 23:31:21 -0400

    Set System.property appconfig.mode so Rules/Tests can act on it.

--- build.sh
+++ build.sh
@@ -237,13 +237,15 @@ function start_nanos {
         JAVA_OPTS="${JAVA_OPTS} -Dhttp.port=$WEB_PORT"
     fi
 
-    if [ -z "$MODE" ]; then
-        JAVA_OPTS="-Dresource.journals.dir=journals ${JAVA_OPTS}"
+    if [ -z "$MODE" ] || [ "$MODE" == "DEVELOPMENT" ] || [ "$MODE" == "STAGING" ] || [ "$MODE" == "TEST" ]; then
+#        JAVA_OPTS="-Dresource.journals.dir=journals ${JAVA_OPTS}"
         # New versions of FOAM require the new nanos.webroot property to be explicitly set to figure out Jetty's resource-base.
         # To maintain the expected familiar behaviour of using the root-dir of the NP proj as the webroot we set the property
         # to be the same as the $PWD -- which at this point is the $PROJECT_HOME
         JAVA_OPTS="-Dnanos.webroot=${PWD} ${JAVA_OPTS}"
     fi
+    JAVA_OPTS="${JAVA_OPTS} -Dappconfig.mode=${MODE}"
+
     JAR=$(ls target/lib/nanopay-*.jar | awk '{print $1}')
 
     echo JAR=$JAR
@@ -531,7 +533,7 @@ if [ "$START_ONLY" -eq 0 ]; then
     build_jar
 fi
 
-if [ "$BUILD_ONLY" -eq 1 ] || [ ! -z "$MODE" ]; then
+if [ "$BUILD_ONLY" -eq 1 ]; then
     if [ -z "$INSTANCE" ] && [ "$TEST" -eq 0 ]; then
         quit 0
     fi
