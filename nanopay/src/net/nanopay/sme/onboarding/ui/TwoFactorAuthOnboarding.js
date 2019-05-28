0e09218e274b002b7d478228d9f6325332855549
Henry Han <henry.han@usask.ca>
2019-05-28 10:53:00 -0400

    Add the 2FA link

--- nanopay/src/net/nanopay/sme/onboarding/ui/TwoFactorAuthOnboarding.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/TwoFactorAuthOnboarding.js
@@ -16,7 +16,32 @@ foam.CLASS({
       margin-top: 40px;
       margin-right: 80px;
     }
+    ^two-factor-link {
+      color: #604aff;
+      display: inline-block;
+      font-size: 16px;
+      margin-top: 8px;
+      text-decoration: none;
+    }
   `,
+
+  constants: [
+    {
+      type: 'String',
+      name: 'IOS_LINK',
+      value: 'https://itunes.apple.com/ca/app/google-authenticator/id388497605?mt=8'
+    },
+    {
+      type: 'String',
+      name: 'ANDROID_LINK',
+      value: 'https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en'
+    },
+  ],
+
+  messages: [
+    { name: 'IOSName', message: 'iOS authenticator download' },
+    { name: 'AndroidName', message: 'Android authenticator download' }
+  ],
 
   methods: [
     function initE() {
@@ -27,6 +52,15 @@ foam.CLASS({
             .addClass('body-paragraph')
             .add(`Download and use your Google Authenticator app on your mobile 
               device to scan the below QR code.`)
+          .end()
+          .start('a').addClass(this.myClass('two-factor-link'))
+            .add(this.IOSName)
+            .attrs({ href: this.IOS_LINK, target: '_blank' })
+          .end()
+          .br()
+          .start('a').addClass(this.myClass('two-factor-link'))
+            .add(this.AndroidName)
+            .attrs({ href: this.ANDROID_LINK, target: '_blank' })
           .end()
           .start({
             class: 'net.nanopay.sme.ui.TwoFactorAuthView',
