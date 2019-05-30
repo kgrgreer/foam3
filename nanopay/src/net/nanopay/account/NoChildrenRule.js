937c17b8d905c00f5fdb4616ba7ce8a41d194f73
noodlemoodle <qiyzhang@icloud.com>
2019-05-30 14:21:21 -0400

    #comment removed unused methods in rules

--- nanopay/src/net/nanopay/account/NoChildrenRule.js
+++ nanopay/src/net/nanopay/account/NoChildrenRule.js
@@ -39,23 +39,6 @@ foam.CLASS({
           }
         }
       `
-    },
-    {
-      name: 'applyReverseAction',
-      javaCode: ` `
-    },
-    {
-      name: 'canExecute',
-      javaCode: `
-      // TODO: add an actual implementation
-      return true;
-      `
-    },
-    {
-      name: 'describe',
-      javaCode: `
-      // TODO: add an actual implementation
-      return "";`
     }
   ]
 });
