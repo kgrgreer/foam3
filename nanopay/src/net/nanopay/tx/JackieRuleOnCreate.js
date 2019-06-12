e6591ad0c17e925d604d9e7c0f2c8b9247a21395
Michal Pasternak <michal.pasternak@nanopay.net>
2019-06-12 14:01:27 -0400

    remove agent from rule

--- nanopay/src/net/nanopay/tx/JackieRuleOnCreate.js
+++ nanopay/src/net/nanopay/tx/JackieRuleOnCreate.js
@@ -35,34 +35,42 @@ foam.CLASS({
       name: 'applyAction',
       javaCode: `
 
-        agent.submit(x, new ContextAgent() {
-          @Override
-          public void execute(X x) {
-            ComplianceTransaction ct = (ComplianceTransaction) obj;
-            Transaction headTx = ct;
-            Count count = (Count) ((DAO) x.get("approvalRequestDAO"))
-              .where(AND(
-                EQ(ApprovalRequest.DAO_KEY, "localTransactionDAO"),
-                EQ(ApprovalRequest.OBJ_ID, ct.getId()),
-                EQ(ApprovalRequest.APPROVER, getJackieId())))
-              .limit(1)
-              .select(new Count());
+        ComplianceTransaction ct = (ComplianceTransaction) obj;
+        Transaction headTx = ct;
+        Count count = (Count) ((DAO) x.get("approvalRequestDAO"))
+          .where(AND(
+            EQ(ApprovalRequest.DAO_KEY, "localTransactionDAO"),
+            EQ(ApprovalRequest.OBJ_ID, ct.getId()),
+            EQ(ApprovalRequest.APPROVER, getJackieId())))
+          .limit(1)
+          .select(new Count());
 
-            if ( count.getValue() == 0 ) {
-              while ( ! SafetyUtil.isEmpty(headTx.getParent())) {
-                headTx = headTx.findParent(x);
-              }
-              ApprovalRequest req = new ApprovalRequest.Builder(x)
-                .setDaoKey("localTransactionDAO")
-                .setObjId(ct.getId())
-                .setApprover(getJackieId())
-                .setDescription("Main Summary txn: "+headTx.getSummary()+" The Id of Summary txn: "+headTx.getId()+ )
-                .build();
-              requestApproval(x, req);
-            }
+        if ( count.getValue() == 0 ) {
+          while ( ! SafetyUtil.isEmpty(headTx.getParent())) {
+            headTx = headTx.findParent(x);
           }
-        }, "Submits an Approval Request to the compliance team for this Compliance transaction");
+          ApprovalRequest req = new ApprovalRequest.Builder(x)
+            .setDaoKey("localTransactionDAO")
+            .setObjId(ct.getId())
+            .setApprover(getJackieId())
+            .setDescription("Main Summary txn: "+headTx.getSummary()+" The Id of Summary txn: "+headTx.getId() )
+            .build();
+
+          requestApproval(x, req);
+        }
       `
+    },
+    {
+      name: 'applyReverseAction',
+      javaCode: '//noop'
+    },
+    {
+      name: 'canExecute',
+      javaCode: 'return true;'
+    },
+    {
+      name: 'describe',
+      javaCode: 'return "";'
     }
   ]
 });
