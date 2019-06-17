4baf53cea7597335c43db29f1f41743c49363e60
blakegreen <blake@nanopay.net>
2019-06-17 15:54:08 -0400

    Added HttpServletResponse error handling to IdentityMindWebAgent

--- nanopay/src/net/nanopay/meter/compliance/identityMind/IdentityMindWebAgent.java
+++ nanopay/src/net/nanopay/meter/compliance/identityMind/IdentityMindWebAgent.java
@@ -8,7 +8,9 @@ import foam.nanos.http.WebAgent;
 import foam.nanos.logger.Logger;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 import net.nanopay.approval.ApprovalStatus;
 import net.nanopay.meter.compliance.ComplianceApprovalRequest;
@@ -24,36 +26,40 @@ public class IdentityMindWebAgent implements WebAgent {
     HttpParameters p = x.get(HttpParameters.class);
     String data = p.getParameter("data");
     HttpServletRequest request = x.get(HttpServletRequest.class);
+    HttpServletResponse response = x.get(HttpServletResponse.class);
     JSONParser jsonParser = new JSONParser();
     jsonParser.setX(x);
 
     try {
-    IdentityMindResponse webhookResponse = (IdentityMindResponse)
+      IdentityMindResponse webhookResponse = (IdentityMindResponse)
         jsonParser.parseString(data, IdentityMindResponse.class);
+      identityMindResponseDAO.put(webhookResponse);
 
-    System.out.println("IDMWEBHOOKBODY###: " + data);
-    System.out.println("IDMWEBHOOKRESPONSE###: " + webhookResponse.toString());
+      System.out.println("IDMWEBHOOKBODY###: " + data);
+      System.out.println("IDMWEBHOOKRESPONSE###: " + webhookResponse.toString());
 
-    ComplianceApprovalRequest approvalRequest = (ComplianceApprovalRequest) approvalRequestDAO.find(
-      EQ(ComplianceApprovalRequest.CAUSE_ID, webhookResponse.getId())
-    );
+      ComplianceApprovalRequest approvalRequest = (ComplianceApprovalRequest) approvalRequestDAO.find(
+        EQ(ComplianceApprovalRequest.CAUSE_ID, webhookResponse.getId())
+      );
 
-    if ( approvalRequest != null ) {
-      System.out.println("APPROVALREQUEST###: " + approvalRequest.toString());
-      if (webhookResponse.getComplianceValidationStatus() == ComplianceValidationStatus.VALIDATED) {
-        approvalRequest.setStatus(ApprovalStatus.APPROVED);
-      } else if (webhookResponse.getComplianceValidationStatus() == ComplianceValidationStatus.REJECTED) {
-        approvalRequest.setStatus(ApprovalStatus.REJECTED);
+      if ( approvalRequest != null ) {
+        System.out.println("APPROVALREQUEST###: " + approvalRequest.toString());
+        if (webhookResponse.getComplianceValidationStatus() == ComplianceValidationStatus.VALIDATED) {
+          approvalRequest.setStatus(ApprovalStatus.APPROVED);
+        } else if (webhookResponse.getComplianceValidationStatus() == ComplianceValidationStatus.REJECTED) {
+          approvalRequest.setStatus(ApprovalStatus.REJECTED);
+        }
+        System.out.println("UPDATEDAPPROVALREQUEST###: " + approvalRequest.toString());
+        approvalRequestDAO.put(approvalRequest);
       }
-      System.out.println("UPDATEDAPPROVALREQUEST###: " + approvalRequest.toString());
-      approvalRequestDAO.put(approvalRequest);
-    }
-
-    identityMindResponseDAO.put(webhookResponse);
     } catch (Exception e) {
       String message = String.format("IdentityMindWebAgent failed.", request.getClass().getSimpleName());
       ((Logger) x.get("logger")).error(message, e);
-      throw new RuntimeException(message);
+      try {
+		    response.sendError(HttpServletResponse.SC_BAD_REQUEST, message);
+	    } catch (IOException e1) {
+		    e1.printStackTrace();
+	    }
     }
   }
 }
