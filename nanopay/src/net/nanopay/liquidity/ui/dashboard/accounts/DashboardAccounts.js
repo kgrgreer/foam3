421cba71b2773683cce9a9c232d84108ba279972
Danny Tharma <daniel@nanopay.net>
2019-06-18 15:31:20 -0400

    Got the find balance

--- nanopay/src/net/nanopay/liquidity/ui/dashboard/accounts/DashboardAccounts.js
+++ nanopay/src/net/nanopay/liquidity/ui/dashboard/accounts/DashboardAccounts.js
@@ -13,6 +13,11 @@ foam.CLASS({
     'foam.mlang.Expressions'
   ],
 
+  imports: [
+    'balanceDAO',
+    'user'
+  ],
+
   documentation: `
     A configurable view to to render a card with 
     configurable contents and rich choice view dropdowns
@@ -43,7 +48,10 @@ foam.CLASS({
     'foam.u2.layout.Rows',
     'foam.u2.ControllerMode',
     'foam.comics.v2.DAOBrowserView',
-    'foam.u2.borders.CardBorder'
+    'foam.u2.borders.CardBorder',
+    'foam.dao.ArraySink',
+    'foam.mlang.sink.GroupBy',
+    'net.nanopay.account.Account'
   ],
   exports: [
     'controllerMode'
@@ -74,6 +82,35 @@ foam.CLASS({
   ],
 
   methods: [
+    function calcTotalBalance() {
+      const self = this;
+      const promisesArray = [];
+      return this.user.accounts.where(this.EQ(this.Account.OWNER, this.user.id))
+        .select(this.GroupBy.create({
+          arg1: this.Account.DENOMINATION,
+          arg2: this.ArraySink.create()
+        }))
+          .then((result) => {
+            const denomKeys = result.groupKeys;
+
+            denomKeys.forEach(denomKey => {
+              const denominatedAccountsArray = result.groups[denomKey].array;
+
+              denominatedAccountsArray.forEach(account => {
+                if ( account.type !== net.nanopay.account.AggregateAccount.name ){
+                  promisesArray.push(account.findBalance(self.__context__));
+                }
+              })
+            })
+
+            // TODO: Account for currency conversion
+            return Promise.all(promisesArray).then(function(balances) {
+              return balances.reduce((total, num) => total + num);
+            });
+          });
+
+    },
+
     function initE() {
       var self = this;
       this.SUPER();
@@ -89,12 +126,11 @@ foam.CLASS({
                       .add(self.CARD_HEADER).addClass(this.myClass('card-header'))
                     .end()
                     .start().addClass(this.myClass('balance'))
-                      // TODO: Work with exchange rates
-                      // .add(data.findBalance(self.__context__).then(balance => self.parseBalanceToDollarString(balance)))
+                      .add(self.calcTotalBalance())
                     .end()
                     .start().addClass(this.myClass('balance-note'))
                       .add(self.BALANCE_NOTE)
-                      // .add(` (${data$denomination})`)
+                      .add(` (${data$denomination})`)
                     .end()
                   .end()
                 .end()
