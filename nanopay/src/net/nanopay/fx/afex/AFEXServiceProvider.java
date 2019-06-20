75a57803a06df89bf118fda060a8443ffd6c8543
Mayowa Olurin <mayowa@nanopay.net>
2019-06-20 13:37:23 +0100

    Added getPayeeInfo to AFEXServiceProvider

--- nanopay/src/net/nanopay/fx/afex/AFEXServiceProvider.java
+++ nanopay/src/net/nanopay/fx/afex/AFEXServiceProvider.java
@@ -3,12 +3,13 @@ package net.nanopay.fx.afex;
 import foam.core.X;
 import foam.core.ContextAwareSupport;
 import foam.dao.DAO;
+import foam.nanos.auth.Address;
+import foam.nanos.auth.User;
+import foam.nanos.logger.Logger;
 import net.nanopay.bank.BankAccount;
 import net.nanopay.fx.FXQuote;
 import net.nanopay.fx.FXService;
 import net.nanopay.payment.PaymentService;
-import foam.nanos.auth.User;
-import foam.nanos.auth.Address;
 import net.nanopay.tx.model.Transaction;
 import java.util.Date;
 
@@ -95,18 +96,60 @@ public class AFEXServiceProvider extends ContextAwareSupport implements FXServic
         AddPayeeResponse addPayeeResponse = this.afexClient.addPayee(addPayeeRequest);
         if ( null == addPayeeResponse ) throw new RuntimeException("Null response got for remote system." );
         if ( addPayeeResponse.getCode() != 200 ) throw new RuntimeException("Unable to create Beneficiary at this time. " +  addPayeeResponse.getInformationMessage());
-
-      } catch(Exception e) {
-        //Log here
+      } catch(Throwable t) {
+        ((Logger) x.get("logger")).error("Error creating AFEX beneficiary.", t);
       }
     }
   }
 
-  public void updatePayee(long userId, long bankAccount, long sourceUser) throws RuntimeException {}
+  public void updatePayee(long userId, long bankAccountId, long sourceUser) throws RuntimeException {
+    User user = User.findUser(x, userId);
+    if ( null == user ) throw new RuntimeException("Unable to find User " + userId);
+
+    Address userAddress = user.getAddress(); 
+    if ( null == userAddress ) throw new RuntimeException("User Address is null " + userId );
+    
+    BankAccount bankAccount = (BankAccount) ((DAO) x.get("localAccountDAO")).find(bankAccountId);
+    if ( null == bankAccount ) throw new RuntimeException("Unable to find Bank account: " + bankAccountId );
+
+    Address bankAddress = bankAccount.getBankAddress(); 
+    if ( null == bankAddress ) throw new RuntimeException("Bank Account Address is null " + bankAccountId );
+
+    UpdatePayeeRequest updatePayeeRequest = new UpdatePayeeRequest();
+    updatePayeeRequest.setBankAccountNumber(bankAccount.getAccountNumber());
+    updatePayeeRequest.setBankCountryCode(bankAddress.getCountryId());
+    //updatePayeeRequest.setBankName(bankAccount.get);
+    updatePayeeRequest.setBankRoutingCode(bankAccount.getRoutingCode(this.x));
+    updatePayeeRequest.setBeneficiaryAddressLine1(bankAddress.getAddress());
+    updatePayeeRequest.setBeneficiaryCity(userAddress.getCity());
+    updatePayeeRequest.setBeneficiaryCountryCode(userAddress.getCountryId());
+    updatePayeeRequest.setBeneficiaryName(user.getLegalName());
+    updatePayeeRequest.setBeneficiaryPostalCode(userAddress.getPostalCode());
+    updatePayeeRequest.setBeneficiaryRegion(userAddress.getRegionId());
+    updatePayeeRequest.setCurrency(bankAccount.getDenomination());
+    updatePayeeRequest.setVendorId(String.valueOf(userId));
+
+    try {
+      UpdatePayeeResponse updatePayeeResponse = this.afexClient.updatePayee(updatePayeeRequest);
+      if ( null == updatePayeeResponse ) throw new RuntimeException("Null response got for remote system." );
+      if ( updatePayeeResponse.getCode() != 0 ) throw new RuntimeException("Unable to update Beneficiary at this time. " +  updatePayeeResponse.getInformationMessage());
+    } catch(Throwable t) {
+      ((Logger) x.get("logger")).error("Error creating AFEX beneficiary.", t);
+    }    
+
+  }
 
   public void deletePayee(long payeeUserId, long payerUserId) throws RuntimeException {}
 
-  public GetPayeeInfoResponse getPayeeInfo(String payeeUserId) throws RuntimeException { return null; }
+  public GetPayeeInfoResponse getPayeeInfo(String payeeUserId) throws RuntimeException {
+    GetPayeeInfoResponse payeeInfo = null;
+    try {
+      payeeInfo = this.afexClient.getPayeeInfo(payeeUserId);
+    } catch(Throwable t) {
+      ((Logger) x.get("logger")).error("Error creating AFEX beneficiary.", t);
+    }
+    return payeeInfo;
+  }
 
   public Transaction submitPayment(Transaction transaction) throws RuntimeException{
     // TODO
