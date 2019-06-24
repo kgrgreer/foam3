cc1b94c43e386d899d8649609c412f84b5465f26
Nick <nick@nanopay.net>
2019-06-24 16:46:28 -0400

    Working on install script

--- deploy/bin/install.sh
+++ deploy/bin/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-NANOPAY_HOME=
+NANOPAY_HOME=/opt/nanopay
+NANOPAY_TARBALL=\~/nanopay-deploy.tar.gz
+NANOPAY_REMOTE_OUTPUT=\~/tar_extract
 
 function quit {
     echo "ERROR :: Install Failed"
@@ -11,18 +13,35 @@ function usage {
     echo "Usage: $0 [OPTIONS]"
     echo ""
     echo "Options are:"
-    echo "  -h : Print usage information."
-    echo "  -N <nanopay_home>   : Nanopay home directory."
+    echo "  -h                  : Print usage information."
+    echo "  -N <nanopay_home>   : Remote Nanopay home directory, default to /opt/nanopay"
+    echo "  -I <path>           : Remote location of tarball, default to ~/nanopay/nanopay-deploy.tar.gz"
+    echo "  -O <path>           : Remote directory tarball is extracted to, default to ~/tar_extract"
     echo ""
 }
 
-while getopts "hN:" opt ; do
+while getopts "hN:O:I:" opt ; do
     case $opt in
-        N) NANOPAY_HOME=$OPTARG;;
         h) usage; exit 0;;
+        N) NANOPAY_HOME=$OPTARG;;
+        O) NANOPAY_REMOTE_OUTPUT=$OPTARG;;
+        I) NANOPAY_TARBALL=$OPTARG;;
         ?) usage; exit 0;;
    esac
 done
+
+if [ ! -f ${NANOPAY_TARBALL} ]; then
+    echo "ERROR :: Tarball ${NANOPAY_TARBALL} doesn't exist on remote server"
+    quit 1
+fi
+
+if [ ! -d ${NANOPAY_REMOTE_OUTPUT} ]; then
+    mkdir -p ${NANOPAY_REMOTE_OUTPUT}
+fi
+
+tar -xzf ${NANOPAY_TARBALL} -C ${NANOPAY_REMOTE_OUTPUT}
+
+exit 0
 
 NANOPAY_HOME_PARENT = $(realpath $(dirname ${NANOPAY_HOME}))
 
@@ -33,30 +52,30 @@ fi
 if [ ! -d $NANOPAY_HOME ]; then
     if [ ! -w ${NANOPAY_HOME_PARENT} ]; then
         echo "ERROR :: Can't write to ${NANOPAY_HOME_PARENT}"
-        quit 1;
+        quit 1
     fi
     mkdir -p ${NANOPAY_HOME}
 fi
 
 if [ ! -w $NANOPAY_HOME ]; then
-    echo "ERROR :: Can't write to $(realpath ${NANOPAY_HOME})";
-    quit 1;
+    echo "ERROR :: Can't write to $(realpath ${NANOPAY_HOME})"
+    quit 1
 fi
 
 if [ ! -d ${NANOPAY_HOME}/lib ]; then
     mkdir -p ${NANOPAY_HOME}/lib
 fi
 
-cp -r ../lib/* ${NANOPAY_HOME}/lib
+cp -r ${NANOPAY_REMOTE_OUTPUT}/lib/* ${NANOPAY_HOME}/lib
 
 if [ ! -d ${NANOPAY_HOME}/bin ]; then
     mkdir -p ${NANOPAY_HOME}/bin
 fi
 
-cp -r ../bin/* ${NANOPAY_HOME}/bin
+cp -r ${NANOPAY_REMOTE_OUTPUT}/bin/* ${NANOPAY_HOME}/bin
 
 if [ ! -d ${NANOPAY_HOME}/etc ]; then
     mkdir -p ${NANOPAY_HOME}/etc
 fi
 
-cp -r ../etc/* ${NANOPAY_HOME}/etc
+cp -r ${NANOPAY_REMOTE_OUTPUT}/etc/* ${NANOPAY_HOME}/etc
---
+++ tools/deployment/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+NANOPAY_HOME=/opt/nanopay
+NANOPAY_TARBALL=nanopay-deploy.tar.gz
+NANOPAY_REMOTE_OUTPUT=\~/
+REMOTE_URL=
+SSH_KEY=
+
+function quit {
+    echo "ERROR :: Install Failed"
+    exit $1
+}
+
+function usage {
+    echo "Usage: $0 [OPTIONS]"
+    echo ""
+    echo "Options are:"
+    echo "  -h                  : Print usage information."
+    echo "  -N <nanopay_home>   : Remote Nanopay home directory, default to /opt/nanopay"
+    echo "  -O <path>           : Remote Location to put tarball, default to ~/"
+    echo "  -T <tarball>        : Name of tarball, looks in target/package, default to nanopay-deploy.tar.gz"
+    echo "  -I <ssh-key>        : SSH Key to use to connect to remote server"
+    echo "  -W <web-address>    : Web address to connect to"
+    echo ""
+}
+
+while getopts "hN:T:W:O:I:" opt ; do
+    case $opt in
+        h) usage; exit 0;;
+        N) NANOPAY_HOME=${OPTARG};;
+        O) NANOPAY_REMOTE_OUTPUT=${OPTARG};;
+        T) NANOPAY_TARBALL=${OPTARG};;
+        I) SSH_KEY=${OPTARG};;
+        W) REMOTE_URL=${OPTARG};;
+        ?) usage; exit 0;;
+   esac
+done
+
+NANOPAY_TARBALL_PATH="target/package/${NANOPAY_TARBALL}"
+
+if [ ! -f $NANOPAY_TARBALL_PATH ]; then
+    echo "ERROR :: Tarball $(realpath ${NANOPAY_TARBALL_PATH}) doesn't exist"
+    quit 1
+fi
+
+echo "INFO :: Copying ${NANOPAY_TARBALL_PATH} to ${REMOTE_URL}:${NANOPAY_REMOTE_OUTPUT}"
+
+scp -i ${SSH_KEY} ${NANOPAY_TARBALL_PATH} "${REMOTE_URL}:${NANOPAY_REMOTE_OUTPUT}/${NANOPAY_TARBALL}"
+
+if [ ! $? -eq 0 ]; then
+    echo "ERROR :: Failed copying tarball to remote server"
+    quit 1
+else
+    echo "INFO :: Successfully copied tarball to remote server"
+fi
+
+echo "INFO :: Running ssh -i ${SSH_KEY} ${REMOTE_URL} ./tools/deployment/installremote.sh -I ${NANOPAY_REMOTE_OUTPUT}/${NANOPAY_TARBALL} -N ${NANOPAY_HOME} -O tar_extract"
+
+ssh -i ${SSH_KEY} ${REMOTE_URL} "bash -s" -- < ./tools/deployment/installremote.sh -I ${NANOPAY_REMOTE_OUTPUT}/${NANOPAY_TARBALL} -N ${NANOPAY_HOME} -O tar_extract
