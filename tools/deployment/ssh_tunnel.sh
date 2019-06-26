de2c722753bdaf5cef2550898cf27d41a4fe37ab
Nick <nick@nanopay.net>
2019-06-26 11:46:53 -0400

    Added option to ssh_tunnel.sh to see if connection is running

--- tools/deployment/ssh_tunnel.sh
+++ tools/deployment/ssh_tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MODE=
-SOCKET_FILE=./tools/deployment/socket
+SOCKET_FILE=./ssh/sockets/nanopay
 RC_FILE=./tools/deployment/remoterc
 
 function usage {
@@ -10,16 +10,18 @@ function usage {
     echo "Options are:"
     echo "  -e                  : Exit ssh tunnel"
     echo "  -h                  : Print usage information."
+    echo "  -i                  : Test if ssh tunnel is connected"
     echo "  -R <filepath>       : remoterc file to load, default to ./tools/deployment/remote"
     echo "  -s                  : Start ssh tunnel"
     echo "  -S <filepath>       : socket file location to use, default to ./tools/deployment/socket"
     echo ""
 }
 
-while getopts "ehR:sS:" opt ; do
+while getopts "ehiR:sS:" opt ; do
     case $opt in
         e) MODE=1;;
         h) usage; exit 0;;
+        i) MODE=2;;
         R) RC_FILE=$OPTARG;;
         s) MODE=0;;
         S) SOCKET_FILE=$OPTARG;;
@@ -30,6 +32,21 @@ done
 if [ -z $MODE ]; then
     echo "ERROR :: No mode specified"
     exit 1
+fi
+
+if [ $MODE -eq 2 ]; then
+    if [ -S $SOCKET_FILE ]; then
+        echo "INFO :: SSH Tunnel is connected"
+        exit 0
+    else
+        echo "INFO :: SSH Tunnel is not connected"
+        exit 1
+    fi
+fi
+
+SOCKET_DIR=$(dirname $SOCKET_FILE)
+if [ ! -d $SOCKET_DIR ]; then
+    mkdir -p $SOCKET_DIR
 fi
 
 if [ -f $RC_FILE ]; then
@@ -43,7 +60,7 @@ if [ $MODE -eq 0 ]; then
         echo "ERROR :: ${SOCKET_FILE} already exists, close connection first"
         exit 1
     fi
-    ssh -f -N -M -S $SOCKET_FILE -L ${LOCAL_PORT}:127.0.0.1:${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_URL} -i ${SSH_KEY} 2>&1 > /dev/null
+    ssh -f -N -M -S $SOCKET_FILE -L ${LOCAL_PORT}:127.0.0.1:${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_URL} -i ${SSH_KEY} > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "INFO :: Connection started"
     else
@@ -56,9 +73,9 @@ else
         echo "ERROR :: ${SOCKET_FILE} doesn't exist, can't close connection"
         exit 1
     fi
-    ssh -S $SOCKET_FILE -O exit ${REMOTE_USER}@${REMOTE_URL} 2>&1 > /dev/null
+    ssh -S $SOCKET_FILE -O exit ${REMOTE_USER}@${REMOTE_URL} > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "INFO :: Connection started"
+        echo "INFO :: Connection exited"
     else
         echo "ERROR :: Error occured attempting to close connection"
         exit 1
