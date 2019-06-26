6629bf2eb3d6cfc34419ac57b06d3e806b7f390c
Michal Pasternak <michal.pasternak@nanopay.net>
2019-06-26 13:59:35 -0400

    fixes to liquidity rule

--- nanopay/src/net/nanopay/liquidity/LiquidityRule.js
+++ nanopay/src/net/nanopay/liquidity/LiquidityRule.js
@@ -18,30 +18,38 @@ foam.CLASS({
     'net.nanopay.liquidity.LiquidityService',
     'net.nanopay.tx.cico.CITransaction',
     'net.nanopay.tx.cico.COTransaction',
+    'net.nanopay.tx.DigitalTransaction',
     'net.nanopay.tx.model.Transaction',
-    'net.nanopay.util.Frequency'
+    'net.nanopay.tx.model.TransactionStatus',
+    'net.nanopay.util.Frequency',
+    'foam.nanos.logger.Logger'
   ],
 
   methods: [
     {
       name: 'applyAction',
       javaCode: `
-
-        if ( obj instanceof DigitalTransaction || obj instanceof CITransaction || obj instanceof COTransaction ) {
+              Logger logger = (Logger) x.get("logger");
+              logger.log("bro 1");
+        if ( (obj instanceof DigitalTransaction || obj instanceof CITransaction || obj instanceof COTransaction) ) {
           Transaction txn = (Transaction) obj;
+          if ( ! (txn.getStatus() == TransactionStatus.COMPLETED) )
+            return;
+                          logger.log("bro 2");
           LiquidityService ls = (LiquidityService) x.get("liquidityService");
           Account source = txn.findSourceAccount(x);
           Account destination = txn.findDestinationAccount(x);
-          if ( ! SafetyUtil.equals(source.getOwner(), destination.getOwner()) {
+          if ( (! SafetyUtil.equals(source.getOwner(), destination.getOwner()) ) || txn instanceof DigitalTransaction ) {
             agency.submit(x, new ContextAgent() {
               @Override
               public void execute(X x) {
+              logger.log("I am running bro");
                 if( source instanceof DigitalAccount )
                   ls.liquifyAccount(source.getId(), Frequency.PER_TRANSACTION, -txn.getAmount());
                 if ( destination instanceof DigitalAccount )
                   ls.liquifyAccount(destination.getId(), Frequency.PER_TRANSACTION, txn.getAmount());
               }
-            }
+            });
           }
         }
       `
