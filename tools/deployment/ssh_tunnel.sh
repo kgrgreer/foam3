e1bf9d15d248d2a90e6f1d2cccd2179201263199
Nick <nick@nanopay.net>
2019-06-26 12:00:47 -0400

    Moved remoterc to ~/.config/nanopay/remoterc, made file optional

--- tools/deployment/ssh_tunnel.sh
+++ tools/deployment/ssh_tunnel.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
 MODE=
-SOCKET_FILE=./ssh/sockets/nanopay
-RC_FILE=./tools/deployment/remoterc
+SOCKET_FILE=~/.ssh/sockets/nanopay
+RC_FILE=~/.config/nanopay/remoterc
+
+REMOTE_USER=
+REMOTE_URL=
+SSH_KEY=
+LOCAL_PORT=
+REMOTE_PORT=
 
 function usage {
     echo "Usage: $0 [OPTIONS]"
     echo ""
     echo "Options are:"
+    echo "  -C <filepath>       : remoterc file to load, default to ./config/nanopay/remoterc"
     echo "  -e                  : Exit ssh tunnel"
     echo "  -h                  : Print usage information."
     echo "  -i                  : Test if ssh tunnel is connected"
-    echo "  -R <filepath>       : remoterc file to load, default to ./tools/deployment/remote"
+    echo "  -I <filepath>       : SSH Key to use"
+    echo "  -L <port>           : Local port to use"
+    echo "  -R <port>           : Remote port to use"
     echo "  -s                  : Start ssh tunnel"
-    echo "  -S <filepath>       : socket file location to use, default to ./tools/deployment/socket"
+    echo "  -S <filepath>       : socket file location to use, default to ./ssh/sockets/nanopay"
+    echo "  -U <user>           : User to connect to"
+    echo "  -W <url>            : Remote URL to connect to"
     echo ""
 }
 
 while getopts "ehiR:sS:" opt ; do
     case $opt in
+        C) RC_FILE=$OPTARG;;
         e) MODE=1;;
         h) usage; exit 0;;
         i) MODE=2;;
-        R) RC_FILE=$OPTARG;;
+        I) SSH_KEY=$OPTARG;;
+        L) LOCAL_PORT=$OPTARG;;
+        R) REMOTE_PORT=$OPTARG;;
         s) MODE=0;;
         S) SOCKET_FILE=$OPTARG;;
+        U) REMOTE_USER=$OPTARG;;
+        W) REMOTE_URL=$OPTARG;;
         ?) usage; exit 0;;
    esac
 done
@@ -67,7 +83,7 @@ if [ $MODE -eq 0 ]; then
         echo "ERROR :: Error occured attempting to start connection"
         exit 1
     fi
-else
+elif [ $MODE -eq 1 ]; then
     echo "INFO :: Exiting ssh tunnel "
     if [ ! -S $SOCKET_FILE ]; then
         echo "ERROR :: ${SOCKET_FILE} doesn't exist, can't close connection"
@@ -80,6 +96,9 @@ else
         echo "ERROR :: Error occured attempting to close connection"
         exit 1
     fi
+else
+    echo "ERROR :: Unknown Mode, exiting"
+    exit 1
 fi
 
 exit 0
