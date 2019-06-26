de8e98c71bbe003ae81539e01fd2906ff9b6a365
penzital <penzital888@gmail.com>
2019-06-26 10:53:24 -0400

    modify KotakPlanDAO and BankAccount.findDefault to slightly improve performance

--- nanopay/src/net/nanopay/tx/KotakFxTransactionPlanDAO.js
+++ nanopay/src/net/nanopay/tx/KotakFxTransactionPlanDAO.js
@@ -42,55 +42,57 @@ foam.CLASS({
       name: 'put_',
       javaCode: `
       TransactionQuote quote = (TransactionQuote) obj;
-      Transaction request = quote.getRequestTransaction();
-      Account sourceAccount = (Account) ((DAO) x.get("localAccountDAO")).inX(x).find(request.getSourceAccount());
-      Account destinationAccount = (Account) ((DAO) x.get("localAccountDAO")).inX(x).find(request.getDestinationAccount());
-      User kotakOwnerCA = (User) ((DAO) x.get("localUserDAO")).find(KOTAK_OWNER_CA_ID);
-      User KotakPartnerIN = (User) ((DAO) x.get("localUserDAO")).find(KOTAK_PARTNER_IN_ID);
-      BankAccount kotakCAbank = BankAccount.findDefault(x, kotakOwnerCA, "CAD");
-      BankAccount kotakINbank = BankAccount.findDefault(x, kotakOwnerCA, "INR");
-      BankAccount kotakINPartnerBank = BankAccount.findDefault(x, KotakPartnerIN, "INR");
+          Transaction request = quote.getRequestTransaction();
+          Account sourceAccount = (Account) ((DAO) x.get("localAccountDAO")).inX(x).find(request.getSourceAccount());
+          Account destinationAccount = (Account) ((DAO) x.get("localAccountDAO")).inX(x).find(request.getDestinationAccount());
 
-      if ( sourceAccount instanceof DigitalAccount && destinationAccount instanceof INBankAccount &&
-      sourceAccount.getDenomination().equalsIgnoreCase("CAD") && destinationAccount.getDenomination().equalsIgnoreCase("INR") &&
-      kotakCAbank != null && kotakCAbank instanceof CABankAccount &&
-      kotakINbank != null && kotakINbank instanceof INBankAccount ) {
-        Transaction txn;
-        // txn 1: CA digital -> Kotak CA bank
-        TransactionQuote q1 = new TransactionQuote.Builder(x).build();
-        q1.copyFrom(quote);
-        Transaction t1 = new Transaction.Builder(x).build();
-        t1.copyFrom(request);
-        t1.setDestinationAccount(kotakCAbank.getId());
-        t1.setDestinationCurrency(request.getSourceCurrency());
-        q1.setRequestTransaction(t1);
-        TransactionQuote c1 = (TransactionQuote) ((DAO) x.get("localTransactionQuotePlanDAO")).put_(x, q1);
-        Transaction cashOutPlan = c1.getPlan();
-        if ( cashOutPlan != null ) {
-          txn = (Transaction) cashOutPlan.fclone();
-        } else {
-          return super.put_(x, quote);
-        }
-        // txn 2: Kotak CA bank -> Kotak IN bank (manual FX rate)
-        KotakFxTransaction t2 = new KotakFxTransaction.Builder(x).build();
-        t2.copyFrom(request);
-        t2.addLineItems( new TransactionLineItem[] { new ETALineItem.Builder(x).setEta(/* 2 days */ 172800000L).build()}, null);
-        t2.setIsQuoted(true);
-        t2.setSourceAccount(kotakCAbank.getId());
-        t2.setDestinationAccount(kotakINbank.getId());
-        txn.addNext(t2);
-        // txn 3: Kotak IN bank -> destination IN bank
-        KotakCOTransaction t3 = new KotakCOTransaction.Builder(x).build();
-        t3.copyFrom(request);
-        t3.addLineItems( new TransactionLineItem[] { new ETALineItem.Builder(x).setEta(/* 2 days */ 172800000L).build()}, null);
-        t3.setIsQuoted(true);
-        t3.setSourceAccount(kotakINPartnerBank.getId());
-        t3.setSourceCurrency(request.getDestinationCurrency());
-        txn.addNext(t3);
-        txn.setIsQuoted(true);
-        quote.addPlan(txn);
-      }
-       return super.put_(x, quote);
+          if ( sourceAccount instanceof DigitalAccount && destinationAccount instanceof INBankAccount &&
+          sourceAccount.getDenomination().equalsIgnoreCase("CAD") && destinationAccount.getDenomination().equalsIgnoreCase("INR") ) {
+
+            User kotakOwnerCA = (User) ((DAO) x.get("localUserDAO")).find(KOTAK_OWNER_CA_ID);
+            User KotakPartnerIN = (User) ((DAO) x.get("localUserDAO")).find(KOTAK_PARTNER_IN_ID);
+            BankAccount kotakCAbank = BankAccount.findDefault(x, kotakOwnerCA, "CAD");
+            BankAccount kotakINbank = BankAccount.findDefault(x, kotakOwnerCA, "INR");
+            BankAccount kotakINPartnerBank = BankAccount.findDefault(x, KotakPartnerIN, "INR");
+            
+            if (kotakCAbank != null && kotakCAbank instanceof CABankAccount &&
+              kotakINbank != null && kotakINbank instanceof INBankAccount ) return getDelegate().put_(x, quote);
+            Transaction txn;
+            // txn 1: CA digital -> Kotak CA bank
+            TransactionQuote q1 = new TransactionQuote.Builder(x).build();
+            q1.copyFrom(quote);
+            Transaction t1 = new Transaction.Builder(x).build();
+            t1.copyFrom(request);
+            t1.setDestinationAccount(kotakCAbank.getId());
+            t1.setDestinationCurrency(request.getSourceCurrency());
+            q1.setRequestTransaction(t1);
+            TransactionQuote c1 = (TransactionQuote) ((DAO) x.get("localTransactionQuotePlanDAO")).put_(x, q1);
+            Transaction cashOutPlan = c1.getPlan();
+            if ( cashOutPlan != null ) {
+              txn = (Transaction) cashOutPlan.fclone();
+            } else {
+              return super.put_(x, quote);
+            }
+            // txn 2: Kotak CA bank -> Kotak IN bank (manual FX rate)
+            KotakFxTransaction t2 = new KotakFxTransaction.Builder(x).build();
+            t2.copyFrom(request);
+            t2.addLineItems( new TransactionLineItem[] { new ETALineItem.Builder(x).setEta(/* 2 days */ 172800000L).build()}, null);
+            t2.setIsQuoted(true);
+            t2.setSourceAccount(kotakCAbank.getId());
+            t2.setDestinationAccount(kotakINbank.getId());
+            txn.addNext(t2);
+            // txn 3: Kotak IN bank -> destination IN bank
+            KotakCOTransaction t3 = new KotakCOTransaction.Builder(x).build();
+            t3.copyFrom(request);
+            t3.addLineItems( new TransactionLineItem[] { new ETALineItem.Builder(x).setEta(/* 2 days */ 172800000L).build()}, null);
+            t3.setIsQuoted(true);
+            t3.setSourceAccount(kotakINPartnerBank.getId());
+            t3.setSourceCurrency(request.getDestinationCurrency());
+            txn.addNext(t3);
+            txn.setIsQuoted(true);
+            quote.addPlan(txn);
+          }
+           return super.put_(x, quote);
       `
     },
   ]
