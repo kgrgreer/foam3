f54a5793d353bfcba76918a03954528b649498c2
Nick <nick@nanopay.net>
2019-07-02 15:17:08 -0400

    Added systemd file

--- deploy/bin/install.sh
+++ deploy/bin/install.sh
@@ -4,6 +4,7 @@ NANOPAY_HOME=
 NANOPAY_ROOT=/opt/nanopay
 NANOPAY_TARBALL=
 NANOPAY_REMOTE_OUTPUT=/tmp/tar_extract
+NANOPAY_SERVICE_FILE=/lib/systemd/system/nanopay.service
 
 function quit {
     echo "ERROR :: Remote Install Failed"
@@ -33,37 +34,6 @@ done
 
 NANOPAY_CURRENT_VERSION=$(readlink -f ${NANOPAY_ROOT} | awk -F- '{print $NF}')
 NANOPAY_NEW_VERSION=$(echo ${NANOPAY_HOME} | awk -F- '{print $NF}')
-
-function setupNanopaySymLink {
-    if [ ! -z $NANOPAY_CURRENT_VERSION ]; then
-        if [ "$NANOPAY_CURRENT_VERSION" = "$NANOPAY_NEW_VERSION" ]; then
-            echo "INFO :: Found v${NANOPAY_CURRENT_VERSION}, leaving installed"
-            return 0
-        fi
-        echo "INFO :: Found v${NANOPAY_CURRENT_VERSION}, replacing with v${NANOPAY_NEW_VERSION}"
-    else
-        echo "INFO :: No version found, installing v${NANOPAY_NEW_VERSION}"
-    fi
-
-    if [ -h ${NANOPAY_ROOT} ]; then
-        unlink ${NANOPAY_ROOT}
-    fi
-
-    ln -s ${NANOPAY_HOME} ${NANOPAY_ROOT}
-}
-
-function setupUserAndPermissions {
-    echo "INFO :: Setting file permissions"
-
-    id -u nanopay > /dev/null
-    if [ ! $? -eq 0 ]; then
-        echo "INFO :: User nanopay not found, creating user nanopay"
-        useradd nanopay
-        usermod -s /bin/false -L nanopay
-    fi
-
-    chown -R nanopay:nanopay $NANOPAY_HOME
-}
 
 function installFiles {
     if [ -z $NANOPAY_HOME ]; then
@@ -101,6 +71,52 @@ function installFiles {
     cp -r ${NANOPAY_REMOTE_OUTPUT}/etc/* ${NANOPAY_HOME}/etc
 }
 
+function setupUserAndPermissions {
+    echo "INFO :: Setting file permissions"
+
+    id -u nanopay > /dev/null
+    if [ ! $? -eq 0 ]; then
+        echo "INFO :: User nanopay not found, creating user nanopay"
+        useradd nanopay
+        usermod -s /bin/false -L nanopay
+    fi
+
+    chown -R nanopay:nanopay $NANOPAY_HOME
+}
+
+function setupNanopaySymLink {
+    if [ ! -z ${NANOPAY_CURRENT_VERSION} ]; then
+        if [ "${NANOPAY_CURRENT_VERSION}" = "${NANOPAY_NEW_VERSION}" ]; then
+            echo "INFO :: Found v${NANOPAY_CURRENT_VERSION}, leaving installed"
+            return 0
+        fi
+        echo "INFO :: Found v${NANOPAY_CURRENT_VERSION}, replacing with v${NANOPAY_NEW_VERSION}"
+    else
+        echo "INFO :: No version found, installing v${NANOPAY_NEW_VERSION}"
+    fi
+
+    if [ -h ${NANOPAY_ROOT} ]; then
+        unlink ${NANOPAY_ROOT}
+    fi
+
+    ln -s ${NANOPAY_HOME} ${NANOPAY_ROOT}
+}
+
+function setupSystemd {
+    systemctl list-units | grep nanopay.service > /dev/null
+    if [ $? -eq 0 ]; then
+        sudo systemctl stop nanopay
+        sudo systemctl disable nanopay
+    fi
+
+    if [ ! -h ${NANOPAY_SERVICE_FILE} ]; then
+        sudo ln -s ${NANOPAY_HOME}/etc/nanopay.service ${NANOPAY_SERVICE_FILE}
+    fi
+
+    sudo systemctl enable nanopay
+    sudo systemctl start nanopay
+}
+
 echo "INFO :: Installing nanopay on remote server"
 
 if [ ! -f ${NANOPAY_TARBALL} ]; then
@@ -128,5 +144,7 @@ installFiles
 setupUserAndPermissions
 
 setupNanopaySymLink
+
+setupSystemd
 
 exit 0
