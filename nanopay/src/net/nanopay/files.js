0bca75e26dacec006726803ff336e84de5b94330
Blake Green <blake@nanopay.net>
2019-07-02 18:12:01 -0400

    Send SecureFact results to IdentityMind (#7281)
    * Added AbstractIdentityMindComplianceRuleAction with method fetchDowJonesMatches to pass the corresponding amount of dow jones matches to the memo3 field in IdentityMindRequests
    
    * Added fetchMemos function in AbstractIdentityMindComplianceRuleAction which fetchs all corresponding results from dow jones and securefact. fetchMemos returns a Map with all the results and gets passed into the identityMindService and set in the request
    
    * Added null checks to methods in AbstractIdentityMindComplianceRuleAction
    
    * Took out entityName from fetchMemo queries in AbstractIdentityMindComplianceRuleAction
    
    * Changed fetchDowJonesMatches to fetch the most recent response using .orderBy(DESC(DowJonesResponse.SEARCH_DATE)) in AbstractIdentityMindComplianceRuleAction
    
    * Checking if sink.getArray().size() > 0 before fetching first element in sink array when fetching corresponding DowJonesResponse in AbstractIdentityMindComplianceRuleAction, took out unused HashMap imports in ConsumerKYCValidator and MerchantKYCValidator

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -1034,6 +1034,7 @@ FOAM_FILES([
   { name: 'net/nanopay/liquidity/ui/account/ThresholdRules' },
 
   // identitymind
+  { name: 'net/nanopay/meter/compliance/identityMind/AbstractIdentityMindComplianceRuleAction' },
   { name: 'net/nanopay/meter/compliance/identityMind/AutomatedReviewEngineResult' },
   { name: 'net/nanopay/meter/compliance/identityMind/ComplianceTransactionValidator' },
   { name: 'net/nanopay/meter/compliance/identityMind/ConditionResult' },
