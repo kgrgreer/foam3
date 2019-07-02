e7f585ea913d1f2e7a866fdbbdd9da3880d276d4
Danny Tharma <daniel@nanopay.net>
2019-07-02 14:24:20 -0400

    Repegged updated foam

--- nanopay/src/net/nanopay/liquidity/ui/dashboard/cicoShadow/DashboardCicoShadow.js
+++ nanopay/src/net/nanopay/liquidity/ui/dashboard/cicoShadow/DashboardCicoShadow.js
@@ -96,6 +96,10 @@ foam.CLASS({
 
   properties: [
     {
+      class: 'Date',
+      name: 'timeFrame',
+    },
+    {
       class: 'Int',
       name: 'yItemsLimit',
       value: 3
@@ -143,63 +147,10 @@ foam.CLASS({
         );
       }
     },
-    {
-      class: 'Map',
-      name: 'config',
-      factory: function() {
-        var self = this;
-        return {
-          type: 'horizontalBar',
-          data: {
-            labels: [ 'test1', 'test2', 'tings'],
-            datasets: [
-              {
-                label: 'Dataset 1',
-                backgroundColor: '#b8e5b3',
-                data: [
-                  0.4,
-                  0.3,
-                  0.1,
-                ]
-              }, {
-                label: 'Dataset 2',
-                backgroundColor: '#f79393',
-                data: [
-                  0.4,
-                  0.3,
-                  0.6,
-                ]
-              }
-            ] 
-          },
-          options: {
-            elements: {
-              rectangle: {
-                borderWidth: 2,
-              }
-            }, 
-            scales: {
-              xAxes: [{
-                barPercentage: 0.5,
-                barThickness: 6,
-                maxBarThickness: 8,
-                gridLines: {
-                    offsetGridLines: true
-                }
-            }]
-            } 
-          }
-        }
-      }
-    },
-    {
-      class: 'Map',
-      name: 'styling',
-      value: {}
-    },
+    // groupBy gLang, SUM
     {
       class: 'String',
-      name: 'timeFrame',
+      name: 'timeFrequency',
       value: 'Weekly',
       view: {
         class: 'foam.u2.view.ChoiceView',
@@ -213,114 +164,66 @@ foam.CLASS({
     },
     {
       name: 'dateRange',
-      expression: function (timeFrame, yItemsLimit) {
-        var today = new Date();
-        switch (timeFrame) {
-          case 'Weekly':
-            var daysToRewind = 7 * (yItemsLimit - 1);
+      expression: function ( timeFrequency, yItemsLimit, timeFrame ) {
+        // TODO: Include timeFrame
+        var max = new Date();
+        var min = new Date();
 
-            var sun = new Date();
-            sun.setHours(0, 0, 0, 0);
-            sun.setDate(sun.getDate() - sun.getDay());
-            sun.setDate(sun.getDate() - daysToRewind);
+        switch (timeFrequency) {
+          case 'Weekly':            
+            var daysFromSun = max.getDay();
+            var daysToRewind = 7 * (yItemsLimit - 1) + daysFromSun;
+            min.setDate(max.getDate() - daysToRewind);
+            break;
 
-            var sat = new Date(sun.getTime());
-            sat.setDate(sat.getDate() + 6);
-            sat.setHours(23, 59, 59, 999);
-
-            return {
-              min: sun,
-              max: sat
-            };
-          case 'Monthly':
-            var monthsToRewind = yItemsLimit - 1;
-
-            var min = new Date();
-            min.setMonth(min.getMonth() - monthsToRewind);
+          case 'Monthly':            
             min.setDate(1);
-            min.setHours(0, 0, 0, 0);
+            min.setMonth(max.getMonth() - (yItemsLimit - 1))
+            break;
 
-            var max = new Date(min.getTime());
-            max.setMonth(max.getMonth() + 1);
-            max.setDate(0);
-            max.setHours(23, 59, 59, 999);
-
-            return {
-              min: min,
-              max: max
-            };
           case 'Quarterly':
-            var monthsToRewind = yItemsLimit * 3;
+            var monthsFromQStart = max.getMonth() % 3;
+            var monthsToRewind = 3 * (yItemsLimit - 1) + monthsFromQStart;
+            min.setDate(max.getMonth() - monthsToRewind);
+            break;
 
-            var currQuarterEndMonth = today.getMonth() + (2 - today.getMonth() % 3) + 1;
-            var currQuarterEnd = new Date(today.getFullYear(), currQuarterEndMonth, 0);
-
-            var min = new Date(currQuarterEnd.getTime());
-            min.setMonth(min.getMonth() - (monthsToRewind - 1));
-            min.setDate(1);
-            var max = new Date(min.getTime());
-            max.setMonth(max.getMonth() + 3);
-            max.setDate(0);
-            max.setHours(23, 59, 59, 999);
-
-            return {
-              min: min,
-              max: max
-            };
           case 'Annually':
-            var start = new Date();
-            start.setFullYear(start.getFullYear() - this.rewindFactor + 1);
-            start.setMonth(0);
-            start.setDate(1);
-            start.setHours(0, 0, 0, 0);
-
-            var end = new Date(start.getTime());
-            end.setMonth(11);
-            end.setDate(31);
-            end.setHours(23, 59, 59, 999);
-
-            return {
-              min: start,
-              max: end
-            };
+            min.setDate(1);
+            min.setFullYear(max.getFullYear() - (yItemsLimit - 1));
+            break;
         }
+        
+        return {
+          min,
+          max
+        };
       }
     },
   ],
 
   methods: [
     function initE() {
-      // this.account$.sub(this.dataUpdate);
-      // this.dateRange$.sub(this.dataUpdate);
-      // this.dataUpdate();
-
       this.addClass(this.myClass())
         .start(this.Cols).style({ 'align-items': 'center' })
           .start().add(this.CARD_HEADER).addClass(this.myClass('card-header-title')).end()
           .start(this.Cols)
             .startContext({ data: this })
-            // TODO: Add a read-only Start Date?
-            // TODO: Add End Date
               .add(this.ACCOUNT)
+              .add(this.TIME_FREQUENCY)
               .add(this.TIME_FRAME)
             .endContext()
           .end()
         .end()
         .start().style({ 'width': '930px', 'height': '266px' }).addClass(this.myClass('chart'))
           .add(this.HorizontalBarDAOChartView.create({
-            data$: this.cicoTransactionsDAO$,
-            config$: this.config$,
-            customDatasetStyling$: this.styling$,
+            account$: this.account$,
+            timeFrequency$: this.timeFrequency$,
+            timeFrame$: this.dateRange$,
+            data: this.cicoTransactionsDAO,
             width: 920,
             height: 240
           }))
         .end()
-    },
-
-    function formatYAxis() {
-      var self = this;
-      this.config.data = {}; // Prevent cloning infinite loop. Will be repopulated.
-      var config = foam.Object.clone(this.config);
     }
   ]
 });
