d4379cb90e8cb53596d717539dc99e2fce1a3f9c
Richard Zhu <z-zrichard@hotmail.com>
2019-07-02 11:30:28 -0400

    CPF 2034/identity mind response edna ui fix (#7273)
    * work in progress
    
    * work in progress
    
    * served IdentityMindResponseEDNAView files to web
    
    * work in progress
    
    * work in progress
    
    * work in progress
    
    * Set eDNA view to detail views for ar and er evaluations and to FObjectArrayTableViews for etr and sc evaluations
    
    * removed unnecessary IdentityMindResponseEDNAView.js
    
    * fixed git merge error
    
    * gave eDNA Score Card label proper capitalization
    
    * Updating pegged FOAM commit

--- nanopay/tools/classes.js
+++ nanopay/tools/classes.js
@@ -623,12 +623,17 @@ var classes = [
   'net.nanopay.meter.compliance.dowJones.PersonNameSearchRequest',
 
   // identitymind
+  'net.nanopay.meter.compliance.identityMind.AutomatedReviewEngineResult',
   'net.nanopay.meter.compliance.identityMind.ComplianceTransactionValidator',
+  'net.nanopay.meter.compliance.identityMind.ConditionResult',
   'net.nanopay.meter.compliance.identityMind.ConsumerKYCValidator',
   'net.nanopay.meter.compliance.identityMind.EntityLoginValidator',
+  'net.nanopay.meter.compliance.identityMind.ExternalizedEvaluationResult',
+  'net.nanopay.meter.compliance.identityMind.ExternalizedRule',
   'net.nanopay.meter.compliance.identityMind.MerchantKYCValidator',
   'net.nanopay.meter.compliance.identityMind.IdentityMindRequest',
   'net.nanopay.meter.compliance.identityMind.IdentityMindResponse',
+  'net.nanopay.meter.compliance.identityMind.IdentityMindResponseEDNA',
   'net.nanopay.meter.compliance.identityMind.IdentityMindService',
 
   // meter tests
