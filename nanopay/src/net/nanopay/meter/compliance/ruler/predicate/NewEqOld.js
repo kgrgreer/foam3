9311814305202b5b4e5dd913056548ca6597e1fc
Chanmann Lim <chanmann@nanopay.net>
2019-07-04 12:08:32 -0400

    Allow ignoring properties (eg., lastModified) for NewEqOld predicate

--- nanopay/src/net/nanopay/meter/compliance/ruler/predicate/NewEqOld.js
+++ nanopay/src/net/nanopay/meter/compliance/ruler/predicate/NewEqOld.js
@@ -7,13 +7,38 @@ foam.CLASS({
   documentation: 'Returns true if NEW_OBJ equals OLD_OBJ.',
 
   javaImports: [
+    'foam.core.FObject',
     'static foam.mlang.MLang.*',
+  ],
+
+  properties: [
+    {
+      name: 'ignores',
+      class: 'String',
+      documentation: 'Ignored properties separated by comma.',
+      value: 'lastModified'
+    }
   ],
 
   methods: [
     {
       name: 'f',
-      javaCode: 'return EQ(NEW_OBJ, OLD_OBJ).f(obj);'
+      javaCode: `
+        FObject old = (FObject) OLD_OBJ.f(obj);
+        FObject nu  = (FObject) NEW_OBJ.f(obj);
+        if ( old == null ) {
+          return nu == null;
+        }
+
+        old = old.fclone();
+        nu = nu.fclone();
+        // nullify ignored properties
+        for ( String prop : getIgnores().split(",") ) {
+          nu.setProperty(prop, null);
+          old.setProperty(prop, null);
+        }
+        return nu.equals(old);
+      `
     }
   ]
 });
