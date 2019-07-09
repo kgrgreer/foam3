dd3dc9394a7b257c1460b76ea8d64b28fd7d8f6d
Chanmann Lim <chanmann@nanopay.net>
2019-07-09 15:18:21 -0400

    Create compliance approval request when running JackieRuleOnCreate

--- nanopay/src/net/nanopay/tx/JackieRuleOnCreate.js
+++ nanopay/src/net/nanopay/tx/JackieRuleOnCreate.js
@@ -11,11 +11,8 @@ foam.CLASS({
     'foam.core.ContextAgent',
     'foam.core.X',
     'foam.dao.DAO',
-    'static foam.mlang.MLang.*',
-    'foam.mlang.sink.Count',
     'foam.util.SafetyUtil',
-    'net.nanopay.approval.ApprovalRequest',
-    'net.nanopay.tx.ComplianceTransaction',
+    'net.nanopay.meter.compliance.ComplianceApprovalRequest',
     'net.nanopay.tx.model.Transaction',
 
   ],
@@ -34,13 +31,12 @@ foam.CLASS({
     {
       name: 'applyAction',
       javaCode: `
-
         ComplianceTransaction ct = (ComplianceTransaction) obj;
         Transaction headTx = ct;
-        while ( ! SafetyUtil.isEmpty(headTx.getParent())) {
+        while ( ! SafetyUtil.isEmpty(headTx.getParent()) ) {
           headTx = headTx.findParent(x);
         }
-        ApprovalRequest req = new ApprovalRequest.Builder(x)
+        ComplianceApprovalRequest req = new ComplianceApprovalRequest.Builder(x)
           .setDaoKey("localTransactionDAO")
           .setObjId(ct.getId())
           .setApprover(getJackieId())
@@ -49,7 +45,7 @@ foam.CLASS({
           .setClassification("Validate Transaction Using Jackie Rule")
           .build();
 
-          agency.submit(x, new ContextAgent() {
+        agency.submit(x, new ContextAgent() {
           @Override
           public void execute(X x) {
             requestApproval(x, req);
