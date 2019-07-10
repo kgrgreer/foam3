3305211bcec151c5306e26ed2efef60ed2e57c7a
Michal Pasternak <michal.pasternak@nanopay.net>
2019-07-10 17:39:06 -0400

    fixes

--- nanopay/src/net/nanopay/tx/TransactionDAO.java
+++ nanopay/src/net/nanopay/tx/TransactionDAO.java
@@ -82,8 +82,7 @@ public class TransactionDAO
     return ( ! SafetyUtil.isEmpty(txn.getId()) ||
       txn instanceof DigitalTransaction ) &&
       (txn.getNext() == null || txn.getNext().length == 0 ) &&
-      (txn.canTransfer(x, oldTxn) ||
-        txn.canReverseTransfer(x, oldTxn));
+      (txn.canTransfer(x, oldTxn));
   }
 
   FObject executeTransaction(X x, Transaction txn, Transaction oldTxn) {
