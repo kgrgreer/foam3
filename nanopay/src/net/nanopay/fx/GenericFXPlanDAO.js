2c51b8a0f68cfb378bff4a7693508cd979f6f9d3
Michal Pasternak <michal.pasternak@nanopay.net>
2019-07-17 10:11:07 -0400

    spacing fix

--- nanopay/src/net/nanopay/fx/GenericFXPlanDAO.js
+++ nanopay/src/net/nanopay/fx/GenericFXPlanDAO.js
@@ -31,36 +31,38 @@ foam.CLASS({
     {
       name: 'put_',
       javaCode: `
-      if (getEnabled()) {
-        TransactionQuote quote = (TransactionQuote) obj;
-        if (quote.getRequestTransaction() instanceof FXTransaction ){
-          FXTransaction txn = (FXTransaction) quote.getRequestTransaction();
-          if ( ! SafetyUtil.equals(txn.getSourceCurrency(),txn.getDestinationCurrency())){
-            // has source and destination but no rate or has all 3.
-            if ( ! SafetyUtil.equals(txn.getAmount(),0) &&  ! SafetyUtil.equals(txn.getDestinationAmount(),0)) {
-              if (SafetyUtil.equals(txn.getFxRate(),0))
-                txn.setFxRate((long)(txn.getDestinationAmount()/txn.getAmount()));
-              quote.addPlan(buildFxTransaction_(x,txn));
-              return super.put_(x,quote);
-            }
+        if (getEnabled()) {
+          TransactionQuote quote = (TransactionQuote) obj;
+          if ( quote.getRequestTransaction() instanceof FXTransaction ) {
+            FXTransaction txn = (FXTransaction) quote.getRequestTransaction();
+            if ( ! SafetyUtil.equals(txn.getSourceCurrency(),txn.getDestinationCurrency()) ) {
 
-               // no source but has rate and destination.
-            if ( SafetyUtil.equals(txn.getAmount(),0) &&  ! SafetyUtil.equals(txn.getDestinationAmount(),0) && ! SafetyUtil.equals(txn.getFxRate(),0)) {
-              txn.setAmount((long)(txn.getDestinationAmount()/txn.getFxRate()));
-              quote.addPlan(buildFxTransaction_(x,txn));
-              return super.put_(x,quote);
-            }
-              // no destination but has rate and source.
-            if ( ! SafetyUtil.equals(txn.getAmount(),0) &&  SafetyUtil.equals(txn.getDestinationAmount(),0)&& ! SafetyUtil.equals(txn.getFxRate(),0)) {
-              txn.setDestinationAmount((long)(txn.getAmount()*txn.getFxRate()));
-              quote.addPlan(buildFxTransaction_(x,txn));
-              return super.put_(x,quote);
+              // has source and destination but no rate or has all 3.
+              if ( ! SafetyUtil.equals(txn.getAmount(),0) && ! SafetyUtil.equals(txn.getDestinationAmount(),0) ) {
+                if ( SafetyUtil.equals(txn.getFxRate(),0) ) {
+                  txn.setFxRate((long)(txn.getDestinationAmount()/txn.getAmount()));
+                }
+                quote.addPlan(buildFxTransaction_(x,txn));
+                return super.put_(x,quote);
+              }
+
+                 // no source but has rate and destination.
+              if ( SafetyUtil.equals(txn.getAmount(),0) &&  ! SafetyUtil.equals(txn.getDestinationAmount(),0) && ! SafetyUtil.equals(txn.getFxRate(),0) ) {
+                txn.setAmount((long)(txn.getDestinationAmount()/txn.getFxRate()));
+                quote.addPlan(buildFxTransaction_(x,txn));
+                return super.put_(x,quote);
+              }
+                // no destination but has rate and source.
+              if ( ! SafetyUtil.equals(txn.getAmount(),0) &&  SafetyUtil.equals(txn.getDestinationAmount(),0) && ! SafetyUtil.equals(txn.getFxRate(),0) ) {
+                txn.setDestinationAmount((long)(txn.getAmount()*txn.getFxRate()));
+                quote.addPlan(buildFxTransaction_(x,txn));
+                return super.put_(x,quote);
+              }
             }
           }
         }
-      }
-      return super.put_(x, obj);
-    `
+        return super.put_(x, obj);
+      `
     },
     {
       name: 'buildFxTransaction_',
