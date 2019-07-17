52dc8ffbc561206f7a7291cc48be94e97f35fcb1
Henry Han <henry.han@usask.ca>
2019-07-17 11:40:48 -0400

    Code formatting

--- nanopay/src/net/nanopay/business/UpdateBusinessEmailRule.js
+++ nanopay/src/net/nanopay/business/UpdateBusinessEmailRule.js
@@ -2,14 +2,15 @@ foam.CLASS({
   package: 'net.nanopay.business',
   name: 'UpdateBusinessEmailRule',
 
-  documentation: '',
+  documentation: `Update email property of the business 
+    once the business has a new signing officer.`,
 
   implements: ['foam.nanos.ruler.RuleAction'],
 
   javaImports: [
     'foam.core.ContextAgent',
-    'foam.dao.DAO',
     'foam.core.X',
+    'foam.dao.DAO',
     'foam.nanos.auth.User',
     'net.nanopay.model.Business',
     'net.nanopay.model.BusinessUserJunction'
@@ -34,7 +35,7 @@ foam.CLASS({
             Business business = (Business) localBusinessDAO
               .find(junction.getSourceId());
     
-            // Update business's email so that the new signing office can
+            // Update business's email so that the new signing officer can
             // receive the email notification
             business.setEmail(user.getEmail());
             localBusinessDAO.put(business);
