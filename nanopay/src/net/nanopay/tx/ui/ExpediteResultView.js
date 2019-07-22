0867d464fdd9166f032ba678a3187526ba031e81
blakegreen <blake@nanopay.net>
2019-07-22 13:35:41 -0400

    Added all WizardSubViews for ExpediteTransactionWizard

---
+++ nanopay/src/net/nanopay/tx/ui/ExpediteResultView.js
@@ -0,0 +1,24 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.ui',
+  name: 'ExpediteResultView',
+  extends: 'net.nanopay.ui.wizard.WizardSubView',
+  documentation: 'Last step in the expedite transaction wizard. Responsible for diplaying the result of the action.',
+
+  implements: [
+    'foam.mlang.Expressions'
+  ],
+
+  imports: [],
+
+  css: ``,
+
+  properties: [],
+
+  messages: [],
+
+  methods: [
+    function initE() {
+      var self = this;
+    }
+  ]
+});
---
+++ nanopay/src/net/nanopay/tx/ui/ExpediteTransactionActionView.js
@@ -0,0 +1,24 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.ui',
+  name: 'ExpediteTransactionActionView',
+  extends: 'net.nanopay.ui.wizard.WizardSubView',
+  documentation: 'First step in the expedite transaction wizard. Responsible for capturing the expedite action.',
+
+  implements: [
+    'foam.mlang.Expressions'
+  ],
+
+  requires: [],
+
+  imports: [],
+
+  css: ``,
+
+  properties: [],
+
+  methods: [
+    function initE() {
+      var self = this;
+    }
+  ]
+});
