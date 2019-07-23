3bb7527505590cda74c1d2afa1f8f8d9b306b770
blakegreen <blake@nanopay.net>
2019-07-23 16:54:03 -0400

    Finished adding actions to ExpediteTransaction model which update cash in or cash out transactions status to completed

--- nanopay/src/net/nanopay/tx/ui/ExpediteTransaction.js
+++ nanopay/src/net/nanopay/tx/ui/ExpediteTransaction.js
@@ -6,6 +6,16 @@ foam.CLASS({
     'foam.mlang.Expressions'
   ],
 
+  imports: [
+    'transactionDAO',
+    'stack'
+  ],
+
+  requires: [
+    'net.nanopay.tx.model.Transaction',
+    'net.nanopay.tx.model.TransactionStatus'
+  ],
+
   properties: [
     {
       class: 'Reference',
@@ -25,6 +35,13 @@ foam.CLASS({
       of: 'net.nanopay.tx.model.Transaction',
       name: 'childTransactions',
       visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'output',
+      visiblityExpression: function(output) {
+        return output ? foam.u2.Visibility.RO : foam.u2.Visiblity.HIDDEN;
+      }
     }
   ],
 
@@ -39,6 +56,68 @@ foam.CLASS({
         }
       }
       return children;
+    }
+  ],
+
+  actions: [
+    {
+      name: 'expediteCashIn',
+      label: 'Expedite Cash In',
+      confirmationRequired: true,
+      code: function(X) {
+        // fetch cash in, update status and put back to dao
+        var cashInFilter = this.childTransactions.filter((t) => t.type == 'AlternaCITransaction');
+        var cashInObj = cashInFilter[0];
+
+        if ( cashInObj == undefined ) {
+          this.output = 'There is no cash in transaction to expedite.';
+          return;
+        }
+        if ( cashInObj.status == this.TransactionStatus.COMPLETED ) {
+          this.output = 'Cash In transaction is already completed.';
+          return;
+        }
+        if ( cashInObj.status == this.TransactionStatus.DECLINED ) {
+          this.output = 'Unable to update transaction if status is declined.';
+          return;
+        }
+        cashInObj.status = this.TransactionStatus.COMPLETED;
+        this.transactionDAO.put(cashInObj);
+        this.output = 'Cash In successfully expedited.';
+      }
+    },
+    {
+      name: 'expediteCashOut',
+      label: 'Expedite Cash Out',
+      confirmationRequired: true,
+      code: function(X) {
+        // fetch cash out, update status and put back to dao
+        var cashOutFilter = this.childTransactions.filter((t) => t.type == 'AlternaCOTransaction');
+        var cashOutObj = cashOutFilter[0];
+
+        if ( cashOutObj == undefined ) {
+          this.output = 'There is no cash out transaction to expedite.';
+          return;
+        }
+        if ( cashOutObj.status == this.TransactionStatus.COMPLETED ) {
+          this.output = 'Cash Out transaction is already completed.';
+          return;
+        }
+        if ( cashOutObj.status == this.TransactionStatus.DECLINED ) {
+          this.output = 'Unable to update transaction if status is declined.';
+          return;
+        }
+        cashOutObj.status = this.TransactionStatus.COMPLETED;
+        this.transactionDAO.put(cashOutObj);
+        this.output = 'Cash Out successfully expedited.';
+      }
+    },
+    {
+      name: 'back',
+      label: 'Back',
+      code: function(X) {
+        this.stack.back();
+      }
     }
   ]
 });
