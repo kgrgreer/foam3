bb10a87a0f392e9fa588280d3a352a7e8ecb95de
Michal Pasternak <michal.pasternak@nanopay.net>
2019-07-23 15:46:38 -0400

    make rule work

--- nanopay/src/net/nanopay/tx/ruler/SlowDownRule.js
+++ nanopay/src/net/nanopay/tx/ruler/SlowDownRule.js
@@ -1,52 +1,55 @@
 foam.CLASS({
-  package: 'net.nanopay.tx',
+  package: 'net.nanopay.tx.ruler',
   name: 'SlowDownRule',
-  extends: 'net.nanopay.meter.compliance.AbstractComplianceRuleAction',
 
-  documentation: 'repays debt after a cashIn during FastPay',
+  documentation: `In the event that clearing account or debt limit is insufficient for a planned debt transaction
+  to successfully complete, transition it to a slow pay transaction.`,
 
   implements: ['foam.nanos.ruler.RuleAction'],
 
   javaImports: [
     'foam.core.ContextAgent',
     'foam.core.X',
-    'foam.dao.DAO',
+    'net.nanopay.account.Account',
     'net.nanopay.tx.model.TransactionStatus',
     'net.nanopay.tx.model.Transaction',
+    'net.nanopay.tx.DebtTransaction',
+    'net.nanopay.tx.CompositeTransaction',
     'net.nanopay.tx.cico.CITransaction',
-    'foam.nanos.auth.User',
-    'net.nanopay.model.Business',
+    'net.nanopay.account.Debtable',
+    'foam.nanos.logger.Logger',
     'net.nanopay.account.DebtAccount',
     'net.nanopay.account.OverdraftAccount',
+    'static foam.mlang.MLang.*',
   ],
 
   methods: [
     {
       name: 'applyAction',
       javaCode: `
-        if ( obj instanceof CITransaction ) {
-          CITransaction cashIn = (CITransaction) obj;
-          CITransaction oldCashIn = (CITransaction) oldObj;
-          User user = cashIn.findDestinationAccount(x).findOwner(x);
-          if( user instanceof Business || user.getGroup().equals("sme") ) { // <- maybe should be part of predicate
-            if( cashIn.getStatus() == TransactionStatus.COMPLETED && oldCashIn.getStatus() != TransactionStatus.COMPLETED ) {
-              OverdraftAccount OD = (OverdraftAccount) cashIn.findDestinationAccount(x);
-              if ( OD != null ) {
-                DebtAccount DA = OD.findDebtAccount(x);
-                if ( DA != null && DA.getLimit() > 0 && ( (long) DA.findBalance(x) ) < 0 ) {
-                  agency.submit(x, new ContextAgent() {
-                    @Override
-                    public void execute(X x) {
-                      Transaction repayment = new Transaction.Builder(x)
-                      .setDestinationAccount(DA.getId())
-                      .setSourceAccount(OD.getId())
-                      .setAmount(cashIn.getAmount())
-                      .build();
-                      ((DAO) x.get("localTransactionDAO")).put(repayment);
-                    }
-                  });
+        if ( obj instanceof DebtTransaction ) {
+          DebtTransaction txn = (DebtTransaction) obj;
+          Transaction parentTxn = txn.findParent(x);
+
+          if ( txn.getStatus() != TransactionStatus.PENDING_PARENT_COMPLETED &&
+          ( (Transaction) oldObj ).getStatus() == TransactionStatus.PENDING_PARENT_COMPLETED &&
+          parentTxn instanceof CompositeTransaction ) {
+            Account senderDigitalAccount = txn.findDestinationAccount(x);
+            if ( ((Debtable) senderDigitalAccount).findDebtAccount(x).getLimit() <
+            ( txn.getAmount() - (long) senderDigitalAccount.findBalance(x) ) ||
+            ( (long) ((Debtable) senderDigitalAccount).findDebtAccount(x).findCreditorAccount(x).findBalance(x) ) <
+            ( txn.getAmount() - (long) senderDigitalAccount.findBalance(x) ) ) {
+
+              agency.submit(x, new ContextAgent() {
+                @Override
+                public void execute(X x) {
+                  CITransaction ciTxn = (CITransaction) parentTxn.getChildren(x).find(INSTANCE_OF(CITransaction.class));
+                  txn.setStatus(TransactionStatus.PENDING_PARENT_COMPLETED);
+                  txn.setParent(ciTxn.getId());
+                  Logger logger = (Logger) x.get("logger");
+                  logger.warning("FastPay Transaction Altered to Slow Pay due to insufficient debtlimit/clearing Amount");
                 }
-              }
+              });
             }
           }
         }
