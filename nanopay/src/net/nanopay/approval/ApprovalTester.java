c7e3e74cf490bad1c79881b5af40186ae553a441
Chanmann Lim <chanmann@nanopay.net>
2019-07-29 11:53:44 -0400

    Get approval state from a collection of approval requests
    - Implement ApprovalTester to aggregate approval state result

---
+++ nanopay/src/net/nanopay/approval/ApprovalTester.java
@@ -0,0 +1,102 @@
+package net.nanopay.approval;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class provides support for getting approval state (combined status) of
+ * a series of approval requests given to it's {@code test} method.
+ *
+ * All approval requests have to run through {@code test()} for the
+ * {@code rejected_} and {@code approval_} points to be collected and follow by
+ * a {@code getState()} call to return the approval {@code state_} for all the
+ * approval requests.
+ *
+ */
+public class ApprovalTester {
+  /**
+   * The total rejected points collected.
+   */
+  protected int rejected_;
+
+  /**
+   * The total approval points collected. Use hash map to store and group the
+   * approval points by approval request classification and the value of the
+   * item in the hash map is a tuple (represented by two-element array) where
+   * the first element store the total approval points collected and the
+   * second is the total approval points required for the approval request.
+   *
+   * For example, the `approval_` with an item of ("class1", [2, 5]) entails
+   * there are approval requests for "class1" that have 2 approval points and
+   * total required points is 5.
+   */
+  protected Map<String, int[]> approval_ = new HashMap<>();
+
+  /**
+   * State is the combined status of approval requests.
+   */
+  protected ApprovalStatus state_;
+
+  /**
+   * Tests an approval request and updates the approval state_.
+   *
+   * If the approval request has status=REQUESTED then set state_ to REQUESTED.
+   *
+   * If there are sufficient rejected points collected then set state_ to
+   * REJECTED.
+   *
+   *
+   * @param request is the approval request to be tested
+   * @return approval status of the test
+   */
+  public ApprovalStatus test(ApprovalRequest request) {
+    if ( request.getStatus() == ApprovalStatus.REQUESTED
+      || incrRejected(request) >= request.getRequiredRejectedPoints()
+    ) {
+      state_ = request.getStatus();
+    }
+    incrApproval(request);
+    return state_;
+  }
+
+  /**
+   * Get approval state of approval requests run through the {@link test} thus
+   * far.
+   *
+   * If state_ hasn't been set, check if there are sufficient approval points
+   * collected for each classification of all the approval requests seen thus
+   * far then returns APPROVED. Otherwise, {@code state_} is returned.
+   *
+   * @return approval state (combined status)
+   */
+  public ApprovalStatus getState() {
+    if ( state_ == null
+      && ! approval_.isEmpty()
+      && approval_.values().stream()
+          .allMatch(value -> value[0] >= value[1])
+    ) {
+      return ApprovalStatus.APPROVED;
+    }
+    return state_;
+  }
+
+  private void incrApproval(ApprovalRequest request) {
+    String key = request.getClassification();
+    int[] value = approval_.get(key);
+    if ( value == null ) {
+      value = new int[] { 0, request.getRequiredPoints() };
+    }
+
+    if ( request.getStatus() == ApprovalStatus.APPROVED ) {
+      value[0] += request.getPoints();
+    }
+    approval_.put(key, value);
+  }
+
+  private int incrRejected(ApprovalRequest request) {
+    if ( request.getStatus() == ApprovalStatus.REJECTED ) {
+      rejected_ += request.getPoints();
+    }
+    return rejected_;
+  }
+}
