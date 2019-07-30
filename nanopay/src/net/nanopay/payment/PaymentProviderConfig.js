5f137231749da53f58b2f119be631bdd64fdfbf6
sirenChen <siren@nanopay.net>
2019-07-30 15:52:41 -0400

    payment provider support

---
+++ nanopay/src/net/nanopay/payment/PaymentProviderConfig.js
@@ -0,0 +1,21 @@
+foam.CLASS({
+  package: 'net.nanopay.payment',
+  name: 'PaymentProviderConfig',
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'id',
+    },
+    {
+      class: 'Reference',
+      of: 'net.nanopay.payment.PaymentProvider',
+      name: 'paymentProvider'
+    },
+    {
+      class: 'Reference',
+      of: 'net.nanopay.payment.Institution',
+      name: 'institution'
+    }
+  ]
+});
