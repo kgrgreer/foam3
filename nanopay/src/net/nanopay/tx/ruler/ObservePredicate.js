a4b0b2bd10e27a0652b5b7f1653dbd1f005b9143
Michal Pasternak <michal.pasternak@nanopay.net>
2019-08-02 16:25:30 -0400

    change to a predicate

---
+++ nanopay/src/net/nanopay/tx/ruler/ObservePredicate.js
@@ -0,0 +1,37 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.ruler',
+  name: 'ObservePredicate',
+
+  documentation: 'Predicate for observing a property',
+
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
+
+  javaImports: [
+    'foam.core.X',
+    'foam.core.FObject',
+    'foam.core.PropertyInfo',
+    'static foam.mlang.MLang.*',
+  ],
+  properties: [
+    {
+      name: 'observedProperty',
+      class: 'String'
+    },
+    {
+      name: 'observedClass',
+      class: 'Class'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+       FObject nu  = (FObject) NEW_OBJ.f(obj);
+       FObject old = (FObject) OLD_OBJ.f(obj);
+       return ( getObservedClass().isInstance(nu) &&  ( nu.getClassInfo().getAxiomByName(getObservedProperty()) != old.getClassInfo().getAxiomByName(getObservedProperty()) ) );
+      `
+    }
+  ]
+});
