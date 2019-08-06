3cbc5223157d63ed5e55a66747f2af62058fa0e4
blakegreen <blake@nanopay.net>
2019-08-06 16:30:59 -0400

    Took out any transaction status predicates and replaced them with TransactionStatusPredicate, passing in the status that needs to be checked in the rule predicate

--- nanopay/src/net/nanopay/meter/compliance/ruler/predicate/TransactionStatusPredicate.js
+++ nanopay/src/net/nanopay/meter/compliance/ruler/predicate/TransactionStatusPredicate.js
@@ -1,16 +1,23 @@
 foam.CLASS({
   package: 'net.nanopay.meter.compliance.ruler.predicate',
-  name: 'IsSentTransaction',
+  name: 'TransactionStatusPredicate',
 
-  documentation: 'Returns true is new object is a transaction with SENT',
+  documentation: 'Returns true if new object is a transaction with given status property.',
 
   extends: 'foam.mlang.predicate.AbstractPredicate',
   implements: ['foam.core.Serializable'],
 
   javaImports: [
     'net.nanopay.tx.model.Transaction',
-    'net.nanopay.tx.model.TransactionStatus',
     'static foam.mlang.MLang.*'
+  ],
+
+  properties: [
+    {
+      name: 'status',
+      class: 'Enum',
+      of: 'net.nanopay.tx.model.TransactionStatus'
+    }
   ],
 
   methods: [
@@ -18,7 +25,7 @@ foam.CLASS({
       name: 'f',
       javaCode: `
         return EQ(
-          DOT(NEW_OBJ, Transaction.STATUS), TransactionStatus.SENT
+          DOT(NEW_OBJ, Transaction.STATUS), getStatus()
         ).f(obj);
       `
     }
