d94cc6d1dc0346dc19ba87d3e2136913d314a5a7
Michal Pasternak <michal.pasternak@nanopay.net>
2019-08-09 17:09:55 -0400

    update rule parameters

--- nanopay/src/net/nanopay/tx/model/Transaction.js
+++ nanopay/src/net/nanopay/tx/model/Transaction.js
@@ -49,7 +49,6 @@ foam.CLASS({
     'net.nanopay.tx.TransactionQuote',
     'net.nanopay.tx.Transfer',
     'net.nanopay.account.Balance',
-    'net.nanopay.tx.model.Transaction',
     'static foam.mlang.MLang.EQ'
   ],
 
