d92d20080f2e9f365670d25f671cda0c7c887125
Mayowa Olurin <mayowa@nanopay.net>
2019-08-12 14:54:48 +0100

    Show Finish button for US Onboarding when not sighning od=fficer

--- nanopay/src/net/nanopay/sme/onboarding/USBusinessOnboarding.js
+++ nanopay/src/net/nanopay/sme/onboarding/USBusinessOnboarding.js
@@ -654,9 +654,10 @@ foam.CLASS({
       documentation: 'Date of Business Formation or Incorporation.',
       validationPredicates: [
         {
-          args: ['businessFormationDate'],
+          args: ['signingOfficer','businessFormationDate'],
           predicateFactory: function(e) {
             return e.OR(
+              e.EQ(net.nanopay.sme.onboarding.USBusinessOnboarding.SIGNING_OFFICER, false),
               foam.mlang.predicate.OlderThan.create({
                 arg1: net.nanopay.sme.onboarding.USBusinessOnboarding.BUSINESS_FORMATION_DATE,
                 timeMs: 24 * 60 * 60 * 1000
@@ -690,9 +691,10 @@ foam.CLASS({
       },
       validationPredicates: [
         {
-          args: ['countryOfBusinessFormation'],
+          args: ['signingOfficer', 'countryOfBusinessFormation'],
           predicateFactory: function(e) {
             return e.OR(
+              e.EQ(net.nanopay.sme.onboarding.USBusinessOnboarding.SIGNING_OFFICER, false),
               e.EQ(net.nanopay.sme.onboarding.USBusinessOnboarding.COUNTRY_OF_BUSINESS_FORMATION, 'CA'),
               e.EQ(net.nanopay.sme.onboarding.USBusinessOnboarding.COUNTRY_OF_BUSINESS_FORMATION, 'US')
             );
@@ -710,8 +712,21 @@ foam.CLASS({
       visibilityExpression: function(countryOfBusinessFormation) {
         return countryOfBusinessFormation === 'US' ? foam.u2.Visibility.RW : foam.u2.Visibility.HIDDEN;
       },
-      minLength: 9,
-      maxLength: 9
+      validationPredicates: [
+        {
+          args: ['signingOfficer', 'businessRegistrationNumber'],
+          predicateFactory: function(e) {
+            return e.OR(
+              e.EQ(net.nanopay.sme.onboarding.USBusinessOnboarding.SIGNING_OFFICER, false),
+              e.EQ(
+                foam.mlang.StringLength.create({
+                  arg1: net.nanopay.sme.onboarding.USBusinessOnboarding.BUSINESS_REGISTRATION_NUMBER
+                }), 9),
+            );
+          },
+          errorString: 'Please enter a valid Federal Tax ID Number (EIN) or Business Registration Number.'
+        }
+      ]
     },
     net.nanopay.sme.onboarding.model.SuggestedUserTransactionInfo.ANNUAL_REVENUE.clone().copyFrom({
       section: 'transactionDetailsSection',
