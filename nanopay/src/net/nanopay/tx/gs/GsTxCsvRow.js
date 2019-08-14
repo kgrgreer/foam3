f1e14ab96925ea1eb35b827ff6fdd9cf5fd60ff2
Mike Carcasole <mikecarcasole@gmail.com>
2019-08-14 12:23:20 -0400

    Initial check-in and peg to local repo

---
+++ nanopay/src/net/nanopay/tx/gs/GsTxCsvRow.js
@@ -0,0 +1,122 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.gs',
+  name: 'GsTxCsvRow',
+  properties: [
+    {
+      class: 'String',
+      name: 'TransactionId'
+    },
+    {
+      class: 'String',
+      name: 'OriginSys'
+    },
+    {
+      class: 'String',
+      name: 'originSysRef'
+    },
+    {
+      class: 'String',
+      name: 'SettleDate'
+    },
+    {
+      class: 'String',
+      name: 'TimeStamp'
+    },
+    {
+      class: 'String',
+      name: 'TimeZone'
+    },
+    {
+      class: 'String',
+      name: 'BankName'
+    },
+    {
+      class: 'String',
+      name: 'BankBranch'
+    },
+    {
+      class: 'String',
+      name: 'BankId'
+    },
+    {
+      class: 'String',
+      name: 'Currency'
+    },
+    {
+      class: 'String',
+      name: 'Company'
+    },
+    {
+      class: 'String',
+      name: 'CashQty'
+    },
+    {
+      class: 'String',
+      name: 'CashUSD'
+    },
+    {
+      class: 'String',
+      name: 'ClrAgent'
+    },
+    {
+      class: 'String',
+      name: 'Account'
+    },
+    {
+      class: 'String',
+      name: 'SecQty'
+    },
+    {
+      class: 'String',
+      name: 'ProductId'
+    },
+    {
+      class: 'String',
+      name: 'MarketValueLocal'
+    },
+    {
+      class: 'String',
+      name: 'MarketValueCCy'
+    },
+    {
+      class: 'String',
+      name: 'MarketValue'
+    },
+    {
+      class: 'String',
+      name: 'SettleType'
+    },
+    {
+      class: 'String',
+      name: 'IsInternal'
+    },
+    {
+      class: 'String',
+      name: 'DescriptionTag'
+    },
+    {
+      class: 'String',
+      name: 'Proto_Liquidity_Hierarchy2'
+    },
+    {
+      class: 'String',
+      name: 'Proto_Liquidity_Hierarchy3'
+    },
+    {
+      class: 'String',
+      name: 'Proto_Liquidity_Hierarchy4'
+    },
+    {
+      class: 'String',
+      name: 'ProductType'
+    },
+    {
+      class: 'String',
+      name: 'ProductCurrency'
+    },
+    {
+      class: 'String',
+      name: 'LiquidityBucket'
+    }
+  ]
+});
