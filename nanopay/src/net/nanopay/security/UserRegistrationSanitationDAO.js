c2889571962620cacc2eecbe52d67c205e118bf0
Troy Wolters <troy@nanopay.net>
2019-08-20 16:59:35 -0400

    Sanitize user object passed to smeBusinessRegistrationDAO

---
+++ nanopay/src/net/nanopay/security/UserRegistrationSanitationDAO.js
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'net.nanopay.security',
+  name: 'UserRegistrationSanitationDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  documentation: `
+    Sanitize the user object being put so that only expected fields are allowed
+    to be set to non-default values.
+  `,
+
+  javaImports: [
+    'foam.nanos.auth.User',
+    'java.util.Objects'
+  ],
+
+  methods: [
+    {
+      name: 'put_',
+      javaCode: `
+        User user = (User) Objects.requireNonNull(obj, "User cannot be null.");
+        return super.put_(x, sanitize(user));
+      `
+    },
+    {
+      name: 'sanitize',
+      type: 'User',
+      args: [{ type: 'User', name: 'user' }],
+      documentation: `Return a sanitized copy of the given user.`,
+      javaCode: `
+        return new User.Builder(getX())
+          .setFirstName(user.getFirstName())
+          .setLastName(user.getLastName())
+          .setEmail(user.getEmail())
+          .setDesiredPassword(user.getDesiredPassword())
+          .setOrganization(user.getOrganization())
+          .setSignUpToken(user.getSignUpToken())
+          .setBusinessAddress(user.getBusinessAddress())
+          .setAddress(user.getAddress())
+
+          // TODO: Remove the need for this property.
+          .setWelcomeEmailSent(user.getWelcomeEmailSent())
+
+          .build();
+      `
+    }
+  ],
+});
