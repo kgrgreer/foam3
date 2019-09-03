f7d1cf96b798cb0ce18eb3ae804b33dc103bbd0b
carl-zzz <carl.chen@nanopay.net>
2019-09-03 17:22:19 -0400

    Formatting

--- nanopay/src/net/nanopay/tx/ruler/BusinessLimitPredicate.js
+++ nanopay/src/net/nanopay/tx/ruler/BusinessLimitPredicate.js
@@ -1,37 +1,36 @@
 foam.CLASS({
-    package: 'net.nanopay.tx.ruler',
-    name: 'BusinessLimitPredicate',
-    extends: 'foam.mlang.predicate.AbstractPredicate',
-    implements: ['foam.core.Serializable'],
-  
-    documentation: 'Returns true if the rule fits the transaction',
-  
-    javaImports: [
-        'foam.core.X',
-        'foam.dao.DAO',
-        'net.nanopay.tx.model.Transaction',
-        'net.nanopay.account.Account',
-        'static foam.mlang.MLang.*',
-    ],
-    properties: [
-        {
-            class: 'Long',
-            name: 'business'
-        },
-        {
-            class: 'Boolean',
-            name: 'send'
-        }
-    ],
-    methods: [
-        {
-            name: 'f',
-            javaCode: `
-            Transaction tx = (Transaction) NEW_OBJ.f(obj);
-            Account account = getSend() ? tx.findSourceAccount((X) obj) : tx.findDestinationAccount((X) obj);
-            return ( tx.getType().equals("DigitalTransaction")  && account.getOwner() == getBusiness());
-            `
-        }
-    ]
+  package: 'net.nanopay.tx.ruler',
+  name: 'BusinessLimitPredicate',
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
 
+  documentation: 'Returns true if the rule fits the transaction',
+
+  javaImports: [
+    'foam.core.X',
+    'foam.dao.DAO',
+    'net.nanopay.tx.model.Transaction',
+    'net.nanopay.account.Account',
+    'static foam.mlang.MLang.*',
+  ],
+  properties: [
+    {
+      class: 'Long',
+      name: 'business'
+    },
+    {
+      class: 'Boolean',
+      name: 'send'
+    }
+  ],
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+      Transaction tx = (Transaction) NEW_OBJ.f(obj);
+      Account account = getSend() ? tx.findSourceAccount((X) obj) : tx.findDestinationAccount((X) obj);
+      return ( tx.getType().equals("DigitalTransaction")  && account.getOwner() == getBusiness());
+      `
+    }
+  ]
 });
