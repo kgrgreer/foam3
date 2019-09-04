3c8406e795a506a2f955e56fc4f15b6e58939f08
Mayowa Olurin <mayowa@nanopay.net>
2019-09-04 18:47:50 +0100

    fix context issue with fx provision rule

--- nanopay/src/net/nanopay/meter/compliance/ruler/AddFXProvisionPayerPermission.js
+++ nanopay/src/net/nanopay/meter/compliance/ruler/AddFXProvisionPayerPermission.js
@@ -8,11 +8,12 @@ foam.CLASS({
   documentation: 'Rule to add fx.provision.payer permissions to a business after it is onboarded.',
 
   javaImports: [
-    'foam.core.ContextAgent',
+    'foam.core.ContextAwareAgent',
     'foam.core.X',
     'foam.dao.DAO',
     'foam.nanos.auth.Address',
     'foam.nanos.auth.Group',
+    'foam.nanos.auth.User',
     'foam.nanos.auth.Permission',
     'foam.nanos.logger.Logger',
     'foam.util.SafetyUtil',
@@ -24,7 +25,7 @@ foam.CLASS({
     {
       name: 'applyAction',
       javaCode: `
-        agency.submit(x, new ContextAgent() {
+        agency.submit(x, new ContextAwareAgent() {
           @Override
           public void execute(X x) {
             if ( ! (obj instanceof Business) ) {
@@ -33,14 +34,13 @@ foam.CLASS({
             Business business = (Business) obj.fclone();
             DAO localGroupDAO = (DAO) x.get("localGroupDAO");
             Address businessAddress = business.getBusinessAddress();
-            
             if ( business.getOnboarded() && null != businessAddress 
                 && ! SafetyUtil.isEmpty(businessAddress.getCountryId()) ) {
               Permission fxProvisionPermission = new Permission.Builder(x).setId("fx.provision.payer").build();
               Group group = (Group) localGroupDAO.find(business.getGroup());
               if ( null != group && ! group.implies(x, new AuthPermission(fxProvisionPermission.getId())) ) {
                 try {
-                  group.getPermissions(x).add(fxProvisionPermission);  
+                  group.getPermissions(getX()).add(fxProvisionPermission);  
                 } catch(Throwable t) {
                     ((Logger) x.get("logger")).error("Error adding fx.provision.payer permission to business " + business.getId(), t);
                 }
