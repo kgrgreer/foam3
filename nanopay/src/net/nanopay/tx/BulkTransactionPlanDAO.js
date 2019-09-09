2cec1abc929adc155ca13f876b185da15a095742
Henry Han <henry.han@usask.ca>
2019-09-09 15:01:52 -0400

    Set the destination of parent txn and child txns properly

--- nanopay/src/net/nanopay/tx/BulkTransactionPlanDAO.js
+++ nanopay/src/net/nanopay/tx/BulkTransactionPlanDAO.js
@@ -3,7 +3,7 @@ foam.CLASS({
   name: 'BulkTransactionPlanDAO',
   extends: 'foam.dao.ProxyDAO',
 
-  documentation: ``,
+  documentation: `A decorator in the localTransactionQuotePlanDAO that handles one to many transactions`,
 
   javaImports: [
     'foam.core.X',
@@ -39,66 +39,70 @@ foam.CLASS({
 
           long sum = 0;
           Transaction[] childTransactions= parentTxn.getNext();
-            CompositeTransaction ct = new CompositeTransaction();
-            ct.setPayerId(parentTxn.getPayerId());
-            ct.setPayeeId(parentTxn.getPayerId());
+          CompositeTransaction ct = new CompositeTransaction();
+          ct.setPayerId(parentTxn.getPayerId());
+          ct.setPayeeId(parentTxn.getPayerId());
 
-            for (Transaction childTransaction : childTransactions) {
-              // Sum amount of child transactions
-              sum = sum + childTransaction.getAmount();
+          // Set the destination of parent transaction to payer's default digital account
+          DigitalAccount payerDigitalAccount = DigitalAccount.findDefault(x, payer, parentTxn.getSourceCurrency());
+          Balance digitalAccountBalance = (Balance) balanceDAO.find(payerDigitalAccount.getId());
+          parentTxn.setDestinationAccount(payerDigitalAccount.getId());
 
-              TransactionQuote childQuote = new TransactionQuote();
+          for (Transaction childTransaction : childTransactions) {
+            // Sum amount of child transactions
+            sum = sum + childTransaction.getAmount();
 
-              // Set the source of each child transaction to its parent destination digital account
-              childTransaction.setSourceAccount(parentTxn.getDestinationAccount());
-              childTransaction.setPayerId(parentTxn.getPayerId());
+            TransactionQuote childQuote = new TransactionQuote();
 
-              // Quote each child transaction
-              childQuote.setRequestTransaction(childTransaction);
+            // Set the source of each child transaction to its parent destination digital account
+            childTransaction.setSourceAccount(parentTxn.getDestinationAccount());
 
-              // Put all the child transaction quotes to TransactionQuotePlanDAO
-              TransactionQuote result = (TransactionQuote) planDAO.put(childQuote);
+            // Set the destination of each child transaction to payee's default digital account
+            User payee = (User) userDAO.find_(x, childTransaction.getPayeeId());
+            DigitalAccount payeeDigitalAccount = DigitalAccount.findDefault(x, payee, "CAD");
+            childTransaction.setDestinationAccount(payeeDigitalAccount.getId());
 
-              // Add the child transaction plans as the next of the compositeTransaction
-              ct.addNext(result.getPlan());
-            }
+            // Quote each child transaction
+            childQuote.setRequestTransaction(childTransaction);
 
-            // Set the total amount on parent
-            parentTxn.setAmount(sum);
+            // Put all the child transaction quotes to TransactionQuotePlanDAO
+            TransactionQuote result = (TransactionQuote) planDAO.put(childQuote);
 
-            DigitalAccount payerDigitalAccount = DigitalAccount.findDefault(x, payer, parentTxn.getSourceCurrency());
-            Balance digitalAccountBalance = (Balance) balanceDAO.find(payerDigitalAccount.getId());
+            // Add the child transaction plans as the next of the compositeTransaction
+            ct.addNext(result.getPlan());
+          }
 
+          // Set the total amount on parent
+          parentTxn.setAmount(sum);
+
+          if ( sum > digitalAccountBalance.getBalance() ) {
             // If digital does not have sufficient funds, then set the source account to their default bank account.
-            // When this is quoted, it will create a Cash-In.
-            if ( sum > digitalAccountBalance.getBalance() ) {
-              BankAccount payerDefaultBankAccount = BankAccount.findDefault(x, payer, parentTxn.getSourceCurrency());
-              parentTxn.setSourceAccount(payerDefaultBankAccount.getId());
-              parentTxn.setDestinationAccount(payerDigitalAccount.getId());
-              
-              Transaction cashInTransaction = new Transaction();
-              cashInTransaction.setSourceAccount(payerDefaultBankAccount.getId());
-              cashInTransaction.setDestinationAccount(payerDigitalAccount.getId());
-              cashInTransaction.setAmount(parentTxn.getAmount());
-              TransactionQuote cashInTransactionQuote = new TransactionQuote();
-              cashInTransactionQuote.setRequestTransaction(cashInTransaction);
-              cashInTransactionQuote = (TransactionQuote) planDAO.put(cashInTransactionQuote);
-              
-              // Add cash-in transaction as the next fo the parent transaction
-              cashInTransaction.addNext(ct);
-              parentTxn.clearNext();
-              parentTxn.addNext(cashInTransaction);
-            } else {
-              // If the payer digital account does have sufficient balance then
-              // set the source and destination as the default digital account.
-              // when this is quoted, the planners will do nothing (which is what we want).
-              parentTxn.setSourceAccount(payerDigitalAccount.getId());
-              parentTxn.setDestinationAccount(payerDigitalAccount.getId());
-              
-              // Add a compositeTransaction as the next of the parent transaction
-              parentTxn.clearNext();
-              parentTxn.addNext(ct);
-            }
+            BankAccount payerDefaultBankAccount = BankAccount.findDefault(x, payer, parentTxn.getSourceCurrency());
+            parentTxn.setSourceAccount(payerDefaultBankAccount.getId());
+
+            // Create a Cash-In transaction
+            Transaction cashInTransaction = new Transaction();
+            cashInTransaction.setSourceAccount(payerDefaultBankAccount.getId());
+            cashInTransaction.setDestinationAccount(payerDigitalAccount.getId());
+            cashInTransaction.setAmount(parentTxn.getAmount());
+            TransactionQuote cashInTransactionQuote = new TransactionQuote();
+            cashInTransactionQuote.setRequestTransaction(cashInTransaction);
+            cashInTransactionQuote = (TransactionQuote) planDAO.put(cashInTransactionQuote);
+            
+            // Add cash-in transaction as the next fo the parent transaction
+            cashInTransaction.addNext(ct);
+            parentTxn.clearNext();
+            parentTxn.addNext(cashInTransaction);
+          } else {
+            // If the payer digital account does have sufficient balance then
+            // set the source and destination as the default digital account.
+            // When this is quoted, the planners will do nothing (which is what we want).
+            parentTxn.setSourceAccount(payerDigitalAccount.getId());
+            
+            // Add a compositeTransaction as the next of the parent transaction
+            parentTxn.clearNext();
+            parentTxn.addNext(ct);
+          }
 
           // Set parent transaction to the parent quote
           parentQuote.setPlan(parentTxn);
