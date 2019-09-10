b6526203ec4c7a93d9a122495ddb0f5e7c5377c9
Mayowa Olurin <mayowa@nanopay.net>
2019-09-10 12:05:53 +0100

    update AFEXAddCurrencyPermissionRule to check approval request

--- nanopay/src/net/nanopay/fx/afex/AFEXAddCurrencyPermissionRule.js
+++ nanopay/src/net/nanopay/fx/afex/AFEXAddCurrencyPermissionRule.js
@@ -19,7 +19,11 @@ foam.CLASS({
     'foam.nanos.logger.Logger',
     'foam.util.SafetyUtil',
     'javax.security.auth.AuthPermission',
+    'net.nanopay.approval.ApprovalRequest',
+    'net.nanopay.approval.ApprovalRequestUtil',
+    'net.nanopay.approval.ApprovalStatus',
     'net.nanopay.model.Business',
+    'static foam.mlang.MLang.AND', 
     'static foam.mlang.MLang.EQ'    
 
   ],
@@ -31,7 +35,6 @@ foam.CLASS({
       agency.submit(x, new ContextAgent() {
         @Override
         public void execute(X x) {
-          System.out.println("inside AFEXAddCurrencyPermissionRule");
           Logger logger = (Logger) x.get("logger");
           
           if ( ! (obj instanceof AFEXBusiness) ) {
@@ -39,31 +42,40 @@ foam.CLASS({
           }
           
           AFEXBusiness afexBusiness = (AFEXBusiness) obj;
-          DAO localBusinessDAO = (DAO) x.get("localBusinessDAO");
-          DAO localGroupDAO = (DAO) x.get("localGroupDAO");
-          
-          Business business = (Business) localBusinessDAO.find(EQ(Business.ID, afexBusiness.getUser())); 
-          if ( null != business ) {
-            System.out.println("business is found " + business.getBusinessName());
-            Address businessAddress = business.getBusinessAddress();
-            if ( null != businessAddress && ! SafetyUtil.isEmpty(businessAddress.getCountryId()) ) {
-              String permissionString = "currency.read.";
-              permissionString = businessAddress.getCountryId().equals("CA") ? permissionString + "USD" : permissionString + "CAD";
-              Permission permission = new Permission.Builder(x).setId(permissionString).build();
-              Group group = (Group) localGroupDAO.find(business.getGroup());
-              while ( group != null ) {
-                group = (Group) group.findParent(x);
-                if ( group != null && group.getId().endsWith("employee") ) break;
+          DAO dao = ((DAO) x.get("approvalRequestDAO"))
+          .where(AND(
+            EQ(ApprovalRequest.DAO_KEY, "localUserDAO"),
+            EQ(ApprovalRequest.OBJ_ID, Long.toString(afexBusiness.getId()))
+          ));
+
+          ApprovalStatus approval = ApprovalRequestUtil.getState(dao);
+          if ( approval != ApprovalStatus.APPROVED ) {
+            DAO localBusinessDAO = (DAO) x.get("localBusinessDAO");
+            DAO localGroupDAO = (DAO) x.get("localGroupDAO");
+            
+            Business business = (Business) localBusinessDAO.find(EQ(Business.ID, afexBusiness.getUser())); 
+            if ( null != business ) {
+              Address businessAddress = business.getBusinessAddress();
+              if ( null != businessAddress && ! SafetyUtil.isEmpty(businessAddress.getCountryId()) ) {
+                String permissionString = "currency.read.";
+                permissionString = businessAddress.getCountryId().equals("CA") ? permissionString + "USD" : permissionString + "CAD";
+                Permission permission = new Permission.Builder(x).setId(permissionString).build();
+                Group group = (Group) localGroupDAO.find(business.getGroup());
+                while ( group != null ) {
+                  group = (Group) group.findParent(x);
+                  if ( group != null && group.getId().endsWith("employee") ) break;
+                }
+                if ( null != group && ! group.implies(x, new AuthPermission(permissionString)) ) {
+                  try {
+                    group.getPermissions(x).add(permission);  
+                  } catch(Throwable t) {
+                    logger.error("Error adding " + permissionString + " to business " + business.getId(), t);
+                    }
+                  } 
+                }
               }
-              if ( null != group && ! group.implies(x, new AuthPermission(permissionString)) ) {
-                try {
-                  group.getPermissions(x).add(permission);  
-                } catch(Throwable t) {
-                  logger.error("Error adding " + permissionString + " to business " + business.getId(), t);
-                  }
-                } 
-              }
-            }
+          }
+
         }
 
       }, "Adds currency.read.FX_CURRENCY permissions to business when AFEXBUsiness is created.");
