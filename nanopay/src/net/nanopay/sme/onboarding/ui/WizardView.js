89c1208517b5d502777d502b810acff4d16dfda3
Patrick Zanowski <patrick@nanopay.net>
2019-09-11 15:10:23 -0400

    added missing imports

--- nanopay/src/net/nanopay/sme/onboarding/ui/WizardView.js
+++ nanopay/src/net/nanopay/sme/onboarding/ui/WizardView.js
@@ -4,6 +4,7 @@ foam.CLASS({
   extends: 'foam.u2.detail.WizardSectionsView',
 
   imports: [
+    'auth',
     'userDAO'
   ],
 
@@ -192,7 +193,7 @@ foam.CLASS({
         var dao = x[foam.String.daoize(this.data.model_.name)];
         dao.
           put(this.data.clone().copyFrom({ status: 'SUBMITTED' })).
-          then(async function() {
+          then(async () => {
             let user = await x.userDAO.find(x.user.id);
             if ( user ) x.user.onboarded = user.onboarded;
             // Invalidate auth cache to register new permissions on group.
