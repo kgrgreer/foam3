cfba40f3b66fa60929db89beaca7bd871932b5ae
Henry Han <henry.han@usask.ca>
2019-09-16 12:57:35 -0400

    Revert "Add explicitCI CO to better support payroll and expenses where, for t…"

--- nanopay/src/net/nanopay/tx/BulkTransaction.js
+++ nanopay/src/net/nanopay/tx/BulkTransaction.js
@@ -1,18 +1,5 @@
 foam.CLASS({
   package: 'net.nanopay.tx',
   name: 'BulkTransaction',
-  extends: 'net.nanopay.tx.SummaryTransaction',
-
-  properties: [
-    {
-      name: 'explicitCI',
-      class: 'Boolean',
-      value: true
-    },
-    {
-      name: 'explicitCO',
-      class: 'Boolean',
-      value: true
-    }
-  ]
+  extends: 'net.nanopay.tx.SummaryTransaction'
 });
