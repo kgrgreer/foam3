a9509ed26d69881966dd4d089908dcff2d7f6271
nanoMichal <michall.pasternak@nanopay.net>
2019-09-24 16:08:35 -0400

    create a predicate for is-property-equal-tovalue

--- nanopay/tools/classes.js
+++ nanopay/tools/classes.js
@@ -364,6 +364,7 @@ var classes = [
   'net.nanopay.tx.ruler.PropertyChangePredicate',
   'net.nanopay.tx.ruler.ChangePropertyAction',
   'net.nanopay.tx.ruler.SendNotification',
+  'net.nanopay.tx.ruler.PropertyEQValue',
   'net.nanopay.tx.RepayDebtOnCIRule',
   'net.nanopay.tx.model.TransactionLimitTimeFrame',
   'net.nanopay.tx.model.TransactionLimitType',
