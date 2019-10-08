b1a79cd52f3a0338d047e856c23a73922f74dfd3
blakegreen <blake@nanopay.net>
2019-10-08 14:12:30 -0400

    Added causeDAOKey to approval request email if approval request is instanceof ComplianceApprovalRequest

--- nanopay/src/net/nanopay/approval/ApprovalRequestNotificationDAO.java
+++ nanopay/src/net/nanopay/approval/ApprovalRequestNotificationDAO.java
@@ -5,6 +5,7 @@ import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.notification.Notification;
+import net.nanopay.meter.compliance.ComplianceApprovalRequest;
 
 public class ApprovalRequestNotificationDAO
 extends ProxyDAO {
@@ -18,17 +19,23 @@ extends ProxyDAO {
     
     FObject old = ((DAO)x.get("approvalRequestDAO")).find_(x, obj);
     ApprovalRequest ret = (ApprovalRequest) getDelegate().put_(x, obj);
+    String causeDAO = "";
     if ( old != null
       || ApprovalStatus.REQUESTED != ret.getStatus()
     ) { 
       return ret;
     }
 
+    if ( ret instanceof ComplianceApprovalRequest ) {
+      ComplianceApprovalRequest complainceApprovalRequest = (ComplianceApprovalRequest) ret;
+      causeDAO = complainceApprovalRequest.getCauseDaoKey();
+    }
+
     Notification notification = new Notification();
     notification.setUserId(ret.getApprover());
-    notification.setNotificationType("approval request for id: " + ret.getObjId());
+    notification.setNotificationType("approval request for id: " + ret.getObjId() + " " + causeDAO);
     notification.setEmailIsEnabled(true);
-    notification.setBody("New approval was requested for id: " + ret.getObjId());
+    notification.setBody("New approval was requested for id: " + ret.getObjId() + " " + causeDAO);
     //notification.setEmailName("future email template name"); !!! PROPER WAY TO SET EMAIL TEMPLATE (when it is done) !!!
     //notification.setEmailArgs(MAP_GOES_HERE); !!! PROPER WAY TO SET EMAIL ARGS FOR TEMPLATE !!!
     ((DAO)x.get("localNotificationDAO")).put(notification);
