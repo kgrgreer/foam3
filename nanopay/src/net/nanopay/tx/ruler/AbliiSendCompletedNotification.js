c2b7d8c796aeae6fb0fb037eff78f9eb93a0c6e2
Garfield <garfield@MacBook-Pro-New.local>
2019-10-09 16:06:09 -0400

    change the receiver notification message to match the requirement

--- nanopay/src/net/nanopay/tx/ruler/AbliiSendCompletedNotification.js
+++ nanopay/src/net/nanopay/tx/ruler/AbliiSendCompletedNotification.js
@@ -47,6 +47,10 @@ foam.CLASS({
                 .append(" ")
                 .append(tx.getDestinationCurrency());
 
+                StringBuilder rb = new StringBuilder()
+                .append(" Received payment from ")
+                .append(sender.label());
+              
               if (
                 invoice.getInvoiceNumber() != null &&
                 ! SafetyUtil.isEmpty(invoice.getInvoiceNumber())
@@ -62,6 +66,7 @@ foam.CLASS({
 
               sb.append(".");
               String notificationMsg = sb.toString();
+              String receiver_notificationMsg = rb.toString();
 
               // notification to sender
               Notification senderNotification = new Notification();
@@ -78,7 +83,7 @@ foam.CLASS({
               if ( receiver.getId() != sender.getId() ) {
                 Notification receiverNotification = new Notification();
                 receiverNotification.setUserId(receiver.getId());
-                receiverNotification.setBody(notificationMsg);
+                receiverNotification.setBody(receiver_notificationMsg);
                 receiverNotification.setNotificationType("Transaction Initiated");
                 receiverNotification.setIssuedDate(invoice.getIssueDate());
                 try {
