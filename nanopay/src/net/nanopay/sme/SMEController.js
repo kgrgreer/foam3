9367737c06a84e02a439e05ce49559db10fa6011
sirenChen <siren@nanopay.net>
2019-10-09 15:01:59 -0400

    fix soft session.

--- nanopay/src/net/nanopay/sme/SMEController.js
+++ nanopay/src/net/nanopay/sme/SMEController.js
@@ -416,7 +416,9 @@ foam.CLASS({
     },
 
     function onSessionTimeout() {
-      if ( this.user.emailVerified ) {
+      debugger
+      if ( (this.user && this.user.emailVerified) ||
+           (this.agent && this.agent.emailVerified) ) {
         this.add(this.SMEModal.create({ closeable: false }).tag({
           class: 'net.nanopay.ui.modal.SessionTimeoutModal',
         }));
