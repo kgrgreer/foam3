b48898b00f4c6e4ac5a33a8c977c937252317975
olha <olha@MacBook-Pro-Image.local>
2019-10-10 10:22:52 -0400

    css added

--- nanopay/src/net/nanopay/invoice/ui/modal/MarkAsVoidModal.js
+++ nanopay/src/net/nanopay/invoice/ui/modal/MarkAsVoidModal.js
@@ -31,21 +31,27 @@ foam.CLASS({
         { name: 'VOID_ERROR', message: 'Invoice could not be voided.'}        
     ],
 
-    // css: `
-    // ^ {
-    //     width: 330px;
-    //     padding: 25px;
-    //     margin: auto;
-    //     font-family: Roboto;
-    //   }
-    // `,
+    css: `
+        ^ .size {
+            width: 330px;
+      }
+      ^ .padding {
+          padding: 0px 25px;
+      }
+      ^ .note-size {
+        margin-left: 20px;
+        padding: 10px 5px;
+        width: 90%;
+      }
+    `,
 
     properties: [
         {
-            class: 'String',
             name: 'note',
-            value: 'bla',
-            view: { class: 'foam.u2.tag.TextArea', rows: 1, cols: 1 }
+            class: 'String',
+            //placeholder: 'note',
+            value: 'note',
+            view: { class: 'foam.u2.tag.TextArea', rows: 1, cols: 1}
         },
         'invoice'
     ],
@@ -60,20 +66,22 @@ foam.CLASS({
 
             this
             .addClass(this.myClass())
-            .start()
-                .start('h2')
+            .start().addClass('size')
+                .start('h2').addClass('padding')
                     .add(this.TITLE)
                 .end()
-                .start('p')
+                .start('p').addClass('padding')
                     .add(this.MSG1)
                 .end()
-                .start('p')
+                .start('p').addClass('padding')
                     .add(this.MSG2)
                 .end()
-                .start(this.NOTE)
+                .start().addClass('label').add('Note').end()
+                .start(this.NOTE)//.addClass('input-box')//.addClass('padding').addClass('note-size')
                 .end()
                 .start({class: 'net.nanopay.sme.ui.wizardModal.WizardModalNavigationBar', 
                     back: this.CANCEL, next: this.VOID_METHOD})
+                    
                 .end()
                 // .start()
                 //     .addClass('button-container')
@@ -96,7 +104,7 @@ foam.CLASS({
             label: 'Void',
             code: function(X) {
                 self.invoice.paymentMethod = self.PaymentStatus.VOID;
-                self.invoice.note = self.note;
+                self.invoice.note = self.NOTE;
                 self.invoiceDAO.put(self.invoice).then((invoice) => {
                   if (invoice.paymentMethod == self.PaymentStatus.VOID) {
                     self.notify(self.VOID_SUCCESS, 'success');
