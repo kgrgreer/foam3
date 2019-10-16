baa36c726e4f487b68a28050364174fde15d1f97
carl-zzz <carl.chen@nanopay.net>
2019-10-16 10:22:03 -0400

    Refactor

--- nanopay/src/net/nanopay/invoice/InvoiceNotificationDAO.js
+++ nanopay/src/net/nanopay/invoice/InvoiceNotificationDAO.js
@@ -22,6 +22,7 @@ foam.CLASS({
     'net.nanopay.invoice.model.Invoice',
     'net.nanopay.invoice.model.InvoiceStatus',
     'net.nanopay.invoice.model.PaymentStatus',
+    'net.nanopay.model.Business',
     'net.nanopay.model.Currency',
     'static foam.mlang.MLang.*'
   ],
@@ -57,7 +58,7 @@ foam.CLASS({
         User payerUser = (User) invoice.findPayerId(x);
         User payeeUser = (User) invoice.findPayeeId(x);
 
-        String businessName = payeeUser.getOrganization();
+        String businessName = payeeUser instanceof Business ? payeeUser.getBusinessName() : payeeUser.getOrganization();
     
         InvoiceStatus newInvoiceStatus = invoice.getStatus();
         InvoiceStatus oldInvoiceStatus = oldInvoice != null ? oldInvoice.getStatus() : null;
