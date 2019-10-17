67e7f575e290bfa87c1f05056e6ce96944b98dd2
nanoMichal <michall.pasternak@nanopay.net>
2019-10-17 10:52:16 -0400

    create a denomination baseclass for currency and security:

---
+++ nanopay/src/net/nanopay/model/Denomination.js
@@ -0,0 +1,64 @@
+foam.CLASS({
+  package: 'net.nanopay.model',
+  name: 'Denomination',
+  abstract: true,
+
+  documentation: `The abstract model for fungible digitized assets`,
+
+  ids: [
+    'alphabeticCode'
+  ],
+
+  imports: [
+    'homeDenomination'
+  ],
+  javaImports: [
+    'foam.util.SafetyUtil'
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'name',
+      documentation: 'name of the asset'
+      required: true
+    },
+    {
+      class: 'String',
+      name: 'alphabeticCode',
+      label: 'Code',
+      documentation: 'The alphabetic code associated with the asset. Used as an ID.',
+      required: true
+    },
+    {
+      class: 'Int',
+      name: 'precision',
+      documentation: 'Defines the number of digits that come after the decimal point. ',
+      required: true
+    }
+  ],
+
+  methods: [
+    {
+      name: 'toSummary',
+      documentation: `When using a reference to the currencyDAO, the labels associated
+        to it will show a chosen property rather than the first alphabetical string
+        property. In this case, we are using the alphabeticCode.
+      `,
+      code: function(x) {
+        return this.alphabeticCode;
+      }
+    },
+    {
+      name: 'format',
+      abstract: true,
+      args: [
+        {
+          class: 'foam.core.Currency',
+          name: 'amount'
+        }
+      ],
+      type: 'String'
+    }
+  ]
+});
