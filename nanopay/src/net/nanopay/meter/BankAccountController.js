aa86e83c215a239e058ee1fc356f8ede9501a022
carl-zzz <carl.chen@nanopay.net>
2019-10-23 15:20:06 -0400

    clean up

--- nanopay/src/net/nanopay/meter/BankAccountController.js
+++ nanopay/src/net/nanopay/meter/BankAccountController.js
@@ -39,7 +39,6 @@ foam.CLASS({
       class: 'foam.u2.ViewSpec',
       name: 'summaryView',
       factory: function() {
-        var self = this;
         return {
           class: 'foam.u2.view.ScrollTableView',
           columns: [
