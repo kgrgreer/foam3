0bf12e9ee2662af19d9009ba29b037ea248c31e1
Joel Hughes <joel@nanopay.net>
2019-10-24 12:10:44 -0400

    Currency to exchangeable package Remove DeletedAwareDAO from localUserDAO

--- nanopay/src/net/nanopay/invoice/InvoiceNotificationDAO.js
+++ nanopay/src/net/nanopay/invoice/InvoiceNotificationDAO.js
@@ -23,7 +23,7 @@ foam.CLASS({
     'net.nanopay.invoice.model.InvoiceStatus',
     'net.nanopay.invoice.model.PaymentStatus',
     'net.nanopay.model.Business',
-    'net.nanopay.model.Currency',
+    'net.nanopay.exchangeable.Currency',
     'static foam.mlang.MLang.*'
   ],
 
