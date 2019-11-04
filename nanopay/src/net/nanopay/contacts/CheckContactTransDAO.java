bb0be28c9a6ecd3089a016c7b54b6d95111a9474
yij793 <jiangarfield@gmail.com>
2019-11-04 11:03:34 -0500

    create function to find the if txn exist bw user and contact

--- nanopay/src/net/nanopay/contacts/CheckContactTransDAO.java
+++ nanopay/src/net/nanopay/contacts/CheckContactTransDAO.java
@@ -1,13 +1,14 @@
 package net.nanopay.contacts;
 
+import java.util.ArrayList;
+import java.util.List;
 import foam.core.FObject;
 import foam.core.X;
+import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
-import foam.nanos.auth.User;
-import foam.util.SafetyUtil;
-import net.nanopay.accounting.quickbooks.model.QuickbooksContact;
-import net.nanopay.accounting.xero.model.XeroContact;
+import net.nanopay.account.Account;
+import net.nanopay.tx.model.Transaction;
 
 import static foam.mlang.MLang.*;
 
@@ -16,14 +17,27 @@ public class CheckContactTransDAO extends ProxyDAO {
 
   public CheckContactTransDAO(X x, DAO delegate) {
     super(x, delegate);
-    transactionDAO_ = ((DAO) x.get("localTransactionDAO")).inX(x);
+    transactionDAO_ = ((DAO) x.get("localTransactionDAO")).inX(getX());
   }
 
   @Override
   public FObject remove_(X x, FObject obj) {
-    if(true) {
-      throw new RuntimeException("Cannot put null!");
+
+    Contact contact = (Contact) obj;
+
+    DAO accountDAO = (DAO) x.get("accountDAO");
+
+    List<Account> accounts = ((ArraySink) accountDAO.where(EQ(Account.OWNER, contact.getId())).select(new ArraySink())).getArray();
+    
+    
+    for ( Account account : accounts ) {
+      Transaction txn = (Transaction) transactionDAO_.find(
+        OR(EQ(Transaction.DESTINATION_ACCOUNT, account.getId()),
+           EQ(Transaction.SOURCE_ACCOUNT, account.getId()))
+        );
+      if ( txn != null ) throw new RuntimeException("Cannot delete this contact!");
     }
+
     return super.remove_(x, obj);
   }
 }
