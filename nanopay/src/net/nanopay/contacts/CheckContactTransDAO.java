49823f4c1c14837048ca5cfc2e05375f34999ab1
yij793 <jiangarfield@gmail.com>
2019-11-05 12:50:38 -0500

    add function remove draft and unpaid invoice as well, if user has business account it will unremvoebale

--- nanopay/src/net/nanopay/contacts/CheckContactTransDAO.java
+++ nanopay/src/net/nanopay/contacts/CheckContactTransDAO.java
@@ -8,6 +8,9 @@ import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import net.nanopay.account.Account;
+import net.nanopay.admin.model.AccountStatus;
+import net.nanopay.invoice.model.Invoice;
+import net.nanopay.invoice.model.InvoiceStatus;
 import net.nanopay.tx.model.Transaction;
 
 import static foam.mlang.MLang.*;
@@ -17,7 +20,7 @@ public class CheckContactTransDAO extends ProxyDAO {
 
   public CheckContactTransDAO(X x, DAO delegate) {
     super(x, delegate);
-    transactionDAO_ = ((DAO) x.get("localTransactionDAO")).inX(getX());
+    transactionDAO_ = ((DAO) x.get("localTransactionDAO")).inX(x);
   }
 
   @Override
@@ -26,9 +29,18 @@ public class CheckContactTransDAO extends ProxyDAO {
     Contact contact = (Contact) obj;
 
     DAO accountDAO = (DAO) x.get("accountDAO");
+    DAO invoiceDAO = (DAO) x.get("invoiceDAO");
+
+    List<Invoice> iv = ((ArraySink) invoiceDAO.where(OR(
+      EQ(Invoice.PAYEE_ID, contact.getId()),
+      EQ(Invoice.PAYER_ID, contact.getId())
+    )).select(new ArraySink())).getArray();
+
+    if ( contact.getBusinessStatus().equals(AccountStatus.ACTIVE) ) {
+      throw new RuntimeException("You can't delete this contact");
+    }
 
     List<Account> accounts = ((ArraySink) accountDAO.where(EQ(Account.OWNER, contact.getId())).select(new ArraySink())).getArray();
-    
     
     for ( Account account : accounts ) {
       Transaction txn = (Transaction) transactionDAO_.find(
@@ -38,6 +50,15 @@ public class CheckContactTransDAO extends ProxyDAO {
       if ( txn != null ) throw new RuntimeException("Cannot delete this contact!");
     }
 
+    for ( Invoice invoice : iv ) {
+      Invoice v = (Invoice) invoiceDAO.find(
+        OR(
+          EQ( InvoiceStatus.UNPAID, invoice.getStatus()),
+          EQ( InvoiceStatus.DRAFT, invoice.getStatus())
+        )
+      );
+      invoiceDAO.remove(v);
+    };
     return super.remove_(x, obj);
   }
 }
