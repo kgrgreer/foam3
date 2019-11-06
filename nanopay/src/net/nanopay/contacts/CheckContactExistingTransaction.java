3037f99a3f083fafccc9b02f9108d6dff80105a4
yij793 <jiangarfield@gmail.com>
2019-11-06 12:16:18 -0500

    remove unuse extendsions,add document to this decorator

--- nanopay/src/net/nanopay/contacts/CheckContactExistingTransaction.java
+++ nanopay/src/net/nanopay/contacts/CheckContactExistingTransaction.java
@@ -1,6 +1,5 @@
 package net.nanopay.contacts;
 
-import java.util.ArrayList;
 import java.util.List;
 import foam.core.FObject;
 import foam.core.X;
@@ -8,47 +7,54 @@ import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import net.nanopay.account.Account;
-import net.nanopay.admin.model.AccountStatus;
 import net.nanopay.invoice.model.Invoice;
 import net.nanopay.invoice.model.InvoiceStatus;
 import net.nanopay.tx.model.Transaction;
 
 import static foam.mlang.MLang.*;
 
-public class CheckContactTransDAO extends ProxyDAO {
-  public DAO transactionDAO_;
+/**
+ * When deleting a contact, check if this contact is already associated with a
+ * business or any transactions. If so, throw an error. The client will catch it let the user
+ * know that they are unable to delete that contact from the list of contact. If not, delete
+ * the invoice associate with this contact.
+ */
 
-  public CheckContactTransDAO(X x, DAO delegate) {
+public class CheckContactExistingTransaction extends ProxyDAO {
+
+  public CheckContactExistingTransaction(X x, DAO delegate) {
     super(x, delegate);
-    transactionDAO_ = ((DAO) x.get("localTransactionDAO")).inX(x);
   }
 
   @Override
   public FObject remove_(X x, FObject obj) {
 
     Contact contact = (Contact) obj;
-
+    
+    DAO transactionDAO_ = ((DAO) x.get("localTransactionDAO"));
     DAO accountDAO = (DAO) x.get("accountDAO");
     DAO invoiceDAO = (DAO) x.get("invoiceDAO");
 
-    List<Invoice> iv = ((ArraySink) invoiceDAO.where(OR(
-      EQ(Invoice.PAYEE_ID, contact.getId()),
-      EQ(Invoice.PAYER_ID, contact.getId())
-    )).select(new ArraySink())).getArray();
-
-    if ( !contact.getSignUpStatus().equals(ContactStatus.NOT_INVITED) ) {
-      throw new RuntimeException("Cannot delete this contact becasue this contact is associated");
+    if ( ! contact.getSignUpStatus().equals(ContactStatus.NOT_INVITED) ) {
+      throw new RuntimeException("Cannot delete this contact because this contact is associated");
     }
 
     List<Account> accounts = ((ArraySink) accountDAO.where(EQ(Account.OWNER, contact.getId())).select(new ArraySink())).getArray();
     
     for ( Account account : accounts ) {
       Transaction txn = (Transaction) transactionDAO_.find(
-        OR(EQ(Transaction.DESTINATION_ACCOUNT, account.getId()),
-           EQ(Transaction.SOURCE_ACCOUNT, account.getId()))
-        );
-      if ( txn != null ) throw new RuntimeException("Cannot delete this contact becasue a transcation is associated with this contact");
+        OR(
+          EQ(Transaction.DESTINATION_ACCOUNT, account.getId()),
+          EQ(Transaction.SOURCE_ACCOUNT, account.getId())
+        )
+      );
+      if ( txn != null ) throw new RuntimeException("Cannot delete this contact because a transcation is associated with this contact");
     }
+
+    List<Invoice> iv = ((ArraySink) invoiceDAO.where(OR(
+      EQ(Invoice.PAYEE_ID, contact.getId()),
+      EQ(Invoice.PAYER_ID, contact.getId())
+    )).select(new ArraySink())).getArray();
 
     for ( Invoice invoice : iv ) {
       Invoice v = (Invoice) invoiceDAO.find(
@@ -60,6 +66,7 @@ public class CheckContactTransDAO extends ProxyDAO {
       );
       if ( v != null )invoiceDAO.remove(v);
     };
+
     return super.remove_(x, obj);
   }
 }
