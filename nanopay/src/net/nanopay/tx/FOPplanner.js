2bb1c215694515e579710dc8dfba3231092d909a
nanoMichal <michall.pasternak@nanopay.net>
2019-11-06 13:03:40 -0500

    predicate for FOPplanner

--- nanopay/src/net/nanopay/tx/FOPplanner.js
+++ nanopay/src/net/nanopay/tx/FOPplanner.js
@@ -2,12 +2,15 @@ foam.CLASS({
   package: 'net.nanopay.tx',
   name: 'FOPplanner',
   documentation: 'A planner for planning FOP type transactions',
-
+//predicate: "predicate":{"class":"net.nanopay.tx.ruler.PropertyEQProperty","prop1":"sourceUnit","prop2":"destinationUnit"}
   implements: ['foam.nanos.ruler.RuleAction'],
 
   javaImports: [
     'foam.nanos.logger.Logger',
     'net.nanopay.tx.SecurityTransaction',
+    'net.nanopay.tx.model.Transaction',
+    'java.util.List',
+    'java.util.ArrayList'
   ],
 
 
@@ -15,17 +18,18 @@ foam.CLASS({
     {
       name: 'applyAction',
       javaCode: `
-        Transaction tx = (Transaction) obj.getRequestTransaction();
-        Transaction plan = new SecurityTransaction.Builder(x).build();
-        plan.setSourceCurrency(obj.getSourceDenomination());
-        plan.setDestinationCurrency(obj.getDestinationDenomination());
+        TransactionQuote txq = (TransactionQuote) obj;
+        Transaction tx = txq.getRequestTransaction();
+        SecurityTransaction plan = new SecurityTransaction.Builder(x).build();
+        plan.setSourceCurrency(txq.getSourceUnit());
+        plan.setDestinationCurrency(txq.getDestinationUnit());
 
-        plan.setSourceAccount(obj.getSourceAccount()); //TODO add the security so we know which account to actually get.
-        plan.setDestinationAccount(obj.getDestinationAccount());
+        plan.setSourceAccount(txq.getSourceAccount().getId()); //TODO add the security so we know which account to actually get.
+        plan.setDestinationAccount(txq.getDestinationAccount().getId());
 
         plan.setIsQuoted(true);
-        plan = createTransfers_(getX(), plan));
-        obj.addPlan(plan);
+        plan.setTransfers(createTransfers_(getX(), plan));
+        txq.addPlan(plan);
       `
     },
     {
@@ -36,14 +40,14 @@ foam.CLASS({
           type: 'Context'
         },
         {
-          name: 'newPlan'
+          name: 'newPlan',
           type: 'net.nanopay.tx.SecurityTransaction'
         },
       ],
       type: 'net.nanopay.tx.Transfer[]',
       javaCode: `
         List all = new ArrayList();
-        TransactionLineItem[] lineItems = getLineItems();
+        TransactionLineItem[] lineItems = newPlan.getLineItems();
         for ( int i = 0; i < lineItems.length; i++ ) {
           TransactionLineItem lineItem = lineItems[i];
           Transfer[] transfers = lineItem.createTransfers(x, null, newPlan, false);
@@ -52,10 +56,10 @@ foam.CLASS({
           }
         }
 
-        all.add(new Transfer.Builder(x).setAccount(newPlan.getSourceAccount()).setAmount(-newPlan.getTotal).build());
-        all.add(new Transfer.Builder(x).setAccount(newPlan.getDestinationAccount()).setAmount(newPlan.getTotal).build());
+        all.add(new Transfer.Builder(x).setAccount(newPlan.getSourceAccount()).setAmount(-newPlan.getTotal()).build());
+        all.add(new Transfer.Builder(x).setAccount(newPlan.getDestinationAccount()).setAmount(newPlan.getTotal()).build());
         return (Transfer[]) all.toArray(new Transfer[0]);
       `
-    },
-
+    }
+  ]
 });
---
+++ nanopay/src/net/nanopay/tx/ruler/PropertyEQProperty.js
@@ -0,0 +1,45 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.ruler',
+  name: 'PropertyEQProperty',
+
+  documentation: 'A predicate that returns true when a specific property equals the other specified property the new or old object.',
+
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
+
+  javaImports: [
+    'foam.core.FObject',
+    'static foam.mlang.MLang.*'
+  ],
+  properties: [
+    {
+      class: 'String',
+      name: 'prop1'
+    },
+    {
+      class: 'String',
+      name: 'prop2'
+    },
+    {
+      class: 'Boolean',
+      name: 'isNew',
+      value: true
+    }
+  ],
+
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+        if ( getIsNew() ) {
+          FObject nu  = (FObject) NEW_OBJ.f(obj);
+          return EQ(nu.getClassInfo().getAxiomByName(getProp1()), nu.getClassInfo().getAxiomByName(getProp2())).f(nu);
+        }
+        else {
+          FObject old = (FObject) OLD_OBJ.f(obj);
+          return EQ(old.getClassInfo().getAxiomByName(getProp1()), old.getClassInfo().getAxiomByName(getProp2())).f(old);
+        }
+      `
+    }
+  ]
+});
