59fab35a379e0bc1d899fb7c5062cefc0e074304
Patrick Zanowski <patrick@nanopay.net>
2019-11-21 20:34:46 -0500

    Predicated bankoperation rules in jrl (damn you Michal)

--- nanopay/tools/classes.js
+++ nanopay/tools/classes.js
@@ -108,9 +108,7 @@ var classes = [
   'net.nanopay.bank.BankHoliday',
   'net.nanopay.bank.BankHolidayService',
   'net.nanopay.bank.BankWeekend',
-  'net.nanopay.bank.VerifiedBankAccountPredicate',
   'net.nanopay.bank.DefaultBankAccountRule',
-  'net.nanopay.bank.IsDefaultPredicate',
   'net.nanopay.bank.IsDefaultRule',
 
   //Exchangeable
