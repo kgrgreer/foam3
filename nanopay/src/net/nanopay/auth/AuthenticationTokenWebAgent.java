83bbf6802434a0ec4bc59a02ff90c1da57c8b1a1
Chanmann Lim <chanmannlim@gmail.com>
2019-11-24 13:56:53 -0500

    Use DigUtil.outputException instead

--- nanopay/src/net/nanopay/auth/AuthenticationTokenWebAgent.java
+++ nanopay/src/net/nanopay/auth/AuthenticationTokenWebAgent.java
@@ -2,24 +2,18 @@ package net.nanopay.auth;
 
 import foam.core.X;
 import foam.dao.DAO;
-import foam.lib.AndPropertyPredicate;
-import foam.lib.NetworkPropertyPredicate;
-import foam.lib.PermissionedPropertyPredicate;
-import foam.lib.PropertyPredicate;
-import foam.lib.json.JSONParser;
-import foam.lib.json.Outputter;
 import foam.nanos.auth.User;
 import foam.nanos.auth.token.TokenService;
+import foam.nanos.dig.DigUtil;
 import foam.nanos.dig.exception.DAOPutException;
 import foam.nanos.dig.exception.DigErrorMessage;
 import foam.nanos.dig.exception.DigSuccessMessage;
 import foam.nanos.dig.exception.GeneralException;
+import foam.nanos.http.Format;
 import foam.nanos.http.WebAgent;
 import foam.util.SafetyUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,40 +47,12 @@ public class AuthenticationTokenWebAgent implements WebAgent {
       DigErrorMessage error = success
         ? new DigSuccessMessage.Builder(x).setMessage(SUCCESS).build()
         : new DAOPutException.Builder(x).setMessage(FAILURE).build();
-      outputException(x, null, "JSON", null, error);
+      DigUtil.outputException(x, null, Format.JSON, null, error);
     } catch (Exception e) {
       DigErrorMessage error = new GeneralException.Builder(x)
         .setMessage(e.toString())
         .build();
-      outputException(x, null, "JSON", null, error);
-    }
-  }
-
-  protected void outputException(X x, HttpServletResponse resp, String format, PrintWriter out, DigErrorMessage error) {
-    if ( resp == null ) resp = x.get(HttpServletResponse.class);
-    if ( out == null ) out = x.get(PrintWriter.class);
-
-    resp.setStatus(Integer.parseInt(error.getStatus()));
-    format = "JSON";  // Currently supporting only JSON
-
-    if ( format.equals("JSON") ) {
-      resp.setContentType("application/json");
-
-      JSONParser jsonParser = new JSONParser();
-      jsonParser.setX(x);
-
-      Outputter outputterJson = new foam.lib.json.Outputter(x)
-        .setPropertyPredicate(
-          new AndPropertyPredicate(x, new PropertyPredicate[]{
-            new NetworkPropertyPredicate(),
-            new PermissionedPropertyPredicate()
-          })
-        );
-      outputterJson.setOutputDefaultValues(true);
-      outputterJson.setOutputClassNames(true);
-      outputterJson.output(error);
-
-      out.println(outputterJson.toString());
+      DigUtil.outputException(x, null, Format.JSON, null, error);
     }
   }
 }
