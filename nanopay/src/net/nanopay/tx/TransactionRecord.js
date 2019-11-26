68849bb9c76c622904f6f05224fe5d0f2ba3d73b
sirenChen <siren@nanopay.net>
2019-11-26 09:50:19 -0500

    remove unused import.

--- nanopay/src/net/nanopay/tx/TransactionRecord.js
+++ nanopay/src/net/nanopay/tx/TransactionRecord.js
@@ -2,13 +2,6 @@ foam.CLASS({
   package: 'net.nanopay.tx',
   name: 'TransactionRecord',
 
-  javaImports: [
-    'foam.core.X',
-    'foam.dao.DAO',
-    'net.nanopay.tx.model.Transaction',
-    'java.util.Date'
-  ],
-
   implements: [
     'foam.nanos.auth.CreatedAware'
   ],
