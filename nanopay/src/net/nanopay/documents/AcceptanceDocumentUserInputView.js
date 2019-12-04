37b3d774babc4a848596b5240e51e0d042d448a6
jin <jin@MacBook-Pro-Catalina-Image-2.local>
2019-12-04 10:27:59 -0500

    add space to checkbox and checkboxtext

--- nanopay/src/net/nanopay/documents/AcceptanceDocumentUserInputView.js
+++ nanopay/src/net/nanopay/documents/AcceptanceDocumentUserInputView.js
@@ -14,6 +14,12 @@ foam.CLASS({
     'acceptanceDocumentService'
   ],
 
+  css: `
+  .spaceToRight{
+    margin-right: 10px;
+  }
+  `,
+
   properties: [
     {
       class: 'String',
@@ -47,7 +53,7 @@ foam.CLASS({
         .startContext({ data: this })
           .start()
             .hide(this.doc$.map((d) => ! d))
-            .start(this.AGREED)
+            .start(this.AGREED).addClass('spaceToRight')  //style({ margin: '0 10px 0 0' })
               .add(this.doc$.dot('checkboxText'))
               .start('a')
                 .add(this.doc$.dot('title'))
