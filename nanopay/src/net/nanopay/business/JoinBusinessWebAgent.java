a27a7cf033cf6d6b7b2e7f6586aae8217b19ba94
Patrick Zanowski <patrick@nanopay.net>
2019-12-04 13:01:16 -0500

    Update nanopay/src/net/nanopay/business/JoinBusinessWebAgent.java
    Co-Authored-By: Anna <anna@nanopay.net>

--- nanopay/src/net/nanopay/business/JoinBusinessWebAgent.java
+++ nanopay/src/net/nanopay/business/JoinBusinessWebAgent.java
@@ -54,7 +54,7 @@ public class JoinBusinessWebAgent implements WebAgent {
     String redirect = request.getParameter("redirect");
     User user = null;
 
-    businessOnboardingDAO_ = (DAO) x.get("businessOnboardingDAO");;
+    businessOnboardingDAO_ = (DAO) x.get("businessOnboardingDAO");
     uSBusinessOnboardingDAO_ = (DAO) x.get("uSBusinessOnboardingDAO");
 
     try {
@@ -165,4 +165,3 @@ public class JoinBusinessWebAgent implements WebAgent {
     }
   }
 }
-
