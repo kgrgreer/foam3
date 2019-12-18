4de859f988fdc870d59818bc4d36761e9188fca7
Scott Head <scott@nanopay.net>
2019-12-18 16:44:50 -0500

    Formatting ingestion code to be more readable

--- nanopay/src/net/nanopay/tx/gs/GsRowToTx.js
+++ nanopay/src/net/nanopay/tx/gs/GsRowToTx.js
@@ -3,18 +3,19 @@ foam.CLASS({
   name: 'GsRowToTx',
 
   javaImports: [
-    'net.nanopay.tx.gs.GsTxCsvRow',
-    'foam.dao.MDAO',
+    'foam.dao.ArraySink',
     'foam.dao.DAO',
     'foam.dao.EasyDAO',
-    'foam.util.SafetyUtil',
+    'foam.dao.MDAO',
     'foam.lib.parse.CSVParser',
     'foam.mlang.MLang',
     'foam.mlang.sink.Count',
-    'foam.dao.ArraySink',
+    'foam.nanos.logger.Logger',
     'foam.util.concurrent.SyncAssemblyLine',
     'foam.util.concurrent.AsyncAssemblyLine',
+    'foam.util.SafetyUtil',
     'java.util.List',
+    'net.nanopay.tx.gs.GsTxCsvRow',
     'net.nanopay.tx.gs.ProgressBarData'
   ],
 
@@ -23,34 +24,40 @@ foam.CLASS({
       documentation: 'Process a file blob and make some transactions',
       name: 'process',
       args: [
-        { name: 'x', type: 'foam.core.X'},
-        { name: 'blob', type: 'foam.blob.Blob'},
-        { name: 'progId', type: 'String'}
+        { name: 'x', type: 'foam.core.X' },
+        { name: 'blob', type: 'foam.blob.Blob' },
+        { name: 'progId', type: 'String' }
       ],
       javaCode: `
-        System.out.println("Reading File.. ");
-      // ---- parse file into GsTxCsvRow Objects..
-
+        Logger logger = (Logger) x.get("logger");
+        logger.info(" ** Reading file ... " + blob.getSize());
+        
+        // ---- parse file into GsTxCsvRow Objects ...
         ProgressBarData pbd = new ProgressBarData ();
         pbd.setId(progId);
         pbd.setName(progId);
         pbd.setStatus("Reading CSV...");
-        DAO pbdDAO = (DAO) x.get("ProgressBarDAO");
-        pbdDAO.put(pbd);
+        DAO progressBarDAO = (DAO) x.get("ProgressBarDAO");
+        progressBarDAO.put(pbd);
 
         java.io.ByteArrayOutputStream os = new java.io.ByteArrayOutputStream((int)blob.getSize());
         blob.read(os, 0, blob.getSize());
         foam.lib.parse.StringPStream ps = new foam.lib.parse.StringPStream(os.toString());
-        System.out.println("blob read");
+        logger.info(" ** Blob read into stream: " + blob.getSize());
+
         foam.lib.parse.ParserContextImpl px = new foam.lib.parse.ParserContextImpl();
         px.set("X", x);
 
+        // Create DAO to store rows from file
         DAO gsTxCsvRowDAO = new foam.dao.SequenceNumberDAO.Builder(x)
-        .setDelegate(new foam.dao.MDAO(GsTxCsvRow.getOwnClassInfo()))
-        .build();
-        ((MDAO) ((foam.dao.ProxyDAO)gsTxCsvRowDAO).getDelegate()).addIndex(new foam.core.PropertyInfo[] {
-            net.nanopay.tx.gs.GsTxCsvRow.CASH_USD
-        });
+          .setDelegate(new foam.dao.MDAO(GsTxCsvRow.getOwnClassInfo()))
+          .build();
+        // Add index to MDAO
+        ((MDAO) ((foam.dao.ProxyDAO)gsTxCsvRowDAO).getDelegate())
+          .addIndex(new foam.core.PropertyInfo[] {
+              net.nanopay.tx.gs.GsTxCsvRow.CASH_USD,
+              net.nanopay.tx.gs.GsTxCsvRow.SEC_QTY
+          });
 
         CSVParser csvParser = new CSVParser(
           net.nanopay.tx.gs.GsTxCsvRow.getOwnClassInfo(),
@@ -58,43 +65,47 @@ foam.CLASS({
             .setDao(gsTxCsvRowDAO)
             .build());
         csvParser.parse(ps, px);
-
         long am = ((Count) gsTxCsvRowDAO.select(MLang.COUNT())).getValue();
+        logger.info(" ** Read and parsed " + am + " rows ...");
 
         pbd.setMaxValue(am);
         pbd.setStatus("Parsing Transaction: 0 of " + am);
-        pbdDAO.put(pbd);
+        progressBarDAO.put(pbd);
 
         SyncAssemblyLine transactionProcessor = new SyncAssemblyLine();
 
         List <GsTxCsvRow> rows = ( (ArraySink) gsTxCsvRowDAO
            .select(new ArraySink())).getArray();
 
-        System.out.println("Lines read: "+am);
-
         // -- begin Job creation and execution
+        logger.info(" ** Processing: " + rows.size() + " rows ...");
         int i = 0;
-        System.out.println("I have: "+ rows.size() + " rows..");
-
+        int modulus = (rows.size() / 10) + 1;
         for ( GsTxCsvRow row1 : rows ) {
-        //System.out.println("on iteration: "+ i);
-        i++;
-        GsTxAssembly job = new GsTxAssembly.Builder(x)
-          .setOutputDAO( (DAO) x.get("localTransactionDAO") )
-          .setRow1(row1)
-          .build();
-        if ( i % 11 == 0){
-          pbd = (ProgressBarData) pbd.fclone();
-          pbd.setValue(i);
-          pbd.setStatus("Parsing Transaction: "+ pbd.getValue() +" of " + rows.size());
-          job.setPbd(pbd);
-          //pbdDAO.put(pbd);
-        }
-          /*long count = ((Count) gsTxCsvRowDAO.select(MLang.COUNT())).getValue();
+          // logger.info(" ** On iteration: " + i + " of: " + am);
+          i++;
+          GsTxAssembly job = new GsTxAssembly.Builder(x)
+            .setOutputDAO( (DAO) x.get("localTransactionDAO") )
+            .setRow1(row1)
+            .build();
+          if ( i % modulus == 0 ) {
+            pbd = (ProgressBarData) pbd.fclone();
+            pbd.setValue(i);
+            pbd.setStatus("Parsing Transaction: " + pbd.getValue() + " of " + rows.size());
+            job.setPbd(pbd);
+            //progressBarDAO.put(pbd);
+
+            logger.info(" ** " + pbd.getStatus());
+          }
+          
+          /*
+          long count = ((Count) gsTxCsvRowDAO.select(MLang.COUNT())).getValue();
           if ( count == 0 )
-            break; */
+            break; 
+            */
+          
           //---- handle external jobs
-          if ( SafetyUtil.equals(row1.getIsInternal(),"0") ) {
+          if ( SafetyUtil.equals(row1.getIsInternal(), "0") ) {
             transactionProcessor.enqueue(job);
             continue;
           }
@@ -102,26 +113,31 @@ foam.CLASS({
 
           //-- Cash txns.
           if ( isCash(row1) ) {
+            // Make sure the amount if positive
             if ( row1.getCashUSD() < 0 ) continue;
 
-          Object [] arr = ( (ArraySink) gsTxCsvRowDAO
-            .where(MLang.EQ(net.nanopay.tx.gs.GsTxCsvRow.CASH_USD, -row1.getCashUSD()))
-            .limit(1)
-            .select(new ArraySink())).getArray().toArray();
-          if ( arr.length == 0 ) {
-            System.out.println("error unmatched internal Cash transaction, no tx made for "+row1.getTransactionId());
-            continue;
-          }
-          job.setRow2( (GsTxCsvRow) arr[0] );
+            // Find the matching cash transaction to the original row
+            Object [] arr = ( (ArraySink) gsTxCsvRowDAO
+              .where(MLang.EQ(net.nanopay.tx.gs.GsTxCsvRow.CASH_USD, -row1.getCashUSD()))
+              .limit(1)
+              .select(new ArraySink())).getArray().toArray();
+            if ( arr.length == 0 ) {
+              logger.warning("Unmatched internal cash transaction, no transaction made for " + row1.getTransactionId() + " - row: " + i);
+              continue;
+            }
+            job.setRow2( (GsTxCsvRow) arr[0] );
           }
           else {
+            // Make sure the quantity is positive
             if ( row1.getSecQty() < 0 ) continue;
+
+            // Find the matching securities transaction to the original row
             Object [] arr = ( (ArraySink) gsTxCsvRowDAO
               .where(MLang.EQ(net.nanopay.tx.gs.GsTxCsvRow.SEC_QTY, -row1.getSecQty()))
               .limit(1)
               .select(new ArraySink())).getArray().toArray();
             if ( arr.length == 0 ) {
-              System.out.println("error unmatched internal Securities transaction, no tx made for "+row1.getTransactionId());
+              logger.warning("Unmatched internal securities transaction, no transaction made for " + row1.getTransactionId() + " - row: " + i);
               continue;
             }
             job.setRow2( (GsTxCsvRow) arr[0] );
@@ -132,17 +148,18 @@ foam.CLASS({
 
         pbd.setValue(am);
         pbd.setStatus("💰🤑💸 Congratualtions File Has  Been Ingested 💸🤑💰");
-        //pbdDAO.put(pbd);
+        //progressBarDAO.put(pbd);
+        logger.info(" ** " + pbd.getStatus());
       `
     },
     {
       name: 'isCash',
       args: [
-      { name: 'row', type: 'net.nanopay.tx.gs.GsTxCsvRow'}
+      { name: 'row', type: 'net.nanopay.tx.gs.GsTxCsvRow' }
       ],
       type: 'Boolean',
       javaCode: `
-        if ( SafetyUtil.equals(row.getSettleType(),"Cash") )
+        if ( SafetyUtil.equals(row.getSettleType(), "Cash") )
           return true;
         return false;
       `
