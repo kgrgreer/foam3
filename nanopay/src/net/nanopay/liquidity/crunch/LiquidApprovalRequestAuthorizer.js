b096ad31e14b3b45a3049c15df0224709f2cc8e6
noodlemoodle <qiyzhang@icloud.com>
2019-12-23 14:09:15 -0500

    liquidapprovalrequest authorizer

---
+++ nanopay/src/net/nanopay/liquidity/crunch/LiquidApprovalRequestAuthorizer.js
@@ -0,0 +1,77 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+ // TODO ruby change outgoingaccount stuff after approvable interface stuff merged
+
+foam.CLASS({
+  package: 'net.nanopay.liquidity.crunch',
+  name: 'LiquidApprovalRequestAuthorizer',
+  extends: 'net.nanopay.liquidity.crunch.LiquidAuthorizer',
+  implements: [ 'foam.nanos.auth.Authorizer' ],
+
+  javaImports: [
+      'foam.nanos.auth.AuthorizationException',
+      'foam.nanos.auth.AuthService',
+      'net.nanopay.liquidity.LiquidApprovalRequest'
+    ],
+
+  methods: [
+    {
+      name: 'createPermission',
+      args: [
+        { name: 'className', class: 'String' },
+        { name: 'outgoingAccountId', class: 'Long' }
+      ],
+      javaCode: `
+        String permission = "canApprove";
+        permission += className.substring(0, 1).toUpperCase() + className.substring(1);
+        if ( outgoingAccountId >= 0 ) permission += "." + outgoingAccountId;
+        return permission;
+      `
+    },
+    {
+      name: 'authorizeOnRead',
+      javaCode:  `
+        this.authorizeOnUpdate(x, obj, null);
+      `
+    },
+    {
+      name: 'authorizeOnUpdate',
+      javaCode:  `
+        foam.nanos.auth.User user = (foam.nanos.auth.User) x.get("user");
+        if ( user != null && ( user.getId() == foam.nanos.auth.User.SYSTEM_USER_ID || user.getGroup().equals("admin") || user.getGroup().equals("system") ) ) return;
+
+        LiquidApprovalRequest ar = (LiquidApprovalRequest) oldObj;
+        Long accountId = ar.getOutgoingAccount().getId();
+        String className = ar.getDaoKey().getOf().getName();
+
+        String permission = createPermission(className, accountId);
+        AuthService authService = (AuthService) x.get("auth");
+
+        if ( ! authService.check(x, permission) ) {
+          throw new AuthorizationException();
+        }
+      `
+    },
+    {
+      name: 'authorizeOnCreate',
+      javaCode:  `
+        foam.nanos.auth.User user = (foam.nanos.auth.User) x.get("user");
+        if ( user != null && ( user.getId() == foam.nanos.auth.User.SYSTEM_USER_ID || user.getGroup().equals("admin") || user.getGroup().equals("system") ) ) return;
+        throw new AuthorizationException("Approval requests can only be created by the system");
+      `
+    },
+    {
+      name: 'authorizeOnDelete',
+      javaCode:  `
+        foam.nanos.auth.User user = (foam.nanos.auth.User) x.get("user");
+        if ( user != null && ( user.getId() == foam.nanos.auth.User.SYSTEM_USER_ID || user.getGroup().equals("admin") || user.getGroup().equals("system") ) ) return;
+        throw new AuthorizationException("Approval requests can only be created by the system");
+      `
+    }
+  ]
+})
+  
