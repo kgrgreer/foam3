c570b16ea805eebefc45d8d68754dac5bdff9ff7
Danny Tharma <daniel@nanopay.net>
2019-12-26 18:49:18 -0500

    Fixed stupid tests

--- nanopay/src/net/nanopay/contacts/ContactAuthorizationTest.java
+++ nanopay/src/net/nanopay/contacts/ContactAuthorizationTest.java
@@ -364,7 +364,7 @@ public class ContactAuthorizationTest
       // Try to find the removed contact.
       Contact findResult = (Contact) contactDAO.find_(adminUserContext_, putResult.getId());
 
-      test(findResult.getDeleted(), "Admin users can remove other users' contacts.");
+      test(findResult.getLifecycleState() == LifecycleState.DELETED, "Admin users can remove other users' contacts.");
     } catch (Throwable t) {
       System.out.println(t.getMessage());
       t.printStackTrace();
@@ -418,7 +418,7 @@ public class ContactAuthorizationTest
 
       // Make the admin select on the contactDAO.
       ArraySink sink = new ArraySink();
-      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.DELETED, false));
+      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.LIFECYCLE_STATE, LifecycleState.ACTIVE));
 
       test(
         sink.getArray().size() == 2,
@@ -485,7 +485,7 @@ public class ContactAuthorizationTest
 
       // Make the admin user select on the contactDAO.
       ArraySink sink = new ArraySink();
-      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.DELETED, false));
+      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.LIFECYCLE_STATE, LifecycleState.ACTIVE));
 
       test(
         sink.getArray().size() == 0,
@@ -520,7 +520,7 @@ public class ContactAuthorizationTest
 
       // Make the admin user select on the contactDAO.
       ArraySink sink = new ArraySink();
-      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.DELETED, false));
+      contactDAO.select_(adminUserContext_, sink, 0, Long.MAX_VALUE, null, EQ(Contact.LIFECYCLE_STATE, LifecycleState.ACTIVE));
 
       test(
         sink.getArray().size() == 1,
