cce385eec6d70286c01636463d2bb06593851892
noodlemoodle <qiyzhang@icloud.com>
2020-01-06 14:49:26 -0500

    removed capabilityrevoke request

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -886,7 +886,6 @@ FOAM_FILES([
    { name: 'net/nanopay/liquidity/crunch/LiquidTransactionAuthorizer' },
    { name: 'net/nanopay/liquidity/crunch/LiquidApprovalRequestAuthorizer' },
    { name: 'net/nanopay/liquidity/crunch/CapabilityAssignmentRequest' },
-   { name: 'net/nanopay/liquidity/crunch/CapabilityRevokingRequest' },
    { name: 'net/nanopay/liquidity/crunch/AddChildrenToCapabilityAccountTemplateOnUCJCreate' },
    { name: 'net/nanopay/liquidity/crunch/AddAccountToUserCapabilityJunctionOnCreate' },
 
