236c6ff44404e88f0611ad04717e3c8ae5ae23cd
noodlemoodle <qiyzhang@icloud.com>
2020-01-06 11:11:17 -0500

    remove foam headers everywhere

--- nanopay/src/net/nanopay/liquidity/crunch/AccountData.js
+++ nanopay/src/net/nanopay/liquidity/crunch/AccountData.js
@@ -1,8 +1,3 @@
-/**
- * @license
- * Copyright 2019 The FOAM Authors. All Rights Reserved.
- * http://www.apache.org/licenses/LICENSE-2.0
- */
 foam.CLASS({
   package: 'net.nanopay.liquidity.crunch',
   name: 'AccountData', 
