cd00aedb8d4799b36ead5834118d391bef389b37
Joel Hughes <joel@nanopay.net>
2020-01-12 23:21:57 -0500

    Make debuging a little less verbose, only print quote when planner entered.

--- nanopay/src/net/nanopay/fx/afex/AFEXTransactionPlanDAO.js
+++ nanopay/src/net/nanopay/fx/afex/AFEXTransactionPlanDAO.js
@@ -84,6 +84,7 @@ foam.CLASS({
       Account sourceAccount = request.findSourceAccount(x);
       Account destinationAccount = request.findDestinationAccount(x);
       Logger logger = (Logger) x.get("logger");
+      logger.debug(this.getClass().getSimpleName(), "generateTransaction", quote);
 
       // Validate that Payer is provisioned for AFEX before proceeding
       AFEXBusiness afexBusiness = afexService.getAFEXBusiness(x, sourceAccount.getOwner());
@@ -145,7 +146,6 @@ foam.CLASS({
 
     Logger logger = (Logger) x.get("logger");
     Transaction request = quote.getRequestTransaction();
-    logger.debug(this.getClass().getSimpleName(), "put", quote);
 
     Account sourceAccount = request.findSourceAccount(x);
     Account destinationAccount = request.findDestinationAccount(x);
