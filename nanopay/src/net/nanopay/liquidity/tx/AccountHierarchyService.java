6c9564b69cdf720107f86ac1089fd365f0988436
blakegreen <blake@nanopay.net>
2020-01-13 17:57:02 -0500

    finalMaps in getAccountsFromCapabilityAccountTemplate and getAccountsFromAccountTemplate in AccountHierarchyService are being initialized as ConcurrentHashMaps instead of HashMaps, refactored code in getChildMap to return the map if it is not null at the beginning of the method

--- nanopay/src/net/nanopay/liquidity/tx/AccountHierarchyService.java
+++ nanopay/src/net/nanopay/liquidity/tx/AccountHierarchyService.java
@@ -16,14 +16,12 @@ public class AccountHierarchyService
   implements AccountHierarchy
 {
   protected Map<String, HashSet<Long>> map_;
-  protected Map<String, CapabilityAccountData> capabilityAccountTemplateMap_;
-  protected Map<String, AccountData> accountTemplateMap_;
 
   protected Map<String, HashSet<Long>> getChildMap(X x) {
     DAO accountDAO = (DAO) x.get("localAccountDAO");
 
-    if ( map_ == null ) {
-      map_ = new ConcurrentHashMap<String, HashSet<Long>>();
+    if ( map_ != null ) {
+      return map_;
     }
 
     Sink purgeSink = new Sink() {
@@ -44,66 +42,8 @@ public class AccountHierarchyService
     };
 
     accountDAO.listen(purgeSink, TRUE);
-
+    map_ = new ConcurrentHashMap<String, HashSet<Long>>();
     return map_;
-  }
-
-  protected Map<String, CapabilityAccountData> getCapabilityAccountTemplateMap(X x) {
-    DAO accountDAO = (DAO) x.get("localAccountDAO");
-
-    if ( capabilityAccountTemplateMap_ == null ) {
-      capabilityAccountTemplateMap_ = new ConcurrentHashMap<String, CapabilityAccountData>();
-    }
-
-    Sink purgeSink = new Sink() {
-      public void put(Object obj, Detachable sub) {
-        capabilityAccountTemplateMap_.clear();
-        sub.detach();
-      }
-      public void remove(Object obj, Detachable sub) {
-        capabilityAccountTemplateMap_.clear();
-        sub.detach();
-      }
-      public void eof() {
-      }
-      public void reset(Detachable sub) {
-        capabilityAccountTemplateMap_.clear();
-        sub.detach();
-      }
-    };
-
-    accountDAO.listen(purgeSink, TRUE);
-
-    return capabilityAccountTemplateMap_;
-  }
-
-  protected Map<String, AccountData> getAccountTemplateMap(X x) {
-    DAO accountDAO = (DAO) x.get("localAccountDAO");
-
-    if ( accountTemplateMap_ == null ) {
-      accountTemplateMap_ = new ConcurrentHashMap<String, AccountData>();
-    }
-
-    Sink purgeSink = new Sink() {
-      public void put(Object obj, Detachable sub) {
-        accountTemplateMap_.clear();
-        sub.detach();
-      }
-      public void remove(Object obj, Detachable sub) {
-        accountTemplateMap_.clear();
-        sub.detach();
-      }
-      public void eof() {
-      }
-      public void reset(Detachable sub) {
-        accountTemplateMap_.clear();
-        sub.detach();
-      }
-    };
-
-    accountDAO.listen(purgeSink, TRUE);
-
-    return accountTemplateMap_;
   }
 
   @Override
@@ -115,8 +55,10 @@ public class AccountHierarchyService
     // Check if parentId exists in map, if it doesn't fetch children and add them to map
     if ( ! map.containsKey(parentIdString) ) {
       Account parentAccount = (Account) accountDAO.find(parentId);
-      List<Account> children = getChildAccounts(x, parentAccount);
-      List<Long> childIdList = new ArrayList<>();
+      List<Account> children = new ArrayList<Account>();
+      List<Long> childIdList = new ArrayList<Long>();
+
+      children = getChildAccounts(x, parentAccount);
 
       if ( children.size() > 0 ) {
         for ( int i = 0; i < children.size(); i++ ) {
@@ -136,13 +78,13 @@ public class AccountHierarchyService
   public List<Account> getChildAccounts(X x, Account account) {
      ArraySink allChildrenSink = (ArraySink) account.getChildren(x).select(new ArraySink());
      List<Account> allChildrenList = allChildrenSink.getArray();
-
-    List<Account> allAccounts = new ArrayList<>();
+    
+    List<Account> allAccounts = new ArrayList<Account>();
     allAccounts.add(account);
 
     if ( allChildrenList.size() > 0 ) {
       for ( int i = 0; i < allChildrenList.size(); i++ ) {
-        Account acc = allChildrenList.get(i);
+        Account acc = (Account) allChildrenList.get(i);
         List<Account> childChildren = getChildAccounts(x, acc);
         allAccounts.addAll(childChildren);
       }
@@ -152,40 +94,39 @@ public class AccountHierarchyService
   }
 
   @Override
-  public AccountApproverMap getAccountsFromCapabilityAccountTemplate(X x, CapabilityAccountTemplate template) {
+  public AccountApproverMap getAccountsFromCapabilityAccountTemplate(X x, CapabilityAccountTemplate template){
+    // TODO: Wire up caching
     Map<String, CapabilityAccountData> templateMap = template.getAccounts();
     Set<String> accountIds = templateMap.keySet();
 
-    Map<String, CapabilityAccountData> finalMap = getCapabilityAccountTemplateMap(x);
+    Map<String, CapabilityAccountData> finalMap = new ConcurrentHashMap<>();
 
     for ( String accountId : accountIds ) {
-      if ( ! finalMap.containsKey(accountId) ) {
-        finalMap.put(accountId, templateMap.get(accountId));
-        addChildrenToCapabilityAccountTemplate(x, accountId, templateMap.get(accountId), finalMap);
-      }
+      finalMap.put(accountId, templateMap.get(accountId));
+      addChildrenToCapabilityAccountTemplate(x, accountId, templateMap.get(accountId), finalMap);
     }
 
     return new AccountApproverMap.Builder(x).setAccounts(finalMap).build();
   }
 
   @Override
-  public AccountMap getAccountsFromAccountTemplate(X x, AccountTemplate template) {
+  public AccountMap getAccountsFromAccountTemplate(X x, AccountTemplate template){
+    // TODO: Wire up caching
     Map<String, AccountData> templateMap = template.getAccounts();
     Set<String> accountIds = templateMap.keySet();
 
-    Map<String, AccountData> finalMap = getAccountTemplateMap(x);
+    Map<String, AccountData> finalMap = new ConcurrentHashMap<>();
 
     for ( String accountId : accountIds ) {
-      if ( ! finalMap.containsKey(accountId) ) {
-        finalMap.put(accountId, templateMap.get(accountId));
-        addChildrenToAccountTemplate(x, accountId, templateMap.get(accountId), finalMap);
-      }
+      finalMap.put(accountId, templateMap.get(accountId));
+      addChildrenToAccountTemplate(x, accountId, templateMap.get(accountId), finalMap);
     }
 
     return new AccountMap.Builder(x).setAccounts(finalMap).build();
+
   }
 
-  private void addChildrenToCapabilityAccountTemplate(X x, String accountId, CapabilityAccountData data, Map<String, CapabilityAccountData> accountMap) {
+  private void addChildrenToCapabilityAccountTemplate(X x, String accountId, CapabilityAccountData data, Map<String, CapabilityAccountData> accountMap){
     DAO accountDAO = (DAO) x.get("accountDAO");
     Account tempAccount = (Account) accountDAO.find(Long.parseLong(accountId));
     List<Account> children = ((ArraySink) ( tempAccount.getChildren(x)).select(new ArraySink())).getArray();
@@ -208,7 +149,7 @@ public class AccountHierarchyService
     }
   }
 
-  private void addChildrenToAccountTemplate(X x, String accountId, AccountData data, Map<String, AccountData> accountMap) {
+  private void addChildrenToAccountTemplate(X x, String accountId, AccountData data, Map<String, AccountData> accountMap){
     DAO accountDAO = (DAO) x.get("accountDAO");
     Account tempAccount = (Account) accountDAO.find(Long.parseLong(accountId));
     List<Account> children = ((ArraySink) ( tempAccount.getChildren(x)).select(new ArraySink())).getArray();
