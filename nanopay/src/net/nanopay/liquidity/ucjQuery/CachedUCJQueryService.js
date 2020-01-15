913b935113a9873531c661f7bf927b711b64eb28
noodlemoodle <qiyzhang@icloud.com>
2020-01-15 18:15:37 -0500

    refacor context in queryservices to be first property

--- nanopay/src/net/nanopay/liquidity/ucjQuery/CachedUCJQueryService.js
+++ nanopay/src/net/nanopay/liquidity/ucjQuery/CachedUCJQueryService.js
@@ -52,12 +52,12 @@ foam.CLASS({
       javaThrows: ['java.lang.RuntimeException'],
       args: [
         {
-          name: 'userId',
-          type: 'Long'
-        },
-        {
           name: 'x',
           type: 'Context'
+        },
+        {
+          name: 'userId',
+          type: 'Long'
         }
       ],
       javaCode: `
@@ -113,12 +113,12 @@ foam.CLASS({
       javaThrows: ['java.lang.RuntimeException'],
       args: [
         {
-          name: 'roleId',
-          type: 'String'
-        },
-        {
           name: 'x',
           type: 'Context'
+        },
+        {
+          name: 'roleId',
+          type: 'String'
         }
       ],
       javaCode: `
@@ -179,16 +179,16 @@ foam.CLASS({
       javaThrows: ['java.lang.RuntimeException'],
       args: [
         {
+          name: 'x',
+          type: 'Context'
+        },
+        {
           name: 'modelToApprove',
           type: 'String'
         },
         {
           name: 'level',
           type: 'Integer'
-        },
-        {
-          name: 'x',
-          type: 'Context'
         }
       ],
       javaCode: `  
