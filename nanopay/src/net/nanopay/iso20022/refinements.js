f74060148f99ae47309a41b631bf836c43e8c367
Olha Bahatiuk <olha@nanopay.net>
2020-01-20 13:25:53 -0500

    String comparison replaced with enum comparison

--- nanopay/src/net/nanopay/iso20022/refinements.js
+++ nanopay/src/net/nanopay/iso20022/refinements.js
@@ -747,15 +747,15 @@ foam.CLASS({
                     //Transaction txn = (Transaction) txnDAO.find((this.getFIToFIPmtStsReq().getOriginalGroupInformation())[i].getOriginalMessageIdentification());
                     Transaction txn = (Transaction) txnDAO.find(EQ(Transaction.MESSAGE_ID, (this.getFIToFIPmtStsReq().getOriginalGroupInformation())[i].getOriginalMessageIdentification()));
 
-                    String cur_txnStatus = null;
+                    TransactionStatus cur_txnStatus = null;
                     String txnStatus  = null;
 
                     if ( txn != null ) {
-                      cur_txnStatus = ((TransactionStatus)txn.getStatus()).getName();
+                      cur_txnStatus = (TransactionStatus)txn.getStatus();
                       System.out.println("txn.getStatus() : " + txn.getStatus());
                       System.out.println("txn.getStatus().getName() : " + txn.getStatus().getName());
 
-                      if ( cur_txnStatus.equals(TransactionStatus.COMPLETED.getName()) ) {
+                      if ( cur_txnStatus.equals(TransactionStatus.COMPLETED) ) {
                         txnStatus = "ACSP"; //TransactionStatus.ACSP;
                       } else {
                         txnStatus = "ACSC"; //TransactionStatus.ACSC;
