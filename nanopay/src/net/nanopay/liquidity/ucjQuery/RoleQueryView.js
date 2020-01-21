c20128070a6608584b8001ccc528fa90fb4b68ea
Scott Head <scott@nanopay.net>
2020-01-21 11:58:46 -0500

    Updating Role Query titles

--- nanopay/src/net/nanopay/liquidity/ucjQuery/RoleQueryView.js
+++ nanopay/src/net/nanopay/liquidity/ucjQuery/RoleQueryView.js
@@ -365,9 +365,9 @@ foam.CLASS({
       view: {
         class: 'foam.u2.view.TabChoiceView',
         choices: [
-          ['globalRoles', 'Global Roles'],
+          ['globalRoles', 'Admin Roles'],
           // ['selectTwo', 'Account Roles (Specific)'],
-          ['selectOne', 'Account Roles'],
+          ['selectOne', 'Transactional Roles'],
         ],
       },
     },
