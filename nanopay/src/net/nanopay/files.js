c8b14e7348aaf70f8266ca3c5686f0f6ead176ab
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2020-01-24 08:52:38 -0500

    add new account to ucj data of maker where cascading is false (#9044)
    * add rule to add child account to ucj if its maker when the maker doesnt have cascading on the parent (otherwise it gets added by another rule)
    
    * make sure to only add to ucj where cap grants accountmaker
    
    * fix smth

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -925,6 +925,7 @@ FOAM_FILES([
    { name: 'net/nanopay/liquidity/crunch/RemoveJunctionsOnCapabilityRemoval' },
    { name: 'net/nanopay/liquidity/crunch/RemoveAccountBasedUCJIfAccountsEmpty' },
    { name: 'net/nanopay/liquidity/crunch/RemoveAccountFromUcjDataOnAccountRemoval' },
+   { name: 'net/nanopay/liquidity/crunch/AddChildAccountToMakerOnAccountApproval' },
    { name: 'net/nanopay/liquidity/crunch/CapabilityAccountTemplateMapView', flags: ['js', 'web'] },
    { name: 'net/nanopay/liquidity/crunch/RootAccounts' },
 
