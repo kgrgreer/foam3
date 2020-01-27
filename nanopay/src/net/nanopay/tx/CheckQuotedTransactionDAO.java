83be0557605194c3316692bdeacf94a9c76051d6
Scott Head <43998323+nanoscott@users.noreply.github.com>
2020-01-27 12:06:42 -0500

    Adding ability to check balance during quoting (#9064)
    * Adding ability to check balance during quoting
    
    * CPF-4341 #comment Maintaining original behaviour with chained constructor

--- nanopay/src/net/nanopay/tx/CheckQuotedTransactionDAO.java
+++ nanopay/src/net/nanopay/tx/CheckQuotedTransactionDAO.java
@@ -4,14 +4,26 @@ import foam.core.FObject;
 import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
+import foam.nanos.logger.Logger;
+import net.nanopay.account.Account;
+import net.nanopay.account.Balance;
 import net.nanopay.tx.model.Transaction;
 import net.nanopay.tx.model.TransactionStatus;
 
 public class CheckQuotedTransactionDAO extends ProxyDAO {
 
-  public CheckQuotedTransactionDAO(X x,DAO delegate) {
+  private boolean enableQuoteTransferValidation;
+
+  public CheckQuotedTransactionDAO(X x, DAO delegate) {
+    this(x, delegate, false);
+  }
+
+  public CheckQuotedTransactionDAO(X x, DAO delegate, boolean enableQuoteTransferValidation) {
     setDelegate(delegate);
     setX(x);
+
+    // Set validation flag
+    this.enableQuoteTransferValidation = enableQuoteTransferValidation;
   }
 
   @Override
@@ -21,8 +33,30 @@ public class CheckQuotedTransactionDAO extends ProxyDAO {
       TransactionQuote quote = new TransactionQuote();
       quote.setRequestTransaction(txn);
       quote = (TransactionQuote) ((DAO) x.get("localTransactionQuotePlanDAO")).inX(x).put(quote);
+      validateQuoteTransfers(x, quote);
       return getDelegate().put_(x, quote.getPlan());
     }
     return getDelegate().put_(x, obj);
+  }
+
+  private void validateQuoteTransfers(X x, TransactionQuote quote) {
+    if ( ! enableQuoteTransferValidation ) 
+      return;
+      
+    DAO balanceDAO = (DAO) x.get("balanceDAO");
+    Logger logger = (Logger) x.get("logger");
+
+    Transaction transaction = quote.getPlan();
+    Transfer[] transfers = transaction.getTransfers();
+
+    for ( Transfer transfer : transfers ) {
+      transfer.validate();
+      Account account = transfer.findAccount(getX());
+      if ( account == null ) {
+        logger.error(this.getClass().getSimpleName(), "validateQuoteTransfers", "transfer account not found: " + transfer.getAccount(), transfer);
+        throw new RuntimeException("Unknown account: " + transfer.getAccount());
+      }
+      account.validateAmount(x, (Balance) balanceDAO.find(account.getId()), transfer.getAmount());
+    }
   }
 }
