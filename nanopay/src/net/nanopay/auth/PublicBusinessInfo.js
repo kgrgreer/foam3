3becf4ada405520f754ac55507e1a53cac1f0ede
yij793 <jiangarfield@gmail.com>
2020-01-28 11:03:58 -0500

    add sector prop in PublicBusineesInfo

--- nanopay/src/net/nanopay/auth/PublicBusinessInfo.js
+++ nanopay/src/net/nanopay/auth/PublicBusinessInfo.js
@@ -5,7 +5,10 @@ foam.CLASS({
   documentation: "Represents a public subset of a business's properties.",
 
   javaImports: [
-    'net.nanopay.model.Business'
+    'net.nanopay.model.Business',
+    'net.nanopay.model.BusinessSector',
+    'foam.core.X',
+    'foam.dao.DAO'
   ],
 
   properties: [
@@ -13,21 +16,25 @@ foam.CLASS({
     net.nanopay.model.Business.BUSINESS_NAME,
     net.nanopay.model.Business.ORGANIZATION,
     net.nanopay.model.Business.ADDRESS,
-    net.nanopay.model.Business.EMAIL
+    net.nanopay.model.Business.EMAIL,
+    net.nanopay.model.Business.SECTOR
   ].map((p) => p.clone().copyFrom({ visibility: foam.u2.Visibility.RO })),
 
   axioms: [
     {
       buildJavaClass: function(cls) {
         cls.extras.push(`
-          public PublicBusinessInfo(Business business) {
+          public PublicBusinessInfo(X x, Business business) {
             if ( business == null ) {
               throw new RuntimeException("PublicBusinessInfo was given a null argument.");
             };
+            DAO businessSectorDAO = (DAO) x.get("businessSectorDAO");
+            BusinessSector businessSector = (BusinessSector) businessSectorDAO.find(business.getBusinessSectorId());
             setId(business.getId());
             setOrganization(business.getOrganization());
             setBusinessName(business.getBusinessName());
             setAddress(business.getAddress());
+            setSector(businessSector);
             // Emails are not to be public.  CPF-1523
             // setEmail(business.getEmail());
           }
