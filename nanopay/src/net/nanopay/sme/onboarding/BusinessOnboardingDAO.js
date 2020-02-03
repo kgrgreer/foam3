54e1fccab704dbc9ee0785eef8e54b0ac837975f
Minsun <minsun@nanopay.net>
2020-02-03 16:35:21 -0500

    test commit

--- nanopay/src/net/nanopay/sme/onboarding/BusinessOnboardingDAO.js
+++ nanopay/src/net/nanopay/sme/onboarding/BusinessOnboardingDAO.js
@@ -104,6 +104,7 @@ foam.CLASS({
 
         // if the businessOnboarding is already set to SUBMITTED, do not allow modification
         if ( old != null && old.getStatus() == net.nanopay.sme.onboarding.OnboardingStatus.SUBMITTED ) return getDelegate().put_(x, businessOnboarding);
+        //if ( old != null && old.getStatus() == net.nanopay.sme.onboarding.OnboardingStatus.SUBMITTED ) return getDelegate().put_(x, businessOnboarding);
 
         Long oldDualPartyAgreement = old == null ? 0 : old.getDualPartyAgreement();
         if ( oldDualPartyAgreement != businessOnboarding.getDualPartyAgreement() ) {
