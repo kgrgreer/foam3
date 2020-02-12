5431d4ef59c1d72fac633627263ba2e9c6a15618
Patrick Zanowski <patrick@nanopay.net>
2020-02-12 14:38:10 -0500

    Account for business email removal on tests and scripts

--- nanopay/src/net/nanopay/sme/ruler/OnboardingPaymentOperationNotification.js
+++ nanopay/src/net/nanopay/sme/ruler/OnboardingPaymentOperationNotification.js
@@ -12,12 +12,13 @@ foam.CLASS({
       'foam.core.X',
       'foam.dao.DAO',
       'foam.nanos.auth.User',
+      'foam.nanos.auth.UserUserJunction',
       'foam.nanos.notification.Notification',
       'foam.nanos.notification.email.EmailMessage',
-      'net.nanopay.model.Business',
-      'net.nanopay.sme.OnboardingPaymentOpsNotification',
       'java.util.HashMap',
       'java.util.Map',
+      'net.nanopay.model.Business',
+      'net.nanopay.sme.OnboardingPaymentOpsNotification',
       'static foam.mlang.MLang.*'
     ],
 
@@ -32,14 +33,15 @@ foam.CLASS({
 
               if (user == null || user.getId() <= 0) return;
               DAO businessDAO = (DAO) x.get("businessDAO");
-              Business bs = (Business) businessDAO.find(user.getId());
-              if (user == null && user.getId() <= 0) return;
+              DAO agentJunctionDAO = (DAO) x.get("agentJunctionDAO");
+              UserUserJunction junction = (UserUserJunction) agentJunctionDAO.find(EQ(UserUserJunction.SOURCE_ID, user.getId()));
+              Business business = (Business) businessDAO.find(junction.getTargetId());
               EmailMessage message = new EmailMessage();
               Map<String, Object>  args = new HashMap<>();
               args.put("userId", user.getId());
               args.put("userEmail", user.getEmail());
-              args.put("businessId", bs.getId());
-              args.put("businessName", bs.getBusinessName());
+              args.put("businessId", business.getId());
+              args.put("businessName", business.getBusinessName());
 
               OnboardingPaymentOpsNotification notification = new OnboardingPaymentOpsNotification.Builder(x)
               .setEmailArgs(args)
