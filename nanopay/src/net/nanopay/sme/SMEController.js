6eb7df7a173e871a32d9f861dd947ba84c45c003
Patrick Zanowski <patrick@nanopay.net>
2020-02-18 13:02:00 -0500

    User label on switch business fix

--- nanopay/src/net/nanopay/sme/SMEController.js
+++ nanopay/src/net/nanopay/sme/SMEController.js
@@ -187,7 +187,7 @@ foam.CLASS({
     },
     {
       name: 'ADDED_TO_BUSINESS_1',
-      message: "You've been successfully added to "
+      message: `You've been successfully added to `
     },
     {
       name: 'ADDED_TO_BUSINESS_2',
