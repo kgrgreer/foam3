54c38534f581dea26ddeee2d87a6709d9569e51c
Patrick Zanowski <patrick@nanopay.net>
2020-02-18 12:01:00 -0500

    revert business onboarding lowercase

--- nanopay/src/net/nanopay/sme/onboarding/USBusinessOnboarding.js
+++ nanopay/src/net/nanopay/sme/onboarding/USBusinessOnboarding.js
@@ -1432,7 +1432,7 @@ foam.CLASS({
       javaThrows: ['IllegalStateException'],
       javaCode: `
         AuthService auth = (AuthService) x.get("auth");
-        DAO usBusinessOnboardingDAO = (DAO) x.get("usBusinessOnboardingDAO");
+        DAO usBusinessOnboardingDAO = (DAO) x.get("uSBusinessOnboardingDAO");
 
         USBusinessOnboarding obj = (USBusinessOnboarding) this;
         USBusinessOnboarding oldObj = (USBusinessOnboarding) usBusinessOnboardingDAO.find(this.getId());
