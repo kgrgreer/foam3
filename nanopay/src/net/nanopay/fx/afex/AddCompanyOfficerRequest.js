dd4fd7f960891c09b011fa30255543f28bf01641
Mayowa Olurin <mayowa@nanopay.net>
2020-03-03 17:19:17 +0100

    fix failing test

---
+++ nanopay/src/net/nanopay/fx/afex/AddCompanyOfficerRequest.js
@@ -0,0 +1,46 @@
+foam.CLASS({
+  package: "net.nanopay.fx.afex",
+  name: "AddCompanyOfficerRequest",
+  properties: [
+    {
+      class: 'String',
+      name: "ApiKey"
+    },
+    {
+      class: 'String',
+      name: "Name"
+    },
+    {
+      class: 'String',
+      name: "PercentOwnership"
+    },
+    {
+      class: "String",
+      name: "Director"
+    },
+    {
+      class: "String",
+      name: "DateOfBirth"
+    },
+    {
+      class: "String",
+      name: "Address1"
+    },
+    {
+      class: "String",
+      name: "City"
+    },
+    {
+      class: "String",
+      name: "CountryCode"
+    },
+    {
+      class: "String",
+      name: "StateRegion"
+    },
+    {
+      class: "String",
+      name: "Zip"
+    }
+  ]
+});
