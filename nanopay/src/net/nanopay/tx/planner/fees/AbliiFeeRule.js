5891d269e9cde01ac3d10a7e3421e5d4d36f2cf4
Neelkanth Patel <neel@nanopay.net>
2020-03-05 10:24:39 -0500

    add ablii fee rule

---
+++ nanopay/src/net/nanopay/tx/planner/fees/AbliiFeeRule.js
@@ -0,0 +1,42 @@
+foam.CLASS({
+  package: 'net.nanopay.tx.planner.fees',
+  name: 'AbliiFeeRule',
+
+  documentation: `
+      Rule that add fees for Ablii transactions
+  `,
+
+  implements: ['foam.nanos.ruler.RuleAction'],
+
+  javaImports: [
+    'net.nanopay.tx.InvoicedFeeLineItem',
+    'net.nanopay.tx.model.Transaction',
+    'net.nanopay.tx.TransactionLineItem'
+  ],
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'domesticFee',
+      value: 75
+    },
+    {
+      class: 'Long',
+      name: 'internationalFee',
+      value: 500
+    }
+
+  ],
+
+  methods: [
+    {
+      name: 'applyAction',
+      javaCode: `
+        Transaction tx = (Transaction) obj;
+        Boolean sameCurrency = tx.findSourceAccount(x).getDenomination().equals(tx.findDestinationAccount(x).getDenomination());
+        Long feeAmount = sameCurrency ? getDomesticFee() : getInternationalFee();
+        tx.addLineItems(new TransactionLineItem[] {new InvoicedFeeLineItem.Builder(getX()).setGroup("InvoiceFee").setAmount(feeAmount).setCurrency(tx.getSourceCurrency()).build()}, null);
+      `
+    }
+  ]
+});
