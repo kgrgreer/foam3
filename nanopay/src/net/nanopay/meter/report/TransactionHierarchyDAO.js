ceab8abbf473dfbe6813824282805141de4247d0
Henry Han <henry.han@usask.ca>
2020-03-18 12:56:34 -0400

    Update transactionDetail

--- nanopay/src/net/nanopay/meter/report/TransactionHierarchyDAO.js
+++ nanopay/src/net/nanopay/meter/report/TransactionHierarchyDAO.js
@@ -26,14 +26,12 @@ foam.CLASS({
     {
       name: 'select_',
       javaCode: `
-        TransactionReport transactionReport = new TransactionReport();
-
         DAO transactionDAO = (DAO) x.get("localTransactionDAO");
 
         String id = (String) ((Constant)((Eq)((And)((Or) predicate).getArgs()[0]).getArgs()[0]).getArg2()).getValue();
         
         // This is for testing purpose only, it will be removed before merge
-        // String id = "32d56fa9-5fff-ed6b-e6b8-8d083897b305";
+        // String id = "448ff913-ca39-e78c-daec-9f6fb971e74c";
         Transaction transaction = (Transaction) transactionDAO.find(id);
 
         TransactionReport transactionDetail = new TransactionReport();
@@ -47,6 +45,7 @@ foam.CLASS({
         transactionDetail.setStatus(transaction.getStatus());
 
         ArraySink txnSink = new ArraySink();
+
         transactionDAO.where(AND(
             EQ(Transaction.PARENT, id),
             OR(INSTANCE_OF(DigitalTransaction.class), INSTANCE_OF(CITransaction.class), INSTANCE_OF(COTransaction.class))
@@ -54,10 +53,10 @@ foam.CLASS({
 
         List<Transaction> txnList = txnSink.getArray();
 
-        transactionReport.setChildTransaction(txnList.toArray(new Transaction[txnList.size()]));
+        transactionDetail.setChildTransaction(txnList.toArray(new Transaction[txnList.size()]));
 
         Sink decoratedSink = decorateSink(x, sink, skip, limit, order, null);
-        decoratedSink.put(transactionReport, null);
+        decoratedSink.put(transactionDetail, null);
 
         return decoratedSink;
       `
