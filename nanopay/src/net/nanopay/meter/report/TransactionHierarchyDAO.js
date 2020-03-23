3a2ea41fa32f99038884ec296a5b9a7ed71589bd
Henry Han <henry.han@usask.ca>
2020-03-23 12:12:17 -0400

    Iterate throw the transaction chain

--- nanopay/src/net/nanopay/meter/report/TransactionHierarchyDAO.js
+++ nanopay/src/net/nanopay/meter/report/TransactionHierarchyDAO.js
@@ -11,15 +11,16 @@ foam.CLASS({
     'foam.dao.ArraySink',
     'foam.dao.DAO',
     'foam.dao.ProxyDAO',
+    'foam.dao.Sink',
+    'static foam.mlang.MLang.*',
+    'foam.mlang.Constant',
+    'foam.mlang.predicate.*',
     'net.nanopay.tx.DigitalTransaction',
     'net.nanopay.tx.cico.CITransaction',
     'net.nanopay.tx.cico.COTransaction',
     'net.nanopay.tx.model.Transaction',
-    'static foam.mlang.MLang.*',
-    'foam.mlang.Constant',
-    'foam.mlang.predicate.*',
     'java.util.List',
-    'foam.dao.Sink'
+    'java.util.ArrayList'
   ],
 
   methods: [
@@ -46,14 +47,31 @@ foam.CLASS({
 
         ArraySink txnSink = new ArraySink();
 
-        transactionDAO.where(AND(
-            EQ(Transaction.PARENT, id),
-            OR(INSTANCE_OF(DigitalTransaction.class), INSTANCE_OF(CITransaction.class), INSTANCE_OF(COTransaction.class))
-        )).select(txnSink);
+        transactionDAO.where(
+          EQ(Transaction.PARENT, id)
+        ).select(txnSink);
 
         List<Transaction> txnList = txnSink.getArray();
+        ArraySink arraySink_txnList = new ArraySink();
+        txnList.get(0).getChildren(x).select(arraySink_txnList);
 
-        transactionDetail.setChildTransaction(txnList.toArray(new Transaction[txnList.size()]));
+        List<Transaction> childTxnList = new ArrayList<>();
+
+        for (int i = 0; i < arraySink_txnList.getArray().size(); i++) {
+          Transaction childTxn = (Transaction) arraySink_txnList.getArray().get(i);
+          ArraySink arraySink_childTxn = new ArraySink();
+
+          transactionDAO.where(AND(
+            EQ(Transaction.PARENT, childTxn.getId()),
+            OR(INSTANCE_OF(DigitalTransaction.class), INSTANCE_OF(CITransaction.class), INSTANCE_OF(COTransaction.class))
+          )).select(arraySink_childTxn);
+
+          for (int j = 0; j< arraySink_childTxn.getArray().size(); j++) {
+            childTxnList.add((Transaction) arraySink_childTxn.getArray().get(j));
+          }
+        }
+
+        transactionDetail.setChildTransaction(childTxnList.toArray(new Transaction[childTxnList.size()]));
 
         Sink decoratedSink = decorateSink(x, sink, skip, limit, order, null);
         decoratedSink.put(transactionDetail, null);
