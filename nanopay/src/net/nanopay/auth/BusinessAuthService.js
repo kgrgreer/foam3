e64549e00833609a3f42ad752f692792fb9c2287
Patrick Zanowski <patrick@nanopay.net>
2020-03-27 14:48:08 -0400

    revert

--- nanopay/src/net/nanopay/auth/BusinessAuthService.js
+++ nanopay/src/net/nanopay/auth/BusinessAuthService.js
@@ -16,7 +16,9 @@ foam.CLASS({
   javaImports: [
     'foam.dao.DAO',
     'foam.nanos.auth.User',
+    'foam.nanos.auth.Group',
     'foam.nanos.auth.AuthenticationException',
+    'foam.nanos.session.Session',
     'foam.nanos.NanoService',
     'net.nanopay.admin.model.AccountStatus'
   ],
@@ -32,16 +34,42 @@ foam.CLASS({
     {
       name: 'getCurrentUser',
       javaCode: `
-        User user = (User) x.get("user");
+        // TODO: PRE 3.7.2 Remove all logic except for account status check change return to call delegate getCurrentUser.
+        Session session = x.get(Session.class);
+
+        // fetch context and check if not null or user id is 0
+        if ( session == null || session.getUserId() == 0 ) {
+          throw new AuthenticationException("Not logged in");
+        }
+
+        // get user from session id
+        User user = (User) ((DAO) getLocalUserDAO()).find(session.getUserId());
         if ( user == null ) {
-          throw new AuthenticationException("User not found");
+          throw new AuthenticationException("User not found: " + session.getUserId());
+        }
+
+        // check if user enabled
+        if ( ! user.getEnabled() ) {
+          throw new AuthenticationException("User disabled");
+        }
+
+        // check if group enabled
+        Group group = (Group) x.get("group");
+        if ( group != null && ! group.getEnabled() ) {
+          throw new AuthenticationException("User group disabled");
+        }
+
+        // check for two-factor authentication
+        if ( user.getTwoFactorEnabled() && ! session.getContext().getBoolean("twoFactorSuccess") ) {
+          throw new AuthenticationException("User requires two-factor authentication");
         }
 
         // check user status is not disabled
         if ( AccountStatus.DISABLED == user.getStatus() ) {
           throw new AuthenticationException("User disabled");
         }
-        return getDelegate().getCurrentUser(x);
+
+        return user;
     `
     }
   ]
