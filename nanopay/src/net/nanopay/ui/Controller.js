66f7f9053a68ab165af43f64512d20095249392e
Scott Head <43998323+nanoscott@users.noreply.github.com>
2020-04-08 13:52:47 -0400

    NP-562 User Updates (#9648)
    * NP-562 Moving the authorizer to liquid specific services file
    
    * Attempting to override all liquid specific views
    
    * Pointing out the unexpected code
    
    * NP-562 #comment Removing custom create view for liquid users
    
    * Fixing user view
    
    * Fixing liquid rule for user create
    
    * Changing priority of the rules
    
    * Formatting
    
    * Formatting
    
    * Adding test to make sure the user gets setup properly
    
    * Isolating group permission

--- nanopay/src/net/nanopay/ui/Controller.js
+++ nanopay/src/net/nanopay/ui/Controller.js
@@ -224,6 +224,7 @@ foam.CLASS({
             // Replace the detail view in non-liquid deployments to give more control over the users
             if (this.appConfig.name != 'liquid') {
               this.__subContext__.register(net.nanopay.meter.UserDetailView, 'net.nanopay.liquidity.ui.user.LiquidUserDetailView');
+              //this.__subContext__.register(foam.u2.detail.SectionedDetailView, 'net.nanopay.liquidity.ui.user.LiquidUserDetailView');
               this.__subContext__.register(foam.u2.detail.SectionedDetailView, "net.nanopay.liquidity.ui.account.AccountDetailView");
               this.__subContext__.register(foam.u2.detail.SectionedDetailView, "net.nanopay.account.AccountDAOSummaryViewView");
               this.__subContext__.register(foam.comics.v2.DAOSummaryView, "net.nanopay.account.AccountDAOSummaryView");
