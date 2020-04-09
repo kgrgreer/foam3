98cbd74526b83ad5c904546bb24bf74d10e676db
Joel Hughes <joel@nanopay.net>
2020-04-08 20:55:49 -0400

    Revert to dev branch changes - TrustAccount.find will be going away soon anyway

--- nanopay/src/net/nanopay/account/TrustAccount.js
+++ nanopay/src/net/nanopay/account/TrustAccount.js
@@ -4,6 +4,7 @@ foam.CLASS({
   extends: 'net.nanopay.account.ZeroAccount',
 
   javaImports: [
+    'net.nanopay.bank.BankAccount',
     'foam.core.Currency',
     'foam.core.X',
     'foam.dao.ArraySink',
@@ -13,23 +14,13 @@ foam.CLASS({
     'foam.mlang.sink.Count',
     'static foam.mlang.MLang.AND',
     'static foam.mlang.MLang.EQ',
-    'static foam.mlang.MLang.GT',
-    'static foam.mlang.MLang.IN',
     'static foam.mlang.MLang.INSTANCE_OF',
-    'static foam.mlang.MLang.OR',
-    'foam.mlang.predicate.Predicate',
     'foam.nanos.auth.ServiceProvider',
     'foam.nanos.auth.User',
     'foam.nanos.logger.Logger',
     'foam.util.SafetyUtil',
-    'net.nanopay.account.Account',
-    'net.nanopay.account.TrustAccount',
-    'net.nanopay.bank.BankAccount',
-    'net.nanopay.model.Branch',
-    'net.nanopay.payment.Institution',
-    'java.util.Arrays',
     'java.util.List',
-    'java.util.stream.Collectors'
+    'net.nanopay.payment.Institution'
   ],
 
   properties: [
@@ -71,8 +62,8 @@ foam.CLASS({
             Count count = new Count();
             count = (Count) accounts.select(count);
             if ( count.getValue() == 0 ) {
-              logger.error("TrustAccounts not found for ", user.getId());
-              throw new RuntimeException("TrustAccounts not found for "+user.getId()+ " currency: "+ currency);
+              logger.error("No TrustAccounts found for ", user.getId());
+              throw new RuntimeException("No TrustAccounts found for "+user.getId()+ " currency: "+ currency);
             }
             return accounts;
           }
@@ -84,70 +75,44 @@ foam.CLASS({
 
           static public TrustAccount find(X x, Account account, String institutionNumber) {
             Logger logger   = (Logger) x.get("logger");
-
             logger.info("TrustAccount.find", "account", account, "institution", institutionNumber);
             if ( SafetyUtil.isEmpty(institutionNumber) ) {
-              return find(x, account);
+              return find(x,account);
             }
 
-            List<Institution> institutions = ((ArraySink) ((DAO) x.get("institutionDAO"))
-              .where(EQ(Institution.INSTITUTION_NUMBER,institutionNumber))
-              .select(new ArraySink())).getArray();
-            Institution institution = null;
-            if ( institutions.size() > 0 ) {
-              institution = institutions.get(0);
-              if ( institutions.size() > 1 ) {
-                logger.warning("Multiple Institutions founnd for institution number", institutionNumber);
+            DAO accounts = find(x, account.findOwner(x), account.getDenomination());
+            List accountList = ((ArraySink)accounts.select(new ArraySink())).getArray();
+            DAO reserveAccs = new MDAO(Account.getOwnClassInfo());
+
+            for ( int i =0 ; i<accountList.size(); i++ ) {
+              Account reserveAccount = ((TrustAccount) accountList.get(i)).findReserveAccount(x);
+              if ( reserveAccount != null ) {
+                reserveAccs.put( reserveAccount );
+              } else {
+                logger.warning("ReserveAccount not found for account", accountList.get(i));
               }
-            } else {
-              logger.error("Institution not found", institutionNumber);
-              throw new RuntimeException("Institution not found: "+institutionNumber);
             }
 
-            List<Branch> branches = ((ArraySink) institution.getBranches(x).select(new ArraySink())).getArray();
-            Long[] branchIds = branches.stream().map(Branch::getId).collect(Collectors.toList()).toArray(new Long[0]);
-
-            DAO trustAccountsDAO = find(x, account.findOwner(x), account.getDenomination());
-
-            Predicate p =
-                AND(
-                  INSTANCE_OF(BankAccount.class),
-                  EQ(Account.ENABLED, true),
-                  EQ(Account.DENOMINATION, account.getDenomination())
-                );
-            if ( branchIds.length > 0 ) {
-              p = AND(
-                    p,
-                    IN(BankAccount.BRANCH, branchIds)
-                  );
-            } else {
-              p = AND(
-                    p,
-                    EQ(BankAccount.INSTITUTION, institution.getId())
-                  );
-            }
-
-            List<BankAccount> bankAccounts = ((ArraySink) ((DAO) x.get("localAccountDAO"))
-              .where(p)
-              .select(new ArraySink())).getArray();
-
-            Long[] bankAccountIds = bankAccounts.stream().map(Account::getId).collect(Collectors.toList()).toArray(new Long[0]);
-
-            List<TrustAccount> trustAccounts = ((ArraySink) trustAccountsDAO
-              .where(
-                IN(TrustAccount.RESERVE_ACCOUNT, bankAccountIds)
+            Institution institution = (Institution) ((DAO) x.get("institutionDAO")).find(EQ(Institution.INSTITUTION_NUMBER,institutionNumber));
+            List reserveAccsList = ( (ArraySink) reserveAccs.where(
+              AND(
+                INSTANCE_OF(BankAccount.class),
+                EQ(BankAccount.INSTITUTION,institution.getId())
               )
-              .select(new ArraySink())).getArray();
+            ).select(new ArraySink())).getArray();
+            accountList = ((ArraySink) accounts.where(
+              EQ(TrustAccount.RESERVE_ACCOUNT,((BankAccount) reserveAccsList.get(0)).getId())
+            ).select(new ArraySink())).getArray();
 
-            if ( trustAccounts.size() == 0 ) {
-              logger.error("TrustAccount with ReserveAccount not found. institution", institution.getId(), "branches", Arrays.toString(branchIds), "banks", Arrays.toString(bankAccountIds));
-              throw new RuntimeException("TrustAccount with ReserveAccount not found.");
+            if ( accountList.size() > 1 ) {
+              logger.error("ERROR, Multiple Trust accounts found for institution: " + institutionNumber);
+              throw new RuntimeException("Multiple Trust accounts found for institution: " + institutionNumber);
             }
-            if ( trustAccounts.size() > 1 ) {
-              logger.error("ERROR, Multiple TrustAccounts with ReserveAccount found. institution", institution.getInstitutionNumber(), "branches", Arrays.toString(branchIds), "banks", Arrays.toString(bankAccountIds));
-              throw new RuntimeException("Multiple TrustAccounts with ReserveAccount found.");
+            if ( accountList.size() == 0 ) {
+              logger.error("Trust account not found for institution: " + institutionNumber);
+              throw new RuntimeException("Trust account not found for institution: "+ institutionNumber);
             }
-            return trustAccounts.get(0);
+            return (TrustAccount) accountList.get(0);
           }
 
           static public TrustAccount find(X x, User sourceUser, Currency currency) {
