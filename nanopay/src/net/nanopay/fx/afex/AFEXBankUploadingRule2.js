c352e6d1b6680da4d646f656fea95c8d48e7c6d1
Neelkanth Patel <neel@nanopay.net>
2020-04-16 11:26:58 -0400

    fix bank uploading

--- nanopay/src/net/nanopay/fx/afex/AFEXBankUploadingRule2.js
+++ nanopay/src/net/nanopay/fx/afex/AFEXBankUploadingRule2.js
@@ -34,21 +34,47 @@ foam.CLASS({
             AFEXBusiness afexBusiness = afexServiceProvider.getAFEXBusiness(x, account.getOwner());
 
             if ( afexBusiness == null ) {
+              sendFailureEmail(x, String.valueOf(account.getId()), "", null);
               return;
             }
             DAO businessDAO = (DAO) x.get("businessDAO");
             Business business = (Business) businessDAO.find(account.getOwner());
 
             if ( ! afexServiceProvider.directDebitEnrollment(business, account) ) {
-              EmailMessage message = new EmailMessage();
-              String body = "Failed to upload bank account: " + account.getId() + ", for AFEX business " + business.getBusinessName() + " " + business.getId();
-              message.setTo(new String[]{"paymentops@nanopay.net"});
-              message.setSubject("Failed AFEX Bank Account Upload");
-              message.setBody(body);
-              EmailsUtility.sendEmailFromTemplate(x, null, message, null, null);
+              sendFailureEmail(x, String.valueOf(account.getId()), String.valueOf(afexBusiness.getUser()), business);
             }
           }
         }, "Uploads business'a bank accounts to AFEX if the business has passed comliance.");
+      `
+    },
+    {
+      name: 'sendFailureEmail',
+      args: [
+        {
+          type: 'Context',
+          name: 'x',
+        },
+        {
+          type: 'String',
+          name: 'account'
+        },
+        {
+          type: 'String',
+          name: 'afexBusiness'
+        },
+        {
+          type: 'Business',
+          name: 'business'
+        }
+      ],
+      javaCode: `
+        EmailMessage message = new EmailMessage();
+        String businessInfo = business == null ? "" : business.getId() + " " + business.getBusinessName();
+        String body = "Failed to upload bank account: " + account + ", for AFEX business " + afexBusiness + ", business: " + businessInfo;
+        message.setTo(new String[]{"paymentops@nanopay.net"});
+        message.setSubject("Failed AFEX Bank Account Upload");
+        message.setBody(body);
+        EmailsUtility.sendEmailFromTemplate(x, null, message, null, null);
       `
     }
   ]
