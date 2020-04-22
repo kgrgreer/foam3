ce91d51537d33fd7eab2025957ef58de3341d03c
blakegreen <blake@nanopay.net>
2020-04-22 17:35:56 -0400

    Implemented algorithm to detect LiquiditySettings loop for accountDAO create/update rule

--- nanopay/src/net/nanopay/liquidity/LiquiditySettingsPreventLoopRule.js
+++ nanopay/src/net/nanopay/liquidity/LiquiditySettingsPreventLoopRule.js
@@ -6,12 +6,29 @@ foam.CLASS({
 
   implements: ['foam.nanos.ruler.RuleAction'],
 
-  javaImports: [],
+  javaImports: [
+    'foam.dao.DAO',
+    'java.util.HashSet',
+    'net.nanopay.account.DigitalAccount',
+    'net.nanopay.liquidity.LiquiditySettings'
+  ],
 
   methods: [
     {
       name: 'applyAction',
       javaCode: `
+        DigitalAccount account = (DigitalAccount) obj;
+        DAO accountDAO = (DAO) x.get("localAccountDAO");
+        DAO liquiditySettingsDAO = (DAO) x.get("localLiquiditySettingsDAO");
+        HashSet<DigitalAccount> seenAccounts = new HashSet<>();
+        while ( account != null ) {
+          if ( seenAccounts.contains(account) ) {
+            throw new Error("Loop detected");
+          }
+          seenAccounts.add(account);
+          LiquiditySettings liquiditySettings = (LiquiditySettings) liquiditySettingsDAO.find(account.getLiquiditySetting());
+          account = (DigitalAccount) accountDAO.find(liquiditySettings.getHighLiquidity().getPushPullAccount());
+        }
       `
     }
   ]
