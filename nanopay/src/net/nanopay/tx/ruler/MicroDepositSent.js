e3d913ec9d7b40035976b6602ba3e2c5cafe1ca3
yij793 <jiangarfield@gmail.com>
2020-05-05 08:42:15 -0400

    remove spid in microdeposit send

--- nanopay/src/net/nanopay/tx/ruler/MicroDepositSent.js
+++ nanopay/src/net/nanopay/tx/ruler/MicroDepositSent.js
@@ -47,11 +47,7 @@ foam.CLASS({
             args.put("userEmail", User.EMAIL);
             args.put("sendTo", User.EMAIL);
             args.put("link", config.getUrl());
-            if ( ! user.getSpid().equals("nanopay") ) {
-              args.put("spid", user.getSpid());
-            } else {
-              args.put("spid", "ablii");
-            }
+
             Notification notification = new Notification.Builder(x)
             .setBody(acc.toSummary() + " is processing ")
             .setNotificationType("bankNotifications")
