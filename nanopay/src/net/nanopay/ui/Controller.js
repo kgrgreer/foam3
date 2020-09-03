c31074c03cf42b70c4508b38decfeba5d028e365
Kristina Smirnova <penzital888@gmail.com>
2020-09-02 20:18:28 -0400

    fix notif permissions (#10516)

--- nanopay/src/net/nanopay/ui/Controller.js
+++ nanopay/src/net/nanopay/ui/Controller.js
@@ -961,9 +961,7 @@ foam.CLASS({
       this.themeUpdated.resolve();
 
       // Listener to check for new toast notifications
-      var userNotificationQueryId = this.subject && this.subject.realUser ?
-      this.subject.realUser.id : this.user.id;
-
+      var userNotificationQueryId = this.subject.realUser.id;
       this.__subSubContext__.notificationDAO.where(
         this.EQ(this.Notification.USER_ID, userNotificationQueryId)
       ).on.put.sub((sub, on, put, obj) => {
