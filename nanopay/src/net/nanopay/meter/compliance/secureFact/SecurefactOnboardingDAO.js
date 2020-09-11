7317f1d9d81456a146ebc471c5678b5d926d2b3a
blakegreen <blake@nanopay.net>
2020-09-11 13:53:54 -0400

    Implemented rough draft of SecurefactOnboardingDAO Decorator

--- nanopay/src/net/nanopay/meter/compliance/secureFact/SecurefactOnboardingDAO.js
+++ nanopay/src/net/nanopay/meter/compliance/secureFact/SecurefactOnboardingDAO.js
@@ -20,10 +20,21 @@
    name: 'SecurefactOnboardingDAO',
    extends: 'foam.dao.ProxyDAO',
 
-   documentation: `Decorating DAO for business onboarding through FlinksLoginId requests.`,
+   documentation: `Decorating DAO for Securefact business onboarding through FlinksLoginId requests.`,
 
    javaImports: [
-     'net.nanopay.flinks.external.FlinksLoginId'
+     'foam.dao.DAO',
+     'foam.nanos.auth.Subject',
+     'foam.nanos.auth.User',
+     'foam.nanos.logger.Logger',
+     'net.nanopay.flinks.external.FlinksLoginId',
+     'net.nanopay.meter.compliance.secureFact.SecurefactService',
+     'net.nanopay.meter.compliance.secureFact.lev.LEVResponse',
+     'net.nanopay.meter.compliance.secureFact.lev.LEVResult',
+     'net.nanopay.meter.compliance.secureFact.lev.document.LEVDocumentDataResponse',
+     'net.nanopay.meter.compliance.secureFact.lev.document.LEVDocumentOrderResponse',
+     'net.nanopay.meter.compliance.secureFact.lev.document.LEVDocumentParty',
+     'net.nanopay.model.Business'
    ],
 
    methods: [
@@ -31,6 +42,39 @@
        name: 'put_',
        javaCode: `
         FlinksLoginId flinksLoginId = (FlinksLoginId) obj;
+        DAO businessDAO = (DAO) x.get("localBusinessDAO");
+        SecurefactService securefactService = (SecurefactService) x.get("securefactService");
+        Business business = flinksLoginId.findBusiness(x);
+
+        if ( business == null ) {
+          User user = flinksLoginId.findUser(x);
+          if (user == null) {
+            user = ((Subject) x.get("subject")).getUser();
+          }
+          business = (Business) businessDAO.find(user.getId());
+        }
+
+        try {
+          LEVResponse response = securefactService.levSearch(x, business);
+          LEVResult[] results = response.getResults();
+          LEVResult chosenResult = null;
+          for ( int i = 0; i < results.length; i++ ) {
+            LEVResult result = results[i];
+            if ( result.getCloseMatch() == true ) {
+              chosenResult = result;
+              break;
+            }
+          }
+
+          if ( chosenResult != null ) {
+            LEVDocumentOrderResponse orderResponse = securefactService.levDocumentOrder(x, chosenResult.getResultId());
+            LEVDocumentDataResponse dataResponse = securefactService.levDocumentData(x, orderResponse.getOrderId());
+            LEVDocumentParty[] parties = dataResponse.getParties();
+          }
+        } catch (Exception e) {
+          ((Logger) x.get("logger")).warning("SecurefactOnboardingDAO failed.", e);
+        }
+        
         return super.put_(x, flinksLoginId);
        `
      }
