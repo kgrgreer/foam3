410c81e9a161c9e55a873dbe39774cc148d0820b
hchoura <choura.hassene@gmail.com>
2020-11-29 22:25:59 -0500

    add msg net.nanopay.bank.BRBankAccount

--- nanopay/src/net/nanopay/bank/BRBankAccount.js
+++ nanopay/src/net/nanopay/bank/BRBankAccount.js
@@ -58,7 +58,12 @@ foam.CLASS({
     { name: 'BANK_CODE_INVALID', message: 'Bank code must be 8 letters and/or digits long' },
     { name: 'BANK_CODE_REQUIRED', message: 'Bank code required' },
     { name: 'BRANCH_CODE_INVALID', message: 'Branch code must be 5 digits long' },
-    { name: 'BRANCH_CODE_REQUIRED', message: 'Branch code required' }
+    { name: 'BRANCH_CODE_REQUIRED', message: 'Branch code required' },
+    { name: 'HOLDER1', message: '1st Holder' },
+    { name: 'HOLDER2', message: '2nd Holder' },
+    { name: 'CURRENT', message: 'Current' },
+    { name: 'SAVINGS', message: 'Savings' },
+    { name: 'PLEASE_SELECT', message: 'Please select' }
   ],
 
   constants: [
@@ -154,13 +159,15 @@ foam.CLASS({
       name: 'accountType',
       updateVisibility: 'RO',
       section: 'accountInformation',
-      view: {
-        class: 'foam.u2.view.ChoiceView',
-        placeholder: 'Please select',
-        choices: [
-          ['c', 'Current'],
-          ['p', 'Savings']
-        ]
+      view: function(_, X) {
+        return {
+          class: 'foam.u2.view.ChoiceView',
+          placeholder: X.data.PLEASE_SELECT,
+          choices: [
+            [true, X.data.CURRENT],
+            [false, X.data.SAVINGS]
+          ]
+        };
       },
       validateObj: function(accountType) {
         if ( accountType === '' || accountType === undefined ) {
@@ -174,13 +181,15 @@ foam.CLASS({
       label: 'Account holder',
       updateVisibility: 'RO',
       section: 'accountInformation',
-      view: {
-        class: 'foam.u2.view.ChoiceView',
-        placeholder: 'Please select',
-        choices: [
-          ['1', '1st Holder'],
-          ['2', '2nd Holder']
-        ]
+      view: function(_, X) {
+        return {
+          class: 'foam.u2.view.ChoiceView',
+          placeholder: X.data.PLEASE_SELECT,
+          choices: [
+            [true, X.data.HOLDER1],
+            [false, X.data.HOLDER2]
+          ]
+        };
       },
       validateObj: function(accountOwnerType) {
         if ( accountOwnerType === '' || accountOwnerType === undefined ) {
@@ -245,7 +254,7 @@ foam.CLASS({
       name: 'validateBankCode',
       type: 'Void',
       javaThrows: ['IllegalStateException'],
-      javaCode: `     
+      javaCode: `
         String bankCode = this.getBankCode();
 
         if ( SafetyUtil.isEmpty(bankCode) ) {
@@ -261,7 +270,7 @@ foam.CLASS({
       name: 'validateBranchCode',
       type: 'Void',
       javaThrows: ['IllegalStateException'],
-      javaCode: `     
+      javaCode: `
         String branchCode = this.getBranchCode();
 
         if ( SafetyUtil.isEmpty(branchCode) ) {
