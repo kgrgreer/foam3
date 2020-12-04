3e9011343fd77bd45421ae9ee51001fd6fc12a71
Eric <eric@nanopay.net>
2020-12-04 10:40:36 -0500

    Fix data object

--- nanopay/src/net/nanopay/crunch/onboardingModels/SigningOfficerByDefaultRule.js
+++ nanopay/src/net/nanopay/crunch/onboardingModels/SigningOfficerByDefaultRule.js
@@ -37,7 +37,13 @@ foam.CLASS({
 
           // Certify data reviewed
           crunchService.updateJunction(
-            subjectX, "554af38a-8225-87c8-dfdf-eeb15f71215f-14", data, GRANTED);
+            subjectX, "554af38a-8225-87c8-dfdf-eeb15f71215f-14",
+            new CertifyDataReviewed.Builder(subjectX)
+              .setReviewed(true)
+              .setSigningOfficer(realUser.getId())
+              .build(),
+            GRANTED
+          );
 
         }, "Add signing officer capability");
       `
