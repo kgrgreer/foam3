1a6f83e2548f47eece6d1768151bf2901726c53c
Arthur Pavlovs <arthur@nanopay.net>
2020-12-08 18:11:13 -0500

    update bank account menu

--- nanopay/src/net/nanopay/files.js
+++ nanopay/src/net/nanopay/files.js
@@ -157,6 +157,7 @@ FOAM_FILES([
   { name: 'net/nanopay/bank/CNBankAccount' },
   { name: 'net/nanopay/bank/SEBankAccount' },
   { name: 'net/nanopay/bank/AddBankAccountAction' },
+  { name: 'net/nanopay/bank/BankAccountTableView', flags: ['web'] },
   { name: 'net/nanopay/bank/BankAccountStatus' },
   { name: 'net/nanopay/bank/BankAccountController', flags: ['web'] },
   { name: 'net/nanopay/bank/ui/BankPickCurrencyView', flags: ['web'] },
