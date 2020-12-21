c83a7f19816d9361ae6970f7218607505ebea2ca
Joel Hughes <joel@nanopay.net>
2020-12-21 11:58:29 -0500

    correct format type mismatch after change of account id from long to string

--- nanopay/src/net/nanopay/onboarding/email/NewBankAccountAddedEmailDAO.js
+++ nanopay/src/net/nanopay/onboarding/email/NewBankAccountAddedEmailDAO.js
@@ -109,7 +109,7 @@ foam.CLASS({
       try {
         EmailsUtility.sendEmailFromTemplate(x, owner, message, "notification-to-onboarding-team", args);
       } catch (Throwable t) {
-        String msg = String.format("Email meant for complaince team Error: User (id = %1$s) has added a BankAccount (id = %2$d).", owner.getId(), account.getId());
+        String msg = String.format("Email meant for complaince team Error: User (id = %1$s) has added a BankAccount (id = %2$s).", owner.getId(), account.getId());
         ((Logger) x.get("logger")).error(msg, t);
       }
 
