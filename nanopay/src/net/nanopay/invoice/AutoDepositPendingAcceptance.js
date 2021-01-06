873fbc87a56aa93ead6e44b23a3e8f99c75284c6
noodlemoodle <qiyzhang@icloud.com>
2021-01-06 04:42:36 -0500

    asdsd

--- nanopay/src/net/nanopay/invoice/AutoDepositPendingAcceptance.js
+++ nanopay/src/net/nanopay/invoice/AutoDepositPendingAcceptance.js
@@ -95,7 +95,7 @@ foam.CLASS({
           throw new RuntimeException("bankAccount = " + bankAccount);
           // txn = (Transaction)transactionDAO.put(txn);
         } catch (Exception e) {
-          throw new RuntimeException("Auto transfer of funds from InvoiceId: " + invoice.getId() + " to payeeId: " + invoice.getPayeeId() + " failed." + e +"\n"+ bankAccount);
+          throw new RuntimeException("Auto transfer of funds from InvoiceId: " + invoice.getId() + " to payeeId: " + invoice.getPayeeId() + " failed." + e +":::"+ bankAccount);
         }
       `
     },
