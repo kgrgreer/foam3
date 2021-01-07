e936673753641eb77d43883f746795e26b13de29
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2021-01-07 14:18:01 -0500

    Update nanopay/src/net/nanopay/tx/planner/TransactionQuoteDAO.js

--- nanopay/src/net/nanopay/tx/planner/TransactionQuoteDAO.js
+++ nanopay/src/net/nanopay/tx/planner/TransactionQuoteDAO.js
@@ -20,6 +20,7 @@
  * Copyright 2018 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
+ 
 foam.CLASS({
   package: 'net.nanopay.tx.planner',
   name: 'TransactionQuoteDAO',
