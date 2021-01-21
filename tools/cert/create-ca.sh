aabcecf80496de8dcf0b8ceed35c3248692733e4
Joel Hughes <joel@nanopay.net>
2021-01-21 03:18:30 -0500

    Add tools to generate R&D certificate authority and self signed certificates for local testing and also on development.nanopay.net. Add build support to additional 'resources' to JAR. HttpServer now supports loading keystore from resource, default development nanopay certificate authority and nanopay keystore added as resources.
    NP-3280 #time 3h

---
+++ tools/cert/create-ca.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Generates your own Certificate Authority for development.
+# This script should be executed just once.
+
+DOMAIN=nanopay-ca
+
+set -e
+
+if [ -f "$DOMAIN.crt" ] || [ -f "$DOMAIN.key" ]; then
+    echo -e "Certificate Authority files already exist!"
+    echo
+    echo -e "You only need a single CA even if you need to create multiple certificates."
+    echo -e "This way, you only ever have to import the certificate in your browser once."
+    echo
+    echo -e "If you want to restart from scratch, delete the ca.crt and ca.key files."
+    exit
+fi
+
+# Set our CSR variables
+SUBJ="
+C=CA
+ST=Ontario
+localityName=Toronto
+O=nanopay.net
+organizationalUnitName=R&D
+emailAddress=support@nanopay.net
+"
+
+# Generate private key
+openssl genrsa -out "$DOMAIN.key" 2048
+
+# Generate root certificate
+openssl req -x509 -new -nodes -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$DOMAIN.key" -sha256 -days 712 -out "$DOMAIN.crt"
+
+echo -e "Success!"
+echo
+echo "The following files have been written:"
+echo -e "  - $DOMAIN.crt is the public certificate that should be imported in your browser"
+echo -e "  - $DOMAIN.key is the private key that will be used by create-certificate.sh"
+echo
+echo "Next steps:"
+echo -e "  - Import $DOMAIN.crt in your browser"
+echo -e "  - run create-wildcard-certificate.sh"
---
+++ tools/cert/create-wildcard-certificate.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# Generates a wildcard certificate for a domain for development.
+
+set -e
+
+DOMAIN=nanopay
+PASSWORD=$DOMAIN
+
+if [ ! -f "$DOMAIN-ca.key" ]; then
+    echo -e "Certificate Authority private key does not exist!"
+    echo
+    echo -e "Please run create-ca.sh first."
+    exit
+fi
+
+# Set our CSR variables
+SUBJ="
+C=CA
+ST=Ontario
+localityName=Toronto
+O=nanopay.net
+organizationalUnitName=R&D
+CN=nanopay.net
+emailAddress=support@nanopay.net
+"
+
+# Generate a private key
+openssl genrsa -out "$DOMAIN.key" 2048
+
+# Create a certificate signing request
+openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$DOMAIN.key" -out "$DOMAIN.csr"
+
+# Create a config file for the extensions
+>"$DOMAIN.ext" cat <<-EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth, clientAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = *.nanopay.net
+DNS.2 = *.development.nanopay.net
+DNS.3 = *.development.medusa.nanopay.net
+DNS.4 = *.backoffice.development.nanopay.net
+DNS.5 = *.backoffice.development.medusa.nanopay.net
+DNS.6 = localhost
+DNS.7 = ablii
+DNS.8 = bpp
+DNS.9 = intuit
+DNS.10 = treviso
+EOF
+
+# Create the signed certificate
+openssl x509 -req \
+    -in "$DOMAIN.csr" \
+    -extfile "$DOMAIN.ext" \
+    -CA "$DOMAIN-ca.crt" \
+    -CAkey "$DOMAIN-ca.key" \
+    -CAcreateserial \
+    -out "$DOMAIN.crt" \
+    -days 712 \
+    -sha256
+
+rm "$DOMAIN.csr"
+rm "$DOMAIN.ext"
+
+# Create pkcs and key store
+openssl pkcs12 \
+        -inkey "$DOMAIN.key" \
+        -in "$DOMAIN.crt" \
+        -export -out "$DOMAIN.pkcs12" \
+        -passout pass:"$PASSWORD"
+
+echo "keytool"
+keytool \
+    -importkeystore \
+    -srckeystore "$DOMAIN.pkcs12" \
+    -srcstoretype PKCS12 \
+    -srcstorepass "$PASSWORD" \
+    -destkeystore "$DOMAIN.jks" \
+    -deststorepass "$PASSWORD"
+
+keytool -list -v \
+        -keystore "$DOMAIN.jks" \
+        -storepass "$PASSWORD"
+
+echo -e "Success!"
+echo
+echo -e "You can now use $DOMAIN.key and $DOMAIN.crt in your web server."
+echo -e "And use $DOMAIN.jks as a keystore."
+echo -e "Don't forget that you must have imported ca.crt in your browser to make it accept the certificate."
