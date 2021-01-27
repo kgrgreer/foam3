0528315bcc69091fe990723daa877df87045d7a7
Chanmann Lim <chanmannlim@gmail.com>
2021-01-27 13:43:38 -0500

    Revert "Fix account information section with blank title"
    This reverts commit 3a510502

--- nanopay/src/net/nanopay/bank/BRBankAccount.js
+++ nanopay/src/net/nanopay/bank/BRBankAccount.js
@@ -47,6 +47,13 @@ foam.CLASS({
     'foam.core.ValidationException'
   ],
 
+  sections: [
+    {
+      name: 'accountInformation',
+      title: ''
+    }
+  ],
+
   messages: [
     { name: 'ACCOUNT_NUMBER_INVALID', message: 'Account number must be 10 digits long' },
     { name: 'ACCOUNT_NUMBER_REQUIRED', message: 'Account number required' },
