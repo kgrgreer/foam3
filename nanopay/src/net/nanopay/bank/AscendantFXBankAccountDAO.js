40d77eeb22b0a5c6638cc0382615b6368cc04f88
Minsun <minsun@nanopay.net>
2021-02-11 19:44:52 -0500

    Revert "noc"
    This reverts commit f4cb637d27c92648cdf89c93e031bb2ee16e4f00.

--- nanopay/src/net/nanopay/bank/AscendantFXBankAccountDAO.js
+++ nanopay/src/net/nanopay/bank/AscendantFXBankAccountDAO.js
@@ -91,7 +91,6 @@ foam.CLASS({
             String message = ORGANIZATION_SETUP_REQUIRED_ERROR_MSG + accountOwner.getId() ;
             Notification notification = new Notification.Builder(x)
               .setTemplate("NOC")
-              .setEmailName("NOC")
               .setBody(message)
               .build();
           ((DAO) x.get("localNotificationDAO")).put(notification);
