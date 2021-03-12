b8b9cd9b5d9126498b99986c787be2ce8f285869
nanoMichal <michall.pasternak@nanopay.net>
2021-03-11 22:07:48 -0500

    remove fclone, reduce getstate calls

--- nanopay/src/net/nanopay/invoice/BillingInvoicesCron.java
+++ nanopay/src/net/nanopay/invoice/BillingInvoicesCron.java
@@ -150,7 +150,7 @@ public class BillingInvoicesCron implements ContextAgent {
         Transaction transaction = (Transaction) obj;
 
         // Only want to charge fees on completed or declined Transaction chains
-        TransactionStatus state = ((Transaction) transaction.fclone()).getState(x);
+        TransactionStatus state =  transaction.getState(x);
 
         Transaction ct = (Transaction) transactionDAO.find(AND(
           EQ(Transaction.PARENT, transaction.getId()),
