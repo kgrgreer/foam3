2ec519335bebe03ed28a549b3a864946def70c63
nanoMichal <michall.pasternak@nanopay.net>
2021-03-12 13:56:50 -0500

    get total needs to be used instead of get amoutn

--- nanopay/src/net/nanopay/tx/rbc/cron/RbcGenerateFileCron.java
+++ nanopay/src/net/nanopay/tx/rbc/cron/RbcGenerateFileCron.java
@@ -166,7 +166,7 @@ public class RbcGenerateFileCron implements ContextAgent {
     long cumulativeAmount = 0;
     for ( int i = 0; i < transactions.size(); i++ ) {
       Transaction txn = (Transaction) transactions.get(i);
-      cumulativeAmount += txn.getAmount();
+      cumulativeAmount += txn.getTotal(x, txn.getSourceAccount());
       if ( cumulativeAmount >= eftLimit ) {
         int leftoverTxns = transactions.size() - i;
         logger.warning("RBC EFT limit of " + eftLimit + " was reached with " + leftoverTxns + " transactions remaining for next EFT file.");
