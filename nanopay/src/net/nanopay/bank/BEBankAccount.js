8cf917982aa2f1eacdaf62cfa93bdc766178a444
Chanmann Lim <chanmannlim@gmail.com>
2021-03-15 17:53:25 -0400

    Add BE bank account validation

--- nanopay/src/net/nanopay/bank/BEBankAccount.js
+++ nanopay/src/net/nanopay/bank/BEBankAccount.js
@@ -23,6 +23,23 @@ foam.CLASS({
 
   documentation: 'Belgium bank account information.',
 
+  javaImports: [
+    'foam.core.ValidationException'
+  ],
+
+  constants: [
+    {
+      name: 'INSTITUTION_NUMBER_PATTERN',
+      type: 'Regex',
+      value: /^\d{3}$/
+    },
+    {
+      name: 'ACCOUNT_NUMBER_PATTERN',
+      type: 'Regex',
+      value: /^\d{7}$/
+    }
+  ],
+
   properties: [
     {
       name: 'country',
@@ -84,6 +101,46 @@ foam.CLASS({
     {
       name: 'branchId',
       visibility: 'HIDDEN'
+    },
+    {
+      name: 'bankRoutingCode',
+      javaPostSet: `
+        if ( val != null && INSTITUTION_NUMBER_PATTERN.matcher(val).matches() ) {
+          clearInstitution();
+          setInstitutionNumber(val);
+        }
+      `
+    }
+  ],
+
+  methods: [
+    {
+      name: 'validate',
+      javaCode: `
+        super.validate(x);
+
+        var accountNumber = this.getAccountNumber();
+        if ( SafetyUtil.isEmpty(accountNumber) ) {
+          throw new ValidationException(this.ACCOUNT_NUMBER_REQUIRED);
+        }
+        if ( ! ACCOUNT_NUMBER_PATTERN.matcher(accountNumber).matches() ) {
+          throw new ValidationException(this.ACCOUNT_NUMBER_INVALID);
+        }
+
+        if ( SafetyUtil.isEmpty(getSwiftCode()) ) {
+          var institutionNumber = this.getInstitutionNumber();
+          if ( SafetyUtil.isEmpty(institutionNumber) ) {
+            throw new ValidationException(this.INSTITUTION_NUMBER_REQUIRED);
+          }
+          if ( ! INSTITUTION_NUMBER_PATTERN.matcher(institutionNumber).matches() ) {
+            throw new ValidationException(this.INSTITUTION_NUMBER_INVALID);
+          }
+        }
+      `
+    },
+    {
+      name: 'getRoutingCode',
+      javaCode: 'return getInstitutionNumber();'
     }
   ]
 });
