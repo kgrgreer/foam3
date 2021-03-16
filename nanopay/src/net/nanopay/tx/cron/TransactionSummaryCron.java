0b95c98d29a3599a3ba92f001ad8c94fc4575ede
blakegreen <blake@nanopay.net>
2021-03-16 11:53:00 -0400

    Added TransactionSummaryCron.java

---
+++ nanopay/src/net/nanopay/tx/cron/TransactionSummaryCron.java
@@ -0,0 +1,10 @@
+package net.nanopay.tx.cron;
+
+import foam.core.ContextAgent;
+import foam.core.X;
+import foam.dao.DAO;
+public class TransactionSummaryCron extends ContextAgent {
+  @Override
+  public void execute(X x) {
+  }
+}
