f41bf386e364dad17f8a0502482abbf1765829ec
Chanmann Lim <chanmannlim@gmail.com>
2021-03-16 09:26:03 -0400

    Add DE bank account validation

--- nanopay/src/net/nanopay/bank/DEBankAccount.js
+++ nanopay/src/net/nanopay/bank/DEBankAccount.js
@@ -23,6 +23,24 @@ foam.CLASS({
 
   documentation: 'German bank account information.',
 
+  javaImports: [
+    'foam.core.ValidationException',
+    'foam.util.SafetyUtil'
+  ],
+
+  constants: [
+    {
+      name: 'BRANCH_ID_PATTERN',
+      type: 'Regex',
+      value: /^\d{8}$/
+    },
+    {
+      name: 'ACCOUNT_NUMBER_PATTERN',
+      type: 'Regex',
+      value: /^\d{10}$/
+    }
+  ],
+
   properties: [
     {
       name: 'country',
@@ -94,6 +112,42 @@ foam.CLASS({
     {
       name: 'branchId',
       visibility: 'HIDDEN'
+    },
+    {
+      name: 'bankRoutingCode',
+      javaPostSet: `
+        if ( val != null && BRANCH_ID_PATTERN.matcher(val).matches() ) {
+          clearBranch();
+          setBranchId(val);
+        }
+      `
+    }
+  ],
+
+  methods: [
+    {
+      name: 'validate',
+      javaCode: `
+        super.validate(x);
+
+        var accountNumber = this.getAccountNumber();
+        if ( SafetyUtil.isEmpty(accountNumber) ) {
+          throw new ValidationException(this.ACCOUNT_NUMBER_REQUIRED);
+        }
+        if ( ! ACCOUNT_NUMBER_PATTERN.matcher(accountNumber).matches() ) {
+          throw new ValidationException(this.ACCOUNT_NUMBER_INVALID);
+        }
+
+        if ( SafetyUtil.isEmpty(getSwiftCode()) ) {
+          var branchId = this.getBranchId();
+          if ( SafetyUtil.isEmpty(branchId) ) {
+            throw new ValidationException(this.BRANCH_ID_REQUIRED);
+          }
+          if ( ! BRANCH_ID_PATTERN.matcher(branchId).matches() ) {
+            throw new ValidationException(this.BRANCH_ID_INVALID);
+          }
+        }
+      `
     }
   ]
 });
