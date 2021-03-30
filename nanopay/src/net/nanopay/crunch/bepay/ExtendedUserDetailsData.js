a7af82702ab35da017032b5dc1b60344f3be6400
Anna <anna@nanopay.net>
2021-03-30 10:06:57 -0400

    adjust CPF crunch model to include DOB (#12259)
    * adjust CPF crunch model to include DOB - since specifically rely on each other

--- nanopay/src/net/nanopay/crunch/bepay/ExtendedUserDetailsData.js
+++ nanopay/src/net/nanopay/crunch/bepay/ExtendedUserDetailsData.js
@@ -21,43 +21,7 @@ foam.CLASS({
 
   documentation: `This model represents the expanded info of a User that must be collect for BePay onboarding.`,
 
-  messages: [
-    { name: 'INVALID_DATE_ERROR', message: 'Valid date of birth required' },
-    { name: 'UNGER_AGE_LIMIT_ERROR', message: 'Must be at least 18 years old' },
-    { name: 'OVER_AGE_LIMIT_ERROR', message: 'Must be under the age of 125 years old' }
-  ],
-
   properties: [
-    foam.nanos.auth.User.BIRTHDAY.clone().copyFrom({
-      label: 'Date of birth',
-      validationPredicates: [
-        {
-          args: ['birthday'],
-          predicateFactory: function(e) {
-            return e.NEQ(net.nanopay.crunch.bepay.ExtendedUserDetailsData.BIRTHDAY, null);
-          },
-          errorMessage: 'INVALID_DATE_ERROR'
-        },
-        {
-          args: ['birthday'],
-          predicateFactory: function(e) {
-            var limit = new Date();
-            limit.setDate(limit.getDate() - ( 18 * 365 ));
-            return e.LT(net.nanopay.crunch.bepay.ExtendedUserDetailsData.BIRTHDAY, limit);
-          },
-          errorMessage: 'UNGER_AGE_LIMIT_ERROR'
-        },
-        {
-          args: ['birthday'],
-          predicateFactory: function(e) {
-            var limit = new Date();
-            limit.setDate(limit.getDate() - ( 125 * 365 ));
-            return e.GT(net.nanopay.crunch.bepay.ExtendedUserDetailsData.BIRTHDAY, limit);
-          },
-          errorMessage: 'OVER_AGE_LIMIT_ERROR'
-        }
-      ]
-    }),
     {
       class: 'String',
       name: 'mothersMaidenName',
