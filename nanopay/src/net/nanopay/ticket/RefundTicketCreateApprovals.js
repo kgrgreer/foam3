30185020787490b3d11b5151608046cbaa2951d1
nanoMichal <michall.pasternak@nanopay.net>
2021-03-30 12:07:14 -0400

    tuesday

--- nanopay/src/net/nanopay/ticket/RefundTicketCreateApprovals.js
+++ nanopay/src/net/nanopay/ticket/RefundTicketCreateApprovals.js
@@ -117,7 +117,7 @@ foam.CLASS({
                 .setObjId(approvable.getId())
                 .setOperation(operation)
                 .setCreatedBy(user.getId())
-                .setGroup(getGroupToNotify())
+                .setGroup(getSpid()+"-"+getGroupToNotify())
                 .setClassification(
                   "Ticket:" +
                   oldRefundTicket.getId() +
