b178311b1b037b391596491b1cbef85dd6265cdb
Mayowa Olurin <mayowa@nanopay.net>
2021-04-09 18:18:08 +0100

    fclone object

--- nanopay/src/net/nanopay/tx/rbc/cron/RbcCompleteCron.java
+++ nanopay/src/net/nanopay/tx/rbc/cron/RbcCompleteCron.java
@@ -38,6 +38,7 @@ public class RbcCompleteCron implements ContextAgent {
     for ( Transaction rbcTransaction : transactions ) {
 
       try {
+        rbcTransaction = (Transaction) rbcTransaction.fclone();
         // if the transaction is not settled, do nothing
         if ( ! ((RbcTransaction)rbcTransaction).getSettled() ) continue;
 
