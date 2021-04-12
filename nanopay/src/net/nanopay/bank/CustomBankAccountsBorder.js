f3a4c78349b23607c16b14150fb0b1a68d23ddcb
hchoura <choura.hassene@gmail.com>
2021-04-12 16:25:07 -0400

    use the decorator to select the Owned account

--- nanopay/src/net/nanopay/bank/CustomBankAccountsBorder.js
+++ nanopay/src/net/nanopay/bank/CustomBankAccountsBorder.js
@@ -1,3 +1,20 @@
+/**
+ * NANOPAY CONFIDENTIAL
+ *
+ * [2021] nanopay Corporation
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of nanopay Corporation.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to nanopay Corporation
+ * and may be covered by Canadian and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from nanopay Corporation.
+ */
+
 foam.CLASS({
   package: 'net.nanopay.bank',
   name: 'CustomBankAccountsBorder',
@@ -5,17 +22,33 @@ foam.CLASS({
   documentation: `
     A border which restricts bankAccountDAO to only custom bank accounts.
   `,
+  requires: [
+    'net.nanopay.bank.BankAccount'
+  ],
 
-  imports: [ 'subject' ],
-  exports: [ 'bankAccountDAO' ],
+  imports: [
+    'config as oldConfig',
+    //'subject',
+    'user'
+  ],
+  exports: [ 'config' ],
 
   properties: [
     {
-      name: 'bankAccountDAO',
+      name: 'config',
       factory: function() {
-        return this.subject.user.accounts
-          .where(this.INSTANCE_OF(this.BankAccount))
+        var config = this.oldConfig.clone();
+        //config.dao = this.subject.user.accounts;
+
+        config.dao = config.dao
+          .where(
+            this.AND(
+              this.INSTANCE_OF(this.BankAccount),
+              this.EQ(this.BankAccount.OWNER, this.user.id),
+            )
+          )
           .orderBy(this.BankAccount.CREATED);
+        return config;
       }
     }
   ]
