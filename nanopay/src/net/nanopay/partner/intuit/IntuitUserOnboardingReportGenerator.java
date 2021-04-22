96cb83de9951ab7aabc704f99ecc296f3e07c6e8
Nicholas Prat <nprat@protonmail.com>
2021-04-22 16:26:48 -0400

    Updated license header dates

--- nanopay/src/net/nanopay/partner/intuit/IntuitUserOnboardingReportGenerator.java
+++ nanopay/src/net/nanopay/partner/intuit/IntuitUserOnboardingReportGenerator.java
@@ -1,7 +1,7 @@
 /**
  * NANOPAY CONFIDENTIAL
  *
- * [2020] nanopay Corporation
+ * [2021] nanopay Corporation
  * All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
