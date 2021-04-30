9c9cca03465490ef4c300a262bece0f6da547ecb
Nicholas Prat <nprat@protonmail.com>
2021-04-30 11:39:32 -0400

    Working

---
+++ nanopay/src/net/nanopay/partner/intuit/BusinessUpdaterDAO.java
@@ -0,0 +1,13 @@
+package net.nanopay.partner.intuit;
+
+import foam.core.FObject;
+import foam.core.X;
+import foam.dao.ProxyDAO;
+
+public class BusinessUpdaterDAO extends ProxyDAO {
+
+  @Override
+  public FObject put_(X x, FObject obj) {
+    return super.put_(x, obj);
+  }
+}
