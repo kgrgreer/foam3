6a688acaa61937a5e594e35fc7ab6fa24a5fb405
nanoMichal <michall.pasternak@nanopay.net>
2021-05-03 13:56:04 -0400

    update error code logic

--- nanopay/src/net/nanopay/tx/ComplianceTransaction.js
+++ nanopay/src/net/nanopay/tx/ComplianceTransaction.js
@@ -81,10 +81,9 @@ foam.CLASS({
       name: 'calculateErrorCode',
       javaCode: `
         if ( getStatus() == TransactionStatus.DECLINED ) {
-          return 991l;
+          setErrorCode(991l);
         }
-
-        return 0;
+        return getErrorCode();
       `
     }
   ]
