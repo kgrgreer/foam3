6cf73c438ab769ccab6ccc6acbfc4128ad9ce320
Nicholas Prat <nprat@protonmail.com>
2021-05-12 14:33:22 -0400

    Working

--- nanopay/src/net/nanopay/partner/intuit/BusinessUpdaterDAO.java
+++ nanopay/src/net/nanopay/partner/intuit/BusinessUpdaterDAO.java
@@ -2,12 +2,26 @@ package net.nanopay.partner.intuit;
 
 import foam.core.FObject;
 import foam.core.X;
+import foam.dao.ArraySink;
+import foam.dao.DAO;
 import foam.dao.ProxyDAO;
+import foam.nanos.crunch.UserCapabilityJunction;
+import net.nanopay.model.Business;
+
+import static foam.mlang.MLang.EQ;
 
 public class BusinessUpdaterDAO extends ProxyDAO {
 
   @Override
   public FObject put_(X x, FObject obj) {
+    var business = (Business) obj;
+    if ( business.getId() != 0 ) return null;
+    var oldBusiness = super.find_(x, business.getId());
+    var ucjDAO = (DAO) x.get("userCapabilityJunctionDAO");
+
+    var sink = (ArraySink) ucjDAO.where(EQ(UserCapabilityJunction.SOURCE_ID, business.getId())).select(new ArraySink());
+
     return super.put_(x, obj);
   }
+
 }
