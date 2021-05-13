e757f0259c1a4ce5902f66ac338b0c3e87662d51
Eric Dubé <eric@nanopay.net>
2021-05-13 12:09:36 -0400

    Fix typo

--- src/foam/nanos/crunch/UCJProperty.js
+++ src/foam/nanos/crunch/UCJProperty.js
@@ -12,7 +12,7 @@ foam.CLASS({
     UCJProperty references a UCJ via a predicate. This property can be used to
     associate a UCJ with an instance either by UUID or by a subject and
     capability id.
-    UCJProperty properties will render in an inline CRUNCH wizard.`
+    UCJProperty properties will render in an inline CRUNCH wizard.
   `,
 
   imports: [
