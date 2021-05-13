d1c1b9d6464da86c5f56f610bbfbcd776500520c
Joel Hughes <joel@nanopay.net>
2021-05-12 21:16:50 -0400

    updates during testing

--- tools/medusa/h-mediators-build.sh
+++ tools/medusa/h-mediators-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build.sh -uJablii,ablii_development,api,intuit,intuit_dev,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,cluster_h,mm -ck
+./build.sh -uJablii,ablii_development,api,intuit,intuit_dev,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,treviso_api,treviso_api_development,cluster_h,mm -ck
