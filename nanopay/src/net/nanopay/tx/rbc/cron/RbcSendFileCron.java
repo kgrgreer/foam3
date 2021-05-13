ffd3e2da5da395ee7fe97e460ca6b7ddbd85e61c
Blake Green <blake@nanopay.net>
2021-05-13 11:03:58 -0400

    Added alarming to all crons in Intuit Configuration script (#12689)
    * Added alarming to all crons in Intuit Configuration script
    
    * Removed setActive when creating Alarms
    
    * Added more alarms to Rbc crons
    
    * Made transaction completed alarm more generic
    
    * Added new alarm to dao
    
    * Removed dao query before creating alarms
    
    * Removed BmoFormatUtil from RbcSendFileCron
    
    * Updated alarm names
    
    * Using new constructor to instatiate Alarms
    
    * Pegged foam
    
    * Removed BmoFormatUtil and setup Alarms in Intuit Config crons
    
    * Deactivating rbc send file alarm when file is successfully sent
    
    * Querying rbc send file alarm, if exists on successful file send, deactivate alarm
    
    * Added logger errors for all alarms thrown in Rbc crons
    
    * Using new Alarm constructor with severity in rbc crons
    
    * Pegged foam change with new Alarm constructor

--- nanopay/src/net/nanopay/tx/rbc/cron/RbcSendFileCron.java
+++ nanopay/src/net/nanopay/tx/rbc/cron/RbcSendFileCron.java
@@ -4,6 +4,7 @@ import foam.core.ContextAgent;
 import foam.core.X;
 import foam.dao.ArraySink;
 import foam.dao.DAO;
+import foam.log.LogLevel;
 import foam.mlang.MLang;
 import foam.nanos.alarming.Alarm;
 import foam.nanos.alarming.AlarmReason;
@@ -14,9 +15,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import net.nanopay.tx.rbc.RbcFileProcessor;
-import net.nanopay.tx.bmo.BmoFormatUtil;
 import net.nanopay.tx.cico.EFTFile;
 import net.nanopay.tx.cico.EFTFileStatus;
+import static foam.mlang.MLang.*;
 
 
 public class RbcSendFileCron implements ContextAgent {
@@ -29,31 +30,46 @@ public class RbcSendFileCron implements ContextAgent {
     /**
      * GET and send generated files
      */
-    DAO eftFileDAO = (DAO) x.get("eftFileDAO");
-    logger = new PrefixLogger(new String[] {"RBC"}, (Logger) x.get("logger"));
-
-    ArraySink sink = (ArraySink) eftFileDAO.where(
-      MLang.OR(
-        MLang.EQ(EFTFile.STATUS, EFTFileStatus.GENERATED),
-        MLang.EQ(EFTFile.STATUS, EFTFileStatus.FAILED)
-      )
-    ).select(new ArraySink());
-    List<EFTFile> files = (ArrayList<EFTFile>) sink.getArray();
-
-    for ( EFTFile file : files ) {
-      /* Send and verify file */
+      String alarmName = "RbcSendFileCron-Status";
+      DAO alarmDAO = (DAO) x.get("alarmDAO");
       try {
-        file = (EFTFile) file.fclone();
-        new RbcFileProcessor(x).send(file);
+        DAO eftFileDAO = (DAO) x.get("eftFileDAO");
+        logger = new PrefixLogger(new String[] {"RBC"}, (Logger) x.get("logger"));
+
+        ArraySink sink = (ArraySink) eftFileDAO.where(
+          MLang.OR(
+            MLang.EQ(EFTFile.STATUS, EFTFileStatus.GENERATED),
+            MLang.EQ(EFTFile.STATUS, EFTFileStatus.FAILED)
+          )
+        ).select(new ArraySink());
+        List<EFTFile> files = (ArrayList<EFTFile>) sink.getArray();
+
+        for ( EFTFile file : files ) {
+          /* Send and verify file */
+          try {
+            file = (EFTFile) file.fclone();
+            new RbcFileProcessor(x).send(file);
+            Alarm alarm = (Alarm) alarmDAO.find(EQ(Alarm.NAME, alarmName));
+            if ( alarm != null && alarm.getIsActive() ) {
+              alarm.setIsActive(false);
+              alarmDAO.put(alarm);
+            }
+          } catch ( Exception e ) {
+            Alarm alarm = new Alarm.Builder(x)
+              .setName(alarmName)
+              .setReason(AlarmReason.TIMEOUT)
+              .setNote("RbcSendFileCron send file failed Exception: " + e.getMessage())
+              .setSeverity(LogLevel.ERROR)
+              .build();
+            alarmDAO.put(alarm);
+            logger.error(alarmName, e);
+          }
+        }
       } catch ( Exception e ) {
-        logger.error("RBC send file failed. " + e.getMessage(), e);
-        ((DAO) x.get("alarmDAO")).put(new Alarm.Builder(x)
-          .setName("RBC send file failed")
-          .setReason(AlarmReason.TIMEOUT)
-          .setNote(e.getMessage())
-          .build());
-        BmoFormatUtil.sendEmail(x, "RBC EFT Error sending and verifying EFT File. " + e.getMessage(), e);
+        String note = "RbcSendFileCron Exception: " + e.getMessage();
+        Alarm alarm = new Alarm(alarmName, note, LogLevel.ERROR);
+        alarmDAO.put(alarm);
+        logger.error(alarmName, e);
       }
-    }
   }
 }
