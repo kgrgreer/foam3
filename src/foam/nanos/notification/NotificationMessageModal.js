62632c8ed9abbbe58aeaef01ea307e6bae9dd986
Jin Jung <jin@nanopay.net>
2021-05-14 00:36:50 -0400

    indent

--- src/foam/nanos/notification/NotificationMessageModal.js
+++ src/foam/nanos/notification/NotificationMessageModal.js
@@ -5,74 +5,73 @@
  */
 
 foam.CLASS({
-    package: 'foam.nanos.notification',
-    name: 'NotificationMessageModal',
-    extends: 'foam.u2.View',
-  
-    documentation: 'View for displaying notification message',
+  package: 'foam.nanos.notification',
+  name: 'NotificationMessageModal',
+  extends: 'foam.u2.View',
 
-    css: `
-      ^ {
-        background-color: #fff;
-        width: 60vw;
-        max-height: 80vh;
-        overflow-y: auto;
-      }
-      ^ .main {
-        padding: 8px 24px 24px;
-      }
-      ^ .bold-label {
-        font-size: 16px;
-        font-weight: bold;
-        padding-bottom: 8px;
-      } 
+  documentation: 'View for displaying notification message',
 
-      ^ .message {
-        white-space: pre-line;
-      } 
-    `,
+  css: `
+    ^ {
+      background-color: #fff;
+      width: 60vw;
+      max-height: 80vh;
+      overflow-y: auto;
+    }
+    ^ .main {
+      padding: 8px 24px 24px;
+    }
+    ^ .bold-label {
+      font-size: 16px;
+      font-weight: bold;
+      padding-bottom: 8px;
+    }
+    ^ .message {
+      white-space: pre-line;
+    } 
+  `,
 
-    requires: [
-      'foam.u2.layout.Rows',
-      'foam.u2.layout.Grid',
-      'foam.u2.ModalHeader'
-    ],
+  requires: [
+    'foam.u2.layout.Rows',
+    'foam.u2.layout.Grid',
+    'foam.u2.ModalHeader'
+  ],
 
-    messages: [
-      { name: 'NATIFICATION_MSG', message: 'Notification' },
-      { name: 'CREATED_MSG', message: 'Created' },
-      { name: 'MESSAGE_MSG', message: 'Message' }
-    ],
-  
-    properties: [
-      'data',
-      'created',
-      'description'
-    ],
-  
-    methods: [
-      function initE() {
-        this.SUPER();
+  messages: [
+    { name: 'NATIFICATION_MSG', message: 'Notification' },
+    { name: 'CREATED_MSG', message: 'Created' },
+    { name: 'MESSAGE_MSG', message: 'Message' }
+  ],
 
-        this.created = this.data.created.toUTCString();
-        this.description = this.data.body;
-        
-        this
-        .addClass(this.myClass())
-        .tag(this.ModalHeader.create({
-          title: this.NATIFICATION_MSG
-        }))
-        .start()
-          .addClass('main')
-            .start(this.Rows).style({padding: '24px 0px'})
-              .start().addClass('bold-label').add(this.CREATED_MSG).end()
-              .start().add(this.created).end()
-            .end()
-            .start(this.Rows).style({padding: '24px 0px'})
-              .start().addClass('bold-label').add(this.MESSAGE_MSG).end()
-              .start().addClass('message').add(this.description).end()
-            .end()
-        .end();
-      }
-    ]
-  });
+  properties: [
+    'data',
+    'created',
+    'description'
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+
+      this.created = this.data.created.toUTCString();
+      this.description = this.data.body;
+      
+      this
+      .addClass(this.myClass())
+      .tag(this.ModalHeader.create({
+        title: this.NATIFICATION_MSG
+      }))
+      .start()
+        .addClass('main')
+          .start(this.Rows).style({padding: '24px 0px'})
+            .start().addClass('bold-label').add(this.CREATED_MSG).end()
+            .start().add(this.created).end()
+          .end()
+          .start(this.Rows).style({padding: '24px 0px'})
+            .start().addClass('bold-label').add(this.MESSAGE_MSG).end()
+            .start().addClass('message').add(this.description).end()
+          .end()
+      .end();
+    }
+  ]
+});
