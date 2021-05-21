e0640c238cdce7d43df489b6e8e7f7516d934501
Nicholas Prat <nprat@protonmail.com>
2021-05-21 09:45:18 -0400

    Updated RBC EFT generation script

--- nanopay/src/net/nanopay/partner/rbc/test/ChecklessRbcGenerateFileCron.java
+++ nanopay/src/net/nanopay/partner/rbc/test/ChecklessRbcGenerateFileCron.java
@@ -1,4 +1,21 @@
-package net.nanopay.script;
+/**
+ * NANOPAY CONFIDENTIAL
+ *
+ * [2021] nanopay Corporation
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of nanopay Corporation.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to nanopay Corporation
+ * and may be covered by Canadian and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from nanopay Corporation.
+ */
+
+package net.nanopay.partner.rbc.test;
 
 import foam.core.X;
 import foam.dao.ArraySink;
@@ -40,7 +57,7 @@ public class ChecklessRbcGenerateFileCron extends RbcGenerateFileCron {
     ).select(new ArraySink());
     ArrayList<Transaction> transactions = (ArrayList<Transaction>) sink.getArray();
 
-    generate(x, transactions);
+    generate(x, transactions, spid);
   }
 
 }
