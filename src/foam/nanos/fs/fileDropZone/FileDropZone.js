472e3f1c6620a1b8d92529bd17af843b522cdbcd
Tala <tala@MacBook-Pro-New-4.local>
2021-05-21 11:50:01 -0400

    Fixed indentation

--- src/foam/nanos/fs/fileDropZone/FileDropZone.js
+++ src/foam/nanos/fs/fileDropZone/FileDropZone.js
@@ -91,7 +91,7 @@ foam.CLASS({
     { name: 'LABEL_BROWSE',        message: 'Select from your device' },
     { name: 'LABEL_SUPPORTED',     message: 'Supported file types:' },
     { name: 'LABEL_MAX_SIZE',      message: 'Max size:' },
-    { name: 'ERROR_FILE_TITLE',     message: 'Error' },
+    { name: 'ERROR_FILE_TITLE',    message: 'Error' },
     { name: 'ERROR_FILE_TYPE',     message: 'Invalid file type' },
     { name: 'ERROR_FILE_SIZE',     message: 'File size exceeds 15MB' }
   ],
