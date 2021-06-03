984c50613a3f04d92b71d8f7255136b12113d013
Joel Hughes <joel@nanopay.net>
2021-06-03 12:19:43 -0400

    remove afex and move intuit_dev

--- tools/medusa/ca-central-development-mediators-build.sh
+++ tools/medusa/ca-central-development-mediators-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build.sh -uJablii,ablii_development,api,intuit,intuit_dev,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,treviso_api,treviso_api_development,afex-release,ca-development,cluster-ca-central-development,mm -ck
+./build.sh -uJablii,ablii_development,api,intuit,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,treviso_api,treviso_api_development,ca-development,intuit_dev,cluster-ca-central-development,mm -ck
