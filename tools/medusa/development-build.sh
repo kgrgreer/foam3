b80486e2a44dc0a0dd4d5e67d1632d85358675ed
Joel Hughes <joel@nanopay.net>
2021-06-03 12:22:26 -0400

    move intuit_dev to end so mock service used.

--- tools/medusa/development-build.sh
+++ tools/medusa/development-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build.sh -uJablii,ablii_development,api,intuit,intuit_dev,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,ca-development -c
+./build.sh -uJablii,ablii_development,api,intuit,intuit_development,br,bepay,bepay_development,bpp,bpp_development,treviso,treviso_development,intuit_dev,ca-development -c
