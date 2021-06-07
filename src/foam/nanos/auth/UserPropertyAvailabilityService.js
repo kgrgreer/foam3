b74c525b4addb6abf6a6c5eb89faf87d2c330244
Christine <wonhwalee52@gmail.com>
2021-06-07 17:17:21 -0400

    creating account update

--- src/foam/nanos/auth/UserPropertyAvailabilityService.js
+++ src/foam/nanos/auth/UserPropertyAvailabilityService.js
@@ -8,7 +8,7 @@ foam.CLASS({
   package: 'foam.nanos.auth',
   name: 'UserPropertyAvailabilityService',
 
-  documentation: 
+  documentation:
     `This is a service that checks whether a user with matching values for a unique given property
     (restricted use for Username and Email) already exists in the system. Thus, this service allows the client to check
     the availability of these property values.
@@ -26,8 +26,8 @@ foam.CLASS({
     'foam.dao.ArraySink',
     'foam.dao.DAO',
     'foam.mlang.MLang',
-    'foam.mlang.MLang.*',
-    'foam.nanos.auth.User'
+    'foam.nanos.auth.User',
+    'foam.nanos.theme.Theme'
   ],
 
   methods: [
@@ -40,13 +40,20 @@ foam.CLASS({
           throw new AuthorizationException();
         }
 
-        ArraySink select = (ArraySink) userDAO.inX(x).where(MLang.EQ(targetProperty.equals("userName") ? User.USER_NAME : User.EMAIL, value)).select(new ArraySink());
-        
+        ArraySink select = (ArraySink) userDAO.inX(x).where(
+          MLang.AND(
+            MLang.EQ(targetProperty.equals("userName") ? User.USER_NAME : User.EMAIL, value),
+            // Find a user within the same spid.
+            // Support having users with the same email or user name in different spids.
+            MLang.EQ(User.SPID, ((Theme) x.get("theme")).getSpid())
+          ))
+          .select(new ArraySink());
+
         if ( select.getArray().size() != 0 ) {
           return false;
         }
         return true;
       `
-    }  
+    }
   ]
 });
