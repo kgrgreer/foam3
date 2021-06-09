c3e9ed54efe95542d839ba7e525b1e1e52a4710e
Christine <wonhwalee52@gmail.com>
2021-06-09 11:55:43 -0400

    update login

--- src/foam/nanos/auth/ThemeAuthService.js
+++ src/foam/nanos/auth/ThemeAuthService.js
@@ -32,13 +32,6 @@ foam.CLASS({
   methods: [
     {
       name: 'login',
-      javaCode: `
-        return login_(x, identifier, password);
-      `
-    },
-    {
-      name: 'login_',
-      documentation: 'Helper logic function to reduce code duplication.',
       type: 'User',
       args: [
         {
@@ -58,18 +51,16 @@ foam.CLASS({
         AuthService auth = (AuthService) x.get("auth");
         User user = super.login(x, identifier, password);
 
-        // Get theme's spid and user's spid
         Theme theme = ((Theme) x.get("theme"));
-        String themeSpid = theme.getSpid();
-        String userSpid = user.getSpid();
 
         // Check if theme's spid and user's spid matched.
         // if matched pass, else throw an error.
         // e.g., throws error: userSpid: "treviso", themeSpid: "intuit"
         if (
           user != null &&
-          ! userSpid.equals(themeSpid) &&
-          ! userSpid.equals(getSuperSpid())
+          theme != null &&
+          ! user.getSpid().equals(theme.getSpid()) &&
+          ! user.getSpid().equals(getSuperSpid())
         ) {
           auth.logout(x);
           throw new AuthenticationException("User not found");
