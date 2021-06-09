00bcff149ad2d07d83fa450b5481fc34acf69881
noodlemoodle <qiyzhang@icloud.com>
2021-06-09 10:52:40 -0400

    fix

--- src/foam/nanos/auth/ResetPassword.js
+++ src/foam/nanos/auth/ResetPassword.js
@@ -26,7 +26,7 @@ foam.CLASS({
   messages: [
     { name: 'RESET_PASSWORD_TITLE', message: 'Reset your password' },
     { name: 'RESET_PASSWORD_SUBTITLE', message: 'Create a new password for your account' },
-    { name: 'SUCCESS_MSG_TITLE', message: 'Password Updated' },
+    // { name: 'SUCCESS_MSG_TITLE', message: 'Password Updated' },
     { name: 'SUCCESS_MSG', message: 'Your password was successfully updated' },
     { name: 'SUCCESS_MSG_TITLE', message: 'Success' },
     { name: 'PASSWORD_LENGTH_10_ERROR', message: 'Password must be at least 10 characters' },
