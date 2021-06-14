1f945a3149a0f2e73e1054e121e907e342d746b5
Scott Head <scott@nanopay.net>
2021-06-14 14:38:43 -0400

    Fixing error

--- src/foam/nanos/auth/Address.js
+++ src/foam/nanos/auth/Address.js
@@ -1072,7 +1072,7 @@ foam.CLASS({
         String regionCode = "";
         Region region = findRegionId(x);
         if ( region != null ) {
-          regoinCode = getRegionCode()
+          regoinCode = region.getRegionCode();
         }
 
         return ! SafetyUtil.isEmpty(regionCode) ?
