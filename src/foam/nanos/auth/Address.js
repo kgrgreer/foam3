fe5edb010129ba431ac1cba162dea4ab6441b32d
Scott Head <scott@nanopay.net>
2021-06-14 14:29:24 -0400

    Adding helper method to retrieve region code

--- src/foam/nanos/auth/Address.js
+++ src/foam/nanos/auth/Address.js
@@ -1051,6 +1051,22 @@ foam.CLASS({
       javaCode: `
       return getShortAddress();
      `
+    },
+    {
+      name: 'retrieveRegionCode',
+      type: 'String',
+      args: [ { name: 'x', type: 'Context' } ],
+      javaCode: `
+        String regionCode = "";
+        Region region = findRegionId(x);
+        if ( region != null ) {
+          regoinCode = getRegionCode()
+        }
+
+        return ! SafetyUtil.isEmpty(regionCode) ?
+          regionCode :
+          getRegionId();
+      `
     }
   ]
 });
