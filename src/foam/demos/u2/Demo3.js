3a9029618796282c75101c861a0feb16bad67b69
Kevin Greer <kgrgreer@gmail.com>
2021-06-25 07:27:52 -0400

    Update U3 demo.

--- src/foam/demos/u2/Demo3.js
+++ src/foam/demos/u2/Demo3.js
@@ -1,11 +1,11 @@
-foam.u2.Element.create().add('children').write(document);
-foam.u2.Element.create().write(document).add('children');
-foam.u2.Element.create({nodeName: 'b'}).add('children').write(document);
-foam.u2.Element.create({nodeName: 'b'}).write(document).add('children');
-foam.u2.Element.create().style({color: 'red'}).add('children').write(document);
-foam.u2.Element.create().write(document).style({color: 'red'}).add('children');
-foam.u2.Element.create().on('click', () => console.log('clicked')).add('clickme').write(document);
-foam.u2.Element.create().write(document).on('click', () => console.log('clicked')).add('clickme');
+foam.u2.Element.create().add('children').write();
+foam.u2.Element.create().write().add('children');
+foam.u2.Element.create({nodeName: 'b'}).add('children').write();
+foam.u2.Element.create({nodeName: 'b'}).write().add('children');
+foam.u2.Element.create().style({color: 'red'}).add('children').write();
+foam.u2.Element.create().write().style({color: 'red'}).add('children');
+foam.u2.Element.create().on('click', () => console.log('clicked')).add('clickme').write();
+foam.u2.Element.create().write().on('click', () => console.log('clicked')).add('clickme');
 
 foam.CLASS({
   name: 'Test',
@@ -15,10 +15,18 @@ foam.CLASS({
 
   methods: [
     function render() {
-      this.add('child1', 'child2').br().start('b').add('bold').end().br();
+      this.add('child1', 'child2').br().start('b').add('bold').end().br().entity('lt').add('>').add('end');
     }
   ]
 });
 
-var t = Test.create().write(document);
+var t = Test.create().write();
 t.add('more children');
+
+var v1 = foam.u2.TextField.create({data: 'data'}).write();
+var v2 = foam.u2.TextField.create({data: 'data'}).write();
+v1.data$ = v2.data$;
+
+var v1 = foam.u2.TextField.create({data: 'data2', onKey: true}).write();
+var v2 = foam.u2.TextField.create({data: 'data2', onKey: true}).write();
+v1.data$ = v2.data$;
