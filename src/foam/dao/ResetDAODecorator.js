5febc8570aa9530f429896013f7f8bb362a7e5c9
Arthur Pavlovs <arthur@nanopay.net>
2021-06-30 18:04:48 -0400

    add ResetDAODecorator

---
+++ src/foam/dao/ResetDAODecorator.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'ResetDAODecorator',
+  extends: 'foam.dao.ProxyDAO',
+
+  documentation: `DAO decorator that resets the given dao and clears it's cache after put.`,
+  
+  requires: [
+    'foam.dao.AbstractDAO',
+    'foam.dao.TTLSelectCachingDAO'
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'daoKey'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'put_',
+      code: function(x, obj) {
+        return this.delegate.put_(x, obj).then(() => {
+          if ( this.daoKey ) {
+            var dao = this.__subContext__[this.daoKey];
+            dao.cmd(this.TTLSelectCachingDAO.PURGE);
+            dao.cmd(this.AbstractDAO.RESET_CMD);
+          }
+        });
+      }
+    }
+  ]
+});
