8f54f7d253c62af06f34a4a1ce154c5bb2cca748
Christine <wonhwalee52@gmail.com>
2021-07-08 13:37:41 -0400

    added colour for Status

--- src/foam/nanos/auth/AgentJunctionStatus.js
+++ src/foam/nanos/auth/AgentJunctionStatus.js
@@ -12,16 +12,22 @@ foam.ENUM({
     {
       name: 'ACTIVE',
       label: 'Active',
+      color: '04612',
+      background: 'EEF7ED',
       documentation: 'Junction is satisfied and agent may act as entity.'
     },
     {
       name: 'DISABLED',
       label: 'Disabled',
+      color: 'A61414',
+      background: 'FFE9E7',
       documentation: 'Junction is unsatisfied disabling agent from acting as entity.'
     },
     {
       name: 'INVITED',
       label: 'Invited',
+      color: '865300',
+      background: 'FFF3C1',
       documentation: 'The person has been invited to join the business.'
     }
   ]
