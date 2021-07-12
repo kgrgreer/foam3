99edbeb7361b70b307f31adfe01f054f331fa21e
noodlemoodle <qiyzhang@icloud.com>
2021-07-12 12:34:13 -0400

    fix multiple data update events

--- src/foam/u2/FloatView.js
+++ src/foam/u2/FloatView.js
@@ -74,8 +74,10 @@ foam.CLASS({
       // to ensure it's formatted properly.
       this.on('blur', function () {
         var value = self.dataToText(data.get());
+        preventFeedback = true;
         view.set('0');
         view.set(value);
+        preventFeedback = false;
       });
 
       var preventFeedback = false;
