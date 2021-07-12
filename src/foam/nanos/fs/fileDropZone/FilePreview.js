7be36d1c171962081863e7e11a9bd45fe9622a7d
Joel Hughes <joel@nanopay.net>
2021-07-12 17:00:19 -0400

    getData is async

--- src/foam/nanos/fs/fileDropZone/FilePreview.js
+++ src/foam/nanos/fs/fileDropZone/FilePreview.js
@@ -105,7 +105,8 @@ foam.CLASS({
         return;
       }
 
-      let d = this.data[pos].data;
+      // getData File method is async, so we need to wait for it to return data before we can show it
+      let d = await this.data[pos].data;
       // If file is stored as a dataString, actual file is already on client side. Otherwise, actual file can be retrieved from server from File.address
       if ( ! d ) {
         url = this.data[pos].address;
