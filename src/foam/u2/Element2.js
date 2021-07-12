e89aa4a4395d30e09d9fad64fd37f6fdebeb21ee
Kevin Greer <kgrgreer@gmail.com>
2021-07-12 09:16:17 -0400

    U3: Add SVG support. Fix nbsp().

--- src/foam/u2/Element2.js
+++ src/foam/u2/Element2.js
@@ -165,7 +165,9 @@ foam.CLASS({
     {
       name: 'element_',
       factory: function() {
-        return this.document.createTextNode('&' + this.name + ';');
+        var ret = this.document.createTextNode('&' + this.name + ';');
+        ret.innerHTML = '&' + this.name + '; ';
+        return ret;
       }
     }
   ]
@@ -551,6 +553,10 @@ foam.CLASS({
     'translationService?'
   ],
 
+  exports: [
+    'isSVG_ as isSVG'
+  ],
+
   implements: [
     'foam.mlang.Expressions'
   ],
@@ -609,8 +615,10 @@ foam.CLASS({
   properties: [
     {
       name: 'element_',
-      expression: function(nodeName) {
-        return this.document.createElement(this.nodeName);
+      factory: function() {
+        return this.isSVG_ ?
+          this.document.createElementNS("http://www.w3.org/2000/svg", this.nodeName) :
+          this.document.createElement(this.nodeName);
       }
     },
     {
@@ -672,8 +680,14 @@ foam.CLASS({
     },
     {
       name: 'nodeName',
-      adapt: function(_, v) { return foam.String.toUpperCase(v); },
-      value: 'DIV'
+      adapt: function(_, v) { return foam.String.toLowerCase(v); },
+      value: 'div'
+    },
+    {
+      name: 'isSVG_',
+      factory: function() {
+        return this.nodeName === 'svg' || this.__context__.isSVG;
+      }
     },
     {
       name: 'attributeMap',
@@ -767,7 +781,7 @@ foam.CLASS({
       this.initKeyboardShortcuts();
       this.render();
       if ( this.initE != foam.u2.Element.prototype.initE ) {
-        console.warn('Deprecated use of Element.initE(). Use render instead: ', this.cls_.name);
+        // console.warn('Deprecated use of Element.initE(). Use render instead: ', this.cls_.name);
         this.initE();
       }
       this.add = foam.u2.Element.prototype.add;
@@ -1193,7 +1207,7 @@ foam.CLASS({
     },
 
     function setNodeName(name) {
-      console.warn('Deprecated use of setNodeName. Set the nodeName property instead.');
+      console.warn('Deprecated use of setNodeName. Set the nodeName property instead. CLASS: ', this.cls_.name);
       this.nodeName = name;
       return this;
     },
@@ -1212,7 +1226,7 @@ foam.CLASS({
     },
 
     function nbsp() {
-      return this.entity('nbsp');
+      return this.add('\xa0');
     },
 
     function addClass(cls) { /* Slot | String */
@@ -1653,8 +1667,7 @@ foam.CLASS({
     },
 
     function addEventListener_(topic, listener, opt_args) {
-      var el = this.el_();
-      el && el.addEventListener(topic, listener, opt_args || false);
+      this.element_.addEventListener(topic, listener, opt_args || false);
     },
 
     function removeEventListener_(topic, listener) {
@@ -1712,7 +1725,7 @@ foam.CLASS({
       class: 'foam.core.ContextMethod',
       name: 'E',
       code: function E(ctx, opt_nodeName) {
-        var nodeName = (opt_nodeName || 'DIV').toUpperCase();
+        var nodeName = (opt_nodeName || 'div').toLowerCase();
 
         // Check if a class has been registered for the specified nodeName
         return (ctx.elementForName(nodeName) || foam.u2.Element).
