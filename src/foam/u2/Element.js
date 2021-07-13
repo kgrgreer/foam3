5a276bbd29bb4bb2fe31f75f8f911e6c9ca629bf
Kevin Greer <kgrgreer@gmail.com>
2021-07-12 21:41:59 -0400

    U3: Implement HTMLView.

--- src/foam/u2/Element.js
+++ src/foam/u2/Element.js
@@ -2985,3 +2985,72 @@ foam.CLASS({
     }
   ]
 });
+
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLValidator',
+  extends: 'foam.u2.DefaultValidator',
+
+  axioms: [ foam.pattern.Singleton.create() ],
+
+  methods: [
+    function sanitizeText(text) {
+      // TODO: validate text
+      return text;
+    }
+  ]
+});
+
+
+// An Element which does not escape HTML content
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLElement',
+  extends: 'foam.u2.Element',
+
+  requires: [ 'foam.u2.HTMLValidator' ],
+
+  exports: [ 'validator as elementValidator' ],
+
+  properties: [
+    {
+      class: 'Proxy',
+      of: 'foam.u2.DefaultValidator',
+      name: 'validator',
+      factory: function() {
+        // Note that HTMLValidator is a singleton so only one instance of
+        // HTMLValidator should ever be created here.
+        return this.HTMLValidator.create()
+      }
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLView',
+//  extends: 'foam.u2.tag.TextArea',
+  extends: 'foam.u2.HTMLElement',
+
+  documentation: 'View for safely displaying HTML content.',
+
+  css: '^ { padding: 6px 0; }',
+
+  properties: [
+    {
+      name: 'data',
+      attribute: true
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+      this.addClass();
+
+      this.add(this.data$);
+    }
+  ]
+});
--- src/foam/u2/Element2.js
+++ src/foam/u2/Element2.js
@@ -2492,3 +2492,68 @@ foam.CLASS({
     }
   ]
 });
+
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLValidator',
+  extends: 'foam.u2.DefaultValidator',
+
+  axioms: [ foam.pattern.Singleton.create() ],
+
+  methods: [
+    function sanitizeText(text) {
+      // TODO: validate text
+      return text;
+    }
+  ]
+});
+
+
+// An Element which does not escape HTML content
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLElement',
+  extends: 'foam.u2.Element',
+
+  requires: [ 'foam.u2.HTMLValidator' ],
+
+  exports: [ 'validator as elementValidator' ],
+});
+
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'HTMLView',
+  extends: 'foam.u2.HTMLElement',
+
+  documentation: 'View for safely displaying HTML content.',
+
+  css: '^ { padding: 6px 0; }',
+
+  properties: [
+    {
+      name: 'data',
+      attribute: true
+    }
+  ],
+
+  methods: [
+    function render() {
+      this.addClass();
+      this.add('htmlview');
+      this.update();
+      this.onDetach(this.data$.sub(this.update()));
+    }
+  ],
+
+  listeners: [
+    {
+      name: 'update',
+      isFramed: true,
+      code: function() {
+        this.element_.innerHTML = this.data;
+      }
+    }
+  ]
+});
