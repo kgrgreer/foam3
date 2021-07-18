64f768d9ad4b3dad24e40ae6508bb0d9a2307cac
xuerongNanopay <xuerong@nanopay.net>
2021-07-18 15:27:09 -0400

    format

--- src/foam/box/sf/RetryStrategy.js
+++ src/foam/box/sf/RetryStrategy.js
@@ -16,36 +16,36 @@
 */
 
 foam.ENUM({
-	package: 'foam.box.sf',
-	name: 'RetryStrategy',
-	
-	values: [
-		{
-			name: 'CONST_FOUR_SECOND',
-			ordinal: 0,
-		},
-		{
-			name: 'INCREMENT_ONE_SECOND',
-			ordinal: 1,
-		}
-	],
-
-	axioms: [
+  package: 'foam.box.sf',
+  name: 'RetryStrategy',
+  
+  values: [
+    {
+      name: 'CONST_FOUR_SECOND',
+      ordinal: 0,
+    },
+    {
+      name: 'INCREMENT_ONE_SECOND',
+      ordinal: 1,
+    }
+  ],
+  
+  axioms: [
     {
       name: 'javaExtras',
       buildJavaClass: function(cls) {
         cls.extras.push(foam.java.Code.create({
           data: `
-						public long next(long cur) {
-							switch ( this ) {
-								case CONST_FOUR_SECOND:
-									return 4000;
-								case INCREMENT_ONE_SECOND:
-									return cur + 1000;
-								default:
-									throw new RuntimeException("unsupport operation");
-							}
-						}
+          public long next(long cur) {
+            switch ( this ) {
+              case CONST_FOUR_SECOND:
+              return 4000;
+              case INCREMENT_ONE_SECOND:
+              return cur + 1000;
+              default:
+              throw new RuntimeException("unsupport operation");
+            }
+          }
           `
         }));
       }
