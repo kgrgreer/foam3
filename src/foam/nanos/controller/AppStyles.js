98ca062bebf5a5d4b66540ecef638498ffd4a926
noodlemoodle <qiyzhang@icloud.com>
2021-07-19 16:29:10 -0400

    relatively sized typography

--- src/foam/nanos/controller/AppStyles.js
+++ src/foam/nanos/controller/AppStyles.js
@@ -14,8 +14,8 @@ foam.CLASS({
   css: `
     body {
       font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      letter-spacing: 0.2px;
+      font-size: 1rem;
+      letter-spacing: 0.014em;
       color: /*%BLACK%*/ #1E1F21;
       background: /*%GREY5%*/ #f5f7fa;
       margin: 0;
@@ -25,85 +25,85 @@ foam.CLASS({
     .h100{
       font-style: normal;
       font-weight: 700;
-      font-size: 35px;
-      line-height: 40px;
+      font-size: 2.5rem;
+      line-height: 1.14;
       margin: 0;
     }
     .h200{
       font-style: normal;
       font-weight: 600;
-      font-size: 29px;
-      line-height: 32px;
+      font-size: 2.07rem;
+      line-height: 1.1;
       margin: 0;
     }
     .h300{
       font-style: normal;
       font-weight: 600;
-      font-size: 24px;
-      line-height: 28px;
+      font-size: 1.71rem;
+      line-height: 1.17;
       margin: 0;
     }
     .h400{
       font-style: normal;
       font-weight: 600;
-      font-size: 20px;
-      line-height: 24px;
+      font-size: 1.43rem;
+      line-height: 1.2;
       margin: 0;
     }
     .h500{
       font-style: normal;
       font-weight: 600;
-      font-size: 16px;
-      line-height: 20px;
+      font-size: 1.14rem;
+      line-height: 1.25;
       margin: 0;
     }
     .h600{
       font-style: normal;
       font-weight: 600;
-      font-size: 14px;
-      line-height: 18px;
+      font-size: 1rem;
+      line-height: 1.29;
       margin: 0;
     }
     .p{
       font-style: normal;
       font-weight: normal;
-      font-size: 14px;
-      line-height: 24px;
+      font-size: 1rem;
+      line-height: 1.71;
       margin: 0;
     }
     .p-lg{
-      font-size: 18px;
+      font-size: 1.29rem;
       font-style: normal;
       font-weight: normal;
-      line-height: 32px;
+      line-height: 1.78;
       margin: 0;
     }
     .p-semiBold{
-      font-size: 14px;
+      font-size: 1rem;
       font-style: normal;
       font-weight: 600;
-      line-height: 24px;
+      line-height: 1.71;
       margin: 0;
     }
     .p-bold{
       font-style: normal;
       font-weight: 700;
-      font-size: 14px;
-      line-height: 24px;
+      font-size: 1rem;
+      line-height: 1.71;
       margin: 0;
     }
     .p-legal{
       font-style: normal;
       font-weight: 500;
-      font-size: 12px;
-      line-height: 14px;
+      font-size: 0.86rem;
+      line-height: 1.17;
       margin: 0;
     }
     .p-label{
       font-style: normal;
       font-weight: 600;
-      font-size: 12px;
-      line-height: 14px;
+      font-size: 0.86rem;
+      line-height: 1.17;
       margin: 0;
     }
     
@@ -140,12 +140,12 @@ foam.CLASS({
     ^ .generic-status {
       display: inline-block;
       font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-      font-size: 12px;
+      font-size: 0.86rem;
       font-weight: normal;
       font-style: normal;
       font-stretch: normal;
       line-height: 1.67;
-      letter-spacing: 0.2px;
+      letter-spacing: 0.017em;
       text-align: center;
       color: #ffffff;
     }
