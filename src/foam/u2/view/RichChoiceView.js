8e11d02952afe263ce0397b9d417eae9b2a077a9
noodlemoodle <qiyzhang@icloud.com>
2021-07-20 18:56:14 -0400

    adjust chevron fontsize and set a width

--- src/foam/u2/view/RichChoiceView.js
+++ src/foam/u2/view/RichChoiceView.js
@@ -205,9 +205,12 @@ foam.CLASS({
       border-color: /*%PRIMARY3%*/ #406dea;
     }
 
+    ^chevron {
+      width: 20%;
+    }
     ^chevron::before {
       content: '▾';
-      font-size: 1.5em;
+      font-size: 1.14em;
       padding-left: 4px;
     }
 
