4b727ca694601822414e5045f91a66580335a96f
noodlemoodle <qiyzhang@icloud.com>
2021-07-20 18:10:38 -0400

    add header to control view width

--- src/foam/nanos/servlet/VirtualHostRoutingServlet.js
+++ src/foam/nanos/servlet/VirtualHostRoutingServlet.js
@@ -88,6 +88,7 @@ foam.CLASS({
       Boolean    customFontsFailed   = false;
 
       out.println("<meta charset=\\"utf-8\\"/>");
+      out.println("<meta name=\\"viewport\\" content=\\"width=device-width initial-scale=1.0\\" />");
       out.print("<title>");
       out.print(theme.getAppName());
       out.println("</title>");
