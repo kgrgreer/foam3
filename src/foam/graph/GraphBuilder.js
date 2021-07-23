bc7e31ae4e69c278b9941698623b4070bd042ac2
Danny Tharma <daniel@nanopay.net>
2021-07-23 15:49:46 -0400

    Added composite relationship recursion in GraphBuilder

--- src/foam/graph/GraphBuilder.js
+++ src/foam/graph/GraphBuilder.js
@@ -35,13 +35,13 @@ foam.CLASS({
       }
 
       var relationshipPromises = compositeRelationship.getForwardNames().map(
-        forwardName => this.fromRelationship(rootObject, forwardName, noRootAdd, true)
+        forwardName => this.fromRelationship(rootObject, forwardName, noRootAdd, compositeRelationship )
       );
 
       return Promise.all(relationshipPromises)
     },
     function fromRelationship(
-      rootObject, relationshipKey, noRootAdd
+      rootObject, relationshipKey, noRootAdd, compositeRelationship
     ) {
       // Add graph node (with no relations yet)
       if ( ! this.data[rootObject.id] ) {
@@ -60,7 +60,11 @@ foam.CLASS({
         .select().then(r => Promise.all(r.array.map(o => {
           parent.forwardLinks = [...parent.forwardLinks, o.id];
           // Add child and its children (recursively)
-          return this.fromRelationship(o, relationshipKey, true).then(() => {
+          var fromPromise = compositeRelationship 
+            ? this.fromCompositeRelationship(o, compositeRelationship, true)
+            : this.fromRelationship(o, relationshipKey, true);
+
+          return fromPromise.then(() => {
             // Add inverse links before resolving the promise
             this.data[o.id].inverseLinks =
               [...this.data[o.id].inverseLinks, rootObject.id];
