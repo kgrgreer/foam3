9c48c92180f9f43e56388985bc03215af079bf44
Mykola Kolombet <mykola@nanopay.net>
2021-07-26 13:26:51 -0400

    work in progress

--- src/foam/nanos/fs/FileCapabilityLabelDAODecorator.js
+++ src/foam/nanos/fs/FileCapabilityLabelDAODecorator.js
@@ -9,12 +9,28 @@ foam.CLASS({
   name: 'FileCapabilityLabelDAODecorator',
   extends: 'foam.dao.ProxyDAO',
   documentation: 'Decorator to add labels from capability to file',
+  javaImports: [
+    'foam.nanos.crunch.UserCapabilityJunction',
+    'net.nanopay.crunch.document.Document',
+    'foam.nanos.fs.File',
+    'foam.dao.DAO'
+  ],
 
   methods: [
     {
       name: 'put_',
       javaCode: `
-        int a = 8;
+        UserCapabilityJunction ucj = (UserCapabilityJunction) obj;
+        DAO fileDAO = (DAO) x.get("fileDAO");
+        if ( ucj.getData() instanceof Document ) {
+          Document document = (Document) ucj.getData();
+          String[] labels = document.getCapability().getLabels();
+          for ( File file: document.getDocuments() ) {
+            File f = (File) fileDAO.find(file.getId());
+            f.setLabels(labels);
+            f = (File) fileDAO.put(f);
+          }
+        }
         return super.put_(x, obj);
       `
     }
