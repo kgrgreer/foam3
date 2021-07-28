24ac479b5edfbc90b8c60c0d404492e80f047257
Joel Hughes <joel@nanopay.net>
2021-07-28 17:57:35 -0400

    Add Info web agent to dump cluster and replay info. Revert health to just up/down/maint/failed.

---
+++ src/foam/nanos/medusa/MedusaInfoWebAgent.java
@@ -0,0 +1,72 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.nanos.medusa;
+
+import foam.core.X;
+import foam.nanos.http.WebAgent;
+import foam.nanos.http.Format;
+import foam.nanos.http.HttpParameters;
+import foam.nanos.logger.Logger;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import foam.lib.json.OutputterMode;
+import foam.lib.xml.Outputter;
+
+/**
+   Report Medusa clusterConfig and replayingInfo.
+ */
+public class MedusaInfoWebAgent
+  implements WebAgent
+{
+  @Override
+  public void execute(X x) {
+    PrintWriter         out      = x.get(PrintWriter.class);
+    HttpServletResponse response = x.get(HttpServletResponse.class);
+    HttpParameters      p       = x.get(HttpParameters.class);
+    Format              format  = (Format) p.get(Format.class);
+    Logger              logger  = (Logger) x.get("logger");
+
+    logger.info(this.getClass().getSimpleName(), "format", format);
+    response.setContentType("text/plain");
+
+    ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+
+    if ( support != null ) {
+      response.setStatus(HttpServletResponse.SC_OK);
+      ClusterConfig config = (ClusterConfig) ((foam.dao.DAO) x.get("clusterConfigDAO")).find(support.getConfigId()).fclone();
+      ReplayingInfo info = config.getReplayingInfo();
+
+      config.SESSION_ID.clear(config);
+      info.REPLAY_NODES.clear(info);
+      config.setReplayingInfo(info);
+
+      if ( format == Format.JSON ) {
+        foam.lib.json.Outputter outputterJson = new foam.lib.json.Outputter(x)
+          .setPropertyPredicate(
+                                new foam.lib.AndPropertyPredicate(x,
+                                                                  new foam.lib.PropertyPredicate[] {
+                                                                    new foam.lib.ExternalPropertyPredicate(),
+                                                                    new foam.lib.NetworkPropertyPredicate(),
+                                                                    new foam.lib.PermissionedPropertyPredicate()}));
+
+        outputterJson.setOutputDefaultValues(true);
+        outputterJson.setOutputClassNames(true);
+        outputterJson.setMultiLine(true);
+
+        outputterJson.output(config);
+
+        // Output the formatted data
+        out.println(outputterJson.toString());
+      } else { // if( format == Format.XML ) {
+        Outputter outputter = new Outputter(out, OutputterMode.NETWORK);
+        out.println(outputter.stringify(config));
+      }
+    } else {
+      response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+    }
+  }
+}
