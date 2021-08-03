ef2c38860c270479438d0a84d39e876560118a74
nanokristina <penzital888@gmail.com>
2021-08-03 17:42:55 -0400

    fix test

--- src/foam/nanos/dig/SugarWebAgent.java
+++ src/foam/nanos/dig/SugarWebAgent.java
@@ -149,9 +149,8 @@ public class SugarWebAgent
           }
           executeMethod(x, resp, out, class_, serviceName, methodName, paramTypes, arglist);
         }
-        if ( ! methodFound ) throw new RuntimeException("Invalid method: " + methodName);
       }
-
+      if ( ! methodFound ) throw new RuntimeException("Invalid method: " + methodName);
     } catch (Exception e) {
       DigUtil.outputException(x, new GeneralException(e.toString()), Format.JSON);
       pm.error(x, e.getMessage());
