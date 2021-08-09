4486dce1df670d29b06edb72b832d39c21b84624
Arthur Pavlovs <arthur@nanopay.net>
2021-08-09 13:41:23 -0400

    rename user group permission string

--- src/foam/nanos/auth/User.js
+++ src/foam/nanos/auth/User.js
@@ -698,7 +698,7 @@ foam.CLASS({
 
         if ( ! findSelf &&
              ! auth.check(x, "user.read." + this.getId()) &&
-             ! auth.check(x, "user.read.byGroup." + this.getGroup())
+             ! auth.check(x, "user.readByGroup." + this.getGroup())
         ) {
           throw new AuthorizationException();
         }
