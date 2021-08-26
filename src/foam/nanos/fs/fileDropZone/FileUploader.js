127c375c4e988cc3a7eaa8b839d8164b77165184
Mykola Kolombet <mykola@nanopay.net>
2021-08-26 07:05:10 -0400

    update fileDropZone style

--- src/foam/nanos/fs/fileDropZone/FileUploader.js
+++ src/foam/nanos/fs/fileDropZone/FileUploader.js
@@ -14,6 +14,11 @@
     'foam.u2.view.ReferenceArrayView'
   ],
 
+  messages: [
+    { name: 'LABEL_TITLE',      message: 'File Uploader' },
+    { name: 'LABEL_FILE_GROUP', message: 'File Group' }
+  ],
+
   documentation: 'View to upload file and assign category',
 
   css: `
@@ -27,6 +32,30 @@
 
     ^button {
       text-align: right;
+    }
+
+    ^container .foam-nanos-fs-fileDropZone-FileDropZone-browse-container {
+       align-items: center;
+       display: flex;
+       flex-direction: row;
+       justify-content: flex-start;
+       gap: 8px;
+     }
+
+    ^ .foam-nanos-fs-fileDropZone-FileDropZone-caption-container {
+      display: none;
+    }
+
+    ^ .foam-nanos-fs-fileDropZone-FileDropZone button {
+      background: none!important;
+      border: none;
+      padding: 0!important;
+      color: /*%PRIMARY3%*/ #406DEA;
+      cursor: pointer;
+    }
+
+    ^ .foam-nanos-fs-fileDropZone-FileDropZone button:hover {
+      text-decoration: underline;
     }
   `,
 
@@ -50,12 +79,12 @@
         .addClass(this.myClass())
         .addClass(this.myClass("container"))
         .start("h1")
-          .add("File Uploader")
+          .add(this.LABEL_TITLE)
         .end()
         .start({class: "foam.nanos.fs.fileDropZone.FileDropZone", files$: this.files$, isMultipleFiles: false})
         .end()
         .start("h4")
-          .add("File Group")
+          .add(this.LABEL_FILE_GROUP)
         .end()
         .start({class: "foam.u2.view.ReferenceArrayView", daoKey: "fileLabelDAO", allowDuplicates: false, data$: this.labels$})
         .end()
