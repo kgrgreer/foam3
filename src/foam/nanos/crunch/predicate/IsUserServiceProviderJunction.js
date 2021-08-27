fdeadabfa35a1ef1b20a9ee50b25898f67d3c459
noodlemoodle <qiyzhang@icloud.com>
2021-08-27 15:18:42 -0400

    check is userspidjunction

---
+++ src/foam/nanos/crunch/predicate/IsUserServiceProviderJunction.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+ foam.CLASS({
+  package: 'foam.nanos.crunch.predicate',
+  name: 'IsUserServiceProviderJunction',
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+
+  javaImports: [
+    'foam.core.X',
+    'foam.dao.DAO',
+    'foam.nanos.auth.ServiceProvider',
+    'foam.nanos.crunch.Capability',
+    'foam.nanos.crunch.UserCapabilityJunction'
+  ],
+
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+        if ( ! ( obj instanceof X ) ) return false;
+        var x = (X) obj;
+
+        UserCapabilityJunction ucj = (UserCapabilityJunction) x.get("NEW");
+        Capability capability = (Capability) ((DAO) x.get("localCapabilityDAO")).find(ucj.getTargetId());
+
+        return capability != null && capability instanceof foam.nanos.auth.ServiceProvider;
+      `
+    }
+  ],
+});
