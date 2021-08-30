f48f7c8bd90a18b137d3db9f5274086d1a7ccdc5
Mykola Kolombet <mykola@nanopay.net>
2021-08-30 13:43:38 -0400

    update title

--- src/foam/nanos/fs/fileDropZone/FileUploader.js
+++ src/foam/nanos/fs/fileDropZone/FileUploader.js
@@ -39,30 +39,6 @@
     ^button {
       text-align: right;
     }
-
-    ^container .foam-nanos-fs-fileDropZone-FileDropZone-browse-container {
-       align-items: center;
-       display: flex;
-       flex-direction: row;
-       justify-content: flex-start;
-       gap: 8px;
-     }
-
-    ^ .foam-nanos-fs-fileDropZone-FileDropZone-caption-container {
-      display: none;
-    }
-
-    ^ .foam-nanos-fs-fileDropZone-FileDropZone button {
-      background: none!important;
-      border: none;
-      padding: 0!important;
-      color: /*%PRIMARY3%*/ #406DEA;
-      cursor: pointer;
-    }
-
-    ^ .foam-nanos-fs-fileDropZone-FileDropZone button:hover {
-      text-decoration: underline;
-    }
   `,
 
   properties: [
@@ -76,7 +52,11 @@
     },
     {
       class: 'String',
-      name: 'title'
+      name: 'title',
+      value: this.LABEL_TITLE,
+      factory: function() {
+        return this.LABEL_TITLE;
+      }
     }
   ],
 
@@ -89,27 +69,22 @@
         .addClass(this.myClass())
         .addClass(this.myClass('container'))
         .start('h1')
-          .add(this.LABEL_TITLE)
+          .add(this.title)
         .end()
         .start({class: 'foam.nanos.fs.fileDropZone.FileDropZone', files$: this.files$, isMultipleFiles: false})
         .end()
         .start('h4')
-          .callIfElse(
-            this.title,
-            function() {
-              this.add(this.title);
-            },
-            function() {
-              this.add(this.LABEL_FILE_GROUP);
-            })
+          .add(this.LABEL_FILE_GROUP)
         .end()
         .start({class: 'foam.u2.view.ReferenceArrayView', daoKey: 'fileLabelDAO', allowDuplicates: false, data$: this.labels$})
         .end()
         .start('br').end()
+        .start('div')
           .startContext({ data: self })
             .addClass(this.myClass('button'))
             .tag(this.UPLOAD, { buttonStyle: foam.u2.ButtonStyle.PRIMARY })
-          .endContext();
+          .endContext()
+        .end();
     }
   ],
 
