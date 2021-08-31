9b0ff3a551762dd97b248813123c88cb14447e71
Adam Fox <adam.fox@nanopay.net>
2021-08-31 16:13:52 -0400

    First version of 1 listener per dao instead of per user id

--- src/foam/nanos/auth/CachingAuthService.java
+++ src/foam/nanos/auth/CachingAuthService.java
@@ -9,7 +9,7 @@ import foam.core.Detachable;
 import foam.core.X;
 import foam.dao.DAO;
 import foam.dao.Sink;
-import foam.mlang.predicate.Predicate;
+import foam.nanos.NanoService;
 import foam.nanos.crunch.UserCapabilityJunction;
 import foam.util.LRULinkedHashMap;
 
@@ -17,14 +17,16 @@ import java.security.Permission;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
 import javax.security.auth.AuthPermission;
-import static foam.mlang.MLang.EQ;
 import static foam.mlang.MLang.TRUE;
 
 /**
  * Decorator to add Caching to AuthService.
  * Stores cache in user Session so that memory is freed when user logs out.
  **/
-public class CachingAuthService extends ProxyAuthService {
+public class CachingAuthService extends ProxyAuthService implements NanoService {
+  private static final String CACHE_NAME = "UserPermissionCache";
+  private static final int CACHE_SIZE = 10000;
+
   /**
    * A list of DAOs that will be listened to. When any of these DAOs update, the
    * cache will be invalidated. Use this to listen to DAOs that are specific to
@@ -33,10 +35,24 @@ public class CachingAuthService extends ProxyAuthService {
    */
   protected String[] extraDAOsToListenTo_;
 
-  private static final String CACHE_NAME = "UserPermissionCache";
-  private static final int CACHE_SIZE = 10000;
-
   protected LRULinkedHashMap<Long, Map<String, Boolean>> userPermissionCache_ = new LRULinkedHashMap<>(CACHE_NAME, CACHE_SIZE);
+
+  // Sink for listening to DAOs for permission changes
+  private final Sink purgeSink = new Sink() {
+    public void put(Object obj, Detachable sub) {
+      purgeCache(obj);
+    }
+
+    public void remove(Object obj, Detachable sub) {
+      purgeCache(obj);
+    }
+
+    public void eof() {}
+
+    public void reset(Detachable sub) {
+      purgeCache(null);
+    }
+  };
 
   public CachingAuthService(AuthService delegate) {
     this(delegate, new String[0]);
@@ -61,7 +77,7 @@ public class CachingAuthService extends ProxyAuthService {
 
     Permission p = new AuthPermission(permission);
 
-    Map<String, Boolean> map = getPermissionMap(x.put("extraDAOsToListenTo", extraDAOsToListenTo_), user);
+    Map<String, Boolean> map = getPermissionMap(user);
 
     if ( map.containsKey(p.getName()) ) return map.get(p.getName());
 
@@ -72,59 +88,58 @@ public class CachingAuthService extends ProxyAuthService {
     return permissionCheck;
   }
 
-  public void purgeCache(X x) {
-    User user = getUserFromContext(x);
-    if ( user != null ) {
-      userPermissionCache_.remove(user.getId());
+  public void purgeCache(Object obj) {
+    Long userId = null;
+
+    // Check for supported types to purge single user permission map
+    if ( obj instanceof User ) {
+      userId = ((User) obj).getId();
+    } else if ( obj instanceof UserCapabilityJunction ) {
+      userId = ((UserCapabilityJunction) obj).getSourceId();
+    }
+
+    if ( userId != null ) {
+      // Reset single user permission map
+      userPermissionCache_.remove(userId);
+    } else {
+      // Reset permission cache
+      // Reassigning LRU cache will garbage collect old cache and reset to empty cache with initial capacity
+      userPermissionCache_ = new LRULinkedHashMap<>(CACHE_NAME, CACHE_SIZE);
     }
   }
 
-  protected Map<String, Boolean> getPermissionMap(final X x, User user) {
-    // TODO: Context is set on user permission cache for OM support, this can be removed when OM is removed from LRULinkListHashMap
-    if ( userPermissionCache_.getX() == null ) {
-      userPermissionCache_.setX(x);
-    }
+  @Override
+  public void setX(X x) {
+    super.setX(x);
 
+    // TODO: Context is set on user permission cache for OM support, this can be removed when OM is removed from LRULinkListHashMap
+    userPermissionCache_.setX(getX());
+  }
+
+  public void start() throws Exception {
+    // Configure listeners for explicit permission DAOs
+    DAO userDAO = (DAO) getX().get("localUserDAO");
+    if ( userDAO != null ) userDAO.listen(purgeSink, TRUE);
+
+    DAO userCapabilityJunctionDAO = (DAO) getX().get("userCapabilityJunctionDAO");
+    if ( userCapabilityJunctionDAO != null ) userCapabilityJunctionDAO.listen(purgeSink, TRUE);
+
+    DAO groupPermissionJunctionDAO = (DAO) getX().get("groupPermissionJunctionDAO");
+    if ( groupPermissionJunctionDAO != null ) groupPermissionJunctionDAO.listen(purgeSink, TRUE);
+
+    // Configure listeners for additional permission DAOs
+    if ( extraDAOsToListenTo_ != null ) {
+      for ( String daoName : extraDAOsToListenTo_ ) {
+        DAO dao = (DAO) getX().get(daoName);
+        if ( dao != null ) dao.listen(purgeSink, TRUE);
+      }
+    }
+  }
+
+  protected Map<String, Boolean> getPermissionMap(User user) {
     long userId = user == null ? -1 : user.getId();
     Map<String, Boolean> map = userPermissionCache_.get(userId);
     if ( map == null ) {
-      Sink purgeSink = new Sink() {
-        public void put(Object obj, Detachable sub) {
-          purgeCache(x);
-          sub.detach();
-        }
-
-        public void remove(Object obj, Detachable sub) {
-          purgeCache(x);
-          sub.detach();
-        }
-
-        public void eof() {
-        }
-
-        public void reset(Detachable sub) {
-          purgeCache(x);
-          sub.detach();
-        }
-      };
-
-      DAO userDAO = (DAO) x.get("localUserDAO");
-      DAO userCapabilityJunction = (DAO) x.get("userCapabilityJunctionDAO");
-      DAO groupPermissionJunctionDAO = (DAO) x.get("groupPermissionJunctionDAO");
-
-      userDAO.listen(purgeSink, EQ(User.ID, userId));
-      userCapabilityJunction.listen(purgeSink, EQ(UserCapabilityJunction.SOURCE_ID, userId));
-      groupPermissionJunctionDAO.listen(purgeSink, TRUE);
-
-      String[] extraDAOsToListenTo = (String[]) x.get("extraDAOsToListenTo");
-
-      if ( extraDAOsToListenTo != null ) {
-        for ( String daoName : extraDAOsToListenTo ) {
-          DAO dao = (DAO) x.get(daoName);
-          if ( dao != null ) dao.listen(purgeSink, TRUE);
-        }
-      }
-
       map = new ConcurrentHashMap<>();
       userPermissionCache_.put(userId, map);
     }
