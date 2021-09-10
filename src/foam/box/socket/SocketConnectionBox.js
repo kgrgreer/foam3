cf07b742c41ba55a26c6a085afcec3da92909c86
Joel Hughes <joel@nanopay.net>
2021-09-09 21:53:34 -0400

    Simplified code, marginal improvements with BufferredInputStream

--- src/foam/box/socket/SocketConnectionBox.js
+++ src/foam/box/socket/SocketConnectionBox.js
@@ -27,9 +27,11 @@ foam.CLASS({
     'foam.nanos.logger.Logger',
     'foam.nanos.om.OMLogger',
     'foam.nanos.pm.PM',
+    'java.io.BufferedInputStream',
     'java.io.BufferedOutputStream',
     'java.io.DataInputStream',
     'java.io.DataOutputStream',
+    'java.io.EOFException',
     'java.io.InputStream',
     'java.io.InputStreamReader',
     'java.io.IOException',
@@ -128,11 +130,13 @@ foam.CLASS({
     setSocket(socket);
 
     out_ = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-    in_ = new DataInputStream(socket.getInputStream());
+    in_ = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+    pending_ = new AtomicLong();
   }
 
   protected DataInputStream in_;
   protected DataOutputStream out_;
+  protected AtomicLong pending_;
 
   protected static final ThreadLocal<foam.lib.formatter.FObjectFormatter> formatter_ = new ThreadLocal<foam.lib.formatter.FObjectFormatter>() {
     @Override
@@ -150,6 +154,19 @@ foam.CLASS({
       return formatter;
     }
   };
+
+  protected byte [] readBytes (InputStream in, int expectedSize)
+    throws IOException {
+    final byte[] buffer = new byte[expectedSize];
+    int totalReadSize = 0;
+    while (totalReadSize < expectedSize) {
+      int readSize = in.read(buffer, totalReadSize, expectedSize - totalReadSize);
+      if (readSize < 0) throw new EOFException ();
+      totalReadSize += readSize;
+    }
+    return buffer;
+  }
+
         `
         }));
       }
@@ -165,10 +182,12 @@ NOTE: duplicated in SocketConnectionReplyBox
 `,
       name: 'send',
       javaCode: `
-      PM pm = PM.create(getX(), this.getClass().getSimpleName(), getId()+":send");
+      long pending = pending_.incrementAndGet();
+      PM pm = PM.create(getX(), this.getClass().getSimpleName(), getId(), "send");
       Box replyBox = (Box) msg.getAttributes().get("replyBox");
       String replyBoxId = null;
       if ( replyBox != null ) {
+        PM pmReplyBox = PM.create(getX(), this.getClass().getSimpleName(), getId(), "send", "replyBox");
         replyBoxId = java.util.UUID.randomUUID().toString();
         getReplyBoxes().put(replyBoxId, new BoxHolder(replyBox, PM.create(getX(), this.getClass().getSimpleName(), getId()+":roundtrip")));
         SocketClientReplyBox box = new SocketClientReplyBox(replyBoxId);
@@ -178,15 +197,18 @@ NOTE: duplicated in SocketConnectionReplyBox
         } else {
           msg.getAttributes().put("replyBox", box);
         }
+        pmReplyBox.log(getX());
       }
       String message = null;
       try {
         OMLogger omLogger = (OMLogger) getX().get("OMLogger");
+        PM pmFormat = PM.create(getX(), this.getClass().getSimpleName(), getId(), "send", "format");
         foam.lib.formatter.FObjectFormatter formatter = formatter_.get();
         formatter.setX(getX());
         formatter.output(msg);
         message = formatter.builder().toString();
         byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+        pmFormat.log(getX());
         Socket socket = (Socket) getSocket();
         if ( socket.isClosed() ||
              ! socket.isConnected() ) {
@@ -194,13 +216,19 @@ NOTE: duplicated in SocketConnectionReplyBox
         }
         omLogger.log(this.getClass().getSimpleName(), getId(), "pending");
         synchronized (out_) {
+          PM pmWrite = PM.create(getX(), this.getClass().getSimpleName(), getId(), "send", "write");
           // NOTE: enable along with send debug call in SocketServerProcessor to monitor all messages.
           // getLogger().debug("send", message);
           out_.writeInt(messageBytes.length);
           out_.write(messageBytes);
-          // TODO/REVIEW
-          out_.flush();
           omLogger.log(this.getClass().getSimpleName(), getId(), "sent");
+          pmWrite.log(getX());
+        }
+        // If no other send operations immediately pending, then flush
+        if ( pending == pending_.longValue() ) {
+          out_.flush();
+        } else {
+          getLogger().debug("send", "flush", false, pending, pending_.longValue());
         }
       } catch ( Throwable t ) {
         pm.error(getX(), t);
@@ -235,46 +263,17 @@ NOTE: duplicated in SocketConnectionReplyBox
         while ( getValid().get() ) {
           PM pm = null;
           try {
-            pm = PM.create(x, this.getClass().getSimpleName(), getId()+":receive");
+            omLogger.log(this.getClass().getSimpleName(), getId(), "receive");
+            pm = PM.create(x, this.getClass().getSimpleName(), getId(), "receive");
 
             int length = in_.readInt();
-            byte[] bytes = new byte[length];
-            StringBuilder data = new StringBuilder();
-            int total = 0;
-            while ( true ) {
-              int bytesRead = 0;
-              try {
-                bytesRead = in_.read(bytes, 0, length - total);
-                if ( bytesRead == -1 ) {
-                  getLogger().debug("eof,-1");
-                  break;
-                }
-              } catch ( java.io.EOFException | java.net.SocketException e ) {
-                getLogger().debug(e.getMessage());
-                break;
-              }
-              data.append(new String(bytes, 0, bytesRead, StandardCharsets.UTF_8));
-              total += bytesRead;
-              if ( total == length ) {
-                break;
-              }
-              if ( total > length ) {
-                // REVIEW: can this happen?
-                getLogger().error("read too much", length, total);
-                break;
-              }
-            }
-            omLogger.log(this.getClass().getSimpleName(), getId(), "received");
-            String message = data.toString();
-            if ( foam.util.SafetyUtil.isEmpty(message) ) {
-              throw new RuntimeException("Received empty message.");
-            }
-            // NOTE: enable along with send debug call in SocketServerProcessor to monitor all messages.
-            // getLogger().debug("receive", message);
-            Message msg = (Message) x.create(JSONParser.class).parseString(message);
+            byte[] bytes = readBytes(in_, length);
+            String data = new String(bytes, 0, length, StandardCharsets.UTF_8);
+            Message msg = (Message) x.create(JSONParser.class).parseString(data);
             if ( msg == null ) {
-              throw new RuntimeException("Failed to parse. message: "+message);
+              throw new IllegalArgumentException("Failed to parse. message: "+data);
             }
+  
             String replyBoxId = (String) msg.getAttributes().get(REPLY_BOX_ID);
             if ( replyBoxId != null ) {
               Box replyBox = null;
@@ -290,7 +289,7 @@ NOTE: duplicated in SocketConnectionReplyBox
               if ( replyBox == null ) {
                 getLogger().error("ReplyBox not found", replyBoxId);
                 ((foam.dao.DAO) x.get("alarmDAO")).put(new foam.nanos.alarming.Alarm("ReplyBox not found"));
-                throw new RuntimeException("ReplyBox not found. message: "+message);
+                throw new RuntimeException("ReplyBox not found. message: "+data);
               }
               getReplyBoxes().remove(replyBoxId);
               replyBox.send(msg);
@@ -305,8 +304,8 @@ NOTE: duplicated in SocketConnectionReplyBox
                 }
                 throw new RuntimeException(re.getMessage());
               }
-              getLogger().error("Failed to process reply", message);
-              throw new RuntimeException("Failed to process reply. message: "+message);
+              getLogger().error("Failed to process reply", data);
+              throw new RuntimeException("Failed to process reply. message: "+data);
             }
           } catch ( java.net.SocketTimeoutException e ) {
             // getLogger().debug("SocketTimeoutException", e.getMessage());
