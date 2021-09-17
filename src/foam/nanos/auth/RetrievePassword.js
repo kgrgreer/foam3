a3a46b0e3fbeae180af7c24dd07800fe08e6ca99
sarthak-marwaha <sarthak@nanopay.net>
2021-09-17 15:12:19 -0400

    Standardise RetrivePassword view

--- src/foam/nanos/auth/RetrievePassword.js
+++ src/foam/nanos/auth/RetrievePassword.js
@@ -38,29 +38,6 @@ foam.CLASS({
     }
   ],
 
-  css: `
-    .foam-nanos-auth-ChangePasswordView .foam-u2-layout-Grid {
-      width: 75%;
-      display: inline-block;
-      vertical-align: bottom;
-    }
-
-    .foam-nanos-auth-ChangePasswordView .foam-u2-layout-Cols {
-      display: inline-block;
-      margin-left: 5%;
-      vertical-align: bottom;
-    }
-
-    .foam-nanos-auth-ChangePasswordView .foam-u2-layout-Rows {
-      display: block;
-    }
-
-    .foam-nanos-auth-ChangePasswordView .foam-u2-detail-SectionedDetailPropertyView-validation-container {
-      position: absolute;
-      margin-left: 0px;
-      display: flex;
-    }
-  `,
 
   properties: [
     {
