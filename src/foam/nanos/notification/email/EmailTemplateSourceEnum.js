23be43a5bf10df46384b19ec97d5b70ddb4adb10
Minsun <minsun@nanopay.net>
2021-09-21 15:08:11 -0400

    added a documentation in enum

--- src/foam/nanos/notification/email/EmailTemplateSourceEnum.js
+++ src/foam/nanos/notification/email/EmailTemplateSourceEnum.js
@@ -7,6 +7,10 @@
  foam.ENUM({
   package: 'foam.nanos.notification.email',
   name: 'EmailTemplateSourceEnum',
+  documentation: `This is used to define a source of email's generation type.
+    A source of email's generation is where sending email function gets called
+    and It can be the below.
+  `,
 
   values: [
     {
