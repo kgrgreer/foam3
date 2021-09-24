11d63f8d53a07e39205b15b0fe1d53c0747773ab
Kevin Greer <kgrgreer@gmail.com>
2021-09-24 08:38:30 -0400

    Rewrite java Join parser to be faster and support optional.

--- src/foam/lib/parse/Join.java
+++ src/foam/lib/parse/Join.java
@@ -6,27 +6,38 @@
 
 package foam.lib.parse;
 
-public class Join implements Parser {
-  Parser d;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Join
+  implements Parser
+{
+  Parser d_;
+
   public Join(Parser d) {
-    this.d = d;
+    d_ = d;
   }
+
   public PStream parse(PStream ps, ParserContext x) {
-    ps = ps.apply(d, x);
+    ps = ps.apply(d_, x);
     if ( ps == null ) return null;
 
-    java.util.List<Object> queue = new java.util.LinkedList<>();
-    queue.add(ps.value());
     StringBuilder sb = new StringBuilder();
-    while ( queue.size() > 0 ) {
-      Object o = queue.remove(0);
-      if ( o instanceof Object[] ) {
-        queue.addAll(java.util.Arrays.asList((Object[])o));
-      } else {
-        sb.append(o.toString());
-      }
-    }
+
+    append(sb, ps.value());
 
     return ps.setValue(sb.toString());
+  }
+
+  void append(StringBuilder sb, Object o) {
+    if ( o == null ) {
+      // happens for optional parsers that don't parse
+    } else if ( o instanceof Object[] ) {
+      Object[] os = (Object[]) o;
+      for ( var i = 0 ; i < os.length ; i++ )
+        append(sb, os[i]);
+    } else {
+      sb.append(o.toString());
+    }
   }
 }
