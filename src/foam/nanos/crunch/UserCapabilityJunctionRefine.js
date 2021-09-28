44debde3bfad632fc1d2421b02363b7a0873853e
Kevin Greer <kgrgreer@gmail.com>
2021-09-28 13:28:43 -0400

    Remove redundant line and add comment.

--- src/foam/nanos/crunch/UserCapabilityJunctionRefine.js
+++ src/foam/nanos/crunch/UserCapabilityJunctionRefine.js
@@ -409,6 +409,7 @@ foam.CLASS({
         }
 
         if ( ucj.getSourceId() == currentSubject.getUser().getId() ) {
+          // setup user chain
           subject.setUser(currentSubject.getRealUser());
           subject.setUser(currentSubject.getUser());
           return subject;
@@ -423,7 +424,6 @@ foam.CLASS({
           subject.setUser((User) userDAO.find(ucj.getSourceId()));
         }
 
-        subject.setUser((User) userDAO.find(ucj.getSourceId()));
         subject.setUser((User) userDAO.find(ucj.getSourceId()));
         return subject;
       `,
