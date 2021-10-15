5d349e6b2784265b068150a9669f3a8cfb422a6d
sarthak-marwaha <sarthak@nanopay.net>
2021-10-15 15:56:31 -0400

    add standard shadows

--- src/foam/nanos/controller/AppStyles.js
+++ src/foam/nanos/controller/AppStyles.js
@@ -110,7 +110,32 @@ foam.CLASS({
       line-height: 1.17;
       margin: 0;
     }
-    
+
+    /* SHADOWS */
+    .base-shadow {
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
+    } 
+
+    .md-shadow {
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .lg-shadow {
+      box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1), 0px 4px 6px rgba(0, 0, 0, 0.05);
+    }
+
+    .xl-shadow {
+      box-shadow: 0px 20px 25px rgba(0, 0, 0, 0.1), 0px 10px 10px rgba(0, 0, 0, 0.04);
+    }
+
+    .2xl-shadow {
+      box-shadow: 0px 25px 50px rgba(0, 0, 0, 0.25);
+    }
+
+    .inner-shadow {
+      box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.06);
+    }
+
     .New {
       width: 35px;
       height: 20px;
