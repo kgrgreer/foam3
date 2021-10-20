1a3dfb8d2ffaf0bf0010ad97f89eddc1ae707f23
Joel Hughes <joel@nanopay.net>
2021-10-20 01:17:50 -0400

    refactor script runners to avoid freezing the running script

--- src/foam/nanos/script/Script.js
+++ src/foam/nanos/script/Script.js
@@ -380,62 +380,57 @@ foam.CLASS({
         }
       ],
       javaCode: `
-        canRun(x);
-        PM               pm          = new PM(this.getClass(), getId());
-        RuntimeException thrown     = null;
+        RuntimeException thrown      = null;
         Language         l           = getLanguage();
-        Thread.currentThread().setPriority(getPriority());
+        ByteArrayOutputStream baos   = new ByteArrayOutputStream();
+        PrintStream            ps    = new PrintStream(baos);
+        PM               pm          = new PM(this.getClass(), getId());
 
         try {
-            ByteArrayOutputStream baos  = new ByteArrayOutputStream();
-            PrintStream           ps    = new PrintStream(baos);
+          Thread.currentThread().setPriority(getPriority());
 
-            try {
-              if ( l == foam.nanos.script.Language.BEANSHELL ) {
-                Interpreter shell = (Interpreter) createInterpreter(x, null);
-                setOutput("");
-                shell.setOut(ps);
-                shell.eval(getCode());
-              } else if ( l == foam.nanos.script.Language.JSHELL ) {
-                String print = null;
-                JShell jShell = (JShell) createInterpreter(x,ps);
-                print = new JShellExecutor().execute(x, jShell, getCode());
-                ps.print(print);
-              } else {
-                throw new RuntimeException("Script language not supported");
-              }
-            } catch (Throwable t) {
-              thrown = new RuntimeException(t);
-              ps.println();
-              t.printStackTrace(ps);
-              Logger logger = (Logger) x.get("logger");
-              logger.error(this.getClass().getSimpleName(), "runScript", getId(), t);
-              pm.error(x, t);
-            } finally {
-              pm.log(x);
-            }
-
+          if ( l == foam.nanos.script.Language.BEANSHELL ) {
+            Interpreter shell = (Interpreter) createInterpreter(x, null);
+            setOutput("");
+            shell.setOut(ps);
+            shell.eval(getCode());
+          } else if ( l == foam.nanos.script.Language.JSHELL ) {
+            String print = null;
+            JShell jShell = (JShell) createInterpreter(x,ps);
+            print = new JShellExecutor().execute(x, jShell, getCode());
+            ps.print(print);
+          } else {
+            throw new RuntimeException("Script language not supported");
+          }
+          pm.log(x);
+        } catch (Throwable t) {
+          thrown = new RuntimeException(t);
+          pm.error(x, t);
+          ps.println();
+          t.printStackTrace(ps);
+          Logger logger = (Logger) x.get("logger");
+          logger.error(this.getClass().getSimpleName(), "runScript", getId(), t);
+          throw thrown;
+        } finally {
           setLastRun(new Date());
           setLastDuration(pm.getTime());
           ps.flush();
           setOutput(baos.toString());
 
-          ScriptEvent event = new ScriptEvent(x);
-          event.setLastRun(this.getLastRun());
-          event.setLastDuration(this.getLastDuration());
-          event.setOutput(this.getOutput());
-          event.setScriptType(this.getClass().getSimpleName());
-          event.setOwner(this.getId());
-          event.setScriptId(this.getId());
-          event.setHostname(System.getProperty("hostname", "localhost"));
-          event.setClusterable(this.getClusterable());
-          ((DAO) x.get(getEventDaoKey())).put(event);
-
-          if ( thrown != null) {
-            throw thrown;
-          }
-        } finally {
           Thread.currentThread().setPriority(Thread.NORM_PRIORITY);
+
+          if ( thrown != null ) {
+            ScriptEvent event = new ScriptEvent(x);
+            event.setLastRun(this.getLastRun());
+            event.setLastDuration(this.getLastDuration());
+            event.setOutput(this.getOutput());
+            event.setScriptType(this.getClass().getSimpleName());
+            event.setOwner(this.getId());
+            event.setScriptId(this.getId());
+            event.setHostname(System.getProperty("hostname", "localhost"));
+            event.setClusterable(this.getClusterable());
+            ((DAO) x.get(getEventDaoKey())).put(event);
+          }
         }
     `
     },
