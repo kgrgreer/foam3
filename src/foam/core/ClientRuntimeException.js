c7e062cf23805ef423de8f63bb78bc5b53e1f7cc
hchoura <choura.hassene@gmail.com>
2021-10-28 23:40:13 -0400

    2 set

--- src/foam/core/ClientRuntimeException.js
+++ src/foam/core/ClientRuntimeException.js
@@ -12,38 +12,29 @@ foam.CLASS({
   javaGenerateConvenienceConstructor: false,
   javaGenerateDefaultConstructor: false,
 
-  axioms: [
-    {
-      name: 'javaExtras',
-      buildJavaClass: function(cls) {
-        cls.extras.push(
-          `
-  public ClientRuntimeException() {
-    super();
-  }
-
-  public ClientRuntimeException(String message) {
-    super(message);
-  }
-
-  public ClientRuntimeException(Throwable cause) {
-    super(cause);
-  }
-
-  public ClientRuntimeException(String message, Throwable cause ) {
-    super(message, cause);
-  }
-          `
-        );
-      }
+  javaCode: `
+    public ClientRuntimeException() {
+      super();
     }
-  ],
+
+    public ClientRuntimeException(String message) {
+      super(message);
+    }
+
+    public ClientRuntimeException(Throwable cause) {
+      super(cause);
+    }
+
+    public ClientRuntimeException(String message, Throwable cause ) {
+      super(message, cause);
+    }
+  `,
 
   methods: [
     {
       // TODO: cloning this property from ExceptionInterface creates a bug.
       name: 'getClientRethrowException',
-      documentation: 
+      documentation:
       `If an exception is intended to go to the client, this
         returns an exception object; it returns null otherwise.`,
       type: 'RuntimeException',
