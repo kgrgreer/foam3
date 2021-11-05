8fe5e680d0a22b592dc5b9e5029c11755f37c2c4
Chanmann Lim <chanmannlim@gmail.com>
2021-11-05 13:55:05 -0400

    Output FUIDProperty as string

--- src/foam/core/AbstractFUIDPropertyInfo.java
+++ src/foam/core/AbstractFUIDPropertyInfo.java
@@ -6,8 +6,19 @@
 
 package foam.core;
 
+import foam.lib.formatter.FObjectFormatter;
+import foam.lib.json.Outputter;
+
 public abstract class AbstractFUIDPropertyInfo
     extends AbstractLongPropertyInfo
 {
+  @Override
+  public void toJSON(Outputter outputter, Object value) {
+    outputter.outputString(String.valueOf(value));
+  }
 
+  @Override
+  public void format(FObjectFormatter formatter, FObject obj) {
+    formatter.output(String.valueOf(get_(obj)));
+  }
 }
