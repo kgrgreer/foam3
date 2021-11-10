ebd6bdf40e531ddcaca62e063afe59df5f428cbd
noodlemoodle <qiyzhang@icloud.com>
2021-11-10 12:48:39 -0500

    ChipView: use p-label font class and some other css fixes

--- src/foam/u2/view/ChipView.js
+++ src/foam/u2/view/ChipView.js
@@ -17,43 +17,40 @@ foam.CLASS({
 
   css: `
     ^ {
-      height: 30px;
+      height: fit-content;
       border-radius: 100px;
       background-color: #a4b3b8;
       margin: auto;
       position: relative;
       float: left;
-      margin: 5px;
+      margin: 0.5rem;
     }
 
-    ^ .label {
-      font-size: 1.2rem;
-      line-height: 1.33;
-      letter-spacing: 0.2px;
+    ^label {
       text-align: left;
-      color: #ffffff;
-      padding: 8px 15px 6px 10px;
-      display: table-cell;
+      color: /*%WHITE%*/ #ffffff;
+      padding: 8px 10px 6px 10px;
+      display: flex;
+      align-items: center;
     }
 
     ^ .foam-u2-ActionView-removeSelf {
-      width: 10px;
-      height: 10px;
+      width: 1rem;
+      height: 1rem;
       object-fit: contain;
       margin: 0;
-      margin-top: -11;
       float: right;
       cursor: pointer;
       display: inline-block;
       outline: 0;
       border: none;
       background: transparent;
-      padding-right: 15x;
+      padding-left: 1em;
     }
 
-    ^ .foam-u2-ActionView-removeSelf img {
-      width: 15px;
-      height: 15px;
+    ^ .foam-u2-ActionView-removeSelf svg {
+      width: 1rem;
+      height: 1rem;
     }
 
     ^ .foam-u2-ActionView-removeSelf:hover {
@@ -68,14 +65,13 @@ foam.CLASS({
 
       this
         .addClass(this.myClass())
-        .start()
-          .start('p')
-            .addClass('label')
-            .add(this.data)
-            .startContext({ data: this })
-              .add(this.REMOVE_SELF)
-            .endContext()
-          .end()
+        .start('p')
+          .addClass('p-label')
+          .addClass(this.myClass('label'))
+          .add(this.data)
+          .startContext({ data: this })
+            .add(this.REMOVE_SELF)
+          .endContext()
         .end();
     }
   ],
@@ -83,6 +79,7 @@ foam.CLASS({
   actions: [
     {
       name: 'removeSelf',
+      label: '',
       icon: 'images/ic-cancelwhite.svg',
       code: function() {
         this.removeChip(this.data);
