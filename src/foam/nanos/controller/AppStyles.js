21119a756c9269165a641761acf74840c5cc9863
noodlemoodle <qiyzhang@icloud.com>
2021-11-15 08:48:43 -0500

    move transfer/style mobile css to appstyles

--- src/foam/nanos/controller/AppStyles.js
+++ src/foam/nanos/controller/AppStyles.js
@@ -272,11 +272,53 @@ foam.CLASS({
     font-weight: bold;
     font-size: 2.8rem;
   }
-  @media only screen and (min-width: 321px) {
+
+  /* mobile */
+  @media only screen and (min-width: 321px) and (max-width: 767px) {
     .large-title { font-size: 3.5rem; }
+
+    .foam-u2-dialog-Popup-inner {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .foam-u2-wizard-ScrollingStepWizardView-hide-X-status {
+      display: none !important;
+    }
+    .foam-u2-wizard-ScrollingStepWizardView {
+      max-width: 100vw !important;
+      max-height: 100vh !important;
+    }
+    .side-nav-view,
+    .side-nav-view .foam-u2-view-TreeViewRow {
+      width: 100%;
+    }
+    .foam-nanos-menu-VerticalMenu-search {
+      width: 100% !important;
+    }
+    .foam-u2-ActionView-refreshTable {
+      display: none !important;
+    }
+    .foam-comics-v2-DAOBrowseControllerView-container {
+      padding: 0 !important;
+    }
+    .foam-comics-v2-DAOBrowseControllerView-header-container {
+      padding-top: 32px;
+    }
   }
-  @media only screen and (min-width: 768px) {
+
+  /* tablet */
+  @media only screen and (min-width: 768px) and (max-width: 959) {
     .large-title { font-size: 6.4rem; }
+    .foam-u2-dialog-Popup-inner {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
   @media only screen and (min-width: 960px) {
     .large-title { font-size: 8.4rem; }
