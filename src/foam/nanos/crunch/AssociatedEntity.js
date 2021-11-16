99bca887da0afd3019fa591a2c2ca81d1cbc17e6
Nauna <anna@nanopay.net>
2021-11-16 08:35:16 -0500

    space :(

--- src/foam/nanos/crunch/AssociatedEntity.js
+++ src/foam/nanos/crunch/AssociatedEntity.js
@@ -3,7 +3,7 @@
  * Copyright 2020 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
- foam.ENUM({
+foam.ENUM({
   package: 'foam.nanos.crunch',
   name: 'AssociatedEntity',
   values: [
