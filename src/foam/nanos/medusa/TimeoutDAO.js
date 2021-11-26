2eb1c08c9e06dff6b5a4063b6414490cbcc827b8
Joel Hughes <joel@nanopay.net>
2021-11-26 11:38:23 -0500

    TimeoutDAO - implement EnabledAware so can be turned off in production if needed

--- src/foam/nanos/medusa/TimeoutDAO.js
+++ src/foam/nanos/medusa/TimeoutDAO.js
@@ -8,6 +8,7 @@ foam.CLASS({
   package: 'foam.nanos.medusa',
   name: 'TimeoutDAO',
   extends: 'foam.dao.ProxyDAO',
+  implements: [ 'foam.nanos.auth.EnabledAware' ],
 
   // TODO: configuration per spid, model, or other SLA metrics
   documentation: `Reject MedusaEntry if it hasn't been processed in an appropriate amount of time.`,
@@ -18,6 +19,11 @@ foam.CLASS({
   ],
 
   properties: [
+    {
+      name: 'enabled',
+      class: 'Boolean',
+      value: true
+    },
     {
       name: 'globalTimeout',
       class: 'Long',
@@ -31,7 +37,8 @@ foam.CLASS({
       javaCode: `
       Logger logger = Loggers.logger(x, this);
       MedusaEntry entry = (MedusaEntry) obj;
-      if ( entry.getLastModified() == null && // not yet saved the first time.
+      if ( getEnabled() &&
+           entry.getLastModified() == null && // not yet saved the first time.
            entry.getCreated() != null &&
            System.currentTimeMillis() - entry.getCreated().getTime() > getGlobalTimeout() ) {
         throw new MedusaEntryTimeoutException();
