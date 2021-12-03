ef4afe6bfa8b81a4398caea045db49ab9db2e74d
Kevin Greer <kgrgreer@gmail.com>
2021-12-03 13:29:44 -0500

    Update colours demo.

--- src/foam/demos/u2/Colours.js
+++ src/foam/demos/u2/Colours.js
@@ -9,20 +9,22 @@ foam.CLASS({
   ],
 
   methods: [
-    function rgbToGrey(rgb) {
+    function rgbToGrey(rgb /*[0..255,0.255,0.255]*/) /* -> 0..1 */ {
       var [r, g, b] = rgb;
 //      return (r/255+g/255+b/255)/3;
 //Gray = (Red * 0.3 + Green * 0.59 + Blue * 0.11)
-      return 0.3 * r/255 + 0.59 * g/255 + 0.11 * b/255;
+//console.log('rgbg', r,g,b,0.3 * r/255 + 0.59 * g/255 + 0.11 * b/255);
+//return 0.3 * r/255 + 0.59 * g/255 + 0.11 * b/255;
+      return 0.3 * r/255.0 + 0.59 * g/255.0 + 0.11 * b/255.0;
     },
 
-    function adjustRGBBrightness(rgb, br) {
+    function adjustRGBBrightness(rgb /*[0..255,0.255,0.255]*/, br/*0..1*/) {
     //  return [255 * br, 255 * br, 255 * br];
       var [r, g, b] = rgb;
       var gr = this.rgbToGrey(rgb);
-
-      console.log('adjustRGB', r,g,b, gr);
       var scale = br/gr;
+
+     //console.log('adjustRGB', r,g,b, ' provided: ', gr, ' desired: ', br, 'achieved: ', this.rgbToGrey([r * scale, g * scale, b * scale]));
       return [r * scale, g * scale, b * scale];
     },
 
@@ -54,7 +56,8 @@ foam.CLASS({
     function hl(hue, l) {
 //      var [r, g, b] = this.hslToRgb(hue/360, 0.8, l/1000);
       var [r, g, b] = this.adjustRGBBrightness(this.hslToRgb(hue/360, 0.95, .6), Math.sqrt((1000-l*0.95)/1000));
-      return `rgb(${r},${g},${b})`;
+      return `rgb(${Math.floor(r)},${Math.floor(g)},${Math.floor(b)})`;
+//      return `rgb(${Math.floor((r*0.3+g*0.6+b*0.1)/3)},${Math.floor((r*0.3+g*0.6+b*0.1)/3)},${Math.floor((r*0.3+g*0.6+b*0.1)/3)})`;
 //      return `hsl(${hue},70%,${l/10}%)`;
     },
 
@@ -66,7 +69,9 @@ foam.CLASS({
             this.start('tr').call(function() {
               for ( var hue = 0 ; hue < 360 ; hue += 20 ) {
                 var l2 = 1000-(l+500)%950;
-                this.start('th').style({xxxcolor: 'black', color: self.hl(hue+180,l2), width: '80px', height: '80px', background: self.hl(hue, l)}).add(l).end();
+                var l3 =
+                self.rgbToGrey(self.adjustRGBBrightness(self.hslToRgb(hue/360, 0.95, .6), Math.sqrt((1000-l*0.95)/1000)));
+                this.start('th').style({xxxcolor: 'black', color: 'black' /*self.hl(hue+180,l2)*/, width: '80px', height: '80px', background: self.hl(hue, l)}).add(Math.floor(100*l3)).end();
               }
             }).end();
           }
