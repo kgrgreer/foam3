e3cfafc1109aff0e406ffbd2a196907d89cae37e
Kevin Greer <kgrgreer@gmail.com>
2021-12-06 09:37:38 -0500

    Update Colours demo.

--- src/foam/demos/u2/Colours.js
+++ src/foam/demos/u2/Colours.js
@@ -9,25 +9,37 @@ foam.CLASS({
   ],
 
   methods: [
-    function rgbToGrey(rgb /*[0..255,0.255,0.255]*/) /* -> 0..1 */ {
+    function normalizeRGB(rgb /*[0..255,0.255,0.255]*/) {
+      /* Adjust an RGB colour so that the brightest component(s) are 255. */
       var [r, g, b] = rgb;
-//      return (r/255+g/255+b/255)/3;
-//Gray = (Red * 0.3 + Green * 0.59 + Blue * 0.11)
-//console.log('rgbg', r,g,b,0.3 * r/255 + 0.59 * g/255 + 0.11 * b/255);
-//return 0.3 * r/255 + 0.59 * g/255 + 0.11 * b/255;
-      return 0.3 * r/255.0 + 0.59 * g/255.0 + 0.11 * b/255.0;
-    },
-
-    function adjustRGBBrightness(rgb /*[0..255,0.255,0.255]*/, br/*0..1*/) {
-    //  return [255 * br, 255 * br, 255 * br];
-      var [r, g, b] = rgb;
-      var gr = this.rgbToGrey(rgb);
-      var scale = br/gr;
-
-     //console.log('adjustRGB', r,g,b, ' provided: ', gr, ' desired: ', br, 'achieved: ', this.rgbToGrey([r * scale, g * scale, b * scale]));
+      var scale = 255/Math.max(r, g, b);
       return [r * scale, g * scale, b * scale];
     },
 
+    function rgbToGrey(rgb /*[0..255,0.255,0.255]*/) /* -> 0..1 */ {
+      var [r, g, b] = rgb;
+      return 0.18 * r/255 + 0.8 * g/255 + 0.02 * b/255; // What Chrome appears to be using
+      return 0.299 * r/255 + 0.587 * g/255 + 0.114 * b/255; // Correct
+   //   return 0.3 * r/255.0 + 0.6 * g/255.0 + 0.1 * b/255.0; // Approximate
+    },
+
+    function adjustRGBBrightness(rgb /*[0..255,0.255,0.255]*/, desired/*0..1*/) {
+    rgb = this.normalizeRGB(rgb);
+      var [r, g, b] = rgb;
+      var gr = this.rgbToGrey(rgb);
+      if ( desired > gr ) {
+        // mix * 1 + (1-mix) * gr = desired
+        // mix + gr - mix*gr = desired
+        // mix - mix*gr = desired - gr
+        // mix * (1-gr) = desired - gr
+        // mix = ( desired - gr ) / (1 - gr)
+        var mix = (desired - gr) / ( 1 - gr);
+        return [ 255 * mix + (1-mix) * r, 255 * mix + (1-mix) * g, 255 * mix + (1-mix) * b];
+      }
+
+       var scale = desired/gr;
+      return [ scale * r, scale * g, scale * b ];
+  },
     function hslToRgb(h, s, l) {
       var r, g, b;
 
@@ -54,11 +66,13 @@ foam.CLASS({
     },
 
     function hl(hue, l) {
-//      var [r, g, b] = this.hslToRgb(hue/360, 0.8, l/1000);
-      var [r, g, b] = this.adjustRGBBrightness(this.hslToRgb(hue/360, 0.95, .6), Math.sqrt((1000-l*0.95)/1000));
-      return `rgb(${Math.floor(r)},${Math.floor(g)},${Math.floor(b)})`;
-//      return `rgb(${Math.floor((r*0.3+g*0.6+b*0.1)/3)},${Math.floor((r*0.3+g*0.6+b*0.1)/3)},${Math.floor((r*0.3+g*0.6+b*0.1)/3)})`;
-//      return `hsl(${hue},70%,${l/10}%)`;
+      var [r, g, b] = this.adjustRGBBrightness(this.hslToRgb(hue/360, 0.8, 0.4), Math.sqrt((1000-l*0.95)/1000));
+      return [r, g, b];
+    },
+
+    function rgbToString(rgb) {
+      var [r, g, b] = rgb;
+      return `rgb(${r.toFixed(4)},${g.toFixed(4)},${b.toFixed(4)})`;
     },
 
     function render() {
@@ -68,10 +82,8 @@ foam.CLASS({
           for ( var l = 50 ; l <= 900 ; l = l == 50 ? 100 : l + 100 ) {
             this.start('tr').call(function() {
               for ( var hue = 0 ; hue < 360 ; hue += 20 ) {
-                var l2 = 1000-(l+500)%950;
-                var l3 =
-                self.rgbToGrey(self.adjustRGBBrightness(self.hslToRgb(hue/360, 0.95, .6), Math.sqrt((1000-l*0.95)/1000)));
-                this.start('th').style({xxxcolor: 'black', color: 'black' /*self.hl(hue+180,l2)*/, width: '80px', height: '80px', background: self.hl(hue, l)}).add(Math.floor(100*l3)).end();
+               var colour = self.hl(hue, l);
+               this.start('th').style({color: 'black', width: '40px', height: '80px', background: self.rgbToString(colour)}).add(l).end();
               }
             }).end();
           }
