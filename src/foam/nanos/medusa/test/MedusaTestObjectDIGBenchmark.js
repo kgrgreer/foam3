1d1c6c22485ea5fd94570eff4de83f67028d97c5
Joel Hughes <jlhughes@users.noreply.github.com>
2021-12-06 20:09:51 -0500

    Medusa test object dig benchmark (#1229)
    * Replace machineId's use of InetAdddress with hostname hashCode.  InetAddress use in java is not realistic, as it always returns the loopback adapter.
    
    * StdoutLogger add constructor taking X, so not dependent on XLocator
    
    * GeneralException - fix typo errro -> error
    
    * Add DIG MedusaTestObject Benchmark
    
    * WebAgentQueryParser - trap and report explicit parse errors, as these may be parse engine errors, rather than bad data
    
    * DIG - test remove
    
    * DIGBenchmark - abstract benchmark provided common load-balancing and setup support

---
+++ src/foam/nanos/medusa/test/MedusaTestObjectDIGBenchmark.js
@@ -0,0 +1,103 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+foam.CLASS({
+  package: 'foam.nanos.medusa.test',
+  name: 'MedusaTestObjectDIGBenchmark',
+  extends: 'foam.nanos.dig.bench.DIGBenchmark',
+
+  javaImports: [
+    'foam.nanos.auth.LifecycleState',
+    'foam.nanos.auth.User',
+    'foam.nanos.auth.ServiceProvider',
+    'foam.nanos.logger.Logger',
+    'foam.nanos.logger.Loggers',
+    'foam.nanos.session.Session',
+    'foam.nanos.dig.DIG'
+  ],
+
+  javaCode: `
+  String userName_ = this.getClass().getSimpleName();
+  String spid_ = "benchmark";
+  User user_;
+  `,
+
+  methods: [
+    {
+      name: 'setup',
+      javaCode: `
+      DIG dig = new DIG.Builder(x)
+        .setNSpecName("serviceProviderDAO")
+        .setPostURL(getSetupUrl())
+        .setSessionId(getSetupSessionId())
+        .setConnectionTimeout(getConnectionTimeout())
+        .setRequestTimeout(getRequestTimeout())
+        .build();
+
+      ServiceProvider sp = (ServiceProvider) dig.find(x, spid_);
+      if ( sp == null ) {
+        sp = new ServiceProvider();
+        sp.setId(spid_);
+        sp.setDescription(spid_+" Spid");
+        sp = (ServiceProvider) dig.put(x, sp);
+      }
+
+      dig.setNSpecName("userDAO");
+      User user = (User) dig.select(x, 0L, 1L, "userName="+userName_);
+      if ( user == null ) {
+        user = new User();
+        user.setUserName(userName_);
+        user.setFirstName(userName_);
+        user.setLastName("Benchmark");
+        user.setEmail(userName_+"@benchmarktest.net");
+        user.setEmailVerified(true);
+        user.setSpid(spid_);
+        user.setGroup("sme");
+        user_ = (User) dig.put(x, user);
+      } else if ( user.getLifecycleState() == LifecycleState.DELETED ) {
+        user = (User) user.fclone();
+        user.setLifecycleState(LifecycleState.ACTIVE);
+        user = (User) dig.put(x, user);
+      }
+      user_ = user;
+
+      dig.setNSpecName("sessionDAO");
+      Session session = (Session) dig.put(x, new Session.Builder(x).setUserId(user_.getId()).build());
+      setSessionId(session.getId());
+      `
+    },
+    {
+      name: 'execute',
+      javaCode: `
+      DIG dig = new DIG.Builder(x)
+        .setNSpecName("medusaTestObjectDAO")
+        .setPostURL(getUrls()[getNextServerIndex(x)])
+        .setSessionId(getSessionId())
+        .setConnectionTimeout(getConnectionTimeout())
+        .setRequestTimeout(getRequestTimeout())
+        .build();
+
+      dig.put(x, new MedusaTestObject());
+      `
+    },
+    {
+      name: 'teardown',
+      javaCode: `
+      DIG dig = new DIG.Builder(x)
+        .setNSpecName("userDAO")
+        .setPostURL(getSetupUrl())
+        .setSessionId(getSetupSessionId())
+        .setConnectionTimeout(getConnectionTimeout())
+        .setRequestTimeout(getRequestTimeout())
+        .build();
+
+      dig.remove(x, user_.getId());
+
+      // dig.setNSpecName("serviceProviderDAO");
+      // dig.remove(x, spid_);
+      `
+    }
+  ]
+});
