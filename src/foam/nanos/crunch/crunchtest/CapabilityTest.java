75e946f97b3aca46f7551733d280293100111702
Patrick Zanowski <zahnno@gmail.com>
2021-12-17 12:09:30 -0500

    update test

--- src/foam/nanos/crunch/crunchtest/CapabilityTest.java
+++ src/foam/nanos/crunch/crunchtest/CapabilityTest.java
@@ -333,7 +333,7 @@ public class CapabilityTest extends Test {
     ));
 
     test(uj1.getStatus() == CapabilityJunctionStatus.GRANTED, "UserCapabilityJunction Status between user and c1 is still granted: " + uj1.getStatus());
-    test( ! ( c1.getLifecycleState() == LifecycleState.ACTIVE ), "c1 is still active");
+    test(c1.getLifecycleState() == LifecycleState.ACTIVE, "c1 is still active");
 
   }
 
