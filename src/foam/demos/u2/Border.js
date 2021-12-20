fd9af9b75443cd504a757e09846045129514185d
Kevin Greer <kgrgreer@gmail.com>
2021-12-19 22:08:37 -0500

    Move Border test code to own source file.

--- src/foam/demos/u2/Border.js
+++ src/foam/demos/u2/Border.js
@@ -11,9 +11,6 @@
 // Alternatively, containers can create explicit content areas like
 // 'leftPane', 'rightPane', 'header', etc.
 
-// Copy 'E' out of root context for convenience.
-var E = foam.__context__.E.bind(foam.__context__);
-
 // Note that this is just a simple Tab view for demonstration purposes.
 // There's a more complete implementation in the foam.u2 package.
 foam.CLASS({
@@ -129,19 +126,6 @@ foam.CLASS({
   ]
 });
 
-var tabs = Tabs.create().
-  start(Tab, {label: 'Tab 1'}).add('tab 1 contents').end().
-  start(Tab, {label: 'Tab 2'}).add('tab 2 contents').end().
-  start(Tab, {label: 'Tab 3'}).add('Even more contents in tab 3').end();
-
-tabs.write();
-
-
-
-E('br').write();
-E('br').write();
-
-
 
 // TODO: add CardDeck example
 foam.CLASS({
@@ -171,12 +155,6 @@ foam.CLASS({
   ]
 });
 
-Card.create().add('content').tag('br').add('more content').tag('br').add('even more conent').write();
-
-
-
-E('br').write();
-E('br').write();
 
 
 
@@ -214,15 +192,6 @@ foam.CLASS({
     }
   ]
 });
-
-var sb = SampleBorder.create({title: 'Title', footer: 'Footer'});
-sb.add('content');
-sb.write();
-
-
-
-E('br').write();
-E('br').write();
 
 
 
@@ -267,16 +236,6 @@ foam.CLASS({
   ]
 });
 
-var sb = LabelledSection.create({title: 'Title'});
-sb.add('content').br().add('more content');
-sb.write();
-
-
-
-E('br').write();
-E('br').write();
-
-
 
 foam.CLASS({
   name: 'SideLabelledSection',
@@ -317,16 +276,6 @@ foam.CLASS({
     }
   ]
 });
-
-var sb = SideLabelledSection.create({title: 'Title'});
-sb.add('content').br().add('more content');
-sb.write();
-
-
-
-E('br').write();
-E('br').write();
-
 
 
 foam.CLASS({
@@ -435,15 +384,6 @@ foam.CLASS({
   ]
 });
 
-var sb = FoldingSection.create({title: 'Title'}).style({width: '500px'});
-sb.add('content').br().add('more content');
-sb.write();
-
-
-
-E('br').write();
-E('br').write();
-
 
 
 foam.CLASS({
@@ -546,16 +486,6 @@ foam.CLASS({
   */
 });
 
-var sb = MDFoldingSection.create({title: 'Title'}).style({width: '500px'});
-sb.add('content').br().add('more content');
-sb.write();
-
-
-
-E('br').write();
-E('br').write();
-
-
 
 foam.CLASS({
   name: 'SampleSplitContainer',
@@ -584,17 +514,6 @@ foam.CLASS({
   ]
 });
 
-var split = SampleSplitContainer.create();
-split.write();
-split.leftPanel.add('leftContent');
-split.rightPanel.add('rightContent');
-
-
-
-E('br').write();
-E('br').write();
-E('br').write();
-
 
 
 foam.CLASS({
@@ -618,17 +537,6 @@ foam.CLASS({
     }
   ]
 });
-
-var blink = Blink.create();
-blink.add('blinking');
-blink.write();
-
-
-
-E('br').write();
-E('br').write();
-
-
 
 foam.CLASS({
   name: 'Columns',
@@ -670,33 +578,6 @@ foam.CLASS({
   ]
 });
 
-var cols = E().
-  start(Columns).
-    start(Column).add('column 1 contents').end().
-    start(Column).add('column 2 contents').br().add('and more content').end().
-  end().
-  start(Columns).
-    start(Column).add('column 1 contents').end().
-    start(Column).add('column 2 contents').br().add('and more content').end().
-    start(Column).add('column 3 contents').br().add('and more content').end().
-  end().
-  start(Tabs).
-    start(Tab, {label: 'Tab 1'}).add('tab 1 contents').end().
-    start(Tab, {label: 'Tab 2'}).add('tab 2 contents').end().
-    start(Tab, {label: 'Tab 3'}).add('Even more contents in tab 3').end().
-  end().
-  start(foam.u2.Tabs).
-    start(foam.u2.Tab, {label: 'Tab 1'}).add('tab 1 contents').end().
-    start(foam.u2.Tab, {label: 'Tab 2'}).add('tab 2 contents').end().
-    start(foam.u2.Tab, {label: 'Tab 3'}).add('Even more contents in tab 3').end().
-  end().
-  start(Tabs).
-    start(Tab, {label: 'Tab 1'}).add('tab 1 contents').end().
-    start(Tab, {label: 'Tab 2'}).add('tab 2 contents').end().
-    start(Tab, {label: 'Tab 3'}).add('Even more contents in tab 3').end().
-  end();
-cols.write();
-
 
 foam.CLASS({
   name: 'Row',
@@ -712,8 +593,7 @@ foam.CLASS({
     function init() {
       this.addClass();
     },
-    function xxxadd() {
-      debugger;
+    function add() {
       for ( var i = 0 ; i < arguments.length ; i++ ) {
         var c = this.createChild_(Column);
         this.add_(c);
