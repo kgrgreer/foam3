4f56b26941323ee73d9e03c34bf3fde0a8407f49
xuerongNanopay <xuerong@nanopay.net>
2022-01-28 11:58:35 -0500

    reindent files

--- src/foam/nanos/medusa/sf/MedusaSFManager.js
+++ src/foam/nanos/medusa/sf/MedusaSFManager.js
@@ -8,7 +8,7 @@ foam.CLASS({
   package: 'foam.nanos.medusa.sf',
   name: 'MedusaSFManager',
   extends: 'foam.box.sf.SFManager',
-
+  
   javaImports: [
     'foam.core.X',
     'foam.box.sf.SFManager',
@@ -21,7 +21,7 @@ foam.CLASS({
     'foam.util.retry.RetryForeverStrategy',
     'java.util.PriorityQueue',
   ],
-
+  
   properties: [
     {
       class: 'Int',
@@ -34,9 +34,9 @@ foam.CLASS({
       class: 'FObjectProperty',
       of: 'foam.util.retry.RetryStrategy',
       javaFactory: `
-        return (new RetryForeverStrategy.Builder(null))
-          .setRetryDelay(1000)
-          .build();
+      return (new RetryForeverStrategy.Builder(null))
+      .setRetryDelay(1000)
+      .build();
       `
     },
     {
@@ -45,44 +45,44 @@ foam.CLASS({
       value: 4096
     },
   ],
-
+  
   methods: [
     {
       documentation: 'override start method for medusa',
       name: 'start',
       javaCode: `
-        super.start();
-
-        final SFManager manager = this;
-        X context = getX();
-        ClusterConfigSupport support = (ClusterConfigSupport) getX().get("clusterConfigSupport");
-        ClusterConfig myConfig = support.getConfig(getX(), support.getConfigId());
-        if ( myConfig.getType() == MedusaType.MEDIATOR ) {
-          for ( ClusterConfig config : support.getSfBroadcastMediators() ) {
-            try {
-              getLogger().info("findMediator: " + config.getId());
-              if ( config.getId().equals(myConfig.getId()) ) continue;
-  
-              SF sf = new SFMedusaClientDAO.Builder(context)
-                        .setId(config.getId())
-                        .setFileName(config.getId())
-                        .setTimeWindow(getMedusaTimeWindow())
-                        .setFileCapacity(getMedusaFileCapacity())
-                        .setRetryStrategy(getMedusaRetryStrategy())
-                        .setMyConfig(myConfig)
-                        .setToConfig(config)
-                        .setManager(manager)
-                        .build();
-              sf.setX(context);
-              sf.initial(context);
-              sf.setReady(true);
-              getSfs().put(sf.getId(), sf);
-              getLogger().info("Initialize successfully: " + sf.getId());
-            } catch ( Throwable t ) {
-              getLogger().error(config.getId(), t);
-            }
+      super.start();
+      
+      final SFManager manager = this;
+      X context = getX();
+      ClusterConfigSupport support = (ClusterConfigSupport) getX().get("clusterConfigSupport");
+      ClusterConfig myConfig = support.getConfig(getX(), support.getConfigId());
+      if ( myConfig.getType() == MedusaType.MEDIATOR ) {
+        for ( ClusterConfig config : support.getSfBroadcastMediators() ) {
+          try {
+            getLogger().info("findMediator: " + config.getId());
+            if ( config.getId().equals(myConfig.getId()) ) continue;
+            
+            SF sf = new SFMedusaClientDAO.Builder(context)
+            .setId(config.getId())
+            .setFileName(config.getId())
+            .setTimeWindow(getMedusaTimeWindow())
+            .setFileCapacity(getMedusaFileCapacity())
+            .setRetryStrategy(getMedusaRetryStrategy())
+            .setMyConfig(myConfig)
+            .setToConfig(config)
+            .setManager(manager)
+            .build();
+            sf.setX(context);
+            sf.initial(context);
+            sf.setReady(true);
+            getSfs().put(sf.getId(), sf);
+            getLogger().info("Initialize successfully: " + sf.getId());
+          } catch ( Throwable t ) {
+            getLogger().error(config.getId(), t);
           }
         }
+      }
       `
     }
   ]
--- src/foam/nanos/medusa/sf/SFMedusaClientDAO.js
+++ src/foam/nanos/medusa/sf/SFMedusaClientDAO.js
@@ -5,85 +5,89 @@
  */
 
 foam.CLASS({
-    package: 'foam.nanos.medusa.sf',
-    name: 'SFMedusaClientDAO',
-    extends: 'foam.box.sf.SFDAO',
+  package: 'foam.nanos.medusa.sf',
+  name: 'SFMedusaClientDAO',
+  extends: 'foam.box.sf.SFDAO',
   
-    javaImports: [
-      'foam.core.FObject',
-      'foam.dao.DAO',
-      'foam.nanos.medusa.ClusterConfig',
-      'foam.nanos.medusa.ClusterConfigSupport',
-      'foam.box.sf.SFEntry',
-      'foam.box.sf.SFManager'
-    ],
+  javaImports: [
+    'foam.core.FObject',
+    'foam.dao.DAO',
+    'foam.nanos.medusa.ClusterConfig',
+    'foam.nanos.medusa.ClusterConfigSupport',
+    'foam.box.sf.SFEntry',
+    'foam.box.sf.SFManager'
+  ],
   
-    properties: [
-      {
-        name: 'myConfig',
-        class: 'FObjectProperty',
-        of: 'foam.nanos.medusa.ClusterConfig'
-      },
-      {
-        name: 'toConfig',
-        class: 'FObjectProperty',
-        of: 'foam.nanos.medusa.ClusterConfig'
-      },
-      {
-        name: 'serviceName',
-        class: 'String',
-        javaFactory: `
-        return "sfBroadcastReceiverDAO";
-        `
-      },
-      {
-        class: 'Proxy',
-        of: 'foam.dao.DAO',
-        name: 'delegate',
-        transient: true,
-        javaSetter: `
-        if ( ! delegateIsSet_ ) {
-          delegate_ = val;
-          delegateIsSet_ = true;
-        }
-        `,
-        javaGetter: ` 
-          ClusterConfigSupport support = (ClusterConfigSupport) getX().get("clusterConfigSupport");
-          DAO dao = support.getBroadcastClientDAO(getX(), getServiceName(), getMyConfig(), getToConfig());
-          return dao;
-        `
+  properties: [
+    {
+      name: 'myConfig',
+      class: 'FObjectProperty',
+      of: 'foam.nanos.medusa.ClusterConfig'
+    },
+    {
+      name: 'toConfig',
+      class: 'FObjectProperty',
+      of: 'foam.nanos.medusa.ClusterConfig'
+    },
+    {
+      name: 'serviceName',
+      class: 'String',
+      javaFactory: `
+      return "sfBroadcastReceiverDAO";
+      `
+    },
+    {
+      class: 'Proxy',
+      of: 'foam.dao.DAO',
+      name: 'delegate',
+      transient: true,
+      javaSetter: `
+      if ( ! delegateIsSet_ ) {
+        delegate_ = val;
+        delegateIsSet_ = true;
       }
-    ],
+      `,
+      javaGetter: ` 
+      ClusterConfigSupport support = (ClusterConfigSupport) getX().get("clusterConfigSupport");
+      DAO dao = support.getBroadcastClientDAO(getX(), getServiceName(), getMyConfig(), getToConfig());
+      return dao;
+      `
+    }
+  ],
   
-    methods: [
-      {
-        name: 'put',
-        code: function() {},
-        swiftCode: '// NOOP',
-        javaCode: `
+  methods: [
+    {
+      name: 'put',
+      code: function() {},
+      swiftCode: '// NOOP',
+      javaCode: `
+      return this.storeAndForward((FObject) obj);     
         return this.storeAndForward((FObject) obj);     
-        `
-      },
-      {
-        name: 'submit',
-        args: 'Context x, SFEntry entry',
-        javaCode: `
-        getDelegate().put(entry);
-        `
-      },
-      {
-        name: 'put_',
-        code: function() {},
-        swiftCode: '// NOOP',
-        javaCode: `
+      return this.storeAndForward((FObject) obj);     
+      `
+    },
+    {
+      name: 'submit',
+      args: 'Context x, SFEntry entry',
+      javaCode: `
+      getDelegate().put(entry);
+      `
+    },
+    {
+      name: 'put_',
+      code: function() {},
+      swiftCode: '// NOOP',
+      javaCode: `
+      return this.storeAndForward((FObject) obj);      
         return this.storeAndForward((FObject) obj);      
-        `
-      },
-      {
-        name: 'createDelegate',
-        documentation: 'creating delegate when start up',
-        javaCode: `
-        `
-      },
-    ]
-  })
+      return this.storeAndForward((FObject) obj);      
+      `
+    },
+    {
+      name: 'createDelegate',
+      documentation: 'creating delegate when start up',
+      javaCode: `
+      `
+    },
+  ]
+})
