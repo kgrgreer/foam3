248e70c8ec9814ec8a6c8ed370c671eff0ce0bee
Christine <wonhwalee52@gmail.com>
2022-02-04 16:49:18 -0500

    Add validation to Start Date and End Date

--- src/foam/nanos/cron/SimpleIntervalSchedule.js
+++ src/foam/nanos/cron/SimpleIntervalSchedule.js
@@ -4,7 +4,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
- foam.CLASS({
+foam.CLASS({
   package: 'foam.nanos.cron',
   name: 'SimpleIntervalSchedule',
 
@@ -12,10 +12,28 @@
     'foam.nanos.cron.Schedule'
   ],
 
+  messages: [
+    { name: 'START_DATE_ERROR', message: 'Start Date must be after today' },
+    { name: 'ENDS_ON_ERROR', message: 'End Date must be after start date' },
+    { name: 'INVALID_DATE_ERROR', message: 'Please provide the date' },
+    { name: 'MAX_DATE_ERROR', message: 'Date cannot be after maximum date' }
+  ],
+
   properties: [
     {
       class: 'Date',
-      name: 'startDate'
+      name: 'startDate',
+      validateObj: function(startDate) {
+        if ( ! startDate ) return this.INVALID_DATE_ERROR;
+
+        // check against max date : set max date 350 years in the future
+        const maxDate = new Date(Date.now() + 1000 * 3600 * 24 * 365 * 350);
+        const df = maxDate.getFullYear() + '/' + (maxDate.getMonth() + 1) + '/' + maxDate.getDate();
+        if ( startDate > maxDate ) return this.MAX_DATE_ERROR + ' [' + df + ']';
+
+        // check against current date
+        if ( startDate <= new Date() ) return this.START_DATE_ERROR;
+      }
     },
     {
       class: 'Int',
@@ -57,7 +75,7 @@
       },
       visibility: function(frequency) {
         if ( frequency != 'Week' )
-           return foam.u2.DisplayMode.HIDDEN;
+          return foam.u2.DisplayMode.HIDDEN;
         return foam.u2.DisplayMode.RW;
       }
     },
@@ -65,12 +83,12 @@
       name: 'monthlyChoice',
       label: '',
       view: {
-       class: 'foam.u2.view.RadioView',
-       choices: [
-         [1, 'On The...'],
-         [2, 'Each']
+        class: 'foam.u2.view.RadioView',
+        choices: [
+          [1, 'On The...'],
+          [2, 'Each']
         ],
-       isHorizontal: true
+        isHorizontal: true
       },
       visibility: function(frequency) {
         if ( frequency != 'Month')
@@ -93,7 +111,7 @@
         if ( frequency != 'Month' )
           return foam.u2.DisplayMode.HIDDEN;
         if ( monthlyChoice != 2 )
-           return foam.u2.DisplayMode.HIDDEN;
+          return foam.u2.DisplayMode.HIDDEN;
         return foam.u2.DisplayMode.RW;
       }
     },
@@ -116,7 +134,7 @@
         if ( frequency != 'Month' )
           return foam.u2.DisplayMode.HIDDEN;
         if ( monthlyChoice !=  1 )
-           return foam.u2.DisplayMode.HIDDEN;
+          return foam.u2.DisplayMode.HIDDEN;
         return foam.u2.DisplayMode.RW;
       }
     },
@@ -139,7 +157,7 @@
         if ( frequency != 'Month' )
           return foam.u2.DisplayMode.HIDDEN;
         if ( monthlyChoice != 1 )
-           return foam.u2.DisplayMode.HIDDEN;
+          return foam.u2.DisplayMode.HIDDEN;
         return foam.u2.DisplayMode.RW;
       }
     },
@@ -170,6 +188,17 @@
         if ( ends != 'On' )
           return foam.u2.DisplayMode.HIDDEN;
         return foam.u2.DisplayMode.RW;
+      },
+      validateObj: function(endsOn) {
+        if ( ! endsOn ) return this.INVALID_DATE_ERROR;
+
+        // check against max date : set max date 350 years in the future
+        const maxDate = new Date(Date.now() + 1000 * 3600 * 24 * 365 * 350);
+        const df = maxDate.getFullYear() + '/' + (maxDate.getMonth() + 1) + '/' + maxDate.getDate();
+        if ( endsOn > maxDate ) return this.MAX_DATE_ERROR + ' [' + df + ']';
+
+        // check against start date
+        if ( endsOn <= this.startDate ) return this.ENDS_ON_ERROR;
       }
     },
     {
