7928f991ca5add576a500f2177458ce33a4f74aa
Jin <jin@jin.local>
2022-02-07 17:16:07 -0500

    Add back RfiTicket but with nothing in it

---
+++ src/foam/nanos/ticket/RfiTicket.js
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.ticket',
+  name: 'RfiTicket',
+  extends: 'foam.nanos.ticket.Ticket',
+  documentation: 'Model was moved to net.nanopay.ticket'
+});
