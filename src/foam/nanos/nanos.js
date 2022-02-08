81b6f88ad2d373eaf381762b3910fea23aea3006
Jin <jin@jin.local>
2022-02-08 03:24:41 -0500

    Add RfiTicket back in nanos.js

--- src/foam/nanos/nanos.js
+++ src/foam/nanos/nanos.js
@@ -446,6 +446,7 @@ FOAM_FILES([
   { name: "foam/nanos/ticket/TicketDAOCreateView" },
   { name: "foam/nanos/ticket/TicketSummaryView" },
   { name: "foam/nanos/ticket/SummaryCard" },
+  { name: "foam/nanos/ticket/RfiTicket", flags: [ "java" ] },
 
   // Dig
   { name: "foam/nanos/dig/bench/DIGBenchmark", flags: [ "java" ] },
