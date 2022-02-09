b16f5e6706f51c60d1a494b0e554d666eb4c09b0
Kevin Greer <kgrgreer@gmail.com>
2022-02-09 17:42:11 -0500

    Remove from classes.

--- tools/classes.js
+++ tools/classes.js
@@ -5,15 +5,6 @@
  */
 
 var classes = [
-  /*
-  'foam.nanos.column.NestedPropertiesExpression',
-  'foam.nanos.column.ColumnPropertyValue',
-  'foam.nanos.column.ColumnConfigToPropertyConverter',
-  'foam.nanos.column.ExpressionForArrayOfNestedPropertiesBuilder',
-  */
-
-  'foam.nanos.notification.push.FirebasePushService',
-  'foam.nanos.medusa.Clusterable',
   'foam.nanos.auth.DeletedAwareDummy',
   'foam.nanos.auth.DeletedAware',
   'foam.nanos.auth.HidePropertiesSink',
