64a730ef98844d9766196d9e4e5b7b221215ecda
Kevin Greer <kgrgreer@gmail.com>
2022-02-09 17:58:45 -0500

    Finish remove use of classes.js for FOAM.

--- tools/classes.js
+++ tools/classes.js
@@ -4,33 +4,10 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
-var classes = [
-  'foam.nanos.auth.DeletedAwareDummy',
-  'foam.nanos.auth.DeletedAware',
-  'foam.nanos.auth.HidePropertiesSink',
-  'foam.nanos.auth.UserUserJunction',
-  'foam.nanos.auth.UserUserJunctionId',
-  'foam.nanos.auth.CheckPermissionsSink',
-  'foam.nanos.auth.GroupPermissionJunction',
-  'foam.nanos.auth.GroupPermissionJunctionId',
-  'foam.nanos.auth.EnabledAwareDummy',
-];
-
-
-var abstractClasses = [
-];
-
-
-var skeletons = [
- ];
-
- var proxies = [
- ];
-
 module.exports = {
-    classes:         classes,
+    classes:         [],
     abstractClasses: [],
-    skeletons:       skeletons,
-    proxies:         proxies,
+    skeletons:       [],
+    proxies:         [],
     blacklist:       []
 };
