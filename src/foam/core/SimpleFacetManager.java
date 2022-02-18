e1f1793b59e211e930ebb8b960ed09f7cf3a7d5d
Jin <jin@jin.local>
2022-02-18 13:12:26 -0500

    Learn how to copy and paste

--- src/foam/core/SimpleFacetManager.java
+++ src/foam/core/SimpleFacetManager.java
@@ -9,89 +9,115 @@ package foam.core;
 import java.util.Collections;
 import java.util.Map;
 
-/** Proxy for X interface. **/
-public class ProxyX
-  extends    ContextAwareSupport
-  implements X
+public class SimpleFacetManager
+  implements FacetManager
 {
 
-  public ProxyX() {
-    this(EmptyX.instance());
+  public Object getInstanceOf(Object value, Class type, X x) {
+    return create(type, x);
   }
 
-  public ProxyX(X x) {
-    setX(x);
+  public <T> T create(Class<T> type, X x) {
+    return create(type, Collections.<String, Object>emptyMap(), x);
   }
 
-  public <T> T get(Class<T> key) {
-    return getX().get(key);
+  public <T> T create(Class<T> type, Map<String, Object> args, X x) {
+    if ( type == foam.core.FObject.class ) {
+      Thread.dumpStack();
+      System.err.println("Unable to create FObject.");
+      return null;
+    }
+
+    try {
+      // Automatically load FooImpl if Foo is abstract.
+      // KGR: Why/where do we do this?
+      // KGR: I Think this is wrong. If Foo is Abstract it should be called AbstractFoos
+      if ( java.lang.reflect.Modifier.isAbstract(type.getModifiers()) ) {
+        try {
+          type = (Class<T>) Class.forName(type.getName() + "Impl");
+        } catch (ClassNotFoundException e) {
+          // NOP
+        }
+      }
+
+      T obj = null;
+      try {
+        java.lang.reflect.Method method = type.getMethod("getOwnClassInfo");
+        ClassInfo classInfo = (ClassInfo) method.invoke(null);
+
+        // First check the context for a custom factory for this type of object.
+        // If there's nothing in the context, check the ClassInfo for an axiom
+        // that creates instances of this type of object. Singletons and
+        // multitons are common examples of this type of axiom.
+        Object f = null;
+        if ( x.get(classInfo.getId() + "_Factory") != null ) {
+          f = x.get(classInfo.getId() + "_Factory");
+        } else if ( classInfo.getAxiomsByClass(XArgsFactory.class).size() == 1 ) {
+          f = classInfo.getAxiomsByClass(XArgsFactory.class).get(0);
+        }
+
+        if ( f != null ) {
+          obj = ((XArgsFactory<T>) f).getInstance(args, x);
+        }
+      } catch (NoSuchMethodException e) {
+        // nop
+      } catch (NullPointerException e) {
+        System.err.println("Unable to create "+type.getName());
+        Thread.dumpStack();
+        throw e;
+      }
+
+      if ( obj == null ) {
+        obj = type.newInstance();
+      }
+
+      if ( obj instanceof ContextAware ) ((ContextAware) obj).setX(x);
+
+      if ( obj instanceof FObject ) {
+        for ( Map.Entry<String, Object> entry : args.entrySet() )
+          ((FObject) obj).setProperty(entry.getKey(), entry.getValue());
+      }
+
+      return obj;
+    } catch (Throwable e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
   }
 
-  public Object get(Object name) {
-    return get(this, name);
+  public Object create(String clsName, X x) {
+    return create(clsName, Collections.<String, Object>emptyMap(), x);
   }
 
-  public Object get(X x, Object name) {
-    return getX().get(x, name);
-  }
+  public Object create(String clsName, Map<String, Object> args, X x) {
+    if ( clsName.equals("foam.core.FObject.class") ) {
+      Thread.dumpStack();
+      System.err.println("Unable to create FObject.");
+      return null;
+    }
 
-  public int getInt(Object key) {
-    return getInt(key, 0);
-  }
+    try {
+      Object obj;
+      try { 
+        Object f = x.get(clsName + "_Factory");
+        obj = ((XArgsFactory<?>) f).getInstance(args, x);
+      } catch (NullPointerException e) {
+        System.err.println("Unable to create " + clsName);
+        Thread.dumpStack();
+        throw e;
+      }
+  
+      if ( obj instanceof ContextAware ) ((ContextAware) obj).setX(x);
+  
+      if ( obj instanceof FObject ) {
+        for ( Map.Entry<String, Object> entry : args.entrySet() )
+          ((FObject) obj).setProperty(entry.getKey(), entry.getValue());
+      }
 
-  public int getInt(Object key, int defaultValue) {
-    return getInt(this, key, defaultValue);
-  }
-
-  public int getInt(X x, Object key, int defaultValue) {
-    return getX().getInt(x, key, defaultValue);
-  }
-
-  public boolean getBoolean(Object key) {
-    return getBoolean(key, false);
-  }
-
-  public boolean getBoolean(Object key, boolean defaultValue) {
-    return getBoolean(this, key, defaultValue);
-  }
-
-  public boolean getBoolean(X x, Object key, boolean defaultValue) {
-    return getX().getBoolean(x, key, defaultValue);
-  }
-
-  public X put(Object name, Object value) {
-    setX(getX().put(name, value));
-
-    return this;
-  }
-
-  public X putFactory(Object name, XFactory factory) {
-    setX(getX().putFactory(name, factory));
-
-    return this;
-  }
-
-  public Object getInstanceOf(Object value, Class type) {
-    return ((FacetManager) getX().get("facetManager")).getInstanceOf(value, type, this);
-  }
-
-  public <T> T create(Class<T> type) {
-    return create(type, Collections.<String, Object>emptyMap());
-  }
-
-  public <T> T create(Class<T> type, Map<String, Object> args) {
-    return ((FacetManager) getX().get("facetManager")).create(type, args, this);
-  }
-
-  public Object create(String clsName) {
-    return create(clsName, Collections.<String, Object>emptyMap());
-  }
-
-  public Object create(String clsName, Map<String, Object> args) {
-    return ((FacetManager) getX().get("facetManager")).create(clsName, args, this);
-  }
-
-  public X cd(String path) {
-    return getX().cd(path);
+      return obj;
+    } catch (Throwable e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
   }
 }
