44beb9e9bc03dc67255f09cf86609708af213b69
jinn9 <58435071+jinn9@users.noreply.github.com>
2022-02-23 12:10:28 -0500

    ticket notification citation view clean up (#1446)
    * Remove rfiTicket in foam
    
    * Remove ticketNotificationNotificationCitationView in files.js
    
    Co-authored-by: Jin <jin@jin.local>

--- src/files.js
+++ src/files.js
@@ -1201,6 +1201,5 @@ FOAM_FILES([
   { name: "foam/nanos/ndiff/NDiffJournal", flags: [ "java" ] },
   { name: "foam/nanos/ndiff/NDiffRuntimeDAO", flags: [ "java" ] },
   { name: "foam/u2/view/ComparisonView" },
-  { name: "foam/u2/view/SuggestedTextField", flags: [ "web" ] },
-  { name: "foam/nanos/ticket/TicketNotificationNotificationCitationView" }
+  { name: "foam/u2/view/SuggestedTextField", flags: [ "web" ] }
   ]);
