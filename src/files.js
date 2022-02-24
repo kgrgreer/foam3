01de42fffa7ab61d361f93f60e545ffc068a0511
Tala <abuatalzz@gmail.com>
2022-02-24 11:17:41 -0500

    csv upload for companybranddao

--- src/files.js
+++ src/files.js
@@ -1201,4 +1201,6 @@ FOAM_FILES([
   { name: "foam/nanos/ndiff/NDiffRuntimeDAO", flags: [ "java" ] },
   { name: "foam/u2/view/ComparisonView" },
   { name: "foam/u2/view/SuggestedTextField", flags: [ "web" ] },
+  { name: "foam/demos/csv/CsvUploadDemo", flags: ["web"] }
+
   ]);
