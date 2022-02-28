67adf37c3290a7a704864a1e7fdfd5f2062b9a76
Kevin Greer <kgrgreer@gmail.com>
2022-02-28 17:53:59 -0500

    Formatting.

--- src/foam/nanos/crunch/ServerCrunchService.java
+++ src/foam/nanos/crunch/ServerCrunchService.java
@@ -12,33 +12,36 @@ import foam.dao.ArraySink;
 import foam.dao.DAO;
 import foam.dao.ProxySink;
 import foam.dao.Sink;
-import foam.mlang.predicate.Predicate;
 import foam.mlang.predicate.Nary;
+import foam.mlang.predicate.Predicate;
 import foam.mlang.sink.GroupBy;
-import foam.nanos.NanoService;
 import foam.nanos.approval.Approvable;
 import foam.nanos.approval.ApprovalRequest;
 import foam.nanos.approval.CompositeApprovable;
+import foam.nanos.auth.AuthService;
 import foam.nanos.auth.LifecycleState;
 import foam.nanos.auth.Subject;
 import foam.nanos.auth.User;
-import foam.nanos.auth.AuthService;
 import foam.nanos.crunch.UCJUpdateApprovable;
 import foam.nanos.crunch.ui.PrerequisiteAwareWizardlet;
 import foam.nanos.crunch.ui.WizardState;
 import foam.nanos.dao.Operation;
 import foam.nanos.logger.Logger;
+import foam.nanos.NanoService;
 import foam.nanos.pm.PM;
 import foam.nanos.session.Session;
 import java.lang.Exception;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static foam.mlang.MLang.*;
 import static foam.nanos.crunch.CapabilityJunctionStatus.*;
 
-public class ServerCrunchService extends ContextAwareSupport implements CrunchService, NanoService {
+public class ServerCrunchService
+  extends    ContextAwareSupport
+  implements CrunchService, NanoService
+{
   public static String CACHE_KEY = "CrunchService.PrerequisiteCache";
 
   @Override
@@ -358,9 +361,7 @@ public class ServerCrunchService extends ContextAwareSupport implements CrunchSe
   }
 
   // see documentation in CrunchService interface
-  public boolean hasPreconditionsMet(
-    X sessionX, String capabilityId
-  ) {
+  public boolean hasPreconditionsMet(X sessionX, String capabilityId) {
     // Return false if capability does not exist or is not available
     var capabilityDAO = ((DAO) sessionX.get("capabilityDAO")).inX(sessionX);
     Capability cap = (Capability) capabilityDAO.find(capabilityId);
@@ -815,7 +816,7 @@ public class ServerCrunchService extends ContextAwareSupport implements CrunchSe
           EQ(AgentCapabilityJunction.EFFECTIVE_USER, user.getId())
         );
       }
-    
+
     return result;
   }
 }
