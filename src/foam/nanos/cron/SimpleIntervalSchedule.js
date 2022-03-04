1f4b24a272bb8b78deb893e7b73a3e47e9284365
Neelkanth Patel <neel@nanopay.net>
2022-03-04 15:35:10 -0500

    add logic for getNextScheduledTime

--- src/foam/nanos/cron/SimpleIntervalSchedule.js
+++ src/foam/nanos/cron/SimpleIntervalSchedule.js
@@ -8,6 +8,18 @@ foam.CLASS({
   package: 'foam.nanos.cron',
   name: 'SimpleIntervalSchedule',
 
+  javaImports: [
+    'foam.nanos.cron.MonthlyChoice',
+    'foam.nanos.cron.ScheduleEnd',
+    'foam.time.TimeUnit',
+    'java.time.DayOfWeek',
+    'java.time.LocalDate',
+    'java.time.ZoneId',
+    'java.time.temporal.ChronoUnit',
+    'java.time.temporal.TemporalAdjusters',
+    'java.util.Date'
+  ],
+
   implements: [
     'foam.nanos.cron.Schedule'
   ],
@@ -51,6 +63,8 @@ foam.CLASS({
       }
     },
     {
+      class: 'FObjectArray',
+      of: 'foam.time.DayOfWeek',
       name: 'dayOfWeek',
       label: 'On',
       view: {
@@ -80,6 +94,8 @@ foam.CLASS({
       }
     },
     {
+      class: 'Array',
+      of: 'Int',
       name: 'dayOfMonth',
       label: '',
       view: {
@@ -161,13 +177,199 @@ foam.CLASS({
   methods: [
     {
       name: 'getNextScheduledTime',
-      code: function() {
-        //TODO:
-        return new Date();
-      },
-      javaCode:`
-        //TODO:
-        return new java.util.Date();
+      javaCode: `
+        LocalDate startDate = getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate endsOn = getEndsOn() == null ? null : getEndsOn().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate nextDate = startDate;
+
+        // Check if schedule has ended
+        if ( getEnds() == ScheduleEnd.AFTER && getEndsAfter() == 0 || getEnds() == ScheduleEnd.ON && ! LocalDate.now().isBefore(endsOn) ) {
+          return null;
+        }
+
+        switch (getFrequency()) {
+          case DAY:
+            nextDate = calculateNextDay(x, nextDate, false);
+            break;
+          case WEEK:
+            nextDate = calculateNextWeek(x, nextDate, false, startDate);
+            break;
+          case MONTH:
+            nextDate = calculateNextMonth(x, nextDate, false, startDate);
+            break;
+          case YEAR:
+            nextDate = calculateNextYear(x, nextDate, false);
+            break;
+        }
+
+        // check if next schedulable date is before the schedule's end date if one exists
+        if ( nextDate == null || getEnds() == ScheduleEnd.ON && ! nextDate.isBefore(endsOn) ) {
+          return null;
+        }
+
+        return Date.from(nextDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+      `
+    },
+    {
+      name: 'calculateNextDay',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'nextDate',
+          type: 'LocalDate'
+        },
+        {
+          name: 'applyWait',
+          type: 'boolean'
+        }
+      ],
+      javaCode: `
+        if ( applyWait ) {
+          nextDate = nextDate.plusDays(getRepeat());
+        } 
+        if ( ! nextDate.isAfter(LocalDate.now()) ) {
+          return calculateNextDay(x, nextDate, true);
+        }
+        return nextDate;
+      `
+    },
+    {
+      name: 'calculateNextWeek',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'nextDate',
+          type: 'LocalDate'
+        },
+        {
+          name: 'applyWait',
+          type: 'boolean'
+        },
+        {
+          name: 'startDate',
+          type: 'LocalDate'
+        }
+      ],
+      javaCode: `
+        if ( applyWait ) {
+          nextDate = nextDate.plusWeeks(getRepeat());
+        }
+        LocalDate minDate = getStartOfWeek(x,nextDate).minusDays(1);
+        LocalDate endOfWeek = getEndOfWeek(x,minDate);
+        foam.time.DayOfWeek[] days = getDayOfWeek();
+        if ( days.length == 0 ) {
+          return null;
+        }
+        nextDate = minDate.with(TemporalAdjusters.next(getWeekday(days[0])));
+        for ( int i=1; i < days.length; i++ ) {
+          LocalDate temp = minDate.with(TemporalAdjusters.next(getWeekday(days[i])));
+          if ( temp.isAfter(LocalDate.now()) && ( temp.isBefore(nextDate) || ! nextDate.isAfter(LocalDate.now())) ) {
+            nextDate = temp;
+          }
+        }
+        if ( nextDate.isAfter(startDate) && nextDate.isAfter(LocalDate.now()) && ! nextDate.isAfter(endOfWeek) ) {
+          return nextDate;
+        } else {
+          return calculateNextWeek(x, endOfWeek, true, startDate);
+        }
+      `
+    },
+    {
+      name: 'calculateNextMonth',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'nextDate',
+          type: 'LocalDate'
+        },
+        {
+          name: 'applyWait',
+          type: 'boolean'
+        },
+        {
+          name: 'startDate',
+          type: 'LocalDate'
+        }
+      ],
+      javaCode: `
+        if ( applyWait ) {
+          nextDate = nextDate.plusMonths(getRepeat());
+        }
+        LocalDate start = nextDate.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate end = nextDate.with(TemporalAdjusters.lastDayOfMonth());
+
+        if ( getMonthlyChoice() == MonthlyChoice.EACH ) {
+          nextDate = LocalDate.now();
+
+          for ( Object day : getDayOfMonth() ) {
+            LocalDate temp = start.plusDays(((long)day)-1);
+            if ( temp.isAfter(LocalDate.now()) && ( temp.isBefore(nextDate) || ! nextDate.isAfter(LocalDate.now())) ) {
+              nextDate = temp;
+            }
+          }
+        } else {
+          switch (getSymbolicFrequency()) {
+            case FIRST:
+              nextDate = nextDate.with(TemporalAdjusters.dayOfWeekInMonth(1,getWeekday(getExpandedDayOfWeek())));
+              break;
+            case SECOND:
+              nextDate = nextDate.with(TemporalAdjusters.dayOfWeekInMonth(2,getWeekday(getExpandedDayOfWeek())));
+              break;
+            case THIRD:
+              nextDate = nextDate.with(TemporalAdjusters.dayOfWeekInMonth(3,getWeekday(getExpandedDayOfWeek())));
+              break;
+            case BEFORE_LAST:
+              nextDate = nextDate.with(TemporalAdjusters.lastInMonth(getWeekday(getExpandedDayOfWeek())));
+              nextDate = nextDate.minusDays(7);
+              break;
+            case LAST:
+              nextDate = nextDate.with(TemporalAdjusters.lastInMonth(getWeekday(getExpandedDayOfWeek())));
+              break;
+          }
+        }
+        if ( nextDate.isAfter(startDate) && nextDate.isAfter(LocalDate.now()) && ! nextDate.isAfter(end) ) {
+          return nextDate;
+        }
+        return calculateNextMonth(x, start, true, startDate);
+      `
+    },
+    {
+      name: 'calculateNextYear',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'nextDate',
+          type: 'LocalDate'
+        },
+        {
+          name: 'applyWait',
+          type: 'boolean'
+        }
+      ],
+      javaCode: `
+        if ( applyWait ) {
+          nextDate = nextDate.plusYears(getRepeat());
+        }
+        if ( ! nextDate.isAfter(LocalDate.now() )) {
+          return calculateNextYear(x, nextDate, true);
+        }
+        return nextDate;
       `
     },
 
@@ -190,6 +392,78 @@ foam.CLASS({
       code: function() {
         return this.ends + ' ' + ( this.ends == this.ScheduleEnd.ON ? this.ENDS_ON.format(this.endsOn) : this.endsAfter )
       }
+    },
+    {
+      name: 'getWeekday',
+      type: 'DayOfWeek',
+      args: [
+        {
+          type: 'foam.time.DayOfWeek',
+          name: 'day'
+        }
+      ],
+      javaCode: `
+        if ( day == foam.time.DayOfWeek.MONDAY ) {
+          return DayOfWeek.MONDAY;
+        } else if ( day == foam.time.DayOfWeek.TUESDAY ) {
+          return DayOfWeek.TUESDAY;
+        } else if ( day == foam.time.DayOfWeek.WEDNESDAY ) {
+          return DayOfWeek.WEDNESDAY;
+        } else if ( day == foam.time.DayOfWeek.THURSDAY ) {
+          return DayOfWeek.THURSDAY;
+        } else if ( day == foam.time.DayOfWeek.FRIDAY ) {
+          return DayOfWeek.FRIDAY;
+        } else if ( day == foam.time.DayOfWeek.SATURDAY ) {
+          return DayOfWeek.SATURDAY;
+        }else {
+          return DayOfWeek.SUNDAY;
+        }
+      `
+    },
+    {
+      name: 'postExecution',
+      javaCode: `
+        int endsAfter = getEndsAfter();
+        if ( endsAfter > 0 ) {
+          setEndsAfter(--endsAfter);
+        }
+      `
+    },
+    {
+      name: 'getEndOfWeek',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'date',
+          type: 'LocalDate'
+        }
+      ],
+      javaCode: `
+        // Todo change end of week based off country 
+        return date.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+      `
+    },
+    {
+      name: 'getStartOfWeek',
+      type: 'java.time.LocalDate',
+      args: [
+        {
+          name: 'x',
+          type: 'foam.core.X'
+        },
+        {
+          name: 'date',
+          type: 'LocalDate'
+        }
+      ],
+      javaCode: `
+        // Todo change start of week based off country 
+        return date.minusWeeks(1).with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+      `
     }
   ]
 });
