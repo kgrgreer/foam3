11f4c2751f03eb5a3332712f400b995b956797f4
KernelDeimos <eric@centos1.rack1.local>
2022-04-05 15:09:25 -0400

    Add ReviewWizardlet

---
+++ src/foam/u2/wizard/common/ReviewItem.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright 2022 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.u2.wizard.common',
+  name: 'ReviewItem',
+  extends: 'foam.comics.v2.namedViews.NamedViewInstance',
+
+  properties: [
+    {
+      name: 'border',
+      class: 'foam.u2.ViewSpec'
+    }
+  ]
+});
--- src/pom.js
+++ src/pom.js
@@ -994,6 +994,8 @@ foam.POM({
   { name: "foam/u2/wizard/PrerequisiteWAO", flags: ["web"] },
   { name: "foam/u2/wizard/common/SuccessWizardlet", flags: ["web"] },
   { name: "foam/u2/wizard/common/SuccessWizardletView", flags: ["web"] },
+  { name: "foam/u2/wizard/common/ReviewWizardletView", flags: ["web"] },
+  { name: "foam/u2/wizard/common/ReviewWizardlet", flags: ["web"] },
   { name: "foam/u2/crunch/wizardflow/SkipMode", flags: [ "web", "java" ] },
   { name: "foam/u2/crunch/EasyCrunchWizard", flags: [ "web", "java" ] },
   { name: "foam/graphics/ZoomMapView" },
