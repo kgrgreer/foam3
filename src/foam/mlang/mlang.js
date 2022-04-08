2e51f0f5fc8896edc9607040984cdbb64eceb2cc
Chanmann Lim <chanmannlim@gmail.com>
2022-04-08 17:22:22 -0400

    Fix nested If

--- src/foam/mlang/mlang.js
+++ src/foam/mlang/mlang.js
@@ -4787,9 +4787,11 @@ foam.CLASS({
     {
       name: 'f',
       javaCode: `
-        if ( getPredicate().f(obj) )
-          return getTrueExpr();
-        return getFalseExpr();
+        var expr = getPredicate().f(obj) ? getTrueExpr() : getFalseExpr();
+        if ( expr instanceof If ) {
+          return ((If) expr).f(obj);
+        }
+        return expr;
       `
     },
     {
