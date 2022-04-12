897923435f530d1f6e6105e1a20d45301f4a72c2
noodlemoodle <qiyzhang@icloud.com>
2022-04-12 08:45:32 -0400

    fix

--- src/foam/nanos/auth/User.js
+++ src/foam/nanos/auth/User.js
@@ -323,7 +323,7 @@ foam.CLASS({
     },
     {
       class: 'Boolean',
-      name: 'emailVerified',value:true,
+      name: 'emailVerified',
       includeInDigest: false,
       documentation: 'Determines whether the email address of the User is valid.',
       writePermissionRequired: true,
