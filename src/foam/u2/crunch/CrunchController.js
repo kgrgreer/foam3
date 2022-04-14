746b83ce671ba28bbd93afd1487156bac0fab286
noodlemoodle <qiyzhang@icloud.com>
2022-04-14 11:45:13 -0400

    add assignanonymousagent to default crunch wizard flow as the first step

--- src/foam/u2/crunch/CrunchController.js
+++ src/foam/u2/crunch/CrunchController.js
@@ -57,6 +57,7 @@ foam.CLASS({
     'foam.u2.crunch.wizardflow.DebugContextInterceptAgent',
     'foam.u2.crunch.wizardflow.DebugAgent',
     'foam.u2.crunch.wizardflow.WAOSettingAgent',
+    'foam.u2.wizard.agents.AssignAnonymousAgent',
     'foam.u2.wizard.agents.ConfigureFlowAgent',
     'foam.u2.wizard.agents.DeveloperModeAgent',
     'foam.u2.wizard.agents.StepWizardAgent',
@@ -116,6 +117,7 @@ foam.CLASS({
         return this.Sequence.create(null, x.createSubContext({
           rootCapability: capabilityOrId
         }))
+          .add(this.AssignAnonymousAgent)
           .add(this.DeveloperModeAgent)
           .add(this.ConfigureFlowAgent)
           .add(this.CapabilityAdaptAgent)
