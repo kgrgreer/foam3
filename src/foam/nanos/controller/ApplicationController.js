fa0a5eaef23a07d0decbbb57e8d8d0c51997cdf6
noodlemoodle <qiyzhang@icloud.com>
2022-04-14 14:11:16 -0400

    fix

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -325,7 +325,7 @@ foam.CLASS({
       memorable: true,
       postSet: function(o, n) {
         // do not try to push the menu the first time route is being set
-        if ( ! this.layoutInitialized && n && this.currentMenu?.id != n ) this.pushMenu(n);
+        if ( this.layoutInitialized && n && this.currentMenu?.id != n ) this.pushMenu(n);
       }
     },
     'currentMenu',
