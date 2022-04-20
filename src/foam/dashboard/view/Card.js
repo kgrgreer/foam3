6a69e23d8fae34d9970f89e9f6e66ad1a9c281e0
nanopay-arthur <57911258+nanopay-arthur@users.noreply.github.com>
2022-04-20 14:03:08 -0400

    Address dashboard refactor comments (#1612)
    * address dashboard refactor comments
    
    * update headers

--- src/foam/dashboard/view/Card.js
+++ src/foam/dashboard/view/Card.js
@@ -93,10 +93,10 @@ foam.CLASS({
       this.
         style({
           width: this.slot(function(data$mode, width) {
-            return data$mode == 'config' ? 'initial' : ( width + 'px' );
+            return data$mode == 'config' ? 'inherit' : ( width + 'px' );
           }),
           height: this.slot(function(data$mode, height) {
-            return data$mode == 'config' ? 'initial' : ( height + 'px' );
+            return data$mode == 'config' ? 'inherit' : ( height + 'px' );
           })
         }).
         addClass(this.myClass()).
