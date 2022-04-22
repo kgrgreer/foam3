3ef6e3104a672a5093339124c187dbf542ea29f6
sarthak <81172969+sarthak-marwaha@users.noreply.github.com>
2022-04-22 13:59:52 -0400

    Add some default token values

---
+++ src/foam/u2/CSSTokens.js
@@ -0,0 +1,22 @@
+/**
+* @license
+* Copyright 2022 The FOAM Authors. All Rights Reserved.
+* http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'CSSTokens',
+  documentation: 'Provides defaults tokens for styling FObjects, should be replaced in ctx or refined',
+
+  cssTokens: {
+    'Blue-50': '#D7E4FF',
+    'Blue-100': '#96B8F9',
+    'Blue-200': '#6795EE',
+    'Blue-300': '#366EDC',
+    'Blue-400': '#0A4AC6',
+    'Blue-500': '#04338D',
+    'Blue-600': '#022568',
+    'Blue-700': '#011B4E'
+  }
+});
