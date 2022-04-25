002981f70630fabbc501968473b44b9248c6c924
Lenore Chen <lenore.chen@nanopay.net>
2022-04-25 10:28:05 -0700

    optimization

--- src/foam/nanos/approval/ApprovalRequest.js
+++ src/foam/nanos/approval/ApprovalRequest.js
@@ -685,6 +685,20 @@ foam.CLASS({
       javaCode: `
         return this.classificationEnumIsSet_;
       `
+    },
+    {
+      name: 'appendMemoReverse',
+      type: 'String',
+      code: function(X, memo) {
+        oldMemo = this.memo;
+        newMemo = memo + ' -- '
+                  + X.subject.user.firstName + ' '
+                  + X.subject.user.lastName
+                  + ' ' + (new Date()).toString()
+                  + (oldMemo && '\n')
+                  + oldMemo;
+        return newMemo;
+      }
     }
   ],
 
@@ -1015,12 +1029,7 @@ foam.CLASS({
       code: function(X, memo) {
         var approvedApprovalRequest = this.clone();
         approvedApprovalRequest.status = this.ApprovalStatus.APPROVED;
-        approvedApprovalRequest.memo = memo + ' -- '
-                                        + X.subject.user.firstName + ' '
-                                        + X.subject.user.lastName
-                                        + ' ' + (new Date()).toString()
-                                        + (approvedApprovalRequest.memo && '\n')
-                                        + approvedApprovalRequest.memo;
+        approvedApprovalRequest.memo = this.appendMemoReverse(X, memo);
 
         this.approvalRequestDAO.put(approvedApprovalRequest).then(req => {
           this.approvalRequestDAO.cmd(foam.dao.DAO.RESET_CMD);
@@ -1047,13 +1056,7 @@ foam.CLASS({
       name: 'addMemoL',
       code: function(X, memo) {
         var newMemoRequest = this.clone();
-        newMemoRequest.memo = memo + ' -- '
-                              + X.subject.user.firstName + ' '
-                              + X.subject.user.lastName
-                              + ' ' + (new Date()).toString()
-                              + (newMemoRequest.memo && '\n')
-                              + newMemoRequest.memo;
-
+        newMemoRequest.memo = this.appendMemoReverse(X, memo);
         this.approvalRequestDAO.put(newMemoRequest).then(req => {
           this.approvalRequestDAO.cmd(foam.dao.DAO.RESET_CMD);
           this.tableViewApprovalRequestDAO.cmd(foam.dao.DAO.RESET_CMD);
@@ -1080,12 +1083,7 @@ foam.CLASS({
       code: function(X, memo) {
         var rejectedApprovalRequest = this.clone();
         rejectedApprovalRequest.status = this.ApprovalStatus.REJECTED;
-        rejectedApprovalRequest.memo = memo + ' -- '
-                                      + X.subject.user.firstName + ' '
-                                      + X.subject.user.lastName
-                                      + ' ' + (new Date()).toString()
-                                      + (rejectedApprovalRequest.memo && '\n')
-                                      + rejectedApprovalRequest.memo;
+        rejectedApprovalRequest.memo = this.appendMemoReverse(X, memo);
 
         this.approvalRequestDAO.put(rejectedApprovalRequest).then(o => {
           this.approvalRequestDAO.cmd(foam.dao.DAO.RESET_CMD);
