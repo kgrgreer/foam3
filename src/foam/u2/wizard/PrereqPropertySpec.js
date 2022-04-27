2b812038208d3102e93d01217d38013bddc31e06
Danny Tharma <daniel@nanopay.net>
2022-04-27 16:41:21 -0400

    Added PrereqPropertySpec and ArrayFromPrerequisiteWAO and fixed up pom.js /wizardlet and /wao files

---
+++ src/foam/u2/wizard/PrereqPropertySpec.js
@@ -0,0 +1,16 @@
+foam.CLASS({
+  package: 'foam.u2.wizard',
+  name: 'PrereqPropertySpec',
+
+
+  properties: [
+    {
+      class: 'String',
+      name: 'capabilityId'
+    },
+    {
+      class: 'foam.u2.wizard.PathProperty',
+      name: 'path'
+    }
+  ]
+})
