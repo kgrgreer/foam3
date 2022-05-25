14002c15f5792fee1eaa95d48b26638de77bdc62
Eric Dubé <eric.alex.dube@gmail.com>
2022-05-25 15:58:49 -0400

    [NP-7105] Update CollapseBorder (#1738)
    * Update CollapseBorder
    
    * Remove unused value
    
    * Remove unused import
    
    * Use button for CollapseBorder
    
    * Play with padding
    
    * Change px to em
    
    * Remove unused class
    
    Co-authored-by: KernelDeimos <eric@centos1.rack1.local>

--- src/pom.js
+++ src/pom.js
@@ -219,6 +219,7 @@ foam.POM({
     { name: "foam/u2/borders/SplitScreenGridBorder",                  flags: "web" },
     { name: "foam/u2/borders/LoadingBorder",                          flags: "web|java" },
     { name: "foam/u2/borders/Block",                                  flags: "web" },
+    { name: "foam/u2/borders/MultiBorder",                            flags: "web" },
     { name: "foam/nanos/u2/navigation/IFrameTopNavigation",           flags: "web" },
     { name: "foam/version/VersionTrait",                              flags: "js" },
     { name: "foam/version/VersionedClass",                            flags: "js" },
