d93215279013d3114e05deb789f72753f609b082
KernelDeimos <eric@centos1.rack1.local>
2022-06-06 16:34:03 -0400

    Update formatting and headers

--- src/foam/foobar/CreateDir.js
+++ src/foam/foobar/CreateDir.js
@@ -5,26 +5,26 @@
  */
 
 foam.CLASS({
-    package: 'foam.foobar',
-    name: 'CreateDir',
-    implements: ['foam.core.ContextAgent'],
-    flags: ['node'],
+  package: 'foam.foobar',
+  name: 'CreateDir',
+  implements: ['foam.core.ContextAgent'],
+  flags: ['node'],
 
-    properties: [
-        {
-            name: 'fs_',
-            factory: () => require('fs').promises
-        },
-        {
-            class: 'String',
-            name: 'path',
-            required: true
-        }
-    ],
+  properties: [
+    {
+      name: 'fs_',
+      factory: () => require('fs').promises
+    },
+    {
+      class: 'String',
+      name: 'path',
+      required: true
+    }
+  ],
 
-    methods: [
-        async function execute () {
-            await this.fs_.mkdir(this.path, { recursive: true });
-        }
-    ]
+  methods: [
+    async function execute () {
+      await this.fs_.mkdir(this.path, { recursive: true });
+    }
+  ]
 });
