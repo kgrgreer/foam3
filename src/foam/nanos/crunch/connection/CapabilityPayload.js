88cff4bd4cc0bc049d6578b30f184072550166e0
hchoura <choura.hassene@gmail.com>
2022-06-07 18:12:19 -0400

    fix nspec of capabilityPayloadDAO

--- src/foam/nanos/crunch/connection/CapabilityPayload.js
+++ src/foam/nanos/crunch/connection/CapabilityPayload.js
@@ -7,9 +7,6 @@
 foam.CLASS({
   package: 'foam.nanos.crunch.connection',
   name: 'CapabilityPayload',
-
-  extends: 'foam.nanos.crunch.Capability',
-
   documentation: `
     CapabilityPayload is created with an id for a specific requested capability and its
     associated capabilities mapped to required data objects in the specified in its grant path
