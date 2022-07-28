254588d0b14154806ba26d9a5caea511d4659035
KernelDeimos <eric@centos1.rack1.local>
2022-07-27 22:59:54 -0400

    Add getSessionCache and DAO listener

--- src/foam/nanos/crunch/SessionCrunchCache.js
+++ src/foam/nanos/crunch/SessionCrunchCache.js
@@ -1,10 +1,17 @@
+/**
+ * @license
+ * Copyright 2022 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 foam.CLASS({
   package: 'foam.nanos.crunch',
   name: 'SessionCrunchCache',
 
   javaImports: [
     'java.util.Map',
-    'java.util.List'
+    'java.util.List',
+    'java.util.concurrent.ConcurrentHashMap'
   ],
 
   properties: [
@@ -22,5 +29,17 @@ foam.CLASS({
       javaType: 'Map<String, List<String>>',
       name: 'dependants'
     }
+  ],
+
+  javaCode: `
+    public SessionCrunchCache(int sequenceId) {
+      setSequenceId(sequenceId);
+      setPrerequisites(new ConcurrentHashMap<String, List<String>>());
+      setDependants(new ConcurrentHashMap<String, List<String>>());
+    }
+  `,
+
+  methods: [
+    // ???: Add getPrereqs and getDependents methods in here?
   ]
 });
