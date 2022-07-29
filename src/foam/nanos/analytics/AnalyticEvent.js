0cfb4700be46b229e586337e705a3a442be9e2b8
xuerongNanopay <56833131+xuerongNanopay@users.noreply.github.com>
2022-07-29 10:17:10 -0400

    NP-7569 AnalyticEvent Model Setup (#1918)
    * analyticsEvent model
    
    * format code
    
    * format code

---
+++ src/foam/nanos/analytics/AnalyticEvent.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2022 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.analytics',
+  name: 'AnalyticEvent',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'id'
+    },
+    {
+      class: 'String',
+      name: 'name'
+    },
+    { 
+      class: 'DateTime',     
+      name: 'timestamp',
+    },
+    {
+      class: 'Duration',
+      name: 'duration',
+      units: 's'
+    },
+    {
+      class: 'StringArray',
+      name: 'tags'
+    },
+    {
+      class: 'String',
+      name: 'traceId'
+    },
+    {
+      class: 'Reference',
+      of: 'foam.nanos.session.Session',
+      name: 'sessionId'
+    },
+    {
+      class: 'Object',
+      name: 'objectId'
+    }
+  ]
+})
