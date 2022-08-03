ce91c8e47e7d026a83fc35b7af65c1c824c2bcac
Joel Hughes <jlhughes@users.noreply.github.com>
2022-08-03 14:00:35 -0400

    Revert "Revert "NP-7573.1 Generate java code for AnalyticEvent""

--- src/foam/nanos/pom.js
+++ src/foam/nanos/pom.js
@@ -506,6 +506,7 @@ foam.POM({
     { name: "test/SerializationTestEchoService",                                          flags: "js|java" },
     { name: "analytics/Foldable",                                                         flags: "js|java" },
     { name: "analytics/Candlestick",                                                      flags: "js|java" },
+    { name: "analytics/AnalyticEvent",                                                    flags: "js|java" },
     { name: "test/ClientEchoService",                                                     flags: "js" },
     { name: "crunch/crunchtest/FakeTestObject",                                           flags: "js|java" },
     { name: "crunch/Renewable",                                                           flags: "js|java" },
