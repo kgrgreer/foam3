da51565c5d6e464b7f74b825f4d260777244dffd
KernelDeimos <eric@centos1.rack1.local>
2022-08-05 14:17:25 -0400

    Make it a Predicate

--- src/foam/mlang/mlang.js
+++ src/foam/mlang/mlang.js
@@ -4200,6 +4200,53 @@ foam.CLASS({
   ]
 });
 
+foam.CLASS({
+  package: 'foam.mlang.predicate',
+  name: 'FScriptPredicate',
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+
+  documentation: `
+    Use FScript as an mlang predicate rather than an mlang expression.
+    This makes it possible to use FScript with interfaces that expect
+    a predicate. If the underlying expression does not return a boolean
+    the result is a casting error.
+  `,
+
+  javaImports: [
+    'foam.core.PropertyInfo'
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'query'
+    },
+    {
+      class: 'Object',
+      name: 'prop',
+      javaType: 'PropertyInfo'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'f',
+      code: function(o) {
+        return foam.mlang.predicate.FScript.create({
+          query: this.query,
+          prop: this.prop
+        }).f(o);
+      },
+      javaCode: `
+        var fScriptExpr = new FScript();
+        fScriptExpr.setQuery(getQuery());
+        fScriptExpr.setProp(getProp());
+        return (boolean) fScriptExpr.f(obj);
+      `
+    }
+  ]
+});
+
 
 foam.CLASS({
   package: 'foam.mlang.predicate',
