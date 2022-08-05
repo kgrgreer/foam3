832d5a518c187516ad67c65ed191b229570bf174
sarthak <81172969+sarthak-marwaha@users.noreply.github.com>
2022-08-04 22:42:20 -0400

    Call fromProperty on TextField in SuggestedTextField

--- src/foam/u2/view/SuggestedTextField.js
+++ src/foam/u2/view/SuggestedTextField.js
@@ -54,6 +54,7 @@
   ],
 
   properties: [
+    'prop',
     {
       class: 'String',
       name: 'daoKey'
@@ -100,7 +101,7 @@
     },
     {
       class: 'String',
-      name:'placeholder'
+      name: 'placeholder'
     },
     'inputFocused'
   ],
@@ -108,8 +109,10 @@
   methods: [
     function render() {
       var self = this;
-      var id;
       this.SUPER();
+      var callFromProperty = function() {
+        self.prop && this.fromProperty && this.fromProperty(self.prop);
+      };
       if ( this.autocompleter )
         this.autocompleter.partial$ = this.data$;
 
@@ -121,6 +124,7 @@
         placeholder$: this.placeholder$,
         autocomplete: false
       })
+        .call(callFromProperty)
         .on('focus', () => {
           this.inputFocused = true;
         })
@@ -148,6 +152,9 @@
              })
           .end();
       });
+    },
+    function fromProperty(prop) {
+      this.prop = prop;
     }
   ],
 
