171b506c523de898c2c67381c8cd42c85e5a6187
noodlemoodle <qiyzhang@icloud.com>
2022-08-09 14:10:03 -0700

    add boolean to configure pub analytic evt for wizardlet

--- src/foam/u2/wizard/wizardlet/BaseWizardlet.js
+++ src/foam/u2/wizard/wizardlet/BaseWizardlet.js
@@ -258,6 +258,11 @@ foam.CLASS({
       class: 'Boolean',
       name: 'showTitle',
       value: true
+    },
+    {
+      class: 'Boolean',
+      name: 'pubAnalyticEvt',
+      value: true
     }
   ],
 
