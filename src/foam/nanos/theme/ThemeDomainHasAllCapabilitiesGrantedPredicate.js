be21c4e4d1a3baa06dfed3e4018acee69f945655
petenanopay <95243528+petenanopay@users.noreply.github.com>
2022-08-23 14:09:55 -0400

    add ThemeDomainHasAllCapabilitiesGrantedPredicate

---
+++ src/foam/nanos/theme/ThemeDomainHasAllCapabilitiesGrantedPredicate.js
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright 2020 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.theme',
+  name: 'ThemeDomainHasAllCapabilitiesGrantedPredicate',
+
+  documentation: 'Predicate that returns true if a ThemeDomain for the active theme on X has all top-level capabilities granted',
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
+
+  javaImports: [
+    'javax.servlet.http.HttpServletRequest',
+    'java.util.List',
+
+    'foam.core.X',
+    'foam.dao.DAO',
+    'foam.dao.ArraySink',
+    'foam.util.SafetyUtil',
+
+  ],
+
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+      X x = (X) obj;
+
+      DAO themeDomainDAO = (DAO)x.get("themeDomainDAO");
+      
+      // grab theme from X
+      Theme theme = (Theme) x.get("theme");
+      if ( theme == null ) {
+        theme = ((Themes) x.get("themes")).findTheme(x);
+      }
+
+      // grab themedomain associated with this theme, if possible.
+      HttpServletRequest request = x.get(HttpServletRequest.class);
+      String domain = request != null ? request.getServerName() : null;
+      ThemeDomain themeDomain = ! SafetyUtil.isEmpty(domain) ? (ThemeDomain) themeDomainDAO.find(domain) : null;
+
+      // themedomain SHOULD have been granted at this point
+      if ( themeDomain != null ) {
+        // grab td/capability junctions
+        // this is a SELECT ALL
+        DAO dao = themeDomain.getCapabilities(x).getDAO();
+        List capabilities = ((ArraySink)dao.select(new ArraySink())).getArray();
+
+        // if no capabilities have been assigned,
+        // we behave as if all of them have been granted
+        if ( capabilities.isEmpty() ) {
+          return true;
+        }
+
+        // otherwise we need to find that ALL capabilities
+        // have been granted to evaluate this to true
+
+        // WRONG! CHANGE LATER!
+        return true;
+      }
+
+      throw new RuntimeException("Can't determine themeDomain!!");
+      `
+    }
+  ]
+});
