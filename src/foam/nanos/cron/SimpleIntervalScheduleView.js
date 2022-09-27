c8d7394302469e8ed1bbc241c6119eb3f4e3a453
Christine <wonhwalee52@gmail.com>
2022-09-27 11:41:48 -0400

    Fix field layout on the Set my schedule wizard

--- src/foam/nanos/cron/SimpleIntervalScheduleView.js
+++ src/foam/nanos/cron/SimpleIntervalScheduleView.js
@@ -15,7 +15,7 @@ foam.CLASS({
     ^ {
       width: 100%;
       display: flex;
-      align-self: center;
+      flex-direction: column;
     }
     ^container {
       display: grid;
@@ -26,6 +26,9 @@ foam.CLASS({
     }
     ^halfWidth {
       grid-column: span 6;
+    }
+    ^halfWidth.foam-u2-PropertyBorder {
+      justify-content: flex-start;
     }
   `,
 
@@ -44,27 +47,27 @@ foam.CLASS({
           }
         ).addClass(this.myClass('container'))
         .start(data.START_DATE.__).addClass(this.myClass('fullWidth')).end()
-        .start(data.REPEAT.__).addClass(this.myClass('halfWidth')).end()
-        .start(data.FREQUENCY.__).addClass(this.myClass('halfWidth')).end()
-        .add(this.slot(function(data$frequency) {
-          if ( data$frequency == 'WEEK' )
-            return this.E().add(data.DAY_OF_WEEK.__).addClass(this.myClass('fullWidth'));
-          if ( data$frequency == 'MONTH' )
-            return self.addGrid().addClass(this.myClass('fullWidth'))
-              .start(data.MONTHLY_CHOICE.__).addClass(this.myClass('fullWidth')).end()
-              .start()
-                .addClass(this.myClass('fullWidth'))
-                .add(this.slot(function(data$monthlyChoice) {
-                  if ( ! data$monthlyChoice ) return this.E().show(false);
-                  if ( data$monthlyChoice == 'EACH' )
-                    return this.E().add(data.DAY_OF_MONTH.__);
-                  return self.addGrid()
-                    .start(data.SYMBOLIC_FREQUENCY.__).addClass(this.myClass('halfWidth')).end()
-                    .start(data.EXPANDED_DAY_OF_WEEK.__).addClass(this.myClass('halfWidth')).end();
-                }))
-              .end();
-          return this.E().show(false);
-        }))
+          .start(data.REPEAT.__).addClass(this.myClass('halfWidth')).end()
+          .start(data.FREQUENCY.__).addClass(this.myClass('halfWidth')).end()
+          .add(this.slot(function(data$frequency) {
+            if ( data$frequency == 'WEEK' )
+              return this.E().add(data.DAY_OF_WEEK.__).addClass(this.myClass('fullWidth'));
+            if ( data$frequency == 'MONTH' )
+              return self.addGrid().addClass(this.myClass('fullWidth'))
+                .start(data.MONTHLY_CHOICE.__, { reserveLabelSpace: false }).addClass(this.myClass('fullWidth')).end()
+                .start()
+                  .addClass(this.myClass('fullWidth'))
+                  .add(this.slot(function(data$monthlyChoice) {
+                    if ( ! data$monthlyChoice ) return this.E().show(false);
+                    if ( data$monthlyChoice == 'EACH' )
+                      return this.E().add(data.DAY_OF_MONTH.__);
+                    return self.addGrid()
+                      .start(data.SYMBOLIC_FREQUENCY.__, { reserveLabelSpace: false }).addClass(this.myClass('halfWidth')).end()
+                      .start(data.EXPANDED_DAY_OF_WEEK.__, { reserveLabelSpace: false }).addClass(this.myClass('halfWidth')).end();
+                  }))
+                .end();
+            return this.E().show(false);
+          }))
         .start(data.ENDS.__).addClass(this.myClass('halfWidth')).end()
         .start().addClass(this.myClass('halfWidth')).add(this.slot(function(data$ends) {
           return data$ends ? (data$ends == foam.nanos.cron.ScheduleEnd.ON ? self.data.ENDS_ON.__ : self.data.ENDS_AFTER.__) : '';
