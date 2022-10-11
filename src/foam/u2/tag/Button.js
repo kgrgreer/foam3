ca97ceda4f37bfe39835a9c18b145b8c6cfdc6ce
Alexey Greer <alexey.greer@nanopay.net>
2022-10-11 10:42:31 -0300

    Adressed Comments

--- src/foam/u2/tag/Button.js
+++ src/foam/u2/tag/Button.js
@@ -198,7 +198,7 @@ foam.CLASS({
 
     ^secondary-destructive:active:not(:disabled) {
       background-color: $buttonSecondaryColor$hover;
-      border-color: $red500;
+      border-color: $$destructive500;
     }
 
     ^secondary-destructive:disabled {
