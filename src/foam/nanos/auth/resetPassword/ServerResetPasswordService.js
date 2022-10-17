2993f1bf3a10af50ce167c587c27ca622b3f81e4
noodlemoodle <qiyzhang@icloud.com>
2022-10-17 16:36:24 -0700

    resetpassword fixes

--- src/foam/nanos/auth/resetPassword/ServerResetPasswordService.js
+++ src/foam/nanos/auth/resetPassword/ServerResetPasswordService.js
@@ -10,15 +10,52 @@
   implements: [ 'foam.nanos.auth.resetPassword.ResetPasswordService' ],
 
   javaImports: [
+    'foam.dao.ArraySink',
     'foam.dao.DAO',
     'foam.nanos.auth.AuthorizationException',
+    'foam.nanos.auth.DuplicateEmailException',
     'foam.nanos.auth.email.EmailVerificationService',
     'foam.nanos.auth.Subject',
     'foam.nanos.auth.User',
+    'foam.nanos.auth.UserNotFoundException',
+    'foam.nanos.logger.Logger',
+    'foam.util.SafetyUtil',
+    'java.util.List',
     'static foam.mlang.MLang.*'
   ],
 
   methods: [
+    {
+      name: 'findUser',
+      args: 'Context x, String email, String userName',
+      type: 'foam.nanos.auth.User',
+      javaCode: `
+        DAO userDAO = ((DAO) x.get("localUserDAO")).where(
+          AND(
+            EQ(User.EMAIL, email),
+            EQ(User.LOGIN_ENABLED, true)
+          ));
+        List list = ((ArraySink) userDAO.select(new ArraySink())).getArray();
+        if ( list == null || list.size() == 0 ) {
+          throw new UserNotFoundException();
+        }
+
+        if ( list.size() > 1 ) {
+          ((Logger) x.get("logger")).warning(this.getClass().getSimpleName(), "verifyByCode", "multiple valid users found for", email);
+
+          if ( SafetyUtil.isEmpty(userName) ) throw new DuplicateEmailException();
+
+          list = ((ArraySink) userDAO
+            .where(EQ(User.USER_NAME, userName))
+            .select(new ArraySink()))
+            .getArray();
+          if ( list == null || list.size() == 0 ) {
+            throw new UserNotFoundException();
+          }
+        }
+        return (User) list.get(0);
+      `
+    },
     {
       name: 'resetPassword',
       javaCode: `
@@ -29,12 +66,11 @@
           User systemUser = ((Subject) getX().get("subject")).getUser();
           Subject subject = new Subject.Builder(x).setUser(systemUser).build();
           x = x.put("subject", subject);
-  
-          DAO userDAO = (DAO) x.get("localUserDAO");
-          User user = (User) userDAO.find(EQ(foam.nanos.auth.User.EMAIL, newPasswordObj.getEmail()));
+
+          User user = findUser(x, newPasswordObj.getEmail(), newPasswordObj.getUserName());
           user = (User) user.fclone();
           user.setDesiredPassword(desiredPassword);
-          userDAO.put_(x, user);
+          ((DAO) x.get("localUserDAO")).put_(x, user);
         } else {
           throw new AuthorizationException("Email verification failed");
         }
