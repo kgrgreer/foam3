918d34c2b81682e6d8e806d7e2b45b7cb92c7206
sarthak <81172969+sarthak-marwaha@users.noreply.github.com>
2022-11-03 19:17:25 -0400

    Add Text button style

--- src/foam/color/colorlib.js
+++ src/foam/color/colorlib.js
@@ -227,6 +227,42 @@ foam.LIB({
       return `rgb(${r.toFixed(4)},${g.toFixed(4)},${b.toFixed(4)})`;
     },
 
+    function rgbToHsl(r, g, b) {
+      r /= 255, g /= 255, b /= 255;
+    
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+      var h, s, l = (max + min) / 2;
+    
+      if (max == min) {
+        h = s = 0; // achromatic
+      } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    
+        switch (max) {
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+        }
+    
+        h /= 6;
+      }
+    
+      return  [Math.floor(h * 360), Math.floor(s * 100), Math.floor(l * 100)];
+    },
+
+    function fromHue(str, s, l) {
+      // TODO: Fine for now but we need to normalise green/yellow hues
+      // becuase they seem lighter than everything else
+      colorObj = foam.Color.parse(str);
+      var [r, g, b] = [colorObj.red, colorObj.green, colorObj.blue].map(v => Math.floor(v));
+      console.log(r, g,b)
+      var [h, _, _] = foam.Color.rgbToHsl(r, g, b);
+
+      debugger;
+      return `hsl(${h} ${s}% ${l}%)`;
+    },
+
     function rgbToGrey(rgb /*[0..255,0.255,0.255]*/) /* -> 0..1 */ {
       var [r, g, b] = rgb;
       return 0.299 * r/255 + 0.587 * g/255 + 0.114 * b/255;
