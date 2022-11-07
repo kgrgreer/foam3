f35b6f11096f29d5d4a6c7c0c655bbe532bd2c81
sarthak <81172969+sarthak-marwaha@users.noreply.github.com>
2022-11-07 11:45:53 -0500

    Only add container when actions are available

--- src/foam/u2/dialog/DialogActionsView.js
+++ src/foam/u2/dialog/DialogActionsView.js
@@ -32,19 +32,48 @@ foam.CLASS({
   ],
 
   methods: [
-    function render () {
+    function render() {
       const self = this;
       this
         .add(this.slot(function ( data ) {
           if ( ! data || data.length === 0 ) return this.E();
+
+          // Ensure actions are sorted by button type
+          data.sort((a, b) => {
+            let buttonStyleA = a.action.buttonStyle;
+            let buttomStyleB = b.action.buttonStyle;
+
+            // Adapt strings to enum values
+            if ( typeof buttonStyleA === 'string' ) {
+              buttonStyleA = foam.u2.ButtonStyle[buttonStyleA];
+            }
+            if ( typeof buttonStyleB === 'string' ) {
+              buttonStyleB = foam.u2.ButtonStyle[buttonStyleB];
+            }
+
+            return buttonStyleA.ordinal - buttomStyleB.ordinal;
+          });
+
+          let slots = [];
+          data.forEach(a => {
+            slots.push(a.action.createIsAvailable$(self.__subContext__, a.data));
+          });
+          let s = foam.core.ArraySlot.create({ slots: slots }, self);
+          let anyAvailable = this.slot(function(slots) {
+            for ( let slot of slots ) {
+              if ( slot ) return true;
+            }
+            return false;
+          }, s);
+
           return this.E()
-            .addClass(self.myClass('actions'))
-            .forEach(data, function ( actionRef ) {
+            .enableClass(self.myClass('actions'), anyAvailable)
+            .forEach(data, function( actionRef ) {
               this.start(actionRef.action, {
                 size: 'LARGE',
                 data$: actionRef.data$
-              }).end()
-            })
+              }).end();
+            });
         }));
     }
   ]
