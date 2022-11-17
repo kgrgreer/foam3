2cb3880f70ec4957d7f201c7f18de5908dbec92a
Kevin Greer <kgrgreer@gmail.com>
2022-11-17 10:14:39 -0500

    Fix tooltips. Add suport for repeat() in descending order. Remove JsLib model from Element.js.

--- src/foam/u2/Element.js
+++ src/foam/u2/Element.js
@@ -221,60 +221,6 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.u2',
-  name: 'JsLib',
-
-  documentation: 'Axiom to install a JS Library on demand.',
-
-  constants: {
-    LOADED: {} // loaded libraries
-  },
-
-  properties: [
-    {
-      class: 'String',
-      name: 'src'
-    },
-    {
-      name: 'name',
-      factory: function() { return 'JsLib-' + this.src; }
-    },
-    [ 'priority', 20 ]
-  ],
-
-  methods: [
-    function installInProto(proto) {
-      var oldRender = proto.render, self = this;
-
-      proto.render = async function() {
-        await self.installLib();
-        oldRender.apply(this, arguments);
-      }
-    },
-
-    function installLib() {
-      if ( ! document ) return;
-      var installedStyles = document.installedStyles || ( document.installedStyles = {} );
-      if ( ! this.LOADED[this.name] ) {
-        var self = this;
-        this.LOADED[this.name] = new Promise(function(resolve, reject) {
-          var id = foam.next$UID();
-          let e  = document.createElement('script')
-          e.setAttribute('id', id)
-          e.setAttribute('src', self.src)
-          document.body.appendChild(e);
-          e.onload = function() {
-            resolve(true);
-          };
-        });
-      }
-      return this.LOADED[this.name];
-    }
-  ]
-});
-
-
-foam.CLASS({
-  package: 'foam.u2',
   name: 'DefaultValidator',
 
   documentation: 'Default Element validator.',
@@ -303,7 +249,7 @@ foam.CLASS({
     },
 
     function sanitizeText(text) {
-      if ( ! text ) return text;
+      if ( text === null || text === undefined ) return text;
       text = text.toString();
       if ( text.search(/[&<"']/) == -1 ) return text;
       return text.replace(/[&<"']/g, (m) => {
@@ -1207,7 +1153,6 @@ foam.CLASS({
     },
 
     function initTooltip() {
-return;
       if ( this.tooltip ) {
         this.Tooltip.create({target: this, text$: this.tooltip$});
       } else if ( this.getAttribute('title') ) {
@@ -1823,9 +1768,14 @@ return;
     },
 
     function repeat(s, e, f) {
-      // TODO: support descending
-      for ( var i = s ; i <= e ; i++ ) {
-        f.call(this, i);
+      if ( s <= e ) {
+        for ( var i = s ; i <= e ; i++ ) {
+          f.call(this, i);
+        }
+      } else {
+        for ( var i = s ; i >= e ; i-- ) {
+          f.call(this, i);
+        }
       }
       return this;
     },
