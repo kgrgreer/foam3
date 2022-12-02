ff3a8540d23225220f58a35565f7e101e105054b
Kevin Greer <kgrgreer@gmail.com>
2022-12-02 16:29:42 -0500

    Avoid doing onDAOUpdate on detached/UNLOADED views, part2.

--- src/foam/u2/view/ChoiceView.js
+++ src/foam/u2/view/ChoiceView.js
@@ -334,11 +334,11 @@ foam.CLASS({
         */
 
         var p = this.mode === foam.u2.DisplayMode.RW ?
-          dao.select().then(s => s.array) :
+          dao.limit(150).select().then(s => s.array) :
           dao.find(this.data).then(o => o ? [o] : []);
 
         p.then(a => {
-          if ( a.length > 10 ) {
+          if ( a.length > 100 ) {
             this.warn('Warning: Inefficient to use ChoiceView for large selections. Count: ' + a.length);
             if ( this.prop_ ) {
               this.warn('For Property: ' + this.prop_.name + ' on ' + this.prop_.forClass_);
