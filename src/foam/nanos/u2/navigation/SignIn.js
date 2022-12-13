1f83974cc70c2d6d5c4d3f094182a38d00beddb1
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2022-12-12 19:13:40 -0800

    fix setting loginfailed on signin (#2301)

--- src/foam/nanos/u2/navigation/SignIn.js
+++ src/foam/nanos/u2/navigation/SignIn.js
@@ -184,6 +184,7 @@ foam.CLASS({
           }
           try {
             let logedInUser = await this.auth.login(x, this.identifier, this.password);
+            this.loginFailed = false;
             if ( ! logedInUser ) return;
             if ( this.token_ ) {
               logedInUser.signUpToken = this.token_;
@@ -201,12 +202,12 @@ foam.CLASS({
               if ( ! this.pureLoginFunction ) await this.nextStep();
             }
           } catch (err) {
-              this.loginFailed = true;
               let e = err && err.data ? err.data.exception : err;
               if ( this.DuplicateEmailException.isInstance(e) ) {
                 if ( this.username ) {
                   try {
                     logedInUser = await this.auth.login(x, this.username, this.password);
+                    this.loginFailed = false;
                     this.subject.user = logedInUser;
                     this.subject.realUser = logedInUser;
                     if ( ! this.pureLoginFunction ) await this.nextStep();
