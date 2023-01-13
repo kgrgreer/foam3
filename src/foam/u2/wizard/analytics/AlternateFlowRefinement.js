9f70fefd70f84d0c3b0e3e9e82bad1d8a44d3d6c
KernelDeimos <eric@centos1.rack1.local>
2023-01-13 11:28:33 -0500

    Add analytic event support for AlternateFlow

---
+++ src/foam/u2/wizard/analytics/AlternateFlowRefinement.js
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2022 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.u2.wizard.analytics',
+  name: 'AlternateFlowRefinement',
+  refines: 'foam.u2.wizard.AlternateFlow',
+
+  imports: [
+    'analyticsAgent?'
+  ],
+
+  properties: [
+    {
+      class: 'Boolean',
+      name: 'enableAnalytics'
+    },
+    {
+      class: 'String',
+      name: 'analyticsName'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'refinedMethod_execute_AlternateFlowRefinement',
+      code: foam.u2.wizard.AlternateFlow.getAxiomByName('execute').code
+    },
+    function execute (x) {
+      console.log('what is this', this);
+      if ( this.enableAnalytics && this.analyticsAgent ) {
+        const analyticsEvent = {
+          name: `AlternateFlow_${this.analyticsName}`,
+          tags: ['wizard']
+        };
+        this.analyticsAgent.pub('event', analyticsEvent);
+      } else {
+        console.warn(
+          'analytics is on for this AlternateFlow, but analytics is disabled',
+          this
+        );
+      }
+
+      const result = this.refinedMethod_execute_AlternateFlowRefinement(x);
+      return result;
+    }
+  ]
+});
