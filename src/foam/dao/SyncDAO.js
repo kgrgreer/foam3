d78883f463350c5f4ae97f42bcf56e0f0746cc02
Scott Head <scott@nanopay.net>
2023-01-17 17:32:29 -0500

    Fixing build warnings

--- src/foam/dao/SyncDAO.js
+++ src/foam/dao/SyncDAO.js
@@ -168,7 +168,6 @@ foam.CLASS({
     },
 
     function validate() {
-      this.SUPER();
       if ( ! this.VersionTrait.isSubClass(this.of) ) {
         throw new Error(`SyncDAO.of must have trait foam.version.VersionTrait`);
       }
