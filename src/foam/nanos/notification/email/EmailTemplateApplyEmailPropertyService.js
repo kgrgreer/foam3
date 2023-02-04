2870997c3f84cf89dfcb995e26ffaa14a70d0dff
Joel Hughes <jlhughes@users.noreply.github.com>
2023-02-04 01:23:15 -0500

    Email message spid (#2468)
    * EmailMessage - calculate spid from user
    
    * EventRecord - add created to table columns
    
    * EmailServiceConfig - hide predicate until a better view layout is chosen
    
    * EmailMessage - ServiceProvider - remove dynamic lookup as incompatible with all email uses. Received email does initially have an owner.
    Disable ServiceProviderAware decoratiohn on Received and Processed mail.
    Ruler - move ruler to 'local' DAOs.
    
    * EmailTemplateApplyEmailPropertyService - retrieve spid from user as it may not be set on emailMessage, or could be set to a spid associated with a Theme which may differ from the email user if the user is super user
    
    * add comments

--- src/foam/nanos/notification/email/EmailTemplateApplyEmailPropertyService.js
+++ src/foam/nanos/notification/email/EmailTemplateApplyEmailPropertyService.js
@@ -15,6 +15,7 @@ foam.CLASS({
   ],
 
   javaImports: [
+    'foam.nanos.auth.User',
     'foam.nanos.logger.Logger',
     'foam.nanos.logger.Loggers',
     'foam.util.SafetyUtil',
@@ -38,7 +39,16 @@ foam.CLASS({
       String locale = (String) templateArgs.get("locale");
 
       // STEP 1) Find EmailTemplate
-      EmailTemplate emailTemplate = EmailTemplateSupport.findTemplate(x, templateName, group, locale, emailMessage.getSpid(), templateArgs);
+
+      // retrieve spid from user as it may not be set on emailMessage,
+      // or could be set to a spid associated with a Theme which may differ
+      // from the email user, if the user is super user
+      String spid = emailMessage.getSpid();
+      User user = emailMessage.findUser(x);
+      if ( user != null ) {
+        spid = user.getSpid();
+      }
+      EmailTemplate emailTemplate = EmailTemplateSupport.findTemplate(x, templateName, group, locale, spid, templateArgs);
       if ( emailTemplate == null ) {
         logger.warning("EmailTemplate not found", templateName, group);
         return emailMessage;
