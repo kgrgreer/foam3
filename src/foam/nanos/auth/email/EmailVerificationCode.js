b24bd71f48e1daea69b8e94724ddd16b36727192
Eric Dubé <eric.alex.dube@gmail.com>
2023-02-16 09:33:04 -0500

    [NP-9034] password reset analytics (#2522)
    * Add error method to Analyticable
    
    * Improve account verify and password reset analytics
    
    ---------
    
    Co-authored-by: KernelDeimos <eric@centos1.rack1.local>

--- src/foam/nanos/auth/email/EmailVerificationCode.js
+++ src/foam/nanos/auth/email/EmailVerificationCode.js
@@ -163,9 +163,7 @@ foam.CLASS({
           this.assert(verified, 'verified should be true when no exception was thrown')
           this.codeVerified = verified;
         } catch (error) {
-          this.report('^verify-failure', ['email-verification'], {
-            errorAsString: error.toString()
-          });
+          this.report('^verify-failure', error);
           if ( error?.data?.exception && this.VerificationCodeException.isInstance(error.data.exception) ) {
             this.remainingAttempts = error.data.exception.remainingAttempts;
             this.codeVerified = false;
@@ -231,6 +229,7 @@ foam.CLASS({
           }));
           return true;
         } catch ( err ) {
+          this.error('^resend-verification-failed', err);
           this.assert('false', 'exception when resending verification', err.message);
           this.ctrl.add(this.NotificationMessage.create({
             err: err.data,
