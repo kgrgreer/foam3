62c740bb125d60334e22b12929656a1f3c1b3fc5
Jin <jin@nanopay.net>
2023-03-03 16:14:55 -0500

    Improve performance

--- src/foam/nanos/auth/SubjectAuthService.js
+++ src/foam/nanos/auth/SubjectAuthService.js
@@ -15,15 +15,27 @@ foam.CLASS({
     'foam.nanos.auth.Subject',
     'foam.nanos.auth.User',
     'foam.util.Auth',
-    'java.util.Map',
-    'java.util.HashMap'
+    'java.util.List',
+    'java.util.HashMap',
+    'java.util.Map'
   ],
 
   methods: [
     {
       name: 'check',
       javaCode: `
+        List<User> userPath = ((Subject) x.get("subject")).getUserPath();
+        if ( userPath.size() == 0 ) return false;
+        
         if ( getDelegate().check(x, permission) ) return true;
+
+        if ( userPath.size() == 1 ) return false;
+
+        if ( userPath.size() == 2 ) {
+          if ( userPath.get(0).getId() == userPath.get(1).getId() ) {
+            return false;
+          }
+        }
 
         Map<Long, Boolean> seen = new HashMap<>();
         for ( User user : ((Subject) x.get("subject")).getUserPath() ) {
