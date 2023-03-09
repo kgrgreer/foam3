2e3138fbe45150be02e6a8ef429df48996b5b46f
Kevin Greer <kgrgreer@gmail.com>
2023-03-09 10:28:54 -0500

    Create C3 class.

---
+++ src/foam/nanos/c3/C3.js
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright 2023 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.c3',
+  name: 'C3',
+  extends: 'foam.u2.Controller',
+
+  methods: [
+    function render() {
+      this.add('C3');
+    }
+  ]
+});
