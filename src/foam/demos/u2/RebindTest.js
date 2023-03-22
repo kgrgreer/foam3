d75a19e1b2ab2736748a70d272cccb97971308bf
Kevin Greer <kgrgreer@gmail.com>
2023-03-22 16:12:05 -0400

    Update U3 test for rebind().

--- src/foam/demos/u2/RebindTest.js
+++ src/foam/demos/u2/RebindTest.js
@@ -6,6 +6,7 @@ foam.CLASS({
 
   methods: [
     function render() {
+      var self = this;
     this.a = 'foo'; this.b = 'bar';
       this.add('A:', this.A, ' B: ', this.B);
 //      this.add('add a:', this.a$, ', b:', this.b$).br();
@@ -15,9 +16,17 @@ foam.CLASS({
       this.recall(function(a) {
         this.add('recalla ', a).br();
       });
+      this.recall(function(a) {
+        if ( a === 'kgr' ) this.add('kgr').br();
+      });
       this.recall(function(b) {
         this.add('recallb ', b).br();
       });
+      this.start('ol').
+        recall(function(b) {
+          b.split(',').forEach(i => this.start('li').add(i).end());
+        }, self).
+      end();
     }
   ]
 });
