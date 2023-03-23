4dcae39a936fef746f3926e910a0a2ae5c2d5597
Kevin Greer <kgrgreer@gmail.com>
2023-03-23 15:09:05 -0400

    Update RebindTest.

--- src/foam/demos/u2/RebindTest.js
+++ src/foam/demos/u2/RebindTest.js
@@ -7,26 +7,32 @@ foam.CLASS({
   methods: [
     function render() {
       var self = this;
-    this.a = 'foo'; this.b = 'bar';
-      this.add('A:', this.A, ' B: ', this.B);
-//      this.add('add a:', this.a$, ', b:', this.b$).br();
+      this.a = 'foo'; this.b = 'a,b,c,d,efg';
+      this.add('A:', this.A, ' B: ', this.B).br().br();
+
       this.recall(function(a, b) {
         this.add('recall a:', a, ', b:', b).br();
       });
+
       this.recall(function(a) {
         this.add('recalla ', a).br();
       });
+
       this.recall(function(a) {
         if ( a === 'kgr' ) this.add('kgr').br();
       });
+
       this.recall(function(b) {
         this.add('recallb ', b).br();
       });
+
       this.start('ol').
         recall(function(b) {
           b.split(',').forEach(i => this.start('li').add(i).end());
         }, self).
-      end();
+      end().
+      
+      add('END').br();
     }
   ]
 });
