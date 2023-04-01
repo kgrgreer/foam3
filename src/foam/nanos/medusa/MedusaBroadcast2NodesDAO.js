29b9fb6565104ec2f37e5361a58befe307ae1a8a
Joel Hughes <joel@nanopay.net>
2023-04-01 00:21:31 -0400

    MedusaBroadcast - listen on ClusterConfigDAO changes and reset client and queue caches

--- src/foam/nanos/medusa/MedusaBroadcast2NodesDAO.js
+++ src/foam/nanos/medusa/MedusaBroadcast2NodesDAO.js
@@ -12,8 +12,10 @@ foam.CLASS({
   documentation: `Broadcast MedusaEntry to Nodes. Each entry is distributed to set of nodes for redundancy and consensus.`,
 
   javaImports: [
+    'foam.core.Detachable',
     'foam.core.FObject',
     'foam.core.X',
+    'foam.dao.AbstractSink',
     'foam.dao.DAO',
     'foam.nanos.logger.Logger',
     'foam.nanos.logger.Loggers',
@@ -52,7 +54,6 @@ foam.CLASS({
       `
     },
     {
-      // TODO: clear on ClusterConfig DAO updates
       name: 'clients',
       class: 'Map',
       javaFactory: 'return new HashMap();'
@@ -61,21 +62,30 @@ foam.CLASS({
       name: 'queues',
       class: 'Map',
       javaFactory: 'return new HashMap();'
-    },
-    {
-      class: 'FObjectProperty',
-      of: 'foam.nanos.logger.Logger',
-      name: 'logger',
-      visibility: 'HIDDEN',
-      transient: true,
-      javaCloneProperty: '//noop',
-      javaFactory: `
-        return Loggers.logger(getX(), this, this.getServiceName());
-      `
-    },
+    }
   ],
 
   methods: [
+    {
+      documentation: 'listen and clear client and queue caches on cluster changes',
+      name: 'init_',
+      javaCode: `
+      DAO dao = (DAO) getX().get("clusterConfigDAO");
+      dao.listen( new AbstractSink() {
+        @Override
+        public void put(Object obj, Detachable sub) {
+          ClusterConfig config = (ClusterConfig) obj;
+          if ( config.getType() == MedusaType.MEDIATOR ) {
+            synchronized ( this ) {
+              ((Logger) getX().get("logger")).info("MedusaBroadcast2NodesDAO,listener,purge");
+              MedusaBroadcast2NodesDAO.CLIENTS.clear(this);
+              MedusaBroadcast2NodesDAO.QUEUES.clear(this);
+            }
+          }
+        }
+      }, null);
+      `
+    },
     {
       documentation: `Distribute entry to each node in one bucket. Mod of entry.id and bucket.size selects the bucket to receive the entry.`,
       name: 'put_',
@@ -90,12 +100,18 @@ foam.CLASS({
       Set<String> bucket = buckets.get(index);
       for ( String id : bucket ) {
         // om.log("medusa.broadcast.nodes", "request");
-        inFlight_.getAndIncrement();
         AssemblyLine queue = (AssemblyLine) getQueues().get(id);
         if ( queue == null ) {
           synchronized ( this ) {
             queue = (AssemblyLine) getQueues().get(id);
             if ( queue == null ) {
+              // Create one AssemblyLine per node and perform dao put under lock
+              // to force node writes to be sequential, but more importantly,
+              // there will only be one retry, rather than every thread retrying.
+              // To be replaced by SAF (Store and Forward)
+              queue = new AsyncAssemblyLine(x, null, support.getThreadPoolName());
+              getQueues().put(id, queue);
+
               ClusterConfig config = support.getConfig(x, id);
               DAO dao = support.getBroadcastClientDAO(x, getServiceName(), myConfig, config);
               // TODO: implment java send in RetryBox and move this to ClusterConfigSupport getSocketClientBox
@@ -105,21 +121,22 @@ foam.CLASS({
                        .setMaxRetryDelay(support.getMaxRetryDelay())
                        .build();
               getClients().put(id, dao);
-              // Create one AssemblyLine per node and perform dao put under lock
-              // to force node writes to be sequential, but more importantly,
-              // there will only be one retry, rather than every thread retrying.
-              // To be replaced by SAF (Store and Forward)
-              queue = new AsyncAssemblyLine(x, null, support.getThreadPoolName());
-              getQueues().put(id, queue);
             }
           }
         }
+        inFlight_.getAndIncrement();
         queue.enqueue(new AbstractAssembly() {
+          // TODO: review use of executeUnderLock
           public void executeUnderLock() {
             // logger.debug("AssemblyLine", "executeUnderLock", id);
-            ((DAO) getClients().get(id)).put_(x, entry);
-            inFlight_.getAndDecrement();
-            ((OMLogger) x.get("OMLogger")).log("medusa.broadcast.mediator-node");
+            try {
+              ((DAO) getClients().get(id)).put_(x, entry);
+            } catch ( Throwable t ) {
+              logger.error("assembly", "executeUnderLock", id, t);
+            } finally {
+              inFlight_.getAndDecrement();
+              ((OMLogger) x.get("OMLogger")).log("medusa.broadcast.mediator-node");
+            }
           }
         });
       }
