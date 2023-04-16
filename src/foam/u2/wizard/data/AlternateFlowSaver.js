192f93c284d867fef6f00e2af278ffea8a66a1d1
noodlemoodle <qiyzhang@icloud.com>
2023-04-16 01:00:35 -0400

    fix alternateflowaction not executing

--- src/foam/u2/wizard/data/AlternateFlowSaver.js
+++ src/foam/u2/wizard/data/AlternateFlowSaver.js
@@ -32,13 +32,18 @@
 
   methods: [
     async function save() {
+      debugger
       // TODO: investigate when this happens
       if ( ! this.wizardController.goNext ) {
         this.alternateFlow.execute(this.__subContext__);
         return;
       }
 
-      var action = this.AlternateFlowAction.create({ alternateFlow: this.alternateFlow });
+      var action = this.AlternateFlowAction.create({
+        alternateFlow: this.alternateFlow,
+        isEnabled: function () { return true; },
+        isAvailable: function () { return true; }
+      });
       var x = this.__subContext__.createSubContext({ data: this.wizardController });
       action.maybeCall(x, this.wizardController);
       return this.delegate.save.call(this, arguments);
