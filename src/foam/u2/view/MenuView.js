31d18e6b43f56a06beea82ae656ff4635baf67b2
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2023-04-21 15:11:32 -0400

    Update src/foam/u2/view/MenuView.js

--- src/foam/u2/view/MenuView.js
+++ src/foam/u2/view/MenuView.js
@@ -32,7 +32,7 @@ foam.CLASS({
       class: 'Function',
       generateJava: false,
       name: 'isEnabled',
-      label: 'Enabled',
+      label: 'Is Enabled',
       documentation: 'Function to determine if button is enabled.',
       value: null
     },
