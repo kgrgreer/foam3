b48bde3aefb86f77fcf4e9c0abad09bb45b780d2
Kevin Greer <kgrgreer@gmail.com>
2023-04-26 09:27:22 -0300

    Spacing.

--- src/foam/nanos/dig/drivers/DigJsonDriver.js
+++ src/foam/nanos/dig/drivers/DigJsonDriver.js
@@ -76,7 +76,7 @@ foam.CLASS({
       if ( x.get(HttpParameters.class).getParameter("fieldValue") != null ) {
         var ret = parseMap(x.get(HttpParameters.class).getParameter("fieldValue"));
         if ( ret != null && ((Map)ret.value()).size() != 0 ) {
-          Map map = (Map)ret.value();
+          Map map  = (Map) ret.value();
           Set keys = map.entrySet();
           for ( Object ob : list ) {
             if ( ob instanceof FObject ) {
