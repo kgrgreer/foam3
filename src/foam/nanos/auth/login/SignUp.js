9c167d4182ad586e21c9f5580f9cd2da3d3ca6d5
noodlemoodle <qiyzhang@icloud.com>
2023-04-27 12:17:34 -0400

    add property availability checks to signup

--- src/foam/nanos/auth/login/SignUp.js
+++ src/foam/nanos/auth/login/SignUp.js
@@ -11,7 +11,10 @@ foam.CLASS({
   messages: [
     { name: 'EMAIL_ERR', message: 'Valid email required' },
     { name: 'PASSWORD_ERR', message: 'Password should be at least 10 characters' },
-    { name: 'USERNAME_ERR', message: 'Username required' }
+    { name: 'USERNAME_ERR', message: 'Username required' },
+    { name: 'USERNAME_AVAILABILITY_ERR', message: 'This username is taken. Please try another.' },
+    { name: 'EMAIL_AVAILABILITY_ERR', message: 'This email is already in use. Please sign in or use a different email' },
+    { name: 'WEAK_PASSWORD_ERR', message: 'Password is weak' }
   ],
 
   properties: [
@@ -25,12 +28,15 @@ foam.CLASS({
           class: 'foam.u2.view.UserPropertyAvailabilityView',
           icon: 'images/checkmark-small-green.svg',
           onKey: true,
+          isAvailable$: X.data.usernameAvailable$,
           inputValidation: /^[^\s\/]+$/,
           restrictedCharacters: /^[^\s\/]$/
         };
       },
-      validateObj: function(username) {
+      validateObj: function(username, usernameAvailable) {
         if ( username.length === 0 ) return this.USERNAME_ERR;
+        // Availability Check
+        if ( ! usernameAvailable ) return this.USERNAME_AVAILABILITY_ERR;
       }
     },
     {
@@ -42,13 +48,16 @@ foam.CLASS({
           class: 'foam.u2.view.UserPropertyAvailabilityView',
           icon: 'images/checkmark-small-green.svg',
           onKey: true,
+          isAvailable$: X.data.emailAvailable$,
           inputValidation: /\S+@\S+\.\S+/,
           restrictedCharacters: /^[^\s]$/,
           displayMode: foam.u2.DisplayMode.RW
         };
       },
-      validateObj: function(email) {
+      validateObj: function(email, emailAvailable) {
         if ( email.length === 0 || ! /\S+@\S+\.\S+/.test(email) ) return this.EMAIL_ERR;
+        // Availability Check
+        if ( ! emailAvailable ) return this.EMAIL_AVAILABILITY_ERR;
       }
     },
     {
@@ -58,15 +67,38 @@ foam.CLASS({
       view: function(_, X) {
         return {
           class: 'foam.u2.view.PasswordView',
+          isAvailable$: X.data.passwordAvailable$,
           passwordIcon: true
         }
       },
-      validateObj: function(desiredPassword) {
+      validateObj: function(desiredPassword, passwordAvailable) {
         if ( ! desiredPassword || desiredPassword.length < 10 ) return this.PASSWORD_ERR;
+        if ( ! passwordAvailable ) return this.WEAK_PASSWORD_ERR;
       }
     },
     {
       name: 'dao_',
+      hidden: true,
+      transient: true
+    },
+    {
+      class: 'Boolean',
+      name: 'emailAvailable',
+      value: true,
+      hidden: true,
+      transient: true
+    },
+    {
+      class: 'Boolean',
+      name: 'usernameAvailable',
+      value: true,
+      hidden: true,
+      transient: true
+    },
+    {
+      class: 'Boolean',
+      name: 'passwordAvailable',
+      value: true,
       hidden: true,
       transient: true
     }
