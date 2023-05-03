8773b9cf723e2324b98e9dddc78eb1170eec1c29
Kevin Greer <kgrgreer@gmail.com>
2023-05-03 16:56:28 -0300

    Initial check-in.

---
+++ doc/templates/Model.js
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * Copyright 2023 The FOAM Authors. All Rights Reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+ foam.CLASS({
+   package: 'com.acme.somepackage', // optional, but should be used for all non-demo code
+   name: 'Thing',
+   extends: 'com.acme.somepackage.SomeObject', // optional, defaults to 'foam.core.FObject'
+
+   implements: [
+
+   ],
+
+   mixins: [
+
+   ],
+
+   plural: 'Things', // optional, defaults to just adding 's' to end of name:
+
+   documentation: `
+     Documentation goes here.
+     Can be multi-line if you quote with back-quotes.
+   `,
+
+   requires: [
+
+   ],
+
+   javaImports: [
+
+   ],
+
+   javaCode: `
+     // Additional free-form code to be added to the generated Java output
+   `,
+
+   axioms: [
+
+   ],
+
+   classes: [
+
+   ],
+
+   constants: [
+
+   ],
+
+   messages: [
+
+   ],
+
+   css: `
+
+   `,
+
+   sections: [
+
+   ],
+
+   tableColumns: [
+
+   ],
+
+   searchColumns: [
+
+   ],
+
+   properties: [
+
+   ],
+
+   methods: [
+
+   ],
+
+   listeners: [
+
+   ],
+
+   actions: [
+
+   ]
+
+ });
