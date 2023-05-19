93702a980bc6acdeabab8f33b0abefab39a572f4
Kevin Greer <kgrgreer@gmail.com>
2023-05-19 09:31:47 -0400

    Work on java compilation in genjava.

--- tools/genjava.js
+++ tools/genjava.js
@@ -38,4 +38,67 @@ for ( var key in foam.USED ) try {
 } catch(x) {}
 
 console.log(`END GENJAVA: ${jCount}/${mCount} models processed, ${X.javaFiles.length} Java files created in ${Math.round((Date.now()-startTime)/1000)}s.`);
-// console.log(X.javaFiles);
+
+/*
+console.log(X.javaFiles);
+console.log(foam.poms);
+
+var fs_   = require('fs');
+var exec_ = require('child_process');
+var found = 0;
+
+function matchJavaSources(pom, f) {
+  if ( f.name.indexOf("AmazonS3") != -1 ) return false;
+  if ( f.name.endsWith('build') ) return false; // temporary
+  return true;
+    // return ! pom.javaSources;
+}
+
+function processDir(pom, location, skipIfHasPOM) {
+  console.log('\tdirectory:', location);
+  var files = fs_.readdirSync(location, {withFileTypes: true});
+
+  if ( skipIfHasPOM && files.find(f => f.name.endsWith('pom.js')) ) return;
+
+  files.forEach(f => {
+    var fn = location + '/' + f.name;
+    if ( f.isDirectory() && ! f.name.startsWith('.') ) {
+      if ( f.name.indexOf('android') != -1 ) return false;
+      if ( f.name.indexOf('examples') != -1 ) return false;
+      processDir(pom, fn, true);
+    } else if ( f.name.endsWith('.java') ) {
+      if ( matchJavaSources(pom, f) ) {
+        console.log('\t\tfile:', fn);
+        found++;
+        X.javaFiles.push(fn);
+      }
+    }
+  });
+}
+
+var seen = {};
+function processPOM(pom) {
+  if ( seen[pom.location] ) return;
+  seen[pom.location] = true;
+  console.log('GENJAVA: Scanning POM for java files:', pom.location);
+  processDir(pom, pom.location, false);
+}
+
+foam.poms.forEach(processPOM);
+console.log(`GENJAVA: Found ${found} java files.`);
+
+console.log(X.javaFiles);
+fs_.writeFileSync('javaFiles', X.javaFiles.join('\n') + '\n'
+);
+var cmd = `javac -parameters -verbose --release 11 -d ./build/classes -classpath "./target/lib/*:./foam3/android/nanos_example_client/gradle/wrapper/gradle-wrapper.jar" @javaFiles`;
+console.log('GENJAVA Executing:', cmd);
+exec_.exec(cmd, [], (error, stdout, stderr) => {
+  if ( error ) {
+    console.log(error);
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
+
+console.log('************ PROCESS', process.env);
+*/
