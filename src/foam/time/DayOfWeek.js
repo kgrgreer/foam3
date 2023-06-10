04765a603438e594f090a4c206b0853969a5fff3
Kevin Greer <kgrgreer@gmail.com>
2023-06-10 14:29:28 -0400

    Formatting.

--- src/foam/time/DayOfWeek.js
+++ src/foam/time/DayOfWeek.js
@@ -16,13 +16,14 @@ foam.ENUM({
       name: 'shortName'
     }
   ],
+
   values: [
-    { name: 'MONDAY',    label: 'Monday', shortName: 'Mon' },
-    { name: 'TUESDAY',   label: 'Tuesday', shortName: 'Tue' },
+    { name: 'MONDAY',    label: 'Monday',    shortName: 'Mon' },
+    { name: 'TUESDAY',   label: 'Tuesday',   shortName: 'Tue' },
     { name: 'WEDNESDAY', label: 'Wednesday', shortName: 'Wed' },
-    { name: 'THURSDAY',  label: 'Thursday', shortName: 'Thu' },
-    { name: 'FRIDAY',    label: 'Friday', shortName: 'Fri' },
-    { name: 'SATURDAY',  label: 'Saturday', shortName: 'Sat' },
-    { name: 'SUNDAY',    label: 'Sunday', shortName: 'Sun' }
+    { name: 'THURSDAY',  label: 'Thursday',  shortName: 'Thu' },
+    { name: 'FRIDAY',    label: 'Friday',    shortName: 'Fri' },
+    { name: 'SATURDAY',  label: 'Saturday',  shortName: 'Sat' },
+    { name: 'SUNDAY',    label: 'Sunday',    shortName: 'Sun' }
   ]
 });
