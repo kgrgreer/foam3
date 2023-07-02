b394f3289f3d1a2c08042954d5ab79f11ccdd559
Kevin Greer <kgrgreer@gmail.com>
2023-07-02 11:35:01 -0400

    Start on M0 GUI.

--- src/foam/demos/m0/M0.js
+++ src/foam/demos/m0/M0.js
@@ -7,8 +7,14 @@
 foam.CLASS({
   package: 'foam.demos.m0',
   name: 'M0',
+  extends: 'foam.u2.Controller',
 
   properties: [
+    {
+      class: 'String',
+      name: 'code',
+      view: { class: 'foam.u2.tag.TextArea', rows: 40, cols: 60 }
+    },
     { name: 'r0' },
     { name: 'r1' },
     { name: 'r2' },
@@ -25,10 +31,26 @@ foam.CLASS({
     { name: 'r13' },
     { name: 'r14' },
     { name: 'r15' },
-    { name: 'r16' },
   ],
 
   methods: [
-
+    function render() {
+      var self = this;
+      this.start().
+        style({display: 'flex'}).
+        start().
+          add(this.CODE).
+        end().
+        start().
+          start('table').
+            call(function() {
+              for ( var i = 0 ; i < 16 ; i++ ) {
+                this.start('tr').start('th').add('R' + i).end().start('td').add(self['R' + i]);
+              }
+            }).
+          end().
+        end().
+      end();
+    }
   ]
 });
