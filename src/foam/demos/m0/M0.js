7a846f2a976215fd176293ab7f1dd5fbbbff67e9
Kevin Greer <kgrgreer@gmail.com>
2023-07-02 12:52:17 -0400

    Start of execution.

--- src/foam/demos/m0/M0.js
+++ src/foam/demos/m0/M0.js
@@ -13,24 +13,31 @@ foam.CLASS({
     {
       class: 'String',
       name: 'code',
-      view: { class: 'foam.u2.tag.TextArea', rows: 40, cols: 60 }
+      view: { class: 'foam.u2.tag.TextArea', rows: 40, cols: 60 },
+      value: `
+          MOV(R0, 100);
+        LABEL('START');
+          ADD(R0, 1);
+        B('START');
+      `
     },
-    { name: 'r0' },
-    { name: 'r1' },
-    { name: 'r2' },
-    { name: 'r3' },
-    { name: 'r4' },
-    { name: 'r5' },
-    { name: 'r6' },
-    { name: 'r7' },
-    { name: 'r8' },
-    { name: 'r9' },
-    { name: 'r10' },
-    { name: 'r11' },
-    { name: 'r12' },
-    { name: 'r13' },
-    { name: 'r14' },
-    { name: 'r15' },
+    { class: 'Int', name: 'r0' },
+    { class: 'Int', name: 'r1' },
+    { class: 'Int', name: 'r2' },
+    { class: 'Int', name: 'r3' },
+    { class: 'Int', name: 'r4' },
+    { class: 'Int', name: 'r5' },
+    { class: 'Int', name: 'r6' },
+    { class: 'Int', name: 'r7' },
+    { class: 'Int', name: 'r8' },
+    { class: 'Int', name: 'r9' },
+    { class: 'Int', name: 'r10' },
+    { class: 'Int', name: 'r11' },
+    { class: 'Int', name: 'r12', shortName: 'IP' },
+    { class: 'Int', name: 'r13', shortName: 'SP' },
+    { class: 'Int', name: 'r14', shortName: 'LR' },
+    { class: 'Int', name: 'r15', shortName: 'PC' },
+    // CPSR
   ],
 
   methods: [
@@ -45,12 +52,43 @@ foam.CLASS({
           start('table').
             call(function() {
               for ( var i = 0 ; i < 16 ; i++ ) {
-                this.start('tr').start('th').add('R' + i).end().start('td').add(self['R' + i]);
+                this.start('tr').
+                  start('th').add('R' + i).end().
+                  start('th').add(self['R' + i].shortName).end().
+                  start('td').add(self['R' + i]).end();
               }
             }).
           end().
         end().
-      end();
+      end().
+      add(this.RUN);
+    },
+
+    function MOV(r, n) {
+      console.log('MOV', r.name, n);
+      r.set(this, n);
+      this.r15 += 16;
+    },
+
+    function ADD(r, n) {
+      console.log('ADD', r.name, n);
+      r.set(this, r.get(this) + n);
+      this.r15 += 16;
+    },
+
+    function LABEL(n) {
+    },
+
+    function B(l) {
+      this.r15 = l;
+    }
+  ],
+
+  actions: [
+    function run() {
+      with ( this ) {
+        eval(this.code);
+      }
     }
   ]
 });
