945a3d4cab017c09d9643c784e33f64d7cd40b4b
Kevin Greer <kgrgreer@gmail.com>
2023-07-02 11:21:35 -0400

    Initial check-in.

---
+++ src/foam/demos/m0/M0.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright 2023 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.demos.m0',
+  name: 'M0',
+
+  properties: [
+    { name: 'r0' },
+    { name: 'r1' },
+    { name: 'r2' },
+    { name: 'r3' },
+    { name: 'r4' },
+    { name: 'r5' },
+    { name: 'r6' },
+    { name: 'r7' },
+    { name: 'r8' },
+    { name: 'r9' },
+    { name: 'r10' },
+    { name: 'r11' },
+    { name: 'r12' },
+    { name: 'r13' },
+    { name: 'r14' },
+    { name: 'r15' },
+    { name: 'r16' },
+  ],
+
+  methods: [
+
+  ]
+});
