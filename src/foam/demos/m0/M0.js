1367f3703e894c90ab88a6b46c3de0a165597539
Kevin Greer <kgrgreer@gmail.com>
2023-07-03 11:53:37 -0400

    Work on M0.

--- src/foam/demos/m0/M0.js
+++ src/foam/demos/m0/M0.js
@@ -6,19 +6,108 @@
 
 foam.CLASS({
   package: 'foam.demos.m0',
+  name: 'Instruction',
+
+  methods: [
+    function emit(m0) {
+      m0.emit(this);
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.demos.m0',
+  name: 'MOV',
+
+  properties: [
+    'dst', 'src'
+  ],
+
+  methods: [
+    function execute(m0) {
+      dst.set(m0, src);
+      m0.r15 += 16;
+    },
+    function toString() {
+      return `MOV(${this.src}, ${this.dst})`;
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.demos.m0',
+  name: 'ADD',
+
+  properties: [
+    'dst', 'amt'
+  ],
+
+  methods: [
+    function execute(m0) {
+      dst.set(m0, dst.get() + this.amt);
+      m0.r15 += 16;
+    },
+    function toString() {
+      return `ADD(${this.src}, ${this.amt})`;
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.demos.m0',
+  name: 'B',
+
+  properties: [
+    'addr' // TODO: add 'label'
+  ],
+
+  methods: [
+    function execute(m0) {
+      m0.ip = this.addr;
+    },
+    function toString() {
+      return `B(${this.addr})`;
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.demos.m0',
   name: 'M0',
   extends: 'foam.u2.Controller',
 
+  imports: [
+    'foam.demos.m0.ADD',
+    'foam.demos.m0.B',
+    'foam.demos.m0.MOV'
+  ],
+
   properties: [
+    {
+      name: 'mem',
+      factory: () => {}
+    },
+    {
+      name: 'labels',
+      factory: () => {}
+    },
+    {
+      class: 'Int',
+      name: 'cp'
+    },
     {
       class: 'String',
       name: 'code',
       view: { class: 'foam.u2.tag.TextArea', rows: 40, cols: 60 },
       value: `
-          MOV(R0, 100);
-        LABEL('START');
-          ADD(R0, 1);
-        B('START');
+  MOV(R0, 100);
+LABEL('START');
+  ADD(R0, 1);
+ B('START');
       `
     },
     { class: 'Int', name: 'r0' },
@@ -61,13 +150,18 @@ foam.CLASS({
           end().
         end().
       end().
-      add(this.RUN);
+      add(this.RUN).
+      start('t3').add('Memory:').end()
+      ;
+    },
+
+    function emit(instr, opt_size) {
+      this.mem[this.cp] = instr;
+      this.cp += opt_size || 16;
     },
 
     function MOV(r, n) {
-      console.log('MOV', r.name, n);
-      r.set(this, n);
-      this.r15 += 16;
+      this.MOV.create({dst: r, src: n}).emit(this);
     },
 
     function ADD(r, n) {
@@ -77,17 +171,25 @@ foam.CLASS({
     },
 
     function LABEL(n) {
+      this.labels[n] = this.cp;
     },
 
     function B(l) {
+      // this.B.create({addr: a}).emit(this);
       this.r15 = l;
     }
   ],
 
   actions: [
-    function run() {
+    function compile() {
       with ( this ) {
         eval(this.code);
+      }
+    },
+    function run() {
+      this.ip = 0;
+      while ( true ) {
+        this.mem[this.ip].execute(this);
       }
     }
   ]
