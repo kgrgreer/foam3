fa2c21e477e41fadcd9c3fdf4a6cfc26450b0f0f
noodlemoodle <qiyzhang@icloud.com>
2023-07-12 14:54:00 -0700

    add wizardletdataaction

--- src/foam/u2/wizard/pom.js
+++ src/foam/u2/wizard/pom.js
@@ -58,6 +58,7 @@ foam.POM({
     { name: "axiom/NullWizardAction",                  flags: "web" },
     { name: "axiom/AlternateFlowAction",               flags: "web" },
     { name: "axiom/GoToMenuAction",                    flags: "web" },
+    { name: "axiom/WizardletDataAction",               flags: "web" },
     { name: "WizardStatus",                            flags: "web" },
     { name: "WizardletIndicator",                      flags: "js|java" },
     { name: "DynamicActionWizardlet",                  flags: "js|java" },
