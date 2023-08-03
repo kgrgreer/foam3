761002d16e3dadde60a39da0968c2c1e4dc21248
Joel Hughes <joel@nanopay.net>
2023-08-03 13:33:15 -0400

    Renewable - refactor Renewable as Interface UserCapabilityJunction - cleanup Created/LastModified Aware Interfaces

--- src/foam/nanos/auth/CreatedByAware.js
+++ src/foam/nanos/auth/CreatedByAware.js
@@ -15,42 +15,8 @@ foam.INTERFACE({
       name: 'createdBy',
       createVisibility: 'HIDDEN',
       updateVisibility: 'RO',
-      documentation: 'User who created the entry'
-    },
-    {
-      class: 'Reference',
-      of: 'foam.nanos.auth.User',
-      name: 'createdByAgent',
-      documentation: 'Agent acting as User who created the entry',
-      createVisibility: 'HIDDEN',
-      updateVisibility: 'RO',
-      columnPermissionRequired: true
-    }
-  ]
-});
-
-/**
- * This refinement is necessary because of the way the class loader works.
- * There wasn't a way in which User could implement CreatedByAware and the
- * CreatedByAware interface to have a reference property without doing this refinement
- */
-foam.CLASS({
-  package: 'foam.nanos.auth',
-  name: 'UserCreatedByAwareRefinement',
-  refines: 'foam.nanos.auth.User',
-
-  implements: [
-    'foam.nanos.auth.CreatedByAware'
-  ],
-
-  properties: [
-    {
-      class: 'Reference',
-      of: 'foam.nanos.auth.User',
-      name: 'createdBy',
       documentation: 'User who created the entry',
-      createVisibility: 'HIDDEN',
-      updateVisibility: 'RO',
+      tableCellFormatter: { class: 'foam.u2.view.ReferenceToSummaryCellFormatter' },
       section: 'userInformation',
       readPermissionRequired: true,
       gridColumns: 6
@@ -62,6 +28,8 @@ foam.CLASS({
       documentation: 'Agent acting as User who created the entry',
       createVisibility: 'HIDDEN',
       updateVisibility: 'RO',
+      columnPermissionRequired: true,
+      tableCellFormatter: { class: 'foam.u2.view.ReferenceToSummaryCellFormatter' },
       section: 'userInformation',
       readPermissionRequired: true,
       gridColumns: 6
