5f6b6040349700d155d385602978163123b8840a
sarthak <sarthak@nanopay.net>
2023-08-08 10:55:04 -0400

    Add clipboardAccess Mixin and a copyonly textfield

---
+++ src/foam/u2/util/ClipboardAccess.js
@@ -0,0 +1,48 @@
+/**
+* @license
+* Copyright 2023 The FOAM Authors. All Rights Reserved.
+* http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+foam.CLASS({
+  package: 'foam.u2.util',
+  name: 'ClipboardAccess',
+  documentation: `Mixin that provides functionality to access web clipboard`,
+
+  imports: ['ctrl','notify'],
+
+  methods: [
+    function copy(data, provideNotifications = true) {
+      let tryCopy = function() {
+        let res = navigator.clipboard ?
+                  navigator.clipboard.writeText(data):
+                  Promise.reject(new Error('No Clipboard Found')) ;
+        if ( provideNotifications ) {
+          res.then(function() {
+            if ( provideNotifications )
+              this.ctrl.notify?.('Copied', '', 'INFO', true);
+          }, (e) => {
+            console.log(e); 
+            if ( provideNotifications )
+              this.ctrl.notify?.('Copy Failed', '', 'ERROR', true);
+          })
+        }
+        return res;
+      }
+      if ( navigator.permissions ) {
+        return navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
+          if (result.state === "granted" || result.state === "prompt") {
+            return tryCopy().bind(this);
+          }
+          if ( provideNotifications )
+            this.ctrl.notify?.('Copy Failed', 'Permission Denied', 'ERROR', true);
+          return Promise.reject(new Error('Permission Denied')) ;
+        })
+      } else {
+        return tryCopy().bind(this);
+      }
+    },
+    function cut() {},
+    function paste() {}
+  ]
+});
