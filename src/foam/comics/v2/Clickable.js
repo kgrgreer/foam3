e4312259bd02e5ca719fc627a83a2822f45628d2
Kevin Greer <kgrgreer@gmail.com>
2023-08-11 08:56:59 -0300

    Simplify Clickable.

--- src/foam/comics/v2/Clickable.js
+++ src/foam/comics/v2/Clickable.js
@@ -22,13 +22,13 @@ foam.CLASS({
   methods: [
     function insertClick(idObj /* expects an object with an id prop*/) {
       var self = this;
-      this.callIf( this.dblclick && ( ! this.config || ! this.config.disableSelection ), function() {
+      this.callIf( this.dblclick && ! ( this.config && this.config.disableSelection ), function() {
         this.on('dblclick', function(evt) {
             if ( self.shouldEscapeEvts(evt) ) return;
             self.dblclick.call(self, null, idObj.id);
         });
       })
-      .callIf( this.click && ( ! this.config || ! this.config.disableSelection ), function() {
+      .callIf( this.click && ! ( this.config && this.config.disableSelection ), function() {
         this.on('click', function(evt) {
           if ( self.shouldEscapeEvts(evt) ) return;
           self.selection = idObj.id;
