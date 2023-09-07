d6d5899dd6847864922ff1e7e6cb55e768af03e4
Kevin Greer <kgrgreer@gmail.com>
2023-09-07 15:11:00 -0300

    Fix bug in translation service with locale variants.

--- src/foam/i18n/ClientCacheTranslationService.js
+++ src/foam/i18n/ClientCacheTranslationService.js
@@ -107,6 +107,7 @@ foam.CLASS({
     },
 
     function addLocale(l) {
+      l.locale = this.locale; // in case it is a locale variant like us-EN
       this.localeEntries[l.source] = l.target;
     },
 
