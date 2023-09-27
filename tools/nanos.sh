5db4fbc93feaca92d3d27ca3d9429c4c8017057b
Kevin Greer <kgrgreer@gmail.com>
2023-09-27 16:18:15 -0300

    Remove old build scripts.

--- tools/nanos.sh
+++
@@ -1,8 +0,0 @@
-# run from parent directory of foam3
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/../../foam3/build/
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
-
-cd ../..
-java -cp `cat foam3/build/cp.txt`:foam3/build/target/foam-1.0-SNAPSHOT.jar -Dfoam.main=main foam.nanos.boot.Boot
