fd2c629b50934efcafa5dce354b1678aef9a0631
Joel Hughes <joel@nanopay.net>
2023-09-29 01:30:57 -0400

    send via AsyncAssemblyLine - return OK on first success

--- src/foam/nanos/http/BroadcastWebAgent.js
+++ src/foam/nanos/http/BroadcastWebAgent.js
@@ -19,9 +19,13 @@ NOTE: do not install nspec with parameters: true`,
     'foam.box.HTTPAuthorizationType',
     'foam.box.socket.SslContextFactory',
     'foam.core.X',
+    'foam.nanos.logger.Logger',
     'foam.nanos.logger.Loggers',
     'foam.nanos.pm.PM',
     'foam.nanos.http.DefaultHttpParameters',
+    'foam.util.concurrent.AbstractAssembly',
+    'foam.util.concurrent.AssemblyLine',
+    'foam.util.concurrent.AsyncAssemblyLine',
     'foam.util.SafetyUtil',
     'java.io.IOException',
     'java.net.CookieHandler',
@@ -33,6 +37,8 @@ NOTE: do not install nspec with parameters: true`,
     'java.net.URI',
     'java.net.URLEncoder',
     'java.time.Duration',
+    'java.util.HashMap',
+    'java.util.Map',
     'java.util.StringTokenizer',
     'javax.net.ssl.SSLContext',
     'javax.servlet.http.HttpServletRequest',
@@ -58,6 +64,11 @@ NOTE: do not install nspec with parameters: true`,
       javaFactory: `
         return new String[] {"Accept-Language", "Content-Type", "User-Agent"};
       `
+    },
+    {
+      documentation: 'Do not wait for a pass or fail on any of hte proxy requests. Immediately return 200',
+      name: 'sendAndForget',
+      class: 'Boolean'
     },
     {
       documentation: 'Connection timeout in milliseconds',
@@ -108,69 +119,119 @@ NOTE: do not install nspec with parameters: true`,
     {
       name: 'execute',
       javaCode: `
-      var req = x.get(HttpServletRequest.class);
-      var resp = x.get(HttpServletResponse.class);
-      var parameters = x.get(HttpParameters.class);
-      try {
-        for ( String to : getTo() ) {
-          if ( ! to.startsWith("http") ) {
-            // use self
-            to = req.getServerName() + ":" + req.getServerPort() + "/" + to;
-            if ( getIsSecure() ) {
-              to = "https://"+ to;
-            }
-            to = "http://"+ to;
-          }
-          String url = buildUrl(x, to, parameters);
-
-          HttpRequest.Builder builder = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .timeout(Duration.ofMillis(getRequestTimeout()));
-
-          // headers
-          for ( String header : getCopyHeaders() ) {
+      final Logger logger = Loggers.logger(x, this);
+      final var req = x.get(HttpServletRequest.class);
+      final var resp = x.get(HttpServletResponse.class);
+      final var parameters = x.get(HttpParameters.class);
+      AssemblyLine line = new AsyncAssemblyLine(x);
+      final Map replies = new HashMap();
+      final Map failures = new HashMap();
+      final Map timeouts = new HashMap();
+      for ( String t : getTo() ) {
+        line.enqueue(new AbstractAssembly() {
+          public void executeJob() {
             try {
-              String value = req.getHeader(header);
-              if ( ! SafetyUtil.isEmpty(value) ) {
-                builder.header(header, value);
+              String to = t;
+              if ( ! to.startsWith("http") ) {
+                // use self
+                to = req.getServerName() + ":" + req.getServerPort() + "/" + to;
+                if ( getIsSecure() ) {
+                  to = "https://"+ to;
+                }
+                to = "http://"+ to;
               }
-            } catch (IllegalArgumentException e) {
-              // nop - ignore restricted headers.
+              String url = buildUrl(x, to);
+
+              HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .timeout(Duration.ofMillis(getRequestTimeout()));
+
+              // headers
+              for ( String header : getCopyHeaders() ) {
+                try {
+                  String value = req.getHeader(header);
+                  if ( ! SafetyUtil.isEmpty(value) ) {
+                    builder.header(header, value);
+                  }
+                } catch (IllegalArgumentException e) {
+                  // nop - ignore restricted headers.
+                }
+              }
+
+              // auth
+              String bearerToken = getBearerToken(req);
+              if ( ! SafetyUtil.isEmpty(bearerToken ) ) {
+                builder = builder.header("Authorization", "BEARER "+bearerToken);
+              }
+
+              // post data
+              if ( Command.PUT.equals(parameters.get("cmd")) ) {
+                String data = parameters.getParameter("data");
+                if ( ! SafetyUtil.isEmpty(data) ) {
+                  builder = builder.POST(HttpRequest.BodyPublishers.ofString(data));
+                }
+              }
+
+              HttpRequest request = builder.build();
+              try {
+                logger.debug("send", url);
+                HttpClient client = client_.get();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if ( response.statusCode() == HttpServletResponse.SC_OK ) {
+                  logger.debug("response", url, response.statusCode(), response.body());
+                  replies.put(url, response.statusCode());
+                } else {
+                  logger.warning("response", url, response.statusCode(), response.body());
+                  failures.put(url, response.statusCode());
+                }
+              } catch (HttpTimeoutException e) {
+                logger.warning(url, e.getMessage());
+                timeouts.put(url, e.getMessage());
+              } catch (java.io.IOException | InterruptedException e) {
+                logger.error(url, e.getMessage());
+                failures.put(url, e.getMessage());
+              }
+            } catch (Throwable t) {
+              logger.error(t);
             }
           }
-
-          // auth
-          String bearerToken = getBearerToken(req);
-          if ( ! SafetyUtil.isEmpty(bearerToken ) ) {
-            builder = builder.header("Authorization", "BEARER "+bearerToken);
-          }
-
-          // post data
-          if ( Command.PUT.equals(parameters.get("cmd")) ) {
-            String data = parameters.getParameter("data");
-            if ( ! SafetyUtil.isEmpty(data) ) {
-              builder = builder.POST(HttpRequest.BodyPublishers.ofString(data));
-            }
-          }
-
-          HttpRequest request = builder.build();
-          try {
-            Loggers.logger(x, this).debug("request", url);
-            HttpClient client = client_.get();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            Loggers.logger(x, this).debug("response", url, response.statusCode(), response.body());
-          } catch (HttpTimeoutException e) {
-            Loggers.logger(x, this).warning(url, e.getMessage());
-          } catch (java.io.IOException | InterruptedException e) {
-            Loggers.logger(x, this).error(url, e.getMessage());
-          }
-        }
-      } catch (Throwable t) {
-        Loggers.logger(x, this).error(t);
-      } finally {
-        resp.setContentType("application/json");
-        resp.setStatus(HttpServletResponse.SC_OK);
+        });
       }
+
+      if ( getSendAndForget() ) {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        return;
+      }
+
+      logger.debug("wait");
+      long waited = 0L;
+      long sleep = 100L;
+      while ( waited < getConnectionTimeout() ) {
+        try {
+          Thread.currentThread().sleep(sleep);
+          waited += sleep;
+        } catch (InterruptedException e) {
+          break;
+        }
+        if ( replies.size() > 0 ) {
+          resp.setStatus(HttpServletResponse.SC_OK);
+          return;
+        }
+        if ( failures.size() == getTo().length ) {
+          logger.warning("failed");
+          resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+          return;
+        }
+        if ( timeouts.size() == getTo().length ) {
+          logger.warning("timeout");
+          resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+          return;
+        }
+      }
+
+      // timeout
+      logger.error("timeout");
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
       `
     },
     {
@@ -193,19 +254,17 @@ NOTE: do not install nspec with parameters: true`,
     },
     {
       name: 'buildUrl',
-      args: 'X x, String to, HttpParameters parameters',
+      args: 'X x, String to',
       type: 'String',
       javaCode: `
+      final var req = x.get(HttpServletRequest.class);
       StringBuilder sb = new StringBuilder();
       sb.append(to);
-      // sb.append("?");
-      // int count = 0;
-      // for ( HttpParameters param : parameters ) {
-      //   if ( "data".equals(param.getName()) ) continue;
-      //   if ( count > 0 ) sb.append("&");
-      //   sb.append(parem.getName(), param.getParameterValue());
-      //   count += 1;
-      // }
+      String query = req.getQueryString();
+      if ( ! SafetyUtil.isEmpty(query) ) {
+        sb.append("?");
+        sb.append(query);
+      }
       return sb.toString();
       `
     }
