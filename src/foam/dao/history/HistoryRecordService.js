1a6c892d159acc72aee01705c8868b015be417cd
Jin <jin@nanopay.net>
2023-10-04 16:19:11 -0400

    Added getRecordById and getRecordsById

--- src/foam/dao/history/HistoryRecordService.js
+++ src/foam/dao/history/HistoryRecordService.js
@@ -18,16 +18,34 @@ foam.INTERFACE({
         Returns the latest record that has 'propertyName' property update
       `,
       async: true,
-      args: 'Context x, foam.dao.DAO dao, String propertyName'
+      args: 'Context x, String daoKey, String propertyName'
+    },
+    {
+      name: 'getRecordById',
+      type: 'HistoryRecord',
+      documentation: `
+        Returns the latest record that has id as it objectId and has 'propertyName' property update
+      `,
+      async: true,
+      args: 'Context x, String daoKey, Object id, String propertyName'
     },
     {
       name: 'getRecords',
-      type: 'HistoryRecord[]',
+      type: 'java.util.List<HistoryRecord>',
       documentation: `
         Returns all the records that have 'propertyName' property update
       `,
       async: true,
-      args: 'Context x, foam.dao.DAO dao, String propertyName'
+      args: 'Context x, String daoKey, String propertyName'
+    },
+    {
+      name: 'getRecordsById',
+      type: 'java.util.List<HistoryRecord>',
+      documentation: `
+        Returns all the records that have id as their objectId and have 'propertyName' property update
+      `,
+      async: true,
+      args: 'Context x, String daoKey, Object id, String propertyName'
     }
   ]
 });
--- src/foam/dao/history/HistoryRecordServiceServer.js
+++ src/foam/dao/history/HistoryRecordServiceServer.js
@@ -15,35 +15,67 @@ foam.CLASS({
     'foam.dao.DAO',
     'foam.mlang.order.Desc',
     'java.util.stream.Collectors',
-    'java.util.List'
+    'java.util.List',
+
+    'static foam.mlang.MLang.*'
   ],
 
   methods: [
     {
       name: 'getRecord',
       type: 'HistoryRecord',
-      args: 'Context x, foam.dao.DAO dao, String propertyName',
+      args: 'Context x, String daoKey, String propertyName',
       javaCode: `
-        if ( dao == null ) {
-          throw new IllegalStateException("DAO does not exist");
-        }
-    
-        if ( ! HistoryRecord.class.isAssignableFrom(dao.getOf().getObjClass()) ) {
-          throw new IllegalStateException("DAO must be of HistoryRecord");
-        }
+        DAO dao = getHistoryDAO(x, daoKey)
+          .orderBy(new Desc(HistoryRecord.TIMESTAMP))
+          .limit(1);
         
-        ArraySink sink = (ArraySink) dao.orderBy(new Desc(HistoryRecord.TIMESTAMP))
-          .limit(1)
-          .select(new ArraySink());
+        List<HistoryRecord> records = getRecords_(x, dao, propertyName);
+        return records.size() > 0 ? records.get(0) : null;
+      `
+    },
+    {
+      name: 'getRecordById',
+      type: 'HistoryRecord',
+      args: 'Context x, String daoKey, Object id, String propertyName',
+      javaCode: `
+        DAO dao = getHistoryDAO(x, daoKey)
+          .where(EQ(HistoryRecord.OBJECT_ID, id))
+          .orderBy(new Desc(HistoryRecord.TIMESTAMP))
+          .limit(1);
         
-        return sink.getArray().size() > 0 ? (HistoryRecord) sink.getArray().get(0) : null;
+        List<HistoryRecord> records = getRecords_(x, dao, propertyName);
+        return records.size() > 0 ? records.get(0) : null;
       `
     },
     {
       name: 'getRecords',
-      type: 'HistoryRecord[]',
-      args: 'Context x, foam.dao.DAO dao, String propertyName',
+      type: 'List<HistoryRecord>',
+      args: 'Context x, String daoKey, String propertyName',
       javaCode: `
+        DAO dao = getHistoryDAO(x, daoKey).orderBy(new Desc(HistoryRecord.TIMESTAMP));
+        return getRecords_(x, dao, propertyName);
+      `
+    },
+    {
+      name: 'getRecordsById',
+      type: 'List<HistoryRecord>',
+      args: 'Context x, String daoKey, Object id, String propertyName',
+      javaCode: `
+        DAO dao = getHistoryDAO(x, daoKey)
+          .where(EQ(HistoryRecord.OBJECT_ID, id))
+          .orderBy(new Desc(HistoryRecord.TIMESTAMP));
+        return getRecords_(x, dao, propertyName);
+      `
+    },
+    {
+      name: 'getHistoryDAO',
+      visibility: 'private',
+      type: 'DAO',
+      args: 'Context x, String daoKey',
+      javaCode: `
+        DAO dao = (DAO) x.get(daoKey);
+
         if ( dao == null ) {
           throw new IllegalStateException("DAO does not exist");
         }
@@ -51,11 +83,23 @@ foam.CLASS({
         if ( ! HistoryRecord.class.isAssignableFrom(dao.getOf().getObjClass()) ) {
           throw new IllegalStateException("DAO must be of HistoryRecord");
         }
-    
-        List<HistoryRecord> records = ((ArraySink) dao.select(new ArraySink())).getArray();
+
+        return dao;
+      `
+    },
+    {
+      name: 'getRecords_',
+      visibility: 'private',
+      type: 'List<HistoryRecord>',
+      args: 'Context x, DAO dao, String propertyName',
+      javaCode: `
+        List<HistoryRecord> records = ((ArraySink) dao
+          .select(new ArraySink()))
+          .getArray();
+
         return records.stream()
           .filter(record -> hasPropertyUpdate(record, propertyName))
-          .toArray(HistoryRecord[]::new);
+          .collect(Collectors.toList());
       `
     },
     {
