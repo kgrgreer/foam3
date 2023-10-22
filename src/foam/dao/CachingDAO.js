b4afdc2dcf60ae5e2a3417bd5d24f1e5a942aa25
Kevin Greer <kgrgreer@gmail.com>
2023-10-22 18:55:51 -0400

    Fix bug in CachingDAO cmd_() doesn't return value.

--- src/foam/dao/CachingDAO.js
+++ src/foam/dao/CachingDAO.js
@@ -149,12 +149,15 @@ foam.CLASS({
     function cmd_(x, obj) {
       if ( foam.dao.DAO.PURGE_CMD === obj ) {
         this.onSrcReset();
-      } else if ( this.PurgeRecordCmd.isInstance(obj) ) {
+        return true;
+      }
+      if ( this.PurgeRecordCmd.isInstance(obj) ) {
         // REVIEW: this.cache is a dao not object, need to call dao.remove(obj)?
         delete this.cache[obj.id];
+        return true;
       }
 
-      this.SUPER(x, obj);
+      return this.src.cmd_(x, obj);
     }
   ],
 
