518a19413b7a3981b2d394293f78c965e54bad00
Kevin Greer <kgrgreer@gmail.com>
2023-10-24 13:04:09 -0400

    Fix GlobalPutAuthorizer for real.

---
+++ src/foam/nanos/auth/GlobalPutAuthorizer.java
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright 2023 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.nanos.auth;
+
+import foam.core.X;
+
+
+public class GlobalPutAuthorizer
+  extends StandardAuthorizer
+{
+
+  // This authorizer is an extension of the standardauthorizer that is meant
+  // to replace the the checkGlobalFind() method to allow any find().
+  // Useful for DAO's where the id's are unknown and unguessable,
+  // like token or invitation DAOs.
+
+  public GlobalPutAuthorizer(String permissionPrefix) {
+    super(permissionPrefix);
+  }
+
+  @Override
+  public void authorizeOnCreate(X x, foam.core.FObject obj) {
+    // NOP to enable put() of a new object
+  }
+
+  @Override
+  public void authorizeOnUpdate(X x, foam.core.FObject obj) {
+    // NOP to enable put() of an updated object
+  }
+}
