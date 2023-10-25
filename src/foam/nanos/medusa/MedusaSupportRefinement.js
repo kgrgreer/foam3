bdbfb282d3048317372ceb81cdb50b225e05d0d2
Joel Hughes <jlhughes@users.noreply.github.com>
2023-10-25 19:31:52 -0400

    Replace 'extends' refinement with actual method implementations. (#3021)
    Refinement of 'extends' is not supported.
    The refinement is added only when building Medusa proper.  Replaced
    extends with actual method overrides which call out to
    ClusterConfigSupport.

--- src/foam/nanos/medusa/MedusaSupportRefinement.js
+++ src/foam/nanos/medusa/MedusaSupportRefinement.js
@@ -8,7 +8,44 @@ foam.CLASS({
   package: 'foam.nanos.medusa',
   name: 'MedusaSupportRefinement',
   refines: 'foam.nanos.medusa.MedusaSupport',
-  extends: 'foam.nanos.medusa.ClusterConfigSupport',
 
-  documentation: 'Enabled by the main medusa pom.'
+  documentation: 'Enabled by the main medusa pom.',
+
+  javaImports: [
+    'foam.nanos.medusa.ClusterConfigSupport'
+  ],
+
+  methods: [
+    {
+      documentation: `check for primary as clusterable cron jobs should
+only run on the primary mediator.`,
+      name: 'cronEnabled',
+      args: 'Context x, boolean clusterable',
+      type: 'Boolean',
+      javaCode: `
+      ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+      return support.cronEnabled(x, clusterable);
+      `
+    },
+    {
+      documentation: 'test if system is replaying',
+      name: 'isReplaying',
+      args: 'Context x',
+      type: 'Boolean',
+      javaCode: `
+      ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+      return support.isReplaying(x);
+      `
+    },
+    {
+      documentation: `Decorate the root serviceDAO to 'cluster' serviceDAO updates`,
+      name: 'clusterServiceDAO',
+      args: 'Context x, foam.dao.DAO delegate',
+      type: 'foam.dao.DAO',
+      javaCode: `
+      ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+      return support.clusterServiceDAO(x, delegate);
+      `
+    }
+  ]
 })
