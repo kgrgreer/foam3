50fd962e995487095bf9492674c5f9dbd8cb5143
Kevin Greer <kgrgreer@gmail.com>
2023-11-11 18:46:30 -0400

    Initial check-in of ByServiceProviderFactory.

---
+++ src/foam/nanos/boot/ByServiceProviderFactory.java
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2023 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.nanos.boot;
+
+import foam.core.X;
+import foam.core.XFactory;
+
+/**
+ * An XFactory which returns a service from the supplied context which is
+ * either prefixed with the <spid>/ or just / if no spid is found or that
+ * service isn't found.
+ *
+ * The purpose of this is so that NSpec's can be scoped by SPID.
+ **/
+public class ByServiceProviderFactory
+  implements XFactory, NSpecAware
+{
+
+  protected NSpec spec_;
+
+  public ByServiceProviderFactory() {
+  }
+
+  // impl NSpecAware
+  public NSpec getNSpec() { return spec_; }
+
+  public void setNSpec(NSpec spec) { spec_ = spec; }
+
+  public void clearNSpec() { }
+
+  // impl XFactory
+  public Object create(X x) {
+    String spid = (String) x.get("spid");
+
+    if ( spid != null ) {
+      Object service = x.get(getNSpec().getName() + "_" + spid);
+
+      if ( service != null ) return service;
+    }
+
+    return x.get(getNSpec().getName() + "_");
+  }
+}
