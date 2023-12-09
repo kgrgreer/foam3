d317d8049d9b4a57311a9f7e9ec1f457ab1ee6d2
Chanmann Lim <chanmannlim@gmail.com>
2023-12-08 21:34:34 -0500

    Always show file browse input

--- src/foam/nanos/fs/fileDropZone/FileDropZone.js
+++ src/foam/nanos/fs/fileDropZone/FileDropZone.js
@@ -173,10 +173,10 @@ foam.CLASS({
           this.supportedFormats[type.toSummary()] = type.abbreviation;
         });
       }
-      var visibilitySlot = this.slot(function(hasFiles, isMultipleFiles, controllerMode) {
+      var visibilitySlot = this.slot(function(isMultipleFiles, controllerMode) {
           let cm = controllerMode != foam.u2.ControllerMode.VIEW;
           if ( isMultipleFiles && cm ) return true;
-          return ! hasFiles && cm;
+          return cm;
         });
 
       this.start('input')
