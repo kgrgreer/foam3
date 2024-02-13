0ab776691e0acde2be3f2076c155079495cae88f
Chanmann Lim <chanmannlim@gmail.com>
2024-02-13 10:34:29 -0500

    Remove dig prop names mapping

--- src/foam/nanos/dig/drivers/DigJsonDriver.js
+++ src/foam/nanos/dig/drivers/DigJsonDriver.js
@@ -135,7 +135,6 @@ foam.CLASS({
       }
 
       var formatter = getFormatter(x);
-      formatter = configurePropNamesMapping(x, formatter);
       formatter.output(fobjects.toArray());
 
       // Output the formatted data
@@ -150,7 +149,6 @@ foam.CLASS({
       if ( obj == null ) return;
 
       var formatter = getFormatter(x);
-      formatter = configurePropNamesMapping(x, formatter);
       formatter.output(obj);
 
       // Output the formatted data
@@ -171,7 +169,7 @@ foam.CLASS({
     },
     {
       name: 'getFormatter',
-      type: 'JSONFObjectFormatter',
+      type: 'FObjectFormatter',
       args: 'Context x',
       javaCode: `
         var formatter = formatter_.get();
@@ -193,20 +191,6 @@ foam.CLASS({
 
         value = value.trim().toLowerCase();
         return "false".equals(value) || "f".equals(value) || "0".equals(value) || "no".equals(value) || "n".equals(value) || "off".equals(value);
-      `
-    },
-    {
-      name: 'configurePropNamesMapping',
-      type: 'JSONFObjectFormatter',
-      args: 'Context x, JSONFObjectFormatter formatter',
-      javaCode: `
-        var p = x.get(HttpParameters.class);
-        if ( p.getParameter("nameMapping") != null ) {
-          var ret = parseMap(p.getParameter("nameMapping"));
-          if ( ret.value() != null )
-            return formatter.setPropNamesMapping((Map) ret.value());
-        }
-        return formatter;
       `
     }
   ]
