484a061c3acff4943f8d9e328da35f8eba20edee
Chanmann Lim <chanmannlim@gmail.com>
2024-03-08 15:34:26 -0500

    Fix required property validation

--- src/foam/core/PropertyInfo.js
+++ src/foam/core/PropertyInfo.js
@@ -114,7 +114,7 @@ foam.INTERFACE({
     'boolean containsDeletablePII() { return false; }',
     `void validateObj(foam.core.X x, foam.core.FObject obj) {
        /* Template Method: override in subclass if required. */
-       if ( getRequired() && ! isSet(obj) ) {
+       if ( getRequired() && (! isSet(obj) || isDefaultValue(obj)) ) {
          throw new ValidationException(getName() + " required");
        }
     }`,
