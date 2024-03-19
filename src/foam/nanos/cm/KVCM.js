2c212d39d933024f016849891f63f7dea5a1cd4c
xuerongNanopay <xuerong@nanopay.net>
2024-03-19 14:38:36 -0400

    add KVCM and SeriesCM

---
+++ src/foam/nanos/cm/KVCM.js
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.cm',
+  name: 'KVCM',
+  extends: 'foam.nanos.cm.CM',
+  documentation: `
+    Handle Key-Value type of results from Code/Script.
+  `
+})
---
+++ src/foam/nanos/cm/SeriesCM.js
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.cm',
+  name: 'SeriesCM',
+  extends: 'foam.nanos.cm.CM',
+  documentation: `
+    Handle Series type of results from Code/Script.
+  `
+})
