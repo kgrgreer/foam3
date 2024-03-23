2794a267af58f43c39615ab7cec8b77d50668f8d
Kevin Greer <kgrgreer@gmail.com>
2024-03-23 14:14:54 -0400

    Replace DAOConfigSummaryView with DataManagement in POM.

--- src/foam/nanos/pom.js
+++ src/foam/nanos/pom.js
@@ -252,7 +252,7 @@ foam.POM({
     { name: "benchmark/AuthorizerBenchmark",                                              flags: "js|java" },
     { name: "boot/NSpec",                                                                 flags: "js|java" },
     { name: "boot/NSpecAware",                                                            flags: "js|java" },
-    { name: "boot/DAOConfigSummaryView",                                                  flags: "web" },
+    { name: "boot/DataManagement",                                                  flags: "web" },
     { name: "boot/DAONSpecMenu",                                                          flags: "js|java" },
     { name: "session/GroupResetSessionRuleAction",                                        flags: "js|java" },
     { name: "session/LocalSetting",                                                       flags: "js|java" },
