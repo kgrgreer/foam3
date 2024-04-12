1b6cc3e09a982b4b82937478c3f7f6382d8dd85d
Joel Hughes <joel@nanopay.net>
2024-04-12 15:52:57 -0400

    update to save previous translation text and replace it on deactivate

--- src/foam/nanos/so/MessageTask.js
+++ src/foam/nanos/so/MessageTask.js
@@ -22,7 +22,11 @@ foam.CLASS({
 
   javaImports: [
     'foam.core.X',
+    'foam.dao.DAO',
+    'foam.i18n.Locale',
     'foam.i18n.TranslationService',
+    'static foam.mlang.MLang.*',
+    'foam.nanos.logger.Loggers',
     'foam.util.SafetyUtil'
   ],
 
@@ -60,34 +64,96 @@ foam.CLASS({
   methods: [
     {
       name: 'updateTranslation',
-      args: 'X x',
-      javaType: 'String',
+      args: 'X x, String source, String replacement',
       javaCode: `
-        var msg = getMessageKeyText();
-        if ( x == null || SafetyUtil.isEmpty(msg) ) return msg;
+        if ( x == null || SafetyUtil.isEmpty(source) ) return;
+        if ( SafetyUtil.isEmpty(replacement) ) return;
 
         var ts = (TranslationService) x.get("translationService");
-        if ( ts == null ) return msg;
+        if ( ts == null ) return;
 
-// TODO: get existing message to save
         var locale = (String) x.get("locale.language");
         if ( SafetyUtil.isEmpty(locale) ) locale = "en";
 
-        return ts.getTranslation(locale, msg, msg);
+        setSavedText(ts.getTranslation(locale, source, source));
+        setTranslation(x, locale, source, replacement);
       `
     },
     {
       name: 'activate',
       args: 'X x',
       javaCode: `
-        this.setReplacementText(updateTranslation(x));
+        updateTranslation(x, getMessageKeyText(), getReplacementText());
       `
     },
     {
       name: 'deactivate',
       args: 'X x',
       javaCode: `
-        this.clearProperty("replacementText");
+        updateTranslation(x, getMessageKeyText(), getSavedText());
+        clearProperty("replacementText");
+      `
+    },
+    {
+      name: 'setTranslation',
+      args: 'X x, String locale, String source, String replacement',
+      javaCode: `
+        if ( SafetyUtil.isEmpty(source) ||
+             SafetyUtil.isEmpty(replacement) )
+          return;
+
+        if ( SafetyUtil.isEmpty(locale) ) {
+          locale = (String) x.get("locale.language");
+          if ( foam.util.SafetyUtil.isEmpty(locale) ) {
+            locale = "en";
+          }
+        }
+
+        DAO localeDAO = (DAO) getX().get("localeDAO");
+
+        // locale - determines locale type (en, fr, es …etc)
+        // variant - Locale variation (CA for en-CA, CA for fr-CA, AT for de_AT …etc)
+        String variant     = "";
+        Locale localeEntry = null;
+
+        // note: 'locale' passed to the function may be in locale format or in local variant format
+        // so we need to check which format is used and parse it if necessary.
+        boolean hasVariant = locale.contains("-");
+        if ( hasVariant ) {
+          variant = locale.substring(3).toUpperCase();
+          locale  = locale.substring(0,2).toLowerCase();
+
+          // search for locale and variant
+          localeEntry = (Locale) localeDAO.find(
+            AND(
+              EQ(Locale.SOURCE,  source),
+              EQ(Locale.LOCALE,  locale),
+              EQ(Locale.VARIANT, variant)
+            )
+          );
+        } else {
+          locale = locale.toLowerCase();
+        }
+
+        // search for locale with no variant
+        if ( localeEntry == null ) {
+          localeEntry = (Locale) localeDAO.find(
+            AND(
+              EQ(Locale.SOURCE,  source),
+              EQ(Locale.LOCALE,  locale),
+              EQ(Locale.VARIANT, "")
+            )
+          );
+        }
+
+        if ( localeEntry != null ) {
+          localeEntry = (Locale) localeEntry.fclone();
+          localeEntry.setTarget(replacement);
+          localeEntry.setNote("MessageText replacement");
+          localeDAO.put(localeEntry);
+        } else {
+          Loggers.logger(x, this).warning("Translation not found", source);
+        }
       `
     }
   ]
