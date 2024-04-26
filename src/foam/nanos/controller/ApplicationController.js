19a00ad15e4debde09270de5c6bc5b1afe7cb176
sarthak <sarthak@nanopay.net>
2024-04-26 13:34:49 -0400

    prefer current menu when using route to dao

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -1027,10 +1027,18 @@ foam.CLASS({
       this.window.location.hash = link;
     },
     async function routeToDAO(dao, id) {
+      // Check if current menu has object
+      if ( id && foam.nanos.menu.DAOMenu2.isInstance(this.currentMenu.handler) ) {
+        var result = await this.currentMenu.handler.config.dao.find(id);
+        if ( result ) {
+          this.routeTo(this.currentMenu.id + '/' + id);
+          return;
+        }
+      }
       // Finds the correct menu for a given dao and optionally an object
       let menuDAOs = (await this.__subContext__.menuDAO.select())
         .array?.filter(v => foam.nanos.menu.DAOMenu2.isInstance(v.handler));
-      menuDAOs = menuDAOs.filter(m => m.handler.config.dao.of == dao.of);
+      menuDAOs = menuDAOs.filter(m => m.handler.config.dao.of?.isSubClass(dao.of) );
       if ( ! id ) {
         return this.routeTo(menuDAOs[0].id);
       }
