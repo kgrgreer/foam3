d4952c09c15f676dc8a290edec2f9b7ad548349a
Joel Hughes <joel@nanopay.net>
2024-05-01 12:38:51 -0400

    Adjust view and border to allow injection of additional content

--- src/foam/nanos/referral/ReferUserView.js
+++ src/foam/nanos/referral/ReferUserView.js
@@ -79,18 +79,10 @@ foam.CLASS({
     'subject'
   ],
 
-   messages: [
+  messages: [
      { name: 'HEADER_1', message: 'Share ${appName} With a Friend!', template: true },
-     { name: 'HEADER_2', message: 'You both get payment credits when your friend completes their first transaction'},
-     { name: 'ITEM_1', message: 'You get ${amount}', template: true },
-     { name: 'SUB_ITEM_1', message: 'On your next transaction'},
-     { name: 'ITEM_2', message: 'They get ${amount}', template: true },
-     { name: 'SUB_ITEM_2', message: 'On their second transaction'},
      { name: 'COPY_FAIL', message: 'Failed to copy!' },
-     { name: 'COPYTEXT', message: 'Refer a friend' },
-     //TODO: AMOUNTS SHOULD BE FETCHED FROM CREDIT CODES
-     { name: 'AMOUNT_1', message: '$10.00' },
-     { name: 'AMOUNT_2', message: '$10.00' }
+     { name: 'COPYTEXT', message: 'Refer a friend' }
    ],
 
   properties: [
@@ -119,39 +111,9 @@ foam.CLASS({
 
       let button = navigator.canShare?.({text: this.referralText}) ? this.SHARE_TEXT : this.COPY_TEXT;
       this
-          .addClass(this.myClass('container'))
-
-          .start()
-            .addClass(this.myClass('header'))
-            .start().addClass('h300').add(this.HEADER_1({ appName: this.theme.appName })).end()
-            .start().addClass('p').add(this.HEADER_2).end()
-          .end()
-          .start()
-            .addClass(this.myClass('item'))
-            .tag(foam.u2.tag.CircleIndicator, {
-              ...iconConfig,
-              icon: this.theme?.glyphs.dollar?.getDataUrl({ fill: fill }),
-              label: this.theme?.glyphs.dollar ? null : '1'
-            })
-            .start()
-              .start().addClass('h400').add(this.ITEM_1({ amount: this.AMOUNT_1 })).end()
-              .start().addClass('p').add(this.SUB_ITEM_1).end()
-            .end()
-          .end()
-          .start()
-            .addClass(this.myClass('item'))
-            .tag(foam.u2.tag.CircleIndicator, {
-              ...iconConfig,
-              icon: this.theme?.glyphs.gift?.getDataUrl({ fill: fill }),
-              label: this.theme.glyphs.gift ? null : '2'
-            })
-            .start()
-              .start().addClass('h400').add(this.ITEM_2({ amount: this.AMOUNT_2 })).end()
-              .start().addClass('p').add(this.SUB_ITEM_2).end()
-            .end()
-          .end()
-        
-          .start()
+        .addClass(this.myClass('container'))
+        .call(this.addContent())
+        .start()
             .addClass(this.myClass('copy-box'))
             .enableClass(this.myClass('error'), this.refLink$.map(v => ! v))
             .add(this.dynamic(function(refLink) {
@@ -169,6 +131,12 @@ foam.CLASS({
           .startContext({data:this})
             .start(button, { buttonStyle: 'PRIMARY', size: 'LARGE' }).addClass(this.myClass('share-button')).end()
           .endContext()
+    },
+    {
+      name: 'addContent',
+      code: function() {
+        // nop
+      }
     }
   ],
   actions: [
