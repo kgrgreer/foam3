72869e21e56a2b1a41488b68f5200ec18f759f84
sarthak-marwaha <sarthakmarwaha@gmail.com>
2024-05-16 10:33:15 -0400

    Only reload stash the first time client loads

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -475,6 +475,9 @@ foam.CLASS({
         if ( self.client != client ) {
           console.log('Stale Client in ApplicationController, waiting for update.');
           await self.client.promise;
+          // Rebuild stack with correct context 
+          self.stack = self.Stack.create({}, self.__subContext__);
+          self.routeTo(window.location.hash.substring(1));
         }
 
         await self.fetchGroup();
@@ -735,8 +738,6 @@ foam.CLASS({
     },
 
     async function pushMenu_(realMenu, menu) {
-      let idCheck = menu && ( menu.id ? menu.id : menu );
-      let currentMenuCheck = this.currentMenu?.id;
       var realMenu = menu;
       dao = this.client.menuDAO;
       let stringMenu = menu && foam.String.isInstance(menu);
