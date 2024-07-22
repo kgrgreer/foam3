0735e48f3c85ae4ddbe37451343b5b98c5419b66
noodlemoodle <qiyzhang@icloud.com>
2024-07-22 11:57:08 -0700

    move user profile creation to rule

--- src/foam/nanos/analytics/mixpanel/MixpanelMessageDAO.js
+++ src/foam/nanos/analytics/mixpanel/MixpanelMessageDAO.js
@@ -15,10 +15,10 @@ foam.CLASS({
     'com.mixpanel.mixpanelapi.MixpanelAPI',
 
     'foam.dao.DAO',
+    'foam.nanos.analytics.AnalyticEvent',
     'foam.nanos.auth.AuthService',
     'foam.nanos.auth.User',
     'foam.nanos.logger.Loggers',
-    'foam.nanos.session.Session',
     'foam.util.SafetyUtil',
 
     'java.io.IOException',
@@ -39,40 +39,14 @@ foam.CLASS({
       name: 'put_',
       javaCode: `
         AnalyticEvent event = (AnalyticEvent) super.put_(x, obj);
-        Session session = (Session) ((DAO) x.get("sessionDAO")).find(event.getSessionId());
+        String trackingId = event.getSessionId();
 
         MessageBuilder messageBuilder = new MessageBuilder(this.PROJECT_TOKEN);
         MixpanelAPI mixpanel = new MixpanelAPI();
 
-        // set user
-        AuthService auth = (AuthService) x.get("auth");
-        User user = session.findUserId(x);
-        var isAdmin = user != null
-            && (user.getId() == User.SYSTEM_USER_ID
-            || user.getGroup().equals("admin")
-            || user.getGroup().equals("system"));
-        var isAnonymous = user != null && auth.isUserAnonymous(x, user.getId());
-
-        String trackingId = session.getId();
-        if ( user != null && ! isAdmin && ! isAnonymous ) {
-          JSONObject userProps = new JSONObject();
-          userProps.put("$id", trackingId);
-          userProps.put("$email", user.getEmail());
-          userProps.put("$userId", user.getId());
-          JSONObject update = messageBuilder.set(Long.toString(user.getId()), userProps);
-
-          try {
-            mixpanel.sendMessage(update);
-          } catch (IOException e) {
-            Loggers.logger(x, this).error("Failed sending user data:", event.getId(), "Can't communicate with Mixpanel");
-          }
-        }
-
         // build message
-        // var distinctId = SafetyUtil.isEmpty(trackingId) ? session.getId() : trackingId;
         messageBuilder = new MessageBuilder(this.PROJECT_TOKEN);
         JSONObject props = new JSONObject(event.toJSON());
-        props.put("sessionId", session.getId());
         JSONObject sentEvent = messageBuilder.event(trackingId, event.getName(), props);
 
         ClientDelivery delivery = new ClientDelivery();
