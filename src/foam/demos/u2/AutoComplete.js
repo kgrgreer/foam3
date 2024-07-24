4c6e3df1c3f5c71023817d69e4e1e277e43edeaf
Kevin Greer <kgrgreer@gmail.com>
2024-07-24 20:08:25 -0300

    Start of autocomplete demo/test.

---
+++ src/foam/demos/u2/AutoComplete.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2016 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  name: 'AutoComplete',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'str1'
+    },
+    {
+      class: 'String',
+      name: 'str2',
+      view: 'foam.u2.view.TextField'
+    }
+  ]
+});
