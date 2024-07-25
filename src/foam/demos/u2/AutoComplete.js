00115eb2dce61e9efb051ac26c0316d6ee336bed
Kevin Greer <kgrgreer@gmail.com>
2024-07-25 09:47:53 -0300

    Work on AutoComplete demo.

--- src/foam/demos/u2/AutoComplete.js
+++ src/foam/demos/u2/AutoComplete.js
@@ -7,15 +7,31 @@
 foam.CLASS({
   name: 'AutoComplete',
 
+  requires: [ 'foam.dao.EasyDAO' ],
+
   properties: [
     {
       class: 'String',
-      name: 'str1'
+      name: 'str1',
+      units: 'units'
     },
     {
       class: 'String',
       name: 'str2',
-      view: 'foam.u2.view.TextField'
+      view: 'foam.u2.view.TextField',
+      units: 'units'
+    },
+    {
+      class: 'String',
+      name: 'str3',
+      view: function(_, x) { return { class: 'foam.u2.view.TextField', autocompleter: x.data }; },
+      units: 'units'
+    },
+    foam.demos.heroes.Controller.HERO_DAO,
+    {
+      name: 'dao',
+      hidden: true,
+      factory: foam.demos.heroes.Controller.HERO_DAO.factory
     }
   ]
 });
