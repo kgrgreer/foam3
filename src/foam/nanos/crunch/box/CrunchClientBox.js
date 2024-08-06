6a04589b3576fa4174d5850b933709199ead61d5
Chanmann Lim <chanmannlim@gmail.com>
2024-08-06 16:53:27 -0400

    Indent

--- src/foam/nanos/crunch/box/CrunchClientBox.js
+++ src/foam/nanos/crunch/box/CrunchClientBox.js
@@ -3,8 +3,8 @@
  * Copyright 2020 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
- 
- foam.CLASS({
+
+foam.CLASS({
   package: 'foam.nanos.crunch.box',
   name: 'CrunchClientBox',
   extends: 'foam.box.ProxyBox',
--- src/foam/u2/wizard/wao/XORMinMaxWAO.js
+++ src/foam/u2/wizard/wao/XORMinMaxWAO.js
@@ -4,7 +4,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
- foam.CLASS({
+foam.CLASS({
   package: 'foam.u2.wizard.wao',
   name: 'XORMinMaxWAO',
   implements: [ 'foam.u2.wizard.wao.WAO' ],
