6990ba6b7a9e5c980d3dfa5a44fd72c3ba722a82
noodlemoodle <qiyzhang@icloud.com>
2024-08-15 15:35:45 -0700

    add logvisitagent

---
+++ src/foam/nanos/analytics/LogVisitAgent.js
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.analytics',
+  name: 'LogVisitAgent',
+  documentation: `
+    log an analyticevent when certain urlsearchparams are present
+  `,
+
+  methods: [
+    function init() {
+      this.logVisit();
+    },
+    function logVisit() {
+      if ( ! window.location.search ) return;
+      extras = Object.fromEntries(
+        [...(new URLSearchParams(window.location.search))]
+          .filter(
+            ([key]) => key.startsWith('utm_') || key == 'referral'
+          )
+      );
+      this.__subContext__.analyticEventDAO.put(
+        foam.nanos.analytics.AnalyticEvent.create({
+          name: 'REFERRAL_OR_AD_CAMPAIGN_VISIT',
+          extra: foam.json.stringify(extras)
+        }),
+        this);
+      localStorage['visited'] = window.location.search;
+    }
+  ]
+});
