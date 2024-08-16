fc5b0afec232a4f1f3b1546993e5b0c63c0a1404
noodlemoodle <qiyzhang@icloud.com>
2024-08-16 11:05:19 -0700

    user listener and import window

--- src/foam/nanos/analytics/LogVisitAgent.js
+++ src/foam/nanos/analytics/LogVisitAgent.js
@@ -11,25 +11,37 @@ foam.CLASS({
     log an analyticevent when certain urlsearchparams are present
   `,
 
+  imports: [
+    'window'
+  ],
+
   methods: [
     function init() {
       this.logVisit();
-    },
-    function logVisit() {
-      if ( ! window.location.search ) return;
-      extras = Object.fromEntries(
-        [...(new URLSearchParams(window.location.search))]
-          .filter(
-            ([key]) => key.startsWith('utm_') || key == 'referral'
-          )
-      );
-      this.__subContext__.analyticEventDAO.put(
-        foam.nanos.analytics.AnalyticEvent.create({
-          name: 'REFERRAL_OR_AD_CAMPAIGN_VISIT',
-          extra: foam.json.stringify(extras)
-        }),
-        this);
-      localStorage['visited'] = window.location.search;
+    }
+  ],
+
+  listeners: [
+    {
+      name: 'logVisit',
+      isMerged: true,
+      mergeDelay: 20000,
+      code: function() {
+        if ( ! this.window.location.search ) return;
+        extras = Object.fromEntries(
+          [...(new URLSearchParams(this.window.location.search))]
+            .filter(
+              ([key]) => key.startsWith('utm_') || key == 'referral'
+            )
+        );
+        this.__subContext__.analyticEventDAO.put(
+          foam.nanos.analytics.AnalyticEvent.create({
+            name: 'REFERRAL_OR_AD_CAMPAIGN_VISIT',
+            extra: foam.json.stringify(extras)
+          }),
+          this);
+        localStorage['visited'] = this.window.location.search;
+      }
     }
   ]
 });
