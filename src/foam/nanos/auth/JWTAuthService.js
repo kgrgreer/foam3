dd624c0b353fd0090906f4d7999e0ecc89b6ade7
Adam Van Ymeren <adam@vany.ca>
2024-08-19 12:29:56 -0700

    actually validate jwk

--- src/foam/nanos/auth/JWTAuthService.js
+++ src/foam/nanos/auth/JWTAuthService.js
@@ -6,6 +6,7 @@ foam.CLASS({
         'foam.nanos.auth.AuthService auth',
         'foam.dao.DAO localUserDAO',
         'foam.nanos.auth.UniqueUserService uniqueUserService',
+        'foam.dao.DAO jwkDAO',
     ],
     methods: [
         {
@@ -25,11 +26,13 @@ foam.CLASS({
             logger.log("jwt parts", parts.length);
             String headerb64 = parts[0];
             String bodyb64 = parts[1];
+            String signatureb64 = parts[2];
             
             logger.log("header", headerb64, "body", bodyb64);
             
             byte[] headerBytes = java.util.Base64.getUrlDecoder().decode(headerb64);
             byte[] bodyBytes = java.util.Base64.getUrlDecoder().decode(bodyb64);
+            byte[] signatureBytes = java.util.Base64.getUrlDecoder().decode(signatureb64);
             
             String header = new String(headerBytes, java.nio.charset.StandardCharsets.UTF_8);
             String body = new String(bodyBytes, java.nio.charset.StandardCharsets.UTF_8);
@@ -41,9 +44,56 @@ foam.CLASS({
             reader = javax.json.Json.createReader(new java.io.StringReader(body));
             javax.json.JsonObject bodyObject = reader.readObject();
             reader.close();
-            
-            // TODO validate JWT
 
+            java.security.Signature signature;
+            
+            try {
+                switch (headerObject.getString("alg")) {
+                    case "RS256":
+                        signature = java.security.Signature.getInstance("SHA256withRSA");
+                        String keyId = headerObject.getString("kid");
+                        String issuer = bodyObject.getString("iss");
+                        
+                        RSA256JWK jwk = (RSA256JWK)(getJwkDAO().find(new JWKId(keyId, issuer)));
+                        if (jwk == null) {
+                          throw new AuthenticationException("no key found for jwt");
+                        }
+                        if (!jwk.getTrusted()) {
+                          throw new AuthenticationException("jwt key untrusted");
+                        }
+                        
+                        // Base64 URL decode the modulus and exponent
+                        byte[] modulusBytes = java.util.Base64.getUrlDecoder().decode(jwk.getN());
+                        byte[] exponentBytes = java.util.Base64.getUrlDecoder().decode(jwk.getE());
+    
+                        // Convert to BigInteger
+                        java.math.BigInteger modulus = new java.math.BigInteger(1, modulusBytes);
+                        java.math.BigInteger exponent = new java.math.BigInteger(1, exponentBytes);
+    
+                        // Create a KeySpec from modulus and exponent
+                        java.security.spec.RSAPublicKeySpec keySpec = new java.security.spec.RSAPublicKeySpec(modulus, exponent);
+    
+                        java.security.PublicKey publicKey = java.security.KeyFactory.getInstance("RSA")
+                                .generatePublic(keySpec);
+    
+                        signature.initVerify(publicKey);
+                        break;
+                    default:
+                        throw new AuthenticationException("Unsupported signature algorithm");
+                }
+            } catch (java.security.NoSuchAlgorithmException | java.security.spec.InvalidKeySpecException | java.security.InvalidKeyException e) {
+              throw new AuthenticationException("Unsupported signature algorithm", e);
+            }
+     
+            try {
+                signature.update((headerb64 + "." + bodyb64).getBytes(java.nio.charset.StandardCharsets.UTF_8));
+            
+                if (!signature.verify(signatureBytes)) {
+                    throw new AuthenticationException("JWT signature does not match");
+                }
+            } catch (java.security.SignatureException e) {
+              throw new AuthenticationException("error validating signature", e);
+            }
                         
             if (!bodyObject.getBoolean("email_verified")) {
                 throw new AuthenticationException("email is not verified");
