0bd404b51b2641e5675f4c93830c310ea5d3bfd2
xuerongNanopay <xuerong@nanopay.net>
2024-08-28 13:56:21 -0400

    add http call

--- src/foam/nanos/place/GooglePlaceService.js
+++ src/foam/nanos/place/GooglePlaceService.js
@@ -21,9 +21,17 @@ foam.CLASS({
     'foam.nanos.logger.Loggers',
     'foam.nanos.place.model.*',
     'foam.nanos.place.configure.GooglePlaceServiceConfigure',
+    'org.apache.http.impl.client.HttpClients',
     'org.apache.http.client.utils.URIBuilder',
     'java.util.Arrays',
     'java.util.ArrayList',
+    'org.apache.http.HttpEntity',
+    'org.apache.http.HttpHeaders',
+    'org.apache.http.client.methods.CloseableHttpResponse',
+    'org.apache.http.client.methods.HttpGet',
+    'org.apache.http.impl.client.CloseableHttpClient',
+    'org.apache.http.impl.client.HttpClients',
+    'org.apache.http.util.EntityUtils',
   ],
 
   methods: [
@@ -51,6 +59,22 @@ foam.CLASS({
         try {
           var config = getConfigure(x);
           var input = req.getAddress1() +  ", " + req.getAddress2() + ", " + req.getCity() + ", " + req.getRegion() + ", " + req.getCountry() + ", " + req.getPostalCode();
+
+          var uri = new URIBuilder("https://maps.googleapis.com/maps/api/place/autocomplete/json")
+                      .addParameter("input", input)
+                      .addParameter("key", config.getApiKey());
+
+          HttpGet request = new HttpGet(uri.toString());
+
+          try (CloseableHttpClient httpClient = HttpClients.createDefault();
+              CloseableHttpResponse response = httpClient.execute(request)) {
+
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                String rawResponse = EntityUtils.toString(entity);
+            }
+
+          }
         } catch ( Exception e ) {
           Loggers.logger(x, this).error("placeAutocomplete", e);
         }
