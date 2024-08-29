ae400de4e6a925723d5fbde8777eb78f7ef90db9
xuerongNanopay <xuerong@nanopay.net>
2024-08-29 13:40:56 -0400

    reindent line

--- src/foam/nanos/place/model/PlaceDetailResult.js
+++ src/foam/nanos/place/model/PlaceDetailResult.js
@@ -1,24 +1,24 @@
 /**
- * @license
- * Copyright 2024 The FOAM Authors. All Rights Reserved.
- * http://www.apache.org/licenses/LICENSE-2.0
- */
+* @license
+* Copyright 2024 The FOAM Authors. All Rights Reserved.
+* http://www.apache.org/licenses/LICENSE-2.0
+*/
 
 foam.CLASS({
   package: 'foam.nanos.place.model',
   name: 'PlaceDetailResult',
-
+  
   properties: [
     {
       name: "formattedAddress",
       shortName: "formatted_address",
       class: "String"
     },
-		{
-			name: "addressComponents",
+    {
+      name: "addressComponents",
       shortName: 'address_components',
-			class: "FObjectArray",
-			of: "foam.nanos.place.model.PlaceDetailAddressComponent"
-		}
+      class: "FObjectArray",
+      of: "foam.nanos.place.model.PlaceDetailAddressComponent"
+    }
   ]
 })
