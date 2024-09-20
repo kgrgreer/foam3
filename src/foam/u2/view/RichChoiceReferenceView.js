52ca8a3bf9544e87ad5e0f705488166b6a7b0ad5
Kevin Greer <kgrgreer@gmail.com>
2024-09-20 16:17:12 -0400

    Fix RichChoiceReferenceView so that it uses Reference Property name to find DAO.

--- src/foam/u2/view/RichChoiceReferenceView.js
+++ src/foam/u2/view/RichChoiceReferenceView.js
@@ -27,10 +27,11 @@ foam.CLASS({
   methods: [
     function fromProperty(prop) {
       if ( this.sections?.length == 0 ) {
-        let dao = this.__context__[prop.targetDAOKey] || this.__context__[foam.String.daoize(prop.of.name)];
+        // name$dao is added by foam.core.Reference
+        let dao = this.__context__[prop.targetDAOKey] || this.__context__[foam.String.daoize(prop.of.name)] || this.__context__.data[prop.name + '$dao'];
         this.sections = [ this.RichChoiceViewSection.create({ heading: (prop.of?.model_?.plural || 'Selections'), dao: dao }) ];
       }
-      this.SUPER(prop);
+      this.SUPER(prop); // has to go second
     }
   ]
 });
