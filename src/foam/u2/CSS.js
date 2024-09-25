kgrgreer/foam3

commit c92343c058b0e7500e8cc02b3acf54d4e460cf02

Fix for inheritCSS Element attribute.

src/foam/u2/CSS.js
@@ -61,31 +61,14 @@ foam.CLASS({
           ( opt_parent.__subContext__ || opt_parent.__context__ || opt_parent ) :
           foam.__context__;
 
-        // Now call through to the original create
+        // Call through to the original create
         try {
-          return oldCreate.call(this, args, X);
+          return oldCreate.call(this, args, cls.model_.inheritCSS ? X : X.createSubContext({skipCSSInstall: true}));
         } finally {
-          // if a class has inheritCSS: false then finish installing its other
-          // CSS axioms, but prevent any parent classes from installing theirs
-          // We put this in the context to communicate to other CSSAxioms
-          // down the chain. The last/first one will revert back to the original
-          // X so that objects aren't created with lastClassToInstallCSSFor
-          // in their contexts.
-          var lastClassToInstallCSSFor = X.lastClassToInstallCSSFor;
-
-          if ( ! lastClassToInstallCSSFor || lastClassToInstallCSSFor == cls ) {
+          if ( ! X.skipCSSInstall ) {
             // Install CSS if not already installed in this document for this cls
             axiom.maybeInstallInDocument(X, this);
           }
-
-          if ( ! lastClassToInstallCSSFor && ! this.model_.inheritCSS ) {
-            X = X.createSubContext({
-              lastClassToInstallCSSFor: this,
-              originalX: X
-            });
-          }
-
-          if ( lastClassToInstallCSSFor && isFirstCSS ) X = X.originalX;
         }
       };
     },

commit 84baf9c74e8643feafea94031fa4403585a54e39

Fix for inheritCSS which was broken in c92343c058b0e7500e8cc02b3acf54d4e460cf02.

src/foam/u2/CSS.js
@@ -50,22 +50,22 @@ foam.CLASS({
 
     function installInClass(cls) {
       // Install myself in this Window, if not already there.
-      var oldCreate   = cls.create;
-      var axiom       = this;
-      var isFirstCSS  = ! cls.private_.hasCSS;
+      var oldCreate  = cls.create;
+      var axiom      = this;
+      var isFirstCSS = ! cls.private_.hasCSS;
 
       if ( isFirstCSS ) cls.private_.hasCSS = true;
 
-      cls.create = function(args, opt_parent) {
+      cls.create = function(args, opt_parent, opt_skipCSS) {
         var X = opt_parent ?
           ( opt_parent.__subContext__ || opt_parent.__context__ || opt_parent ) :
           foam.__context__;
 
         // Call through to the original create
         try {
-          return oldCreate.call(this, args, cls.model_.inheritCSS ? X : X.createSubContext({skipCSSInstall: true}));
+          return oldCreate.call(this, args, X, opt_skipCSS || ! cls.model_.inheritCSS);
         } finally {
-          if ( ! X.skipCSSInstall ) {
+          if ( ! opt_skipCSS ) {
             // Install CSS if not already installed in this document for this cls
             axiom.maybeInstallInDocument(X, this);
           }