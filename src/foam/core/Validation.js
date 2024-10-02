6cef7feffe16e4767818dbbdafa539f67acd8c8a
Kevin Greer <kgrgreer@gmail.com>
2024-10-02 12:47:31 +0200

    Comment out DatePropertyValidationRefinement because it doesn't appear to work.

--- src/foam/core/Validation.js
+++ src/foam/core/Validation.js
@@ -595,7 +595,7 @@ foam.CLASS({
   ]
 });
 
-
+/*
 foam.CLASS({
   package: 'foam.core',
   name: 'DatePropertyValidationRefinement',
@@ -607,10 +607,11 @@ foam.CLASS({
       of: 'foam.core.ValidationPredicate',
       name: 'internalValidationPredicates',
       factory: function() {
+        var query = 'thisValue !exists||thisValue<=' + foam.Date.MAX_DATE.toISOString().slice(1,16) + '&&thisValue>=' + foam.Date.MIN_DATE.toISOString().slice(0,16);
         return [
           {
             args: [ this.name ],
-            query: 'thisValue !exists||thisValue<=' + foam.Date.MAX_DATE.toISOString().slice(1,16) + '&&thisValue>=' + foam.Date.MIN_DATE.toISOString().slice(0,16),
+            query: query,
             errorString: 'Invalid date value'
           }
         ];
@@ -618,6 +619,7 @@ foam.CLASS({
     }
   ]
 });
+*/
 
 
 foam.CLASS({
