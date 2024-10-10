1ade202870c448a72d5b5dee7ea6e434a0032f10
Kevin Greer <kgrgreer@gmail.com>
2024-10-10 10:22:50 -0400

    Add POM for examples demo. Add code editor example.

---
+++ src/foam/demos/examples/pom.js
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.POM({
+  name: "fbe",
+  projects: [
+    { name: '../../../../foam3/pom' }
+  ],
+  files: [
+    { name: 'FBE' }
+  ]
+});
